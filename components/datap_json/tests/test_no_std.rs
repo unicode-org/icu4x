727f40b886226c552bd70bfeb02c215a096e682f
Shane F. Carr <shane@unicode.org>
2020-05-07 01:20:08 -0500

    Running cargo fmt

--- components/datap_json/tests/test_no_std.rs
+++ components/datap_json/tests/test_no_std.rs
@@ -1,7 +1,7 @@
 extern crate icu_datap_json;
 
-use std::prelude::v1::*;
 use std::borrow::Cow;
+use std::prelude::v1::*;
 
 use icu_datap_json::JsonDataProvider;
 use icu_util::datap;
@@ -19,20 +19,25 @@ const DATA: &'static str = r#"{
 
 fn get_response() -> datap::Response {
     let json_data_provider = JsonDataProvider::from_str(DATA).unwrap();
-    return json_data_provider.load(datap::Request {
-        locale: "root".to_string(),
-        category: datap::Category::Decimal,
-        key: datap::Key::Decimal(datap::decimal::Key::SymbolsV1),
-        payload: None
-    }).unwrap();
+    return json_data_provider
+        .load(datap::Request {
+            locale: "root".to_string(),
+            category: datap::Category::Decimal,
+            key: datap::Key::Decimal(datap::decimal::Key::SymbolsV1),
+            payload: None,
+        })
+        .unwrap();
 }
 
 fn check_data(decimal_data: &datap::decimal::SymbolsV1) {
-    assert_eq!(decimal_data, &datap::decimal::SymbolsV1 {
-        zero_digit: '0',
-        decimal_separator: ".".to_string(),
-        grouping_separator: ",".to_string(),
-    });
+    assert_eq!(
+        decimal_data,
+        &datap::decimal::SymbolsV1 {
+            zero_digit: '0',
+            decimal_separator: ".".to_string(),
+            grouping_separator: ",".to_string(),
+        }
+    );
 }
 
 #[test]
@@ -52,7 +57,6 @@ fn test_borrow_payload_mut() {
 #[test]
 fn test_take_payload() {
     let response = get_response();
-    let decimal_data: Cow<'static, datap::decimal::SymbolsV1> =
-        response.take_payload().unwrap();
+    let decimal_data: Cow<'static, datap::decimal::SymbolsV1> = response.take_payload().unwrap();
     check_data(&decimal_data);
 }
