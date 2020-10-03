52d9787c64decd179f71c62abb7448bfbb0178a3
Zibi Braniecki <zibi@braniecki.net>
2020-10-03 00:56:28 -0700

    Reorganize PluralRules benchmarks

--- components/pluralrules/benches/parser.rs
+++ components/pluralrules/benches/parser.rs
@@ -3,30 +3,66 @@ mod helpers;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use icu_pluralrules::PluralCategory;
+use icu_data_provider::{icu_data_key, structs, DataEntry, DataProvider, DataRequest};
+use icu_fs_data_provider::FsDataProvider;
+use std::borrow::Cow;
 
-fn plurals_bench(c: &mut Criterion) {
-    use icu_pluralrules::rules::Lexer;
+fn parser(c: &mut Criterion) {
+    use icu_pluralrules::rules::parse_condition;
 
     let plurals_data = helpers::get_plurals_data();
 
-    let pl_data = plurals_data
-        .rules
-        .get("pl")
-        .expect("Polish data should be in the fixture.");
-    let pl_data: Vec<&String> = PluralCategory::all()
-        .filter_map(|cat| pl_data.get(cat))
-        .collect();
+    let provider = FsDataProvider::try_new("./tests/data/json_plurals_37")
+        .expect("Loading file from testdata directory");
 
-    c.bench_function("parser/lex", |b| {
+    let mut rules = vec![];
+
+    for langid in &plurals_data.langs {
+        let response = provider
+            .load(&DataRequest {
+                data_key: icu_data_key!(plurals: cardinal@1),
+                data_entry: DataEntry {
+                    variant: None,
+                    langid: langid.clone(),
+                },
+            })
+            .unwrap();
+        let plurals_data: Cow<structs::plurals::PluralRuleStringsV1> =
+            response.take_payload().unwrap();
+
+        let r = &[
+            &plurals_data.zero,
+            &plurals_data.one,
+            &plurals_data.two,
+            &plurals_data.few,
+            &plurals_data.many,
+        ];
+
+        for i in r {
+            if let Some(x) = i {
+                rules.push(x.clone());
+            }
+        }
+    }
+
+    c.bench_function("parser/overview", |b| {
         b.iter(|| {
-            for val in &pl_data {
-                let lexer = Lexer::new(black_box(val.as_bytes()));
-                let _ = lexer.count();
+            for rule in &rules {
+                let _ = parse_condition(black_box(rule.as_bytes()));
+            }
+        })
+    });
+
+    #[cfg(feature = "bench")]
+    c.bench_function("parser/lex", |b| {
+        use icu_pluralrules::rules::Lexer;
+        b.iter(|| {
+            for rule in &rules {
+                let _ = Lexer::new(black_box(rule.as_bytes())).count();
             }
         })
     });
 }
 
-criterion_group!(benches, plurals_bench,);
+criterion_group!(benches, parser,);
 criterion_main!(benches);
--- components/pluralrules/benches/pluralrules.rs
+++ components/pluralrules/benches/pluralrules.rs
@@ -1,38 +1,56 @@
 mod fixtures;
 mod helpers;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 use icu_fs_data_provider::FsDataProvider;
-use icu_locale::LanguageIdentifier;
 use icu_pluralrules::{PluralRuleType, PluralRules};
 
-fn plurals_bench(c: &mut Criterion) {
+fn pluralrules(c: &mut Criterion) {
     let plurals_data = helpers::get_plurals_data();
     let numbers_data = helpers::get_numbers_data();
 
     let provider = FsDataProvider::try_new("./tests/data/json_plurals_37")
         .expect("Loading file from testdata directory");
 
-    c.bench_function("pluralrules/construct/fs", |b| {
+    c.bench_function("pluralrules/overview", |b| {
         b.iter(|| {
             for lang in &plurals_data.langs {
-                PluralRules::try_new(lang.clone(), PluralRuleType::Ordinal, &provider).unwrap();
-                PluralRules::try_new(lang.clone(), PluralRuleType::Cardinal, &provider).unwrap();
-            }
-        });
-    });
-
-    let loc: LanguageIdentifier = "pl".parse().unwrap();
-    let pr = PluralRules::try_new(loc, PluralRuleType::Cardinal, &provider).unwrap();
-    c.bench_function("pluralrules/select/fs", |b| {
-        b.iter(|| {
-            for s in &numbers_data.usize {
-                let _ = pr.select(black_box(*s));
+                let pr = PluralRules::try_new(lang.clone(), PluralRuleType::Cardinal, &provider)
+                    .unwrap();
+                for s in &numbers_data.usize {
+                    let _ = pr.select(*s);
+                }
             }
         })
     });
+
+    #[cfg(feature = "bench")]
+    {
+        use criterion::black_box;
+        use icu_locale::LanguageIdentifier;
+
+        c.bench_function("pluralrules/construct/fs", |b| {
+            b.iter(|| {
+                for lang in &plurals_data.langs {
+                    PluralRules::try_new(lang.clone(), PluralRuleType::Ordinal, &provider).unwrap();
+                    PluralRules::try_new(lang.clone(), PluralRuleType::Cardinal, &provider)
+                        .unwrap();
+                }
+            });
+        });
+
+        let loc: LanguageIdentifier = "pl".parse().unwrap();
+        let pr = PluralRules::try_new(loc, PluralRuleType::Cardinal, &provider).unwrap();
+        c.bench_function("plurals/select/fs", |b| {
+            b.iter(|| {
+                for s in &numbers_data.usize {
+                    let _ = pr.select(black_box(*s));
+                }
+            })
+        });
+    }
 }
 
-criterion_group!(benches, plurals_bench,);
+criterion_group!(benches, pluralrules,);
 criterion_main!(benches);
