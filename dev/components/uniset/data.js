935776ab7c36f23ba6439382599e4982d206e7b0
github-action-benchmark <github@users.noreply.github.com>
2020-10-13 20:55:51 +0000

    add Rust Benchmark (cargo) benchmark result for 6fddde66b8f481ae480d988f9664773f1de8d80e

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602622381469,
+  "lastUpdate": 1602622551346,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -605,6 +605,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 30505172,
             "range": "± 1941997",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "distinct": true,
+          "id": "6fddde66b8f481ae480d988f9664773f1de8d80e",
+          "message": "Hotfix github workflow",
+          "timestamp": "2020-10-13T13:51:51-07:00",
+          "tree_id": "66b95d6457876f8388c27c053225371a3e838a30",
+          "url": "https://github.com/unicode-org/icu4x/commit/6fddde66b8f481ae480d988f9664773f1de8d80e"
+        },
+        "date": 1602622548651,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 33755331,
+            "range": "± 1006799",
             "unit": "ns/iter"
           }
         ]
