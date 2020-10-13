b886f842db0fab9ba92f0e09ae6af90da7cc99f1
github-action-benchmark <github@users.noreply.github.com>
2020-10-13 20:55:47 +0000

    add Rust Benchmark (cargo) benchmark result for 6fddde66b8f481ae480d988f9664773f1de8d80e

---
+++ dev/components/locid/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1602622547559,
+  "repoUrl": "https://github.com/unicode-org/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "distinct": true,
+          "id": "6fddde66b8f481ae480d988f9664773f1de8d80e",
+          "message": "Hotfix github workflow",
+          "timestamp": "2020-10-13T13:51:51-07:00",
+          "tree_id": "66b95d6457876f8388c27c053225371a3e838a30",
+          "url": "https://github.com/unicode-org/icu4x/commit/6fddde66b8f481ae480d988f9664773f1de8d80e"
+        },
+        "date": 1602622547047,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 5658,
+            "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
