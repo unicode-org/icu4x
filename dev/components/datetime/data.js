3c7a8a4c3ea6a4b69db5a1d3f3de4e2a1d80e3fb
github-action-benchmark <github@users.noreply.github.com>
2020-10-15 14:56:06 +0000

    add Rust Benchmark (cargo) benchmark result for 90f742a5bbad4a3588e37303f5f7cdda10f02c5c

--- dev/components/datetime/data.js
+++ dev/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602738396770,
+  "lastUpdate": 1602773766136,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -89,6 +89,36 @@ window.BENCHMARK_DATA = {
             "name": "datetime/overview",
             "value": 846576,
             "range": "± 72250",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "90f742a5bbad4a3588e37303f5f7cdda10f02c5c",
+          "message": "Add reference to #355 from DateTimeType API docs (#356)",
+          "timestamp": "2020-10-15T07:51:57-07:00",
+          "tree_id": "8cb06e5985121110bea3015bbd75fd8eabbc3093",
+          "url": "https://github.com/unicode-org/icu4x/commit/90f742a5bbad4a3588e37303f5f7cdda10f02c5c"
+        },
+        "date": 1602773763972,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1032083,
+            "range": "± 106884",
             "unit": "ns/iter"
           }
         ]
