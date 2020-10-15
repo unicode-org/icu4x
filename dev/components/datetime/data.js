99777b97f0d8b74353516777104fd6c495c0f9b4
github-action-benchmark <github@users.noreply.github.com>
2020-10-15 04:55:57 +0000

    add Rust Benchmark (cargo) benchmark result for 88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2

---
+++ dev/components/datetime/data.js
@@ -0,0 +1,68 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1602737757800,
+  "repoUrl": "https://github.com/unicode-org/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2",
+          "message": "Add per-component benchmark tests in CI (#353)",
+          "timestamp": "2020-10-14T21:52:15-07:00",
+          "tree_id": "a2f3a475ec905d5abe931e0c3e3eeea42cf96860",
+          "url": "https://github.com/unicode-org/icu4x/commit/88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2"
+        },
+        "date": 1602737754627,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1002418,
+            "range": "± 22699",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2",
+          "message": "Add per-component benchmark tests in CI (#353)",
+          "timestamp": "2020-10-14T21:52:15-07:00",
+          "tree_id": "a2f3a475ec905d5abe931e0c3e3eeea42cf96860",
+          "url": "https://github.com/unicode-org/icu4x/commit/88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2"
+        },
+        "date": 1602737754627,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1002418,
+            "range": "± 22699",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
