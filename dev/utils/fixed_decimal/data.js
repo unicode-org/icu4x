a106dac825c390eb67362cb793dfce456d21b8d7
github-action-benchmark <github@users.noreply.github.com>
2020-10-15 05:06:56 +0000

    add Rust Benchmark (cargo) benchmark result for 174d846a131ca79802b7da3eb004982d442e399d

--- dev/utils/fixed_decimal/data.js
+++ dev/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602737754213,
+  "lastUpdate": 1602738416689,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 154102,
             "range": "± 7602",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "174d846a131ca79802b7da3eb004982d442e399d",
+          "message": "Update README.md",
+          "timestamp": "2020-10-14T22:03:28-07:00",
+          "tree_id": "6bc0a97dd78518e59b2560279d89e9afbc749241",
+          "url": "https://github.com/unicode-org/icu4x/commit/174d846a131ca79802b7da3eb004982d442e399d"
+        },
+        "date": 1602738416241,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 180077,
+            "range": "± 6842",
             "unit": "ns/iter"
           }
         ]
