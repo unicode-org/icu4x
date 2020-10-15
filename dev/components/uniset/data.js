ba0915150ea6d221bb0efa1709c462bdf741c70e
github-action-benchmark <github@users.noreply.github.com>
2020-10-15 04:56:01 +0000

    add Rust Benchmark (cargo) benchmark result for 88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602736734832,
+  "lastUpdate": 1602737761066,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1079,6 +1079,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34619828,
             "range": "± 1900309",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2",
+          "message": "Add per-component benchmark tests in CI (#353)",
+          "timestamp": "2020-10-14T21:52:15-07:00",
+          "tree_id": "a2f3a475ec905d5abe931e0c3e3eeea42cf96860",
+          "url": "https://github.com/unicode-org/icu4x/commit/88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2"
+        },
+        "date": 1602737757058,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 38163164,
+            "range": "± 126330",
             "unit": "ns/iter"
           }
         ]
