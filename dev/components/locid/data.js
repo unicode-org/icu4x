72c3e9feffcaa4b0e2929aafa703111261f02f26
github-action-benchmark <github@users.noreply.github.com>
2020-10-15 05:06:49 +0000

    add Rust Benchmark (cargo) benchmark result for 174d846a131ca79802b7da3eb004982d442e399d

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602737781457,
+  "lastUpdate": 1602738409831,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -719,6 +719,36 @@ window.BENCHMARK_DATA = {
             "name": "langid/overview",
             "value": 5339,
             "range": "± 201",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "174d846a131ca79802b7da3eb004982d442e399d",
+          "message": "Update README.md",
+          "timestamp": "2020-10-14T22:03:28-07:00",
+          "tree_id": "6bc0a97dd78518e59b2560279d89e9afbc749241",
+          "url": "https://github.com/unicode-org/icu4x/commit/174d846a131ca79802b7da3eb004982d442e399d"
+        },
+        "date": 1602738409445,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 4160,
+            "range": "± 252",
             "unit": "ns/iter"
           }
         ]
