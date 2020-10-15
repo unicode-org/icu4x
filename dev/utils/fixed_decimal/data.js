86f9daff1a43ab5284f78c6a038e6c0febbf05ac
github-action-benchmark <github@users.noreply.github.com>
2020-10-15 04:55:54 +0000

    add Rust Benchmark (cargo) benchmark result for 88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2

---
+++ dev/utils/fixed_decimal/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1602737754213,
+  "repoUrl": "https://github.com/unicode-org/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2",
+          "message": "Add per-component benchmark tests in CI (#353)",
+          "timestamp": "2020-10-14T21:52:15-07:00",
+          "tree_id": "a2f3a475ec905d5abe931e0c3e3eeea42cf96860",
+          "url": "https://github.com/unicode-org/icu4x/commit/88783ab4a36ea6e9e1b8aca4e75ba871f67b7df2"
+        },
+        "date": 1602737753485,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 154102,
+            "range": "± 7602",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
