a3e804d0abf9e3588dec9b252abdfd6e4651b43e
github-action-benchmark <github@users.noreply.github.com>
2020-10-24 19:03:18 +0000

    add Rust Benchmark (cargo) benchmark result for b3b6ba29bacf5cb17114cac22d35ef22b66c8d1a

--- dev/utils/fixed_decimal/data.js
+++ dev/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1603525067271,
+  "lastUpdate": 1603566198625,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -239,6 +239,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 169774,
             "range": "± 5201",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b3b6ba29bacf5cb17114cac22d35ef22b66c8d1a",
+          "message": "Separate keywords in unicode extensions (#369)",
+          "timestamp": "2020-10-24T12:00:49-07:00",
+          "tree_id": "59e796e05c9b4d64a6090f8e2e6ad1c08eca8b06",
+          "url": "https://github.com/unicode-org/icu4x/commit/b3b6ba29bacf5cb17114cac22d35ef22b66c8d1a"
+        },
+        "date": 1603566197836,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 144018,
+            "range": "± 13448",
             "unit": "ns/iter"
           }
         ]
