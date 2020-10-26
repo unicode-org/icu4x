515bc878069536b7fa7a77206b83a2f768d11a68
github-action-benchmark <github@users.noreply.github.com>
2020-10-26 09:59:54 +0000

    add Rust Benchmark (cargo) benchmark result for fbce283a133bf8865ded6c2806cd992a201a82db

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1603585712038,
+  "lastUpdate": 1603706394939,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -419,6 +419,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 113754,
             "range": "± 251",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fbce283a133bf8865ded6c2806cd992a201a82db",
+          "message": "Merge pull request #366 from unicode-org/mihai_latin1\n\nFix for issue #357, Latin 1 definition",
+          "timestamp": "2020-10-26T02:56:13-07:00",
+          "tree_id": "c000495ad2a67b40a7f97be0e3058e31901c02b1",
+          "url": "https://github.com/unicode-org/icu4x/commit/fbce283a133bf8865ded6c2806cd992a201a82db"
+        },
+        "date": 1603706394485,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1408,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 5945,
+            "range": "± 73",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 109010,
+            "range": "± 812",
             "unit": "ns/iter"
           }
         ]
