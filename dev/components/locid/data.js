e7a4e214d288242761ffb79ea80f6fbc440901e3
github-action-benchmark <github@users.noreply.github.com>
2020-11-06 18:41:55 +0000

    add Rust Benchmark (cargo) benchmark result for dcab5bbd27d2bf8403ee3871a7cdb7db537511cb

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1604687633928,
+  "lastUpdate": 1604688115037,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1109,6 +1109,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 7307,
             "range": "± 425",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dcab5bbd27d2bf8403ee3871a7cdb7db537511cb",
+          "message": "Document the ICU and ICU4X boundary logic for UnicodeSet from a range (#377)",
+          "timestamp": "2020-11-06T12:38:49-06:00",
+          "tree_id": "f4c74f1111c3483b97db707f3bb0c037f79eb3d5",
+          "url": "https://github.com/unicode-org/icu4x/commit/dcab5bbd27d2bf8403ee3871a7cdb7db537511cb"
+        },
+        "date": 1604688114429,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 5555,
+            "range": "± 20",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 9249,
+            "range": "± 49",
             "unit": "ns/iter"
           }
         ]
