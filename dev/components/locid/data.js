33bcce0f48876aaffddcc98885bedbbab063b153
github-action-benchmark <github@users.noreply.github.com>
2020-11-06 18:42:25 +0000

    add Rust Benchmark (cargo) benchmark result for 11db1cdcfb2bb5f1c48b7993719596133702023f

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1604688115037,
+  "lastUpdate": 1604688145140,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1145,6 +1145,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 9249,
             "range": "± 49",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "11db1cdcfb2bb5f1c48b7993719596133702023f",
+          "message": "Remove flaky unit test of fn for remote file download (#378)",
+          "timestamp": "2020-11-06T12:39:26-06:00",
+          "tree_id": "33e88af77eb1eee501d83e73ee1371d26f638fbc",
+          "url": "https://github.com/unicode-org/icu4x/commit/11db1cdcfb2bb5f1c48b7993719596133702023f"
+        },
+        "date": 1604688144590,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 5371,
+            "range": "± 569",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 8293,
+            "range": "± 674",
             "unit": "ns/iter"
           }
         ]
