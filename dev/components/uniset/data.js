4ef0466c19f4dc5219a51c68f96ddbd82fc95534
github-action-benchmark <github@users.noreply.github.com>
2020-11-09 20:42:45 +0000

    add Rust Benchmark (cargo) benchmark result for 23a283b2a8d159b99c45f3b459b0b5fe635794b9

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1604688129804,
+  "lastUpdate": 1604954565518,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1529,6 +1529,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 33972489,
             "range": "± 1069953",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "23a283b2a8d159b99c45f3b459b0b5fe635794b9",
+          "message": "Adds icu4x-announce mailing list to README\n\nAlso moves documentation section above the quick start code section.",
+          "timestamp": "2020-11-09T14:40:02-06:00",
+          "tree_id": "1b043d0ee64e1864c22a24483f3a021224f4d17b",
+          "url": "https://github.com/unicode-org/icu4x/commit/23a283b2a8d159b99c45f3b459b0b5fe635794b9"
+        },
+        "date": 1604954561619,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 32534163,
+            "range": "± 1324944",
             "unit": "ns/iter"
           }
         ]
