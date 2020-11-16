716cc3212a54c1532aead2571cc6f76c85ec0c31
github-action-benchmark <github@users.noreply.github.com>
2020-11-16 22:07:48 +0000

    add Rust Benchmark (cargo) benchmark result for 10bb36be3a47f6c7d95995babb10c522da1858a3

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1605225249780,
+  "lastUpdate": 1605564468377,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1739,6 +1739,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 33042585,
             "range": "± 1096512",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chris@sourcefoundry.org",
+            "name": "Chris Simpkins",
+            "username": "chrissimpkins"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "10bb36be3a47f6c7d95995babb10c522da1858a3",
+          "message": "Fix README links following documentation move (#391)",
+          "timestamp": "2020-11-16T16:04:28-06:00",
+          "tree_id": "41695b662fcad3a36311f96aa7232c3cc28b7d1a",
+          "url": "https://github.com/unicode-org/icu4x/commit/10bb36be3a47f6c7d95995babb10c522da1858a3"
+        },
+        "date": 1605564467223,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 36580750,
+            "range": "± 2493330",
             "unit": "ns/iter"
           }
         ]
