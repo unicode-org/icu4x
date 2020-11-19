1141a80f9d89864d228c29a59c05131bd224927e
github-action-benchmark <github@users.noreply.github.com>
2020-11-19 01:39:41 +0000

    add Rust Benchmark (cargo) benchmark result for 4ead8619942be315d6521100509117f843669ebc

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1605661944316,
+  "lastUpdate": 1605749981092,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1613,6 +1613,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 9846,
             "range": "± 589",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4ead8619942be315d6521100509117f843669ebc",
+          "message": "Add T-techdebt",
+          "timestamp": "2020-11-18T19:25:03-06:00",
+          "tree_id": "132e494cbd370040881c40cc23dd493da1dab513",
+          "url": "https://github.com/unicode-org/icu4x/commit/4ead8619942be315d6521100509117f843669ebc"
+        },
+        "date": 1605749980512,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3585,
+            "range": "± 336",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 8973,
+            "range": "± 494",
             "unit": "ns/iter"
           }
         ]
