5caea50f872b442435619bd847ae6a5c5e4ae789
github-action-benchmark <github@users.noreply.github.com>
2020-11-19 01:43:16 +0000

    add Rust Benchmark (cargo) benchmark result for 0e9a5553000618577ab37f1e9ffbba5c96d838b6

--- dev/components/datetime/data.js
+++ dev/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1605749992792,
+  "lastUpdate": 1605750196772,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -869,6 +869,36 @@ window.BENCHMARK_DATA = {
             "name": "datetime/overview",
             "value": 864429,
             "range": "± 43895",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0e9a5553000618577ab37f1e9ffbba5c96d838b6",
+          "message": "Update triaging.md\n\nHide discuss issues from issues needing owner",
+          "timestamp": "2020-11-18T19:28:56-06:00",
+          "tree_id": "031561d9643b32575ee340920888fb4287f836ae",
+          "url": "https://github.com/unicode-org/icu4x/commit/0e9a5553000618577ab37f1e9ffbba5c96d838b6"
+        },
+        "date": 1605750195616,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 855255,
+            "range": "± 23254",
             "unit": "ns/iter"
           }
         ]
