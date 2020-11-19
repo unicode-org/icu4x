0f0be07af84924a7648f97dcb04c2ee4762cdfa7
github-action-benchmark <github@users.noreply.github.com>
2020-11-19 21:53:59 +0000

    add Rust Benchmark (cargo) benchmark result for 561e10a3ea242397466781a9162c5ba37736e97e

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1605810320673,
+  "lastUpdate": 1605822839984,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1757,6 +1757,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 10362,
             "range": "± 1047",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "561e10a3ea242397466781a9162c5ba37736e97e",
+          "message": "Add WASM build task for examples (#365)",
+          "timestamp": "2020-11-19T15:41:20-06:00",
+          "tree_id": "659274e93fa5b90dc30c97ec85f3f9f9ae564b92",
+          "url": "https://github.com/unicode-org/icu4x/commit/561e10a3ea242397466781a9162c5ba37736e97e"
+        },
+        "date": 1605822839333,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3820,
+            "range": "± 107",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 8653,
+            "range": "± 233",
             "unit": "ns/iter"
           }
         ]
