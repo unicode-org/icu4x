804e29c9841dac1867fae8c2877ca7631fb146d9
github-action-benchmark <github@users.noreply.github.com>
2020-11-20 18:39:33 +0000

    add Rust Benchmark (cargo) benchmark result for fd2728764d5561f1e130546eb969b3811a226de5

--- dev/utils/fixed_decimal/data.js
+++ dev/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1605822828197,
+  "lastUpdate": 1605897573323,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -959,6 +959,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 170725,
             "range": "± 3813",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fd2728764d5561f1e130546eb969b3811a226de5",
+          "message": "Switch to use Rust intra-doc (#400)",
+          "timestamp": "2020-11-20T10:25:17-08:00",
+          "tree_id": "26ee4a83a1a5056ef2520cc25f21b9c77f34b65e",
+          "url": "https://github.com/unicode-org/icu4x/commit/fd2728764d5561f1e130546eb969b3811a226de5"
+        },
+        "date": 1605897572707,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 155589,
+            "range": "± 8040",
             "unit": "ns/iter"
           }
         ]
