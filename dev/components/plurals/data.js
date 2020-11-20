f6fc82cb3f80d784836530d83eb52ee3049ba06a
github-action-benchmark <github@users.noreply.github.com>
2020-11-20 18:40:05 +0000

    add Rust Benchmark (cargo) benchmark result for fd2728764d5561f1e130546eb969b3811a226de5

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1605822892132,
+  "lastUpdate": 1605897605285,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1343,6 +1343,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 111513,
             "range": "± 5852",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fd2728764d5561f1e130546eb969b3811a226de5",
+          "message": "Switch to use Rust intra-doc (#400)",
+          "timestamp": "2020-11-20T10:25:17-08:00",
+          "tree_id": "26ee4a83a1a5056ef2520cc25f21b9c77f34b65e",
+          "url": "https://github.com/unicode-org/icu4x/commit/fd2728764d5561f1e130546eb969b3811a226de5"
+        },
+        "date": 1605897604676,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1145,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 5584,
+            "range": "± 86",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 96655,
+            "range": "± 427",
             "unit": "ns/iter"
           }
         ]
