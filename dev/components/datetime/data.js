1ef6904082870e4d5822bbf93903d9745c734b9c
github-action-benchmark <github@users.noreply.github.com>
2020-12-08 22:18:35 +0000

    add Rust Benchmark (cargo) benchmark result for 17ff2cfa39a1f7442515ab903244bd270deb39c3

--- dev/components/datetime/data.js
+++ dev/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1607128675899,
+  "lastUpdate": 1607465915397,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1109,6 +1109,36 @@ window.BENCHMARK_DATA = {
             "name": "datetime/overview",
             "value": 991017,
             "range": "± 49246",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "17ff2cfa39a1f7442515ab903244bd270deb39c3",
+          "message": "Replace placeholders in docs README.md with actual content (#412)",
+          "timestamp": "2020-12-08T14:03:44-08:00",
+          "tree_id": "ec1bd9d1e0761130ee3cf859398e445e59ec2bd4",
+          "url": "https://github.com/unicode-org/icu4x/commit/17ff2cfa39a1f7442515ab903244bd270deb39c3"
+        },
+        "date": 1607465913428,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 807027,
+            "range": "± 52747",
             "unit": "ns/iter"
           }
         ]
