a48babce96d84466d085451833321df28bb10cd5
github-action-benchmark <github@users.noreply.github.com>
2020-12-23 03:26:18 +0000

    add Rust Benchmark (cargo) benchmark result for 05490a5c8606d6e3cb69fd443682843ad7cb3303

--- dev/utils/fixed_decimal/data.js
+++ dev/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608691241984,
+  "lastUpdate": 1608693978234,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1259,6 +1259,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 138651,
             "range": "± 9098",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "05490a5c8606d6e3cb69fd443682843ad7cb3303",
+          "message": "Fix link warnings for the docs (#425)\n\nThese link warnings were generated while running cargo doc on the root\r\nof the project.",
+          "timestamp": "2020-12-22T19:11:05-08:00",
+          "tree_id": "d941409e76b443dc324cf3fe52a46f30be7cba9f",
+          "url": "https://github.com/unicode-org/icu4x/commit/05490a5c8606d6e3cb69fd443682843ad7cb3303"
+        },
+        "date": 1608693977589,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 148321,
+            "range": "± 15060",
             "unit": "ns/iter"
           }
         ]
