c8052d0bc403940e8d1bcea062ed874b0ed02767
github-action-benchmark <github@users.noreply.github.com>
2021-01-04 23:05:31 +0000

    add Rust Benchmark (cargo) benchmark result for 3f1e984e849558b20d561606237c19375c060d59

--- dev/utils/fixed_decimal/data.js
+++ dev/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608694037689,
+  "lastUpdate": 1609801531897,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1319,6 +1319,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 143975,
             "range": "± 512",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58569820+nordzilla@users.noreply.github.com",
+            "name": "Erik Nordin",
+            "username": "nordzilla"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3f1e984e849558b20d561606237c19375c060d59",
+          "message": "Otpimize parsing Language::from_bytes() (#432)\n\nChanges the `Language::from_bytes()` function to return\r\nan Err if the length does not match before creating a\r\nTinyStr and checking `is_ascii_alphabetic()`.\r\n\r\nThis is a small optimization, but the change is congruent\r\nwith `Region::from_bytes()` and `Script::from_bytes()`",
+          "timestamp": "2021-01-04T14:51:24-08:00",
+          "tree_id": "dfb64ec5d5e308e104122ab709f6ed86728e3c97",
+          "url": "https://github.com/unicode-org/icu4x/commit/3f1e984e849558b20d561606237c19375c060d59"
+        },
+        "date": 1609801530862,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 150619,
+            "range": "± 8625",
             "unit": "ns/iter"
           }
         ]
