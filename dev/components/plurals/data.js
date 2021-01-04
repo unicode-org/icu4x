cc9476f8c05e73c96ea938ac3be4f1648b790fa4
github-action-benchmark <github@users.noreply.github.com>
2021-01-04 23:05:56 +0000

    add Rust Benchmark (cargo) benchmark result for 3f1e984e849558b20d561606237c19375c060d59

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608694111494,
+  "lastUpdate": 1609801556726,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1847,6 +1847,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 101772,
             "range": "± 6022",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58569820+nordzilla@users.noreply.github.com",
+            "name": "Erik Nordin",
+            "username": "nordzilla"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3f1e984e849558b20d561606237c19375c060d59",
+          "message": "Otpimize parsing Language::from_bytes() (#432)\n\nChanges the `Language::from_bytes()` function to return\r\nan Err if the length does not match before creating a\r\nTinyStr and checking `is_ascii_alphabetic()`.\r\n\r\nThis is a small optimization, but the change is congruent\r\nwith `Region::from_bytes()` and `Script::from_bytes()`",
+          "timestamp": "2021-01-04T14:51:24-08:00",
+          "tree_id": "dfb64ec5d5e308e104122ab709f6ed86728e3c97",
+          "url": "https://github.com/unicode-org/icu4x/commit/3f1e984e849558b20d561606237c19375c060d59"
+        },
+        "date": 1609801555574,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 925,
+            "range": "± 31",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 5130,
+            "range": "± 351",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 85705,
+            "range": "± 2617",
             "unit": "ns/iter"
           }
         ]
