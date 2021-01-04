f3e4333f3325572515ef28755188ff694401863b
github-action-benchmark <github@users.noreply.github.com>
2021-01-04 23:08:05 +0000

    add Rust Benchmark (cargo) benchmark result for 03ae8315ff1ccb4dfcb3336bf436066176d2cd7c

--- dev/utils/fixed_decimal/data.js
+++ dev/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609801531897,
+  "lastUpdate": 1609801685636,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1349,6 +1349,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 150619,
             "range": "± 8625",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dminor@mozilla.com",
+            "name": "Dan Minor",
+            "username": "dminor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03ae8315ff1ccb4dfcb3336bf436066176d2cd7c",
+          "message": "Use parsing iterator for Locale PartialEq<str>  (#402)\n\n* Fix display of transform fields\r\n\r\nWithout these changes, fields after the first are not separated by\r\n`-`, so `en-US-t-h0-hybrid-k0-platform-s0-true` is displayed as\r\n`en-US-t-h0-hybridk0-platforms0-`.\r\n\r\n* Use parsing iterator for Locale PartialEq<str>\r\n\r\nThis is similar to what was done for LanguateIdentifier PartialEq<str>.\r\nThe extensions are parsed all at once on the assumption that they will\r\nnot be encountered often enough for the potential performance\r\nbenefits of parsing them one at a time to justify the additional\r\ncode complexity.",
+          "timestamp": "2021-01-04T14:52:08-08:00",
+          "tree_id": "4153026a6098501fa2161321bb5baf3f89942a6a",
+          "url": "https://github.com/unicode-org/icu4x/commit/03ae8315ff1ccb4dfcb3336bf436066176d2cd7c"
+        },
+        "date": 1609801685011,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 171375,
+            "range": "± 2373",
             "unit": "ns/iter"
           }
         ]
