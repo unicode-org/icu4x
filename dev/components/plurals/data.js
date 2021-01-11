7a6ceb98b9a509aebd5195e7dc4d7bffc442db04
github-action-benchmark <github@users.noreply.github.com>
2021-01-11 19:51:53 +0000

    add Rust Benchmark (cargo) benchmark result for 273ec285057bad902c6a08880df4c07a188a7968

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610364429885,
+  "lastUpdate": 1610394713941,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2141,6 +2141,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 112078,
             "range": "± 1009",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "273ec285057bad902c6a08880df4c07a188a7968",
+          "message": "Updating provider_cldr and testdata to CLDR 38.1.0-BETA4 (#441)",
+          "timestamp": "2021-01-11T13:45:58-06:00",
+          "tree_id": "33914139d1bbfbe04a8cab54884d53fa45bca8e0",
+          "url": "https://github.com/unicode-org/icu4x/commit/273ec285057bad902c6a08880df4c07a188a7968"
+        },
+        "date": 1610394710431,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1382,
+            "range": "± 66",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 5900,
+            "range": "± 244",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 96195,
+            "range": "± 6072",
             "unit": "ns/iter"
           }
         ]
