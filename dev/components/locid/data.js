6deb637b38e16f26ae2ee53762d93d784ef2932e
github-action-benchmark <github@users.noreply.github.com>
2021-01-11 11:26:39 +0000

    add Rust Benchmark (cargo) benchmark result for 4c8d9dff321ceb822bab5a3c7b4e250792fffeb6

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610152747103,
+  "lastUpdate": 1610364399256,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2441,6 +2441,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 6764,
             "range": "± 516",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4c8d9dff321ceb822bab5a3c7b4e250792fffeb6",
+          "message": "Enable test-all-features and rename some GitHub Actions targets (#436)",
+          "timestamp": "2021-01-11T05:20:41-06:00",
+          "tree_id": "c2fe16c76d99ba703e3d55a9d0a0dc52a9f71b5c",
+          "url": "https://github.com/unicode-org/icu4x/commit/4c8d9dff321ceb822bab5a3c7b4e250792fffeb6"
+        },
+        "date": 1610364397237,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 4165,
+            "range": "± 110",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 6531,
+            "range": "± 230",
             "unit": "ns/iter"
           }
         ]
