893a971023b2ed298d33b423f32fa30391b13743
github-action-benchmark <github@users.noreply.github.com>
2021-01-14 06:16:21 +0000

    add Rust Benchmark (cargo) benchmark result for 20dd293f708382695be8d730d7492400bf50520f

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610394713941,
+  "lastUpdate": 1610604981734,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2183,6 +2183,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 96195,
             "range": "± 6072",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "20dd293f708382695be8d730d7492400bf50520f",
+          "message": "Make the DateTime bags more explicit (#429)\n\n* Make the DateTime bags more explicit\r\n\r\nI found myself confused on the usage of the bags and the difference\r\nbetween them and the options provided to the date time. I felt that it\r\nwas better to be explicit in the examples, rather than giving more\r\nterse, but potentially misleading examples.\r\n\r\n* Address feedback on adding into() examples",
+          "timestamp": "2021-01-13T22:10:10-08:00",
+          "tree_id": "cf42e024d6e4b6463169ec87f02ad4433bc5055c",
+          "url": "https://github.com/unicode-org/icu4x/commit/20dd293f708382695be8d730d7492400bf50520f"
+        },
+        "date": 1610604981094,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1576,
+            "range": "± 143",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6429,
+            "range": "± 402",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 105882,
+            "range": "± 5823",
             "unit": "ns/iter"
           }
         ]
