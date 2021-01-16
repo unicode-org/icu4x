8c045c513a0f90f1e03f53218567a58531232c34
github-action-benchmark <github@users.noreply.github.com>
2021-01-16 06:29:32 +0000

    add Rust Benchmark (cargo) benchmark result for 19cc9749dd2f6d29fa70c6a663aef24aa06820e7

--- dev/components/datetime/data.js
+++ dev/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610765885346,
+  "lastUpdate": 1610778572663,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1679,6 +1679,36 @@ window.BENCHMARK_DATA = {
             "name": "datetime/overview",
             "value": 762882,
             "range": "± 39914",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "19cc9749dd2f6d29fa70c6a663aef24aa06820e7",
+          "message": "Add environment variable to use custom testdata (#456)",
+          "timestamp": "2021-01-16T00:24:07-06:00",
+          "tree_id": "911f9237447741a675d4d1029ae392f19333a141",
+          "url": "https://github.com/unicode-org/icu4x/commit/19cc9749dd2f6d29fa70c6a663aef24aa06820e7"
+        },
+        "date": 1610778572011,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1010518,
+            "range": "± 33747",
             "unit": "ns/iter"
           }
         ]
