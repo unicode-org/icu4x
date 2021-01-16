6458a51fd420553c0fe92f56c14c9476355a43fa
github-action-benchmark <github@users.noreply.github.com>
2021-01-16 06:30:39 +0000

    add Rust Benchmark (cargo) benchmark result for d9347aa4ec3d55df1102a19eb1c438beb1fdd54d

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610778545129,
+  "lastUpdate": 1610778639416,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2729,6 +2729,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 30461482,
             "range": "± 62411",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d9347aa4ec3d55df1102a19eb1c438beb1fdd54d",
+          "message": "Adding const tests for Locale und (#458)",
+          "timestamp": "2021-01-16T00:25:06-06:00",
+          "tree_id": "ebc2c67a0e92cd96801ad1e1f8263ed3f4edde86",
+          "url": "https://github.com/unicode-org/icu4x/commit/d9347aa4ec3d55df1102a19eb1c438beb1fdd54d"
+        },
+        "date": 1610778627027,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 31811541,
+            "range": "± 1502837",
             "unit": "ns/iter"
           }
         ]
