19cc9749dd2f6d29fa70c6a663aef24aa06820e7
Shane F. Carr <sffc@google.com>
2021-01-16 00:24:07 -0600

    Add environment variable to use custom testdata (#456)

--- resources/testdata/src/test_data_provider.rs
+++ resources/testdata/src/test_data_provider.rs
@@ -6,10 +6,15 @@ use std::path::PathBuf;
 
 /// Get a `DataProvider` loading from test data. Panics if unable to load the data.
 pub fn get_provider() -> FsDataProvider {
-    FsDataProvider::try_new(
-        PathBuf::from(std::env!("CARGO_MANIFEST_DIR"))
+    let path: PathBuf = match std::env::var_os("ICU4X_TESTDATA_DIR") {
+        Some(val) => val.into(),
+        None => PathBuf::from(std::env!("CARGO_MANIFEST_DIR"))
             .join("data")
-            .join("json"),
-    )
-    .expect("The test data directory was unable to be opened")
+            .join("json")
+            .into(),
+    };
+    FsDataProvider::try_new(&path).expect(&format!(
+        "The test data directory was unable to be opened: {:?}",
+        path
+    ))
 }
