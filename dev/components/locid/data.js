efe6be5b066fc440a64a3b6ab280fe143ab5559a
github-action-benchmark <github@users.noreply.github.com>
2021-01-21 23:05:55 +0000

    add Rust Benchmark (cargo) benchmark result for fd37c88bb580c618ae9294e3fd668a4b5c0676ef

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611256448805,
+  "lastUpdate": 1611270355793,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2729,6 +2729,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5468,
             "range": "± 10",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fd37c88bb580c618ae9294e3fd668a4b5c0676ef",
+          "message": "Create format_to_parts.md (#399)",
+          "timestamp": "2021-01-21T17:00:36-06:00",
+          "tree_id": "e8a3bf2389b3b0dd1d1970ab82912b46fed737db",
+          "url": "https://github.com/unicode-org/icu4x/commit/fd37c88bb580c618ae9294e3fd668a4b5c0676ef"
+        },
+        "date": 1611270353756,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3740,
+            "range": "± 782",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 6357,
+            "range": "± 243",
             "unit": "ns/iter"
           }
         ]
