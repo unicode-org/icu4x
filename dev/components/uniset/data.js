2007972584739e9b5f2abb8dceb81c52c93ea7e4
github-action-benchmark <github@users.noreply.github.com>
2021-01-21 23:05:44 +0000

    add Rust Benchmark (cargo) benchmark result for fd37c88bb580c618ae9294e3fd668a4b5c0676ef

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611256466026,
+  "lastUpdate": 1611270344799,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2789,6 +2789,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 35234022,
             "range": "± 503265",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fd37c88bb580c618ae9294e3fd668a4b5c0676ef",
+          "message": "Create format_to_parts.md (#399)",
+          "timestamp": "2021-01-21T17:00:36-06:00",
+          "tree_id": "e8a3bf2389b3b0dd1d1970ab82912b46fed737db",
+          "url": "https://github.com/unicode-org/icu4x/commit/fd37c88bb580c618ae9294e3fd668a4b5c0676ef"
+        },
+        "date": 1611270337717,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 30364753,
+            "range": "± 927084",
             "unit": "ns/iter"
           }
         ]
