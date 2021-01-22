5c2ff16ea14c191971fbd91a7a22d004677ea999
github-action-benchmark <github@users.noreply.github.com>
2021-01-22 20:44:09 +0000

    add Rust Benchmark (cargo) benchmark result for e609a54f363ebee202d619a8560e6857b07a6339

--- dev/components/datetime/data.js
+++ dev/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611270340612,
+  "lastUpdate": 1611348249096,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -1799,6 +1799,36 @@ window.BENCHMARK_DATA = {
             "name": "datetime/overview",
             "value": 962109,
             "range": "± 113435",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dminor@mozilla.com",
+            "name": "Dan Minor",
+            "username": "dminor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e609a54f363ebee202d619a8560e6857b07a6339",
+          "message": "Add LocaleCanonicalizer to support likely subtags (#423)\n\nAdd LocaleCanonicalizer to support likely subtagss",
+          "timestamp": "2021-01-22T15:38:02-05:00",
+          "tree_id": "7426de0f884f04f21e80aff7dcab0e1782f258f0",
+          "url": "https://github.com/unicode-org/icu4x/commit/e609a54f363ebee202d619a8560e6857b07a6339"
+        },
+        "date": 1611348246925,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1135278,
+            "range": "± 96829",
             "unit": "ns/iter"
           }
         ]
