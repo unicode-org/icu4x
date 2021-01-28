3e667a7aec1fa88a6026ac5a5a3166fed7375638
github-action-benchmark <github@users.noreply.github.com>
2021-01-28 18:58:52 +0000

    add Rust Benchmark (cargo) benchmark result for de60d1698f9c2a04a5733982c10998087c071f2c

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611782598508,
+  "lastUpdate": 1611860332422,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3017,6 +3017,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4745,
             "range": "± 14",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "de60d1698f9c2a04a5733982c10998087c071f2c",
+          "message": "Adding review contract examples (#464)",
+          "timestamp": "2021-01-28T12:51:55-06:00",
+          "tree_id": "540015789534a6c35e0066be69bbd67fbb77b91e",
+          "url": "https://github.com/unicode-org/icu4x/commit/de60d1698f9c2a04a5733982c10998087c071f2c"
+        },
+        "date": 1611860328866,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3388,
+            "range": "± 82",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5008,
+            "range": "± 139",
             "unit": "ns/iter"
           }
         ]
