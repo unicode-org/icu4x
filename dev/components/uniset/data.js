7f2da3951cbec1e4e753e7a98337a376445d6301
github-action-benchmark <github@users.noreply.github.com>
2021-01-29 03:26:57 +0000

    add Rust Benchmark (cargo) benchmark result for d2f47f76a82b64a1204bec2a81130fcb70fcea06

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611860368506,
+  "lastUpdate": 1611890817707,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3059,6 +3059,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34588864,
             "range": "± 1529854",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d2f47f76a82b64a1204bec2a81130fcb70fcea06",
+          "message": "Moving data provider structs into their own crates (#451)\n\nAdds \"provider_serde\" feature across components",
+          "timestamp": "2021-01-28T21:22:30-06:00",
+          "tree_id": "9bf2dd468ae1bd5e4e0759bddb20b89a7513073f",
+          "url": "https://github.com/unicode-org/icu4x/commit/d2f47f76a82b64a1204bec2a81130fcb70fcea06"
+        },
+        "date": 1611890817136,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 32450450,
+            "range": "± 78519",
             "unit": "ns/iter"
           }
         ]
