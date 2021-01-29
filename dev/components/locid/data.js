01f64476715c183973fdd4d1465492885f4491b0
github-action-benchmark <github@users.noreply.github.com>
2021-01-29 03:27:39 +0000

    add Rust Benchmark (cargo) benchmark result for d2f47f76a82b64a1204bec2a81130fcb70fcea06

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611860332422,
+  "lastUpdate": 1611890859413,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3053,6 +3053,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5008,
             "range": "± 139",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d2f47f76a82b64a1204bec2a81130fcb70fcea06",
+          "message": "Moving data provider structs into their own crates (#451)\n\nAdds \"provider_serde\" feature across components",
+          "timestamp": "2021-01-28T21:22:30-06:00",
+          "tree_id": "9bf2dd468ae1bd5e4e0759bddb20b89a7513073f",
+          "url": "https://github.com/unicode-org/icu4x/commit/d2f47f76a82b64a1204bec2a81130fcb70fcea06"
+        },
+        "date": 1611890858729,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3593,
+            "range": "± 23",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5353,
+            "range": "± 7",
             "unit": "ns/iter"
           }
         ]
