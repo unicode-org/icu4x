2c63a9d45f85040960739d0df7b3bc1d1d01ff7f
Shane F. Carr <shane@unicode.org>
2021-02-03 03:32:20 -0600

    Fix docs test

--- components/icu/src/lib.rs
+++ components/icu/src/lib.rs
@@ -39,7 +39,7 @@
 //!
 //! let provider = icu_testdata::get_provider();
 //!
-//! let lid = langid!("en");
+//! let locale = langid!("en").into();
 //!
 //! let options = style::Bag {
 //!     date: Some(style::Date::Long),
@@ -47,7 +47,7 @@
 //!     ..Default::default()
 //! }.into();
 //!
-//! let dtf = DateTimeFormat::try_new(lid, &provider, &options)
+//! let dtf = DateTimeFormat::try_new(locale, &provider, &options)
 //!     .expect("Failed to create DateTimeFormat instance.");
 //!
 //! let date: MockDateTime = "2020-09-12T12:35:00".parse()
@@ -77,7 +77,7 @@ pub mod datetime {
     //!
     //! let provider = icu_testdata::get_provider();
     //!
-    //! let lid = langid!("en");
+    //! let locale = langid!("en").into();
     //!
     //! let options = style::Bag {
     //!     date: Some(style::Date::Medium),
@@ -85,7 +85,7 @@ pub mod datetime {
     //!     ..Default::default()
     //! }.into();
     //!
-    //! let dtf = DateTimeFormat::try_new(lid, &provider, &options)
+    //! let dtf = DateTimeFormat::try_new(locale, &provider, &options)
     //!     .expect("Failed to create DateTimeFormat instance.");
     //!
     //! let date: MockDateTime = "2020-09-12T12:35:00".parse()
