a9c2a123142473fec64366503cb0e14750794a8f
github-action-benchmark <github@users.noreply.github.com>
2021-02-08 23:25:34 +0000

    add Rust Benchmark (cargo) benchmark result for ad895a860fc83a7617a0ab604d19ac36d1380915

--- dev/components/datetime/data.js
+++ dev/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612813831394,
+  "lastUpdate": 1612826734676,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2249,6 +2249,36 @@ window.BENCHMARK_DATA = {
             "name": "datetime/overview",
             "value": 936736,
             "range": "± 70470",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ad895a860fc83a7617a0ab604d19ac36d1380915",
+          "message": "Change DateTimeFormat constructor to take Locale instead of LangID (#475)",
+          "timestamp": "2021-02-08T17:19:38-06:00",
+          "tree_id": "76c5cf0b9b2525cd88649614be13f54c1b8c849c",
+          "url": "https://github.com/unicode-org/icu4x/commit/ad895a860fc83a7617a0ab604d19ac36d1380915"
+        },
+        "date": 1612826732666,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 1159421,
+            "range": "± 62610",
             "unit": "ns/iter"
           }
         ]
