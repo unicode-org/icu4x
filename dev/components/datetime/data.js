049304646b776ebc6bf234e32de369fc5af5a4ca
github-action-benchmark <github@users.noreply.github.com>
2021-02-08 19:50:31 +0000

    add Rust Benchmark (cargo) benchmark result for 01d85cc1bd793aecbd189a1826d22ed3ddaba771

--- dev/components/datetime/data.js
+++ dev/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612807519146,
+  "lastUpdate": 1612813831394,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2219,6 +2219,36 @@ window.BENCHMARK_DATA = {
             "name": "datetime/overview",
             "value": 1197139,
             "range": "± 68500",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "01d85cc1bd793aecbd189a1826d22ed3ddaba771",
+          "message": "Fix deploying memory benchmarks to GitHub pages (#472)",
+          "timestamp": "2021-02-08T13:44:52-06:00",
+          "tree_id": "2c56364a53a4a7ae440c17ab874dea143b804d24",
+          "url": "https://github.com/unicode-org/icu4x/commit/01d85cc1bd793aecbd189a1826d22ed3ddaba771"
+        },
+        "date": 1612813826700,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/overview",
+            "value": 936736,
+            "range": "± 70470",
             "unit": "ns/iter"
           }
         ]
