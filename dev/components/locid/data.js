319cfbb4705775477ddae79bcd91cac66d440fa6
github-action-benchmark <github@users.noreply.github.com>
2021-02-08 19:51:06 +0000

    add Rust Benchmark (cargo) benchmark result for 01d85cc1bd793aecbd189a1826d22ed3ddaba771

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612807524805,
+  "lastUpdate": 1612813866945,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3269,6 +3269,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5317,
             "range": "± 130",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "01d85cc1bd793aecbd189a1826d22ed3ddaba771",
+          "message": "Fix deploying memory benchmarks to GitHub pages (#472)",
+          "timestamp": "2021-02-08T13:44:52-06:00",
+          "tree_id": "2c56364a53a4a7ae440c17ab874dea143b804d24",
+          "url": "https://github.com/unicode-org/icu4x/commit/01d85cc1bd793aecbd189a1826d22ed3ddaba771"
+        },
+        "date": 1612813866268,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3894,
+            "range": "± 137",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5760,
+            "range": "± 582",
             "unit": "ns/iter"
           }
         ]
