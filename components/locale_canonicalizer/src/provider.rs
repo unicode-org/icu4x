efba26f9e066c514a2f1bfce9a8020e7e0a2b653
Dan Minor <dminor@mozilla.com>
2021-02-09 14:33:46 -0500

    Optimize likely subtags (#469)
    This breaks up the current monolithic likely subtags data into multiple tables
    based upon the subtags which are used for searching by the maximize algorithm.
    Because of this change, searching can be done using references to the subtags
    of the input Locale rather than creating a copy of it.
    
    The result of a search always shares data with the search key. This change
    takes advantage of this fact to only store the delta between the search key
    and the result.

--- components/locale_canonicalizer/src/provider.rs
+++ components/locale_canonicalizer/src/provider.rs
@@ -1,7 +1,6 @@
 // This file is part of ICU4X. For terms of use, please see the file
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/master/LICENSE ).
-
 use icu_locid::LanguageIdentifier;
 
 pub mod key {
@@ -15,5 +14,11 @@ pub mod key {
     derive(serde::Serialize, serde::Deserialize)
 )]
 pub struct LikelySubtagsV1 {
-    pub entries: Vec<(LanguageIdentifier, LanguageIdentifier)>,
+    pub language_script: Vec<(LanguageIdentifier, LanguageIdentifier)>,
+    pub language_region: Vec<(LanguageIdentifier, LanguageIdentifier)>,
+    pub language: Vec<(LanguageIdentifier, LanguageIdentifier)>,
+    pub script_region: Vec<(LanguageIdentifier, LanguageIdentifier)>,
+    pub script: Vec<(LanguageIdentifier, LanguageIdentifier)>,
+    pub region: Vec<(LanguageIdentifier, LanguageIdentifier)>,
+    pub und: LanguageIdentifier,
 }
--- components/provider_cldr/src/transform/likelysubtags.rs
+++ components/provider_cldr/src/transform/likelysubtags.rs
@@ -98,11 +98,80 @@ impl<'d> IterableDataProvider<'d> for LikelySubtagsProvider<'d> {
 
 impl From<&cldr_json::Resource> for LikelySubtagsV1 {
     fn from(other: &cldr_json::Resource) -> Self {
-        let mut entries = other.supplemental.likely_subtags.clone();
-        // We sort here because the ordering from sorting by LanguageIdentifier
-        // is not necessarily the order in the underlying CLDR data.
-        entries.sort_unstable();
-        Self { entries }
+        use icu_locid::LanguageIdentifier;
+
+        let mut language_script: Vec<(LanguageIdentifier, LanguageIdentifier)> = Vec::new();
+        let mut language_region: Vec<(LanguageIdentifier, LanguageIdentifier)> = Vec::new();
+        let mut language: Vec<(LanguageIdentifier, LanguageIdentifier)> = Vec::new();
+        let mut script_region: Vec<(LanguageIdentifier, LanguageIdentifier)> = Vec::new();
+        let mut script: Vec<(LanguageIdentifier, LanguageIdentifier)> = Vec::new();
+        let mut region: Vec<(LanguageIdentifier, LanguageIdentifier)> = Vec::new();
+        let mut und = LanguageIdentifier::default();
+
+        // Create a result LanguageIdentifier. We only need to store the delta
+        // between the search LanguageIdentifier and the result LanguageIdentifier.
+        let extract_result =
+            |entry: &(LanguageIdentifier, LanguageIdentifier)| -> LanguageIdentifier {
+                LanguageIdentifier {
+                    language: if entry.0.language != entry.1.language {
+                        entry.1.language
+                    } else {
+                        icu_locid::subtags::Language::und()
+                    },
+                    script: if entry.0.script != entry.1.script {
+                        entry.1.script
+                    } else {
+                        None
+                    },
+                    region: if entry.0.region != entry.1.region {
+                        entry.1.region
+                    } else {
+                        None
+                    },
+                    variants: icu_locid::subtags::Variants::default(),
+                }
+            };
+
+        for entry in other.supplemental.likely_subtags.iter() {
+            if !entry.0.language.is_empty() {
+                if entry.0.script.is_some() {
+                    language_script.push((entry.0.clone(), extract_result(entry)));
+                } else if entry.0.region.is_some() {
+                    language_region.push((entry.0.clone(), extract_result(entry)));
+                } else {
+                    language.push((entry.0.clone(), extract_result(entry)));
+                }
+            } else if entry.0.script.is_some() {
+                if entry.0.region.is_some() {
+                    script_region.push((entry.0.clone(), extract_result(entry)));
+                } else {
+                    script.push((entry.0.clone(), extract_result(entry)));
+                }
+            } else if entry.0.region.is_some() {
+                region.push((entry.0.clone(), extract_result(entry)));
+            } else {
+                und = entry.1.clone();
+            }
+        }
+
+        // We sort here to ensure that they are sorted properly by the subtags
+        // we will use to search the data. This is not necessary the order in
+        // the underlying CLDR data.
+        language_script.sort_unstable_by_key(|k| (k.0.language, k.0.script));
+        language_region.sort_unstable_by_key(|k| (k.0.language, k.0.region));
+        language.sort_unstable_by_key(|k| k.0.language);
+        script_region.sort_unstable_by_key(|k| (k.0.script, k.0.region));
+        script.sort_unstable_by_key(|k| k.0.script);
+        region.sort_unstable_by_key(|k| k.0.script);
+        Self {
+            language_script,
+            language_region,
+            language,
+            script_region,
+            script,
+            region,
+            und,
+        }
     }
 }
 
@@ -138,8 +207,12 @@ fn test_basic() {
 
     let langid = langid!("cu-Glag");
     let entry = result
-        .entries
-        .binary_search_by_key(&&langid, |(l, _)| l)
+        .language_script
+        .binary_search_by_key(&(langid.language, langid.script), |(l, _)| {
+            (l.language, l.script)
+        })
         .unwrap();
-    assert_eq!(result.entries[entry].1, "cu-Glag-BG");
+    assert!(result.language_script[entry].1.language.is_empty());
+    assert!(result.language_script[entry].1.script.is_none());
+    assert_eq!(result.language_script[entry].1.region.unwrap(), "BG");
 }
