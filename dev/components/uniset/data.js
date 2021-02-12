f6c85c67fada06534265519b91b4629a9d381bc1
github-action-benchmark <github@users.noreply.github.com>
2021-02-12 17:46:20 +0000

    add Rust Benchmark (cargo) benchmark result for 324d9edd2def40e0d000646748172cce370d92db

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612899566048,
+  "lastUpdate": 1613151980492,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3329,6 +3329,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 38128455,
             "range": "± 158592",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "324d9edd2def40e0d000646748172cce370d92db",
+          "message": "Re-write DateTimeType trait with DateTimeInput (#445)",
+          "timestamp": "2021-02-12T11:40:51-06:00",
+          "tree_id": "5882c86bb80a06b39e3b924ddded92a7d6d33b64",
+          "url": "https://github.com/unicode-org/icu4x/commit/324d9edd2def40e0d000646748172cce370d92db"
+        },
+        "date": 1613151979800,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 33344242,
+            "range": "± 83551",
             "unit": "ns/iter"
           }
         ]
