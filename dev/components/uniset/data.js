b5d85dda4a1cc5a13394b618be13f44cad93adeb
github-action-benchmark <github@users.noreply.github.com>
2021-02-12 20:12:45 +0000

    add Rust Benchmark (cargo) benchmark result for 3be597cdf89432e493e7777bb4dec54854a47f97

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1613151980492,
+  "lastUpdate": 1613160765228,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3359,6 +3359,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 33344242,
             "range": "± 83551",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3be597cdf89432e493e7777bb4dec54854a47f97",
+          "message": "Update string type recommendations in style_guide.md (#490)",
+          "timestamp": "2021-02-12T14:07:12-06:00",
+          "tree_id": "3516e2db54f25c315b8e63f51df7cb8fc3f4de38",
+          "url": "https://github.com/unicode-org/icu4x/commit/3be597cdf89432e493e7777bb4dec54854a47f97"
+        },
+        "date": 1613160760810,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 25923988,
+            "range": "± 760439",
             "unit": "ns/iter"
           }
         ]
