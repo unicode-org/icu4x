400db4edc6d0988890127bc62026a9e402008a3c
github-action-benchmark <github@users.noreply.github.com>
2021-02-27 10:45:27 +0000

    add Rust Benchmark (cargo) benchmark result for df7b6edf44c3360d3ad4127c12ed03c5f039ff6b

--- dev/components/uniset/data.js
+++ dev/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614296380172,
+  "lastUpdate": 1614422727855,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3449,6 +3449,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 30536495,
             "range": "± 74846",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "df7b6edf44c3360d3ad4127c12ed03c5f039ff6b",
+          "message": "Update data_pipeline.md to discuss caching (#497)",
+          "timestamp": "2021-02-27T04:39:28-06:00",
+          "tree_id": "4d625493d15543672a3f6413db21e02a8d3e0c6e",
+          "url": "https://github.com/unicode-org/icu4x/commit/df7b6edf44c3360d3ad4127c12ed03c5f039ff6b"
+        },
+        "date": 1614422724693,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 33394465,
+            "range": "± 1436794",
             "unit": "ns/iter"
           }
         ]
