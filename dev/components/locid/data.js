2b7d69c0e660680e6d211ffebf18210956541e61
github-action-benchmark <github@users.noreply.github.com>
2021-03-02 12:09:15 +0000

    add Rust Benchmark (cargo) benchmark result for b15c0b36901095406a67e3620a3d1d0e294b7692

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614666144855,
+  "lastUpdate": 1614686955373,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3665,6 +3665,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4397,
             "range": "± 20",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dminor@mozilla.com",
+            "name": "Dan Minor",
+            "username": "dminor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b15c0b36901095406a67e3620a3d1d0e294b7692",
+          "message": "Make LanguageIdentifier a field of Locale (#492)\n\nMake LanguageIdentifier a field of Locale.\r\n\r\nFixes #447",
+          "timestamp": "2021-03-02T07:03:43-05:00",
+          "tree_id": "8c6d75a7dbcdd0015e8419882389c04863145644",
+          "url": "https://github.com/unicode-org/icu4x/commit/b15c0b36901095406a67e3620a3d1d0e294b7692"
+        },
+        "date": 1614686954800,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2950,
+            "range": "± 133",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 3917,
+            "range": "± 220",
             "unit": "ns/iter"
           }
         ]
