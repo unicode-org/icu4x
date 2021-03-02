72ccd7ad357e0508974696a141c0bb71012177b5
github-action-benchmark <github@users.noreply.github.com>
2021-03-02 12:10:25 +0000

    add Rust Benchmark (cargo) benchmark result for b15c0b36901095406a67e3620a3d1d0e294b7692

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614666195196,
+  "lastUpdate": 1614687025107,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3527,6 +3527,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 107200,
             "range": "± 2424",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dminor@mozilla.com",
+            "name": "Dan Minor",
+            "username": "dminor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b15c0b36901095406a67e3620a3d1d0e294b7692",
+          "message": "Make LanguageIdentifier a field of Locale (#492)\n\nMake LanguageIdentifier a field of Locale.\r\n\r\nFixes #447",
+          "timestamp": "2021-03-02T07:03:43-05:00",
+          "tree_id": "8c6d75a7dbcdd0015e8419882389c04863145644",
+          "url": "https://github.com/unicode-org/icu4x/commit/b15c0b36901095406a67e3620a3d1d0e294b7692"
+        },
+        "date": 1614687024428,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1675,
+            "range": "± 107",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 8147,
+            "range": "± 497",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 114119,
+            "range": "± 4250",
             "unit": "ns/iter"
           }
         ]
