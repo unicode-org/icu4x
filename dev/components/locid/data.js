cfb5531788efe850fa3ec3b843dc50ad70803c30
github-action-benchmark <github@users.noreply.github.com>
2021-03-04 18:31:00 +0000

    add Rust Benchmark (cargo) benchmark result for 68d6b2bdcfbf36c7d25848a71009a2159ea3d780

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614882541283,
+  "lastUpdate": 1614882660292,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3809,6 +3809,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5126,
             "range": "± 15",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "68d6b2bdcfbf36c7d25848a71009a2159ea3d780",
+          "message": "Separate Patterns and Symbols trait methods in DateTimeFormat. (#517)",
+          "timestamp": "2021-03-04T10:25:02-08:00",
+          "tree_id": "6b5351a233b4d758cc95b5cd352a9bca13818898",
+          "url": "https://github.com/unicode-org/icu4x/commit/68d6b2bdcfbf36c7d25848a71009a2159ea3d780"
+        },
+        "date": 1614882659176,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3132,
+            "range": "± 509",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5374,
+            "range": "± 435",
             "unit": "ns/iter"
           }
         ]
