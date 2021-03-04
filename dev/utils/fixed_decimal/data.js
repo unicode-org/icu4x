c6afa40251997e74d6eb3601606dfade490478c2
github-action-benchmark <github@users.noreply.github.com>
2021-03-04 17:39:32 +0000

    add Rust Benchmark (cargo) benchmark result for 40c5dd3205c8fa4beee91a9ac47b18f628a7e550

--- dev/utils/fixed_decimal/data.js
+++ dev/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614686961881,
+  "lastUpdate": 1614879572730,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2549,6 +2549,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 162869,
             "range": "± 708",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "40c5dd3205c8fa4beee91a9ac47b18f628a7e550",
+          "message": "Update tinytemplate to fix nightly breakage (#529)",
+          "timestamp": "2021-03-04T09:33:16-08:00",
+          "tree_id": "a408baa9d9867ae1aafe7557813ceaa087254972",
+          "url": "https://github.com/unicode-org/icu4x/commit/40c5dd3205c8fa4beee91a9ac47b18f628a7e550"
+        },
+        "date": 1614879570568,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 134751,
+            "range": "± 7593",
             "unit": "ns/iter"
           }
         ]
