f6346e222e28c9620084dd8a32a40730854541a3
github-action-benchmark <github@users.noreply.github.com>
2021-03-04 18:32:08 +0000

    add Rust Benchmark (cargo) benchmark result for 68d6b2bdcfbf36c7d25848a71009a2159ea3d780

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614882580789,
+  "lastUpdate": 1614882728940,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3695,6 +3695,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 105629,
             "range": "± 10310",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "68d6b2bdcfbf36c7d25848a71009a2159ea3d780",
+          "message": "Separate Patterns and Symbols trait methods in DateTimeFormat. (#517)",
+          "timestamp": "2021-03-04T10:25:02-08:00",
+          "tree_id": "6b5351a233b4d758cc95b5cd352a9bca13818898",
+          "url": "https://github.com/unicode-org/icu4x/commit/68d6b2bdcfbf36c7d25848a71009a2159ea3d780"
+        },
+        "date": 1614882727713,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1764,
+            "range": "± 122",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 8523,
+            "range": "± 466",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 115487,
+            "range": "± 291",
             "unit": "ns/iter"
           }
         ]
