d145c5dc6112376d939104ffbfbcaf7c6c2c95e0
github-action-benchmark <github@users.noreply.github.com>
2021-03-04 18:15:04 +0000

    add Rust Benchmark (cargo) benchmark result for 9b0fa09152aea4de2f8a37628d0bebb9ebb50f3d

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614879583098,
+  "lastUpdate": 1614881704641,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3737,6 +3737,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5081,
             "range": "± 22",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9b0fa09152aea4de2f8a37628d0bebb9ebb50f3d",
+          "message": "litemap: Serialize to maps, add tests (#516)",
+          "timestamp": "2021-03-04T12:08:48-06:00",
+          "tree_id": "fac0d611c10d52bfd9390ec23ca617338358fd7e",
+          "url": "https://github.com/unicode-org/icu4x/commit/9b0fa09152aea4de2f8a37628d0bebb9ebb50f3d"
+        },
+        "date": 1614881700838,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3441,
+            "range": "± 46",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5167,
+            "range": "± 69",
             "unit": "ns/iter"
           }
         ]
