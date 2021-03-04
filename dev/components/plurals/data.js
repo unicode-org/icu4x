7741a8e15170534b16d605c6cfd8624d14f8e520
github-action-benchmark <github@users.noreply.github.com>
2021-03-04 20:49:04 +0000

    add Rust Benchmark (cargo) benchmark result for 2179f6fee8764aec332b2d251514ac30970ec021

--- dev/components/plurals/data.js
+++ dev/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614886001881,
+  "lastUpdate": 1614890944803,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3779,6 +3779,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 115190,
             "range": "± 8481",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2179f6fee8764aec332b2d251514ac30970ec021",
+          "message": "Add a cargo make command and a CI check for bincode (#521)",
+          "timestamp": "2021-03-04T14:42:19-06:00",
+          "tree_id": "e3ca34ef3a4d4f167fe1688db94643a4d44d8ed7",
+          "url": "https://github.com/unicode-org/icu4x/commit/2179f6fee8764aec332b2d251514ac30970ec021"
+        },
+        "date": 1614890942093,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1644,
+            "range": "± 13",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 7706,
+            "range": "± 142",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 111072,
+            "range": "± 1598",
             "unit": "ns/iter"
           }
         ]
