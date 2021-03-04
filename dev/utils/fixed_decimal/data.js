30485067d8f738d9cad552e8252be9645ce335c1
github-action-benchmark <github@users.noreply.github.com>
2021-03-04 20:48:13 +0000

    add Rust Benchmark (cargo) benchmark result for 2179f6fee8764aec332b2d251514ac30970ec021

--- dev/utils/fixed_decimal/data.js
+++ dev/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614885955371,
+  "lastUpdate": 1614890893733,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2699,6 +2699,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 135288,
             "range": "± 7588",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2179f6fee8764aec332b2d251514ac30970ec021",
+          "message": "Add a cargo make command and a CI check for bincode (#521)",
+          "timestamp": "2021-03-04T14:42:19-06:00",
+          "tree_id": "e3ca34ef3a4d4f167fe1688db94643a4d44d8ed7",
+          "url": "https://github.com/unicode-org/icu4x/commit/2179f6fee8764aec332b2d251514ac30970ec021"
+        },
+        "date": 1614890893080,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 146439,
+            "range": "± 10394",
             "unit": "ns/iter"
           }
         ]
