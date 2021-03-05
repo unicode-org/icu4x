aeef5125e1974cf597aae5e088b0edcd7f350184
github-action-benchmark <github@users.noreply.github.com>
2021-03-05 15:46:02 +0000

    add Rust Benchmark (cargo) benchmark result for 2d6664a376df209f86d4af47f0a686b91b8172e0

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614890918410,
+  "lastUpdate": 1614959162537,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3917,6 +3917,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5109,
             "range": "± 364",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2d6664a376df209f86d4af47f0a686b91b8172e0",
+          "message": "Move DTF to only store DateSymbols and a single pattern. (#518)",
+          "timestamp": "2021-03-05T07:40:03-08:00",
+          "tree_id": "c6e88347fda54bd0e73aba4c94510803800e4025",
+          "url": "https://github.com/unicode-org/icu4x/commit/2d6664a376df209f86d4af47f0a686b91b8172e0"
+        },
+        "date": 1614959161739,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3035,
+            "range": "± 169",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4499,
+            "range": "± 225",
             "unit": "ns/iter"
           }
         ]
