bcf31ef788e2726463ae0cf8dff347dc34f00354
Shane F. Carr <shane@unicode.org>
2021-03-12 02:40:13 -0600

    Update rust fmt

--- components/provider/tests/data_provider.rs
+++ components/provider/tests/data_provider.rs
@@ -225,7 +225,10 @@ fn test_warehouse_owned_dyn_generic() {
 fn test_warehouse_owned_dyn_erased_alt() {
     let warehouse = get_warehouse(DATA);
     let response = get_payload_alt(&warehouse as &dyn ErasedDataProvider);
-    assert!(matches!(response, Err(DataError::UnsupportedResourceKey { .. })));
+    assert!(matches!(
+        response,
+        Err(DataError::UnsupportedResourceKey { .. })
+    ));
 }
 
 #[test]
@@ -283,7 +286,10 @@ fn test_warehouse_ref_dyn_generic() {
 fn test_warehouse_ref_dyn_erased_alt() {
     let warehouse = get_warehouse(DATA);
     let response = get_payload_alt(&&warehouse as &dyn ErasedDataProvider);
-    assert!(matches!(response, Err(DataError::UnsupportedResourceKey { .. })));
+    assert!(matches!(
+        response,
+        Err(DataError::UnsupportedResourceKey { .. })
+    ));
 }
 
 #[test]
