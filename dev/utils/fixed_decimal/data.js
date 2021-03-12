a7af2bc1c4914996806f402a914aff7ae456db70
github-action-benchmark <github@users.noreply.github.com>
2021-03-12 21:07:51 +0000

    add Rust Benchmark (cargo) benchmark result for c5104b3ab7cce22e11a71f9797d1dbaff9b40f2c

--- dev/utils/fixed_decimal/data.js
+++ dev/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614959127477,
+  "lastUpdate": 1615583271012,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2759,6 +2759,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 145368,
             "range": "± 6494",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c5104b3ab7cce22e11a71f9797d1dbaff9b40f2c",
+          "message": "Small quality-of-life fixes (#543)",
+          "timestamp": "2021-03-12T15:01:43-06:00",
+          "tree_id": "01203783bcee98b2dc8dd6e86af26e01f3a2da42",
+          "url": "https://github.com/unicode-org/icu4x/commit/c5104b3ab7cce22e11a71f9797d1dbaff9b40f2c"
+        },
+        "date": 1615583270307,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 159353,
+            "range": "± 518",
             "unit": "ns/iter"
           }
         ]
