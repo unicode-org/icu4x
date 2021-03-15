aa87f76ab6be492324512a2600f50e058fb995b4
github-action-benchmark <github@users.noreply.github.com>
2021-03-15 17:30:13 +0000

    add Rust Benchmark (cargo) benchmark result for ed9db541404a2cc36c0c1f813f0b365a136ac085

--- dev/components/locid/data.js
+++ dev/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1615584371824,
+  "lastUpdate": 1615829413878,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4025,6 +4025,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5582,
             "range": "± 252",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ed9db541404a2cc36c0c1f813f0b365a136ac085",
+          "message": "Update testing instructions on CI doc (#482)\n\n* Update testing instructions on CI doc\r\n\r\n* Respond to PR feedback\r\n\r\n* Add section on caching to help ICU",
+          "timestamp": "2021-03-15T10:23:30-07:00",
+          "tree_id": "8641e96858fdad8258a651439f28f8a30b188fa4",
+          "url": "https://github.com/unicode-org/icu4x/commit/ed9db541404a2cc36c0c1f813f0b365a136ac085"
+        },
+        "date": 1615829411461,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3434,
+            "range": "± 52",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5000,
+            "range": "± 90",
             "unit": "ns/iter"
           }
         ]
