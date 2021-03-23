19263ceab7455b66f5615c9f77484c9f64c8b259
github-action-benchmark <github@users.noreply.github.com>
2021-03-23 23:40:09 +0000

    add Rust Benchmark (cargo) benchmark result for 43ad4cd4a13fea1878dc267ac18d5c681bc9fa8e

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1616177447588,
+  "lastUpdate": 1616542809810,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2969,6 +2969,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 155658,
             "range": "± 733",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "43ad4cd4a13fea1878dc267ac18d5c681bc9fa8e",
+          "message": "Split type erasure logic from erased_serde logic in ErasedDataProvider (#564)",
+          "timestamp": "2021-03-23T18:34:30-05:00",
+          "tree_id": "1ffcb907aaa7c75722c21a5e3b17fe792ca72603",
+          "url": "https://github.com/unicode-org/icu4x/commit/43ad4cd4a13fea1878dc267ac18d5c681bc9fa8e"
+        },
+        "date": 1616542808609,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 150186,
+            "range": "± 12497",
             "unit": "ns/iter"
           }
         ]
