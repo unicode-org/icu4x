4566836a8b7db80a4161a5e2e8ae836e1b823701
Erik Nordin <nordzilla@mozilla.com>
2021-03-26 23:27:57 -0700

    Fix license haders in relevant files

--- components/provider_cldr/src/transform/timezones/mod.rs
+++ components/provider_cldr/src/transform/timezones/mod.rs
@@ -1,6 +1,7 @@
 // This file is part of ICU4X. For terms of use, please see the file
 // called LICENSE at the top level of the ICU4X source tree
-// (online at: https://github.com/unicode-org/icu4x/blob/master/LICENSE ).
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 use crate::cldr_langid::CldrLangID;
 use crate::error::Error;
 use crate::reader::{get_subdirectories, open_reader};
