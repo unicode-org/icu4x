da55bb9d05ddbfe575051f4bcc1a2e84dfe031f5
github-action-benchmark <github@users.noreply.github.com>
2021-03-31 20:52:03 +0000

    add Rust Benchmark (cargo) benchmark result for 6991943a7378dadc5284d2ff487edc09f553a8ce

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617167154643,
+  "lastUpdate": 1617223923494,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4409,6 +4409,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 111172,
             "range": "± 5692",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6991943a7378dadc5284d2ff487edc09f553a8ce",
+          "message": "Simplify DeserializeSkeletonBincode with one less branch (#582)",
+          "timestamp": "2021-03-31T15:45:52-05:00",
+          "tree_id": "84ffa0d705f15e5fee280ffd3dfebc9239017f0c",
+          "url": "https://github.com/unicode-org/icu4x/commit/6991943a7378dadc5284d2ff487edc09f553a8ce"
+        },
+        "date": 1617223922305,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1225,
+            "range": "± 57",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6028,
+            "range": "± 335",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 84642,
+            "range": "± 3576",
             "unit": "ns/iter"
           }
         ]
