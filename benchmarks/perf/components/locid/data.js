5ad09e49b1e4973858b5e18e6f64be9e09df93b6
github-action-benchmark <github@users.noreply.github.com>
2021-04-02 20:03:08 +0000

    add Rust Benchmark (cargo) benchmark result for f55040c082e977103dcc22784bd5a2d414cf2d9d

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617320313975,
+  "lastUpdate": 1617393788130,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4529,6 +4529,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5359,
             "range": "± 20",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58569820+nordzilla@users.noreply.github.com",
+            "name": "Erik Nordin",
+            "username": "nordzilla"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f55040c082e977103dcc22784bd5a2d414cf2d9d",
+          "message": "Fix some broken links due to the switch from master -> main (#604)",
+          "timestamp": "2021-04-02T12:57:08-07:00",
+          "tree_id": "75c5d3e62b85fd1be2d2c7f0e35c589a03ae4875",
+          "url": "https://github.com/unicode-org/icu4x/commit/f55040c082e977103dcc22784bd5a2d414cf2d9d"
+        },
+        "date": 1617393786487,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3251,
+            "range": "± 107",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4756,
+            "range": "± 126",
             "unit": "ns/iter"
           }
         ]
