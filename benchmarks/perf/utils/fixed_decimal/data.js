70d00adedcf9ae6813c5c7d4281b1375bda1b336
github-action-benchmark <github@users.noreply.github.com>
2021-04-03 07:15:45 +0000

    add Rust Benchmark (cargo) benchmark result for ad53955ad557a1cd2c4e991f5df2b8ffe7bc15c5

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617433981833,
+  "lastUpdate": 1617434145831,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3299,6 +3299,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 130122,
             "range": "± 10724",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ad53955ad557a1cd2c4e991f5df2b8ffe7bc15c5",
+          "message": "Update triaging.md",
+          "timestamp": "2021-04-03T02:09:16-05:00",
+          "tree_id": "a0a0cc572905030980fc3e7f9d5a8cc83c1d2511",
+          "url": "https://github.com/unicode-org/icu4x/commit/ad53955ad557a1cd2c4e991f5df2b8ffe7bc15c5"
+        },
+        "date": 1617434144589,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 135834,
+            "range": "± 6706",
             "unit": "ns/iter"
           }
         ]
