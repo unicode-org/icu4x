f938a4d7b466253e684b6f681b14205071553be1
Shane F. Carr <shane@unicode.org>
2021-04-05 03:42:24 -0500

    Refactor and format examples

--- utils/litemap/examples/language_names_hash_map.rs
+++ utils/litemap/examples/language_names_hash_map.rs
@@ -9,8 +9,23 @@
 
 icu_benchmark_macros::static_setup!();
 
-use std::collections::HashMap;
+use icu_locid::subtags::Language;
 use icu_locid_macros::language;
+use std::collections::HashMap;
+
+const DATA: [(Language, &'static str); 11] = [
+    (language!("ar"), "Arabic"),
+    (language!("bn"), "Bangla"),
+    (language!("ccp"), "Chakma"),
+    (language!("en"), "English"),
+    (language!("es"), "Spanish"),
+    (language!("fr"), "French"),
+    (language!("ja"), "Japanese"),
+    (language!("ru"), "Russian"),
+    (language!("sr"), "Serbian"),
+    (language!("th"), "Thai"),
+    (language!("tr"), "Turkish"),
+];
 
 #[no_mangle]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
@@ -18,20 +33,12 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize {
 
     let mut map = HashMap::new();
     // https://github.com/rust-lang/rust/issues/62633 was declined :(
-    map.insert(language!("ar"), "Arabic").ok_or(()).unwrap_err();
-    map.insert(language!("bn"), "Bangla").ok_or(()).unwrap_err();
-    map.insert(language!("ccp"), "Chakma").ok_or(()).unwrap_err();
-    map.insert(language!("en"), "English").ok_or(()).unwrap_err();
-    map.insert(language!("es"), "Spanish").ok_or(()).unwrap_err();
-    map.insert(language!("fr"), "French").ok_or(()).unwrap_err();
-    map.insert(language!("ja"), "Japanese").ok_or(()).unwrap_err();
-    map.insert(language!("ru"), "Russian").ok_or(()).unwrap_err();
-    map.insert(language!("sr"), "Serbian").ok_or(()).unwrap_err();
-    map.insert(language!("th"), "Thai").ok_or(()).unwrap_err();
-    map.insert(language!("tr"), "Turkish").ok_or(()).unwrap_err();
+    for (lang, name) in DATA.iter() {
+        map.insert(lang, name).ok_or(()).unwrap_err();
+    }
 
     debug_assert_eq!(11, map.len());
-    debug_assert_eq!(Some(&"Thai"), map.get(&language!("th")));
+    debug_assert_eq!(Some(&&"Thai"), map.get(&language!("th")));
     debug_assert_eq!(None, map.get(&language!("de")));
 
     0
--- utils/litemap/examples/language_names_lite_map.rs
+++ utils/litemap/examples/language_names_lite_map.rs
@@ -9,8 +9,23 @@
 
 icu_benchmark_macros::static_setup!();
 
-use litemap::LiteMap;
+use icu_locid::subtags::Language;
 use icu_locid_macros::language;
+use litemap::LiteMap;
+
+const DATA: [(Language, &'static str); 11] = [
+    (language!("ar"), "Arabic"),
+    (language!("bn"), "Bangla"),
+    (language!("ccp"), "Chakma"),
+    (language!("en"), "English"),
+    (language!("es"), "Spanish"),
+    (language!("fr"), "French"),
+    (language!("ja"), "Japanese"),
+    (language!("ru"), "Russian"),
+    (language!("sr"), "Serbian"),
+    (language!("th"), "Thai"),
+    (language!("tr"), "Turkish"),
+];
 
 #[no_mangle]
 fn main(_argc: isize, _argv: *const *const u8) -> isize {
@@ -18,20 +33,12 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize {
 
     let mut map = LiteMap::new();
     // https://github.com/rust-lang/rust/issues/62633 was declined :(
-    map.try_append(language!("ar"), "Arabic").ok_or(()).unwrap_err();
-    map.try_append(language!("bn"), "Bangla").ok_or(()).unwrap_err();
-    map.try_append(language!("ccp"), "Chakma").ok_or(()).unwrap_err();
-    map.try_append(language!("en"), "English").ok_or(()).unwrap_err();
-    map.try_append(language!("es"), "Spanish").ok_or(()).unwrap_err();
-    map.try_append(language!("fr"), "French").ok_or(()).unwrap_err();
-    map.try_append(language!("ja"), "Japanese").ok_or(()).unwrap_err();
-    map.try_append(language!("ru"), "Russian").ok_or(()).unwrap_err();
-    map.try_append(language!("sr"), "Serbian").ok_or(()).unwrap_err();
-    map.try_append(language!("th"), "Thai").ok_or(()).unwrap_err();
-    map.try_append(language!("tr"), "Turkish").ok_or(()).unwrap_err();
+    for (lang, name) in DATA.iter() {
+        map.try_append(lang, name).ok_or(()).unwrap_err();
+    }
 
     debug_assert_eq!(11, map.len());
-    debug_assert_eq!(Some(&"Thai"), map.get(&language!("th")));
+    debug_assert_eq!(Some(&&"Thai"), map.get(&language!("th")));
     debug_assert_eq!(None, map.get(&language!("de")));
 
     0
