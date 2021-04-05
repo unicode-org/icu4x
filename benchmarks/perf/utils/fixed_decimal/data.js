644d2debc66ae4a3dbed4c513731398a5b57e42b
github-action-benchmark <github@users.noreply.github.com>
2021-04-05 19:32:45 +0000

    add Rust Benchmark (cargo) benchmark result for ee6501eca99d1f2fa0a5a920bc7c6ea3da972544

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617649267013,
+  "lastUpdate": 1617651165328,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3419,6 +3419,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 150635,
             "range": "± 5429",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ee6501eca99d1f2fa0a5a920bc7c6ea3da972544",
+          "message": "Add examples to LiteMap and improve WASM build tooling (#609)",
+          "timestamp": "2021-04-05T14:27:04-05:00",
+          "tree_id": "caddd5dcd160e1cb5c0147b32683c1df2e8c82a3",
+          "url": "https://github.com/unicode-org/icu4x/commit/ee6501eca99d1f2fa0a5a920bc7c6ea3da972544"
+        },
+        "date": 1617651164177,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 109806,
+            "range": "± 2547",
             "unit": "ns/iter"
           }
         ]
