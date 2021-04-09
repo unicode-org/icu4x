9b3c6db25a00d7300c024a9f94d1204d9ab41815
Elango Cheran <elango@unicode.org>
2021-04-08 22:45:43 -0700

    Make Uniset private for now

--- components/uniset/src/lib.rs
+++ components/uniset/src/lib.rs
@@ -2,6 +2,9 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+//! *** Note: DO NOT USE THESE APIs FOR NOW. ****
+//!  Performance improvements and other fixes are still needed on this component.
+//!
 //! `icu_uniset` is one of the [`ICU4X`] components.
 //!
 //! This API provides necessary functionality for highly efficient querying of sets of Unicode characters.
@@ -53,7 +56,7 @@
 mod builder;
 mod conversions;
 pub mod enum_props;
-pub mod props;
+mod props;
 pub mod provider;
 mod uniset;
 mod utils;
