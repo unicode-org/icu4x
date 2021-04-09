83bf649d8b0746f0f5850e6373d2651af832198d
github-action-benchmark <github@users.noreply.github.com>
2021-04-09 21:07:11 +0000

    add Rust Benchmark (cargo) benchmark result for ca7cd105377e9ae6ee4f2649a9867e13075125d6

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618002379744,
+  "lastUpdate": 1618002431706,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3599,6 +3599,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 129780,
             "range": "± 4894",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ca7cd105377e9ae6ee4f2649a9867e13075125d6",
+          "message": "Rename .iter() to .iter_chars() for UnicodeSet (#626)",
+          "timestamp": "2021-04-09T14:01:04-07:00",
+          "tree_id": "ac173cfac5564f177ec96e224ea05049976ef342",
+          "url": "https://github.com/unicode-org/icu4x/commit/ca7cd105377e9ae6ee4f2649a9867e13075125d6"
+        },
+        "date": 1618002429145,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 155695,
+            "range": "± 10386",
             "unit": "ns/iter"
           }
         ]
