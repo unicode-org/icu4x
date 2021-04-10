85279b47bd2d7a96de42345eefd247ae5533e7cc
Shane F. Carr <shane@unicode.org>
2021-04-10 16:45:15 -0500

    Use dlmalloc for Valgrind build task

--- tools/benchmark/macros/src/lib.rs
+++ tools/benchmark/macros/src/lib.rs
@@ -2,17 +2,25 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-// Re-export dhat so that macros can access it without needing dhat as a dependency.
+// Re-export tools so that macros can access it without needing a dependency.
 #[cfg(feature = "benchmark_memory")]
 pub use dhat;
+#[cfg(feature = "rust_global_allocator")]
+pub use dlmalloc;
 
-#[cfg(not(feature = "benchmark_memory"))]
+#[cfg(not(any(feature = "benchmark_memory", feature = "rust_global_allocator")))]
 #[macro_export]
 macro_rules! static_setup {
     () => {};
 }
 
-#[cfg(feature = "benchmark_memory")]
+#[cfg(all(feature = "benchmark_memory", feature = "rust_global_allocator"))]
+#[macro_export]
+macro_rules! static_setup {
+    () => {};
+}
+
+#[cfg(all(feature = "benchmark_memory", not(feature = "rust_global_allocator")))]
 #[macro_export]
 macro_rules! static_setup {
     () => {
@@ -21,6 +29,18 @@ macro_rules! static_setup {
         // Use the DhatAlloc global allocator to instrument memory usage.
         #[global_allocator]
         static ALLOCATOR: DhatAlloc = DhatAlloc;
+    };
+}
+
+#[cfg(all(feature = "rust_global_allocator", not(feature = "benchmark_memory")))]
+#[macro_export]
+macro_rules! static_setup {
+    () => {
+        use icu_benchmark_macros::dlmalloc::GlobalDlmalloc;
+
+        // Use Dlmalloc to remove the system allocator dependency
+        #[global_allocator]
+        static ALLOCATOR: GlobalDlmalloc = GlobalDlmalloc;
     };
 }
 
