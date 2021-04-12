ac6c68abef437896a3cef0d7c73029c546a9bbb4
github-action-benchmark <github@users.noreply.github.com>
2021-04-12 15:43:05 +0000

    add Rust Benchmark (cargo) benchmark result for 5079646bf049b3fd9a9ca75aa7ae913dbac173dc

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618002409686,
+  "lastUpdate": 1618242185803,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4679,6 +4679,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 31121601,
             "range": "± 66654",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5079646bf049b3fd9a9ca75aa7ae913dbac173dc",
+          "message": "Pin the stable version of Rust with rust-toolchain (#618)",
+          "timestamp": "2021-04-12T10:36:45-05:00",
+          "tree_id": "84e47ae208c5c912eb2edf4013993ea3d9c2db0c",
+          "url": "https://github.com/unicode-org/icu4x/commit/5079646bf049b3fd9a9ca75aa7ae913dbac173dc"
+        },
+        "date": 1618242184876,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 37170734,
+            "range": "± 402661",
             "unit": "ns/iter"
           }
         ]
