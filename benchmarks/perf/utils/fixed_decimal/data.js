16998d1647d32cd8341e26fc9f69bb1e233a2fa8
github-action-benchmark <github@users.noreply.github.com>
2021-04-13 17:12:09 +0000

    add Rust Benchmark (cargo) benchmark result for 180d4e5a43780ab078b462c00dc8f0328b5cc00b

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618332436349,
+  "lastUpdate": 1618333929167,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3809,6 +3809,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 145464,
             "range": "± 3581",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "180d4e5a43780ab078b462c00dc8f0328b5cc00b",
+          "message": "Optimize performance of LocaleCanonicalizer::maximize. (#504)\n\n* Add From<Subtag> for TinyStr.\r\n\r\n* Optimize performance of LocaleCanonicalizer by storing resources in custom data structures.",
+          "timestamp": "2021-04-13T10:05:36-07:00",
+          "tree_id": "a5a6b61f4fd777a23b2707a291d281b1845fba71",
+          "url": "https://github.com/unicode-org/icu4x/commit/180d4e5a43780ab078b462c00dc8f0328b5cc00b"
+        },
+        "date": 1618333924813,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 156047,
+            "range": "± 2024",
             "unit": "ns/iter"
           }
         ]
