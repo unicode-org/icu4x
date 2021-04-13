2fd2fb4ac093a76f08a7738bbfe7205d2cf5a107
github-action-benchmark <github@users.noreply.github.com>
2021-04-13 18:37:10 +0000

    add Rust Benchmark (cargo) benchmark result for 6d99323dc945a09af01d1147477584b469b6fe4b

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618338392389,
+  "lastUpdate": 1618339030722,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4919,6 +4919,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 35222588,
             "range": "± 1752006",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6d99323dc945a09af01d1147477584b469b6fe4b",
+          "message": "Mute clippy warnings in a few places, apply the suggestions in remaining cases. (#642)\n\n* Mute clippy warnings in a few places, apply the suggestions in remaining cases.\r\n\r\n* Fix clippy fmt\r\n\r\n* Separate line after license header\r\n\r\n* Remove accidentally added diffs",
+          "timestamp": "2021-04-13T11:30:56-07:00",
+          "tree_id": "b3ec6302e8f8bfcb8fea56433fbc548249ed7289",
+          "url": "https://github.com/unicode-org/icu4x/commit/6d99323dc945a09af01d1147477584b469b6fe4b"
+        },
+        "date": 1618339026414,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 28112892,
+            "range": "± 1441357",
             "unit": "ns/iter"
           }
         ]
