8035edf4dc13844bc67ddfc33bb3d432271b5ccb
github-action-benchmark <github@users.noreply.github.com>
2021-04-14 20:01:22 +0000

    add Rust Benchmark (cargo) benchmark result for 496e0759cf7f6810e248544a6cf964bbbf34425d

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618339040175,
+  "lastUpdate": 1618430482860,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -5321,6 +5321,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4503,
             "range": "± 277",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58569820+nordzilla@users.noreply.github.com",
+            "name": "Erik Nordin",
+            "username": "nordzilla"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "496e0759cf7f6810e248544a6cf964bbbf34425d",
+          "message": "Update ICU Component Documentation Examples (#616)\n\n* Update documentation for components/datetime\r\n\r\n* Update documentation for components/locid\r\n\r\n* Update documentation for components/plurals\r\n\r\n* Update documentation for components/uniset\r\n\r\n* Alphabetize Cargo.toml changes\r\n\r\n* Use cargo-readme to generate README files",
+          "timestamp": "2021-04-14T12:54:13-07:00",
+          "tree_id": "87ef342899e93da76627c071c21f8ad306fa5838",
+          "url": "https://github.com/unicode-org/icu4x/commit/496e0759cf7f6810e248544a6cf964bbbf34425d"
+        },
+        "date": 1618430477910,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3222,
+            "range": "± 139",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5154,
+            "range": "± 307",
             "unit": "ns/iter"
           }
         ]
