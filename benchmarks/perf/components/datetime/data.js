71bb3223b10d7183016b3ebc0226710b9d575d7c
github-action-benchmark <github@users.noreply.github.com>
2021-04-15 04:39:46 +0000

    add Rust Benchmark (cargo) benchmark result for a3d0a257cb7588fe912bd8a114e4ebb3fbf41280

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618430470717,
+  "lastUpdate": 1618461586665,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4007,6 +4007,42 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 2508766,
             "range": "± 20028",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a3d0a257cb7588fe912bd8a114e4ebb3fbf41280",
+          "message": "Update tinystr to 0.4.5 for perf wins on locid and locale_canonicalizer. (#646)\n\n* Update tinystr to 0.4.4 for perf wins on locid and locale_canonicalizer.\r\n\r\n* Update to tinystr 0.4.5",
+          "timestamp": "2021-04-14T21:32:52-07:00",
+          "tree_id": "8efe3c114253eafafff86f7ed6df39ab8064ac25",
+          "url": "https://github.com/unicode-org/icu4x/commit/a3d0a257cb7588fe912bd8a114e4ebb3fbf41280"
+        },
+        "date": 1618461584160,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 1157115,
+            "range": "± 71148",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 2277208,
+            "range": "± 243521",
             "unit": "ns/iter"
           }
         ]
