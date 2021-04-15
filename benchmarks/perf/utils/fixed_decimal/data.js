d5e57f1006aeb45a48b6c78004e8c7b9160d5dc0
github-action-benchmark <github@users.noreply.github.com>
2021-04-15 04:39:22 +0000

    add Rust Benchmark (cargo) benchmark result for a3d0a257cb7588fe912bd8a114e4ebb3fbf41280

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618430429850,
+  "lastUpdate": 1618461562275,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -3929,6 +3929,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 141881,
             "range": "± 4591",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a3d0a257cb7588fe912bd8a114e4ebb3fbf41280",
+          "message": "Update tinystr to 0.4.5 for perf wins on locid and locale_canonicalizer. (#646)\n\n* Update tinystr to 0.4.4 for perf wins on locid and locale_canonicalizer.\r\n\r\n* Update to tinystr 0.4.5",
+          "timestamp": "2021-04-14T21:32:52-07:00",
+          "tree_id": "8efe3c114253eafafff86f7ed6df39ab8064ac25",
+          "url": "https://github.com/unicode-org/icu4x/commit/a3d0a257cb7588fe912bd8a114e4ebb3fbf41280"
+        },
+        "date": 1618461561483,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 138502,
+            "range": "± 6761",
             "unit": "ns/iter"
           }
         ]
