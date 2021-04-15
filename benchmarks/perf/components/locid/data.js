46ff5236b11b9316bc9f8d765c03a8310fcf8d0c
github-action-benchmark <github@users.noreply.github.com>
2021-04-15 21:47:40 +0000

    add Rust Benchmark (cargo) benchmark result for 0b328f8a4cff123fc48416c7a03d0f96cc9c2ab0

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618513988830,
+  "lastUpdate": 1618523260955,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -5429,6 +5429,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5023,
             "range": "± 356",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0b328f8a4cff123fc48416c7a03d0f96cc9c2ab0",
+          "message": "Adding ZeroVec, zero-copy vector abstraction over a byte buffer (#647)",
+          "timestamp": "2021-04-15T16:41:00-05:00",
+          "tree_id": "f3eadaebeff876cfbe022497a6417c1756993eec",
+          "url": "https://github.com/unicode-org/icu4x/commit/0b328f8a4cff123fc48416c7a03d0f96cc9c2ab0"
+        },
+        "date": 1618523259602,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3510,
+            "range": "± 32",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5147,
+            "range": "± 42",
             "unit": "ns/iter"
           }
         ]
