bdd54f84849889dc1db04347ca69be86ea6d0945
github-action-benchmark <github@users.noreply.github.com>
2021-04-16 06:02:13 +0000

    add Rust Benchmark (cargo) benchmark result for d6e5e2d96bcf62d27d07f0ffe3acff8b82e44e08

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618523209726,
+  "lastUpdate": 1618552933218,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4019,6 +4019,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 129676,
             "range": "± 473",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d6e5e2d96bcf62d27d07f0ffe3acff8b82e44e08",
+          "message": "Adding Valgrind build task (#631)",
+          "timestamp": "2021-04-16T00:56:17-05:00",
+          "tree_id": "3dfdb297dd2df7f83d2283f739da22a8d7eeb60d",
+          "url": "https://github.com/unicode-org/icu4x/commit/d6e5e2d96bcf62d27d07f0ffe3acff8b82e44e08"
+        },
+        "date": 1618552931923,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 129755,
+            "range": "± 513",
             "unit": "ns/iter"
           }
         ]
