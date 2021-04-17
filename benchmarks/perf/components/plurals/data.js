557e81232260f3b8ba6407d54c7b44ad2aa2af1a
github-action-benchmark <github@users.noreply.github.com>
2021-04-17 02:10:48 +0000

    add Rust Benchmark (cargo) benchmark result for a46f3336d0a2f0c9c9193fc2e0893c8834e30fcf

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618624079050,
+  "lastUpdate": 1618625447969,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -5795,6 +5795,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 95133,
             "range": "± 4692",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58569820+nordzilla@users.noreply.github.com",
+            "name": "Erik Nordin",
+            "username": "nordzilla"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a46f3336d0a2f0c9c9193fc2e0893c8834e30fcf",
+          "message": "Remove .DS_Store files and add them to the .gitignore (#655)",
+          "timestamp": "2021-04-16T19:03:34-07:00",
+          "tree_id": "1b73a4ca319bae725e66ef371f86f6b667f77836",
+          "url": "https://github.com/unicode-org/icu4x/commit/a46f3336d0a2f0c9c9193fc2e0893c8834e30fcf"
+        },
+        "date": 1618625446626,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1321,
+            "range": "± 48",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 7501,
+            "range": "± 294",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 118687,
+            "range": "± 5479",
             "unit": "ns/iter"
           }
         ]
