148bd6139c8179211341e2b44cf0310dbe1c006b
github-action-benchmark <github@users.noreply.github.com>
2021-04-20 20:50:44 +0000

    add Rust Benchmark (cargo) benchmark result for 938f943154dc8574a9d6e70c130cea2ee05ed527

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618866085231,
+  "lastUpdate": 1618951844231,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -5861,6 +5861,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5237,
             "range": "± 188",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "938f943154dc8574a9d6e70c130cea2ee05ed527",
+          "message": "Auto and manually apply selected nursery clippies. (#661)",
+          "timestamp": "2021-04-20T13:43:52-07:00",
+          "tree_id": "32b06d6e7705aafb49f1e9e9a9a4b6840faf451f",
+          "url": "https://github.com/unicode-org/icu4x/commit/938f943154dc8574a9d6e70c130cea2ee05ed527"
+        },
+        "date": 1618951842664,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3548,
+            "range": "± 115",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 6140,
+            "range": "± 551",
             "unit": "ns/iter"
           }
         ]
