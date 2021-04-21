7d78c20bd171cbdf15fd9026bac4e6b2997baa41
Manish Goregaokar <manishsmail@gmail.com>
2021-04-20 19:44:52 -0700

    Add VarZeroVec::into_owned() (#666)

--- utils/zerovec/src/varzerovec/mod.rs
+++ utils/zerovec/src/varzerovec/mod.rs
@@ -327,15 +327,45 @@ impl<'a, T: AsVarULE> VarZeroVec<'a, T> {
         }
     }
 
+    /// Converts a borrowed ZeroVec to an owned ZeroVec. No-op if already owned.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::str::Utf8Error;
+    /// # use zerovec::VarZeroVecError;
+    /// # use zerovec::VarZeroVec;
+    ///
+    /// let strings = vec!["foo".to_owned(), "bar".to_owned(),
+    ///                    "baz".to_owned(), "quux".to_owned()];
+    /// let bytes = VarZeroVec::get_serializable_bytes(&strings).unwrap();
+    /// let mut vec: VarZeroVec<String> = VarZeroVec::try_from_bytes(&bytes)?;
+    ///
+    /// assert_eq!(vec.len(), 4);
+    /// // has 'static lifetime
+    /// let owned = vec.into_owned();
+    /// # Ok::<(), VarZeroVecError<Utf8Error>>(())
+    /// ```
+    pub fn into_owned(mut self) -> VarZeroVec<'static, T>
+    where
+        T: Clone,
+    {
+        self.make_mut();
+        match self.0 {
+            VarZeroVecInner::Owned(vec) => vec.into(),
+            _ => unreachable!(),
+        }
+    }
+
     /// Obtain an owned `Vec<T>` out of this
-    pub fn to_owned(&self) -> Vec<T>
+    pub fn to_vec(&self) -> Vec<T>
     where
         T: Clone,
     {
         let mut new = self.clone();
         new.make_mut();
         match new.0 {
-            VarZeroVecInner::Owned(vec) => vec,
+            VarZeroVecInner::Owned(vec) => vec.into(),
             _ => unreachable!(),
         }
     }
@@ -366,7 +396,7 @@ impl<'a, T: AsVarULE> VarZeroVec<'a, T> {
     /// let bytes = VarZeroVec::get_serializable_bytes(&strings).unwrap();
     ///
     /// let mut borrowed: VarZeroVec<String> = VarZeroVec::try_from_bytes(&bytes)?;
-    /// assert_eq!(borrowed.to_owned(), strings);
+    /// assert_eq!(borrowed, &*strings);
     ///
     /// # Ok::<(), VarZeroVecError<Utf8Error>>(())
     /// ```
