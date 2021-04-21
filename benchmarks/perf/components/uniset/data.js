973bce7806ec7b02df5bce75d6c1e41415a41b44
github-action-benchmark <github@users.noreply.github.com>
2021-04-21 02:51:34 +0000

    add Rust Benchmark (cargo) benchmark result for 7d78c20bd171cbdf15fd9026bac4e6b2997baa41

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618951806726,
+  "lastUpdate": 1618973494887,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -5429,6 +5429,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34627101,
             "range": "± 1067433",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7d78c20bd171cbdf15fd9026bac4e6b2997baa41",
+          "message": "Add VarZeroVec::into_owned() (#666)",
+          "timestamp": "2021-04-20T19:44:52-07:00",
+          "tree_id": "270fd9cdd7909625fe6cbd8305f20bd4df88069f",
+          "url": "https://github.com/unicode-org/icu4x/commit/7d78c20bd171cbdf15fd9026bac4e6b2997baa41"
+        },
+        "date": 1618973493368,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 31799315,
+            "range": "± 2167817",
             "unit": "ns/iter"
           }
         ]
