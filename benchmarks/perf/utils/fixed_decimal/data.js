38895ff9f86b1a102ab650b10b9b6e59289e4d75
github-action-benchmark <github@users.noreply.github.com>
2021-04-22 17:45:24 +0000

    add Rust Benchmark (cargo) benchmark result for 6daf09323a50001967fcabd778d96aa9176f3cc5

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618973473109,
+  "lastUpdate": 1619113524440,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4409,6 +4409,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 132782,
             "range": "± 6234",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6daf09323a50001967fcabd778d96aa9176f3cc5",
+          "message": "Make it so that unsupported skeleton fields only emit a warning (#670)",
+          "timestamp": "2021-04-22T12:38:53-05:00",
+          "tree_id": "68d8c45c4df3696e814f2666ec28c859ec5aabda",
+          "url": "https://github.com/unicode-org/icu4x/commit/6daf09323a50001967fcabd778d96aa9176f3cc5"
+        },
+        "date": 1619113522913,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 146411,
+            "range": "± 6713",
             "unit": "ns/iter"
           }
         ]
