9b8b0f35c9ef0091435ea8c23e1400f6606b8452
github-action-benchmark <github@users.noreply.github.com>
2021-04-26 22:14:11 +0000

    add Rust Benchmark (cargo) benchmark result for 9015f7820c614afd288567efb2cd80b1eccd2a2e

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619464269679,
+  "lastUpdate": 1619475251101,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -5609,6 +5609,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 27469396,
             "range": "± 1480029",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9015f7820c614afd288567efb2cd80b1eccd2a2e",
+          "message": "Update failing generated-readme-check error message (#641)",
+          "timestamp": "2021-04-26T17:07:01-05:00",
+          "tree_id": "ad42378f451284a1a1ac7b05308b91f7e8c43b5d",
+          "url": "https://github.com/unicode-org/icu4x/commit/9015f7820c614afd288567efb2cd80b1eccd2a2e"
+        },
+        "date": 1619475249456,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 36503779,
+            "range": "± 1980657",
             "unit": "ns/iter"
           }
         ]
