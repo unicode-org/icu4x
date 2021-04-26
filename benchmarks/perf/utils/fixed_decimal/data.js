e5d3ece64cea77e7cf23c89b0c9a48956aaeeb69
github-action-benchmark <github@users.noreply.github.com>
2021-04-26 19:11:26 +0000

    add Rust Benchmark (cargo) benchmark result for 04ec2da09a759409019103e3aa7ca65337811cd1

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619462124006,
+  "lastUpdate": 1619464286904,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4529,6 +4529,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 167822,
             "range": "± 565",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "04ec2da09a759409019103e3aa7ca65337811cd1",
+          "message": "Change the components bag to default to None (#675)",
+          "timestamp": "2021-04-26T14:04:45-05:00",
+          "tree_id": "57980f5a294511c1540dd4902a0e0a38e1a8d586",
+          "url": "https://github.com/unicode-org/icu4x/commit/04ec2da09a759409019103e3aa7ca65337811cd1"
+        },
+        "date": 1619464285852,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 144069,
+            "range": "± 8017",
             "unit": "ns/iter"
           }
         ]
