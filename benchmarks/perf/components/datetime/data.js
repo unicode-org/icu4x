633cebf0373d2cd04a469b44ba89650f51bf796b
github-action-benchmark <github@users.noreply.github.com>
2021-04-29 18:37:31 +0000

    add Rust Benchmark (cargo) benchmark result for 8e93c88897cdd086c49524ef05c7f383a885d804

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619720634071,
+  "lastUpdate": 1619721451592,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -5123,6 +5123,42 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 2395761,
             "range": "± 205107",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "distinct": true,
+          "id": "8e93c88897cdd086c49524ef05c7f383a885d804",
+          "message": "Add description to decimal",
+          "timestamp": "2021-04-29T11:30:16-07:00",
+          "tree_id": "a163e1859f11737ea095d198c3ad7bbc22fec00b",
+          "url": "https://github.com/unicode-org/icu4x/commit/8e93c88897cdd086c49524ef05c7f383a885d804"
+        },
+        "date": 1619721449852,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 1298580,
+            "range": "± 3429",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 2480048,
+            "range": "± 6313",
             "unit": "ns/iter"
           }
         ]
