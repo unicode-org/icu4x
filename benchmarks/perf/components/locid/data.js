41fe73c28d38c3906fe09f43b51053db27387b4d
github-action-benchmark <github@users.noreply.github.com>
2021-04-29 20:17:58 +0000

    add Rust Benchmark (cargo) benchmark result for 3a458106e62befb111b54fd62014b5b712fc2e8c

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619721466200,
+  "lastUpdate": 1619727478199,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6509,6 +6509,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5413,
             "range": "± 317",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3a458106e62befb111b54fd62014b5b712fc2e8c",
+          "message": "Update README.md to 0.2 (#691)",
+          "timestamp": "2021-04-29T13:11:07-07:00",
+          "tree_id": "4399d22e228d2353940543901bd575f1890e658f",
+          "url": "https://github.com/unicode-org/icu4x/commit/3a458106e62befb111b54fd62014b5b712fc2e8c"
+        },
+        "date": 1619727476635,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3301,
+            "range": "± 36",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5079,
+            "range": "± 41",
             "unit": "ns/iter"
           }
         ]
