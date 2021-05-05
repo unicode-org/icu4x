ef0405104145562756929b9b1748b700c92a57e9
github-action-benchmark <github@users.noreply.github.com>
2021-05-05 19:44:28 +0000

    add Rust Benchmark (cargo) benchmark result for d0f4879f5ab94dca5494925c0a9fd0c44eb30f2a

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1620194281157,
+  "lastUpdate": 1620243868389,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6089,6 +6089,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 30802626,
             "range": "± 1654767",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d0f4879f5ab94dca5494925c0a9fd0c44eb30f2a",
+          "message": "Discussion about correctness, performance, and style (#701)",
+          "timestamp": "2021-05-05T14:37:36-05:00",
+          "tree_id": "ab5169a90ccd68e8463d034a4b324f2f5ab3326c",
+          "url": "https://github.com/unicode-org/icu4x/commit/d0f4879f5ab94dca5494925c0a9fd0c44eb30f2a"
+        },
+        "date": 1620243866765,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 34069085,
+            "range": "± 1588455",
             "unit": "ns/iter"
           }
         ]
