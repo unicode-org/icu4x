dd93a751bc733490c2c11f85e5889e3e45bf0459
github-action-benchmark <github@users.noreply.github.com>
2021-05-07 17:36:16 +0000

    add Rust Benchmark (cargo) benchmark result for 3085cc6dbe3018a4029c82f10b0f6452fde949fd

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1620243868389,
+  "lastUpdate": 1620408976032,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6119,6 +6119,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34069085,
             "range": "± 1588455",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3085cc6dbe3018a4029c82f10b0f6452fde949fd",
+          "message": "Add GitHub workflow for documentation preview (#694)",
+          "timestamp": "2021-05-07T12:30:38-05:00",
+          "tree_id": "c22f52d0d9c1e4495fb9dc3cf4a52d0205745e69",
+          "url": "https://github.com/unicode-org/icu4x/commit/3085cc6dbe3018a4029c82f10b0f6452fde949fd"
+        },
+        "date": 1620408971747,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 29815291,
+            "range": "± 631307",
             "unit": "ns/iter"
           }
         ]
