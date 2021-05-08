0a1257ab81f5740c751523be40cc61d9607d0d3c
github-action-benchmark <github@users.noreply.github.com>
2021-05-08 18:26:18 +0000

    add Rust Benchmark (cargo) benchmark result for 8a91a7c33f5bb56f52c198161a4a4c3a117583a9

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1620409273415,
+  "lastUpdate": 1620498378249,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6797,6 +6797,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4522,
             "range": "± 197",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8a91a7c33f5bb56f52c198161a4a4c3a117583a9",
+          "message": "Require GCP key to build docs preview (#708)",
+          "timestamp": "2021-05-08T13:19:59-05:00",
+          "tree_id": "0595fdc51c3a25f889e671b09e13dd8facc63db6",
+          "url": "https://github.com/unicode-org/icu4x/commit/8a91a7c33f5bb56f52c198161a4a4c3a117583a9"
+        },
+        "date": 1620498376728,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3195,
+            "range": "± 98",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4774,
+            "range": "± 133",
             "unit": "ns/iter"
           }
         ]
