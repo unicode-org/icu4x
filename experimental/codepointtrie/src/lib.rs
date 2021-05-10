9143a09ab619188369b3d1398e72b723ba635324
Elango Cheran <elango@unicode.org>
2021-05-10 14:03:11 -0700

    Update enum names; construct a test CodePointTrie struct instance

--- experimental/codepointtrie/src/lib.rs
+++ experimental/codepointtrie/src/lib.rs
@@ -5,22 +5,25 @@ pub fn hello() {
 /// The width of the elements in the data array of a CodePointTrie.
 /// See UCPTrieValueWidth in ICU4C.
 pub enum CodePointTrieValueWidth {
-    ValueBitsAny = -1,
-    ValueBits16 = 0,
-    ValueBits32 = 1,
-    ValueBits8 = 2,
+    BitsAny = -1,
+    Bits16 = 0,
+    Bits32 = 1,
+    Bits8 = 2,
 }
 
 /// The type of trie represents whether the trie has an optimization that
 /// would make it small or fast.
 /// See UCPTrieType in ICU4C.
 pub enum CodePointTrieType {
-    CodePointTrieTypeAny = -1,
-    CodePointTrieTypeFast = 0,
-    CodePointTrieTypeSmall = 1,
+    Any = -1,
+    Fast = 0,
+    Small = 1,
 }
 
 pub struct CodePointTrieData<'trie> {
+    // void: bool,  // Do we need an equivalent to the `void *ptr0` field in
+    // UCPTrieData? Is it derivative of a None value for the other fields? Is
+    // there a performance / convenience to having it?
     data_8_bit: Option<&'trie [u8]>,
     data_16_bit: Option<&'trie [u16]>,
     data_32_bit: Option<&'trie [u32]>,
@@ -40,26 +43,42 @@ pub struct CodePointTrie<'trie> {
     data: &'trie CodePointTrieData<'trie>,
 }
 
+pub fn get_code_point_trie_type(trie_type_int: u8) -> CodePointTrieType {
+    match trie_type_int {
+        0 => CodePointTrieType::Fast,
+        1 => CodePointTrieType::Small,
+        _ => CodePointTrieType::Any,
+    }
+}
+
+pub fn get_code_point_trie_value_width(value_width_int: u8) -> CodePointTrieValueWidth {
+    match value_width_int {
+        0 => CodePointTrieValueWidth::Bits16,
+        1 => CodePointTrieValueWidth::Bits32,
+        2 => CodePointTrieValueWidth::Bits8,
+        _ => CodePointTrieValueWidth::BitsAny
+    }
+}
 
 // pub fn fast_trie_fast_range_data_array_index(cp: i32) -> i32 {
 // }
 
 #[test]
 pub fn bit_shifting_test() {
-    let _index_length: u32 = 1024;
-    let _data_length: u32 = 260;
+    let index_length: u32 = 1024;
+    let data_length: u32 = 260;
     // Question: in ICU4C, `highStart` is a `UChar32` type. Does it make sense
     // to represent it as a u32 since UnicodeSet deals with `u32` instead of
     // the Rust `char` type?
-    let _high_start: u32 = 0xa00;
-    let _shifted12_high_start: u16 = 0x1;
-    let _trie_type: u8 = 0;
-    let _value_width: u8 = 2;
-    let _index3_null_offset: u16 = 0x7fff;
-    let _data_null_offset: u32 = 0x0;
-    let _null_value: u32 = 0x1;
+    let high_start: u32 = 0xa00;
+    let shifted12_high_start: u16 = 0x1;
+    let trie_type: u8 = 0;
+    let value_width: u8 = 2;
+    let index3_null_offset: u16 = 0x7fff;
+    let data_null_offset: u32 = 0x0;
+    let null_value: u32 = 0x1;
 
-    let _index: [u16; 1024] = [
+    let index: [u16; 1024] = [
         0, 0x40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0x80, 0xc0, 0xc0, 0xc0, 0xc0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -97,7 +116,7 @@ pub fn bit_shifting_test() {
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ];
 
-    let _data_8: [u8; 260] = [
+    let data_8: [u8; 260] = [
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -108,6 +127,26 @@ pub fn bit_shifting_test() {
         3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
         3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 0xad,
     ];
+
+    let trie: CodePointTrie =
+        CodePointTrie {
+            index_length,
+            data_length,
+            high_start,
+            shifted12_high_start,
+            trie_type: get_code_point_trie_type(trie_type),
+            value_width: get_code_point_trie_value_width(value_width),
+            index3_null_offset,
+            data_null_offset,
+            null_value,
+            index: &index,
+            data: 
+                &CodePointTrieData {
+                    data_8_bit: Some(&data_8),
+                    data_16_bit: None,
+                    data_32_bit: None,
+                },
+        };
 
     let _check_ranges: [u32; 10] = [0, 1, 0x740, 1, 0x780, 2, 0x880, 3, 0x110000, 1];
 
