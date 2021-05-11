77186b6ab109d30f0d1bc16b117142196d7c7e19
github-action-benchmark <github@users.noreply.github.com>
2021-05-11 17:26:25 +0000

    add Rust Benchmark (cargo) benchmark result for 51e1e72b8a6a4d2df0221e8f617e3d9a6819b815

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1620704895019,
+  "lastUpdate": 1620753984993,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -5189,6 +5189,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 138836,
             "range": "± 8407",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "51e1e72b8a6a4d2df0221e8f617e3d9a6819b815",
+          "message": "Fix keyword in Cargo.toml (#714)\n\n* Fix keyword in Cargo.toml\r\n\r\n* Update zerovec crate",
+          "timestamp": "2021-05-11T10:19:50-07:00",
+          "tree_id": "87f47eb5a6b8194bcf66add1ca291a2fd29b58ba",
+          "url": "https://github.com/unicode-org/icu4x/commit/51e1e72b8a6a4d2df0221e8f617e3d9a6819b815"
+        },
+        "date": 1620753983051,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 147304,
+            "range": "± 9298",
             "unit": "ns/iter"
           }
         ]
