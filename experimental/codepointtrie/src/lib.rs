0041b24324ffba012530e057fca4cbc82ab8162c
Elango Cheran <elango@unicode.org>
2021-05-10 20:32:34 -0700

    Add untested remaining code for data array index pos getter helper fns

--- experimental/codepointtrie/src/lib.rs
+++ experimental/codepointtrie/src/lib.rs
@@ -2,7 +2,53 @@ pub fn hello() {
     println!("Hello, world!");
 }
 
-const CODE_POINT_TRIE_SMALL_TYPE_FAST_INDEXING_LIMIT: u32 = 0xfff;
+const CODE_POINT_TRIE_FAST_TYPE_SHIFT: i32 = 6;
+
+const CODE_POINT_TRIE_FAST_TYPE_DATA_BLOCK_LENGTH: u32 = 1 << CODE_POINT_TRIE_FAST_TYPE_SHIFT;
+
+const CODE_POINT_TRIE_FAST_TYPE_DATA_MASK: u32 = CODE_POINT_TRIE_FAST_TYPE_DATA_BLOCK_LENGTH - 1;
+
+// Fast indexing limit for "fast"-type trie
+const CODE_POINT_TRIE_FAST_TYPE_FAST_INDEXING_MAX: u32 = 0xffff;
+
+// Fast indexing limit for "small"-type trie
+const CODE_POINT_TRIE_SMALL_TYPE_FAST_INDEXING_MAX: u32 = 0xfff;
+
+const CODE_POINT_TRIE_ERROR_VALUE_NEG_DATA_OFFSET: u32 = 1;
+
+const CODE_POINT_TRIE_HIGH_VALUE_NEG_DATA_OFFSET: u32 = 2;
+
+const CODE_POINT_TRIE_BMP_INDEX_LENGTH: u32 = 0x10000 >> CODE_POINT_TRIE_FAST_TYPE_SHIFT;
+
+const CODE_POINT_TRIE_SMALL_LIMIT: u32 = 0x10000;
+
+const CODE_POINT_TRIE_SMALL_INDEX_LENGTH: u32 = CODE_POINT_TRIE_SMALL_LIMIT >> CODE_POINT_TRIE_FAST_TYPE_SHIFT;
+
+const CODE_POINT_TRIE_SHIFT_3: u32 = 4;
+
+const CODE_POINT_TRIE_SHIFT_2: u32 = 5 + CODE_POINT_TRIE_SHIFT_3;
+
+const CODE_POINT_TRIE_SHIFT_1: u32 = 5 + CODE_POINT_TRIE_SHIFT_2;
+
+const CODE_POINT_TRIE_SHIFT_2_3: u32 = CODE_POINT_TRIE_SHIFT_2 - CODE_POINT_TRIE_SHIFT_3;
+
+const CODE_POINT_TRIE_SHIFT_1_2: u32 = CODE_POINT_TRIE_SHIFT_1 - CODE_POINT_TRIE_SHIFT_2;
+
+const CODE_POINT_TRIE_OMITTED_BMP_INDEX_1_LENGTH: u32 = 0x10000 >> CODE_POINT_TRIE_SHIFT_1;
+
+const CODE_POINT_TRIE_INDEX_2_BLOCK_LENGTH: u32 = 1 << CODE_POINT_TRIE_SHIFT_2;
+
+const CODE_POINT_TRIE_INDEX_2_MASK: u32 = CODE_POINT_TRIE_INDEX_2_BLOCK_LENGTH - 1;
+
+const CODE_POINT_TRIE_CP_PER_INDEX_2_ENTRY: u32 = 1 << CODE_POINT_TRIE_SHIFT_2;
+
+const CODE_POINT_TRIE_INDEX_3_BLOCK_LENGTH: u32 = 1 << CODE_POINT_TRIE_SHIFT_2_3;
+
+const CODE_POINT_TRIE_INDEX_3_MASK: u32 = CODE_POINT_TRIE_INDEX_3_BLOCK_LENGTH - 1;
+
+const CODE_POINT_TRIE_SMALL_DATA_BLOCK_LENGTH: u32 = 1 << CODE_POINT_TRIE_SHIFT_3;
+
+const CODE_POINT_TRIE_SMALL_DATA_MASK: u32 = CODE_POINT_TRIE_SMALL_DATA_BLOCK_LENGTH - 1;
 
 /// The width of the elements in the data array of a CodePointTrie.
 /// See UCPTrieValueWidth in ICU4C.
@@ -63,13 +109,55 @@ fn get_code_point_trie_value_width(value_width_int: u8) -> CodePointTrieValueWid
     }
 }
 
+fn trie_internal_small_index(trie: &CodePointTrie, c: u32) -> u32 {
+    let mut i1: u32 = c >> CODE_POINT_TRIE_SHIFT_1;
+    if trie.trie_type == CodePointTrieType::Fast {
+        assert!(0xffff < c && c < trie.high_start);
+        i1 = i1 + CODE_POINT_TRIE_BMP_INDEX_LENGTH - CODE_POINT_TRIE_OMITTED_BMP_INDEX_1_LENGTH;
+    } else {
+        assert!(c < trie.high_start && trie.high_start > CODE_POINT_TRIE_SMALL_LIMIT);
+        i1 = i1 + CODE_POINT_TRIE_SMALL_INDEX_LENGTH;
+    }
+    let mut i3_block: u32 = trie.index[ (trie.index[i1 as usize] as u32 + ((c >> CODE_POINT_TRIE_SHIFT_2) & CODE_POINT_TRIE_INDEX_2_MASK)) as usize] as u32;
+    let mut i3: u32 = (c >> CODE_POINT_TRIE_SHIFT_3) & CODE_POINT_TRIE_INDEX_3_MASK;
+    let mut data_block: u32;
+    if i3_block & 0x8000 == 0 {
+        // 16-bit indexes
+        data_block = trie.index[(i3_block + i3) as usize] as u32;
+    } else {
+        // 18-bit indexes stored in groups of 9 entries per 8 indexes.
+        i3_block = (i3_block & 0x7fff) + (i3 & !7) + (i3 >> 3);
+        i3 = i3 & 7;
+        data_block = ((trie.index[(i3_block + 1) as usize] << (2 + (2 * i3))) as u32 & 0x30000) as u32;
+        data_block = data_block | trie.index[(i3_block + i3) as usize] as u32;
+    }
+    data_block + (c & CODE_POINT_TRIE_SMALL_DATA_MASK)
+}
+
+fn trie_small_index(trie: &CodePointTrie, c: u32) -> u32 {
+    if c >= trie.high_start {
+        trie.data_length - CODE_POINT_TRIE_HIGH_VALUE_NEG_DATA_OFFSET
+    } else {
+        trie_internal_small_index(trie, c)
+    }
+}
+
+fn trie_fast_index(trie: &CodePointTrie, c: u32) -> u32 {
+    let index_array_pos: u32 = (c >> CODE_POINT_TRIE_FAST_TYPE_SHIFT) +
+        (c & CODE_POINT_TRIE_FAST_TYPE_DATA_MASK);
+    trie.index[index_array_pos as usize] as u32
+}
+
 /// Get trie data array index position for code point value `c` that is beyond
 /// ASCII range.
 fn trie_cp_index(trie: &CodePointTrie, fast_max: u32, c: u32) -> u32 {
-    
-    // TODO: implement
-
-    0
+    if c <= fast_max {
+        trie_fast_index(trie, c)
+    } else if c < 0x10ffff {
+        trie_small_index(trie, c)
+    } else {
+        trie.data_length - CODE_POINT_TRIE_ERROR_VALUE_NEG_DATA_OFFSET
+    }
 }
 
 /// Get trie data array index position for code point value `c`.
@@ -79,9 +167,9 @@ fn trie_index(trie: &CodePointTrie, c: u32) -> u32 {
     } else {
         let fast_indexing_limit: u32 =
             if trie.trie_type == CodePointTrieType::Fast {
-                0xffff
+                CODE_POINT_TRIE_FAST_TYPE_FAST_INDEXING_MAX // 0xffff
             } else {
-                CODE_POINT_TRIE_SMALL_TYPE_FAST_INDEXING_LIMIT
+                CODE_POINT_TRIE_SMALL_TYPE_FAST_INDEXING_MAX // 0xfff
             };
         trie_cp_index(trie, fast_indexing_limit, c)
     }
@@ -95,8 +183,10 @@ fn trie_index(trie: &CodePointTrie, c: u32) -> u32 {
 //     }
 // }
 
+// Exported trie data from free-blocks.8.toml. This file represents a
+// fast-type trie with 8-bit width data.
 #[test]
-pub fn bit_shifting_test() {
+pub fn fast_type_8_bit_trie_test() {
     let index_length: u32 = 1024;
     let data_length: u32 = 260;
     // Question: in ICU4C, `highStart` is a `UChar32` type. Does it make sense
@@ -183,9 +273,14 @@ pub fn bit_shifting_test() {
     assert_eq!(trie_index(&trie, 1), 1, "ASCII range code points index pos is code point value");
     assert_eq!(trie_index(&trie, 65), 65, "ASCII range code points index pos is code point value");
     assert_eq!(trie_index(&trie, 127), 127, "ASCII range code points index pos is code point value");
-    assert_ne!(trie_index(&trie, 999), 999, "ASCII range code points index pos is code point value (Not yet properly implemented)");
+
+    assert_eq!(trie_index(&trie, 999), 0);
+
+    // TODO: add more test cases for index values
+
+    // TODO: add impl & tests for data-getting fns using the index fns
+
 
     let _check_ranges: [u32; 10] = [0, 1, 0x740, 1, 0x780, 2, 0x880, 3, 0x110000, 1];
 
-    assert_eq!(1, 2 - 1, "heyo!");
 }
