9832bef47aa12cd24d06e6b671d2db1d0b3f006d
Ting-Yu Lin <aethanyc@gmail.com>
2021-05-12 10:59:05 -0700

    Add license template to all the files
    See README.md for the instruction to re-generated the tables via
    generate_properties.py.

--- experimental/segmenter/src/lib.rs
+++ experimental/segmenter/src/lib.rs
@@ -1,3 +1,7 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 //! A line breaking iterator of [Unicode Standard Annex #14](http://www.unicode.org/reports/tr14/) compatible rules.
 //!
 //! ```rust
--- experimental/segmenter/src/line_breaker.rs
+++ experimental/segmenter/src/line_breaker.rs
@@ -1,3 +1,7 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 extern crate unicode_width;
 
 use crate::lb_define::*;
