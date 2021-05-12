a7b7206b50e9f91fa6bcec28e6f4d3f293282bce
github-action-benchmark <github@users.noreply.github.com>
2021-05-12 20:02:47 +0000

    add Rust Benchmark (cargo) benchmark result for 9f897fb14dc1ed8093e152819427b98cdeddc657

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1620762128921,
+  "lastUpdate": 1620849767407,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6941,6 +6941,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5262,
             "range": "± 264",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9f897fb14dc1ed8093e152819427b98cdeddc657",
+          "message": "Re-organize ICU4X data generation tools (#704)\n\n- Adds new crate under `tools/datagen`\r\n- Moves `icu4x-cldr-export` into that crate, renamed as `icu4x-datagen`\r\n- Replaces `icu4x-gen-testdata` with a new tool `icu4x-testdata-download`\r\n- Adds commands in Makefile.toml to invoke these tools",
+          "timestamp": "2021-05-12T14:55:57-05:00",
+          "tree_id": "b1614a6467b59fab9c68620bae91ffd7d63d6c08",
+          "url": "https://github.com/unicode-org/icu4x/commit/9f897fb14dc1ed8093e152819427b98cdeddc657"
+        },
+        "date": 1620849765754,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2901,
+            "range": "± 193",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4695,
+            "range": "± 344",
             "unit": "ns/iter"
           }
         ]
