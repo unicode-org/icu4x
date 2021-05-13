a8599e4e3f972b3120887252d290ea6e750cbf60
Shane F. Carr <sffc@google.com>
2021-05-13 18:00:29 -0500

    Rename package directories in repo (#721)

--- components/datetime/src/skeleton.rs
+++ components/datetime/src/skeleton.rs
@@ -251,7 +251,7 @@ impl From<fields::SymbolError> for SkeletonError {
             fields::SymbolError::Unknown(byte) => {
                 // NOTE: If you remove a symbol due to it now being supported,
                 //       make sure to regenerate the test data.
-                //       https://github.com/unicode-org/icu4x/blob/main/resources/testdata/README.md
+                //       https://github.com/unicode-org/icu4x/blob/main/provider/testdata/README.md
                 match byte {
                     // TODO(#487) - Flexible day periods
                     b'B'
@@ -797,7 +797,7 @@ mod test {
     // NOTE: If you are moving this to the SUPPORTED section, make sure to remove the match
     //       on your symbol from impl From<fields::SymbolError> for SkeletonError
     //       and then regenerate the test data.
-    //       https://github.com/unicode-org/icu4x/blob/main/resources/testdata/README.md
+    //       https://github.com/unicode-org/icu4x/blob/main/provider/testdata/README.md
     #[rustfmt::skip]
     const UNSUPPORTED_STRING_SKELETONS: [&str; 19] = [
         // TODO(#487) - Flexible day periods
