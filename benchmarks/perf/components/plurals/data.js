15cd60287137db4b81d8fe3c261ccd010a092c1c
github-action-benchmark <github@users.noreply.github.com>
2021-05-13 23:07:18 +0000

    add Rust Benchmark (cargo) benchmark result for a8599e4e3f972b3120887252d290ea6e750cbf60

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1620932736768,
+  "lastUpdate": 1620947238877,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -7517,6 +7517,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 117868,
             "range": "± 8357",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a8599e4e3f972b3120887252d290ea6e750cbf60",
+          "message": "Rename package directories in repo (#721)",
+          "timestamp": "2021-05-13T16:00:29-07:00",
+          "tree_id": "803a86066a5a41a378a5b2ec01d4968d40b75133",
+          "url": "https://github.com/unicode-org/icu4x/commit/a8599e4e3f972b3120887252d290ea6e750cbf60"
+        },
+        "date": 1620947237741,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1133,
+            "range": "± 33",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6892,
+            "range": "± 218",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 106985,
+            "range": "± 2707",
             "unit": "ns/iter"
           }
         ]
