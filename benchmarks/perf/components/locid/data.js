26f13a98292659633abb995372b810752962e40c
github-action-benchmark <github@users.noreply.github.com>
2021-05-21 06:08:41 +0000

    add Rust Benchmark (cargo) benchmark result for 5fa630f86b31880cee7000793467d5ab9fd5abc6

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621556900648,
+  "lastUpdate": 1621577321839,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -7301,6 +7301,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4260,
             "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5fa630f86b31880cee7000793467d5ab9fd5abc6",
+          "message": "Store DataPayload instead of Cow throughout ICU4X (#729)",
+          "timestamp": "2021-05-21T01:02:44-05:00",
+          "tree_id": "8f998589d9317d31432f0563cfba4936addb3260",
+          "url": "https://github.com/unicode-org/icu4x/commit/5fa630f86b31880cee7000793467d5ab9fd5abc6"
+        },
+        "date": 1621577320545,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3146,
+            "range": "± 141",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4885,
+            "range": "± 210",
             "unit": "ns/iter"
           }
         ]
