7d6b315c6065ca65ae91ad7db5a8f6316b04a7d4
github-action-benchmark <github@users.noreply.github.com>
2021-05-21 00:28:24 +0000

    add Rust Benchmark (cargo) benchmark result for 48c47db44cace4a608b627f6065de1dd39aac166

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621555902184,
+  "lastUpdate": 1621556904178,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6569,6 +6569,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34211790,
             "range": "± 432890",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "48c47db44cace4a608b627f6065de1dd39aac166",
+          "message": "Make clippy warnings report as errors and update clippy args (#712)",
+          "timestamp": "2021-05-20T17:21:57-07:00",
+          "tree_id": "e16af60d9b2284d5943f4f01b26b3a156f27aa36",
+          "url": "https://github.com/unicode-org/icu4x/commit/48c47db44cace4a608b627f6065de1dd39aac166"
+        },
+        "date": 1621556899433,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 31196123,
+            "range": "± 1972883",
             "unit": "ns/iter"
           }
         ]
