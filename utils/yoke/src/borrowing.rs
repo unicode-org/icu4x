3512b98ad03a046003a5790edb6aac91259c1c15
Shane F. Carr <shane@unicode.org>
2021-05-31 17:25:30 -0500

    Add V7 and V8 (does not compile)

--- utils/yoke/src/borrowing.rs
+++ utils/yoke/src/borrowing.rs
@@ -4,7 +4,7 @@ use std::borrow::Borrow;
 use std::borrow::Cow;
 use std::mem;
 
-trait ZeroCopyCloneV2 {
+pub trait ZeroCopyCloneV2 {
     type Yokeable: for<'a> Yokeable<'a>;
 
     fn zero_copy_clone_v2<'b>(&'b self) -> <Self::Yokeable as Yokeable<'b>>::Output;
@@ -14,20 +14,32 @@ trait ZeroCopyCloneV2 {
     }
 }
 
-trait ZeroCopyCloneV4: for<'a> Yokeable<'a> {
-    fn zero_copy_clone_v4<'b>(this: &'b <Self as Yokeable<'b>>::Output) -> <Self as Yokeable<'b>>::Output;
+pub trait ZeroCopyCloneV4: for<'a> Yokeable<'a> {
+    fn zero_copy_clone_v4<'b>(
+        this: &'b <Self as Yokeable<'b>>::Output,
+    ) -> <Self as Yokeable<'b>>::Output;
 }
 
-// trait ZeroCopyCloneV5<'d> {
+// pub trait ZeroCopyCloneV5<'d> {
 //     type Yokeable: for<'a> Yokeable<'a> + Yokeable<'d, Output = Self>;
 
 //     fn zero_copy_clone_v5<'b>(&self) -> <Self::Yokeable as Yokeable<'b>>::Output;
 // }
 
-trait ZeroCopyCloneV6<'d>: 'd {
+pub trait ZeroCopyCloneV6<'d>: 'd {
     type Yokeable: for<'a> Yokeable<'a>;
 
     fn zero_copy_clone_v6<'b>(&'b self) -> <Self::Yokeable as Yokeable<'b>>::Output;
+}
+
+pub trait ZeroCopyCloneV7<'s> {
+    fn zero_copy_clone_v7(&'s self) -> Self;
+}
+
+pub trait ZeroCopyCloneV8<'o, 'i: 'o>: 'i {
+    type Output: 'o;
+
+    fn zero_copy_clone_v8(&'o self) -> Self::Output;
 }
 
 struct DataStruct<'s> {
@@ -76,6 +88,15 @@ impl<'s> ZeroCopyCloneV4 for DataStruct<'static> {
     }
 }
 
+impl<'s> ZeroCopyCloneV7<'s> for DataStruct<'s> {
+    fn zero_copy_clone_v7(&'s self) -> DataStruct<'s> {
+        DataStruct {
+            f1: Cow::Borrowed(self.f1.borrow()),
+            f2: Cow::Borrowed(self.f2.borrow()),
+        }
+    }
+}
+
 impl ZeroCopyCloneV2 for str {
     type Yokeable = &'static str;
 
@@ -86,12 +107,56 @@ impl ZeroCopyCloneV2 for str {
 
 impl<'b, B> Yoke<<B as ZeroCopyCloneV2>::Yokeable, &'b B>
 where
-    B: ZeroCopyCloneV2
+    B: ZeroCopyCloneV2,
 {
-    fn from_borrowed(b: &'b B) -> Self {
+    fn from_borrowed_v2(b: &'b B) -> Self {
         Self::attach_to_cart_badly(b, B::zero_copy_clone_v2)
     }
 }
+
+/*
+fn zcc_v7_helper<'de, 'b, B>(v: &'de <B as Yokeable<'de>>::Output) -> <B as Yokeable<'de>>::Output
+where
+    B: for<'a> Yokeable<'a>,
+    for<'a> <B as Yokeable<'a>>::Output: ZeroCopyCloneV7<'a>,
+    'b: 'de,
+{
+    v.zero_copy_clone_v7()
+}
+
+impl<'b, B> Yoke<B, &'b <B as Yokeable<'b>>::Output>
+where
+    B: for<'a> Yokeable<'a>,
+    <B as Yokeable<'b>>::Output: ZeroCopyCloneV7<'b>,
+{
+    fn from_borrowed_v7(b: &'b <B as Yokeable<'b>>::Output) -> Self {
+        Self::attach_to_cart_badly(
+            b,
+            ZeroCopyCloneV7::zero_copy_clone_v7,
+        )
+    }
+}
+
+fn zcc_v8_helper<'de, 'b, B>(v: &'de <B as Yokeable<'b>>::Output) -> <B as Yokeable<'de>>::Output
+where
+    B: for<'a> Yokeable<'a>,
+    <B as Yokeable<'b>>::Output: ZeroCopyCloneV8<'de, 'b, Output = <B as Yokeable<'de>>::Output>,
+    'b: 'de,
+{
+    v.zero_copy_clone_v8()
+}
+
+impl<'b, B> Yoke<B, &'b <B as Yokeable<'b>>::Output>
+where
+    B: for<'a> Yokeable<'a>,
+    <B as Yokeable<'b>>::Output:
+        for<'de> ZeroCopyCloneV8<'de, 'b, Output = <B as Yokeable<'de>>::Output>,
+{
+    fn from_borrowed_v8(b: &'b <B as Yokeable<'b>>::Output) -> Self {
+        Self::attach_to_cart_badly(b, zcc_v8_helper)
+    }
+}
+*/
 
 #[test]
 fn test_borrowing() {
@@ -105,7 +170,7 @@ fn test_borrowing() {
     assert_eq!(yoke.get().f1, "foo");
     assert!(matches!(yoke.get().f1, Cow::Borrowed(_)));
 
-    let yoke = Yoke::<DataStruct<'static>, &DataStruct<'_>>::from_borrowed(&data_struct);
+    let yoke = Yoke::<DataStruct<'static>, &DataStruct<'_>>::from_borrowed_v2(&data_struct);
 
     assert_eq!(yoke.get().f1, "foo");
     assert!(matches!(yoke.get().f1, Cow::Borrowed(_)));
