ed1b98b5e84dd40c35f8d2f7aac64cb69bba8a96
Shadaj Laddad <shadaj@users.noreply.github.com>
2021-06-04 15:56:08 -0700

    Address feedback

--- ffi/capi/examples/wasm/main.mjs
+++
@@ -1,9 +0,0 @@
-import { FixedDecimal, BufferWritable } from "../../wasm-lib/high-level.mjs"
-
-const decimal = new FixedDecimal(BigInt(1234));
-console.log(decimal.multiply_pow10(-2));
-decimal.negate();
-
-const outWritable = new BufferWritable();
-decimal.write_to(outWritable);
-console.log(outWritable.getString());
