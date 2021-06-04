d4577bae4bd03c8e0f69721d4ff01429fc8ddcaa
github-action-benchmark <github@users.noreply.github.com>
2021-06-04 20:47:20 +0000

    add Rust Benchmark (cargo) benchmark result for dc8e5416de0febccbb9c09971bb52111c45f022e

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1622766413672,
+  "lastUpdate": 1622839640803,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -2591,6 +2591,42 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 2243698,
             "range": "± 144191",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dc8e5416de0febccbb9c09971bb52111c45f022e",
+          "message": "Migrate DataPayload to Yoke (#745)\n\n- Adds new DataMarker trait and migrates all data structs to it",
+          "timestamp": "2021-06-04T15:41:37-05:00",
+          "tree_id": "e94e9f15867212d4092e72ef18064582bab92166",
+          "url": "https://github.com/unicode-org/icu4x/commit/dc8e5416de0febccbb9c09971bb52111c45f022e"
+        },
+        "date": 1622839639024,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 1188461,
+            "range": "± 7006",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 2292527,
+            "range": "± 4109",
             "unit": "ns/iter"
           }
         ]
