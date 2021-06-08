e8fc4bdb18ced5fa8e24001777cb0bca2e9e0efb
github-action-benchmark <github@users.noreply.github.com>
2021-06-08 22:58:44 +0000

    add Rust Benchmark (cargo) benchmark result for 2a47704e5e3d8fd276de2a530feceb43e74d5a61

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623186403467,
+  "lastUpdate": 1623193124198,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8441,6 +8441,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 127176,
             "range": "± 6530",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2a47704e5e3d8fd276de2a530feceb43e74d5a61",
+          "message": "Switch static data provider to using bincode to reduce heap footprint (#775)",
+          "timestamp": "2021-06-08T17:51:51-05:00",
+          "tree_id": "c0f9f660e5e9b44caa1eaadcb6c7d34874d59c42",
+          "url": "https://github.com/unicode-org/icu4x/commit/2a47704e5e3d8fd276de2a530feceb43e74d5a61"
+        },
+        "date": 1623193121896,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1346,
+            "range": "± 36",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 8053,
+            "range": "± 250",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 123821,
+            "range": "± 3710",
             "unit": "ns/iter"
           }
         ]
