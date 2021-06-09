5f91fa7f60d2f945a523c19f1f1509f95914b7dc
github-action-benchmark <github@users.noreply.github.com>
2021-06-09 00:29:23 +0000

    add Rust Benchmark (cargo) benchmark result for 500e458fbb8d5f0295f5f2baf3b7af10f937cd21

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623193030463,
+  "lastUpdate": 1623198563863,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6059,6 +6059,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 130649,
             "range": "± 353",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "500e458fbb8d5f0295f5f2baf3b7af10f937cd21",
+          "message": "Clarify `top level` in testdata README (#773)\n\n* Clarify `top level` in testdata README\r\n\r\n* Match lib.rs with README",
+          "timestamp": "2021-06-08T17:24:07-07:00",
+          "tree_id": "53c70b71af41aff3dba5f5dfb90519354efef8a7",
+          "url": "https://github.com/unicode-org/icu4x/commit/500e458fbb8d5f0295f5f2baf3b7af10f937cd21"
+        },
+        "date": 1623198562043,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 130668,
+            "range": "± 534",
             "unit": "ns/iter"
           }
         ]
