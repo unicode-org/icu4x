766f3db11ebf58564c7444767a3f9667742fdf90
github-action-benchmark <github@users.noreply.github.com>
2021-06-10 13:56:25 +0000

    add Rust Benchmark (cargo) benchmark result for d8af2a93578bfa44027f540b38d92b9435e52957

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623280245498,
+  "lastUpdate": 1623333384989,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8021,6 +8021,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 3780,
             "range": "± 5",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d8af2a93578bfa44027f540b38d92b9435e52957",
+          "message": "Fix links in docs (#786)\n\n* Fix links in docs\r\n\r\n* fix fixeddecimal",
+          "timestamp": "2021-06-10T06:49:50-07:00",
+          "tree_id": "47494dd555f1ff2454c97d4a7f0dc8f23a432ed0",
+          "url": "https://github.com/unicode-org/icu4x/commit/d8af2a93578bfa44027f540b38d92b9435e52957"
+        },
+        "date": 1623333379296,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3169,
+            "range": "± 173",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4876,
+            "range": "± 260",
             "unit": "ns/iter"
           }
         ]
