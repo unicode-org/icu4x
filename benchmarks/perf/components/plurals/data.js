9752d1a250f837b1ce3c041d9d34921a81ea98e9
github-action-benchmark <github@users.noreply.github.com>
2021-06-15 19:27:50 +0000

    add Rust Benchmark (cargo) benchmark result for a0d4cf41951316c7f1c8733e20e8577fcd7e3f51

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623695695406,
+  "lastUpdate": 1623785270220,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8903,6 +8903,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 117847,
             "range": "± 638",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a0d4cf41951316c7f1c8733e20e8577fcd7e3f51",
+          "message": "Change link to good first issues in CONTRIBUTING.md (#793)",
+          "timestamp": "2021-06-15T14:20:49-05:00",
+          "tree_id": "eddac0795580cbeb18250e822aff0474ff5e7bd9",
+          "url": "https://github.com/unicode-org/icu4x/commit/a0d4cf41951316c7f1c8733e20e8577fcd7e3f51"
+        },
+        "date": 1623785268370,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1170,
+            "range": "± 61",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6866,
+            "range": "± 379",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 108247,
+            "range": "± 6624",
             "unit": "ns/iter"
           }
         ]
