8928722470860fc5a351ffd530e5cc91b320dffa
github-action-benchmark <github@users.noreply.github.com>
2021-06-15 23:31:47 +0000

    add Rust Benchmark (cargo) benchmark result for 1766db71feddc10e1d84b4c66ff40b8cce4c91c4

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623791641004,
+  "lastUpdate": 1623799907615,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6419,6 +6419,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 156196,
             "range": "± 1433",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58569820+nordzilla@users.noreply.github.com",
+            "name": "Erik Nordin",
+            "username": "nordzilla"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1766db71feddc10e1d84b4c66ff40b8cce4c91c4",
+          "message": "Expose PluralRules::categories() over capi FFI (#796)\n\n* Expose PluralRules::categories() over capi FFI\r\n\r\n- Add new struct ICU4XPluralCategories to hold whether each category has\r\n  rules or not for this PluralRules object.\r\n- Add new function icu4x_plural_rules_categories()\r\n\r\n* Remove FromIterator impl since its used only once",
+          "timestamp": "2021-06-15T16:26:01-07:00",
+          "tree_id": "c7813a383f94c7e03a691d7de98c64aed9e84918",
+          "url": "https://github.com/unicode-org/icu4x/commit/1766db71feddc10e1d84b4c66ff40b8cce4c91c4"
+        },
+        "date": 1623799905626,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 141792,
+            "range": "± 6910",
             "unit": "ns/iter"
           }
         ]
