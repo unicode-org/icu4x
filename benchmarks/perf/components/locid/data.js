c66cd847c39391dd40af00032bba09f91c977639
github-action-benchmark <github@users.noreply.github.com>
2021-06-17 17:28:31 +0000

    add Rust Benchmark (cargo) benchmark result for 7636296239e52b8573bf21a1bad2e3993fbf3f00

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623950554810,
+  "lastUpdate": 1623950911581,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8489,6 +8489,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4390,
             "range": "± 304",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7636296239e52b8573bf21a1bad2e3993fbf3f00",
+          "message": "Add Sphinx docs for C++ (#806)\n\n* Set up Sphinx doc dir\r\n\r\n* Update license check for RST comments\r\n\r\n* Add basic docs for C++ FFI\r\n\r\n* Add cppdocs CI run\r\n\r\n* Address review comments\r\n\r\n* Fix `cargo make tidy`",
+          "timestamp": "2021-06-17T10:22:59-07:00",
+          "tree_id": "3f78b4b509bcbb51590204f7308dce1280a4861b",
+          "url": "https://github.com/unicode-org/icu4x/commit/7636296239e52b8573bf21a1bad2e3993fbf3f00"
+        },
+        "date": 1623950910021,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2774,
+            "range": "± 6",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4266,
+            "range": "± 3",
             "unit": "ns/iter"
           }
         ]
