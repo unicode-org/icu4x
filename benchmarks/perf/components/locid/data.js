ee5e8a3cb0aa1944b858f090ea09947fe06b8de2
github-action-benchmark <github@users.noreply.github.com>
2021-06-18 00:17:23 +0000

    add Rust Benchmark (cargo) benchmark result for c5135af6babd7cdbea02f0971ef8656fb6d68b0b

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623950911581,
+  "lastUpdate": 1623975443615,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8525,6 +8525,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4266,
             "range": "± 3",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c5135af6babd7cdbea02f0971ef8656fb6d68b0b",
+          "message": "Only load symbols data if pattern requires them. (#791)",
+          "timestamp": "2021-06-17T17:10:44-07:00",
+          "tree_id": "43a8f51fcb27b486b23f278c6ee37a0fda3f2777",
+          "url": "https://github.com/unicode-org/icu4x/commit/c5135af6babd7cdbea02f0971ef8656fb6d68b0b"
+        },
+        "date": 1623975440370,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3342,
+            "range": "± 106",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5255,
+            "range": "± 286",
             "unit": "ns/iter"
           }
         ]
