f6258549d08ed41e86f2a720ebece646d2cb1204
github-action-benchmark <github@users.noreply.github.com>
2021-06-22 19:00:20 +0000

    add Rust Benchmark (cargo) benchmark result for 80c70663dc7110db9a5eb274c310c6f832675f7a

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1624252009287,
+  "lastUpdate": 1624388420580,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -7739,6 +7739,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 31861666,
             "range": "± 1371469",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58569820+nordzilla@users.noreply.github.com",
+            "name": "Erik Nordin",
+            "username": "nordzilla"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80c70663dc7110db9a5eb274c310c6f832675f7a",
+          "message": "Add bincode example to icu4x-datagen readme (#819)\n\n* Add bincode example to icu4x-datagen readme\r\n\r\n* Update changes in main.rs to generate README\r\n\r\n* Reorder flags\r\n\r\n* Re-run the generation",
+          "timestamp": "2021-06-22T11:52:52-07:00",
+          "tree_id": "4697cb4f854f18279386ddf130ad7c3e5534b94d",
+          "url": "https://github.com/unicode-org/icu4x/commit/80c70663dc7110db9a5eb274c310c6f832675f7a"
+        },
+        "date": 1624388418721,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 38689457,
+            "range": "± 66725",
             "unit": "ns/iter"
           }
         ]
