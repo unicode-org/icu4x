641c55e18e4702b97281b3100b52738e5c487dfd
github-action-benchmark <github@users.noreply.github.com>
2021-06-25 02:33:38 +0000

    add Rust Benchmark (cargo) benchmark result for 6b79658c0e00dd22a7bb03fa4f0509c4567134d9

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1624570404545,
+  "lastUpdate": 1624588418470,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9491,6 +9491,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 113888,
             "range": "± 682",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b79658c0e00dd22a7bb03fa4f0509c4567134d9",
+          "message": "Fix cargo quick (#826)",
+          "timestamp": "2021-06-24T21:27:31-05:00",
+          "tree_id": "670b05ab8d4efc93589ef7021f2098240833b75a",
+          "url": "https://github.com/unicode-org/icu4x/commit/6b79658c0e00dd22a7bb03fa4f0509c4567134d9"
+        },
+        "date": 1624588416539,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1112,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6501,
+            "range": "± 39",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 101576,
+            "range": "± 425",
             "unit": "ns/iter"
           }
         ]
