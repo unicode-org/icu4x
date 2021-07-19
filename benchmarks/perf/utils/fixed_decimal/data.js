830035ae382588bdb129eb7363ea5e7772565858
github-action-benchmark <github@users.noreply.github.com>
2021-07-19 18:35:45 +0000

    add Rust Benchmark (cargo) benchmark result for a8377791ffc70c7c6f3aa7f6aa27023e0e59aaca

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626704892356,
+  "lastUpdate": 1626719745401,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -7199,6 +7199,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 127816,
             "range": "± 341",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a8377791ffc70c7c6f3aa7f6aa27023e0e59aaca",
+          "message": "Progress towards no_std (#865)\n\n* hardcode thumb\r\n\r\n* Fix writeable\r\n\r\n* Fix stable_deref_trait dep\r\n\r\n* no-default-features on tinystr\r\n\r\n* Update tinystr\r\n\r\n* Use serde/serde-json with alloc feature\r\n\r\n* Move to newer resolver\r\n\r\n* Bump tinystr\r\n\r\n* Bump tinystr again\r\n\r\n* fix optional=true after rebase\r\n\r\n* Add alloc feature to yoke\r\n\r\n* fmt\r\n\r\n* Move provider_fs to std for now\r\n\r\n* rm config.toml changes\r\n\r\n* not test in yoke\r\n\r\n* fix tinystr\r\n\r\n* fix features",
+          "timestamp": "2021-07-19T11:29:45-07:00",
+          "tree_id": "c063ff96e6ce96c0258946e3f6a69b833ab42d55",
+          "url": "https://github.com/unicode-org/icu4x/commit/a8377791ffc70c7c6f3aa7f6aa27023e0e59aaca"
+        },
+        "date": 1626719743560,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 152052,
+            "range": "± 1739",
             "unit": "ns/iter"
           }
         ]
