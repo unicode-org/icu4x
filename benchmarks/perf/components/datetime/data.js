aa1262e9774fe66a5334ef2f0d0de87bd2fe1ca6
github-action-benchmark <github@users.noreply.github.com>
2021-07-20 00:13:46 +0000

    add Rust Benchmark (cargo) benchmark result for e5f7a5e5c737ee6e3ba0c53e789d131d3eb277a8

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626719789833,
+  "lastUpdate": 1626740026299,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4247,6 +4247,42 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 1266338,
             "range": "± 19276",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e5f7a5e5c737ee6e3ba0c53e789d131d3eb277a8",
+          "message": "Remove use of `dyn Error` in library code in favor of Strings (#867)\n\n* Remove std::error::Error from provider\r\n\r\n* Remove std::error::Error from fs data provider\r\n\r\n* Pull in serde std only when exporting\r\n\r\n* fmt",
+          "timestamp": "2021-07-19T17:07:01-07:00",
+          "tree_id": "332ef8ced9691040c640d1d282f255d16ce794ed",
+          "url": "https://github.com/unicode-org/icu4x/commit/e5f7a5e5c737ee6e3ba0c53e789d131d3eb277a8"
+        },
+        "date": 1626740024058,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 590633,
+            "range": "± 48447",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 1133012,
+            "range": "± 97644",
             "unit": "ns/iter"
           }
         ]
