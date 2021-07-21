87bd6d68309db9620e7c463cd7bc59e3f28d1bc2
github-action-benchmark <github@users.noreply.github.com>
2021-07-21 00:06:42 +0000

    add Rust Benchmark (cargo) benchmark result for ff56a70f588b63f134714956a8eb8e1ee37a99d3

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626740081734,
+  "lastUpdate": 1626826001950,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10163,6 +10163,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 126218,
             "range": "± 5481",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ff56a70f588b63f134714956a8eb8e1ee37a99d3",
+          "message": "Move locid and fixed_decimal over to no_std (#868)\n\n* Fix fixed-decimal for no_std\r\n\r\n* Move locid to no_std\r\n\r\n* Fix unused error\r\n\r\n* remove displaydoc/std",
+          "timestamp": "2021-07-20T17:00:20-07:00",
+          "tree_id": "12046357894214fed8784d275b2d9e6ecac3e9f5",
+          "url": "https://github.com/unicode-org/icu4x/commit/ff56a70f588b63f134714956a8eb8e1ee37a99d3"
+        },
+        "date": 1626826000345,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1121,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6712,
+            "range": "± 16",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 100744,
+            "range": "± 412",
             "unit": "ns/iter"
           }
         ]
