59a0c1f55b0dc882a9c8c37c6d46689902fa6d76
github-action-benchmark <github@users.noreply.github.com>
2021-07-21 21:43:06 +0000

    add Rust Benchmark (cargo) benchmark result for 4052c2ec46d082142f12c09e680d42fae6d62a82

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626832211392,
+  "lastUpdate": 1626903786087,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8369,6 +8369,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34237657,
             "range": "± 1333084",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4052c2ec46d082142f12c09e680d42fae6d62a82",
+          "message": "Modify patterns from a skeleton match to have the correct width (#832)",
+          "timestamp": "2021-07-21T16:36:33-05:00",
+          "tree_id": "77d6dcabe60d44573d2531361ee356e61b6761d8",
+          "url": "https://github.com/unicode-org/icu4x/commit/4052c2ec46d082142f12c09e680d42fae6d62a82"
+        },
+        "date": 1626903783715,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 30692185,
+            "range": "± 1401661",
             "unit": "ns/iter"
           }
         ]
