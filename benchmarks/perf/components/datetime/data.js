d0cc675cdb6013c5fda6f252d346cad858a9b7a6
github-action-benchmark <github@users.noreply.github.com>
2021-07-22 21:35:35 +0000

    add Rust Benchmark (cargo) benchmark result for 9a25738b476593d40353e19a2dd0b2c5f9ab81a6

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626986107996,
+  "lastUpdate": 1626989735345,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -4679,6 +4679,42 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 1435857,
             "range": "± 70968",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9a25738b476593d40353e19a2dd0b2c5f9ab81a6",
+          "message": "Handle no_std for icu_capi (#890)\n\n* capi nostd\r\n\r\n* replace\r\n\r\n* fmt\r\n\r\n* fix warning\r\n\r\n* Add freertos glue\r\n\r\n* fix warning\r\n\r\n* Add tests for cortex\r\n\r\n* Fix wasm glue\r\n\r\n* fix attr\r\n\r\n* fmt\r\n\r\n* update test-cortex\r\n\r\n* fix actions\r\n\r\n* link to #891",
+          "timestamp": "2021-07-22T14:28:42-07:00",
+          "tree_id": "b2fbc849d3ba824f44f91de9233d4f1579d7e28f",
+          "url": "https://github.com/unicode-org/icu4x/commit/9a25738b476593d40353e19a2dd0b2c5f9ab81a6"
+        },
+        "date": 1626989732843,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 647051,
+            "range": "± 22970",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 1338002,
+            "range": "± 38074",
             "unit": "ns/iter"
           }
         ]
