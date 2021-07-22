82c3c2de9997156650412ab567b15561e5d7743f
Manish Goregaokar <manishsmail@gmail.com>
2021-07-22 11:20:46 -0700

    fix attr

--- ffi/capi/src/lib.rs
+++ ffi/capi/src/lib.rs
@@ -5,7 +5,8 @@
 #![cfg_attr(target_os = "none", feature(alloc_error_handler))]
 #![allow(clippy::upper_case_acronyms)]
 
-#[cfg_attr(not(target_arch = "wasm32"), no_std)]
+#![cfg_attr(not(target_arch = "wasm32"), no_std)]
+
 extern crate alloc;
 
 #[macro_use]
