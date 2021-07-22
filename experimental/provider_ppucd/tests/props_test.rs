6df5ffc44246d68586ccf888b13a761a365a30c7
Elango <elango@google.com>
2021-07-22 13:28:13 -0700

    Set explicit stable values for enum discriminants that match ICU4C (#698)

--- experimental/provider_ppucd/tests/props_test.rs
+++ experimental/provider_ppucd/tests/props_test.rs
@@ -16,8 +16,8 @@ fn test_wspace_getter() {
     let wspace_uniset: UnicodeSet =
         icu_uniset::props::get_white_space_property(&ppucd_provider).unwrap();
     let exp_uniset: UnicodeSet = UnicodeSet::from_inversion_list(vec![
-        9, 14, 32, 33, 133, 134, 160, 161, 5760, 5761, 8192, 8203, 8232, 8234, 8239, 8240, 8287,
-        8288, 12288, 12289,
+        0x9, 0xE, 0x20, 0x21, 0x85, 0x86, 0xA0, 0xA1, 0x1680, 0x1681, 0x2000, 0x200B, 0x2028,
+        0x202A, 0x202F, 0x2030, 0x205F, 0x2060, 0x3000, 0x3001,
     ])
     .unwrap();
     assert_eq!(wspace_uniset, exp_uniset);
@@ -29,27 +29,13 @@ fn test_enum_props_getters() {
     let ppucd_property_file_str = std::fs::read_to_string(ppucd_property_files_root_path).unwrap();
     let ppucd_provider: PpucdDataProvider = PpucdDataProvider::new(&ppucd_property_file_str);
 
-    // lb=LF
-    let lb_lf_uniset: UnicodeSet =
-        icu_uniset::props::get_line_break_val_set(&ppucd_provider, LineBreak::LineFeed).unwrap();
-    let exp_lb_lf_uniset: UnicodeSet = UnicodeSet::from_inversion_list(vec![10, 11]).unwrap();
-    assert_eq!(lb_lf_uniset, exp_lb_lf_uniset);
-}
-
-#[test]
-fn test_enum_props_getters_truncated_resc_key_subcategory() {
-    let ppucd_property_files_root_path = "tests/testdata/ppucd-inpc-topbotleft-test.txt";
-    let ppucd_property_file_str = std::fs::read_to_string(ppucd_property_files_root_path).unwrap();
-    let ppucd_provider: PpucdDataProvider = PpucdDataProvider::new(&ppucd_property_file_str);
-
-    // InPC=Top_And_Bottom_And_Left
-    let inpc_topbotleft_uniset: UnicodeSet =
-        icu_uniset::props::get_indic_positional_category_val_set(
-            &ppucd_provider,
-            IndicPositionalCategory::TopAndBottomAndLeft,
-        )
-        .unwrap();
-    let exp_inpc_topbotleft_uniset: UnicodeSet =
-        UnicodeSet::from_inversion_list(vec![4156, 4157, 71454, 71455]).unwrap();
-    assert_eq!(inpc_topbotleft_uniset, exp_inpc_topbotleft_uniset);
+    // gc=Zs
+    let gc_zs_uniset: UnicodeSet = icu_uniset::props::get_general_category_val_set(
+        &ppucd_provider,
+        GeneralCategory::SpaceSeparator,
+    )
+    .unwrap();
+    let exp_gc_zs_uniset: UnicodeSet =
+        UnicodeSet::from_inversion_list(vec![0x20, 0x21, 0xA0, 0xA1]).unwrap();
+    assert_eq!(gc_zs_uniset, exp_gc_zs_uniset);
 }
