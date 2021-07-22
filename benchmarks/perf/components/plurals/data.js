f243c1aea0ff5d19cc34b4ec4c626b06d91cf870
github-action-benchmark <github@users.noreply.github.com>
2021-07-22 17:36:26 +0000

    add Rust Benchmark (cargo) benchmark result for 9acd1e99b12cc2c3eac2338fbe07f0059686b6e7

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626974859025,
+  "lastUpdate": 1626975386405,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10457,6 +10457,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 112785,
             "range": "± 5917",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9acd1e99b12cc2c3eac2338fbe07f0059686b6e7",
+          "message": "Fix name of Bidi_M (#884)",
+          "timestamp": "2021-07-22T12:29:58-05:00",
+          "tree_id": "8d221a68e13801f21723f33ecff92e92651865ff",
+          "url": "https://github.com/unicode-org/icu4x/commit/9acd1e99b12cc2c3eac2338fbe07f0059686b6e7"
+        },
+        "date": 1626975380782,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1123,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6580,
+            "range": "± 25",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 101465,
+            "range": "± 367",
             "unit": "ns/iter"
           }
         ]
