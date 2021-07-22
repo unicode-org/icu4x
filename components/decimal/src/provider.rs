22de6c1b5e625a06dbc09af86c093f3f65f011a6
Manish Goregaokar <manishsmail@gmail.com>
2021-07-22 09:30:37 -0700

    fmt

--- components/decimal/src/provider.rs
+++ components/decimal/src/provider.rs
@@ -6,8 +6,8 @@
 //!
 //! Read more about data providers: [`icu_provider`]
 
-use icu_provider::yoke::{self, *};
 use alloc::borrow::Cow;
+use icu_provider::yoke::{self, *};
 
 pub mod key {
     //! Resource keys for [`icu_decimal`](crate).
