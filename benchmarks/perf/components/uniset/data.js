9edba5e8c35efd8969a0e6ff7a609c515d32b2e7
github-action-benchmark <github@users.noreply.github.com>
2021-07-22 16:25:44 +0000

    add Rust Benchmark (cargo) benchmark result for 77e8269d25798b7a4963bee1a0b639659bfe2d57

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626905784436,
+  "lastUpdate": 1626971144198,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8459,6 +8459,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 30777485,
             "range": "± 84200",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "77e8269d25798b7a4963bee1a0b639659bfe2d57",
+          "message": "Move locale_canonicalizer and StaticDataProvider to no_std, use SDP correctly in capi (#880)\n\n* make locale_canonicalizer no_std\r\n\r\n* Remove fs provider dep from capi when there is no OS\r\n\r\n* Move testdata to no_std\r\n\r\n* fmt\r\n\r\n* include sdp in test\r\n\r\n* fixup import",
+          "timestamp": "2021-07-22T09:19:40-07:00",
+          "tree_id": "1ad244828aae34b87a37b4124230d3df8bcfbc67",
+          "url": "https://github.com/unicode-org/icu4x/commit/77e8269d25798b7a4963bee1a0b639659bfe2d57"
+        },
+        "date": 1626971142421,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 32157782,
+            "range": "± 1295461",
             "unit": "ns/iter"
           }
         ]
