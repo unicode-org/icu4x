a1b3f36cb6d5afe2a90dcf6abbc2ab42bcf1e64f
github-action-benchmark <github@users.noreply.github.com>
2021-07-23 13:48:31 +0000

    add Rust Benchmark (cargo) benchmark result for 2fb8ea7943b3344555191bfdf0fbc5cfb13f7c82

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626989741808,
+  "lastUpdate": 1627048111320,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10667,6 +10667,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 122147,
             "range": "± 2616",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2fb8ea7943b3344555191bfdf0fbc5cfb13f7c82",
+          "message": "Correctly apply the hour cycle in the components::Bag (#846)",
+          "timestamp": "2021-07-23T08:42:21-05:00",
+          "tree_id": "ebd677c67f6136cb15ff12dd7a871e9934bd3080",
+          "url": "https://github.com/unicode-org/icu4x/commit/2fb8ea7943b3344555191bfdf0fbc5cfb13f7c82"
+        },
+        "date": 1627048109620,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1090,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6717,
+            "range": "± 33",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 99358,
+            "range": "± 927",
             "unit": "ns/iter"
           }
         ]
