fce194fdf3566fdedfac88b1ae74b28c6f33be8f
github-action-benchmark <github@users.noreply.github.com>
2021-07-25 17:19:50 +0000

    add Rust Benchmark (cargo) benchmark result for 9d82dabc1dbc2b41e7467b769d736af02b738227

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627233567961,
+  "lastUpdate": 1627233590855,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8789,6 +8789,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 30805657,
             "range": "± 75852",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9d82dabc1dbc2b41e7467b769d736af02b738227",
+          "message": "Make `cargo quick` quicker (#897)",
+          "timestamp": "2021-07-25T12:13:22-05:00",
+          "tree_id": "aad69299b0283402d2e72e41f6ac028b6504324c",
+          "url": "https://github.com/unicode-org/icu4x/commit/9d82dabc1dbc2b41e7467b769d736af02b738227"
+        },
+        "date": 1627233588446,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 33813615,
+            "range": "± 878362",
             "unit": "ns/iter"
           }
         ]
