8e8190b35f7aa1d4d8c8eebf3d82c2d1bcf8f33c
github-action-benchmark <github@users.noreply.github.com>
2021-07-25 17:19:28 +0000

    add Rust Benchmark (cargo) benchmark result for 774ee840a9630b225496caf797badb6bb6e38094

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627075312760,
+  "lastUpdate": 1627233567961,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8759,6 +8759,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 35292151,
             "range": "± 1475813",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "774ee840a9630b225496caf797badb6bb6e38094",
+          "message": "Improve Yoke Clone docs, saying it is not generally expensive (#899)",
+          "timestamp": "2021-07-25T12:13:44-05:00",
+          "tree_id": "8420afcc00ca207da960205fe6dc79d8b3dc38c1",
+          "url": "https://github.com/unicode-org/icu4x/commit/774ee840a9630b225496caf797badb6bb6e38094"
+        },
+        "date": 1627233566217,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 30805657,
+            "range": "± 75852",
             "unit": "ns/iter"
           }
         ]
