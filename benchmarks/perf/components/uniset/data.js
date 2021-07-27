ca99b3be7dd6d175f8684df1193ba41a417f7646
github-action-benchmark <github@users.noreply.github.com>
2021-07-27 22:45:18 +0000

    add Rust Benchmark (cargo) benchmark result for becafac79b327f18e8f1721683de638e828f2419

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627416542515,
+  "lastUpdate": 1627425918302,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -8909,6 +8909,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34843973,
             "range": "± 1167475",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "becafac79b327f18e8f1721683de638e828f2419",
+          "message": "Improve docs on DataProvider lifetimes (#915)",
+          "timestamp": "2021-07-27T17:39:28-05:00",
+          "tree_id": "d44cf184c25ca2457c67a931b558426e59bfad1f",
+          "url": "https://github.com/unicode-org/icu4x/commit/becafac79b327f18e8f1721683de638e828f2419"
+        },
+        "date": 1627425915763,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 31932477,
+            "range": "± 2109748",
             "unit": "ns/iter"
           }
         ]
