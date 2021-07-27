e477a01f9795e0317c2584588f913b20f1497d22
github-action-benchmark <github@users.noreply.github.com>
2021-07-27 22:47:02 +0000

    add Rust Benchmark (cargo) benchmark result for 5970335819dd63caf5653ca76529bd9e9e90650e

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627425958692,
+  "lastUpdate": 1627426022225,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10073,6 +10073,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5290,
             "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5970335819dd63caf5653ca76529bd9e9e90650e",
+          "message": "Fix FFI lifetimes for DataProvider (#914)",
+          "timestamp": "2021-07-27T17:39:49-05:00",
+          "tree_id": "5e873125b3716f751c3ccd3acf1a7c938dd499af",
+          "url": "https://github.com/unicode-org/icu4x/commit/5970335819dd63caf5653ca76529bd9e9e90650e"
+        },
+        "date": 1627426016243,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3152,
+            "range": "± 131",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4862,
+            "range": "± 258",
             "unit": "ns/iter"
           }
         ]
