c03d3f57a52466a122b792ca36c0a6acd4a06445
github-action-benchmark <github@users.noreply.github.com>
2021-07-27 22:46:24 +0000

    add Rust Benchmark (cargo) benchmark result for becafac79b327f18e8f1721683de638e828f2419

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627416567927,
+  "lastUpdate": 1627425984041,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -11003,6 +11003,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 99932,
             "range": "± 365",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "becafac79b327f18e8f1721683de638e828f2419",
+          "message": "Improve docs on DataProvider lifetimes (#915)",
+          "timestamp": "2021-07-27T17:39:28-05:00",
+          "tree_id": "d44cf184c25ca2457c67a931b558426e59bfad1f",
+          "url": "https://github.com/unicode-org/icu4x/commit/becafac79b327f18e8f1721683de638e828f2419"
+        },
+        "date": 1627425981650,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1151,
+            "range": "± 101",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6847,
+            "range": "± 556",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 108447,
+            "range": "± 9763",
             "unit": "ns/iter"
           }
         ]
