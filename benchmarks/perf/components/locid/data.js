ad385b526d2a5ca59797c89880e322b591302120
github-action-benchmark <github@users.noreply.github.com>
2021-07-29 22:06:36 +0000

    add Rust Benchmark (cargo) benchmark result for 0b38c6f03d4c182d6dd899e300b8e1b207b74895

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627582892586,
+  "lastUpdate": 1627596395990,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10325,6 +10325,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4636,
             "range": "± 258",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0b38c6f03d4c182d6dd899e300b8e1b207b74895",
+          "message": "Rename data errors to MissingResource (#893)",
+          "timestamp": "2021-07-29T16:59:43-05:00",
+          "tree_id": "fe3cc86ca1fb9881c8571afffd60f5b2d058b6c6",
+          "url": "https://github.com/unicode-org/icu4x/commit/0b38c6f03d4c182d6dd899e300b8e1b207b74895"
+        },
+        "date": 1627596393720,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2580,
+            "range": "± 169",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4022,
+            "range": "± 257",
             "unit": "ns/iter"
           }
         ]
