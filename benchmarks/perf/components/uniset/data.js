200e71de58161479304bc3842621bf9fb8114b71
github-action-benchmark <github@users.noreply.github.com>
2021-07-29 22:37:12 +0000

    add Rust Benchmark (cargo) benchmark result for 288aab2ca18012798d51f0454b4ba36f178b815d

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627596377726,
+  "lastUpdate": 1627598232297,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9179,6 +9179,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 28016241,
             "range": "± 1539774",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "288aab2ca18012798d51f0454b4ba36f178b815d",
+          "message": "Update CHANGELOG for 0.3",
+          "timestamp": "2021-07-29T17:29:47-05:00",
+          "tree_id": "8b5972ebb2af9f6f13c26d11d46b8c175689a3c4",
+          "url": "https://github.com/unicode-org/icu4x/commit/288aab2ca18012798d51f0454b4ba36f178b815d"
+        },
+        "date": 1627598226213,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 31289629,
+            "range": "± 1976107",
             "unit": "ns/iter"
           }
         ]
