847f70c01a910cf229e886f66406cd61745b6d70
github-action-benchmark <github@users.noreply.github.com>
2021-07-29 17:34:11 +0000

    add Rust Benchmark (cargo) benchmark result for 8d99c160c4581dc247127c328ced20eda809fc43

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627504111119,
+  "lastUpdate": 1627580051099,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10217,6 +10217,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5180,
             "range": "± 25",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41129501+gnrunge@users.noreply.github.com",
+            "name": "Norbert Runge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8d99c160c4581dc247127c328ced20eda809fc43",
+          "message": "Binary size benchmarking: Rust script to measure size of the ICU4X examples compiled into wasm binaries (#871)\n\nSet up GHA to build wasm binaries, measure file sizes, push results into benchmark dashboard .\r\n\r\nResolves ticket #140.\r\n\r\nCo-authored-by: Greg Tatum <gregtatum@users.noreply.github.com>\r\n\r\nCo-authored-by: Greg Tatum <gregtatum@users.noreply.github.com>",
+          "timestamp": "2021-07-29T12:27:49-05:00",
+          "tree_id": "9e0744b9ce8c059ab9d583a862264748644a9a25",
+          "url": "https://github.com/unicode-org/icu4x/commit/8d99c160c4581dc247127c328ced20eda809fc43"
+        },
+        "date": 1627580048867,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3360,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5117,
+            "range": "± 7",
             "unit": "ns/iter"
           }
         ]
