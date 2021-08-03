455d57aed9ecec54467412c1d34b731702a163a5
Elango <elango@google.com>
2021-08-03 09:14:32 -0700

    Port ICU4C code point trie (#711)
    Co-authored-by: Shane F. Carr <shane@unicode.org>
    
    Co-authored-by: Manish Goregaokar <manishsmail@gmail.com>
    Co-authored-by: Shane F. Carr <shane@unicode.org>

---
+++ experimental/codepointtrie/src/lib.rs
@@ -0,0 +1,8 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+pub mod codepointtrie;
+pub mod error;
+mod impl_const;
+pub mod planes;
