ae33d691f1a54e41e0123239a443f37098190909
github-action-benchmark <github@users.noreply.github.com>
2021-08-06 00:27:56 +0000

    add Rust Benchmark (cargo) benchmark result for b412c7696eb43ad10864f468a41041edf28c6ee2

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1628205763774,
+  "lastUpdate": 1628209676651,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -5615,6 +5615,42 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 1429266,
             "range": "± 65035",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b412c7696eb43ad10864f468a41041edf28c6ee2",
+          "message": "Call destructor for locale (#937)",
+          "timestamp": "2021-08-05T17:21:26-07:00",
+          "tree_id": "9375203a41643aafe4d8d6cbe65f0589a25562cb",
+          "url": "https://github.com/unicode-org/icu4x/commit/b412c7696eb43ad10864f468a41041edf28c6ee2"
+        },
+        "date": 1628209673696,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 656908,
+            "range": "± 31589",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 1374345,
+            "range": "± 66259",
             "unit": "ns/iter"
           }
         ]
