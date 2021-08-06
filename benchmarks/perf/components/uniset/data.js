d56954775881eb43c74ffb139303117e50d20e56
github-action-benchmark <github@users.noreply.github.com>
2021-08-06 21:04:38 +0000

    add Rust Benchmark (cargo) benchmark result for a1b5ada1142196384e18a01d9abca87f2bc5f942

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1628271468192,
+  "lastUpdate": 1628283878881,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9479,6 +9479,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 40619949,
             "range": "± 2917555",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a1b5ada1142196384e18a01d9abca87f2bc5f942",
+          "message": "Add feature flags to CAPI; update WearOS build steps (#939)\n\n* Generate smaller testdata\r\n\r\n* Add smaller_static feature to testdata\r\n\r\n* Add features to capi\r\n\r\n* Update build command for cortex\r\n\r\n* fix ci\r\n\r\n* fix ci\r\n\r\n* skip optional\r\n\r\n* fix target_os\r\n\r\n* Remove feature slicing\r\n\r\n* Only use smaller_static in capi\r\n\r\n* nit",
+          "timestamp": "2021-08-06T13:58:58-07:00",
+          "tree_id": "566c1ec91b00bb8a3b542c76939aebfa00997047",
+          "url": "https://github.com/unicode-org/icu4x/commit/a1b5ada1142196384e18a01d9abca87f2bc5f942"
+        },
+        "date": 1628283876911,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 34353969,
+            "range": "± 58753",
             "unit": "ns/iter"
           }
         ]
