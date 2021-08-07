9b478963b32835c077d3f65b9077b0938e3f5e58
github-action-benchmark <github@users.noreply.github.com>
2021-08-07 02:24:50 +0000

    add Rust Benchmark (cargo) benchmark result for 220f4590e91bebe1aa71a1c60b78fe6c4e68abec

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1628283878881,
+  "lastUpdate": 1628303090869,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9509,6 +9509,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34353969,
             "range": "± 58753",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "220f4590e91bebe1aa71a1c60b78fe6c4e68abec",
+          "message": "Make icu_capi always no_std (#941)",
+          "timestamp": "2021-08-06T19:18:20-07:00",
+          "tree_id": "e9bd413f14e3089b569be6ea09df4c7831afb914",
+          "url": "https://github.com/unicode-org/icu4x/commit/220f4590e91bebe1aa71a1c60b78fe6c4e68abec"
+        },
+        "date": 1628303088482,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 42962766,
+            "range": "± 2874394",
             "unit": "ns/iter"
           }
         ]
