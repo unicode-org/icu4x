7f3e00f678413933bb09c3b39974da3506d8a0eb
Shane F. Carr <shane@unicode.org>
2021-08-10 20:56:33 -0500

    Add another test

--- components/datetime/tests/de.rs
+++ components/datetime/tests/de.rs
@@ -9,7 +9,17 @@ fn de() {
 
 #[test]
 fn roundtrip() {
-    let data = gregory::DatePatternsV1::default();
-    let blob: Vec<u8> = postcard::to_stdvec(&data).unwrap();
+    let mut json_path = icu_testdata::paths::icu4x_json_root();
+    json_path.push("datetime");
+    json_path.push("gregory_patterns@1");
+    json_path.push("en.json");
+    let json_str = std::fs::read_to_string(json_path).unwrap();
+    let json_data: gregory::DatePatternsV1 = serde_json::from_str(&json_str).unwrap();
+    let blob: Vec<u8> = postcard::to_stdvec(&json_data).unwrap();
+
+    let provider = icu_testdata::get_static_provider();
+    let file = provider.get_file_at_path("/datetime/gregory_patterns@1/en").unwrap();
+    assert_eq!(file, blob);
+
     let _de: gregory::DatePatternsV1 = postcard::from_bytes(&blob).unwrap();
 }
