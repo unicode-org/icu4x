2617e6d92825fd54e72177cb7f3de3edfb0c89ec
github-action-benchmark <github@users.noreply.github.com>
2021-08-11 23:19:55 +0000

    add Rust Benchmark (cargo) benchmark result for 07f659080d9beb013402570c6b18791a0ecd247f

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1628698147952,
+  "lastUpdate": 1628723995509,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10901,6 +10901,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4799,
             "range": "± 191",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "07f659080d9beb013402570c6b18791a0ecd247f",
+          "message": "Change benches to use static data provider (#892)",
+          "timestamp": "2021-08-11T18:13:32-05:00",
+          "tree_id": "22ec80426360506800cb47c6e7e5e55d705ac7d7",
+          "url": "https://github.com/unicode-org/icu4x/commit/07f659080d9beb013402570c6b18791a0ecd247f"
+        },
+        "date": 1628723992995,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2466,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4023,
+            "range": "± 17",
             "unit": "ns/iter"
           }
         ]
