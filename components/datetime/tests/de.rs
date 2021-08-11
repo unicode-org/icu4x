3532bc537c5f76ba0e44c0d11670f4e3661a9f90
Shane F. Carr <shane@unicode.org>
2021-08-10 19:44:48 -0500

    Add basic roundtrip test

--- components/datetime/tests/de.rs
+++ components/datetime/tests/de.rs
@@ -6,3 +6,10 @@ fn de() {
     let file = provider.get_file_at_path("/datetime/gregory_patterns@1/en").unwrap();
     let _de: gregory::DatePatternsV1 = postcard::from_bytes(file).unwrap();
 }
+
+#[test]
+fn roundtrip() {
+    let data = gregory::DatePatternsV1::default();
+    let blob: Vec<u8> = postcard::to_stdvec(&data).unwrap();
+    let _de: gregory::DatePatternsV1 = postcard::from_bytes(&blob).unwrap();
+}
