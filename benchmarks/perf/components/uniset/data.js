8e2da3922d11682fd51247f6c5df8866754a5c56
github-action-benchmark <github@users.noreply.github.com>
2021-08-17 21:05:33 +0000

    add Rust Benchmark (cargo) benchmark result for 2edf576106fe276e2e811e8d2869070729af800d

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629232506793,
+  "lastUpdate": 1629234333590,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9719,6 +9719,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 44161304,
             "range": "± 1210391",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2edf576106fe276e2e811e8d2869070729af800d",
+          "message": "Add \"x86tiny\" feature to icu_capi (#957)",
+          "timestamp": "2021-08-17T15:58:43-05:00",
+          "tree_id": "51351e73881d1851ab4434ff7dd516c7837ead09",
+          "url": "https://github.com/unicode-org/icu4x/commit/2edf576106fe276e2e811e8d2869070729af800d"
+        },
+        "date": 1629234331015,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 44834617,
+            "range": "± 1047194",
             "unit": "ns/iter"
           }
         ]
