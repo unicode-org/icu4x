be380402bb38a21570511c53aae2f6b6bd3d7ec0
github-action-benchmark <github@users.noreply.github.com>
2021-08-18 06:42:43 +0000

    add Rust Benchmark (cargo) benchmark result for 245eb95ee01b2cabdea611a2f1b8436b4ad7890b

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629267401418,
+  "lastUpdate": 1629268963539,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6083,6 +6083,42 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 774105,
             "range": "± 27022",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "245eb95ee01b2cabdea611a2f1b8436b4ad7890b",
+          "message": "Update Diplomat and add `pub` to all exported functions (#963)",
+          "timestamp": "2021-08-17T23:36:13-07:00",
+          "tree_id": "02b981699dd728b559fcc90e4e47b7d1f049da58",
+          "url": "https://github.com/unicode-org/icu4x/commit/245eb95ee01b2cabdea611a2f1b8436b4ad7890b"
+        },
+        "date": 1629268960464,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 355618,
+            "range": "± 25918",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 676587,
+            "range": "± 38526",
             "unit": "ns/iter"
           }
         ]
