d886991f48ab485492c9a5d6ee9bd8da46f583b9
github-action-benchmark <github@users.noreply.github.com>
2021-08-18 18:42:33 +0000

    add Rust Benchmark (cargo) benchmark result for d320b0439d7cc48d2b9ac579245f43a16c439705

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629268957339,
+  "lastUpdate": 1629312153404,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12347,6 +12347,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 22712,
             "range": "± 1553",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d320b0439d7cc48d2b9ac579245f43a16c439705",
+          "message": "Add size-optimized FFI functions for Locale and DataProvider (#962)",
+          "timestamp": "2021-08-18T13:35:47-05:00",
+          "tree_id": "4a9f5d7fa3ad807ff877c351757d8fc6e5333859",
+          "url": "https://github.com/unicode-org/icu4x/commit/d320b0439d7cc48d2b9ac579245f43a16c439705"
+        },
+        "date": 1629312150939,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1375,
+            "range": "± 20",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 7702,
+            "range": "± 143",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 24245,
+            "range": "± 372",
             "unit": "ns/iter"
           }
         ]
