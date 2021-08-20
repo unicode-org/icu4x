b850fd5a606cd81f4c77df9adbdd509dbb52a600
github-action-benchmark <github@users.noreply.github.com>
2021-08-20 13:11:18 +0000

    add Rust Benchmark (cargo) benchmark result for 9b41685af03e5cbc8ddd97348ae19719ace30753

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629378554219,
+  "lastUpdate": 1629465078757,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9959,6 +9959,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 43525788,
             "range": "± 2247881",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dminor@mozilla.com",
+            "name": "Dan Minor",
+            "username": "dminor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9b41685af03e5cbc8ddd97348ae19719ace30753",
+          "message": "Reject empty unicode extensions (#970)\n\nFrom https://www.unicode.org/reports/tr35/#Unicode_locale_identifier,\r\nunicode extensions must include at least one attribute or at least one\r\nkeyword.",
+          "timestamp": "2021-08-20T09:04:27-04:00",
+          "tree_id": "1becf955de38f8291f81982f6322c5a11a1328f9",
+          "url": "https://github.com/unicode-org/icu4x/commit/9b41685af03e5cbc8ddd97348ae19719ace30753"
+        },
+        "date": 1629465075474,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 42293926,
+            "range": "± 1952369",
             "unit": "ns/iter"
           }
         ]
