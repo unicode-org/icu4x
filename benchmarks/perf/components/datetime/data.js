d01237b83a2e71fa2b2615ee7bf2e5811d173c05
github-action-benchmark <github@users.noreply.github.com>
2021-08-24 21:07:46 +0000

    add Rust Benchmark (cargo) benchmark result for b66b237d22db389c8333c558937e66e16d12db34

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629465103854,
+  "lastUpdate": 1629839266802,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6299,6 +6299,42 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 709608,
             "range": "± 44340",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b66b237d22db389c8333c558937e66e16d12db34",
+          "message": "Load Diplomat and twiggy from cache; update diplomat (#974)\n\n* Update diplomat\r\n\r\n* Add make tasks for updating diplomat\r\n\r\n* Load Diplomat from cache\r\n\r\n* Load twiggy from cache\r\n\r\n* No need to sync anymore\r\n\r\n* indent\r\n\r\n* Include tool name in cache key name",
+          "timestamp": "2021-08-24T13:55:41-07:00",
+          "tree_id": "7ba0519a6e4776039c31421f98948c41043ab754",
+          "url": "https://github.com/unicode-org/icu4x/commit/b66b237d22db389c8333c558937e66e16d12db34"
+        },
+        "date": 1629839263995,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_overview",
+            "value": 370778,
+            "range": "± 1050",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 686163,
+            "range": "± 3257",
             "unit": "ns/iter"
           }
         ]
