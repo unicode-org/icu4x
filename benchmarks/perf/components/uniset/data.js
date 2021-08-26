bfba27323cfc8a968b850c2c20c5977c9242d92b
github-action-benchmark <github@users.noreply.github.com>
2021-08-26 18:05:06 +0000

    add Rust Benchmark (cargo) benchmark result for 0bd9e98b0bce1cc6724f596e91f51332fd266b28

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629991614950,
+  "lastUpdate": 1630001106181,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10229,6 +10229,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 33799210,
             "range": "± 980041",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dminor@mozilla.com",
+            "name": "Dan Minor",
+            "username": "dminor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0bd9e98b0bce1cc6724f596e91f51332fd266b28",
+          "message": "Add LICENSE to Cargo.toml include section (#1016)\n\nWe need to actually add LICENSE to the Cargo.toml include\r\nsection in order for it to be vendored properly.",
+          "timestamp": "2021-08-26T13:58:26-04:00",
+          "tree_id": "e6d9d779bae57b5480cbd008a90e0a95a503e757",
+          "url": "https://github.com/unicode-org/icu4x/commit/0bd9e98b0bce1cc6724f596e91f51332fd266b28"
+        },
+        "date": 1630001101651,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 49022552,
+            "range": "± 838283",
             "unit": "ns/iter"
           }
         ]
