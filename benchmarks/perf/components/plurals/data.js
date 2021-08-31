56985db324a484711a54937d9d4f232288a6a695
github-action-benchmark <github@users.noreply.github.com>
2021-08-31 15:31:13 +0000

    add Rust Benchmark (cargo) benchmark result for 26b1be1d27a533ae9254c38c76573929fcf14491

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1630423512192,
+  "lastUpdate": 1630423873672,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -13187,6 +13187,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 23301,
             "range": "± 1269",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "26b1be1d27a533ae9254c38c76573929fcf14491",
+          "message": "Lint missing docs in icu_datetime (#725)",
+          "timestamp": "2021-08-31T10:23:54-05:00",
+          "tree_id": "fcfbb60de2b4549617d9271414663d2820394abb",
+          "url": "https://github.com/unicode-org/icu4x/commit/26b1be1d27a533ae9254c38c76573929fcf14491"
+        },
+        "date": 1630423870411,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1373,
+            "range": "± 45",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 7689,
+            "range": "± 344",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 24117,
+            "range": "± 671",
             "unit": "ns/iter"
           }
         ]
