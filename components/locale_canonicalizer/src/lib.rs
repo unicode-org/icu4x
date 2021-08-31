4264691de43ff2b3139f4eb0a7ca7416def33eca
Daniel Minor <dminor@mozilla.com>
2021-08-31 13:40:26 -0400

    Address review feedback

--- components/locale_canonicalizer/src/lib.rs
+++ components/locale_canonicalizer/src/lib.rs
@@ -7,7 +7,9 @@
 //! This API provides functionality to canonicalize locale identifiers based
 //! upon [`CLDR`] data.
 //!
-//! It currently supports the minimize and maximize likely subtags algorithms
+//! It currently supports locale canonicalization based upon the canonicalization
+//! algorithm from [`UTS #35: Unicode LDML 3. LocaleId Canonicalization`],
+//! as well as the minimize and maximize likely subtags algorithms
 //! as described in [`UTS #35: Unicode LDML 3. Likely Subtags`].
 //!
 //! The maximize method potentially updates a passed in locale in place
@@ -18,6 +20,20 @@
 //! 'Remove Likely Subtags' algorithm from [`UTS #35: Unicode LDML 3. Likely Subtags`].
 //!
 //! # Examples
+//!
+//! ```
+//! use icu_locale_canonicalizer::{CanonicalizationResult, LocaleCanonicalizer};
+//! use icu_locid::Locale;
+//!
+//! let provider = icu_testdata::get_provider();
+//! let lc = LocaleCanonicalizer::new(&provider)
+//!     .expect("create failed");
+//!
+//! let mut locale : Locale = "ja-Latn-fonipa-hepburn-heploc".parse()
+//!     .expect("parse failed");
+//! assert_eq!(lc.canonicalize(&mut locale), CanonicalizationResult::Modified);
+//! assert_eq!(locale.to_string(), "ja-Latn-alalc97-fonipa");
+//! ```
 //!
 //! ```
 //! use icu_locale_canonicalizer::{CanonicalizationResult, LocaleCanonicalizer};
@@ -57,23 +73,10 @@
 //! assert_eq!(locale.to_string(), "zh");
 //! ```
 //!
-//! ```
-//! use icu_locale_canonicalizer::{CanonicalizationResult, LocaleCanonicalizer};
-//! use icu_locid::Locale;
-//!
-//! let provider = icu_testdata::get_provider();
-//! let lc = LocaleCanonicalizer::new(&provider)
-//!     .expect("create failed");
-//!
-//! let mut locale : Locale = "ja-Latn-fonipa-hepburn-heploc".parse()
-//!     .expect("parse failed");
-//! assert_eq!(lc.canonicalize(&mut locale), CanonicalizationResult::Modified);
-//! assert_eq!(locale.to_string(), "ja-Latn-alalc97-fonipa");
-//! ```
-//!
 //! [`ICU4X`]: ../icu/index.html
 //! [`CLDR`]: http://cldr.unicode.org/
 //! [`UTS #35: Unicode LDML 3. Likely Subtags`]: https://www.unicode.org/reports/tr35/#Likely_Subtags.
+//! [`UTS #35: Unicode LDML 3. LocaleId Canonicalization`]: http://unicode.org/reports/tr35/#LocaleId_Canonicalization,
 
 #![warn(missing_docs)]
 #![no_std]
--- components/locale_canonicalizer/src/provider.rs
+++ components/locale_canonicalizer/src/provider.rs
@@ -28,7 +28,18 @@ pub mod key {
     derive(serde::Serialize, serde::Deserialize)
 )]
 #[yoke(cloning_zcf)]
-/// Alias data for locale canonicalization.
+/// This alias data is used for locale canonicalization. Each field defines a
+/// mapping from an old identifier to a new identifier, based upon the rules in
+/// from http://unicode.org/reports/tr35/#LocaleId_Canonicalization. The data
+/// is stored in sorted order, allowing for binary search to identify rules to
+/// apply. It is broken down into smaller vectors based upon some characteristic
+/// of the data, to help avoid unnecessary searches. For example, the `sgn_region`
+/// field contains aliases for sign language and region, so that it is not
+/// necessary to search the data unless the input is a sign language.
+///
+/// The algorithm in tr35 is not guaranteed to terminate on data other than what
+/// is currently in CLDR. For this reason, it is not a good idea to attempt to add
+/// or modify aliases for use in this structure.
 pub struct AliasesV1 {
     /// Language data not covered by other rules, normally this will be empty.
     pub language: Vec<(LanguageIdentifier, LanguageIdentifier)>,
@@ -61,7 +72,20 @@ pub struct AliasesV1 {
     derive(serde::Serialize, serde::Deserialize)
 )]
 #[yoke(cloning_zcf)]
-/// Likely subtags data for maximize and minimize.
+/// This likely subtags data is used for the minimize and maximize operations.
+/// Each field defines a mapping from an old identifier to a new identifier,
+/// based upon the rules in
+/// https://www.unicode.org/reports/tr35/#Likely_Subtags.
+///
+/// The data is stored in sorted order, allowing for binary search to identify
+/// rules to apply. It is broken down into smaller vectors based upon the rules
+/// defined for the likely subtags maximize algorithm.
+///
+/// For efficiency, only the relevant part of the LanguageIdentifier is stored
+/// for searching. E.g., the `language_script` field is used to store rules for
+/// `LanguageIdentifier` that contain a language and a script, but not a region.
+/// This also allows for space savings by using a `TinyStr4` rather than a full
+/// `LanguageIdentifier`.
 pub struct LikelySubtagsV1 {
     /// Language and script.
     pub language_script: Vec<(TinyStr4, TinyStr4, LanguageIdentifier)>,
