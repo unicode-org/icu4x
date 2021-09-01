ffdbb9d508f5d678d6ad6f442ec456aadac5397f
github-action-benchmark <github@users.noreply.github.com>
2021-09-01 17:20:21 +0000

    add Rust Benchmark (cargo) benchmark result for a305081cde7d46598c5a2acf8c8681d237c54dfc

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1630423813750,
+  "lastUpdate": 1630516821641,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9449,6 +9449,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 134180,
             "range": "± 8123",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dminor@mozilla.com",
+            "name": "Dan Minor",
+            "username": "dminor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a305081cde7d46598c5a2acf8c8681d237c54dfc",
+          "message": "Add documentation for LocaleCanonicalizer (#1031)\n\n* Add documentation for LocaleCanonicalizer\r\n\r\n* Address review feedback\r\n\r\n* Update generated readme",
+          "timestamp": "2021-09-01T13:13:55-04:00",
+          "tree_id": "7ae03d964d0280e872b26ac0813ce7cdb1ec3808",
+          "url": "https://github.com/unicode-org/icu4x/commit/a305081cde7d46598c5a2acf8c8681d237c54dfc"
+        },
+        "date": 1630516818608,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 122480,
+            "range": "± 7395",
             "unit": "ns/iter"
           }
         ]
