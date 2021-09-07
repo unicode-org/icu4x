04218fbfb5f188675975d741000c0286d2041903
github-action-benchmark <github@users.noreply.github.com>
2021-09-07 15:50:22 +0000

    add Rust Benchmark (cargo) benchmark result for 5f34ab9faa7a0a640118361d44831fdf462bf007

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1630625898560,
+  "lastUpdate": 1631029822572,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -6983,6 +6983,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 565161,
             "range": "± 7335",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5f34ab9faa7a0a640118361d44831fdf462bf007",
+          "message": "Add a DateTimeFormat components bag benchmark (#1037)",
+          "timestamp": "2021-09-07T10:42:46-05:00",
+          "tree_id": "397e07a278e5895f3c0e3e7829ffa0617e85a585",
+          "url": "https://github.com/unicode-org/icu4x/commit/5f34ab9faa7a0a640118361d44831fdf462bf007"
+        },
+        "date": 1631029819347,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 325458,
+            "range": "± 7898",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1712786,
+            "range": "± 58525",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 643793,
+            "range": "± 30526",
             "unit": "ns/iter"
           }
         ]
