45f20c54373d9746be4cd6f13ea44b2fe64a7e04
github-action-benchmark <github@users.noreply.github.com>
2021-09-09 19:04:31 +0000

    add Rust Benchmark (cargo) benchmark result for 8e66c5ea835b43a6a46e9b994b31b6d92f224aed

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1631119825602,
+  "lastUpdate": 1631214271117,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9629,6 +9629,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 123200,
             "range": "± 6751",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8e66c5ea835b43a6a46e9b994b31b6d92f224aed",
+          "message": "Improve safety of SerdeSeDataStructWrap (#1040)",
+          "timestamp": "2021-09-09T13:58:59-05:00",
+          "tree_id": "cda8af44ed85c96b7a5f746befed84dc89b660cd",
+          "url": "https://github.com/unicode-org/icu4x/commit/8e66c5ea835b43a6a46e9b994b31b6d92f224aed"
+        },
+        "date": 1631214268233,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 119740,
+            "range": "± 386",
             "unit": "ns/iter"
           }
         ]
