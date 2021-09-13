f23deb525706a527d9d0724c26f16936a88e4386
Erik Nordin <nordzilla@mozilla.com>
2021-09-13 13:58:43 -0700

    Clean up comments

--- utils/yoke/derive/examples/derive.rs
+++ utils/yoke/derive/examples/derive.rs
@@ -8,28 +8,29 @@ use std::borrow::Cow;
 use yoke_derive::{Yokeable, ZeroCopyFrom};
 use zerovec::{VarZeroVec, ZeroMap, ZeroVec};
 
-// #[derive(Yokeable)]
-// pub struct StringExample {
-//     x: String,
-//}
+#[derive(Yokeable)]
+pub struct StringExample {
+    x: String,
+}
 
-// #[derive(Yokeable, ZeroCopyFrom)]
-// pub struct CowExample<'a> {
-//     x: u8,
-//     y: &'a str,
-//     z: Cow<'a, str>,
-//     w: Cow<'a, [u8]>,
-// }
+#[derive(Yokeable, ZeroCopyFrom)]
+pub struct CowExample<'a> {
+    x: u8,
+    y: &'a str,
+    z: Cow<'a, str>,
+    w: Cow<'a, [u8]>,
+}
 
-// #[derive(Yokeable, ZeroCopyFrom)]
-// #[yoke(prove_covariance_manually)]
-// pub struct ZeroVecExample<'a> {
-//     // https://github.com/unicode-org/icu4x/issues/844
-//     // map: ZeroMap<'a, String, String>,
-//     var: VarZeroVec<'a, String>,
-//     vec: ZeroVec<'a, u16>,
-// }
+#[derive(Yokeable, ZeroCopyFrom)]
+pub struct ZeroVecExample<'a> {
+    var: VarZeroVec<'a, String>,
+    vec: ZeroVec<'a, u16>,
+}
 
+// Since ZeroMap has generic parameters, the Rust compiler cannot
+// prove the covariance of the lifetimes. To use derive(Yokeable)
+// with a type such as ZeroMap, you just add the attribute
+// yoke(prove_covariance_manually)
 #[derive(Yokeable)]
 #[yoke(prove_covariance_manually)]
 pub struct ZeroMapExample<'a> {
