47f93c67f30dd9a1d784b816464929133ecd9bd2
github-action-benchmark <github@users.noreply.github.com>
2021-09-14 19:27:38 +0000

    add Rust Benchmark (cargo) benchmark result for 0719bef4e5e77ebc0dfe5f8cade17707baa81f7e

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1631640481603,
+  "lastUpdate": 1631647658502,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12377,6 +12377,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4939,
             "range": "± 574",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0719bef4e5e77ebc0dfe5f8cade17707baa81f7e",
+          "message": "Add provider_json and provider_bincode features (#1049)\n\n- Migrates manifest to serde-json-core\r\n- Renames bincode to provider_bincode\r\n- Fails early when syntax is not supported",
+          "timestamp": "2021-09-14T14:20:55-05:00",
+          "tree_id": "89bb880f2dce4e5076374c935f9845d80c20547f",
+          "url": "https://github.com/unicode-org/icu4x/commit/0719bef4e5e77ebc0dfe5f8cade17707baa81f7e"
+        },
+        "date": 1631647655760,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2416,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4153,
+            "range": "± 12",
             "unit": "ns/iter"
           }
         ]
