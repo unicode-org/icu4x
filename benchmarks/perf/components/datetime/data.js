df364d67aa7babc1bb94b8afeb756f28f8651304
github-action-benchmark <github@users.noreply.github.com>
2021-09-16 14:27:07 +0000

    add Rust Benchmark (cargo) benchmark result for 630b25eefe50fad986919fe604ed40f81b39ad6d

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1631647697144,
+  "lastUpdate": 1631802427520,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -7403,6 +7403,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 605675,
             "range": "± 44850",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "630b25eefe50fad986919fe604ed40f81b39ad6d",
+          "message": "Add basic setter implementations to the ICU4XLocale FFI (#1055)",
+          "timestamp": "2021-09-16T09:18:50-05:00",
+          "tree_id": "60d51cf91d6c826177d1d5b209efa6c7e4baea3d",
+          "url": "https://github.com/unicode-org/icu4x/commit/630b25eefe50fad986919fe604ed40f81b39ad6d"
+        },
+        "date": 1631802422786,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 375652,
+            "range": "± 16865",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1899783,
+            "range": "± 47404",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 700007,
+            "range": "± 28138",
             "unit": "ns/iter"
           }
         ]
