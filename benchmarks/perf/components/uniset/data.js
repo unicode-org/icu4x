48975ac8cac3a8f8c52ad793545aa6fc4d04e190
github-action-benchmark <github@users.noreply.github.com>
2021-09-16 17:21:35 +0000

    add Rust Benchmark (cargo) benchmark result for a40b7bd39520ba58eb95bd211f539eeb22a3a842

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1631802372524,
+  "lastUpdate": 1631812895046,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10889,6 +10889,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 43230096,
             "range": "± 1128145",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a40b7bd39520ba58eb95bd211f539eeb22a3a842",
+          "message": "Update the READMEs for FFI (#1054)",
+          "timestamp": "2021-09-16T12:14:56-05:00",
+          "tree_id": "ac0704750c7f4f7a3316c1037c156a64c9762ddf",
+          "url": "https://github.com/unicode-org/icu4x/commit/a40b7bd39520ba58eb95bd211f539eeb22a3a842"
+        },
+        "date": 1631812892262,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 35599679,
+            "range": "± 57612",
             "unit": "ns/iter"
           }
         ]
