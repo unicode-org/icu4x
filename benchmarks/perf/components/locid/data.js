df669b4c3c23e74b8d7d0c4cee0b7e1c0509c0dd
github-action-benchmark <github@users.noreply.github.com>
2021-09-20 17:32:38 +0000

    add Rust Benchmark (cargo) benchmark result for f19a64b4662d56c2588a592685e87c1e799c7f2a

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1631900463242,
+  "lastUpdate": 1632159158211,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12557,6 +12557,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4454,
             "range": "± 252",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41129501+gnrunge@users.noreply.github.com",
+            "name": "Norbert Runge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f19a64b4662d56c2588a592685e87c1e799c7f2a",
+          "message": "Add data size benchmark: monitor size of file (#1051)\n\nprovider/testdata/data/testdata.postcard.\r\n\r\nRemove erroneous second print-out of results.\r\n\r\nMoves directory datasize/ with the output data under 'benchmarks/' directory,\r\nfrom 'benchmarks/binsize/'.",
+          "timestamp": "2021-09-20T10:25:55-07:00",
+          "tree_id": "470c8072eebfc55a2fad062841a147b4b49fdae1",
+          "url": "https://github.com/unicode-org/icu4x/commit/f19a64b4662d56c2588a592685e87c1e799c7f2a"
+        },
+        "date": 1632159155099,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2413,
+            "range": "± 143",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4954,
+            "range": "± 211",
             "unit": "ns/iter"
           }
         ]
