f19a64b4662d56c2588a592685e87c1e799c7f2a
Norbert Runge <41129501+gnrunge@users.noreply.github.com>
2021-09-20 10:25:55 -0700

    Add data size benchmark: monitor size of file (#1051)
    provider/testdata/data/testdata.postcard.
    
    Remove erroneous second print-out of results.
    
    Moves directory datasize/ with the output data under 'benchmarks/' directory,
    from 'benchmarks/binsize/'.

--- tools/benchmark/binsize/src/main.rs
+++ tools/benchmark/binsize/src/main.rs
@@ -2,9 +2,11 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-// This module takes as argument a directory path, looks for wasm
-// executables in the directory (i.e., extension .wasm), takes the size in
-// bytes of each, and writes the result in ndjson format to stdout.
+// This module has two modi operandi.
+// 1.: It looks for wasm binaries in a directory (i.e., extension .wasm),
+// notes the size in bytes of each, and writes the result in ndjson format
+// to stdout. Likewise for benchmarking gnuzip'd wasm binaries.
+// 2.: It writes the size of file in ndjson format to stdout.
 // Note that the ICU4X wasm executables have to be build prior to
 // execution of this module. Use 'cargo make wasm-example'.
 use std::env;
@@ -31,23 +33,42 @@ fn wasm_filesize(dir: &str, filesuffix: &str) -> Result<u64, std::io::Error> {
     Ok(count)
 }
 
+fn any_file_size(filepath: &str) -> Result<bool, std::io::Error> {
+    let exists: bool = std::path::Path::new(filepath).exists();
+    if exists {
+        let fsize = fs::metadata(filepath)?.len();
+        println!(
+            "{{\"biggerIsBetter\":false,\"name\":{:?},\"unit\":\"bytes\",\"value\":{}}}",
+            filepath, fsize
+        );
+    }
+    Ok(exists)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
-        eprintln!("Usage: cargo run --package icu_benchmark_binsize -- <WASM BINARY DIRECTORY> <SUFFIX: wasm | gz>");
+        eprintln!("Usage: cargo run --package icu_benchmark_binsize -- <PATH> <wasm | gz | file>");
+        process::exit(1);
+    }
+    let path = &args[1];
+    let path_type = &args[2];
+
+    if path_type != "wasm" && path_type != "gz" && path_type != "file" {
+        eprintln!("Invalid path type {}, use wasm or gz", path_type);
         process::exit(1);
     }
 
-    let wasmdir = &args[1];
-    let wasmsuffix = &args[2];
-    if wasmsuffix != "wasm" && wasmsuffix != "gz" {
-        eprintln!("Invalid file suffix {}, use wasm or gz", wasmsuffix);
-        process::exit(1);
-    }
-
-    let count = wasm_filesize(wasmdir, wasmsuffix);
-    if count.unwrap() == 0 {
-        eprintln!("No wasm binaries found in directory {}", wasmdir);
-        process::exit(1);
+    if path_type == "file" {
+        let rc = any_file_size(path);
+        if !rc.unwrap() {
+            eprintln!("File {} not found", path);
+        }
+    } else {
+        let count = wasm_filesize(path, path_type);
+        if count.unwrap() == 0 {
+            eprintln!("No wasm binaries found in directory {}", path);
+            process::exit(1);
+        }
     }
 }
