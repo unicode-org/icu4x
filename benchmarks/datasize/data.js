ad6b420d769e5379db0628f1809853665fbeb273
github-action-benchmark <github@users.noreply.github.com>
2021-09-21 18:28:21 +0000

    add Benchmark (ndjson) benchmark result for adf02273511847c7b3422a0916e05d8ac7c236ec

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632171415572,
+  "lastUpdate": 1632248901574,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -53,6 +53,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/04cfbd2684f1eaa0439ffe3e394fd496e6160a85"
         },
         "date": 1632171412441,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 582678,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "adf02273511847c7b3422a0916e05d8ac7c236ec",
+          "message": "Add DataPayload::map_project functions (#920)",
+          "timestamp": "2021-09-21T13:26:51-05:00",
+          "tree_id": "24a3d122e6416385e322c8bfb13b5d2cb24bfacd",
+          "url": "https://github.com/unicode-org/icu4x/commit/adf02273511847c7b3422a0916e05d8ac7c236ec"
+        },
+        "date": 1632248897832,
         "tool": "ndjson",
         "benches": [
           {
