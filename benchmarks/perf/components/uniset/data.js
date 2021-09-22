8a8aad1d75d43675e20aca61dfc4c72723d4f96d
github-action-benchmark <github@users.noreply.github.com>
2021-09-22 05:11:19 +0000

    add Rust Benchmark (cargo) benchmark result for fea0eb7cba1a52e4316b444edff46575f4639c94

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632278938057,
+  "lastUpdate": 1632287479424,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -11099,6 +11099,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 37292325,
             "range": "± 1828074",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fea0eb7cba1a52e4316b444edff46575f4639c94",
+          "message": "More docs for ZeroMap and add Vec<u8> as supported type (#1057)",
+          "timestamp": "2021-09-22T00:05:37-05:00",
+          "tree_id": "e375ec6c540f24dc62f6fefe1339c992de9efbed",
+          "url": "https://github.com/unicode-org/icu4x/commit/fea0eb7cba1a52e4316b444edff46575f4639c94"
+        },
+        "date": 1632287476754,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 35119623,
+            "range": "± 151010",
             "unit": "ns/iter"
           }
         ]
