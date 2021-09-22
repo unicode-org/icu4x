2d8b3df0735d971f17db495582b8bf2d8e1541c9
github-action-benchmark <github@users.noreply.github.com>
2021-09-22 02:48:53 +0000

    add Rust Benchmark (cargo) benchmark result for 007b532720771ef334a02d8c4704db6f689b8493

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632249226494,
+  "lastUpdate": 1632278933597,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12665,6 +12665,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4156,
             "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "007b532720771ef334a02d8c4704db6f689b8493",
+          "message": "Restructure FFI folder (#1060)\n\n* Move all diplomat generated stuff into diplomat folder\n\n* Update readme\n\n* Update CI paths\n\n* Update tests\n\n* Add c readme\n\n* Update script\n\n* fix readmes\n\n* fix wasm",
+          "timestamp": "2021-09-21T19:42:24-07:00",
+          "tree_id": "684b960c5edb80e5e5f92613a3bf00cef648d7f9",
+          "url": "https://github.com/unicode-org/icu4x/commit/007b532720771ef334a02d8c4704db6f689b8493"
+        },
+        "date": 1632278930779,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2437,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4158,
+            "range": "± 5",
             "unit": "ns/iter"
           }
         ]
