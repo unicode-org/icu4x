307a5e6a6a855e463112fe36e0551f82fddebf28
Shane F. Carr <shane@unicode.org>
2021-09-22 01:13:42 -0500

    Refactoring

--- provider/blob/src/blob_data_provider.rs
+++ provider/blob/src/blob_data_provider.rs
@@ -5,6 +5,7 @@
 use crate::blob_schema::BlobSchema;
 use crate::path_util;
 use alloc::rc::Rc;
+use alloc::string::String;
 use icu_provider::prelude::*;
 use icu_provider::serde::{SerdeDeDataProvider, SerdeDeDataReceiver};
 use serde::de::Deserialize;
@@ -35,29 +36,40 @@ where
     for<'de> YokeTraitHack<<M::Yokeable as Yokeable<'de>>::Output>: serde::de::Deserialize<'de>,
 {
     fn load_payload(&self, req: &DataRequest) -> Result<DataResponse<'data, M>, DataError> {
+        enum LocalError {
+            MissingResourceKey,
+            Postcard(postcard::Error),
+        }
         let path = path_util::resource_path_to_string(&req.resource_path);
-        let raw_payload: Result<Yoke<M::Yokeable, Rc<[u8]>>, DataError> =
-            self.blob.try_project_cloned_with_capture(
-                (path, req.resource_path.key),
-                move |blob, (path, key), _| {
+        let raw_payload = self
+            .blob
+            .try_project_cloned_with_capture::<M::Yokeable, String, LocalError>(
+                path,
+                move |blob, path, _| {
                     let BlobSchema::V001(blob) = blob;
                     let file = blob
                         .resources
                         .get(&*path)
-                        .ok_or(DataError::MissingResourceKey(key))
+                        .ok_or(LocalError::MissingResourceKey)
                         .map(|v| *v)?;
                     let mut d = postcard::Deserializer::from_bytes(file);
                     let data =
                         YokeTraitHack::<<M::Yokeable as Yokeable>::Output>::deserialize(&mut d)
-                            .map_err(DataError::new_resc_error)?;
+                            .map_err(LocalError::Postcard)?;
                     Ok(data.0)
                 },
-            );
+            )
+            .map_err(|local_error| match local_error {
+                LocalError::MissingResourceKey => {
+                    DataError::MissingResourceKey(req.resource_path.key)
+                }
+                LocalError::Postcard(err) => DataError::new_resc_error(err),
+            })?;
         Ok(DataResponse {
             metadata: DataResponseMetadata {
                 data_langid: req.resource_path.options.langid.clone(),
             },
-            payload: Some(DataPayload::from_rc_buffer_yoke(raw_payload?)),
+            payload: Some(DataPayload::from_rc_buffer_yoke(raw_payload)),
         })
     }
 }
