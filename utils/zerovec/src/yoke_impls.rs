ba0cd6ab25418cc91fb5bb1a97af86af6793512a
Manish Goregaokar <manishsmail@gmail.com>
2021-09-23 14:15:15 -0700

    cargo fix --features yoke

--- utils/zerovec/src/yoke_impls.rs
+++ utils/zerovec/src/yoke_impls.rs
@@ -2,7 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use alloc::vec::Vec;
+
 use crate::map::ZeroMapKV;
 use crate::ule::*;
 use crate::{VarZeroVec, ZeroMap, ZeroVec};
