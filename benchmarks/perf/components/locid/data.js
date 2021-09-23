b86d2d8a10a30bf97486e5b2e2c4e311ec6921bb
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 15:01:36 +0000

    add Rust Benchmark (cargo) benchmark result for ea4b7d9d7b2363dbcfd5d883e9890fe881ddea45

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632362446128,
+  "lastUpdate": 1632409296888,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12845,6 +12845,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5260,
             "range": "± 215",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ea4b7d9d7b2363dbcfd5d883e9890fe881ddea45",
+          "message": "Adding BlobDataProvider for dynamically loaded data blobs (#1084)\n\n- Adds try_project_cloned_with_capture",
+          "timestamp": "2021-09-23T09:54:39-05:00",
+          "tree_id": "0215c38db8f7aa7793fc7798353b719114dd4b5f",
+          "url": "https://github.com/unicode-org/icu4x/commit/ea4b7d9d7b2363dbcfd5d883e9890fe881ddea45"
+        },
+        "date": 1632409293449,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2882,
+            "range": "± 96",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4841,
+            "range": "± 161",
             "unit": "ns/iter"
           }
         ]
