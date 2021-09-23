e167e77fbbb329b2456b1024f005b4155e252e98
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 00:39:18 +0000

    add Rust Benchmark (cargo) benchmark result for c6ec54a4d7bb189161c11fd0cb8784a27066e782

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632348987115,
+  "lastUpdate": 1632357558382,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12773,6 +12773,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4215,
             "range": "± 2",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6ec54a4d7bb189161c11fd0cb8784a27066e782",
+          "message": "Upgrade nightly version to 2021-09-22 (#1088)",
+          "timestamp": "2021-09-22T19:31:17-05:00",
+          "tree_id": "ec2512ee6bd90179a95e58722b1b4e14e0974b14",
+          "url": "https://github.com/unicode-org/icu4x/commit/c6ec54a4d7bb189161c11fd0cb8784a27066e782"
+        },
+        "date": 1632357555128,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2510,
+            "range": "± 54",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4300,
+            "range": "± 15",
             "unit": "ns/iter"
           }
         ]
