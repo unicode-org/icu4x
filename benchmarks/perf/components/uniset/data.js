a7e3c9780e8b67e3f213f70c11bf79a97a839c48
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 21:14:25 +0000

    add Rust Benchmark (cargo) benchmark result for 292419806edd3c5a3e1e430f5bc820c296c8ee83

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632409294129,
+  "lastUpdate": 1632431665366,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -11249,6 +11249,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34014771,
             "range": "± 1214326",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58569820+nordzilla@users.noreply.github.com",
+            "name": "Erik Nordin",
+            "username": "nordzilla"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "292419806edd3c5a3e1e430f5bc820c296c8ee83",
+          "message": "Clean up type fallback in TimeZonesProvider (#1063)\n\n* Clean up type fallback in TimeZonesProvider\r\n\r\nThe old way of handling type fallback in the DataProvider made assumptions\r\nabout the shape of the CLDR data (that I realize now were wrong, in at least one case).\r\n\r\nThis adds an explicit function to handle type fallback exactly as specified by UTS-35,\r\nimproving the readability of the code.\r\n\r\nUpdates the data and test case to fix the prior mistake.",
+          "timestamp": "2021-09-23T14:08:46-07:00",
+          "tree_id": "4a013e7a2297f7351f548b28a8b0813ac08abd0a",
+          "url": "https://github.com/unicode-org/icu4x/commit/292419806edd3c5a3e1e430f5bc820c296c8ee83"
+        },
+        "date": 1632431662117,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 36243690,
+            "range": "± 63609",
             "unit": "ns/iter"
           }
         ]
