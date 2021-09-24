6dfdac6ae98bdd2d96d83c13f423a42c7f307b93
Zibi Braniecki <zibi@braniecki.net>
2021-09-23 18:40:36 -0700

    Switch DTF field symbols to use macros. (#1093)
    * Switch DTF field symbols to use macros.
    
    * Revert Minute changes

--- components/datetime/src/fields/mod.rs
+++ components/datetime/src/fields/mod.rs
@@ -2,6 +2,8 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+#[macro_use]
+mod macros;
 mod length;
 pub(crate) mod symbols;
 
