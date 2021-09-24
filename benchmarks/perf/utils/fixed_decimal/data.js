be4ca73b24824709836930214389763f7f54102b
github-action-benchmark <github@users.noreply.github.com>
2021-09-24 23:15:48 +0000

    add Rust Benchmark (cargo) benchmark result for b890acff2184b6a8857a5193fd20cbf55b55cd6b

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632448070065,
+  "lastUpdate": 1632525348090,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10319,6 +10319,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 113254,
             "range": "± 6958",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b890acff2184b6a8857a5193fd20cbf55b55cd6b",
+          "message": "Make DTF PatternItem Copy (#1098)\n\n* Switch Skeletons to work on chars\r\n\r\n* Switch API of field type index operations to be more explicit.\r\n\r\n* Switch PatternItem::Literal to be a char",
+          "timestamp": "2021-09-24T16:09:33-07:00",
+          "tree_id": "aec04be38b879ace95b7434cb704bd2e92cde362",
+          "url": "https://github.com/unicode-org/icu4x/commit/b890acff2184b6a8857a5193fd20cbf55b55cd6b"
+        },
+        "date": 1632525344259,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 127894,
+            "range": "± 16226",
             "unit": "ns/iter"
           }
         ]
