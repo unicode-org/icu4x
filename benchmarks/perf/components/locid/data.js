e9c1f27351871a8ed8f62ff8c42521cc2824ec22
github-action-benchmark <github@users.noreply.github.com>
2021-09-24 01:48:22 +0000

    add Rust Benchmark (cargo) benchmark result for 6dfdac6ae98bdd2d96d83c13f423a42c7f307b93

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632433434806,
+  "lastUpdate": 1632448102045,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12953,6 +12953,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5029,
             "range": "± 327",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6dfdac6ae98bdd2d96d83c13f423a42c7f307b93",
+          "message": "Switch DTF field symbols to use macros. (#1093)\n\n* Switch DTF field symbols to use macros.\r\n\r\n* Revert Minute changes",
+          "timestamp": "2021-09-23T18:40:36-07:00",
+          "tree_id": "a90826366e0d1cec72aaeef4a1c4311b3bda0835",
+          "url": "https://github.com/unicode-org/icu4x/commit/6dfdac6ae98bdd2d96d83c13f423a42c7f307b93"
+        },
+        "date": 1632448098788,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2659,
+            "range": "± 94",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4665,
+            "range": "± 254",
             "unit": "ns/iter"
           }
         ]
