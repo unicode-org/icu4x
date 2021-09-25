680217a2dfa1bc31a2107b8c5febadac0c8e5ca9
github-action-benchmark <github@users.noreply.github.com>
2021-09-25 00:43:03 +0000

    add Rust Benchmark (cargo) benchmark result for 49079e09fa7fb6baa998ed1bcc0949a8174dbbe7

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632528642972,
+  "lastUpdate": 1632530583516,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -11429,6 +11429,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 35809805,
             "range": "± 1451556",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "49079e09fa7fb6baa998ed1bcc0949a8174dbbe7",
+          "message": "Fix wasm-cpp-emscripten (#1099)",
+          "timestamp": "2021-09-24T19:37:04-05:00",
+          "tree_id": "51edaf9605a21b5d191b90ccf1836fcf4a8b7c7b",
+          "url": "https://github.com/unicode-org/icu4x/commit/49079e09fa7fb6baa998ed1bcc0949a8174dbbe7"
+        },
+        "date": 1632530579575,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 41433127,
+            "range": "± 158935",
             "unit": "ns/iter"
           }
         ]
