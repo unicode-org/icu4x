183a2e1be460955c50bc15512f2ffcb1f161d713
github-action-benchmark <github@users.noreply.github.com>
2021-09-29 04:18:08 +0000

    add Benchmark (ndjson) benchmark result for 30a5f4ad1417d5c1bd2189d139ac09b8726644f2

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632870273759,
+  "lastUpdate": 1632889088699,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -713,6 +713,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/5d367e88ef6a21ef055acff920ace6cec87c5b1c"
         },
         "date": 1632870269695,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 583108,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "30a5f4ad1417d5c1bd2189d139ac09b8726644f2",
+          "message": "Introduce DTF runtime Pattern backed by ZV (#1112)\n\n* Introduce DTF runtime Pattern backed by ZV\r\n\r\n* Apply reviewers feedback\r\n\r\n* Make num_enum work with no_std\r\n\r\n* Apply reviewers feedback\r\n\r\n* Append final feedback",
+          "timestamp": "2021-09-28T21:16:55-07:00",
+          "tree_id": "0f1da621772ceece2c525267ba0490f2dfc2a7d3",
+          "url": "https://github.com/unicode-org/icu4x/commit/30a5f4ad1417d5c1bd2189d139ac09b8726644f2"
+        },
+        "date": 1632889084740,
         "tool": "ndjson",
         "benches": [
           {
