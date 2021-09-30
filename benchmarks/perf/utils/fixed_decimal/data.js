4395a7bdf2b4eb26ca4e52aa14135bde45ec1140
github-action-benchmark <github@users.noreply.github.com>
2021-09-30 23:46:20 +0000

    add Rust Benchmark (cargo) benchmark result for 20100345fc28379015107c7b4aa8b3d088bd41c8

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633037389632,
+  "lastUpdate": 1633045580298,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10769,6 +10769,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 104201,
             "range": "± 7871",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "20100345fc28379015107c7b4aa8b3d088bd41c8",
+          "message": "Move Display bound on ULE::Error for Serde (#1130)",
+          "timestamp": "2021-09-30T18:41:01-05:00",
+          "tree_id": "ae42e9daadea8e64133edc94add784ed47a65c9c",
+          "url": "https://github.com/unicode-org/icu4x/commit/20100345fc28379015107c7b4aa8b3d088bd41c8"
+        },
+        "date": 1633045577087,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 116642,
+            "range": "± 286",
             "unit": "ns/iter"
           }
         ]
