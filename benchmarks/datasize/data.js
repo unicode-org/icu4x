d9806f169050c66cf0b8df68cd120769e34d19d7
github-action-benchmark <github@users.noreply.github.com>
2021-09-30 21:25:30 +0000

    add Benchmark (ndjson) benchmark result for 47f667eae48d6b8a446a36e1cae01b8eccf66881

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632957534197,
+  "lastUpdate": 1633037130619,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -773,6 +773,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/848f20957de134138ff1a27d341105140df7d702"
         },
         "date": 1632957529700,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 583108,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "47f667eae48d6b8a446a36e1cae01b8eccf66881",
+          "message": "Add ZeroVec::iter_mut() (#1128)\n\n* Add ZeroVec::iter_mut()\r\n\r\n* Add ZeroVec::for_each_mut()\r\n\r\n* Expose make_mut, remove iter_mut, expose try_for_each_mut\r\n\r\n* Update utils/zerovec/src/zerovec/mod.rs\r\n\r\nCo-authored-by: Shane F. Carr <shane@unicode.org>\r\n\r\n* apply suggestion\r\n\r\n* make_mut -> to_mut\r\n\r\n* fix\r\n\r\nCo-authored-by: Shane F. Carr <shane@unicode.org>",
+          "timestamp": "2021-09-30T14:24:14-07:00",
+          "tree_id": "d605158ffdabb5d6a61471cdcba0f8cff72a2da2",
+          "url": "https://github.com/unicode-org/icu4x/commit/47f667eae48d6b8a446a36e1cae01b8eccf66881"
+        },
+        "date": 1633037126547,
         "tool": "ndjson",
         "benches": [
           {
