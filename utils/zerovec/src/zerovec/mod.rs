81f5cf14a9206cdf1c3ac42ce38dd7f97601636d
Manish Goregaokar <manishsmail@gmail.com>
2021-09-30 13:50:29 -0700

    Expose make_mut, remove iter_mut, expose try_for_each_mut

--- utils/zerovec/src/zerovec/mod.rs
+++ utils/zerovec/src/zerovec/mod.rs
@@ -392,31 +392,6 @@ where
         self.as_slice().iter().map(T::from_unaligned)
     }
 
-    /// Gets a mutable iterator over the elements. If you just need to mutate
-    /// elements in-place, consider using [`ZeroVec::for_each_mut()`] instead.
-    ///
-    /// This will convert the ZeroVec into an owned ZeroVec if not already the case.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use zerovec::ZeroVec;
-    /// use zerovec::ule::AsULE;
-    ///
-    /// let bytes: &[u8] = &[0xD3, 0x00, 0x19, 0x01, 0xA5, 0x01, 0xCD, 0x01];
-    /// let mut zerovec: ZeroVec<u16> = ZeroVec::parse_byte_slice(bytes).expect("infallible");
-    ///
-    /// for i in zerovec.iter_mut() {
-    ///     *i = (u16::from_unaligned(i) + 1).into();
-    /// }
-    ///
-    /// assert_eq!(zerovec.to_vec(), &[212, 282, 422, 462]);
-    /// ```
-    #[inline]
-    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T::ULE> + '_ {
-        self.make_mut().iter_mut()
-    }
-
     /// Mutates each element according to a given function, meant to be
     /// a more convenient version of [`ZeroVec::iter_mut()`] which serves fewer use cases.
     ///
@@ -444,6 +419,38 @@ where
         });
     }
 
+    /// Same as [`ZeroVec::for_each_mut()`], but bubbles up errors.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use zerovec::ZeroVec;
+    /// use zerovec::ule::AsULE;
+    ///
+    /// let bytes: &[u8] = &[0xD3, 0x00, 0x19, 0x01, 0xA5, 0x01, 0xCD, 0x01];
+    /// let mut zerovec: ZeroVec<u16> = ZeroVec::parse_byte_slice(bytes).expect("infallible");
+    ///
+    /// zerovec.try_for_each_mut(|item| {
+    ///     *item = item.checked_add(1).ok_or(())?;
+    ///     Ok(())   
+    /// })?;
+    ///
+    /// assert_eq!(zerovec.to_vec(), &[212, 282, 422, 462]);
+    /// # Ok::<(), ()>(())
+    /// ```
+    #[inline]
+    pub fn try_for_each_mut<E>(
+        &mut self,
+        mut f: impl FnMut(&mut T) -> Result<(), E>,
+    ) -> Result<(), E> {
+        self.make_mut().iter_mut().try_for_each(|item| {
+            let mut aligned = T::from_unaligned(item);
+            f(&mut aligned)?;
+            *item = aligned.as_unaligned();
+            Ok(())
+        })
+    }
+
     /// Converts a borrowed ZeroVec to an owned ZeroVec. No-op if already owned.
     ///
     /// # Example
@@ -468,7 +475,8 @@ where
         }
     }
 
-    /// Allows the ZeroVec to be mutated by converting it to an owned variant
+    /// Allows the ZeroVec to be mutated by converting it to an owned variant, and producing
+    /// a mutable vector of ULEs.
     ///
     /// # Example
     ///
@@ -483,10 +491,7 @@ where
     /// zerovec.make_mut().push(12_u16.as_unaligned());
     /// assert!(matches!(zerovec, ZeroVec::Owned(_)));
     /// ```
-    //
-    // This function is crate-public for now since we don't yet want to stabilize
-    // the internal implementation details
-    pub(crate) fn make_mut(&mut self) -> &mut Vec<T::ULE> {
+    pub fn make_mut(&mut self) -> &mut Vec<T::ULE> {
         match self {
             ZeroVec::Owned(ref mut vec) => vec,
             ZeroVec::Borrowed(_) => {
