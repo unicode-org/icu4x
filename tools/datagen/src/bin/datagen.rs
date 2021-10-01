bb2f1e279675e374233d260112cb2f12125d2a05
Manish Goregaokar <manishsmail@gmail.com>
2021-10-01 15:32:21 -0700

    Set default datagen log level to INFO (#1138)
    * Update SimpleLogger
    
    * Set default datagen log level to INFO

--- tools/datagen/src/bin/datagen.rs
+++ tools/datagen/src/bin/datagen.rs
@@ -207,7 +207,11 @@ fn main() -> anyhow::Result<()> {
         .get_matches();
 
     match matches.occurrences_of("VERBOSE") {
-        0 => SimpleLogger::from_env().init().unwrap(),
+        0 => SimpleLogger::new()
+            .env()
+            .with_level(log::LevelFilter::Info)
+            .init()
+            .unwrap(),
         1 => SimpleLogger::new()
             .with_level(log::LevelFilter::Info)
             .init()
