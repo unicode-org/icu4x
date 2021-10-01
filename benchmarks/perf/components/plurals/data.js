82a68dde91453bb239467c4af6ba078903e6071b
github-action-benchmark <github@users.noreply.github.com>
2021-10-01 17:25:34 +0000

    add Rust Benchmark (cargo) benchmark result for e5067da312ef0cd0b31894e82239be70a91f5602

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633108763233,
+  "lastUpdate": 1633109134740,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15287,6 +15287,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 18590,
             "range": "± 88",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e5067da312ef0cd0b31894e82239be70a91f5602",
+          "message": "Clean up DTF Skeleton. (#1132)\n\n* Clean up DTF Skeleton.\r\n\r\n* Fix fmt\r\n\r\n* Fix clippy\r\n\r\n* Fix regression in prefer_matched_pattern",
+          "timestamp": "2021-10-01T10:18:36-07:00",
+          "tree_id": "5d38e239551c13728f04748c2f3734200a7a2c78",
+          "url": "https://github.com/unicode-org/icu4x/commit/e5067da312ef0cd0b31894e82239be70a91f5602"
+        },
+        "date": 1633109130221,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1050,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6407,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 20907,
+            "range": "± 110",
             "unit": "ns/iter"
           }
         ]
