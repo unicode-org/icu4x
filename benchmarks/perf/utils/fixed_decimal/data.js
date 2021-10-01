41002b7da8c2db798dd1dd6a1cf6722308ee5d70
github-action-benchmark <github@users.noreply.github.com>
2021-10-01 17:25:14 +0000

    add Rust Benchmark (cargo) benchmark result for e5067da312ef0cd0b31894e82239be70a91f5602

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633108734625,
+  "lastUpdate": 1633109114739,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -10919,6 +10919,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 139995,
             "range": "± 562",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e5067da312ef0cd0b31894e82239be70a91f5602",
+          "message": "Clean up DTF Skeleton. (#1132)\n\n* Clean up DTF Skeleton.\r\n\r\n* Fix fmt\r\n\r\n* Fix clippy\r\n\r\n* Fix regression in prefer_matched_pattern",
+          "timestamp": "2021-10-01T10:18:36-07:00",
+          "tree_id": "5d38e239551c13728f04748c2f3734200a7a2c78",
+          "url": "https://github.com/unicode-org/icu4x/commit/e5067da312ef0cd0b31894e82239be70a91f5602"
+        },
+        "date": 1633109109820,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 136774,
+            "range": "± 11112",
             "unit": "ns/iter"
           }
         ]
