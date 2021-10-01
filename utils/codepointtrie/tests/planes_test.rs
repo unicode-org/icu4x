d11c8404177eb0f762cda93d376a772e08f932bd
Elango <elango@google.com>
2021-10-01 07:32:54 -0700

    Move CodePointTrie to components/utils (#1114)

--- utils/codepointtrie/tests/planes_test.rs
+++ utils/codepointtrie/tests/planes_test.rs
@@ -7,10 +7,12 @@ mod test_util;
 use icu_codepointtrie::codepointtrie::*;
 use icu_codepointtrie::error::Error;
 use icu_codepointtrie::planes::get_planes_trie;
+use test_util::UnicodeEnumeratedProperty;
+
+use core::convert::TryFrom;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use test_util::UnicodeEnumeratedProperty;
 use zerovec::ZeroVec;
 
 #[test]
@@ -39,19 +41,28 @@ fn planes_trie_deserialize_check_test() {
 
     let code_point_trie_struct = planes_enum_prop.code_point_trie.trie_struct;
 
+    let trie_type_enum = match TrieTypeEnum::try_from(code_point_trie_struct.trie_type_enum_val) {
+        Ok(enum_val) => enum_val,
+        _ => {
+            panic!(
+                "Could not parse trie_type serialized enum value in test data file: {}",
+                code_point_trie_struct.name
+            );
+        }
+    };
+
     let trie_header = CodePointTrieHeader {
-        index_length: code_point_trie_struct.index_length,
-        data_length: code_point_trie_struct.data_length,
         high_start: code_point_trie_struct.high_start,
         shifted12_high_start: code_point_trie_struct.shifted12_high_start,
         index3_null_offset: code_point_trie_struct.index3_null_offset,
         data_null_offset: code_point_trie_struct.data_null_offset,
         null_value: code_point_trie_struct.null_value,
+        trie_type: trie_type_enum,
     };
 
     let data = ZeroVec::from_slice(code_point_trie_struct.data_8.as_ref().unwrap());
     let index = ZeroVec::from_slice(&code_point_trie_struct.index);
-    let trie_result: Result<CodePointTrie<u8, Small>, Error> =
+    let trie_result: Result<CodePointTrie<u8>, Error> =
         CodePointTrie::try_new(trie_header, index, data);
     let act_planes_trie = trie_result.unwrap();
 
