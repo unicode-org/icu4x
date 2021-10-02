c4e0ddc04a0f9f1c2a663fc4eca37215aeda22ad
github-action-benchmark <github@users.noreply.github.com>
2021-10-02 07:49:46 +0000

    add Benchmark (ndjson) benchmark result for e6245c2001931f22c60fd889f9523e6830597d1e

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633139577177,
+  "lastUpdate": 1633160986412,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -1043,6 +1043,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/d169c30a21eb8a784801ee88a90a741f8da86902"
         },
         "date": 1633139573557,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 583108,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kupiakos@gmail.com",
+            "name": "Alyssa Haroldsen",
+            "username": "kupiakos"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e6245c2001931f22c60fd889f9523e6830597d1e",
+          "message": "Implement and consolidate VarZeroVec mutation operations (#1127)\n\n* Implement and consolidate VarZeroVec mutation operations\r\n\r\nThis consolidates all bit-shifting mutation operations into a single\r\nfunction, since there's significant shared logic.\r\n\r\nFixes #1080.\r\n\r\nCo-authored-by: Manish Goregaokar <manishsmail@gmail.com>",
+          "timestamp": "2021-10-02T00:48:26-07:00",
+          "tree_id": "3de484e2b5cf81706a5b2e80bed524f52139229d",
+          "url": "https://github.com/unicode-org/icu4x/commit/e6245c2001931f22c60fd889f9523e6830597d1e"
+        },
+        "date": 1633160982144,
         "tool": "ndjson",
         "benches": [
           {
