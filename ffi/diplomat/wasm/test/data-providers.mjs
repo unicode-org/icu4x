6e064596c97f841642913cf82760a852b6274d94
Shane F. Carr <sffc@google.com>
2021-10-03 00:25:39 -0500

    Move package.json up one level and add better path resolution (#1143)

--- ffi/diplomat/wasm/test/data-providers.mjs
+++ ffi/diplomat/wasm/test/data-providers.mjs
@@ -1,13 +1,17 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 import test from 'ava';
 import { promises as fsPromises } from 'fs';
 
 import { ICU4XFixedDecimal, ICU4XLocale, ICU4XDataProvider, ICU4XFixedDecimalFormat, ICU4XFixedDecimalFormatOptions } from "../lib/api.mjs"
 
-const BLOB_PATH = "../../../../provider/testdata/data/testdata.postcard";
+import { TESTDATA_POSTCARD_PATH } from "../lib/paths.mjs"
 
 test("use create_from_byte_slice to format a simple decimal", async t => {
   const locale = ICU4XLocale.create("bn");
-  const nodeBuffer = await fsPromises.readFile(BLOB_PATH);
+  const nodeBuffer = await fsPromises.readFile(TESTDATA_POSTCARD_PATH);
   const bytes = new Uint8Array(nodeBuffer.buffer, nodeBuffer.byteOffset, nodeBuffer.length);
   const result = ICU4XDataProvider.create_from_byte_slice(bytes);
   t.assert(result.success);
