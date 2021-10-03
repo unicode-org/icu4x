fc7600e7dce4a2546ca6d14f49391541f3687fbb
github-action-benchmark <github@users.noreply.github.com>
2021-10-03 05:40:17 +0000

    add Rust Benchmark (cargo) benchmark result for 2d64d81d4661090c0d446b7bd700e65035840a5e

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633239484058,
+  "lastUpdate": 1633239617466,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9167,6 +9167,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 611298,
             "range": "± 71575",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2d64d81d4661090c0d446b7bd700e65035840a5e",
+          "message": "Update tracking bug for removal of YokeTraitHack (#1134)",
+          "timestamp": "2021-10-03T00:29:22-05:00",
+          "tree_id": "3b946fe94fb1248550596d343b88e32157cc0a96",
+          "url": "https://github.com/unicode-org/icu4x/commit/2d64d81d4661090c0d446b7bd700e65035840a5e"
+        },
+        "date": 1633239613130,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 345825,
+            "range": "± 14127",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1798535,
+            "range": "± 87039",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 696528,
+            "range": "± 32975",
             "unit": "ns/iter"
           }
         ]
