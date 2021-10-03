4b0e20cee964f44f293dfa84c60aac2fcb46e00d
github-action-benchmark <github@users.noreply.github.com>
2021-10-03 05:37:22 +0000

    add Rust Benchmark (cargo) benchmark result for f7f0bc43874d7d0b24f44011498f83ef55c1d0ce

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633239330094,
+  "lastUpdate": 1633239442182,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12089,6 +12089,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 32205375,
             "range": "± 1731182",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f7f0bc43874d7d0b24f44011498f83ef55c1d0ce",
+          "message": "Add try_map_project_[cloned]_with_capture (#1135)",
+          "timestamp": "2021-10-03T00:28:14-05:00",
+          "tree_id": "21fd95b724da056d4e2cbb3184864e82a70b9815",
+          "url": "https://github.com/unicode-org/icu4x/commit/f7f0bc43874d7d0b24f44011498f83ef55c1d0ce"
+        },
+        "date": 1633239437922,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 33118590,
+            "range": "± 1054702",
             "unit": "ns/iter"
           }
         ]
