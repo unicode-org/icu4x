b9802a82cca7674c9cb93acffdffc59f1638352a
github-action-benchmark <github@users.noreply.github.com>
2021-10-04 22:56:14 +0000

    add Benchmark (ndjson) benchmark result for 807116bfedf24c04d6bd7987431eb6b2ddc8434f

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633379645432,
+  "lastUpdate": 1633388174290,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -1318,6 +1318,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "provider/testdata/data/testdata.postcard",
             "value": 583108,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "807116bfedf24c04d6bd7987431eb6b2ddc8434f",
+          "message": "Separate SkeletonPatterns into its own key (#1139)\n\n* Separate SkeletonPatterns into its own key\r\n\r\n* Fix test\r\n\r\n* Make cargo clippy happy",
+          "timestamp": "2021-10-04T15:54:54-07:00",
+          "tree_id": "de502eed1d3bc762828b7b391506edb1a59bb162",
+          "url": "https://github.com/unicode-org/icu4x/commit/807116bfedf24c04d6bd7987431eb6b2ddc8434f"
+        },
+        "date": 1633388169552,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 583709,
             "unit": "bytes",
             "biggerIsBetter": false
           }
