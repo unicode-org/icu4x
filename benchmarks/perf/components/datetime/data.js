8064bb68800249808c177f5d88f7847a2fdfc091
github-action-benchmark <github@users.noreply.github.com>
2021-10-04 23:02:08 +0000

    add Rust Benchmark (cargo) benchmark result for 807116bfedf24c04d6bd7987431eb6b2ddc8434f

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633380047988,
+  "lastUpdate": 1633388527986,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -9419,6 +9419,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 654739,
             "range": "± 68285",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "807116bfedf24c04d6bd7987431eb6b2ddc8434f",
+          "message": "Separate SkeletonPatterns into its own key (#1139)\n\n* Separate SkeletonPatterns into its own key\r\n\r\n* Fix test\r\n\r\n* Make cargo clippy happy",
+          "timestamp": "2021-10-04T15:54:54-07:00",
+          "tree_id": "de502eed1d3bc762828b7b391506edb1a59bb162",
+          "url": "https://github.com/unicode-org/icu4x/commit/807116bfedf24c04d6bd7987431eb6b2ddc8434f"
+        },
+        "date": 1633388524501,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 92659,
+            "range": "± 1070",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1421054,
+            "range": "± 1495",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 282747,
+            "range": "± 465",
             "unit": "ns/iter"
           }
         ]
