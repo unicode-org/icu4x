2e79cef5f7712d86cb81e0b728a06f5e3f721821
Iain Ireland <iireland@mozilla.com>
2021-10-07 15:41:03 -0700

    Cargo fmt + minor fixes

--- utils/codepointtrie/src/provider.rs
+++ utils/codepointtrie/src/provider.rs
@@ -2,7 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use core::{mem,ptr};
+use core::{mem, ptr};
 
 use crate::codepointtrie::{CodePointTrie, TrieValue};
 use icu_provider::yoke::*;
@@ -58,16 +58,18 @@ unsafe impl<'a, T: TrieValue> Yokeable<'a> for UnicodePropertyMapV1<'static, T> 
     }
 }
 
-impl<'a, T: TrieValue> ZeroCopyFrom<UnicodePropertyMapV1<'a, T>> for UnicodePropertyMapV1<'static,T> {
-    fn zero_copy_from<'b>(cart: &'b UnicodePropertyMapV1<'a, T>) -> UnicodePropertyMapV1<'b,T> {
+impl<'a, T: TrieValue> ZeroCopyFrom<UnicodePropertyMapV1<'a, T>>
+    for UnicodePropertyMapV1<'static, T>
+{
+    fn zero_copy_from<'b>(cart: &'b UnicodePropertyMapV1<'a, T>) -> UnicodePropertyMapV1<'b, T> {
         UnicodePropertyMapV1 {
-            codepoint_trie: CodePointTrie::<'static, T>::zero_copy_from(&cart.codepoint_trie)
+            codepoint_trie: CodePointTrie::<'static, T>::zero_copy_from(&cart.codepoint_trie),
         }
     }
 }
 
 pub struct UnicodePropertyMapV1Marker<T: TrieValue> {
-    _phantom: core::marker::PhantomData<T>
+    _phantom: core::marker::PhantomData<T>,
 }
 
 impl<'data, T: TrieValue> icu_provider::DataMarker<'data> for UnicodePropertyMapV1Marker<T> {
