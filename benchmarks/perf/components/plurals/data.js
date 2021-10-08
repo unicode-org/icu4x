ebd861875f9d3bf1900cb5defe7f22d91d0f381c
github-action-benchmark <github@users.noreply.github.com>
2021-10-08 19:11:33 +0000

    add Rust Benchmark (cargo) benchmark result for c0c1380a7cf4ab82fe0dd952c86b16606e95e247

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633715915782,
+  "lastUpdate": 1633720293342,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16085,6 +16085,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 24094,
             "range": "± 1303",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zibi@braniecki.net",
+            "name": "Zibi Braniecki",
+            "username": "zbraniecki"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c0c1380a7cf4ab82fe0dd952c86b16606e95e247",
+          "message": "Reorganize skeleton and separate runtime (#1156)\n\n* Reorganize skeleton and separate runtime\r\n\r\n* Update based on the design from Pattern serde",
+          "timestamp": "2021-10-08T12:04:06-07:00",
+          "tree_id": "93bfcd68c3680c4a92255bf18d27fa5947b11a8f",
+          "url": "https://github.com/unicode-org/icu4x/commit/c0c1380a7cf4ab82fe0dd952c86b16606e95e247"
+        },
+        "date": 1633720288516,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1170,
+            "range": "± 79",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6583,
+            "range": "± 453",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 23397,
+            "range": "± 2019",
             "unit": "ns/iter"
           }
         ]
