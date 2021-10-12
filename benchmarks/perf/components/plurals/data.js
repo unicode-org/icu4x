56a75cce6e481ad29dfd9236e1b4c3541f1770ed
github-action-benchmark <github@users.noreply.github.com>
2021-10-12 22:10:12 +0000

    add Rust Benchmark (cargo) benchmark result for 651f113cdcf85f620b56793f6b5c8b5c865a9a53

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634069646935,
+  "lastUpdate": 1634076612391,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16211,6 +16211,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 21771,
             "range": "± 1548",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "iireland@mozilla.com",
+            "name": "iainireland",
+            "username": "iainireland"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "651f113cdcf85f620b56793f6b5c8b5c865a9a53",
+          "message": "Add doc comments to icu_uniset (#1154)\n\n* Add doc comments to icu_uniset\r\n\r\n* More detailed doc comments for UnicodeSetError",
+          "timestamp": "2021-10-12T15:02:45-07:00",
+          "tree_id": "b0ce9939c014e7921227ed0a22f8cfa2496b280c",
+          "url": "https://github.com/unicode-org/icu4x/commit/651f113cdcf85f620b56793f6b5c8b5c865a9a53"
+        },
+        "date": 1634076607925,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1348,
+            "range": "± 15",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 7585,
+            "range": "± 107",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 23859,
+            "range": "± 314",
             "unit": "ns/iter"
           }
         ]
