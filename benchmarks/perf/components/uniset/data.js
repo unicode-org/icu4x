99cb7aa1d71a48cfc57b4b12ad5724f98cbf5a72
github-action-benchmark <github@users.noreply.github.com>
2021-10-12 23:51:42 +0000

    add Rust Benchmark (cargo) benchmark result for 9c8afa504fed91f346a256ab216079663208c6bb

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634076553080,
+  "lastUpdate": 1634082702746,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12629,6 +12629,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 28594854,
             "range": "± 1339956",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "m_kato@ga2.so-net.ne.jp",
+            "name": "Makoto Kato",
+            "username": "makotokato"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9c8afa504fed91f346a256ab216079663208c6bb",
+          "message": "Move Latin1Indices and Utf16Indices to its own file (#1165)\n\n* Move *Indices to own file to use other segmenter.\r\n\r\n* Fix per review comment.\r\n\r\n* Add comment for surrogate pair.",
+          "timestamp": "2021-10-13T08:45:17+09:00",
+          "tree_id": "70e3d274a5ab85bf9be4d4e9e47e0c86ee7285c7",
+          "url": "https://github.com/unicode-org/icu4x/commit/9c8afa504fed91f346a256ab216079663208c6bb"
+        },
+        "date": 1634082698539,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 40030419,
+            "range": "± 1827384",
             "unit": "ns/iter"
           }
         ]
