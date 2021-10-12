e3541305cbea66965d0fcde7298519c088450d90
github-action-benchmark <github@users.noreply.github.com>
2021-10-12 20:13:30 +0000

    add Rust Benchmark (cargo) benchmark result for a3ef861aa8531297b04b93104b8e774e0ed0d1f5

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633735789437,
+  "lastUpdate": 1634069610690,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -14465,6 +14465,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5036,
             "range": "± 385",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a3ef861aa8531297b04b93104b8e774e0ed0d1f5",
+          "message": "Add FromIterator impl for ZeroVec (#1169)\n\n* Add FromIterator impl for ZeroVec\r\n\r\n* fmt",
+          "timestamp": "2021-10-12T13:06:11-07:00",
+          "tree_id": "4c5d61e4adde2ac1da042e1e25cd3cba1229a627",
+          "url": "https://github.com/unicode-org/icu4x/commit/a3ef861aa8531297b04b93104b8e774e0ed0d1f5"
+        },
+        "date": 1634069605445,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2999,
+            "range": "± 82",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5354,
+            "range": "± 251",
             "unit": "ns/iter"
           }
         ]
