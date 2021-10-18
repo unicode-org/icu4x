cea4d4947132f25ce51dd3ed9bc335219dda844d
github-action-benchmark <github@users.noreply.github.com>
2021-10-18 21:51:57 +0000

    add Rust Benchmark (cargo) benchmark result for a430f66c32040c3ac1e055620ee3f5c6515ab1e8

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634309412961,
+  "lastUpdate": 1634593917003,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -11669,6 +11669,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 129180,
             "range": "± 9711",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "iireland@mozilla.com",
+            "name": "iainireland",
+            "username": "iainireland"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a430f66c32040c3ac1e055620ee3f5c6515ab1e8",
+          "message": "Adds UnicodePropertyMapV1 data struct for enumerated properties (#1161)\n\n* Rename TrieTypeEnum to TrieType\r\n\r\nTrieType no longer exists, so we don't need an awkward name for TrieTypeEnum.\r\n\r\n* Implement Yokeable/ZeroCopyFrom for CodePointTrie and data struct\r\n\r\n* Cargo fmt + minor fixes\r\n\r\n* Rebase on yoke-generics\r\n\r\n* Add doc comments\r\n\r\n* Address feedback\r\nFunny how everyone is talking about C9 having an easy route and not GenG lmao.\r\n\r\n* Add additional derives\r\n\r\n* Update comment on DATA_GET_ERROR_VALUE\r\n\r\nCo-authored-by: Shane F. Carr <shane@unicode.org>\r\n\r\n* Cargo fmt\r\n\r\nCo-authored-by: Shane F. Carr <shane@unicode.org>",
+          "timestamp": "2021-10-18T14:43:18-07:00",
+          "tree_id": "b35ea471e60581d01924e11d6dd7d1f10d642fcc",
+          "url": "https://github.com/unicode-org/icu4x/commit/a430f66c32040c3ac1e055620ee3f5c6515ab1e8"
+        },
+        "date": 1634593913013,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 99515,
+            "range": "± 3243",
             "unit": "ns/iter"
           }
         ]
