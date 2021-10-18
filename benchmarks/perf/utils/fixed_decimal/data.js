b1f7b1f291582b6b295a78a7ff50c4f5be8143ff
github-action-benchmark <github@users.noreply.github.com>
2021-10-18 22:22:44 +0000

    add Rust Benchmark (cargo) benchmark result for b09e5ea677f74c241dad129a305a05bc2dc3eead

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634593917003,
+  "lastUpdate": 1634595764778,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -11699,6 +11699,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 99515,
             "range": "± 3243",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b09e5ea677f74c241dad129a305a05bc2dc3eead",
+          "message": "Add testing section to CONTRIBUTING.md (#1164)\n\n* Add ci-job-clippy\n\n* Add testing section to CONTRIBUTING.md\n\n* node 14\n\n* review fix\n\n* Update CONTRIBUTING.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update CONTRIBUTING.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update CONTRIBUTING.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update Makefile.toml\n\nCo-authored-by: Elango <elango@unicode.org>\n\n* Update CONTRIBUTING.md\n\nCo-authored-by: Elango <elango@unicode.org>\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\nCo-authored-by: Elango <elango@unicode.org>",
+          "timestamp": "2021-10-18T15:16:26-07:00",
+          "tree_id": "9932d4ce0b08bd9417f74d638c04eec088480e5f",
+          "url": "https://github.com/unicode-org/icu4x/commit/b09e5ea677f74c241dad129a305a05bc2dc3eead"
+        },
+        "date": 1634595760257,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 118746,
+            "range": "± 9490",
             "unit": "ns/iter"
           }
         ]
