1ee3946a41f803652fcf6c3dba3114acd1f82f29
Shane Carr <shane@unicode.org>
2021-10-18 14:50:19 -0700

    Add small comment to ZeroVec::from_iter

--- utils/zerovec/src/zerovec/mod.rs
+++ utils/zerovec/src/zerovec/mod.rs
@@ -536,6 +536,7 @@ where
 }
 
 impl<T: AsULE> FromIterator<T> for ZeroVec<'_, T> {
+    /// Creates a [`ZeroVec::Owned`] from an iterator of values.
     fn from_iter<I>(iter: I) -> Self
     where
         I: IntoIterator<Item = T>,
@@ -543,6 +544,7 @@ impl<T: AsULE> FromIterator<T> for ZeroVec<'_, T> {
         ZeroVec::Owned(iter.into_iter().map(|t| t.as_unaligned()).collect())
     }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
