fd8a73d854985abe6d944fb89714b65b20c51fe6
github-action-benchmark <github@users.noreply.github.com>
2021-10-19 04:11:43 +0000

    add Rust Benchmark (cargo) benchmark result for 8a016a0398676d1d7316cab83667a92983f903f7

--- benchmarks/perf/components/uniset/data.js
+++ benchmarks/perf/components/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634597494262,
+  "lastUpdate": 1634616703495,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12779,6 +12779,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34723182,
             "range": "± 1914643",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "m_kato@ga2.so-net.ne.jp",
+            "name": "Makoto Kato",
+            "username": "makotokato"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8a016a0398676d1d7316cab83667a92983f903f7",
+          "message": "Cleanup linebreak segmenter specification test to use other segmenters (#1168)\n\n* Cleanup specification test for linebreak since other specification tests will use same test format.\r\n\r\nUAX#29 test data uses same format for UAX#14. So I would like to clean up it\r\nto use other tests for UAX#29.\r\n\r\n* Fix per review comment.",
+          "timestamp": "2021-10-19T13:05:29+09:00",
+          "tree_id": "58ea476df51df851563e4c8d874194ddd6ccc018",
+          "url": "https://github.com/unicode-org/icu4x/commit/8a016a0398676d1d7316cab83667a92983f903f7"
+        },
+        "date": 1634616698739,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 34441150,
+            "range": "± 1498142",
             "unit": "ns/iter"
           }
         ]
