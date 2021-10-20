b7da9b3839da9131564ad5ec8cefa1a54baa4ea0
github-action-benchmark <github@users.noreply.github.com>
2021-10-20 20:04:06 +0000

    add Rust Benchmark (cargo) benchmark result for 81aaa42a07cc1a9342dbdeb8014012f96e542acc

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634739912491,
+  "lastUpdate": 1634760246217,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16673,6 +16673,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 20545,
             "range": "± 244",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "81aaa42a07cc1a9342dbdeb8014012f96e542acc",
+          "message": "Make ULE types Copy, add PairULE (#1193)\n\n* Add PairULE\r\n\r\n* Make ULE Copy\r\n\r\n* take AsULE values by-copy",
+          "timestamp": "2021-10-20T12:57:06-07:00",
+          "tree_id": "7b0bc17f0356b6545d9995af465d04c7d8ec89d2",
+          "url": "https://github.com/unicode-org/icu4x/commit/81aaa42a07cc1a9342dbdeb8014012f96e542acc"
+        },
+        "date": 1634760241137,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1159,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6565,
+            "range": "± 18",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 20372,
+            "range": "± 66",
             "unit": "ns/iter"
           }
         ]
