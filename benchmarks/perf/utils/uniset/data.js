007a70caa8d917a9d210a58b9328e3342f85d190
github-action-benchmark <github@users.noreply.github.com>
2021-10-25 19:06:15 +0000

    add Rust Benchmark (cargo) benchmark result for 3920ad263ca5d63d1cc206bde3266cf5e013318c

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635188240748,
+  "lastUpdate": 1635188775480,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -13169,6 +13169,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34007014,
             "range": "± 447222",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3920ad263ca5d63d1cc206bde3266cf5e013318c",
+          "message": "Add more methods to EncodeAsVarULE (#1199)",
+          "timestamp": "2021-10-25T11:59:29-07:00",
+          "tree_id": "441529dd6dc23bf43b0ca4923d22acf95448bbbd",
+          "url": "https://github.com/unicode-org/icu4x/commit/3920ad263ca5d63d1cc206bde3266cf5e013318c"
+        },
+        "date": 1635188771087,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 27523552,
+            "range": "± 1318216",
             "unit": "ns/iter"
           }
         ]
