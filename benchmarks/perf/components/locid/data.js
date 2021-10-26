80a66d7ec711dad26e435fac0d4859eb38911761
github-action-benchmark <github@users.noreply.github.com>
2021-10-26 23:46:03 +0000

    add Rust Benchmark (cargo) benchmark result for 131b84548c7a7a204c91dbfaa8d57b647199c4d5

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635289525467,
+  "lastUpdate": 1635291963856,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15293,6 +15293,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5327,
             "range": "± 235",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "m_kato@ga2.so-net.ne.jp",
+            "name": "Makoto Kato",
+            "username": "makotokato"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "131b84548c7a7a204c91dbfaa8d57b647199c4d5",
+          "message": "Implement DataMarker to use DataProvider for LstmData. (#1142)\n\n* Implement DataMarker to use DataProvider for LstmData.\r\n\r\n* Fix cargo wasm-build-release\r\n\r\n* Fix per review comment.\r\n\r\n* Use icu_provider::data_struct.\r\n\r\n* Fix fmt error\r\n\r\n* Use DataPayload for Lstm struct.\r\n\r\n* Fix fmt.\r\n\r\n* Use DataPayload methods instead of receiver.\r\n\r\n* Don't panic key isn't found.",
+          "timestamp": "2021-10-27T08:40:02+09:00",
+          "tree_id": "07787927fab2faddd9d72fbf0b7e6fbc277bb2b0",
+          "url": "https://github.com/unicode-org/icu4x/commit/131b84548c7a7a204c91dbfaa8d57b647199c4d5"
+        },
+        "date": 1635291959789,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2507,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4181,
+            "range": "± 28",
             "unit": "ns/iter"
           }
         ]
