d9d378e2d946788d84e82e9d5276a3b3418d4655
github-action-benchmark <github@users.noreply.github.com>
2021-10-28 16:05:48 +0000

    add Rust Benchmark (cargo) benchmark result for 26d779ac767f613aace34a2031d9d474b11c913a

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635382577788,
+  "lastUpdate": 1635437148449,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17303,6 +17303,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 19422,
             "range": "± 1236",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "26d779ac767f613aace34a2031d9d474b11c913a",
+          "message": "Fix crates.io badges no longer loading (#1036) (#1219)\n\n* Fix crates.io badges no longer loading (#1036)\r\n\r\n* Don't try to fix the diplomat link, just the badge",
+          "timestamp": "2021-10-28T08:57:56-07:00",
+          "tree_id": "95ec2ad4fde73e130f5600e70bac200e43788cac",
+          "url": "https://github.com/unicode-org/icu4x/commit/26d779ac767f613aace34a2031d9d474b11c913a"
+        },
+        "date": 1635437143484,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1300,
+            "range": "± 58",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 7637,
+            "range": "± 483",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 24697,
+            "range": "± 914",
             "unit": "ns/iter"
           }
         ]
