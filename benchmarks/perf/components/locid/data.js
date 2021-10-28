339377c0b45f084370c3868ef55bfb5a0d9365d8
github-action-benchmark <github@users.noreply.github.com>
2021-10-28 16:05:18 +0000

    add Rust Benchmark (cargo) benchmark result for 26d779ac767f613aace34a2031d9d474b11c913a

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635382573264,
+  "lastUpdate": 1635437118814,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15437,6 +15437,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5024,
             "range": "± 271",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nmihai_2000@yahoo.com",
+            "name": "Mihai Nita",
+            "username": "mihnita"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "26d779ac767f613aace34a2031d9d474b11c913a",
+          "message": "Fix crates.io badges no longer loading (#1036) (#1219)\n\n* Fix crates.io badges no longer loading (#1036)\r\n\r\n* Don't try to fix the diplomat link, just the badge",
+          "timestamp": "2021-10-28T08:57:56-07:00",
+          "tree_id": "95ec2ad4fde73e130f5600e70bac200e43788cac",
+          "url": "https://github.com/unicode-org/icu4x/commit/26d779ac767f613aace34a2031d9d474b11c913a"
+        },
+        "date": 1635437112900,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2858,
+            "range": "± 138",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4868,
+            "range": "± 273",
             "unit": "ns/iter"
           }
         ]
