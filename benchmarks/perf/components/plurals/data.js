65b769eb2cc26cc72b0bc43bd1687a3c5dd04d28
github-action-benchmark <github@users.noreply.github.com>
2021-10-28 00:56:17 +0000

    add Rust Benchmark (cargo) benchmark result for 3a63e2d49c1d8c7700289bd60bfe4145a559af41

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635372338653,
+  "lastUpdate": 1635382577788,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17261,6 +17261,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 26048,
             "range": "± 2414",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3a63e2d49c1d8c7700289bd60bfe4145a559af41",
+          "message": "Make VarZeroVecBorrowed/VarZeroVecOwned public (#1223)\n\n* SliceComponents -> VZVBorrowed\n\n* get_components() -> as_borrowed()\n\n* Remove duplicate method\n\n* make compile again\n\n* components -> borrowed\n\n* Add docs, make VZVBorrowed public\n\n* more docs\n\n* more docs etc\n\n* Make VZVOwned public\n\n* Fixup doc warnings and fmt\n\n* Make VZV a publicly inspectable enum of VZVO/VZVB\n\n* clippy\n\n* move docs\n\n* Update utils/zerovec/src/varzerovec/borrowed.rs\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update utils/zerovec/src/varzerovec/mod.rs\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update utils/zerovec/src/varzerovec/mod.rs\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Add examples to docs; add VZVO::push() and VZV::new()\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>",
+          "timestamp": "2021-10-27T17:48:58-07:00",
+          "tree_id": "b8ae9b8419d77c1ad679cdb53612405634143950",
+          "url": "https://github.com/unicode-org/icu4x/commit/3a63e2d49c1d8c7700289bd60bfe4145a559af41"
+        },
+        "date": 1635382572989,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1044,
+            "range": "± 86",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6181,
+            "range": "± 307",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 19422,
+            "range": "± 1236",
             "unit": "ns/iter"
           }
         ]
