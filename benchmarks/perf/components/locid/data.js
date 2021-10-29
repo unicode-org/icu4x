549b3867d2e77d5be9fe7b0eed6f1533f97c9a40
github-action-benchmark <github@users.noreply.github.com>
2021-10-29 20:47:41 +0000

    add Rust Benchmark (cargo) benchmark result for 0df5de32e80dd283920d351cc1889d7d0b8c65f2

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635533027413,
+  "lastUpdate": 1635540461305,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15761,6 +15761,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4814,
             "range": "± 209",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0df5de32e80dd283920d351cc1889d7d0b8c65f2",
+          "message": "Update to CLDR 40 (#1216)\n\n- Removes CldrLangID in favor of vanilla LanguageIdentifier",
+          "timestamp": "2021-10-29T13:24:28-07:00",
+          "tree_id": "8693b87d0fca66745b7cb13906c8b56daee4ceeb",
+          "url": "https://github.com/unicode-org/icu4x/commit/0df5de32e80dd283920d351cc1889d7d0b8c65f2"
+        },
+        "date": 1635540456149,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2974,
+            "range": "± 36",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5045,
+            "range": "± 22",
             "unit": "ns/iter"
           }
         ]
