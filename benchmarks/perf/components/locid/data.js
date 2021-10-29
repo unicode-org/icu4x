d9b63c313dd02790f8623b5938f8e940ae20a523
github-action-benchmark <github@users.noreply.github.com>
2021-10-29 18:43:47 +0000

    add Rust Benchmark (cargo) benchmark result for d1d29c4aa848a5bcf9a81ef1e59d7f7034586552

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635530028890,
+  "lastUpdate": 1635533027413,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15725,6 +15725,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4142,
             "range": "± 3",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d1d29c4aa848a5bcf9a81ef1e59d7f7034586552",
+          "message": "Skip skeletons with -alt-variant when processing CLDR JSON (#1224)",
+          "timestamp": "2021-10-29T11:36:26-07:00",
+          "tree_id": "fafab1b15d35fed8839ae3ac416bf808bc48e148",
+          "url": "https://github.com/unicode-org/icu4x/commit/d1d29c4aa848a5bcf9a81ef1e59d7f7034586552"
+        },
+        "date": 1635533021949,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2699,
+            "range": "± 142",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4814,
+            "range": "± 209",
             "unit": "ns/iter"
           }
         ]
