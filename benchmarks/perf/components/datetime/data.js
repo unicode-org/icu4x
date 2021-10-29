d800350e183b2b0915fb69f4babc0ae88981e3cb
github-action-benchmark <github@users.noreply.github.com>
2021-10-29 17:54:56 +0000

    add Rust Benchmark (cargo) benchmark result for 2313561b1ad423cf30574815f8e285f91f124ffd

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635472127679,
+  "lastUpdate": 1635530096230,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -11225,6 +11225,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 288917,
             "range": "± 2813",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2313561b1ad423cf30574815f8e285f91f124ffd",
+          "message": "Connect properties provider to the icu4x_datagen exporter tool (#1204)",
+          "timestamp": "2021-10-29T10:47:08-07:00",
+          "tree_id": "136a34b69ef8b1d1ccea8621faf9a511d1788736",
+          "url": "https://github.com/unicode-org/icu4x/commit/2313561b1ad423cf30574815f8e285f91f124ffd"
+        },
+        "date": 1635530090437,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 128929,
+            "range": "± 6402",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1471415,
+            "range": "± 75027",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 373298,
+            "range": "± 22033",
             "unit": "ns/iter"
           }
         ]
