2e3639e26eab1d05e0ab5a32a205aef03cbd04fb
github-action-benchmark <github@users.noreply.github.com>
2021-11-01 22:18:04 +0000

    add Rust Benchmark (cargo) benchmark result for 9892697f554b36238613348b323aead3cdd75be1

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635796514485,
+  "lastUpdate": 1635805084456,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -13739,6 +13739,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 45794496,
             "range": "± 2256834",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9892697f554b36238613348b323aead3cdd75be1",
+          "message": "Updating CHANGELOG for ICU4X 0.4",
+          "timestamp": "2021-11-01T15:12:03-07:00",
+          "tree_id": "bde858ffe0098794cea6f83b4ac95847625df9ce",
+          "url": "https://github.com/unicode-org/icu4x/commit/9892697f554b36238613348b323aead3cdd75be1"
+        },
+        "date": 1635805079113,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 40859083,
+            "range": "± 1438488",
             "unit": "ns/iter"
           }
         ]
