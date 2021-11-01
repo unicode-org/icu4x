488deae01abbc6b1ed078c8b20cf77963ab730c9
github-action-benchmark <github@users.noreply.github.com>
2021-11-01 23:33:02 +0000

    add Rust Benchmark (cargo) benchmark result for e8b38536c46afca5d9e04dbdfb07b51c9384fccf

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635807269293,
+  "lastUpdate": 1635809582759,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -11561,6 +11561,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 284286,
             "range": "± 15379",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e8b38536c46afca5d9e04dbdfb07b51c9384fccf",
+          "message": "Bump yoke-derive in preparation for release (#1253)\n\n* Bump yoke-derive to 0.1.2\r\n\r\n* Bump yoke to 0.3.1\r\n\r\n* Update users of yoke and yoke-derive",
+          "timestamp": "2021-11-01T16:25:02-07:00",
+          "tree_id": "30a4caba07bf220bd63a0fa5d0b12c51ea0c5038",
+          "url": "https://github.com/unicode-org/icu4x/commit/e8b38536c46afca5d9e04dbdfb07b51c9384fccf"
+        },
+        "date": 1635809576881,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 118434,
+            "range": "± 7590",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1487584,
+            "range": "± 36071",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 365475,
+            "range": "± 18101",
             "unit": "ns/iter"
           }
         ]
