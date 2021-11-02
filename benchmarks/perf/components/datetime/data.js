f046cd0b00a156b81328ca844fd9a62578f247d3
github-action-benchmark <github@users.noreply.github.com>
2021-11-02 17:10:22 +0000

    add Rust Benchmark (cargo) benchmark result for 0c855b106c6e8b8a66142defc6102885ccc8c7c5

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635813497541,
+  "lastUpdate": 1635873022508,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -11687,6 +11687,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 362460,
             "range": "± 16489",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aethanyc@gmail.com",
+            "name": "Ting-Yu Lin",
+            "username": "aethanyc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0c855b106c6e8b8a66142defc6102885ccc8c7c5",
+          "message": "Implement Grapheme_Cluster_Break, Word_Break, and Sentence_Break Unicode properties (#1233)\n\nThe obsolete enum values in GraphemeClusterBreak and WordBreak are added\r\nto retain the compatibility with ICU.\r\n\r\nThe TOML file was obtained from Azure artifact archive built on\r\nunicode-org/icu, commit 2921a81ee4c67459ff455e31c599e7d7a09086ab titled\r\n\"ICU-21811 TZ update 2021a (2021e)\" on maint/maint-70 branch. This\r\ncommit imports TrieType::Small flavor of the uprops files.\r\n\r\nThe json and postcard files are generated via `cargo make testdata`.",
+          "timestamp": "2021-11-02T10:01:52-07:00",
+          "tree_id": "c6fef0184e27cf1c3e920786d6034e64f16479c8",
+          "url": "https://github.com/unicode-org/icu4x/commit/0c855b106c6e8b8a66142defc6102885ccc8c7c5"
+        },
+        "date": 1635873016615,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 116133,
+            "range": "± 1272",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1371558,
+            "range": "± 13558",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 337054,
+            "range": "± 1529",
             "unit": "ns/iter"
           }
         ]
