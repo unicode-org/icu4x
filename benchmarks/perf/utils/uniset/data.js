1e0f2a9c7be85901dd99fe4866536e61543a029b
github-action-benchmark <github@users.noreply.github.com>
2021-11-10 21:21:49 +0000

    add Rust Benchmark (cargo) benchmark result for c6857ec1508b0fd74e6d15e20a76220bad211067

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1636578857548,
+  "lastUpdate": 1636579309756,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -14399,6 +14399,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 35821825,
             "range": "± 2000734",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6857ec1508b0fd74e6d15e20a76220bad211067",
+          "message": "Burn DataMarker::Cart down with fire (#1279)\n\n* Add Yoke::replace_cart()\r\n\r\n* Use ErasedCart\r\n\r\n* Replace M::Cart with ErasedCart in RcStruct\r\n\r\n* Remove Cart type from DataMarker\r\n\r\n* Remove 'data from DataMarker",
+          "timestamp": "2021-11-10T13:14:00-08:00",
+          "tree_id": "6e7c0f3da973b0e47ec23491c260e3c359071dc2",
+          "url": "https://github.com/unicode-org/icu4x/commit/c6857ec1508b0fd74e6d15e20a76220bad211067"
+        },
+        "date": 1636579303292,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 38062347,
+            "range": "± 3164525",
             "unit": "ns/iter"
           }
         ]
