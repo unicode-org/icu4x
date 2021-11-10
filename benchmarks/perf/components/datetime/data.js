825093a2d4788e1ee1151e02e679660c5d9ae8dd
github-action-benchmark <github@users.noreply.github.com>
2021-11-10 19:40:55 +0000

    add Rust Benchmark (cargo) benchmark result for 052c398ed85fe63c64b746d2e4c58167d47564f5

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1636572720215,
+  "lastUpdate": 1636573255684,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12275,6 +12275,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 390652,
             "range": "± 20341",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "052c398ed85fe63c64b746d2e4c58167d47564f5",
+          "message": "Unify handling of TOML in uprops provider (#1254)",
+          "timestamp": "2021-11-10T11:32:56-08:00",
+          "tree_id": "7b37812cc03345076bcf62d494a78cf7478d1ef3",
+          "url": "https://github.com/unicode-org/icu4x/commit/052c398ed85fe63c64b746d2e4c58167d47564f5"
+        },
+        "date": 1636573249856,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 106282,
+            "range": "± 6646",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1241113,
+            "range": "± 98247",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 313519,
+            "range": "± 26035",
             "unit": "ns/iter"
           }
         ]
