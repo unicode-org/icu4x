a73eb9272a6637ad841f26d272825f2aca766826
github-action-benchmark <github@users.noreply.github.com>
2021-11-10 21:22:09 +0000

    add Rust Benchmark (cargo) benchmark result for c6857ec1508b0fd74e6d15e20a76220bad211067

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1636578878828,
+  "lastUpdate": 1636579328969,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16661,6 +16661,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4757,
             "range": "± 342",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6857ec1508b0fd74e6d15e20a76220bad211067",
+          "message": "Burn DataMarker::Cart down with fire (#1279)\n\n* Add Yoke::replace_cart()\r\n\r\n* Use ErasedCart\r\n\r\n* Replace M::Cart with ErasedCart in RcStruct\r\n\r\n* Remove Cart type from DataMarker\r\n\r\n* Remove 'data from DataMarker",
+          "timestamp": "2021-11-10T13:14:00-08:00",
+          "tree_id": "6e7c0f3da973b0e47ec23491c260e3c359071dc2",
+          "url": "https://github.com/unicode-org/icu4x/commit/c6857ec1508b0fd74e6d15e20a76220bad211067"
+        },
+        "date": 1636579321357,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2776,
+            "range": "± 115",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5049,
+            "range": "± 196",
             "unit": "ns/iter"
           }
         ]
