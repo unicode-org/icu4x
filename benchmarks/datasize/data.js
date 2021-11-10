94a7e860aa8f9a761345424d9beaf963f3b6a32e
github-action-benchmark <github@users.noreply.github.com>
2021-11-10 22:32:05 +0000

    add Benchmark (ndjson) benchmark result for 92abc789dd46000a3dcfd3d5b46a592f76e834da

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1636581720544,
+  "lastUpdate": 1636583525214,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -3473,6 +3473,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/1ed57813d760147f20cea0084de736790561b730"
         },
         "date": 1636581714121,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 846762,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "92abc789dd46000a3dcfd3d5b46a592f76e834da",
+          "message": "Update tutorial to include uprops data (#1286)",
+          "timestamp": "2021-11-10T14:30:34-08:00",
+          "tree_id": "b376f260011fd1c5a660f2df1511b68d6b11fe81",
+          "url": "https://github.com/unicode-org/icu4x/commit/92abc789dd46000a3dcfd3d5b46a592f76e834da"
+        },
+        "date": 1636583518917,
         "tool": "ndjson",
         "benches": [
           {
