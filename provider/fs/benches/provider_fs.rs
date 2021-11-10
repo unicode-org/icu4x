af0a5bf58982db9c09310a9ea2c357c5849e4ae1
Zibi Braniecki <zibi@braniecki.net>
2021-11-09 19:00:56 -0600

    Fix provider benchmarks

--- provider/fs/benches/provider_fs.rs
+++ provider/fs/benches/provider_fs.rs
@@ -50,7 +50,7 @@ fn json_bench(c: &mut Criterion) {
 
     c.bench_function("json/generic", |b| {
         b.iter(|| {
-            let _: DataPayload<PluralRuleStringsV1Marker> = black_box(&provider)
+            let _: DataPayload<PluralRulesV1Marker> = black_box(&provider)
                 .load_payload(&DataRequest {
                     resource_path: ResourcePath {
                         key: key::CARDINAL_V1,
@@ -68,7 +68,7 @@ fn json_bench(c: &mut Criterion) {
 
     c.bench_function("json/erased_serde", |b| {
         b.iter(|| {
-            let _: DataPayload<PluralRuleStringsV1Marker> =
+            let _: DataPayload<PluralRulesV1Marker> =
                 black_box(&provider as &dyn SerdeDeDataProvider)
                     .load_payload(&DataRequest {
                         resource_path: ResourcePath {
@@ -93,7 +93,7 @@ fn bincode_bench(c: &mut Criterion) {
 
     c.bench_function("bincode/generic", |b| {
         b.iter(|| {
-            let _: DataPayload<PluralRuleStringsV1Marker> = black_box(&provider)
+            let _: DataPayload<PluralRulesV1Marker> = black_box(&provider)
                 .load_payload(&DataRequest {
                     resource_path: ResourcePath {
                         key: key::CARDINAL_V1,
@@ -111,7 +111,7 @@ fn bincode_bench(c: &mut Criterion) {
 
     c.bench_function("bincode/erased_serde", |b| {
         b.iter(|| {
-            let _: DataPayload<PluralRuleStringsV1Marker> =
+            let _: DataPayload<PluralRulesV1Marker> =
                 black_box(&provider as &dyn SerdeDeDataProvider)
                     .load_payload(&DataRequest {
                         resource_path: ResourcePath {
