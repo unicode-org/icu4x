867c9ab9055eb314046aecf82240fa0a7c6c94c2
github-action-benchmark <github@users.noreply.github.com>
2021-11-13 01:42:55 +0000

    add Rust Benchmark (cargo) benchmark result for 0845aa18ff40b5f7a0dfad8b7d63ec86b55d3d59

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1636766604831,
+  "lastUpdate": 1636767775097,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19067,6 +19067,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 24529,
             "range": "± 793",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0845aa18ff40b5f7a0dfad8b7d63ec86b55d3d59",
+          "message": "Add CodePointTrie and ZeroVec conversion methods (#1291)",
+          "timestamp": "2021-11-12T17:35:22-08:00",
+          "tree_id": "27420c27afdad4d6295266a3afa6acc186ed3c4c",
+          "url": "https://github.com/unicode-org/icu4x/commit/0845aa18ff40b5f7a0dfad8b7d63ec86b55d3d59"
+        },
+        "date": 1636767768822,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1206,
+            "range": "± 54",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 7827,
+            "range": "± 462",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 24158,
+            "range": "± 1047",
             "unit": "ns/iter"
           }
         ]
