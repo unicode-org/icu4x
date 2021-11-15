7e73204fd6f6b8c6cd4e29c24a208fc03d2b86b8
github-action-benchmark <github@users.noreply.github.com>
2021-11-15 23:34:42 +0000

    add Rust Benchmark (cargo) benchmark result for cee2b5fa9dc7649e78b29e5e2d67e3e7c33764e3

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637012331502,
+  "lastUpdate": 1637019282328,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -12947,6 +12947,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 383262,
             "range": "± 28349",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cee2b5fa9dc7649e78b29e5e2d67e3e7c33764e3",
+          "message": "Remove RcStruct (#1299)\n\n* Use owned data structs in SerdeSeDataStruct\r\n\r\n* Move ErasedCart to Yoke\r\n\r\n* Remove DataPayload::from_partial_owned()\r\n\r\n* Remove RcStruct",
+          "timestamp": "2021-11-15T15:26:37-08:00",
+          "tree_id": "1c45872111bf72096ebed3c3a6723b48db0f6841",
+          "url": "https://github.com/unicode-org/icu4x/commit/cee2b5fa9dc7649e78b29e5e2d67e3e7c33764e3"
+        },
+        "date": 1637019275289,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 115938,
+            "range": "± 5732",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1320066,
+            "range": "± 73670",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 332846,
+            "range": "± 24923",
             "unit": "ns/iter"
           }
         ]
