3a0b59ac736768ef08536ea4b464c298a437bb74
Manish Goregaokar <manishsmail@gmail.com>
2021-11-15 14:55:43 -0800

    Fix test

--- utils/yoke/src/yoke.rs
+++ utils/yoke/src/yoke.rs
@@ -1,6 +1,8 @@
+// This file is part of ICU4X. For terms of use, please see the file
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+#[cfg(feature = "alloc")]
 use crate::erased::{ErasedBoxCart, ErasedRcCart};
 use crate::trait_hack::YokeTraitHack;
 use crate::IsCovariant;
