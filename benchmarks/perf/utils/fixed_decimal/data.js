141e0f04a7b7b088044a0210f7eb031b7173bed4
github-action-benchmark <github@users.noreply.github.com>
2021-11-15 23:33:25 +0000

    add Rust Benchmark (cargo) benchmark result for cee2b5fa9dc7649e78b29e5e2d67e3e7c33764e3

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637012258319,
+  "lastUpdate": 1637019205055,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -13799,6 +13799,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 137303,
             "range": "± 2962",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cee2b5fa9dc7649e78b29e5e2d67e3e7c33764e3",
+          "message": "Remove RcStruct (#1299)\n\n* Use owned data structs in SerdeSeDataStruct\r\n\r\n* Move ErasedCart to Yoke\r\n\r\n* Remove DataPayload::from_partial_owned()\r\n\r\n* Remove RcStruct",
+          "timestamp": "2021-11-15T15:26:37-08:00",
+          "tree_id": "1c45872111bf72096ebed3c3a6723b48db0f6841",
+          "url": "https://github.com/unicode-org/icu4x/commit/cee2b5fa9dc7649e78b29e5e2d67e3e7c33764e3"
+        },
+        "date": 1637019198077,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 124892,
+            "range": "± 10442",
             "unit": "ns/iter"
           }
         ]
