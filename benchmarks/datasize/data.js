c8f3cbcc3e715a9e0ac97d166c272015c76e342d
github-action-benchmark <github@users.noreply.github.com>
2021-11-17 21:26:39 +0000

    add Benchmark (ndjson) benchmark result for 1c296f0cce90c50742d4f820dfa298b06d04f0c3

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637179212857,
+  "lastUpdate": 1637184399760,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -3923,6 +3923,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/a0a65962002a56f610a9f171f0aee6ed0caeead7"
         },
         "date": 1637179203386,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 866778,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1c296f0cce90c50742d4f820dfa298b06d04f0c3",
+          "message": "Add Buddhist calendar calculations (#1305)\n\n* Add Buddhist calendar calculations\n\n* move into const\n\n* Remove intermediate layer of inner date",
+          "timestamp": "2021-11-17T13:25:18-08:00",
+          "tree_id": "8ea32a66097faf4ee3362c136f2092facffc3e8c",
+          "url": "https://github.com/unicode-org/icu4x/commit/1c296f0cce90c50742d4f820dfa298b06d04f0c3"
+        },
+        "date": 1637184393955,
         "tool": "ndjson",
         "benches": [
           {
