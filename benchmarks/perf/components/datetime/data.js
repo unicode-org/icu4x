3ecd7bed46aacc1b7d230f4eb762c338cdaf7452
github-action-benchmark <github@users.noreply.github.com>
2021-11-17 21:33:05 +0000

    add Rust Benchmark (cargo) benchmark result for 1c296f0cce90c50742d4f820dfa298b06d04f0c3

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637179580022,
+  "lastUpdate": 1637184785220,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -13073,6 +13073,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 347471,
             "range": "± 13583",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1c296f0cce90c50742d4f820dfa298b06d04f0c3",
+          "message": "Add Buddhist calendar calculations (#1305)\n\n* Add Buddhist calendar calculations\n\n* move into const\n\n* Remove intermediate layer of inner date",
+          "timestamp": "2021-11-17T13:25:18-08:00",
+          "tree_id": "8ea32a66097faf4ee3362c136f2092facffc3e8c",
+          "url": "https://github.com/unicode-org/icu4x/commit/1c296f0cce90c50742d4f820dfa298b06d04f0c3"
+        },
+        "date": 1637184778860,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 106731,
+            "range": "± 3934",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1248580,
+            "range": "± 32103",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 318284,
+            "range": "± 15224",
             "unit": "ns/iter"
           }
         ]
