4612629296f2ca75d6e742c941b766ebe6efd5a9
github-action-benchmark <github@users.noreply.github.com>
2021-11-17 22:17:36 +0000

    add Rust Benchmark (cargo) benchmark result for f2a5bf5e9dcad52e072e23e3770d5c7decf67a64

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637184785220,
+  "lastUpdate": 1637187456222,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -13115,6 +13115,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 318284,
             "range": "± 15224",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f2a5bf5e9dcad52e072e23e3770d5c7decf67a64",
+          "message": "Homogenize yoke generic impls to always work with Yokeable, add OwnedYokeable (#1302)",
+          "timestamp": "2021-11-17T14:09:09-08:00",
+          "tree_id": "ba9ebd58c27b92d3b49c79602137cfd4378f1303",
+          "url": "https://github.com/unicode-org/icu4x/commit/f2a5bf5e9dcad52e072e23e3770d5c7decf67a64"
+        },
+        "date": 1637187448568,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 122379,
+            "range": "± 7601",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1427322,
+            "range": "± 105304",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 371966,
+            "range": "± 32153",
             "unit": "ns/iter"
           }
         ]
