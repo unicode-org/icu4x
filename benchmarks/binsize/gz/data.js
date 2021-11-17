5a9cd500a5bf3a7b513100d34c9fd1f7d5c5b8b6
github-action-benchmark <github@users.noreply.github.com>
2021-11-17 02:34:32 +0000

    add Benchmark (ndjson) benchmark result for 608fbd45586a8f392421b30087c6cd1c2cf902ca

--- benchmarks/binsize/gz/data.js
+++ benchmarks/binsize/gz/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637019106042,
+  "lastUpdate": 1637116472152,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -21046,6 +21046,144 @@ window.BENCHMARK_DATA = {
           {
             "name": "simple+opt.wasm.gz",
             "value": 189963,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "permyriad+opt.wasm.gz",
+            "value": 3595,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "litemap_postcard+opt.wasm.gz",
+            "value": 3955,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids+opt.wasm.gz",
+            "value": 5831,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "borrowed_pattern+opt.wasm.gz",
+            "value": 6044,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "syntatically_canonicalize_locales+opt.wasm.gz",
+            "value": 12906,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "608fbd45586a8f392421b30087c6cd1c2cf902ca",
+          "message": "Update data model for non-gregorian calendars (#1300)\n\n* Parametrize cldr_dates() on calendar type\n\n* Refactor date code into common\n\n* Use litemap in CLDR provider\n\n* Start producing locale data in per-calendar subfolders\n\n* Regen testdata\n\n* Update tests\n\n* Use gregorian variant in datetime\n\n* Rename data keys to remove 'gregory'\n\n* Regen testdata\n\n* Rename GREGORY_ keys to not say GREGORY_\n\n* Move gregory -> calendar\n\n* fmt\n\n* gregory -> calendar\n\n* appease clippy\n\n* Add MissingVariant\n\n* gregorian -> gregory\n\n* Regen data\n\n* rm pubself\n\n* Handle other calendars in the JSON\n\n* Rename CLDR field types\n\n* fmt\n\n* MissingVariant -> NeedsVariant",
+          "timestamp": "2021-11-16T18:29:12-08:00",
+          "tree_id": "da0101cec667cc87db0ea4471ad72ed687e6cfae",
+          "url": "https://github.com/unicode-org/icu4x/commit/608fbd45586a8f392421b30087c6cd1c2cf902ca"
+        },
+        "date": 1637116465288,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "code_line_diff+opt.wasm.gz",
+            "value": 188281,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unread_emails+opt.wasm.gz",
+            "value": 188968,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "zv_serde+opt.wasm.gz",
+            "value": 225,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "owned_pattern+opt.wasm.gz",
+            "value": 7114,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "writeable_message+opt.wasm.gz",
+            "value": 4025,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unicode_bmp_blocks_selector+opt.wasm.gz",
+            "value": 6058,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_hash_map+opt.wasm.gz",
+            "value": 4688,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_lite_map+opt.wasm.gz",
+            "value": 3271,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "litemap_bincode+opt.wasm.gz",
+            "value": 9183,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "derive+opt.wasm.gz",
+            "value": 3806,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log+opt.wasm.gz",
+            "value": 221808,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "tui+opt.wasm.gz",
+            "value": 230120,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "elevator_floors+opt.wasm.gz",
+            "value": 188259,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "simple+opt.wasm.gz",
+            "value": 189962,
             "unit": "bytes",
             "biggerIsBetter": false
           },
