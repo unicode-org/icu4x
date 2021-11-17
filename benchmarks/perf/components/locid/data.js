783e3fb362427022680deba995a9cf484c1dc1aa
github-action-benchmark <github@users.noreply.github.com>
2021-11-17 22:16:53 +0000

    add Rust Benchmark (cargo) benchmark result for f2a5bf5e9dcad52e072e23e3770d5c7decf67a64

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637184787778,
+  "lastUpdate": 1637187413012,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17309,6 +17309,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5273,
             "range": "± 194",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f2a5bf5e9dcad52e072e23e3770d5c7decf67a64",
+          "message": "Homogenize yoke generic impls to always work with Yokeable, add OwnedYokeable (#1302)",
+          "timestamp": "2021-11-17T14:09:09-08:00",
+          "tree_id": "ba9ebd58c27b92d3b49c79602137cfd4378f1303",
+          "url": "https://github.com/unicode-org/icu4x/commit/f2a5bf5e9dcad52e072e23e3770d5c7decf67a64"
+        },
+        "date": 1637187406451,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3076,
+            "range": "± 159",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5250,
+            "range": "± 463",
             "unit": "ns/iter"
           }
         ]
