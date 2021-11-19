44fce96fd5c98c2031a22f34591db38f288ec966
github-action-benchmark <github@users.noreply.github.com>
2021-11-19 20:19:28 +0000

    add Rust Benchmark (cargo) benchmark result for c78b611172290d7ab729a985f48879b2f724b63e

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637349530168,
+  "lastUpdate": 1637353168752,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -14039,6 +14039,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 130999,
             "range": "± 12482",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c78b611172290d7ab729a985f48879b2f724b63e",
+          "message": "Add Default impls for all zerovec types (#1330)",
+          "timestamp": "2021-11-19T12:12:57-08:00",
+          "tree_id": "94a7417e5e9735ada9ce18c952d9d279e2e825d3",
+          "url": "https://github.com/unicode-org/icu4x/commit/c78b611172290d7ab729a985f48879b2f724b63e"
+        },
+        "date": 1637353161105,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 130336,
+            "range": "± 7234",
             "unit": "ns/iter"
           }
         ]
