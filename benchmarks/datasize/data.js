7d7986e090c3d41ca5899de3d786574635c45a72
github-action-benchmark <github@users.noreply.github.com>
2021-11-19 20:14:35 +0000

    add Benchmark (ndjson) benchmark result for c78b611172290d7ab729a985f48879b2f724b63e

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637349200933,
+  "lastUpdate": 1637352875574,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -4073,6 +4073,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/a96c0aa8873370bbba80fb0f041f65945ab30d37"
         },
         "date": 1637349194643,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 903688,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c78b611172290d7ab729a985f48879b2f724b63e",
+          "message": "Add Default impls for all zerovec types (#1330)",
+          "timestamp": "2021-11-19T12:12:57-08:00",
+          "tree_id": "94a7417e5e9735ada9ce18c952d9d279e2e825d3",
+          "url": "https://github.com/unicode-org/icu4x/commit/c78b611172290d7ab729a985f48879b2f724b63e"
+        },
+        "date": 1637352868843,
         "tool": "ndjson",
         "benches": [
           {
