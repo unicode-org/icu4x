5f93622a73c16df4e2d003a6656a3e9aab4f2118
Shane F. Carr <shane@unicode.org>
2021-11-18 17:42:03 -0800

    Migrate BlobProvider to ZeroMap

--- provider/blob/src/blob_schema.rs
+++ provider/blob/src/blob_schema.rs
@@ -3,9 +3,12 @@
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
 use litemap::LiteMap;
+use zerovec::map::ZeroMapBorrowed;
+use alloc::string::String;
+use alloc::vec::Vec;
 
 /// A versioned Serde schema for ICU4X data blobs.
-#[derive(serde::Serialize, serde::Deserialize, yoke::Yokeable)]
+#[derive(serde::Serialize, serde::Deserialize)]
 pub enum BlobSchema<'data> {
     #[serde(borrow)]
     V001(BlobSchemaV1<'data>),
@@ -14,7 +17,6 @@ pub enum BlobSchema<'data> {
 /// Version 1 of the ICU4X data blob schema.
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct BlobSchemaV1<'data> {
-    // TODO(#829): Use ZeroMap instead of LiteMap.
     #[serde(borrow)]
-    pub resources: LiteMap<&'data str, &'data [u8]>,
+    pub resources: ZeroMapBorrowed<'data, str, [u8]>,
 }
