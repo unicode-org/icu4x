be8caf2b532ff983a84f8ef895e8d8ffe973a19f
github-action-benchmark <github@users.noreply.github.com>
2021-11-19 23:22:06 +0000

    add Rust Benchmark (cargo) benchmark result for 9258d0505eab7165723c6e052c51e26f085cd1fa

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637353211639,
+  "lastUpdate": 1637364126720,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17489,6 +17489,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5171,
             "range": "± 177",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9258d0505eab7165723c6e052c51e26f085cd1fa",
+          "message": "Add PartialEq, Debug, Clone impls to ZeroMap (#1332)\n\n* Add PartialEq, Debug, Clone impls to ZeroMap\r\n\r\n* fmt\r\n\r\n* Add ?Sized bounds",
+          "timestamp": "2021-11-19T15:14:31-08:00",
+          "tree_id": "b796e4cc08c29f67ced0c4544209936495c84873",
+          "url": "https://github.com/unicode-org/icu4x/commit/9258d0505eab7165723c6e052c51e26f085cd1fa"
+        },
+        "date": 1637364120378,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2624,
+            "range": "± 734",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4382,
+            "range": "± 282",
             "unit": "ns/iter"
           }
         ]
