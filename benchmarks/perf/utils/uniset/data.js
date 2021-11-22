476cdaaf32e4b16d956b872e1047cfe89f4eaa4a
github-action-benchmark <github@users.noreply.github.com>
2021-11-22 20:39:05 +0000

    add Rust Benchmark (cargo) benchmark result for ca2dd56cc13ad6c18d66d7b2f4df989acc5a71c5

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637610334951,
+  "lastUpdate": 1637613545446,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15179,6 +15179,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 36933500,
             "range": "± 1569803",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ca2dd56cc13ad6c18d66d7b2f4df989acc5a71c5",
+          "message": "Bump zerovec to 0.5 (#1336)",
+          "timestamp": "2021-11-22T12:31:57-08:00",
+          "tree_id": "58002db444f6e981b88b31e01c282e95f58d72f2",
+          "url": "https://github.com/unicode-org/icu4x/commit/ca2dd56cc13ad6c18d66d7b2f4df989acc5a71c5"
+        },
+        "date": 1637613538123,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 40752198,
+            "range": "± 340824",
             "unit": "ns/iter"
           }
         ]
