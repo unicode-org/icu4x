e1eda3dbf02e7d12b351be6482e4d8fe8f998139
github-action-benchmark <github@users.noreply.github.com>
2021-11-22 19:39:57 +0000

    add Benchmark (ndjson) benchmark result for 08f539e48bac34b218a27df29ef18dc62ede132e

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637369636973,
+  "lastUpdate": 1637609997530,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -4163,6 +4163,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/1c5901fa24fd17605e0534a1c2bcee89d84807e3"
         },
         "date": 1637369629874,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 903688,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "08f539e48bac34b218a27df29ef18dc62ede132e",
+          "message": "Allow cloning_zcf to be applied to individual variants or fields (#1307)\n\n* Move has_cloning_zcf_attr to function\r\n\r\n* Extend cloning_zcf to work on fields as well\r\n\r\n* Add test\r\n\r\n* fmt",
+          "timestamp": "2021-11-22T11:38:21-08:00",
+          "tree_id": "4aade8279b57f5efda2967fda8fa2e670fe6e8f0",
+          "url": "https://github.com/unicode-org/icu4x/commit/08f539e48bac34b218a27df29ef18dc62ede132e"
+        },
+        "date": 1637609990557,
         "tool": "ndjson",
         "benches": [
           {
