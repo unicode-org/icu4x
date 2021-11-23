67335924169640552660a4d86f0f671768bc9eaf
github-action-benchmark <github@users.noreply.github.com>
2021-11-23 07:46:52 +0000

    add Rust Benchmark (cargo) benchmark result for 9d0137c20b31d2fd222cd13a2257e8d98416b86c

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637613626715,
+  "lastUpdate": 1637653612019,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19865,6 +19865,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 22445,
             "range": "± 4287",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9d0137c20b31d2fd222cd13a2257e8d98416b86c",
+          "message": "Migrate StaticDataProvider and BlobDataProvider to ZeroMap (#1058)",
+          "timestamp": "2021-11-22T23:39:30-08:00",
+          "tree_id": "2a458590b63493adabe0d3da0c5bc6b6b9887558",
+          "url": "https://github.com/unicode-org/icu4x/commit/9d0137c20b31d2fd222cd13a2257e8d98416b86c"
+        },
+        "date": 1637653605612,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1185,
+            "range": "± 71",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 5790,
+            "range": "± 294",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 20291,
+            "range": "± 1155",
             "unit": "ns/iter"
           }
         ]
