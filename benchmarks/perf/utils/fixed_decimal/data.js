c157671f8230125a616e5d88d705e1aa46cd0081
github-action-benchmark <github@users.noreply.github.com>
2021-11-24 19:14:14 +0000

    add Rust Benchmark (cargo) benchmark result for 7418270529534848b000a195299802c74e9971ff

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637781252521,
+  "lastUpdate": 1637781254156,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -14339,6 +14339,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 140044,
             "range": "± 885",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7418270529534848b000a195299802c74e9971ff",
+          "message": "Migrate the various map impls in the CLDR transformer to LiteMap (#1341)",
+          "timestamp": "2021-11-24T11:07:30-08:00",
+          "tree_id": "54277d03057779d7e9bfb95a3db4faaf77f3470d",
+          "url": "https://github.com/unicode-org/icu4x/commit/7418270529534848b000a195299802c74e9971ff"
+        },
+        "date": 1637781243255,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 139469,
+            "range": "± 5394",
             "unit": "ns/iter"
           }
         ]
