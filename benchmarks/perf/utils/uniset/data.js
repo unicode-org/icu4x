7c23bbecb1e9e1f09cb41a091e582a34284c1a10
github-action-benchmark <github@users.noreply.github.com>
2021-11-24 19:14:39 +0000

    add Rust Benchmark (cargo) benchmark result for 92dbabdbf4f81f55492d65dcfde9a003aaa9de24

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637781236158,
+  "lastUpdate": 1637781279094,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15389,6 +15389,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 33487091,
             "range": "± 1453029",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "92dbabdbf4f81f55492d65dcfde9a003aaa9de24",
+          "message": "Remove DataPayloadInner and migrate to a single Yoke type (#1342)",
+          "timestamp": "2021-11-24T11:07:48-08:00",
+          "tree_id": "16b9957252cb5158b6f9e499a38fe0945886838d",
+          "url": "https://github.com/unicode-org/icu4x/commit/92dbabdbf4f81f55492d65dcfde9a003aaa9de24"
+        },
+        "date": 1637781271575,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 41078082,
+            "range": "± 122383",
             "unit": "ns/iter"
           }
         ]
