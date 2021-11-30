041f2ebc35730ea2822e29959b1db2c224557eae
github-action-benchmark <github@users.noreply.github.com>
2021-11-30 01:51:43 +0000

    add Rust Benchmark (cargo) benchmark result for fd5eb4b373b48870b7ffd4dd19946fdcc46e2cd9

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637859645308,
+  "lastUpdate": 1638237103386,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -13871,6 +13871,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 398084,
             "range": "± 28103",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fd5eb4b373b48870b7ffd4dd19946fdcc46e2cd9",
+          "message": "Remove attach_to_option_cart() (#1348)",
+          "timestamp": "2021-11-29T17:39:44-08:00",
+          "tree_id": "dff9d68f81b1d9c112ea3ed080853f8f8b7f24be",
+          "url": "https://github.com/unicode-org/icu4x/commit/fd5eb4b373b48870b7ffd4dd19946fdcc46e2cd9"
+        },
+        "date": 1638237094825,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 142678,
+            "range": "± 9531",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1585178,
+            "range": "± 79907",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 391488,
+            "range": "± 53206",
             "unit": "ns/iter"
           }
         ]
