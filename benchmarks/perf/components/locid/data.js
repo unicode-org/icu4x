c300c65b6e3955ad5eecc1ecee61deebe5e36089
github-action-benchmark <github@users.noreply.github.com>
2021-11-30 02:29:17 +0000

    add Rust Benchmark (cargo) benchmark result for fcf248c8a8ff1c6da7e18800a21ee4c95a3b97ca

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638237066534,
+  "lastUpdate": 1638239357491,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17993,6 +17993,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5043,
             "range": "± 224",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fcf248c8a8ff1c6da7e18800a21ee4c95a3b97ca",
+          "message": "Implement era formatting (#1346)\n\n* Add era field parsing\r\n\r\n* Handle eras in components bag\r\n\r\n* Apply era formatting\r\n\r\n* Add tests for buddhist date formatting",
+          "timestamp": "2021-11-29T18:18:45-08:00",
+          "tree_id": "2bc3073d9d69c2d89d93c86684541854e1c17582",
+          "url": "https://github.com/unicode-org/icu4x/commit/fcf248c8a8ff1c6da7e18800a21ee4c95a3b97ca"
+        },
+        "date": 1638239341564,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3013,
+            "range": "± 169",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5544,
+            "range": "± 370",
             "unit": "ns/iter"
           }
         ]
