6b0d912d47a23d738d12543dc5e295c94bd42479
Manish Goregaokar <manishsmail@gmail.com>
2021-12-03 11:20:03 +0530

    Fix tests

--- utils/zerovec/src/ule/custom/mod.rs
+++ utils/zerovec/src/ule/custom/mod.rs
@@ -88,18 +88,18 @@
 //! }
 //!
 //! fn main() {
-//!     let mut foos = vec![Foo {field1: 'u', field2: 983, field3: ZeroVec::alloc_from_slice(&[1212,2309,500,7000])},
-//!                         Foo {field1: 'l', field2: 1010, field3: ZeroVec::alloc_from_slice(&[1932, 0, 8888, 91237])}];
+//!     let mut foos = vec![Foo {field1: 'u', field2: 983, field3: ZeroVec::<u32>::alloc_from_slice(&[1212,2309,500,7000])},
+//!                         Foo {field1: 'l', field2: 1010, field3: ZeroVec::<u32>::alloc_from_slice(&[1932, 0, 8888, 91237])}];
 //!
 //!     let vzv = VarZeroVec::from(&*foos);
 //!
 //!     assert_eq!(char::from_unaligned(vzv.get(0).unwrap().field1), 'u');
 //!     assert_eq!(u32::from_unaligned(vzv.get(0).unwrap().field2), 983);
-//!     assert_eq!(&vzv.get(0).unwrap().field3, ZeroVec::alloc_from_slice(&[1212,2309,500,7000]).as_slice());
+//!     assert_eq!(&vzv.get(0).unwrap().field3, ZeroVec::<u32>::alloc_from_slice(&[1212,2309,500,7000]).as_slice());
 //!
 //!     assert_eq!(char::from_unaligned(vzv.get(1).unwrap().field1), 'l');
 //!     assert_eq!(u32::from_unaligned(vzv.get(1).unwrap().field2), 1010);
-//!     assert_eq!(&vzv.get(1).unwrap().field3, ZeroVec::alloc_from_slice(&[1932, 0, 8888, 91237]).as_slice());
+//!     assert_eq!(&vzv.get(1).unwrap().field3, ZeroVec::<u32>::alloc_from_slice(&[1932, 0, 8888, 91237]).as_slice());
 //! }
 //! ```
 
