5d4eaaa20347ec739d02910ae910c67d6b103969
github-action-benchmark <github@users.noreply.github.com>
2021-12-04 10:06:59 +0000

    add Benchmark (ndjson) benchmark result for a0f78c5dcea33368cd89c06332e1b59e335e4de3

--- benchmarks/binsize/gz/data.js
+++ benchmarks/binsize/gz/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638429797172,
+  "lastUpdate": 1638612419354,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -24658,6 +24658,144 @@ window.BENCHMARK_DATA = {
           {
             "name": "elevator_floors+opt.wasm.gz",
             "value": 192015,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "derive+opt.wasm.gz",
+            "value": 3806,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "10595307+mildgravitas@users.noreply.github.com",
+            "name": "mildgravitas",
+            "username": "mildgravitas"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a0f78c5dcea33368cd89c06332e1b59e335e4de3",
+          "message": "Year of week-of-year support (#1206)\n\n* feat(datetime): Add support for week of year (this previously just used the calendar year).\r\n\r\n* feat(datetime): Add year of week-of-year to components::Bag & fix width adjustments for it.\r\n\r\nTo do this I've loosened get_best_available_format_pattern() to match on\r\nFieldSymbol enums but not their data. From the function's greater/lesser\r\nmatching this is apparently what the function tried to do all along. Without\r\nthis Year::NumericWeekOf wouldn't match as CLDR skeletons use 'y' even for\r\npatterns with 'Y'\r\n\r\nThis accessorily improves full & long time_h11_h12/time_h23_h24\r\npatterns: the h11_h12/h23_h24 coercion logic matches adjusted patterns\r\nagainst skeletons & previously 'z' was not matched againts 'v' leading\r\nto the time zone being dropped.\r\n\r\nIf we don't care to expose the week-of year variants in components::Bag\r\n& don't care about coerced time patterns then only\r\nadjust_pattern_field_lengths() need be adjusted.\r\n\r\n* doc(datetime): add examples to the descriptions of datetime::options::components::Year.\r\n\r\n* fix(datetime): remove serde renames & further expand the documentation of datetime::options::components::Year\r\n\r\n* fix(datetime): also update year enum names in benches.\r\n\r\n* fix(datetime): swich datetime::options::components::Year & Week to kebab case for serialization\r\n\r\n* style(datetime): use is_eq instead of == Ordering:::Equal\r\n\r\n* fix(datetime): use kebab-case for all options::components enums\r\n\r\n* tests(datetime): add extra locales to some test cases\r\n\r\n* fix(datetime): Return an error in DateTimeFormat::try_new instead of an empty pattern if there are no matches.",
+          "timestamp": "2021-12-04T02:01:23-08:00",
+          "tree_id": "db0214ac36483d76bfc51ef378fdda271fc23dbe",
+          "url": "https://github.com/unicode-org/icu4x/commit/a0f78c5dcea33368cd89c06332e1b59e335e4de3"
+        },
+        "date": 1638612411203,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "unread_emails+opt.wasm.gz",
+            "value": 192433,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "syntatically_canonicalize_locales+opt.wasm.gz",
+            "value": 12938,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "litemap_bincode+opt.wasm.gz",
+            "value": 9182,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log+opt.wasm.gz",
+            "value": 224115,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "zv_serde+opt.wasm.gz",
+            "value": 225,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_lite_map+opt.wasm.gz",
+            "value": 3271,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "litemap_postcard+opt.wasm.gz",
+            "value": 3955,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids+opt.wasm.gz",
+            "value": 5830,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "owned_pattern+opt.wasm.gz",
+            "value": 7114,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "tui+opt.wasm.gz",
+            "value": 235070,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "code_line_diff+opt.wasm.gz",
+            "value": 192320,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "writeable_message+opt.wasm.gz",
+            "value": 4025,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unicode_bmp_blocks_selector+opt.wasm.gz",
+            "value": 6058,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_hash_map+opt.wasm.gz",
+            "value": 4688,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "borrowed_pattern+opt.wasm.gz",
+            "value": 6044,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "simple_static+opt.wasm.gz",
+            "value": 193806,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "permyriad+opt.wasm.gz",
+            "value": 3595,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "elevator_floors+opt.wasm.gz",
+            "value": 192060,
             "unit": "bytes",
             "biggerIsBetter": false
           },
