c83dbbcd5e2f43f963e8a5a5abe2e412af5e8e61
github-action-benchmark <github@users.noreply.github.com>
2021-12-07 11:10:29 +0000

    add Rust Benchmark (cargo) benchmark result for bc9c9ffec1e492407a7e23fc9b8d08b12b0ae5c0

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638802368735,
+  "lastUpdate": 1638875429386,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -18209,6 +18209,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4593,
             "range": "± 202",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bc9c9ffec1e492407a7e23fc9b8d08b12b0ae5c0",
+          "message": "Custom fallbacks (#1309)",
+          "timestamp": "2021-12-07T03:03:23-08:00",
+          "tree_id": "fc4695f8f16e7fc01525bc01b851d0991df11380",
+          "url": "https://github.com/unicode-org/icu4x/commit/bc9c9ffec1e492407a7e23fc9b8d08b12b0ae5c0"
+        },
+        "date": 1638875422239,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2913,
+            "range": "± 88",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4681,
+            "range": "± 99",
             "unit": "ns/iter"
           }
         ]
