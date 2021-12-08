717a01f9816cc507034d5df6d47a74313c5081e0
github-action-benchmark <github@users.noreply.github.com>
2021-12-08 05:36:35 +0000

    add Rust Benchmark (cargo) benchmark result for 7d774380d73d8726f84259818884dc7c883cdcb3

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638938344405,
+  "lastUpdate": 1638941795334,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -14249,6 +14249,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 298383,
             "range": "± 2284",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "m_kato@ga2.so-net.ne.jp",
+            "name": "Makoto Kato",
+            "username": "makotokato"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7d774380d73d8726f84259818884dc7c883cdcb3",
+          "message": "Implement word segmenter using rule based segmenter (#1273)\n\n* Implement word segmenter using rule based segmenter\r\n\r\n* Fix cargo make generate-readmes.\r\n\r\n* Update segmenter table generation\r\n\r\n- Use builtin data for SA\r\n- Expand codepoint for property\r\n\r\n* UAX documents should use https URL\r\n\r\n* Change data format to toml.\r\n\r\n* Convert json file to toml fomrat\r\n\r\n* cargo make generate-readmes\r\n\r\n* Fix per review comment",
+          "timestamp": "2021-12-08T14:27:42+09:00",
+          "tree_id": "d3e79a1bbf16f1d5b5a88da1869b2d08fba62794",
+          "url": "https://github.com/unicode-org/icu4x/commit/7d774380d73d8726f84259818884dc7c883cdcb3"
+        },
+        "date": 1638941788038,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 127772,
+            "range": "± 551",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1495803,
+            "range": "± 12801",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 362867,
+            "range": "± 1043",
             "unit": "ns/iter"
           }
         ]
