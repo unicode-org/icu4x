8049eb0e4122da599f962c1890f745f94d065fdf
github-action-benchmark <github@users.noreply.github.com>
2021-12-09 17:15:32 +0000

    add Rust Benchmark (cargo) benchmark result for 1d9ddad75f72a378881526e0fc2cd3ac0f37c6e0

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639039130956,
+  "lastUpdate": 1639070132663,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15839,6 +15839,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 30018933,
             "range": "± 1847576",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1d9ddad75f72a378881526e0fc2cd3ac0f37c6e0",
+          "message": "Replace SerdeDeDataProvider with BufferProvider (#1369)\n\n- Add buffer_format to DataResponseMetadata\r\n- Migrate fs provider to BufferFormat enum\r\n- impl BufferProvider for FsDataProvider\r\n- Migrate FsDataProvider to use DeserializingProvider for deserialization\r\n- Rename serialization primitives\r\n- Rename features",
+          "timestamp": "2021-12-09T11:08:39-06:00",
+          "tree_id": "591f615255693682b5ded5400247c27665e1954a",
+          "url": "https://github.com/unicode-org/icu4x/commit/1d9ddad75f72a378881526e0fc2cd3ac0f37c6e0"
+        },
+        "date": 1639070125853,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 46241492,
+            "range": "± 1731254",
             "unit": "ns/iter"
           }
         ]
