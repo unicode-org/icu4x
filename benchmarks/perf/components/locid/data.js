a6b2d9220756ae2c235c8dbad9685254f7081024
github-action-benchmark <github@users.noreply.github.com>
2021-12-10 05:27:31 +0000

    add Rust Benchmark (cargo) benchmark result for eae3099d496d66f4fdd27168ff89e5211d0b3bc7

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639070213124,
+  "lastUpdate": 1639114051136,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -18425,6 +18425,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 5156,
             "range": "± 229",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eae3099d496d66f4fdd27168ff89e5211d0b3bc7",
+          "message": "Moving deduplicating_array to its own crate (#1364)",
+          "timestamp": "2021-12-09T21:18:56-08:00",
+          "tree_id": "2b9b89f49d206e34b9782ded38c5359320adc574",
+          "url": "https://github.com/unicode-org/icu4x/commit/eae3099d496d66f4fdd27168ff89e5211d0b3bc7"
+        },
+        "date": 1639114043582,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2755,
+            "range": "± 110",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4576,
+            "range": "± 240",
             "unit": "ns/iter"
           }
         ]
