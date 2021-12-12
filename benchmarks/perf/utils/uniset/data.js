75a56f313aa1b17fcbbe8f5fe5cad1fc054face3
github-action-benchmark <github@users.noreply.github.com>
2021-12-12 00:53:43 +0000

    add Rust Benchmark (cargo) benchmark result for 7e1d77e59e3145175d1267905eef35eefecc34b7

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639265410848,
+  "lastUpdate": 1639270423025,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15989,6 +15989,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 33315059,
             "range": "± 2074594",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58569820+nordzilla@users.noreply.github.com",
+            "name": "Erik Nordin",
+            "username": "nordzilla"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7e1d77e59e3145175d1267905eef35eefecc34b7",
+          "message": "Fix typo in Yokeable documentation (#1386)",
+          "timestamp": "2021-12-11T16:45:39-08:00",
+          "tree_id": "905795728b12e9ff444346a16fe51a3544626d9f",
+          "url": "https://github.com/unicode-org/icu4x/commit/7e1d77e59e3145175d1267905eef35eefecc34b7"
+        },
+        "date": 1639270416045,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 44554617,
+            "range": "± 1662639",
             "unit": "ns/iter"
           }
         ]
