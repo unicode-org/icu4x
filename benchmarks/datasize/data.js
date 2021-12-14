d7f830e0ec97a388801a431fbc4d7ee537ac5931
github-action-benchmark <github@users.noreply.github.com>
2021-12-14 05:06:50 +0000

    add Benchmark (ndjson) benchmark result for 7ae1e6df3178f839840e313b0065dcd1915a151c

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639456791753,
+  "lastUpdate": 1639458409921,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -5093,6 +5093,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/290e46c3969d5345cebc4587e33638ed1251dd61"
         },
         "date": 1639456784773,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 921273,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7ae1e6df3178f839840e313b0065dcd1915a151c",
+          "message": "Simplify ZeroVec/VarZeroVec error handling, consolidate ULEError type (#1389)\n\n* Replace all ZeroVec errors with ULEError\r\n\r\n* Use ULEError for VarZeroVec\r\n\r\n* Use new ULE impl everywhere",
+          "timestamp": "2021-12-13T21:05:07-08:00",
+          "tree_id": "ddfe182d74812e7523ff713f0c5be85cb4d287b7",
+          "url": "https://github.com/unicode-org/icu4x/commit/7ae1e6df3178f839840e313b0065dcd1915a151c"
+        },
+        "date": 1639458401468,
         "tool": "ndjson",
         "benches": [
           {
