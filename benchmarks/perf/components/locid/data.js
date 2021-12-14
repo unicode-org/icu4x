9ace7358aee5301d9e1a290e11a7f6c51245d896
github-action-benchmark <github@users.noreply.github.com>
2021-12-14 16:34:52 +0000

    add Rust Benchmark (cargo) benchmark result for 5002e70343983cc08163b3df2da0b48dadc9856f

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639497346235,
+  "lastUpdate": 1639499691919,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -18749,6 +18749,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4941,
             "range": "± 27",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5002e70343983cc08163b3df2da0b48dadc9856f",
+          "message": "Add LineBreakSegmenter and rewire code to use it (#1387)",
+          "timestamp": "2021-12-14T10:26:06-06:00",
+          "tree_id": "6ea782251c3b62bbbe5c845b771213c7e88d0221",
+          "url": "https://github.com/unicode-org/icu4x/commit/5002e70343983cc08163b3df2da0b48dadc9856f"
+        },
+        "date": 1639499683403,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2944,
+            "range": "± 324",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4744,
+            "range": "± 219",
             "unit": "ns/iter"
           }
         ]
