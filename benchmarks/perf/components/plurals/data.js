7da5b7572e694aa2aa4c5818470d2a2bfe7e6786
github-action-benchmark <github@users.noreply.github.com>
2021-12-15 09:30:30 +0000

    add Rust Benchmark (cargo) benchmark result for f3db25335e27591a3b1d83d1fd0325bc4403958b

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639517296465,
+  "lastUpdate": 1639560630713,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -21377,6 +21377,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 20726,
             "range": "± 22",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3db25335e27591a3b1d83d1fd0325bc4403958b",
+          "message": "Making ListFormatterPattern construction more private (#1398)",
+          "timestamp": "2021-12-15T10:21:30+01:00",
+          "tree_id": "30473ad60984458f19b0b97d7e4daeff06551689",
+          "url": "https://github.com/unicode-org/icu4x/commit/f3db25335e27591a3b1d83d1fd0325bc4403958b"
+        },
+        "date": 1639560623313,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1159,
+            "range": "± 68",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6543,
+            "range": "± 564",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 20693,
+            "range": "± 1852",
             "unit": "ns/iter"
           }
         ]
