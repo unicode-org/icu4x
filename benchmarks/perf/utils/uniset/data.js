d6ad42bcba0f74e03e279c8dc6694bba91a92c46
github-action-benchmark <github@users.noreply.github.com>
2021-12-16 23:57:32 +0000

    add Rust Benchmark (cargo) benchmark result for 685622ee653d322b6e824b72d765db2cfd99ef03

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639696790055,
+  "lastUpdate": 1639699052020,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16439,6 +16439,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 37624069,
             "range": "± 1445871",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "685622ee653d322b6e824b72d765db2cfd99ef03",
+          "message": "Implement data transformer for Script_Extensions map data (#1353)",
+          "timestamp": "2021-12-16T15:49:38-08:00",
+          "tree_id": "474f7633428f399e7a54d9448fe76c5d94a200f6",
+          "url": "https://github.com/unicode-org/icu4x/commit/685622ee653d322b6e824b72d765db2cfd99ef03"
+        },
+        "date": 1639699043916,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 42867249,
+            "range": "± 135074",
             "unit": "ns/iter"
           }
         ]
