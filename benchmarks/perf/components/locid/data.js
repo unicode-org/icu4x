86127fcfad01e6bc5d0c25707bbd7262dc25a7f3
github-action-benchmark <github@users.noreply.github.com>
2021-12-16 23:58:45 +0000

    add Rust Benchmark (cargo) benchmark result for 294c66ff679eaf646d9a0fd91888135b54ee8869

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639699090189,
+  "lastUpdate": 1639699125315,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19145,6 +19145,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4946,
             "range": "± 60",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "294c66ff679eaf646d9a0fd91888135b54ee8869",
+          "message": "Rename resource key category for properties (#1406)",
+          "timestamp": "2021-12-16T15:50:16-08:00",
+          "tree_id": "b8c91afc9736a504006dd38c159dae3edf411c99",
+          "url": "https://github.com/unicode-org/icu4x/commit/294c66ff679eaf646d9a0fd91888135b54ee8869"
+        },
+        "date": 1639699117651,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2653,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4098,
+            "range": "± 16",
             "unit": "ns/iter"
           }
         ]
