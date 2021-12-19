92f8108d41864e33a364c9d93be07ab6ff81b103
github-action-benchmark <github@users.noreply.github.com>
2021-12-19 08:18:40 +0000

    add Rust Benchmark (cargo) benchmark result for 55aeb880fc1e2dd86717227b0e7b918d2e444ec4

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639901897612,
+  "lastUpdate": 1639901919935,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15467,6 +15467,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 368915,
             "range": "± 18937",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "55aeb880fc1e2dd86717227b0e7b918d2e444ec4",
+          "message": "Rename PlainOldULE to RawBytesULE (#1413)",
+          "timestamp": "2021-12-19T00:09:40-08:00",
+          "tree_id": "6ea2fc3a0e5dbba74634602b8a60a02b88db8121",
+          "url": "https://github.com/unicode-org/icu4x/commit/55aeb880fc1e2dd86717227b0e7b918d2e444ec4"
+        },
+        "date": 1639901911888,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 135159,
+            "range": "± 14944",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1440107,
+            "range": "± 120118",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 357931,
+            "range": "± 33097",
             "unit": "ns/iter"
           }
         ]
