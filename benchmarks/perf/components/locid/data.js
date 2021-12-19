f6e8c17f95c619fc14c36e7745a76c99bd8538b2
github-action-benchmark <github@users.noreply.github.com>
2021-12-19 05:57:01 +0000

    add Rust Benchmark (cargo) benchmark result for da07d1d672a0ce53c69bc79e52fc33eddba94728

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639794186496,
+  "lastUpdate": 1639893421486,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19253,6 +19253,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4454,
             "range": "± 490",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "da07d1d672a0ce53c69bc79e52fc33eddba94728",
+          "message": "Improve EncodeAsVarULE (#1385)",
+          "timestamp": "2021-12-18T21:49:10-08:00",
+          "tree_id": "1fff779fa4b2bb235086d18c2aee5d51fa1cf5a2",
+          "url": "https://github.com/unicode-org/icu4x/commit/da07d1d672a0ce53c69bc79e52fc33eddba94728"
+        },
+        "date": 1639893413568,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2915,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4921,
+            "range": "± 6",
             "unit": "ns/iter"
           }
         ]
