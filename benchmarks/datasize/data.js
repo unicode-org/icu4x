76ba456d229e2d6c796804b549f6375a49986459
github-action-benchmark <github@users.noreply.github.com>
2021-12-19 11:44:29 +0000

    add Benchmark (ndjson) benchmark result for e82c1fbbd9827de1db96334edcc21c5a8a67d5c0

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1639903008536,
+  "lastUpdate": 1639914269088,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -5693,6 +5693,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/8328551082ea99dda9e32a16ce126991830e81a8"
         },
         "date": 1639903000313,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 1393792,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "m_kato@ga2.so-net.ne.jp",
+            "name": "Makoto Kato",
+            "username": "makotokato"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e82c1fbbd9827de1db96334edcc21c5a8a67d5c0",
+          "message": "Add Grapheme segmenter that is a part of UAX29. (#1388)\n\n* Add Grapheme segmenter that is a part of UAX29.\r\n\r\n* Fix review comment.",
+          "timestamp": "2021-12-19T20:42:53+09:00",
+          "tree_id": "6443b26a9bbf0b4643465016869233d9df1f19a1",
+          "url": "https://github.com/unicode-org/icu4x/commit/e82c1fbbd9827de1db96334edcc21c5a8a67d5c0"
+        },
+        "date": 1639914261815,
         "tool": "ndjson",
         "benches": [
           {
