997fc1bd1d240f49defc26f29f37be16affd8ae8
github-action-benchmark <github@users.noreply.github.com>
2021-12-20 21:21:35 +0000

    add Rust Benchmark (cargo) benchmark result for e96204ea6b758acc25b7cad2cbbac5ec705c265b

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640032441656,
+  "lastUpdate": 1640035295504,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -15719,6 +15719,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 115255,
             "range": "± 460",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e96204ea6b758acc25b7cad2cbbac5ec705c265b",
+          "message": "Implementing Writeable for all integers (#1408)",
+          "timestamp": "2021-12-20T22:12:52+01:00",
+          "tree_id": "3eb10e05d14aa09c84c18b9250c5fb9c6ffc8a7b",
+          "url": "https://github.com/unicode-org/icu4x/commit/e96204ea6b758acc25b7cad2cbbac5ec705c265b"
+        },
+        "date": 1640035287200,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 116802,
+            "range": "± 4365",
             "unit": "ns/iter"
           }
         ]
