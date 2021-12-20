70581e0458684d6ff5b583cd69080cc351a2a692
github-action-benchmark <github@users.noreply.github.com>
2021-12-20 21:22:24 +0000

    add Rust Benchmark (cargo) benchmark result for e96204ea6b758acc25b7cad2cbbac5ec705c265b

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640032519274,
+  "lastUpdate": 1640035344295,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22091,6 +22091,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 21053,
             "range": "± 1188",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e96204ea6b758acc25b7cad2cbbac5ec705c265b",
+          "message": "Implementing Writeable for all integers (#1408)",
+          "timestamp": "2021-12-20T22:12:52+01:00",
+          "tree_id": "3eb10e05d14aa09c84c18b9250c5fb9c6ffc8a7b",
+          "url": "https://github.com/unicode-org/icu4x/commit/e96204ea6b758acc25b7cad2cbbac5ec705c265b"
+        },
+        "date": 1640035337073,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1089,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 5471,
+            "range": "± 12",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 20547,
+            "range": "± 27",
             "unit": "ns/iter"
           }
         ]
