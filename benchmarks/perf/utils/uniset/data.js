40fd0d9a93100de2f1903f267068210bf75234df
github-action-benchmark <github@users.noreply.github.com>
2021-12-22 00:40:15 +0000

    add Rust Benchmark (cargo) benchmark result for f4fee4264c86ea324e6cf5202632ca8838d6803b

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640118263936,
+  "lastUpdate": 1640133615362,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16919,6 +16919,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 25999750,
             "range": "± 734024",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "m_kato@ga2.so-net.ne.jp",
+            "name": "Makoto Kato",
+            "username": "makotokato"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f4fee4264c86ea324e6cf5202632ca8838d6803b",
+          "message": "Add build scripts in Cargo.toml for cargo vendor. (#1424)",
+          "timestamp": "2021-12-22T09:32:46+09:00",
+          "tree_id": "4a6285a9b07b5dd8bc8e49ffa5135480951aea00",
+          "url": "https://github.com/unicode-org/icu4x/commit/f4fee4264c86ea324e6cf5202632ca8838d6803b"
+        },
+        "date": 1640133606326,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 36669153,
+            "range": "± 2600307",
             "unit": "ns/iter"
           }
         ]
