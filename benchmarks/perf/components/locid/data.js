ed79a4a75f4e87759ea62fc487b3b71214e6d49f
github-action-benchmark <github@users.noreply.github.com>
2021-12-22 17:20:28 +0000

    add Rust Benchmark (cargo) benchmark result for 5cfbb17701d725fe739afb5164c9a75d565674eb

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640170112148,
+  "lastUpdate": 1640193628851,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19757,6 +19757,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4922,
             "range": "± 7",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5cfbb17701d725fe739afb5164c9a75d565674eb",
+          "message": "Rewriting ListFormatter to only use appends (#1395)",
+          "timestamp": "2021-12-22T18:13:12+01:00",
+          "tree_id": "f3b5d4e6abfb8beea793cd0eed768989808bc5b2",
+          "url": "https://github.com/unicode-org/icu4x/commit/5cfbb17701d725fe739afb5164c9a75d565674eb"
+        },
+        "date": 1640193621049,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2424,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4115,
+            "range": "± 15",
             "unit": "ns/iter"
           }
         ]
