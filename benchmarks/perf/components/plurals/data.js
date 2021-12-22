6548705f793e903db982725d58359baf70a8a038
github-action-benchmark <github@users.noreply.github.com>
2021-12-22 18:09:04 +0000

    add Rust Benchmark (cargo) benchmark result for 0c1f751b6a50b74fbfa367758f9cf89cda55273a

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640194180340,
+  "lastUpdate": 1640196543845,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22427,6 +22427,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 18368,
             "range": "± 74",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0c1f751b6a50b74fbfa367758f9cf89cda55273a",
+          "message": "Using new list data invariants (#1434)",
+          "timestamp": "2021-12-22T19:00:03+01:00",
+          "tree_id": "9aea966e2390f60b4377c37a1f75d3d72412831f",
+          "url": "https://github.com/unicode-org/icu4x/commit/0c1f751b6a50b74fbfa367758f9cf89cda55273a"
+        },
+        "date": 1640196534951,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1303,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6652,
+            "range": "± 409",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 25793,
+            "range": "± 2149",
             "unit": "ns/iter"
           }
         ]
