6434224466423abcc3de574423652bb487527ddc
github-action-benchmark <github@users.noreply.github.com>
2021-12-23 21:30:11 +0000

    add Rust Benchmark (cargo) benchmark result for d9cd5870dd444ba64ed6671089e1de4c471d1dd9

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640269981467,
+  "lastUpdate": 1640295011811,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16079,6 +16079,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 125446,
             "range": "± 6701",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d9cd5870dd444ba64ed6671089e1de4c471d1dd9",
+          "message": "Remove SmallStr and recommend Cow<str> (#1417)",
+          "timestamp": "2021-12-23T13:23:10-08:00",
+          "tree_id": "78ad5c6a020995304aa59d662dc7a9dea4a5302a",
+          "url": "https://github.com/unicode-org/icu4x/commit/d9cd5870dd444ba64ed6671089e1de4c471d1dd9"
+        },
+        "date": 1640295002118,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 127075,
+            "range": "± 15927",
             "unit": "ns/iter"
           }
         ]
