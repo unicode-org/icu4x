7c0cc2ef0db13a4ed0b63584ce26cc8e026dd0d5
github-action-benchmark <github@users.noreply.github.com>
2021-12-30 22:30:49 +0000

    add Rust Benchmark (cargo) benchmark result for 714acbd4ed5ce70722a60f102ad693ee48189048

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640896200943,
+  "lastUpdate": 1640903449638,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20225,6 +20225,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4059,
             "range": "± 206",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "714acbd4ed5ce70722a60f102ad693ee48189048",
+          "message": "ListFormatter example (#1456)",
+          "timestamp": "2021-12-30T23:22:43+01:00",
+          "tree_id": "989ecc8c140bb226aa6c7b1cf061a97b20c95fb7",
+          "url": "https://github.com/unicode-org/icu4x/commit/714acbd4ed5ce70722a60f102ad693ee48189048"
+        },
+        "date": 1640903441063,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2227,
+            "range": "± 16",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4181,
+            "range": "± 8",
             "unit": "ns/iter"
           }
         ]
