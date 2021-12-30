956a0e0e7c20fd9fca0a8b6ed6936c82a365ce55
github-action-benchmark <github@users.noreply.github.com>
2021-12-30 00:53:32 +0000

    add Rust Benchmark (cargo) benchmark result for fe67c68dcbb6996ac9d68fad4c05946f76a0342d

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640759673202,
+  "lastUpdate": 1640825612114,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17279,6 +17279,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 34394139,
             "range": "± 1312002",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe67c68dcbb6996ac9d68fad4c05946f76a0342d",
+          "message": "Add categories to yoke and zerovec (#1451)\n\n* Add categories to yoke and zerovec\n\n* data-structures",
+          "timestamp": "2021-12-29T16:45:15-08:00",
+          "tree_id": "964a4d62e164f9e759154f7dd9012170e7ac2991",
+          "url": "https://github.com/unicode-org/icu4x/commit/fe67c68dcbb6996ac9d68fad4c05946f76a0342d"
+        },
+        "date": 1640825602082,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 37873791,
+            "range": "± 1478945",
             "unit": "ns/iter"
           }
         ]
