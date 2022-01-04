3246dc156aa212bc7877a179abdbbcce518d94ab
github-action-benchmark <github@users.noreply.github.com>
2022-01-04 00:27:34 +0000

    add Rust Benchmark (cargo) benchmark result for 9fdbd62e6de1f46f83248533955905a1bca167ad

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641204224382,
+  "lastUpdate": 1641256054710,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20369,6 +20369,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4437,
             "range": "± 142",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9fdbd62e6de1f46f83248533955905a1bca167ad",
+          "message": "Add ForkByKeyProvider, first multi-source data provider (#1463)\n\n* Update HelloWorldProvider and add docs to BufferProvider\r\n* Support BufferProvider in filters",
+          "timestamp": "2022-01-03T18:19:25-06:00",
+          "tree_id": "3bbea7b75819f81bea66ba9e8da9d85e0440113c",
+          "url": "https://github.com/unicode-org/icu4x/commit/9fdbd62e6de1f46f83248533955905a1bca167ad"
+        },
+        "date": 1641256045098,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 3156,
+            "range": "± 229",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4927,
+            "range": "± 293",
             "unit": "ns/iter"
           }
         ]
