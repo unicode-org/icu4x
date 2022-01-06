74b25932aa8e41212e4143257087ea021003f426
github-action-benchmark <github@users.noreply.github.com>
2022-01-06 01:13:07 +0000

    add Rust Benchmark (cargo) benchmark result for 0521917d5d6d75b927fd40bd3462a6275bde94e5

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641256054710,
+  "lastUpdate": 1641431587238,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20405,6 +20405,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4927,
             "range": "± 293",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41129501+gnrunge@users.noreply.github.com",
+            "name": "Norbert Runge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0521917d5d6d75b927fd40bd3462a6275bde94e5",
+          "message": "Remove almost empty and unused package-lock.json file from root directory. (#1469)",
+          "timestamp": "2022-01-05T17:04:02-08:00",
+          "tree_id": "7a0cff1cfa1f315add0e480dc6dc63da3bb4f6e7",
+          "url": "https://github.com/unicode-org/icu4x/commit/0521917d5d6d75b927fd40bd3462a6275bde94e5"
+        },
+        "date": 1641431578914,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2428,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4154,
+            "range": "± 6",
             "unit": "ns/iter"
           }
         ]
