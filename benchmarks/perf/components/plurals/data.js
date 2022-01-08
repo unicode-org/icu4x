dac99071d8aa7d3d5142182873f57edd1009f127
github-action-benchmark <github@users.noreply.github.com>
2022-01-08 02:55:13 +0000

    add Rust Benchmark (cargo) benchmark result for 89ce5f09b461ce302c4c7372e82a482a61007bbb

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641604808405,
+  "lastUpdate": 1641610513107,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23435,6 +23435,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 19955,
             "range": "± 47",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "samchen61661@gmail.com",
+            "name": "samchen",
+            "username": "samchen61661"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "89ce5f09b461ce302c4c7372e82a482a61007bbb",
+          "message": "Retrieve localized metazone info from timeZoneNames.json (#1354)\n\nAdds functionality and data to have local overrides for metazone formatting, such as Europe/London formatting preferring to use \"British Summer Time\" for its \"daylight\" variant, instead of the default GMT metazone formatting.",
+          "timestamp": "2022-01-07T18:45:48-08:00",
+          "tree_id": "03c6822a228b060905d833db1f231b3de5bd7881",
+          "url": "https://github.com/unicode-org/icu4x/commit/89ce5f09b461ce302c4c7372e82a482a61007bbb"
+        },
+        "date": 1641610503023,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1329,
+            "range": "± 73",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6741,
+            "range": "± 391",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 25450,
+            "range": "± 1998",
             "unit": "ns/iter"
           }
         ]
