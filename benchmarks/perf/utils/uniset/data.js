e347ce0cea5e2097f90270ce6ee6a7d8973793f6
github-action-benchmark <github@users.noreply.github.com>
2022-01-08 00:56:44 +0000

    add Rust Benchmark (cargo) benchmark result for 96601c93b793a604608243be81cb10b44dc9f95a

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641602857536,
+  "lastUpdate": 1641603404354,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17699,6 +17699,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 36345669,
             "range": "± 112053",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "96601c93b793a604608243be81cb10b44dc9f95a",
+          "message": "Add FFI for constructing fixed decimals from float (#1483)\n\n* Expose fixed decimal construction functions from f32\r\n\r\n* float -> f64\r\n\r\n* regen\r\n\r\n* update method\r\n\r\n* regen\r\n\r\n* Update names",
+          "timestamp": "2022-01-07T16:49:05-08:00",
+          "tree_id": "eada95e71c75e562da4f2940079c985972bea299",
+          "url": "https://github.com/unicode-org/icu4x/commit/96601c93b793a604608243be81cb10b44dc9f95a"
+        },
+        "date": 1641603394182,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 37360126,
+            "range": "± 1705181",
             "unit": "ns/iter"
           }
         ]
