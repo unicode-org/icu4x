44b9acfcaf33045e907fa23370e276a77db60e90
Shane F. Carr <shane@unicode.org>
2022-01-11 00:59:49 -0800

    More AnyProvider

--- provider/core/src/any.rs
+++ provider/core/src/any.rs
@@ -9,11 +9,13 @@ use alloc::rc::Rc;
 use core::any::Any;
 use yoke::trait_hack::YokeTraitHack;
 use yoke::Yokeable;
+use yoke::ZeroCopyFrom;
 
 /// Representations of the `Any` trait object.
 ///
 /// **Important Note:** The types enclosed by `StructRef` and `PayloadRc` are NOT the same!
 /// The first refers to the struct itself, whereas the second refers to a `DataPayload`.
+#[derive(Debug, Clone)]
 enum AnyPayloadInner {
     /// A reference to `M::Yokeable`
     StructRef(&'static dyn Any),
@@ -22,11 +24,13 @@ enum AnyPayloadInner {
 }
 
 /// A type-erased data payload.
+#[derive(Debug, Clone)]
 pub struct AnyPayload {
     inner: AnyPayloadInner,
 }
 
 impl AnyPayload {
+    /// Transforms a type-erased `AnyPayload` to a concrete `DataPayload`.
     pub fn downcast<M>(self) -> Result<DataPayload<M>, DataError>
     where
         M: DataMarker + 'static,
@@ -39,7 +43,11 @@ impl AnyPayload {
                 let down_ref: &'static M::Yokeable = any_ref.downcast_ref().ok_or_else(|| {
                     DataErrorKind::MismatchedType(any_ref.type_id()).with_type_context::<M>()
                 })?;
-                // TODO: ZCC
+                // TODO: Use ZeroCopyFrom here.
+                // Although the Clone impl is expected to perform a zero-copy clone since we have
+                // a reference to a const-constructed object, (1) there is no guarantee that it
+                // does not clone, and (2) ZeroCopyFrom only builds the zero-copy code, not the
+                // potentially heavier full-clone code.
                 Ok(DataPayload::from_owned(down_ref.clone()))
             }
             PayloadRc(any_rc) => {
@@ -50,4 +58,45 @@ impl AnyPayload {
             }
         }
     }
+}
+
+impl<M> DataPayload<M>
+where
+    M: DataMarker + 'static,
+{
+    pub fn into_any_payload(self) -> AnyPayload {
+        AnyPayload {
+            inner: AnyPayloadInner::PayloadRc(Rc::from(self))
+        }
+    }
+}
+
+/// A response object containing an object as payload and metadata about it.
+#[derive(Debug, Clone)]
+pub struct AnyResponse {
+    /// Metadata about the returned object.
+    pub metadata: DataResponseMetadata,
+
+    /// The object itself; None if it was not loaded.
+    pub payload: Option<AnyPayload>,
+}
+
+impl AnyResponse {
+    /// Transforms a type-erased `AnyResponse` to a concrete `DataResponse`.
+    pub fn downcast<M>(self) -> Result<DataResponse<M>, DataError>
+    where
+        M: DataMarker + 'static,
+        M::Yokeable: Clone,
+        for<'a> YokeTraitHack<<M::Yokeable as Yokeable<'a>>::Output>: Clone,
+    {
+        Ok(DataResponse {
+            metadata: self.metadata,
+            payload: self.payload.map(|p| p.downcast()).transpose()?,
+        })
+    }
+}
+
+/// An object-safe data provider that returns Rust objects cast to `dyn Any` trait objects.
+pub trait AnyProvider {
+    fn load_buffer(&self, req: &DataRequest) -> Result<AnyResponse, DataError>;
 }
