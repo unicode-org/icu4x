d0384090708705cc047bb933704638f1d85fbd48
github-action-benchmark <github@users.noreply.github.com>
2022-01-11 21:09:16 +0000

    add Rust Benchmark (cargo) benchmark result for 8b26c199bcc07cb383ccbe6a41772e6fce714bc4

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641935322502,
+  "lastUpdate": 1641935356712,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -16829,6 +16829,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 137534,
             "range": "± 1413",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8b26c199bcc07cb383ccbe6a41772e6fce714bc4",
+          "message": "Make most Yokeable impls inline (#1493)",
+          "timestamp": "2022-01-11T13:02:02-08:00",
+          "tree_id": "0e2a30c9c0739e991de1733b2e4b7dabcc4a48ce",
+          "url": "https://github.com/unicode-org/icu4x/commit/8b26c199bcc07cb383ccbe6a41772e6fce714bc4"
+        },
+        "date": 1641935347211,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 114871,
+            "range": "± 8579",
             "unit": "ns/iter"
           }
         ]
