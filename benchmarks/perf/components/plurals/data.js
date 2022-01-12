eab16b42efd61a71c624ae7d1b8b1fbbfe9af7d6
github-action-benchmark <github@users.noreply.github.com>
2022-01-12 23:00:16 +0000

    add Rust Benchmark (cargo) benchmark result for e8c682dcf1ac02d74e567c166c03bd354b179612

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642027855149,
+  "lastUpdate": 1642028416380,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23939,6 +23939,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 18737,
             "range": "± 1309",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e8c682dcf1ac02d74e567c166c03bd354b179612",
+          "message": "Update diplomat (#1502)\n\n* Update diplomat\r\n\r\n* regen",
+          "timestamp": "2022-01-12T14:50:48-08:00",
+          "tree_id": "fe5db1d4389a6a04eceeb518b3646934c40c5842",
+          "url": "https://github.com/unicode-org/icu4x/commit/e8c682dcf1ac02d74e567c166c03bd354b179612"
+        },
+        "date": 1642028402616,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1261,
+            "range": "± 70",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6527,
+            "range": "± 284",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 21275,
+            "range": "± 1312",
             "unit": "ns/iter"
           }
         ]
