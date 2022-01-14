ec15036114829af81577fd94c54eea26304ef8dc
github-action-benchmark <github@users.noreply.github.com>
2022-01-14 04:01:14 +0000

    add Benchmark (ndjson) benchmark result for f13e0948790b0f7adf52adf6d700c2953eeddaa9

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642046916579,
+  "lastUpdate": 1642132874665,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -7193,6 +7193,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/05235a6119b6ecd5b3aae2d55e328879950376c6"
         },
         "date": 1642046908462,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 1472057,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f13e0948790b0f7adf52adf6d700c2953eeddaa9",
+          "message": "Rewrite ErasedDataProvider as AnyProvider (#1495)",
+          "timestamp": "2022-01-13T21:59:49-06:00",
+          "tree_id": "2896593f232beb400d779d553b9a640eaa53d43b",
+          "url": "https://github.com/unicode-org/icu4x/commit/f13e0948790b0f7adf52adf6d700c2953eeddaa9"
+        },
+        "date": 1642132866518,
         "tool": "ndjson",
         "benches": [
           {
