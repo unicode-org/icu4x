b611570cf56c0a57bfae7e84518e2eebe4cbf570
github-action-benchmark <github@users.noreply.github.com>
2022-01-14 04:06:59 +0000

    add Rust Benchmark (cargo) benchmark result for f13e0948790b0f7adf52adf6d700c2953eeddaa9

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642047268801,
+  "lastUpdate": 1642133219662,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -18179,6 +18179,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 43447592,
             "range": "± 588209",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f13e0948790b0f7adf52adf6d700c2953eeddaa9",
+          "message": "Rewrite ErasedDataProvider as AnyProvider (#1495)",
+          "timestamp": "2022-01-13T21:59:49-06:00",
+          "tree_id": "2896593f232beb400d779d553b9a640eaa53d43b",
+          "url": "https://github.com/unicode-org/icu4x/commit/f13e0948790b0f7adf52adf6d700c2953eeddaa9"
+        },
+        "date": 1642133211151,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 28584818,
+            "range": "± 1856892",
             "unit": "ns/iter"
           }
         ]
