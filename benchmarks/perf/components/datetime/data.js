95ffeee06872c13d8567abe9165a17275d000531
github-action-benchmark <github@users.noreply.github.com>
2022-01-15 06:46:03 +0000

    add Rust Benchmark (cargo) benchmark result for a0c80db85e5157535a2f4fc591bc0e93a653fd16

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642206369898,
+  "lastUpdate": 1642229163203,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17735,6 +17735,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 365115,
             "range": "± 10091",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a0c80db85e5157535a2f4fc591bc0e93a653fd16",
+          "message": "Improve Writeable impl for ResourceOptions (#1510)",
+          "timestamp": "2022-01-15T00:37:37-06:00",
+          "tree_id": "2b7d7231d3b1386be7690942f8ed5d21b8e83617",
+          "url": "https://github.com/unicode-org/icu4x/commit/a0c80db85e5157535a2f4fc591bc0e93a653fd16"
+        },
+        "date": 1642229154762,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 86687,
+            "range": "± 658",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 941116,
+            "range": "± 1086",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 317379,
+            "range": "± 456",
             "unit": "ns/iter"
           }
         ]
