d579ad95ea71f60282420daebac6137617ed8e9a
github-action-benchmark <github@users.noreply.github.com>
2022-01-15 06:45:50 +0000

    add Rust Benchmark (cargo) benchmark result for a0c80db85e5157535a2f4fc591bc0e93a653fd16

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642206359002,
+  "lastUpdate": 1642229150226,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -21269,6 +21269,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4497,
             "range": "± 259",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a0c80db85e5157535a2f4fc591bc0e93a653fd16",
+          "message": "Improve Writeable impl for ResourceOptions (#1510)",
+          "timestamp": "2022-01-15T00:37:37-06:00",
+          "tree_id": "2b7d7231d3b1386be7690942f8ed5d21b8e83617",
+          "url": "https://github.com/unicode-org/icu4x/commit/a0c80db85e5157535a2f4fc591bc0e93a653fd16"
+        },
+        "date": 1642229140654,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 1958,
+            "range": "± 109",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4278,
+            "range": "± 301",
             "unit": "ns/iter"
           }
         ]
