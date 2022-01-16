b4be693523b76642f3d9bd66427020a434cdfdc6
github-action-benchmark <github@users.noreply.github.com>
2022-01-16 19:31:51 +0000

    add Rust Benchmark (cargo) benchmark result for d1e9add0a88b06e73c025bb972e2485b4fd269d9

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642229077279,
+  "lastUpdate": 1642361511853,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17189,6 +17189,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 96569,
             "range": "± 334",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d1e9add0a88b06e73c025bb972e2485b4fd269d9",
+          "message": "Add handling of leading zeroes to truncate_left() (#1507)\n\n* Add handling of leading zeroes to truncate_left()\n\n* rm extra condition",
+          "timestamp": "2022-01-16T11:24:33-08:00",
+          "tree_id": "4deebba894862cb09892d9ff8ce33877d35d7ac2",
+          "url": "https://github.com/unicode-org/icu4x/commit/d1e9add0a88b06e73c025bb972e2485b4fd269d9"
+        },
+        "date": 1642361502527,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 92063,
+            "range": "± 6835",
             "unit": "ns/iter"
           }
         ]
