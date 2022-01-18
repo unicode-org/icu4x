602bac798b11cb99c5807c9689e23beb7be3d0b6
github-action-benchmark <github@users.noreply.github.com>
2022-01-18 18:04:25 +0000

    add Benchmark (ndjson) benchmark result for 30a3f07f203328ab54d54aade2e64fcac4b9730c

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642461175662,
+  "lastUpdate": 1642529064854,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -7343,6 +7343,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/521ac66e80c6ddade400d9a8c2a2b626dd535285"
         },
         "date": 1642461165475,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 1472057,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "30a3f07f203328ab54d54aade2e64fcac4b9730c",
+          "message": "Add tinystr-neo to experimental/ (#1508)\n\n* Add tinystr_neo experimental crate\r\n\r\n* Basic TinyAsciiStr type\r\n\r\n* Add serde impls\r\n\r\n* Add tests for serde\r\n\r\n* fix var name\r\n\r\n* rm bytes\r\n\r\n* fmt\r\n\r\n* tidy\r\n\r\n* Make macro panic at compile time\r\n\r\n* clippy\r\n\r\n* move panic\r\n\r\n* clip",
+          "timestamp": "2022-01-18T10:02:35-08:00",
+          "tree_id": "0dc986cb8a4646323a529c63a6bdff44f7b54404",
+          "url": "https://github.com/unicode-org/icu4x/commit/30a3f07f203328ab54d54aade2e64fcac4b9730c"
+        },
+        "date": 1642529051607,
         "tool": "ndjson",
         "benches": [
           {
