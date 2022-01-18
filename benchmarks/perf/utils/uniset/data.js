e6b89bdd68f3d87a6168cea73003f76dbc56a311
github-action-benchmark <github@users.noreply.github.com>
2022-01-18 21:38:43 +0000

    add Rust Benchmark (cargo) benchmark result for 7c1a36c74a8ded45d533ff5d7d6919855fa05e3b

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642529449171,
+  "lastUpdate": 1642541923064,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -18359,6 +18359,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 44894423,
             "range": "± 99465",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7c1a36c74a8ded45d533ff5d7d6919855fa05e3b",
+          "message": "Fix cargo check --all-targets --no-default-features (#1514)",
+          "timestamp": "2022-01-18T13:30:49-08:00",
+          "tree_id": "d026ff0eaaed6a7aba28c35ef772f4d7905fbb86",
+          "url": "https://github.com/unicode-org/icu4x/commit/7c1a36c74a8ded45d533ff5d7d6919855fa05e3b"
+        },
+        "date": 1642541913695,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 48205169,
+            "range": "± 2150078",
             "unit": "ns/iter"
           }
         ]
