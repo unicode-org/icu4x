e39451d840086fee831c72269ee4d18aff74c9fa
github-action-benchmark <github@users.noreply.github.com>
2022-01-18 23:43:31 +0000

    add Benchmark (ndjson) benchmark result for 55f95e12c3f5bdc05784ee9d2adafc029c697e8f

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642544515424,
+  "lastUpdate": 1642549411394,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -7433,6 +7433,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/6cc59be97eee8512b4e75f02817ef78539174a86"
         },
         "date": 1642544504157,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 1472057,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sffc@google.com",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "55f95e12c3f5bdc05784ee9d2adafc029c697e8f",
+          "message": "Add fxhash_32 (#1504)",
+          "timestamp": "2022-01-18T15:41:43-08:00",
+          "tree_id": "bd47416425bc015198eee3904cd2d2d5a0b84f22",
+          "url": "https://github.com/unicode-org/icu4x/commit/55f95e12c3f5bdc05784ee9d2adafc029c697e8f"
+        },
+        "date": 1642549402269,
         "tool": "ndjson",
         "benches": [
           {
