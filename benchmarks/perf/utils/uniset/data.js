cff6aad47941d3722869ffbcaa08e27ed3f807f8
github-action-benchmark <github@users.noreply.github.com>
2022-01-20 17:47:01 +0000

    add Rust Benchmark (cargo) benchmark result for 78176585a0a5390348b5163573d5b60df5dfd280

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642696698973,
+  "lastUpdate": 1642700821775,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -18689,6 +18689,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 44702733,
             "range": "± 1873782",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "78176585a0a5390348b5163573d5b60df5dfd280",
+          "message": "Add ULE impls to tinystr-neo (#1524)",
+          "timestamp": "2022-01-20T09:37:41-08:00",
+          "tree_id": "bb19220dc4a8bb3727421bd1721182436d23fc9b",
+          "url": "https://github.com/unicode-org/icu4x/commit/78176585a0a5390348b5163573d5b60df5dfd280"
+        },
+        "date": 1642700810462,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 53788915,
+            "range": "± 271663",
             "unit": "ns/iter"
           }
         ]
