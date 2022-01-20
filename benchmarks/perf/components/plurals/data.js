e4b76ee53ace9390246f71e5f9e034508099feb8
github-action-benchmark <github@users.noreply.github.com>
2022-01-20 17:49:04 +0000

    add Rust Benchmark (cargo) benchmark result for 78176585a0a5390348b5163573d5b60df5dfd280

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642696886307,
+  "lastUpdate": 1642700944292,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -24737,6 +24737,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 16266,
             "range": "± 897",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "78176585a0a5390348b5163573d5b60df5dfd280",
+          "message": "Add ULE impls to tinystr-neo (#1524)",
+          "timestamp": "2022-01-20T09:37:41-08:00",
+          "tree_id": "bb19220dc4a8bb3727421bd1721182436d23fc9b",
+          "url": "https://github.com/unicode-org/icu4x/commit/78176585a0a5390348b5163573d5b60df5dfd280"
+        },
+        "date": 1642700933874,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1081,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6415,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 16075,
+            "range": "± 31",
             "unit": "ns/iter"
           }
         ]
