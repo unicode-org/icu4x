19ffbf45c442eb2d8e3ad62ef72f4956f522770b
github-action-benchmark <github@users.noreply.github.com>
2022-01-20 17:48:50 +0000

    add Rust Benchmark (cargo) benchmark result for 78176585a0a5390348b5163573d5b60df5dfd280

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642696760659,
+  "lastUpdate": 1642700930453,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -18365,6 +18365,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 314855,
             "range": "± 1434",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "78176585a0a5390348b5163573d5b60df5dfd280",
+          "message": "Add ULE impls to tinystr-neo (#1524)",
+          "timestamp": "2022-01-20T09:37:41-08:00",
+          "tree_id": "bb19220dc4a8bb3727421bd1721182436d23fc9b",
+          "url": "https://github.com/unicode-org/icu4x/commit/78176585a0a5390348b5163573d5b60df5dfd280"
+        },
+        "date": 1642700919685,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 114551,
+            "range": "± 3456",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1228343,
+            "range": "± 33306",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 418407,
+            "range": "± 15807",
             "unit": "ns/iter"
           }
         ]
