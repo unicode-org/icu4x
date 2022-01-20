20dc20fafeca763e0fa9b808200ecd223c1b8b53
Robert Bastian <robertbastian@users.noreply.github.com>
2022-01-20 13:45:41 +0100

    Datagen key file support and test scaffolding

--- tools/datagen/README.md
+++ tools/datagen/README.md
@@ -11,39 +11,39 @@ More details on each tool can be found by running `--help`.
 
 ## Examples
 
-Generate ICU4X JSON file tree:
+Generate ICU4X Postcard blob (single file) for all keys and all locales:
 
 ```bash
 # Run from the icu4x project folder
 $ cargo run --bin icu4x-datagen -- \
-   --cldr-tag 39.0.0 \
-   --all-keys \
-   --all-locales \
-   --out /tmp/icu4x_data/json
+    --cldr-tag 39.0.0 \
+    --all-keys \
+    --all-locales \
+    --format blob \
+    --out /tmp/icu4x_data/icu4x_data.postcard
 ```
 
-Generate ICU4X Postcard blob (single file):
+Extract the keys used by an executable into a key file:
 
 ```bash
 # Run from the icu4x project folder
-$ cargo run --bin icu4x-datagen -- \
-   --cldr-tag 39.0.0 \
-   --all-keys \
-   --all-locales \
-   --format blob \
-   --out /tmp/icu4x_data/icu4x_data.postcard
+$ cargo build --example work_log --release
+$ cargo make icu4x-key-extract \
+    target/release/examples/work_log \
+    /tmp/icu4x_data/work_log+keys.txt
+$ cat /tmp/icu4x_data/work_log+keys.txt
 ```
 
-Generate ICU4X Bincode file tree:
+Generate ICU4X JSON file tree from the key file for Spanish and German:
 
 ```bash
 # Run from the icu4x project folder
 $ cargo run --bin icu4x-datagen -- \
-   --cldr-tag 39.0.0 \
-   --all-keys \
-   --all-locales \
-   --syntax bincode \
-   --out /tmp/icu4x_data/bincode
+    --cldr-tag 39.0.0 \
+    --key-file /tmp/icu4x_data/work_log+keys.txt \
+    --locales es \
+    --locales de \
+    --out /tmp/icu4x_data/work_log_json
 ```
 
 ## More Information
--- tools/datagen/src/main.rs
+++ tools/datagen/src/main.rs
@@ -13,40 +13,40 @@
 //!
 //! # Examples
 //!
-//! Generate ICU4X JSON file tree:
+//! Generate ICU4X Postcard blob (single file) for all keys and all locales:
 //!
-//!```bash
-//!# Run from the icu4x project folder
-//!$ cargo run --bin icu4x-datagen -- \
-//!    --cldr-tag 39.0.0 \
-//!    --all-keys \
-//!    --all-locales \
-//!    --out /tmp/icu4x_data/json
-//!```
+//! ```bash
+//! # Run from the icu4x project folder
+//! $ cargo run --bin icu4x-datagen -- \
+//!     --cldr-tag 39.0.0 \
+//!     --all-keys \
+//!     --all-locales \
+//!     --format blob \
+//!     --out /tmp/icu4x_data/icu4x_data.postcard
+//! ```
 //!
-//! Generate ICU4X Postcard blob (single file):
+//! Extract the keys used by an executable into a key file:
 //!
-//!```bash
-//!# Run from the icu4x project folder
-//!$ cargo run --bin icu4x-datagen -- \
-//!    --cldr-tag 39.0.0 \
-//!    --all-keys \
-//!    --all-locales \
-//!    --format blob \
-//!    --out /tmp/icu4x_data/icu4x_data.postcard
-//!```
+//! ```bash
+//! # Run from the icu4x project folder
+//! $ cargo build --example work_log --release
+//! $ cargo make icu4x-key-extract \
+//!     target/release/examples/work_log \
+//!     /tmp/icu4x_data/work_log+keys.txt
+//! $ cat /tmp/icu4x_data/work_log+keys.txt
+//! ```
 //!
-//! Generate ICU4X Bincode file tree:
+//! Generate ICU4X JSON file tree from the key file for Spanish and German:
 //!
-//!```bash
-//!# Run from the icu4x project folder
-//!$ cargo run --bin icu4x-datagen -- \
-//!    --cldr-tag 39.0.0 \
-//!    --all-keys \
-//!    --all-locales \
-//!    --syntax bincode \
-//!    --out /tmp/icu4x_data/bincode
-//!```
+//! ```bash
+//! # Run from the icu4x project folder
+//! $ cargo run --bin icu4x-datagen -- \
+//!     --cldr-tag 39.0.0 \
+//!     --key-file /tmp/icu4x_data/work_log+keys.txt \
+//!     --locales es \
+//!     --locales de \
+//!     --out /tmp/icu4x_data/work_log_json
+//! ```
 
 fn main() {
     panic!("Please run a more specific binary")
