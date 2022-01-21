b11a4878fe7a737305dd8889b76e35bd959fd26c
github-action-benchmark <github@users.noreply.github.com>
2022-01-21 18:43:24 +0000

    add Rust Benchmark (cargo) benchmark result for 88970e95ff718ba0bd7988d6536f08f7ab0d1f22

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642785323116,
+  "lastUpdate": 1642790604808,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -17729,6 +17729,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 122508,
             "range": "± 1731",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "88970e95ff718ba0bd7988d6536f08f7ab0d1f22",
+          "message": "ListFormatter documentation and misc improvements (#1528)",
+          "timestamp": "2022-01-21T19:35:02+01:00",
+          "tree_id": "af17b4c11e152a7c6bf93a8630b4fddc7c59c516",
+          "url": "https://github.com/unicode-org/icu4x/commit/88970e95ff718ba0bd7988d6536f08f7ab0d1f22"
+        },
+        "date": 1642790593476,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 112625,
+            "range": "± 4396",
             "unit": "ns/iter"
           }
         ]
