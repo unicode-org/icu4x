6e6b50896f5c82ad29defd67fef383b057e5408a
github-action-benchmark <github@users.noreply.github.com>
2022-01-21 18:44:52 +0000

    add Rust Benchmark (cargo) benchmark result for 88970e95ff718ba0bd7988d6536f08f7ab0d1f22

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642785376883,
+  "lastUpdate": 1642790692108,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -18533,6 +18533,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 302374,
             "range": "± 618",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "88970e95ff718ba0bd7988d6536f08f7ab0d1f22",
+          "message": "ListFormatter documentation and misc improvements (#1528)",
+          "timestamp": "2022-01-21T19:35:02+01:00",
+          "tree_id": "af17b4c11e152a7c6bf93a8630b4fddc7c59c516",
+          "url": "https://github.com/unicode-org/icu4x/commit/88970e95ff718ba0bd7988d6536f08f7ab0d1f22"
+        },
+        "date": 1642790681431,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 109988,
+            "range": "± 7419",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1146610,
+            "range": "± 31858",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 387039,
+            "range": "± 14497",
             "unit": "ns/iter"
           }
         ]
