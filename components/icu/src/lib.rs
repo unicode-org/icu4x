a03b930bf11d41f011f4c0e7f6b7f970d0057ffd
Robert Bastian <robertbastian@users.noreply.github.com>
2022-01-21 21:10:42 +0100

    Stabilizing icu_list

--- components/icu/src/lib.rs
+++ components/icu/src/lib.rs
@@ -434,3 +434,48 @@ pub mod properties {
     //! [`maps`]: maps
     pub use icu_properties::*;
 }
+
+pub mod list {
+    //! List formatting
+    //!
+    //! This API provides the [`ListFormatter`] which renders sequences of [`Writeable`](
+    //! writeable::Writeable)s as lists in a locale-sensitive way.
+    //!
+    //! # Examples
+    //!
+    //! ## Format a list of strings in Spanish
+    //!
+    //! ```
+    //! use icu_list::{ListFormatter, ListType, ListStyle};
+    //! use icu_locid::Locale;
+    //! use icu_locid_macros::langid;
+    //! use writeable::Writeable;
+    //!
+    //! let locale: Locale = langid!("es").into();
+    //! let provider = icu_testdata::get_provider();
+    //! let list_formatter = ListFormatter::try_new(locale, &provider, ListType::And, ListStyle::Wide)
+    //!     .expect("Data should load successfully");
+    //!
+    //! assert_eq!(
+    //!     list_formatter.format(["España", "Suiza"].iter())
+    //!         .writeable_to_string(),
+    //!     "España y Suiza"
+    //! );
+    //!
+    //! // The Spanish 'y' sometimes becomes an 'e':
+    //! assert_eq!(
+    //!     list_formatter.format(["España", "Suiza", "Italia"].iter())
+    //!         .writeable_to_string(),
+    //!     "España, Suiza e Italia"
+    //! );
+    //!
+    //! // We can use any Writeables as inputs:
+    //! assert_eq!(
+    //!     list_formatter.format(1..=10).writeable_to_string(),
+    //!     "1, 2, 3, 4, 5, 6, 7, 8, 9 y 10"
+    //! );
+    //!```
+    //!
+    //! [`ListFormatter`]: ListFormatter
+    pub use icu_list::*;
+}
