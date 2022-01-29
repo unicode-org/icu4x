d45b7b23bb19ce05da321d8c81cd43d5bca0f1e6
github-action-benchmark <github@users.noreply.github.com>
2022-01-29 01:03:46 +0000

    add Rust Benchmark (cargo) benchmark result for ededcd21953402b28815ee8ba0a62e29e820826e

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643417183323,
+  "lastUpdate": 1643418226650,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -25493,6 +25493,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 13291,
             "range": "± 745",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ededcd21953402b28815ee8ba0a62e29e820826e",
+          "message": "Add design doc for zerovec (#1537)\n\n* Add design doc for zerovec\n\n* Update utils/zerovec/design_doc.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update utils/zerovec/design_doc.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update utils/zerovec/design_doc.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update utils/zerovec/design_doc.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update utils/zerovec/design_doc.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update utils/zerovec/design_doc.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update utils/zerovec/design_doc.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* Update utils/zerovec/design_doc.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* fix get_copied\n\n* mention repr rust\n\n* Update utils/zerovec/design_doc.md\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>\n\n* repr c\n\nCo-authored-by: Shane F. Carr <shane@unicode.org>",
+          "timestamp": "2022-01-28T16:52:50-08:00",
+          "tree_id": "ff65a8b9f977e29fc81621a12e08596c95173521",
+          "url": "https://github.com/unicode-org/icu4x/commit/ededcd21953402b28815ee8ba0a62e29e820826e"
+        },
+        "date": 1643418215008,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 1029,
+            "range": "± 32",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6903,
+            "range": "± 367",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 15614,
+            "range": "± 1542",
             "unit": "ns/iter"
           }
         ]
