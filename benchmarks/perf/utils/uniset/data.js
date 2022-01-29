cfb4f2d8b238f832546d8d8fa46ccfcca1ee6715
github-action-benchmark <github@users.noreply.github.com>
2022-01-29 00:44:33 +0000

    add Rust Benchmark (cargo) benchmark result for bc6c319fb288310c5f6a70bd0fc067f697e794d0

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643408231462,
+  "lastUpdate": 1643417073197,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19199,6 +19199,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 46013049,
             "range": "± 1865953",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@google.com",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bc6c319fb288310c5f6a70bd0fc067f697e794d0",
+          "message": "Add fn to return UnicodeSet for Script_Extensions (#1529)",
+          "timestamp": "2022-01-28T16:35:59-08:00",
+          "tree_id": "9b60c61c22dab1728f127aa2f922818158293a30",
+          "url": "https://github.com/unicode-org/icu4x/commit/bc6c319fb288310c5f6a70bd0fc067f697e794d0"
+        },
+        "date": 1643417061931,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 54880651,
+            "range": "± 934215",
             "unit": "ns/iter"
           }
         ]
