7c82fc44617b9cd5d2919a1d16108bc1deb628cd
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2022-01-31 11:45:45 +0900

    Remove unused code

--- experimental/segmenter/src/lib.rs
+++ experimental/segmenter/src/lib.rs
@@ -133,13 +133,6 @@
 //! let breakpoints: Vec<usize> = segmenter.segment_latin1(b"Hello World").collect();
 //! assert_eq!(&breakpoints, &[0, 11]);
 //! ```
-//!
-//! # Generating property table
-//!
-//! Copy the following files to `tools` directory. Then run `./generate_properties.py` in `tools` directory (requires Python 3.8+). Machine generated files are moved to `src` directory.
-//! - <https://www.unicode.org/Public/UCD/latest/ucd/LineBreak.txt>
-//! - <https://www.unicode.org/Public/UCD/latest/ucd/EastAsianWidth.txt>
-//! - <https://www.unicode.org/Public/UCD/latest/ucd/emoji/emoji-data.txt>
 
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
 
