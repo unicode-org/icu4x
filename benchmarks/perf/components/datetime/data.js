f53fa5b141e5f687ac51b0f0923bc9cafdc33991
github-action-benchmark <github@users.noreply.github.com>
2022-02-02 23:59:15 +0000

    add Rust Benchmark (cargo) benchmark result for 10f3d2de502f82807d45ed5b5c2c3f2f853c7543

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643836956197,
+  "lastUpdate": 1643846355233,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19667,6 +19667,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 255742,
             "range": "± 382",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "10f3d2de502f82807d45ed5b5c2c3f2f853c7543",
+          "message": "Use ResourceHash in blob data provider (#1574)\n\n* Add ULE impls for ResourceHash\r\n\r\n* Update blob data providers to use ResourceKeyHash\r\n\r\n* Fix VZV integrity check",
+          "timestamp": "2022-02-02T15:50:15-08:00",
+          "tree_id": "e10e69aaeff72168c67899a70dc8565c7fa60b1d",
+          "url": "https://github.com/unicode-org/icu4x/commit/10f3d2de502f82807d45ed5b5c2c3f2f853c7543"
+        },
+        "date": 1643846344335,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 71087,
+            "range": "± 52",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 759534,
+            "range": "± 1827",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 271174,
+            "range": "± 3172",
             "unit": "ns/iter"
           }
         ]
