eda94680fabb05184325a3b2974e7ca40cad3aae
github-action-benchmark <github@users.noreply.github.com>
2022-02-02 23:51:56 +0000

    add Benchmark (ndjson) benchmark result for 10f3d2de502f82807d45ed5b5c2c3f2f853c7543

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643836541648,
+  "lastUpdate": 1643845916205,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -8638,6 +8638,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "provider/testdata/data/testdata.postcard",
             "value": 1575756,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "10f3d2de502f82807d45ed5b5c2c3f2f853c7543",
+          "message": "Use ResourceHash in blob data provider (#1574)\n\n* Add ULE impls for ResourceHash\r\n\r\n* Update blob data providers to use ResourceKeyHash\r\n\r\n* Fix VZV integrity check",
+          "timestamp": "2022-02-02T15:50:15-08:00",
+          "tree_id": "e10e69aaeff72168c67899a70dc8565c7fa60b1d",
+          "url": "https://github.com/unicode-org/icu4x/commit/10f3d2de502f82807d45ed5b5c2c3f2f853c7543"
+        },
+        "date": 1643845905664,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 1571719,
             "unit": "bytes",
             "biggerIsBetter": false
           }
