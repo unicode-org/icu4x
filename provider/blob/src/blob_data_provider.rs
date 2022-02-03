d566bb37bed913eebbb4778f433b6ceea6e98fd8
Robert Bastian <robertbastian@users.noreply.github.com>
2022-02-03 19:54:31 +0100

    blob

--- provider/blob/src/blob_data_provider.rs
+++ provider/blob/src/blob_data_provider.rs
@@ -138,7 +138,7 @@ impl IterableProvider for BlobDataProvider {
         resc_key: &ResourceKey,
     ) -> Result<Box<dyn Iterator<Item = ResourceOptions> + '_>, DataError> {
         Ok(match self.data.get().iter_keys1(resc_key.get_path()) {
-            Some(iter) => Box::new(iter.map(ResourceOptions::from_str)),
+            Some(iter) => Box::new(iter.map(|s| ResourceOptions::from_str(s).unwrap())),
             None => Box::new(core::iter::empty()),
         })
     }
