c4dafec360b2372b5e32b69355bbdacb7d6adb58
github-action-benchmark <github@users.noreply.github.com>
2022-02-09 18:52:29 +0000

    add Rust Benchmark (cargo) benchmark result for 5ef9302855c9a1d8a02e99f2fe465ecf3fee1783

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644338411564,
+  "lastUpdate": 1644432749368,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23177,6 +23177,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 3691,
             "range": "± 3",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5ef9302855c9a1d8a02e99f2fe465ecf3fee1783",
+          "message": "Alternative List API (#1594)",
+          "timestamp": "2022-02-09T19:43:12+01:00",
+          "tree_id": "6287be375cebdf6df6c2dc432f27b260faa642d7",
+          "url": "https://github.com/unicode-org/icu4x/commit/5ef9302855c9a1d8a02e99f2fe465ecf3fee1783"
+        },
+        "date": 1644432736758,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2197,
+            "range": "± 474",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4732,
+            "range": "± 314",
             "unit": "ns/iter"
           }
         ]
