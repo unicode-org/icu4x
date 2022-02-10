7d4dfb7225d27f988707d71ef709bd8709468027
Robert Bastian <robertbastian@users.noreply.github.com>
2022-02-10 11:28:53 +0100

    Hiding PluralRulesV1Marker

--- components/plurals/src/provider.rs
+++ components/plurals/src/provider.rs
@@ -8,13 +8,13 @@
 
 use crate::rules::runtime::ast::Rule;
 use icu_provider::yoke;
+use icu_provider::DataMarker;
 
 /// Plural rule strings conforming to UTS 35 syntax. Includes separate fields for five of the six
 /// standard plural forms. If none of the rules match, the "other" category is assumed.
 ///
 /// More information: <https://unicode.org/reports/tr35/tr35-numbers.html#Language_Plural_Rules>
 #[icu_provider::data_struct(
-    PluralRulesV1Marker,
     CardinalV1Marker = "plurals/cardinal@1",
     OrdinalV1Marker = "plurals/ordinal@1"
 )]
@@ -35,4 +35,10 @@ pub struct PluralRulesV1<'data> {
     pub few: Option<Rule<'data>>,
     #[cfg_attr(feature = "provider_serde", serde(borrow))]
     pub many: Option<Rule<'data>>,
+}
+
+pub(crate) struct ErasedPluralRulesV1Marker;
+
+impl DataMarker for ErasedPluralRulesV1Marker {
+    type Yokeable = PluralRulesV1<'static>;
 }
