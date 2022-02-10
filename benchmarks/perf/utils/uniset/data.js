82b237928c33eda7ea3ffd702aa524a5157ba34b
github-action-benchmark <github@users.noreply.github.com>
2022-02-10 16:17:46 +0000

    add Rust Benchmark (cargo) benchmark result for bede0608eb784f6b93dbf3d280d7553e41b73e7e

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644484540286,
+  "lastUpdate": 1644509866591,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19949,6 +19949,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 47469546,
             "range": "± 115919",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bede0608eb784f6b93dbf3d280d7553e41b73e7e",
+          "message": "Make tinystr publishable (#1597)",
+          "timestamp": "2022-02-10T08:09:30-08:00",
+          "tree_id": "f313183d63d10525d92338549c26c8e0d2a16e1b",
+          "url": "https://github.com/unicode-org/icu4x/commit/bede0608eb784f6b93dbf3d280d7553e41b73e7e"
+        },
+        "date": 1644509854937,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 47560299,
+            "range": "± 72005",
             "unit": "ns/iter"
           }
         ]
