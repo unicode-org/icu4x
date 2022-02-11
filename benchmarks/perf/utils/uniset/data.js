ccc8a70595c896682963fbd9627e33b5a63491ca
github-action-benchmark <github@users.noreply.github.com>
2022-02-11 19:10:35 +0000

    add Rust Benchmark (cargo) benchmark result for 7b62eee1c222523f4d56f02798d7e2420fd74962

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644604980380,
+  "lastUpdate": 1644606635432,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20069,6 +20069,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 55907726,
             "range": "± 607359",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7b62eee1c222523f4d56f02798d7e2420fd74962",
+          "message": "UnicodeSet ZCF (#1604)",
+          "timestamp": "2022-02-11T20:01:48+01:00",
+          "tree_id": "2cefdbf994ba60e442e691cc3f9f43f51b0b9a4c",
+          "url": "https://github.com/unicode-org/icu4x/commit/7b62eee1c222523f4d56f02798d7e2420fd74962"
+        },
+        "date": 1644606625467,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 42067240,
+            "range": "± 60377",
             "unit": "ns/iter"
           }
         ]
