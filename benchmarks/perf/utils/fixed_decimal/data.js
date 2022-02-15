15a36d7dd6e95733102b1bb8972926feb4d46f05
github-action-benchmark <github@users.noreply.github.com>
2022-02-15 20:34:21 +0000

    add Rust Benchmark (cargo) benchmark result for 5d84e2f8eb80ce491c7a8d6cad9a7f2d38d3567a

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644952601695,
+  "lastUpdate": 1644957261074,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19259,6 +19259,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 102880,
             "range": "± 291",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5d84e2f8eb80ce491c7a8d6cad9a7f2d38d3567a",
+          "message": "Update the contributing docs after 2022-02-03 meeting (#1580)",
+          "timestamp": "2022-02-15T14:25:40-06:00",
+          "tree_id": "c9490aa0d3ebb82fe5c7c2ea4c1996b2d59f766d",
+          "url": "https://github.com/unicode-org/icu4x/commit/5d84e2f8eb80ce491c7a8d6cad9a7f2d38d3567a"
+        },
+        "date": 1644957248842,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 106064,
+            "range": "± 6681",
             "unit": "ns/iter"
           }
         ]
