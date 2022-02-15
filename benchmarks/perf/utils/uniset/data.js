939d3415d2858a3fa8f794b23b13ffe5c4930999
github-action-benchmark <github@users.noreply.github.com>
2022-02-15 17:32:42 +0000

    add Rust Benchmark (cargo) benchmark result for 9ca9780f230b903b2fae7581c4674015da7bdcd8

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644914761668,
+  "lastUpdate": 1644946362269,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20249,6 +20249,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 47648558,
             "range": "± 171908",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "elango@unicode.org",
+            "name": "Elango",
+            "username": "echeran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9ca9780f230b903b2fae7581c4674015da7bdcd8",
+          "message": "Add user-facing provider-aware API for Script_Extensions data result (#1593)",
+          "timestamp": "2022-02-15T09:23:42-08:00",
+          "tree_id": "f7a558832521232e3eb939993b15ce4558bd6425",
+          "url": "https://github.com/unicode-org/icu4x/commit/9ca9780f230b903b2fae7581c4674015da7bdcd8"
+        },
+        "date": 1644946350474,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 47245863,
+            "range": "± 323726",
             "unit": "ns/iter"
           }
         ]
