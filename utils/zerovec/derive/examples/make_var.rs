f2193e4cfdc0be9df1c58d7170a1700b7c09a57a
Manish Goregaokar <manishsmail@gmail.com>
2022-02-14 17:40:26 -0800

    Generate Encode

--- utils/zerovec/derive/examples/make_var.rs
+++ utils/zerovec/derive/examples/make_var.rs
@@ -10,9 +10,50 @@ use zerovec_derive::*;
 #[make_varule]
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 struct VarStruct<'a> {
-    a: u8,
+    a: u32,
     b: char,
     c: Cow<'a, str>,
 }
 
-fn main() {}
+fn main() {
+    use zerovec::ule::AsULE;
+    let varzerovec: VarZeroVec<VarStructULE> = TEST_VARSTRUCTS.into();
+
+    assert_eq!(varzerovec.len(), TEST_VARSTRUCTS.len());
+
+    for (stack, zero) in TEST_VARSTRUCTS.iter().zip(varzerovec.iter()) {
+        assert_eq!(zero.a, stack.a.as_unaligned());
+        assert_eq!(zero.b, stack.b.as_unaligned());
+        assert_eq!(zero.c, stack.c);
+    }
+
+    let bytes = varzerovec.as_bytes();
+    let reparsed: VarZeroVec<VarStructULE> = VarZeroVec::parse_byte_slice(bytes)
+        .unwrap_or_else(|_| panic!("{}", "Parsing VarZeroVec<VarStructULE> should succeed"));
+
+    assert_eq!(reparsed.len(), TEST_VARSTRUCTS.len());
+
+    for (stack, zero) in TEST_VARSTRUCTS.iter().zip(reparsed.iter()) {
+        assert_eq!(zero.a, stack.a.as_unaligned());
+        assert_eq!(zero.b, stack.b.as_unaligned());
+        assert_eq!(zero.c, stack.c);
+    }
+}
+
+const TEST_VARSTRUCTS: &[VarStruct<'static>] = &[
+    VarStruct {
+        a: 101,
+        b: 'ø',
+        c: Cow::Borrowed("testīng strīng"),
+    },
+    VarStruct {
+        a: 9499,
+        b: '⸘',
+        c: Cow::Borrowed("a diﬀərənt ştring"),
+    },
+    VarStruct {
+        a: 3478,
+        b: '月',
+        c: Cow::Borrowed("好多嘅 string"),
+    },
+];
