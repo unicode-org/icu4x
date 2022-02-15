fd8d137936c2559c643aa5065ed054e322febb70
github-action-benchmark <github@users.noreply.github.com>
2022-02-15 20:35:19 +0000

    add Rust Benchmark (cargo) benchmark result for 5d84e2f8eb80ce491c7a8d6cad9a7f2d38d3567a

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644952694153,
+  "lastUpdate": 1644957318943,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20675,6 +20675,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 170253,
             "range": "± 390",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gregtatum@users.noreply.github.com",
+            "name": "Greg Tatum",
+            "username": "gregtatum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5d84e2f8eb80ce491c7a8d6cad9a7f2d38d3567a",
+          "message": "Update the contributing docs after 2022-02-03 meeting (#1580)",
+          "timestamp": "2022-02-15T14:25:40-06:00",
+          "tree_id": "c9490aa0d3ebb82fe5c7c2ea4c1996b2d59f766d",
+          "url": "https://github.com/unicode-org/icu4x/commit/5d84e2f8eb80ce491c7a8d6cad9a7f2d38d3567a"
+        },
+        "date": 1644957307576,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 73079,
+            "range": "± 681",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 738681,
+            "range": "± 2993",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 169911,
+            "range": "± 318",
             "unit": "ns/iter"
           }
         ]
