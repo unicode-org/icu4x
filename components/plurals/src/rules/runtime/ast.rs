b6270554c89c861d38f69eebad572660bf02f198
Manish Goregaokar <manishsmail@gmail.com>
2022-02-15 14:57:23 -0800

    fmt

--- components/plurals/src/rules/runtime/ast.rs
+++ components/plurals/src/rules/runtime/ast.rs
@@ -13,7 +13,7 @@ use core::{
 use icu_provider::{yoke, zerofrom};
 use num_enum::{IntoPrimitive, TryFromPrimitive, UnsafeFromPrimitive};
 use zerovec::{
-    ule::{EncodeAsVarULE, AsULE, PairULE, RawBytesULE, VarULE, ZeroVecError, ULE},
+    ule::{AsULE, EncodeAsVarULE, PairULE, RawBytesULE, VarULE, ZeroVecError, ULE},
     {VarZeroVec, ZeroVec},
 };
 
