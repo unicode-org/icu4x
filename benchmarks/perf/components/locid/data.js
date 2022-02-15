4292b2b8caf96a49316b0f3530cc52a5ec6aaceb
github-action-benchmark <github@users.noreply.github.com>
2022-02-15 19:16:57 +0000

    add Rust Benchmark (cargo) benchmark result for df10af0932e903d97d1d1bd6da808fd2f0735243

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644952489017,
+  "lastUpdate": 1644952617611,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23753,6 +23753,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4989,
             "range": "± 255",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "df10af0932e903d97d1d1bd6da808fd2f0735243",
+          "message": "Move ZeroCopyFrom to its own crate, rename to ZeroFrom (#1610)",
+          "timestamp": "2022-02-15T11:08:18-08:00",
+          "tree_id": "2d682d83898ec78e175e11843ea1bd0ca0852d93",
+          "url": "https://github.com/unicode-org/icu4x/commit/df10af0932e903d97d1d1bd6da808fd2f0735243"
+        },
+        "date": 1644952607120,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 1996,
+            "range": "± 12",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 3646,
+            "range": "± 4",
             "unit": "ns/iter"
           }
         ]
