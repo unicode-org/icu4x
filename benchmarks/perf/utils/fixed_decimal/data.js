89d9c75131149a006bb9d22019b4808652924b1a
github-action-benchmark <github@users.noreply.github.com>
2022-02-16 17:35:31 +0000

    add Rust Benchmark (cargo) benchmark result for 68e051ab78f262caa9223614874f777b055918d0

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644988411368,
+  "lastUpdate": 1645032931639,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19349,6 +19349,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 123435,
             "range": "± 561",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "68e051ab78f262caa9223614874f777b055918d0",
+          "message": "Removing `KeyedDataProvider` from CLDR (#1599)",
+          "timestamp": "2022-02-16T18:26:24+01:00",
+          "tree_id": "bb867d24548961f0133a1ffce9e5e2d0acbd832f",
+          "url": "https://github.com/unicode-org/icu4x/commit/68e051ab78f262caa9223614874f777b055918d0"
+        },
+        "date": 1645032917851,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 111126,
+            "range": "± 3034",
             "unit": "ns/iter"
           }
         ]
