9f653c9ab983f7037f45b94bac0dfa1530ded8a0
github-action-benchmark <github@users.noreply.github.com>
2022-02-17 18:03:17 +0000

    add Rust Benchmark (cargo) benchmark result for 70f0f5eed936e206d225c26bcdf2157982518c27

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645076624359,
+  "lastUpdate": 1645120996960,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20927,6 +20927,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 215219,
             "range": "± 13925",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "70f0f5eed936e206d225c26bcdf2157982518c27",
+          "message": "Rename as_unaligned to to_unaligned (#1619)\n\n* as_unaligned -> to_unaligned\r\n\r\n* everywhere else",
+          "timestamp": "2022-02-17T09:53:01-08:00",
+          "tree_id": "aabd94d5df812967a54c6949213ed053f13209e8",
+          "url": "https://github.com/unicode-org/icu4x/commit/70f0f5eed936e206d225c26bcdf2157982518c27"
+        },
+        "date": 1645120983786,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 96288,
+            "range": "± 8251",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 978064,
+            "range": "± 92697",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 221272,
+            "range": "± 16413",
             "unit": "ns/iter"
           }
         ]
