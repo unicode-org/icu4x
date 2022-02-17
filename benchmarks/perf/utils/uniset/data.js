8d8f3fc2c66d9b4ff3ec8bb96e6d99d920260577
github-action-benchmark <github@users.noreply.github.com>
2022-02-17 05:42:01 +0000

    add Rust Benchmark (cargo) benchmark result for 412119d18e61f533c33b3526c42cefa39fefb3c4

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645048969232,
+  "lastUpdate": 1645076521047,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20489,6 +20489,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 49522454,
             "range": "± 2614086",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "412119d18e61f533c33b3526c42cefa39fefb3c4",
+          "message": "Add encode_varule_to_box(), clean up organization of `custom` module (#1612)\n\n* Add encode_to_box\n\n* Move EncodeAsVar out of custom::\n\n* Improve docs a bit\n\n* make it cfg(doc)\n\n* fmt\n\n* fix\n\n* add into_boxed_slice\n\n* review\n\n* fmt",
+          "timestamp": "2022-02-16T21:33:46-08:00",
+          "tree_id": "a9d9fabfbd1b9bbd056bbcec66d9575942e0cb5e",
+          "url": "https://github.com/unicode-org/icu4x/commit/412119d18e61f533c33b3526c42cefa39fefb3c4"
+        },
+        "date": 1645076509758,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 45775995,
+            "range": "± 3274125",
             "unit": "ns/iter"
           }
         ]
