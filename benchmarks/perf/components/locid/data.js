8c9a05ee3559b65cb24fa9cc5d23d2f00e575568
github-action-benchmark <github@users.noreply.github.com>
2022-02-17 05:42:29 +0000

    add Rust Benchmark (cargo) benchmark result for 412119d18e61f533c33b3526c42cefa39fefb3c4

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645048963353,
+  "lastUpdate": 1645076549249,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23969,6 +23969,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4092,
             "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "412119d18e61f533c33b3526c42cefa39fefb3c4",
+          "message": "Add encode_varule_to_box(), clean up organization of `custom` module (#1612)\n\n* Add encode_to_box\n\n* Move EncodeAsVar out of custom::\n\n* Improve docs a bit\n\n* make it cfg(doc)\n\n* fmt\n\n* fix\n\n* add into_boxed_slice\n\n* review\n\n* fmt",
+          "timestamp": "2022-02-16T21:33:46-08:00",
+          "tree_id": "a9d9fabfbd1b9bbd056bbcec66d9575942e0cb5e",
+          "url": "https://github.com/unicode-org/icu4x/commit/412119d18e61f533c33b3526c42cefa39fefb3c4"
+        },
+        "date": 1645076538241,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2266,
+            "range": "± 16",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4096,
+            "range": "± 52",
             "unit": "ns/iter"
           }
         ]
