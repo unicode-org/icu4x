08eafe1813531497c115d95a42f78556b204971a
github-action-benchmark <github@users.noreply.github.com>
2022-02-18 17:51:54 +0000

    add Benchmark (ndjson) benchmark result for ca764905dc100e05641b84a950ac1cf72c887235

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645199650615,
+  "lastUpdate": 1645206713898,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -9718,6 +9718,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "provider/testdata/data/testdata.postcard",
             "value": 1670453,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ca764905dc100e05641b84a950ac1cf72c887235",
+          "message": "Checking for key hash collisions and fixing one (#1622)",
+          "timestamp": "2022-02-18T18:49:45+01:00",
+          "tree_id": "b5a1b68fcfea8acb15ee1be02f910bf2d6dfb3f1",
+          "url": "https://github.com/unicode-org/icu4x/commit/ca764905dc100e05641b84a950ac1cf72c887235"
+        },
+        "date": 1645206699600,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 1670502,
             "unit": "bytes",
             "biggerIsBetter": false
           }
