269df246751c67b18ea34678d2b48193f8fad0b0
github-action-benchmark <github@users.noreply.github.com>
2022-02-22 17:02:04 +0000

    add Benchmark (ndjson) benchmark result for f574d2a7313cef08cfa8c3869859a1b318b9d97b

--- benchmarks/binsize/gz/data.js
+++ benchmarks/binsize/gz/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645206933081,
+  "lastUpdate": 1645549323885,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -49762,6 +49762,174 @@ window.BENCHMARK_DATA = {
           {
             "name": "work_log+opt.wasm.gz",
             "value": 343358,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unread_emails+opt.wasm.gz",
+            "value": 261752,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "owned_pattern+opt.wasm.gz",
+            "value": 7119,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "writeable_message+opt.wasm.gz",
+            "value": 9063,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "litemap_postcard+opt.wasm.gz",
+            "value": 4077,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "elevator_floors+opt.wasm.gz",
+            "value": 261769,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "make+opt.wasm.gz",
+            "value": 3637,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "code_line_diff+opt.wasm.gz",
+            "value": 260108,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "syntatically_canonicalize_locales+opt.wasm.gz",
+            "value": 13210,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_lite_map+opt.wasm.gz",
+            "value": 3169,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "10595307+mildgravitas@users.noreply.github.com",
+            "name": "mildgravitas",
+            "username": "mildgravitas"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f574d2a7313cef08cfa8c3869859a1b318b9d97b",
+          "message": "Fix get_best_available_format_pattern()'s skeleton matching logic. (#1549)\n\nThe compare was inverted resulting in matching at best stopping at the first\r\nmissing symbol rather than iterating through.\r\n\r\nFor example, with:\r\n\r\nfields = \"hs\"\r\nskeletons = [\"h\", \"hms\"]\r\n\r\nPreviously for \"hms\" we'd get a score of missing+extra = 11000:\r\n\r\nrequested_fields.peek() | skeleton_fields.peek() | result\r\n------------------------|------------------------|--------\r\n                    \"h\" | \"h\"                    | match\r\n                    \"s\" | \"m\"                    | missing\r\n                   None | \"s\"                    | extra\r\n\r\nWhereas we now get 1000:\r\n\r\nrequested_fields.peek() | skeleton_fields.peek() | result\r\n------------------------|------------------------|--------\r\n                    \"h\" | \"h\"                    | match\r\n                    \"s\" | \"m\"                    | extra\r\n                    \"s\" | \"s\"                    | match\r\n\r\n\"h\"'s score is missing = 10000 both before & now resulting in the best match changing from \"h\" to \"hms\".\r\n\r\nWorst case scenario was when a candidate pattern started with a symbol <\r\nthe first requested symbol. In such a case the pattern fails to match at\r\nall: e.g. requested = \"s\" won't match \"hms\".",
+          "timestamp": "2022-02-22T17:55:48+01:00",
+          "tree_id": "1cd6685e94fd7120f186e4ee0069077ffc18d1c9",
+          "url": "https://github.com/unicode-org/icu4x/commit/f574d2a7313cef08cfa8c3869859a1b318b9d97b"
+        },
+        "date": 1645549311063,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "postcard+opt.wasm.gz",
+            "value": 5428,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "zv_serde+opt.wasm.gz",
+            "value": 225,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "permyriad+opt.wasm.gz",
+            "value": 3860,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "derive+opt.wasm.gz",
+            "value": 3103,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "and_list+opt.wasm.gz",
+            "value": 276207,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "make_var+opt.wasm.gz",
+            "value": 6986,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "borrowed_pattern+opt.wasm.gz",
+            "value": 5922,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "simple_static+opt.wasm.gz",
+            "value": 262648,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "litemap_bincode+opt.wasm.gz",
+            "value": 9269,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unicode_bmp_blocks_selector+opt.wasm.gz",
+            "value": 6258,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids+opt.wasm.gz",
+            "value": 6044,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "tui+opt.wasm.gz",
+            "value": 361823,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_hash_map+opt.wasm.gz",
+            "value": 5095,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "derives+opt.wasm.gz",
+            "value": 6095,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log+opt.wasm.gz",
+            "value": 343355,
             "unit": "bytes",
             "biggerIsBetter": false
           },
