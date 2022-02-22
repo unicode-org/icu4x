8f6ff988f231c939d1ca882ba610c663f70a5ae4
Robert Bastian <robertbastian@users.noreply.github.com>
2022-02-22 18:14:59 +0100

    Making more tinystr operations const (#1628)

--- utils/tinystr/src/ule.rs
+++ utils/tinystr/src/ule.rs
@@ -24,7 +24,7 @@ unsafe impl<const N: usize> ULE for TinyAsciiStr<N> {
         }
         // Validate the bytes
         for chunk in bytes.chunks_exact(N) {
-            let _ = TinyAsciiStr::<N>::from_bytes_inner(chunk, true)
+            let _ = TinyAsciiStr::<N>::from_bytes_inner(chunk, 0, N, true)
                 .map_err(|_| ZeroVecError::parse::<Self>())?;
         }
         Ok(())
