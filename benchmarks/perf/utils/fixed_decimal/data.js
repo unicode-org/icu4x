1d95e091978622e2f6fd4c7826db97afcf211461
github-action-benchmark <github@users.noreply.github.com>
2022-02-22 17:23:56 +0000

    add Rust Benchmark (cargo) benchmark result for 8f6ff988f231c939d1ca882ba610c663f70a5ae4

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645549494642,
+  "lastUpdate": 1645550636278,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -19649,6 +19649,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 108199,
             "range": "± 10815",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8f6ff988f231c939d1ca882ba610c663f70a5ae4",
+          "message": "Making more tinystr operations const (#1628)",
+          "timestamp": "2022-02-22T18:14:59+01:00",
+          "tree_id": "c4cd53125ed12b46515d54eb4f4655698d881859",
+          "url": "https://github.com/unicode-org/icu4x/commit/8f6ff988f231c939d1ca882ba610c663f70a5ae4"
+        },
+        "date": 1645550622450,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 122940,
+            "range": "± 1088",
             "unit": "ns/iter"
           }
         ]
