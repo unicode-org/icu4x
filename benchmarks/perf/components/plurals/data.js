fbf80c4a2faca723b2692590eb51f28c9c2e1a60
github-action-benchmark <github@users.noreply.github.com>
2022-02-22 21:14:53 +0000

    add Rust Benchmark (cargo) benchmark result for 9347b7993c7ba173b76a16cad9e166236bf72c12

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645552052104,
+  "lastUpdate": 1645564492969,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -27677,6 +27677,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 15003,
             "range": "± 59",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9347b7993c7ba173b76a16cad9e166236bf72c12",
+          "message": "`DataExporter : Sync` (#1617)",
+          "timestamp": "2022-02-22T22:04:07+01:00",
+          "tree_id": "491ee746ef77f0a2fae9a291f8a088d88da6b413",
+          "url": "https://github.com/unicode-org/icu4x/commit/9347b7993c7ba173b76a16cad9e166236bf72c12"
+        },
+        "date": 1645564480043,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 950,
+            "range": "± 26",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6350,
+            "range": "± 183",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 16689,
+            "range": "± 595",
             "unit": "ns/iter"
           }
         ]
