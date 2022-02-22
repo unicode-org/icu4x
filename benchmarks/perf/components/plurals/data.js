184b5fb4dddddfb322170bd8c68ed46c4fa7d1dc
github-action-benchmark <github@users.noreply.github.com>
2022-02-22 17:47:32 +0000

    add Rust Benchmark (cargo) benchmark result for 6cbd05a2226fce1d41e20b7838a7e118af492f4d

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645550728521,
+  "lastUpdate": 1645552052104,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -27635,6 +27635,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 15016,
             "range": "± 103",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6cbd05a2226fce1d41e20b7838a7e118af492f4d",
+          "message": "Add paragraph about alignment tradeoff (#1625)",
+          "timestamp": "2022-02-22T11:37:16-06:00",
+          "tree_id": "0f818159cc08cae017d20075553070b37288649c",
+          "url": "https://github.com/unicode-org/icu4x/commit/6cbd05a2226fce1d41e20b7838a7e118af492f4d"
+        },
+        "date": 1645552040231,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 949,
+            "range": "± 10",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 5470,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 15003,
+            "range": "± 59",
             "unit": "ns/iter"
           }
         ]
