9ce1f5e7f080d1577e72a35cff811f822864194b
github-action-benchmark <github@users.noreply.github.com>
2022-02-24 04:53:55 +0000

    add Benchmark (ndjson) benchmark result for 5208806e15382eba7d0d033dace0cbdf64c242c6

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645675060891,
+  "lastUpdate": 1645678435147,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -9983,6 +9983,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/938ed36605d6dc6861d1ff60fe409e35e2761a00"
         },
         "date": 1645675046847,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 1672368,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5208806e15382eba7d0d033dace0cbdf64c242c6",
+          "message": "Update icu4x-announce link",
+          "timestamp": "2022-02-23T22:52:08-06:00",
+          "tree_id": "6ab8e62bc6410d2b80df904adfe99fc9c453308a",
+          "url": "https://github.com/unicode-org/icu4x/commit/5208806e15382eba7d0d033dace0cbdf64c242c6"
+        },
+        "date": 1645678423615,
         "tool": "ndjson",
         "benches": [
           {
