4ab2d27d6fdcb27528036beb7c4e34427d08d0d5
github-action-benchmark <github@users.noreply.github.com>
2022-02-24 05:00:23 +0000

    add Rust Benchmark (cargo) benchmark result for 5208806e15382eba7d0d033dace0cbdf64c242c6

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645675505517,
+  "lastUpdate": 1645678823285,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20939,6 +20939,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 43580587,
             "range": "± 187257",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5208806e15382eba7d0d033dace0cbdf64c242c6",
+          "message": "Update icu4x-announce link",
+          "timestamp": "2022-02-23T22:52:08-06:00",
+          "tree_id": "6ab8e62bc6410d2b80df904adfe99fc9c453308a",
+          "url": "https://github.com/unicode-org/icu4x/commit/5208806e15382eba7d0d033dace0cbdf64c242c6"
+        },
+        "date": 1645678811571,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 43764805,
+            "range": "± 81063",
             "unit": "ns/iter"
           }
         ]
