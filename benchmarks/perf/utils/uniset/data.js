43945d3fdfc9b821dba5e7a7d4faaceada26140c
github-action-benchmark <github@users.noreply.github.com>
2022-02-25 00:50:16 +0000

    add Rust Benchmark (cargo) benchmark result for 4c22ef7d6aa78aa8d1bc4f7193619ccd03f65976

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645691682943,
+  "lastUpdate": 1645750215796,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20999,6 +20999,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 55101145,
             "range": "± 598173",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4c22ef7d6aa78aa8d1bc4f7193619ccd03f65976",
+          "message": "Add serde support to the zerovec proc macro; reexport from the zerovec crate (#1632)\n\n* Add serde reexport\r\n\r\n* Add zerovec::serde\r\n\r\n* Reexport derive, add docs\r\n\r\n* fixup vis\r\n\r\n* fixup feature\r\n\r\n* Update utils/zerovec/derive/src/lib.rs\r\n\r\nCo-authored-by: Shane F. Carr <shane@unicode.org>\r\n\r\n* readme\r\n\r\n* fixup test deps\r\n\r\nCo-authored-by: Shane F. Carr <shane@unicode.org>",
+          "timestamp": "2022-02-24T16:36:53-08:00",
+          "tree_id": "a579238d57bb74a14423b7d3043dcd164cd000fe",
+          "url": "https://github.com/unicode-org/icu4x/commit/4c22ef7d6aa78aa8d1bc4f7193619ccd03f65976"
+        },
+        "date": 1645750201366,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 52563864,
+            "range": "± 172281",
             "unit": "ns/iter"
           }
         ]
