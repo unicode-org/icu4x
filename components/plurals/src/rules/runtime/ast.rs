8fb1c36abc0ce85ad64766270938469af2d0556b
Manish Goregaokar <manishsmail@gmail.com>
2022-02-28 15:50:52 -0800

    fmt

--- components/plurals/src/rules/runtime/ast.rs
+++ components/plurals/src/rules/runtime/ast.rs
@@ -285,8 +285,7 @@ pub(crate) struct AndOrPolarityOperandULE(u8);
 unsafe impl ULE for AndOrPolarityOperandULE {
     fn validate_byte_slice(bytes: &[u8]) -> Result<(), ZeroVecError> {
         for byte in bytes {
-            Operand::new_from_u8(byte & 0b0011_1111)
-                .ok_or(ZeroVecError::parse::<Self>())?;
+            Operand::new_from_u8(byte & 0b0011_1111).ok_or(ZeroVecError::parse::<Self>())?;
         }
         Ok(())
     }
