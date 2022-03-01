fd2d6dcf4d93954ed81b0a34238b91ba174cb1fc
github-action-benchmark <github@users.noreply.github.com>
2022-03-01 00:38:45 +0000

    add Rust Benchmark (cargo) benchmark result for 1a4e610165ff33eea11c25271915058ca1bbf5eb

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646086181853,
+  "lastUpdate": 1646095125629,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -24725,6 +24725,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4029,
             "range": "± 6",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aethanyc@gmail.com",
+            "name": "Ting-Yu Lin",
+            "username": "aethanyc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1a4e610165ff33eea11c25271915058ca1bbf5eb",
+          "message": "Bring LineBreakDataV1 data structure closer to RuleBreakDataV1 (#1634)\n\n* Align LineBreakDataV1 with RuleBreakDataV1\r\n\r\nMake LineBreakDataV1 data more similar to RuleBreakDataV1 via:\r\n\r\n- Include generated_line_table.rs in `line_data` mod in provider.rs.\r\n- Move `property_count` from LineBreakRuleTable into LineBreakDataV1.\r\n\r\n* Rename LineBreakRuleTable to LineBreakStateTable\r\n\r\nIt's because we have RuleBreakDataV1::RuleBreakStateTable.\r\n\r\n* Add more fields from line data to LineBreakDataV1\r\n\r\nCurrently, only `eot_property` is used in LineBreakIterator.\r\n\r\n* Use `crate` link to refer to module-level documentation\r\n\r\nhttps://doc.rust-lang.org/rustdoc/linking-to-items-by-name.html?highlight=super#valid-links\r\n\r\n* Expand the types to export in line mod",
+          "timestamp": "2022-02-28T16:28:36-08:00",
+          "tree_id": "6e627e63dae593b460d6b22d45e0525d7663c44f",
+          "url": "https://github.com/unicode-org/icu4x/commit/1a4e610165ff33eea11c25271915058ca1bbf5eb"
+        },
+        "date": 1646095110963,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2594,
+            "range": "± 178",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4816,
+            "range": "± 495",
             "unit": "ns/iter"
           }
         ]
