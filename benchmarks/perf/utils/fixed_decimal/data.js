652a79d60fe75182e5833c6ce72cfbf3673c2516
github-action-benchmark <github@users.noreply.github.com>
2022-03-02 03:31:29 +0000

    add Rust Benchmark (cargo) benchmark result for fa5e8c369e6eaaba1b379105624f43a45fcdaeac

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646184241180,
+  "lastUpdate": 1646191889525,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20189,6 +20189,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 110676,
             "range": "± 4631",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fa5e8c369e6eaaba1b379105624f43a45fcdaeac",
+          "message": "Add cast methods to ZeroVec and ZeroSlice (#1651)",
+          "timestamp": "2022-03-01T19:23:22-08:00",
+          "tree_id": "1df10a482a3ca80d2a3718eac147e1eac1ad5763",
+          "url": "https://github.com/unicode-org/icu4x/commit/fa5e8c369e6eaaba1b379105624f43a45fcdaeac"
+        },
+        "date": 1646191875964,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 106121,
+            "range": "± 6280",
             "unit": "ns/iter"
           }
         ]
