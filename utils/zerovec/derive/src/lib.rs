b5516d0e7d6d4b55ca32143552c1d1e7a7f34bb8
Manish Goregaokar <manishsmail@gmail.com>
2022-03-02 11:34:13 -0800

    Add proc macro examples

--- utils/zerovec/derive/src/lib.rs
+++ utils/zerovec/derive/src/lib.rs
@@ -13,7 +13,10 @@ mod varule;
 
 /// Custom derive for `zerovec::ULE`,
 ///
-/// This can be attached to `Copy` structs containing only ULE types
+/// This can be attached to `Copy` structs containing only ULE types.
+///
+/// Most of the time, it is recommended one use [`#[make_ule]`](make_ule) instead of defining
+/// a custom ULE type.
 #[proc_macro_derive(ULE)]
 pub fn ule_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -22,7 +25,10 @@ pub fn ule_derive(input: TokenStream) -> TokenStream {
 
 /// Custom derive for `zerovec::VarULE`,
 ///
-/// This can be attached to structs containing only ULE types with one VarULE type at the end
+/// This can be attached to structs containing only ULE types with one VarULE type at the end.
+///
+/// Most of the time, it is recommended one use [`#[make_varule]`](make_varule) instead of defining
+/// a custom VarULE type.
 #[proc_macro_derive(VarULE)]
 pub fn varule_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -45,6 +51,39 @@ pub fn varule_derive(input: TokenStream) -> TokenStream {
 /// For enums, this implementation will generate a crate-public `fn new_from_u8(value: u8) -> Option<Self>`
 /// method on the main type that allows one to construct the value from a u8. If this method is desired
 /// to be more public, it should be wrapped.
+///
+/// # Example
+///
+/// ```rust
+/// use zerovec::ZeroVec;
+///
+/// #[zerovec::make_ule(DateULE)]
+/// #[derive(Copy, Clone, PartialEq, Eq, Ord, PartialOrd, serde::Serialize, serde::Deserialize)]
+/// struct Date {
+///     y: u64,
+///     m: u8,
+///     d: u8
+/// }
+///
+/// #[derive(serde::Serialize, serde::Deserialize)]
+/// struct Dates<'a> {
+///     #[serde(borrow)]
+///     dates: ZeroVec<'a, Date>   
+/// }
+///
+/// let dates = Dates { dates: ZeroVec::alloc_from_slice(&[Date {y: 1985, m: 9, d: 3}, Date {y: 1970, m: 2, d: 20}, Date {y: 1990, m: 6, d: 13}]) };
+///
+/// let postcard_bytes = postcard::to_stdvec(&dates)
+///     .expect("Serialization should be successful");
+///
+/// // Will deserialize without allocations
+/// let deserialized: Dates = postcard::from_bytes(&postcard_bytes)
+///     .expect("Deserialization should be successful");
+///
+/// assert_eq!(deserialized.dates.get(1).unwrap().y, 1970);
+/// assert_eq!(deserialized.dates.get(2).unwrap().d, 13);
+///
+/// ```
 #[proc_macro_attribute]
 pub fn make_ule(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
@@ -59,6 +98,10 @@ pub fn make_ule(attr: TokenStream, item: TokenStream) -> TokenStream {
 ///
 /// The type must be `PartialEq` and `Eq`.
 ///
+/// `EncodeAsVarULE` and `ZeroFrom` are useful for avoiding the need to deal with the VarULE type directly. In particular,
+/// it is recommended to use `ZeroFrom` to convert the VarULE type back to this type in a cheap, zero-copy way (see the example
+/// for more details).
+///
 /// Provided the type implements `serde::Serialize` and `serde::Deserialize`, this attribute can also generate
 /// the relevant serialize/deserialize implementations for the `VarULE` type if you apply the `#[zerovec::serde]`
 /// attribute. Those impls are required to support human-readable serialization of the VarZeroVec.
@@ -69,6 +112,73 @@ pub fn make_ule(attr: TokenStream, item: TokenStream) -> TokenStream {
 ///
 /// This implementation will also by default autogenerate `Ord` and `PartialOrd` on the VarULE type based on
 /// the implementation on `Self`. You can opt out of this with `#[zerovec::skip_ord]`
+///
+/// # Example
+///
+/// ```rust
+/// use zerovec::{ZeroVec, VarZeroVec, ZeroMap};
+/// use std::borrow::Cow;
+/// use zerovec::ule::encode_varule_to_box;
+/// use zerofrom::ZeroFrom;
+///
+/// // custom fixed-size ULE type for ZeroVec
+/// #[zerovec::make_ule(DateULE)]
+/// #[derive(Copy, Clone, PartialEq, Eq, Ord, PartialOrd, serde::Serialize, serde::Deserialize)]
+/// struct Date {
+///     y: u64,
+///     m: u8,
+///     d: u8
+/// }
+///
+/// // custom variable sized VarULE type for VarZeroVec
+/// #[zerovec::make_varule(PersonULE)]
+/// #[zerovec::serde]
+/// #[derive(Clone, PartialEq, Eq, Ord, PartialOrd, serde::Serialize, serde::Deserialize)]
+/// struct Person<'a> {
+///     birthday: Date,
+///     favorite_character: char,
+///     #[serde(borrow)]
+///     name: Cow<'a, str>,
+/// }
+///
+/// #[derive(serde::Serialize, serde::Deserialize)]
+/// struct Data<'a> {
+///     // note: VarZeroVec always must reference the ULE type directly
+///     #[serde(borrow)]
+///     important_people: VarZeroVec<'a, PersonULE>,
+/// }
+///
+///
+/// let person1 = Person {
+///     birthday: Date { y: 1990, m: 9, d: 7},
+///     favorite_character: 'π',
+///     name: Cow::from("Kate")
+/// };
+/// let person2 = Person {
+///     birthday: Date { y: 1960, m: 5, d: 25},
+///     favorite_character: '冇',
+///     name: Cow::from("Jesse")
+/// };
+///
+/// let important_people = VarZeroVec::from(&[person1.clone(), person2.clone()]);
+/// let data = Data { important_people };
+///
+/// let postcard_bytes = postcard::to_stdvec(&data)
+///     .expect("Serialization should be successful");
+///
+/// // Will deserialize without allocations
+/// let deserialized: Data = postcard::from_bytes(&postcard_bytes)
+///     .expect("Deserialization should be successful");
+///
+/// assert_eq!(&deserialized.important_people.get(1).unwrap().name, "Jesse");
+/// assert_eq!(&deserialized.important_people.get(0).unwrap().name, "Kate");
+///
+/// // Since VarZeroVec produces PersonULE types, it's convenient to use ZeroFrom
+/// // to recoup Person values in a zero-copy way
+/// let person_converted: Person = ZeroFrom::zero_from(deserialized.important_people.get(1).unwrap());
+/// assert_eq!(person_converted.name, "Jesse");
+/// assert_eq!(person_converted.birthday.y, 1960);
+/// ```
 #[proc_macro_attribute]
 pub fn make_varule(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
