27c8d4afb2e1de2c69f99ce7b86cefdef3ceb11c
github-action-benchmark <github@users.noreply.github.com>
2022-03-03 00:20:31 +0000

    add Rust Benchmark (cargo) benchmark result for afcd45a277729a2fb7fd4091711b55a916415ff9

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646192010915,
+  "lastUpdate": 1646266831542,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -21977,6 +21977,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 209008,
             "range": "± 13914",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "afcd45a277729a2fb7fd4091711b55a916415ff9",
+          "message": "Turn all errors into Copy types (#1657)\n\n* Move DateTimeError to Copy\r\n\r\n* Move DTF errors to Copy\r\n\r\n* Display for TinyStr\r\n\r\n* copy types in plurals\r\n\r\n* Copy for core DateTimeFormatError\r\n\r\n* Copy for decimal, locid, properties errors\r\n\r\n* clip\r\n\r\n* clip",
+          "timestamp": "2022-03-02T16:11:28-08:00",
+          "tree_id": "3bb8b7ddf0c0bb0c5b2a08d8ebe0e59865a06c33",
+          "url": "https://github.com/unicode-org/icu4x/commit/afcd45a277729a2fb7fd4091711b55a916415ff9"
+        },
+        "date": 1646266819833,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 65368,
+            "range": "± 106",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 713764,
+            "range": "± 953",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 152930,
+            "range": "± 198",
             "unit": "ns/iter"
           }
         ]
