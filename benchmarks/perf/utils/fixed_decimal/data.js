438ae093b4164441089332c36c297011f46d8923
github-action-benchmark <github@users.noreply.github.com>
2022-03-03 17:57:12 +0000

    add Rust Benchmark (cargo) benchmark result for 03031533317965904c1dbdd744f985d52d749c0c

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646303260533,
+  "lastUpdate": 1646330231918,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20279,6 +20279,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 117763,
             "range": "± 7217",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pandusonu@google.com",
+            "name": "Gollapudi Vamsi Krishna",
+            "username": "pandusonu2"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03031533317965904c1dbdd744f985d52d749c0c",
+          "message": "Add Calendar Arithmetic (#1614)\n\n* Add Calendar Arithmetic\r\n\r\n* Remove calendar extension\r\n\r\n* Add until()\r\n\r\n* Fix errors in calendar_arithmetic\r\n\r\n* Fix few more errors\r\n\r\n* Fix other type errors\r\n\r\n* mutability\r\n\r\n* Fix tests\r\n\r\n* Clippy suggestions\r\n\r\n* More clippy\r\n\r\n* Fix comments\r\n\r\nCo-authored-by: Manish Goregaokar <manishsmail@gmail.com>",
+          "timestamp": "2022-03-03T09:49:31-08:00",
+          "tree_id": "3dc08370fd8fa52f2c828a3d502b3a3d6709c8b4",
+          "url": "https://github.com/unicode-org/icu4x/commit/03031533317965904c1dbdd744f985d52d749c0c"
+        },
+        "date": 1646330219207,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 115356,
+            "range": "± 5383",
             "unit": "ns/iter"
           }
         ]
