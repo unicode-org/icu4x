06fdc2bb5e51f11c503546a4d2b841d59e7b3615
github-action-benchmark <github@users.noreply.github.com>
2022-03-03 10:28:14 +0000

    add Rust Benchmark (cargo) benchmark result for 3e8c8fdff33ee346511799655043b9a7a896fb93

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646266831542,
+  "lastUpdate": 1646303294655,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22019,6 +22019,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 152930,
             "range": "± 198",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3e8c8fdff33ee346511799655043b9a7a896fb93",
+          "message": "ZeroFrom JapaneseEras (#1635)",
+          "timestamp": "2022-03-03T11:18:57+01:00",
+          "tree_id": "f1f96e9f5f674ce282c20c739b7bb0754c073bf1",
+          "url": "https://github.com/unicode-org/icu4x/commit/3e8c8fdff33ee346511799655043b9a7a896fb93"
+        },
+        "date": 1646303281974,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 72610,
+            "range": "± 161",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 775436,
+            "range": "± 963",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 157610,
+            "range": "± 517",
             "unit": "ns/iter"
           }
         ]
