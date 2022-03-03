627e4eb820ae9981217c31a44751e2749c3b7dbd
github-action-benchmark <github@users.noreply.github.com>
2022-03-03 17:59:13 +0000

    add Rust Benchmark (cargo) benchmark result for 03031533317965904c1dbdd744f985d52d749c0c

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646303294655,
+  "lastUpdate": 1646330353235,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22061,6 +22061,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 157610,
             "range": "± 517",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pandusonu@google.com",
+            "name": "Gollapudi Vamsi Krishna",
+            "username": "pandusonu2"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03031533317965904c1dbdd744f985d52d749c0c",
+          "message": "Add Calendar Arithmetic (#1614)\n\n* Add Calendar Arithmetic\r\n\r\n* Remove calendar extension\r\n\r\n* Add until()\r\n\r\n* Fix errors in calendar_arithmetic\r\n\r\n* Fix few more errors\r\n\r\n* Fix other type errors\r\n\r\n* mutability\r\n\r\n* Fix tests\r\n\r\n* Clippy suggestions\r\n\r\n* More clippy\r\n\r\n* Fix comments\r\n\r\nCo-authored-by: Manish Goregaokar <manishsmail@gmail.com>",
+          "timestamp": "2022-03-03T09:49:31-08:00",
+          "tree_id": "3dc08370fd8fa52f2c828a3d502b3a3d6709c8b4",
+          "url": "https://github.com/unicode-org/icu4x/commit/03031533317965904c1dbdd744f985d52d749c0c"
+        },
+        "date": 1646330338420,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 105795,
+            "range": "± 4767",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 1041738,
+            "range": "± 54997",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 224676,
+            "range": "± 13076",
             "unit": "ns/iter"
           }
         ]
