9b64da30e77f580fed4a5e28c60de2d15f01fa2b
github-action-benchmark <github@users.noreply.github.com>
2022-03-05 00:00:56 +0000

    add Rust Benchmark (cargo) benchmark result for b6c6994edc1ac8f069446063b5b5151a70c328eb

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646354460108,
+  "lastUpdate": 1646438456513,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -25121,6 +25121,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4806,
             "range": "± 405",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b6c6994edc1ac8f069446063b5b5151a70c328eb",
+          "message": "Split docs into their own CI job (#1563)",
+          "timestamp": "2022-03-04T17:51:53-06:00",
+          "tree_id": "d809fae9de931659277ba42cba604bc72f49f2da",
+          "url": "https://github.com/unicode-org/icu4x/commit/b6c6994edc1ac8f069446063b5b5151a70c328eb"
+        },
+        "date": 1646438441570,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2349,
+            "range": "± 216",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4829,
+            "range": "± 307",
             "unit": "ns/iter"
           }
         ]
