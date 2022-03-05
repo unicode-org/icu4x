fa7a565969094dd79e0517684e4a89a20b0f9fed
github-action-benchmark <github@users.noreply.github.com>
2022-03-05 00:01:22 +0000

    add Rust Benchmark (cargo) benchmark result for b6c6994edc1ac8f069446063b5b5151a70c328eb

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646354464666,
+  "lastUpdate": 1646438481942,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22187,6 +22187,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 171731,
             "range": "± 152",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b6c6994edc1ac8f069446063b5b5151a70c328eb",
+          "message": "Split docs into their own CI job (#1563)",
+          "timestamp": "2022-03-04T17:51:53-06:00",
+          "tree_id": "d809fae9de931659277ba42cba604bc72f49f2da",
+          "url": "https://github.com/unicode-org/icu4x/commit/b6c6994edc1ac8f069446063b5b5151a70c328eb"
+        },
+        "date": 1646438467293,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 98630,
+            "range": "± 4449",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 992848,
+            "range": "± 40639",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 213679,
+            "range": "± 11928",
             "unit": "ns/iter"
           }
         ]
