b9cc7f3a4430648924773005aa4878cefcfcd15d
github-action-benchmark <github@users.noreply.github.com>
2022-03-09 18:35:58 +0000

    add Rust Benchmark (cargo) benchmark result for 54804ff5a6b252a8fb2fe4a805e62667d356fd7f

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646791463699,
+  "lastUpdate": 1646850958049,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -21689,6 +21689,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 42094208,
             "range": "± 3082941",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aethanyc@gmail.com",
+            "name": "Ting-Yu Lin",
+            "username": "aethanyc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "54804ff5a6b252a8fb2fe4a805e62667d356fd7f",
+          "message": "Make line break tests run faster (#1681)\n\nCurrently, we load line break data *every time* we call `is_break` or\r\n`get_linebreak_property`. It is sufficient to load the data once per test.\r\n\r\nOn my machine, the running time of `cargo test break_rule` is reduce from 2.66s\r\nto 0.05s, and `cargo test linebreak_property` from 0.62s to 0.05s.",
+          "timestamp": "2022-03-09T10:23:48-08:00",
+          "tree_id": "90e9f635d80de15197f7096e1520c500e754881f",
+          "url": "https://github.com/unicode-org/icu4x/commit/54804ff5a6b252a8fb2fe4a805e62667d356fd7f"
+        },
+        "date": 1646850945036,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 55059778,
+            "range": "± 780030",
             "unit": "ns/iter"
           }
         ]
