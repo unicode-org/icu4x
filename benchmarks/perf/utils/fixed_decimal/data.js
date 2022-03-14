27f775efeebc37eba887fa7f1d9109009b2864ee
github-action-benchmark <github@users.noreply.github.com>
2022-03-14 21:14:51 +0000

    add Rust Benchmark (cargo) benchmark result for 989bade3905208e2c7bdb332a04f9488ec36c854

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1647279421375,
+  "lastUpdate": 1647292491483,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -20789,6 +20789,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 123096,
             "range": "± 941",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "989bade3905208e2c7bdb332a04f9488ec36c854",
+          "message": "Using TinyAsciiStr for locale_canonicalizer and locid (#1683)",
+          "timestamp": "2022-03-14T14:07:54-07:00",
+          "tree_id": "f53d15791bd3580d2a60d2a548be122dd18788ff",
+          "url": "https://github.com/unicode-org/icu4x/commit/989bade3905208e2c7bdb332a04f9488ec36c854"
+        },
+        "date": 1647292476556,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 123177,
+            "range": "± 1299",
             "unit": "ns/iter"
           }
         ]
