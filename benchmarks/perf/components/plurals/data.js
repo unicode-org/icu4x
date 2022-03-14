bec6e25f46e3615d8619380d0e80372ee7e2e0f2
github-action-benchmark <github@users.noreply.github.com>
2022-03-14 21:17:28 +0000

    add Rust Benchmark (cargo) benchmark result for 989bade3905208e2c7bdb332a04f9488ec36c854

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1647279615612,
+  "lastUpdate": 1647292648321,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -29189,6 +29189,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 16158,
             "range": "± 880",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "989bade3905208e2c7bdb332a04f9488ec36c854",
+          "message": "Using TinyAsciiStr for locale_canonicalizer and locid (#1683)",
+          "timestamp": "2022-03-14T14:07:54-07:00",
+          "tree_id": "f53d15791bd3580d2a60d2a548be122dd18788ff",
+          "url": "https://github.com/unicode-org/icu4x/commit/989bade3905208e2c7bdb332a04f9488ec36c854"
+        },
+        "date": 1647292632849,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 954,
+            "range": "± 63",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6652,
+            "range": "± 591",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 15110,
+            "range": "± 980",
             "unit": "ns/iter"
           }
         ]
