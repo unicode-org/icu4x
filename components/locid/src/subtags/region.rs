095c74db534e3a7c92054c8a0b9d7c9d04ca160f
Shane F. Carr <shane@unicode.org>
2022-03-14 19:05:07 -0700

    Add ULEs for Region

--- components/locid/src/subtags/region.rs
+++ components/locid/src/subtags/region.rs
@@ -22,6 +22,7 @@ use tinystr::TinyAsciiStr;
 ///
 /// [`unicode_region_id`]: https://unicode.org/reports/tr35/#unicode_region_id
 #[derive(Debug, PartialEq, Eq, Clone, Hash, PartialOrd, Ord, Copy)]
+#[repr(transparent)]
 pub struct Region(TinyAsciiStr<REGION_NUM_LENGTH>);
 
 const REGION_ALPHA_LENGTH: usize = 2;
@@ -167,4 +168,80 @@ impl From<Region> for TinyAsciiStr<3> {
     fn from(input: Region) -> Self {
         input.0
     }
+}
+
+// Safety checklist for ULE:
+//
+// 1. Must not include any uninitialized or padding bytes (true since transparent over a ULE).
+// 2. Must have an alignment of 1 byte (true since transparent over a ULE).
+// 3. ULE::validate_byte_slice() checks that the given byte slice represents a valid slice.
+// 4. ULE::validate_byte_slice() checks that the given byte slice has a valid length.
+// 5. All other methods must be left with their default impl.
+// 6. Byte equality is semantic equality.
+#[cfg(feature = "zerovec")]
+unsafe impl zerovec::ule::ULE for Region {
+    fn validate_byte_slice(bytes: &[u8]) -> Result<(), zerovec::ZeroVecError> {
+        let it = bytes.chunks_exact(core::mem::size_of::<Self>());
+        if it.remainder().len() > 0 {
+            return Err(zerovec::ZeroVecError::length::<Self>(bytes.len()));
+        }
+        for v in it {
+            // The following can be removed once `array_chunks` is stabilized.
+            let mut a = [0; 3];
+            a.copy_from_slice(v);
+            let s = TinyAsciiStr::<REGION_NUM_LENGTH>::try_from_raw(&a)
+                .map_err(|_| zerovec::ZeroVecError::parse::<Self>())?;
+            match s.len() {
+                REGION_ALPHA_LENGTH => {
+                    if s.is_ascii_uppercase() {
+                        continue;
+                    }
+                }
+                REGION_NUM_LENGTH => {
+                    if s.is_ascii_numeric() {
+                        continue;
+                    }
+                }
+                _ => ()
+            };
+            return Err(zerovec::ZeroVecError::parse::<Self>());
+        }
+        Ok(())
+    }
+}
+
+/// Impl enabling `Region` to be used in a [`ZeroVec`]. Enabled with the `"zerovec"` feature.
+///
+/// # Example
+///
+/// ```
+/// use icu::locid::subtags::Region;
+/// use icu::locid::macros::region;
+/// use zerovec::ZeroVec;
+///
+/// let zv = ZeroVec::<Region>::parse_byte_slice(b"GB\0419001DE\0")
+///     .expect("Valid region subtags");
+/// assert_eq!(zv.get(1), Some(region!("419")));
+///
+/// ZeroVec::<Region>::parse_byte_slice(b"invalid")
+///     .expect_err("Invalid byte slice");
+/// ```
+///
+/// [`ZeroVec`]: zerovec::ZeroVec
+#[cfg(feature = "zerovec")]
+impl zerovec::ule::AsULE for Region {
+    type ULE = Self;
+    fn to_unaligned(self) -> Self::ULE {
+        self
+    }
+    fn from_unaligned(unaligned: Self::ULE) -> Self {
+        unaligned
+    }
+}
+
+#[cfg(feature = "zerovec")]
+impl<'a> zerovec::maps::ZeroMapKV<'a> for Region {
+    type Container = zerovec::ZeroVec<'a, Region>;
+    type GetType = Region;
+    type OwnedType = Region;
 }
