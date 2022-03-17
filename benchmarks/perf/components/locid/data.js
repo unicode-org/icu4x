257c54e094f831796200b5df6dd286ccdde9a137
github-action-benchmark <github@users.noreply.github.com>
2022-03-17 00:36:51 +0000

    add Rust Benchmark (cargo) benchmark result for dbfbee78528d019cc9293247a5e2c7d2973d02f9

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1647447279048,
+  "lastUpdate": 1647477411714,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -25805,6 +25805,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4205,
             "range": "± 9",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dbfbee78528d019cc9293247a5e2c7d2973d02f9",
+          "message": "Create scorecards.yml",
+          "timestamp": "2022-03-16T17:29:24-07:00",
+          "tree_id": "e009661b07a9818ee7e47b490036ae064246fcfc",
+          "url": "https://github.com/unicode-org/icu4x/commit/dbfbee78528d019cc9293247a5e2c7d2973d02f9"
+        },
+        "date": 1647477399692,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 1884,
+            "range": "± 6",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 3838,
+            "range": "± 5",
             "unit": "ns/iter"
           }
         ]
