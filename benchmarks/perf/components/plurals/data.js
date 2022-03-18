9a8f5af76a76b1856c785718bba65056d9935641
github-action-benchmark <github@users.noreply.github.com>
2022-03-18 00:52:27 +0000

    add Rust Benchmark (cargo) benchmark result for d118e3d1f3264096ee7e92d867ebf2c46069ef56

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1647552197479,
+  "lastUpdate": 1647564747573,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -29651,6 +29651,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 15446,
             "range": "± 1328",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d118e3d1f3264096ee7e92d867ebf2c46069ef56",
+          "message": "Add ULE for Language and fix bug in Region (#1711)",
+          "timestamp": "2022-03-17T17:43:25-07:00",
+          "tree_id": "a73151c42998aa6efef50482c63dfac90fbb522a",
+          "url": "https://github.com/unicode-org/icu4x/commit/d118e3d1f3264096ee7e92d867ebf2c46069ef56"
+        },
+        "date": 1647564733874,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 928,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 5407,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 13842,
+            "range": "± 17",
             "unit": "ns/iter"
           }
         ]
