50800705e4f9fe56a668a8f865d2c22b86963af0
github-action-benchmark <github@users.noreply.github.com>
2022-03-21 13:32:46 +0000

    add Rust Benchmark (cargo) benchmark result for d3cbc2855e2623056e72cb209b3dc39e04e47afc

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1647617927131,
+  "lastUpdate": 1647869566719,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -26093,6 +26093,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 3570,
             "range": "± 16",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "10595307+mildgravitas@users.noreply.github.com",
+            "name": "mildgravitas",
+            "username": "mildgravitas"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d3cbc2855e2623056e72cb209b3dc39e04e47afc",
+          "message": "Add support for week-of-month. (#1468)\n\nContrary to UTS 35 this always uses min_days = 1 as there's no month of\r\nweek-of-month field so there would be inconsistencies otherwise (e.g. in\r\nthe ISO calendar 2021-01-01 is the last week of December but 'MMMMW' would\r\nhave it formatted as 'week 5 of January').",
+          "timestamp": "2022-03-21T08:24:03-05:00",
+          "tree_id": "fff92bb6e7022879cfa45bc6031a7be0f218dcb6",
+          "url": "https://github.com/unicode-org/icu4x/commit/d3cbc2855e2623056e72cb209b3dc39e04e47afc"
+        },
+        "date": 1647869549594,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2583,
+            "range": "± 101",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 5048,
+            "range": "± 281",
             "unit": "ns/iter"
           }
         ]
