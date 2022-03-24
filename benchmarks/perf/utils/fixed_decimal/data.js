e6289c94ed819e59f9a2cab61a9e2c721ed7b01b
github-action-benchmark <github@users.noreply.github.com>
2022-03-24 15:21:38 +0000

    add Rust Benchmark (cargo) benchmark result for a4c650b3cf434a6a803b7e1e2c8443740a5ae3f7

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648088204605,
+  "lastUpdate": 1648135298844,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -21329,6 +21329,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 130082,
             "range": "± 19592",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a4c650b3cf434a6a803b7e1e2c8443740a5ae3f7",
+          "message": "Treat ZeroMap sort order as an optional invariant (#1727)",
+          "timestamp": "2022-03-24T10:14:11-05:00",
+          "tree_id": "ec0f2bd628c9542509eedeecb9a999487f71fb59",
+          "url": "https://github.com/unicode-org/icu4x/commit/a4c650b3cf434a6a803b7e1e2c8443740a5ae3f7"
+        },
+        "date": 1648135280789,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 130011,
+            "range": "± 576",
             "unit": "ns/iter"
           }
         ]
