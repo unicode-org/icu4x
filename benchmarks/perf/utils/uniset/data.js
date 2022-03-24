c90a5861c864bafc1f8e3fe19176e672ad7b1f0e
github-action-benchmark <github@users.noreply.github.com>
2022-03-24 02:16:08 +0000

    add Rust Benchmark (cargo) benchmark result for eae38373145ad911bc7b063a3753c062fbcbca0d

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648083649779,
+  "lastUpdate": 1648088168161,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22379,6 +22379,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 37277856,
             "range": "± 1371958",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eae38373145ad911bc7b063a3753c062fbcbca0d",
+          "message": "Add ZeroMap::get_copied_by (#1722)",
+          "timestamp": "2022-03-23T19:09:38-07:00",
+          "tree_id": "5dc0bd6896086265fbea0ec16ccabdaec3309498",
+          "url": "https://github.com/unicode-org/icu4x/commit/eae38373145ad911bc7b063a3753c062fbcbca0d"
+        },
+        "date": 1648088153936,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 55025760,
+            "range": "± 646396",
             "unit": "ns/iter"
           }
         ]
