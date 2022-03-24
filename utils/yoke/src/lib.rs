ef25f50d83d05734bc3d8e54ca02cc0694d1bc09
Shane Carr <shane@unicode.org>
2022-03-24 16:59:34 -0700

    Re-fix yoke docs

--- utils/yoke/src/lib.rs
+++ utils/yoke/src/lib.rs
@@ -25,7 +25,7 @@
 //! See the documentation of [`Yoke`] for more details.
 
 // https://github.com/unicode-org/icu4x/blob/main/docs/process/boilerplate.md#library-annotations
-#![cfg_attr(not(test), no_std)]
+#![cfg_attr(all(not(test), not(doc)), no_std)]
 #![cfg_attr(
     not(test),
     deny(
