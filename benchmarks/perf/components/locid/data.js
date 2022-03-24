65054a5935b7646d2922aebec1bd11476498329f
github-action-benchmark <github@users.noreply.github.com>
2022-03-24 21:17:43 +0000

    add Rust Benchmark (cargo) benchmark result for 2180717961d049fb638b008ce5bb051e9a38fb72

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648156101122,
+  "lastUpdate": 1648156663582,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -26453,6 +26453,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4145,
             "range": "± 22",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2180717961d049fb638b008ce5bb051e9a38fb72",
+          "message": "Split off non-core functionality from icu_provider into icu_provider_adapters (#1730)",
+          "timestamp": "2022-03-24T14:10:14-07:00",
+          "tree_id": "435be6aecf50b3a1a6e6c5e2e5890efe34b78781",
+          "url": "https://github.com/unicode-org/icu4x/commit/2180717961d049fb638b008ce5bb051e9a38fb72"
+        },
+        "date": 1648156649166,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2162,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4091,
+            "range": "± 3",
             "unit": "ns/iter"
           }
         ]
