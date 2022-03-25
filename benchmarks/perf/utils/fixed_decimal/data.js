9b7667c40f5dc5cfb7ed0012be68aa0556bbe75f
github-action-benchmark <github@users.noreply.github.com>
2022-03-25 05:02:11 +0000

    add Rust Benchmark (cargo) benchmark result for c53194da5a76593641b818447d44846fbb538c78

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648172192146,
+  "lastUpdate": 1648184530972,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -21539,6 +21539,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 108054,
             "range": "± 410",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c53194da5a76593641b818447d44846fbb538c78",
+          "message": "Run docs tests that failed prior to Rust 1.57 and update links (#1732)",
+          "timestamp": "2022-03-24T21:56:03-07:00",
+          "tree_id": "cc8bd2f1dc1f0f419d82283bcac64ecf541d2013",
+          "url": "https://github.com/unicode-org/icu4x/commit/c53194da5a76593641b818447d44846fbb538c78"
+        },
+        "date": 1648184516025,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 107936,
+            "range": "± 384",
             "unit": "ns/iter"
           }
         ]
