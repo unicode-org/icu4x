372d922f86e5a2fd2c2eadb79daaaa7835d9d57c
github-action-benchmark <github@users.noreply.github.com>
2022-03-26 20:58:03 +0000

    add Rust Benchmark (cargo) benchmark result for 7eeafcc3fab59fc9c90d2df5cf9ca3664c7df8c2

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648184530972,
+  "lastUpdate": 1648328283443,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -21569,6 +21569,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 107936,
             "range": "± 384",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7eeafcc3fab59fc9c90d2df5cf9ca3664c7df8c2",
+          "message": "Add OptionULE and OptionVarULE (#1736)\n\n* Add OptionULE\n\n* tests\n\n* git add\n\n* add OptionVarULE\n\n* review comments\n\n* add KV impl for OptionVarULE\n\n* add test\n\n* clarify\n\n* fix\n\n* clip",
+          "timestamp": "2022-03-26T13:50:32-07:00",
+          "tree_id": "017a2bcf12eb14e899c778d2dffdb76f1861d268",
+          "url": "https://github.com/unicode-org/icu4x/commit/7eeafcc3fab59fc9c90d2df5cf9ca3664c7df8c2"
+        },
+        "date": 1648328268111,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 87764,
+            "range": "± 4710",
             "unit": "ns/iter"
           }
         ]
