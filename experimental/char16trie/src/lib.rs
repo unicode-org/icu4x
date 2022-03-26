a716e9942875ab6e0066aaf5f886149f35eec29f
Alex Touchet <alextouchet@outlook.com>
2022-03-26 13:56:07 -0700

    Fix broken crates.io badges

--- experimental/char16trie/src/lib.rs
+++ experimental/char16trie/src/lib.rs
@@ -2,7 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-//! # icu_char16trie [![crates.io](http://meritbadge.herokuapp.com/icu_char16trie)](https://crates.io/crates/icu_char16trie)
+//! # icu_char16trie [![crates.io](https://img.shields.io/crates/v/icu_char16trie)](https://crates.io/crates/icu_char16trie)
 //!
 //! `icu_char16trie` is a utility crate of the [`ICU4X`] project.
 //!
