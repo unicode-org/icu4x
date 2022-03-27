d0363420d51b739efadf798b328ba1d1f3b2e757
github-action-benchmark <github@users.noreply.github.com>
2022-03-27 23:05:19 +0000

    add Rust Benchmark (cargo) benchmark result for 4c700aa91c8ce080fab9aecebd43c498367c601a

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648328283443,
+  "lastUpdate": 1648422319716,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -21599,6 +21599,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 87764,
             "range": "± 4710",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4c700aa91c8ce080fab9aecebd43c498367c601a",
+          "message": "Merge CLDR, UProps, and Segmenter providers into icu_datagen (#1740)",
+          "timestamp": "2022-03-27T15:58:12-07:00",
+          "tree_id": "4c52f627a427a2816a920ee668f4ed58c7de3681",
+          "url": "https://github.com/unicode-org/icu4x/commit/4c700aa91c8ce080fab9aecebd43c498367c601a"
+        },
+        "date": 1648422302012,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 117791,
+            "range": "± 11504",
             "unit": "ns/iter"
           }
         ]
