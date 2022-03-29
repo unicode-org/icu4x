0ae357756999804fc819705a943076f4a857e474
github-action-benchmark <github@users.noreply.github.com>
2022-03-29 16:14:25 +0000

    add Rust Benchmark (cargo) benchmark result for 1fdaa3570b217512f690c4a9c3715406c03758df

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648530849707,
+  "lastUpdate": 1648570465707,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22859,6 +22859,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 45974458,
             "range": "± 2053810",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1fdaa3570b217512f690c4a9c3715406c03758df",
+          "message": "Using the cached-path crate for downloading and unzipping (#1744)",
+          "timestamp": "2022-03-29T09:06:22-07:00",
+          "tree_id": "c2960ed28a65da17c2b073d9127ced5ab3c7cebd",
+          "url": "https://github.com/unicode-org/icu4x/commit/1fdaa3570b217512f690c4a9c3715406c03758df"
+        },
+        "date": 1648570447344,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 44863394,
+            "range": "± 2612968",
             "unit": "ns/iter"
           }
         ]
