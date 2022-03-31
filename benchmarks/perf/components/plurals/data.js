dac7104b9e704e24d3123ddd673d8fdcdeb3070c
github-action-benchmark <github@users.noreply.github.com>
2022-03-31 17:10:43 +0000

    add Rust Benchmark (cargo) benchmark result for 93b64e3fec45d77ef391257e7f161f82d83f9e21

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648745253279,
+  "lastUpdate": 1648746643184,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -30995,6 +30995,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 16256,
             "range": "± 725",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "93b64e3fec45d77ef391257e7f161f82d83f9e21",
+          "message": "impl_auto_deserializing doc (#1755)",
+          "timestamp": "2022-03-31T19:01:57+02:00",
+          "tree_id": "d27bdb16dd6871bcdfedfc9c8cc4a7f9dcd3df02",
+          "url": "https://github.com/unicode-org/icu4x/commit/93b64e3fec45d77ef391257e7f161f82d83f9e21"
+        },
+        "date": 1648746627353,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 877,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 5182,
+            "range": "± 20",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 12876,
+            "range": "± 39",
             "unit": "ns/iter"
           }
         ]
