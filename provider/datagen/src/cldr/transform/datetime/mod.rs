18e79ea9bb64e684b1567445d79e2509b4035a1b
Shane F. Carr <shane@unicode.org>
2022-03-30 23:32:59 -0700

    fix

--- provider/datagen/src/cldr/transform/datetime/mod.rs
+++ provider/datagen/src/cldr/transform/datetime/mod.rs
@@ -46,7 +46,6 @@ macro_rules! impl_resource_provider {
                 ) -> Result<DataResponse<$marker>, DataError> {
                     let langid = req.langid();
                     let variant = req.options.temp_get_extension("ca")
-                    .map(writeable::Writeable::write_to_string)
                     .ok_or_else(|| DataErrorKind::NeedsVariant.with_req(<$marker>::KEY, req))?;
 
                     let dates = if let Some(dates) = self.data.get(&req.options) {
