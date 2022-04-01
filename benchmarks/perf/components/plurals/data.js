0e049067f05c17fa7a78b378708ddc1273808dbe
github-action-benchmark <github@users.noreply.github.com>
2022-04-01 02:28:16 +0000

    add Rust Benchmark (cargo) benchmark result for 8b97e42cbd2dbcbcd3abf7ee350c0153361f1f64

--- benchmarks/perf/components/plurals/data.js
+++ benchmarks/perf/components/plurals/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648748488494,
+  "lastUpdate": 1648780095964,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -31079,6 +31079,48 @@ window.BENCHMARK_DATA = {
             "name": "plurals/pluralrules/overview",
             "value": 13867,
             "range": "± 11",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8b97e42cbd2dbcbcd3abf7ee350c0153361f1f64",
+          "message": "Add unicode_key! macro (#1759)",
+          "timestamp": "2022-03-31T19:19:14-07:00",
+          "tree_id": "6b31a5b178ce04432488fd895b3180639c54fff7",
+          "url": "https://github.com/unicode-org/icu4x/commit/8b97e42cbd2dbcbcd3abf7ee350c0153361f1f64"
+        },
+        "date": 1648780079494,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "plurals/operands/overview",
+            "value": 913,
+            "range": "± 67",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/parser/overview",
+            "value": 6234,
+            "range": "± 294",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "plurals/pluralrules/overview",
+            "value": 14099,
+            "range": "± 700",
             "unit": "ns/iter"
           }
         ]
