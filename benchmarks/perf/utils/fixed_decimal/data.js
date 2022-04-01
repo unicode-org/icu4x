15a4a83484147c94f3b73d31c3f789eeaf4401cf
github-action-benchmark <github@users.noreply.github.com>
2022-04-01 18:36:20 +0000

    add Rust Benchmark (cargo) benchmark result for 1df7fa8511472130d34359a50027a4bd08076315

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648828263102,
+  "lastUpdate": 1648838180763,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22229,6 +22229,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 129848,
             "range": "± 1128",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1df7fa8511472130d34359a50027a4bd08076315",
+          "message": "Prep work for #1760 (#1761)",
+          "timestamp": "2022-04-01T20:29:13+02:00",
+          "tree_id": "a6411e5c03ac43562d785be7ee6a55aa096edce9",
+          "url": "https://github.com/unicode-org/icu4x/commit/1df7fa8511472130d34359a50027a4bd08076315"
+        },
+        "date": 1648838162574,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 114651,
+            "range": "± 5621",
             "unit": "ns/iter"
           }
         ]
