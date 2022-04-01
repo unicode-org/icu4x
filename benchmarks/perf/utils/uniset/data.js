714d366ac41b6c2942f62974c3f704bf3217331a
github-action-benchmark <github@users.noreply.github.com>
2022-04-01 23:56:39 +0000

    add Rust Benchmark (cargo) benchmark result for e265c6023a7fcad356632274177359e1ab9d5fa8

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648849600068,
+  "lastUpdate": 1648857399649,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23369,6 +23369,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 49432260,
             "range": "± 100165",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e265c6023a7fcad356632274177359e1ab9d5fa8",
+          "message": "Removing CLDR 40 compatibility (#1765)",
+          "timestamp": "2022-04-01T16:49:32-07:00",
+          "tree_id": "f7b6f073f4e51636631f8efea7de4046c891b82b",
+          "url": "https://github.com/unicode-org/icu4x/commit/e265c6023a7fcad356632274177359e1ab9d5fa8"
+        },
+        "date": 1648857380804,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 45036746,
+            "range": "± 1744365",
             "unit": "ns/iter"
           }
         ]
