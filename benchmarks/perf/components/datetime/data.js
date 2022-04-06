2ae154348e1a030fd9d342fee26467bef8d9468c
github-action-benchmark <github@users.noreply.github.com>
2022-04-06 06:39:19 +0000

    add Rust Benchmark (cargo) benchmark result for ea907ef79f513765a4986a2574599bb6ff747665

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649211132360,
+  "lastUpdate": 1649227158919,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -25295,6 +25295,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 149693,
             "range": "± 556",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ea907ef79f513765a4986a2574599bb6ff747665",
+          "message": "Enable pluggable LiteMap backends (#1769)",
+          "timestamp": "2022-04-05T23:31:12-07:00",
+          "tree_id": "5fadd42ab1aebcac401ee2e590e3bcfbf8bbb72f",
+          "url": "https://github.com/unicode-org/icu4x/commit/ea907ef79f513765a4986a2574599bb6ff747665"
+        },
+        "date": 1649227144711,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 64873,
+            "range": "± 765",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 818196,
+            "range": "± 4260",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 150366,
+            "range": "± 566",
             "unit": "ns/iter"
           }
         ]
