da208a5b4ba5b1a8a7cc41ed38e838d3e0f90d65
github-action-benchmark <github@users.noreply.github.com>
2022-04-06 17:55:11 +0000

    add Rust Benchmark (cargo) benchmark result for 3ebc5b28ed38785f3d94dd6168490878e6a2084e

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649253074629,
+  "lastUpdate": 1649267710856,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22649,6 +22649,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 112764,
             "range": "± 3711",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pandusonu@google.com",
+            "name": "Gollapudi Vamsi Krishna",
+            "username": "pandusonu2"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3ebc5b28ed38785f3d94dd6168490878e6a2084e",
+          "message": "Add Indian Calendar to cldr (#1757)\n\n* Add Indian calendar to cldr\r\n\r\n* rustfmt\r\n\r\n* Fix saka era name",
+          "timestamp": "2022-04-06T17:46:33Z",
+          "tree_id": "ad991859ccd33d697c32b44f270eef168e6c9f6f",
+          "url": "https://github.com/unicode-org/icu4x/commit/3ebc5b28ed38785f3d94dd6168490878e6a2084e"
+        },
+        "date": 1649267688346,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 136425,
+            "range": "± 8109",
             "unit": "ns/iter"
           }
         ]
