deea917f7bf27ade1d02f3d5c666240bfea92f76
github-action-benchmark <github@users.noreply.github.com>
2022-04-06 21:58:29 +0000

    add Rust Benchmark (cargo) benchmark result for 248fbf581a09aff18f18f70c4faf73dc9de4a03a

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649267623102,
+  "lastUpdate": 1649282309719,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23729,6 +23729,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 50122424,
             "range": "± 667621",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "248fbf581a09aff18f18f70c4faf73dc9de4a03a",
+          "message": "Various ZeroVec derive improvements (#1780)\n\n* add semicolon in encodeasvar impl\r\n\r\n* clean up derive\r\n\r\n* Add more extensible derive/skip framework, support splitting Serialize/Deserialize\r\n\r\n* pass in attrs, add debug parsing\r\n\r\n* Add support for debug\r\n\r\n* docs\r\n\r\n* address",
+          "timestamp": "2022-04-06T21:51:02Z",
+          "tree_id": "0a71121cd13c476ea807a72918a689139f2ab3a3",
+          "url": "https://github.com/unicode-org/icu4x/commit/248fbf581a09aff18f18f70c4faf73dc9de4a03a"
+        },
+        "date": 1649282294264,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 40872054,
+            "range": "± 82153",
             "unit": "ns/iter"
           }
         ]
