d74f3d9e94510eb6fcc6e81149f3623e8ea413f0
github-action-benchmark <github@users.noreply.github.com>
2022-04-06 00:58:21 +0000

    add Rust Benchmark (cargo) benchmark result for 1193250b38d5ad227abd7578a89dc6dca9cae6b4

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649205916393,
+  "lastUpdate": 1649206700876,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -25127,6 +25127,48 @@ window.BENCHMARK_DATA = {
             "name": "datetime/zoned_datetime_overview",
             "value": 198192,
             "range": "± 9460",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1193250b38d5ad227abd7578a89dc6dca9cae6b4",
+          "message": "Add unicode_ext_value! macro, enabled by new helper ShortVec (#1767)",
+          "timestamp": "2022-04-05T17:49:53-07:00",
+          "tree_id": "23e15ce2ec1af33b8b344681cab7fecc44cf1131",
+          "url": "https://github.com/unicode-org/icu4x/commit/1193250b38d5ad227abd7578a89dc6dca9cae6b4"
+        },
+        "date": 1649206685981,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 77748,
+            "range": "± 315",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 815806,
+            "range": "± 5584",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 178926,
+            "range": "± 443",
             "unit": "ns/iter"
           }
         ]
