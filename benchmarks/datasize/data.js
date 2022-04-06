7d4fd0fb4a82352cf12bfa4eedb566954d7f1e59
github-action-benchmark <github@users.noreply.github.com>
2022-04-06 00:38:36 +0000

    add Benchmark (ndjson) benchmark result for e8e77d12f21331ddf210beab1335ffa92df08382

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649194674110,
+  "lastUpdate": 1649205516149,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -12623,6 +12623,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/2cdcd0c777ef63accc95ca7b8881f61ea71ea600"
         },
         "date": 1649194655676,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 4470980,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e8e77d12f21331ddf210beab1335ffa92df08382",
+          "message": "Minor fixes to yoke docs; publish new yoke/yoke-derive (#1773)\n\n* Move eithercart functions to yoke module\r\n\r\n* Mention more explicitly that the cart type is C\r\n\r\n* Bump yoke to 0.4.1, yoke-derive to 0.4.2",
+          "timestamp": "2022-04-06T00:36:53Z",
+          "tree_id": "5b273b613c7b91e536f252b97e938a92ea200e59",
+          "url": "https://github.com/unicode-org/icu4x/commit/e8e77d12f21331ddf210beab1335ffa92df08382"
+        },
+        "date": 1649205497580,
         "tool": "ndjson",
         "benches": [
           {
