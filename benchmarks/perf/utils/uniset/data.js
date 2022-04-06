34d1893b82fc57260938789e9f2a6e34f9b069de
github-action-benchmark <github@users.noreply.github.com>
2022-04-06 06:38:25 +0000

    add Rust Benchmark (cargo) benchmark result for ea907ef79f513765a4986a2574599bb6ff747665

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649211127219,
+  "lastUpdate": 1649227105359,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23639,6 +23639,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 39484609,
             "range": "± 1675612",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ea907ef79f513765a4986a2574599bb6ff747665",
+          "message": "Enable pluggable LiteMap backends (#1769)",
+          "timestamp": "2022-04-05T23:31:12-07:00",
+          "tree_id": "5fadd42ab1aebcac401ee2e590e3bcfbf8bbb72f",
+          "url": "https://github.com/unicode-org/icu4x/commit/ea907ef79f513765a4986a2574599bb6ff747665"
+        },
+        "date": 1649227090253,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 40692851,
+            "range": "± 84672",
             "unit": "ns/iter"
           }
         ]
