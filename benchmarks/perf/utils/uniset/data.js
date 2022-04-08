f544bd0913f759edafabc263c482a2cdb3f88dac
github-action-benchmark <github@users.noreply.github.com>
2022-04-08 16:43:29 +0000

    add Rust Benchmark (cargo) benchmark result for 6590b5ddaabab6666592224037f3eb2cab2c4ccb

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649424259629,
+  "lastUpdate": 1649436209262,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23909,6 +23909,36 @@ window.BENCHMARK_DATA = {
             "name": "uniset/overview",
             "value": 56974438,
             "range": "± 1033883",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6590b5ddaabab6666592224037f3eb2cab2c4ccb",
+          "message": "ZeroCopy aliases (#1777)",
+          "timestamp": "2022-04-08T18:35:53+02:00",
+          "tree_id": "e1f9b3f329e408e693c7b91f6f13c2c468c08b44",
+          "url": "https://github.com/unicode-org/icu4x/commit/6590b5ddaabab6666592224037f3eb2cab2c4ccb"
+        },
+        "date": 1649436190840,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 59099170,
+            "range": "± 572664",
             "unit": "ns/iter"
           }
         ]
