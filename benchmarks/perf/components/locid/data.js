6cf910d8791702426a124cddc5c80a0f388bb1ea
github-action-benchmark <github@users.noreply.github.com>
2022-04-08 02:05:46 +0000

    add Rust Benchmark (cargo) benchmark result for 967670b3e515ba1c37881959e867eb1e6d688b3a

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649374615789,
+  "lastUpdate": 1649383546673,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -28037,6 +28037,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 3664,
             "range": "± 5",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "967670b3e515ba1c37881959e867eb1e6d688b3a",
+          "message": "update cargo-all-features dep to ^1.7 (#1785)",
+          "timestamp": "2022-04-07T18:57:37-07:00",
+          "tree_id": "70f44ebd4c9aac510fe2a5c39bedc23830620661",
+          "url": "https://github.com/unicode-org/icu4x/commit/967670b3e515ba1c37881959e867eb1e6d688b3a"
+        },
+        "date": 1649383525878,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2375,
+            "range": "± 85",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4237,
+            "range": "± 234",
             "unit": "ns/iter"
           }
         ]
