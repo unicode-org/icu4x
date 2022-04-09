14f06c038a09dfe36e046c227b44f220e7c7e949
github-action-benchmark <github@users.noreply.github.com>
2022-04-09 00:21:11 +0000

    add Rust Benchmark (cargo) benchmark result for ebf0a95ec52e69d0fbbbcb3682396dbd8c597051

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1649436225799,
+  "lastUpdate": 1649463671448,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -22889,6 +22889,36 @@ window.BENCHMARK_DATA = {
             "name": "fixed_decimal/overview",
             "value": 113626,
             "range": "± 5941",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ebf0a95ec52e69d0fbbbcb3682396dbd8c597051",
+          "message": "Add note about Copy Error types to style guide (#1787)",
+          "timestamp": "2022-04-08T17:14:13-07:00",
+          "tree_id": "42000b85cd05045468dc930570c784e2113c6a08",
+          "url": "https://github.com/unicode-org/icu4x/commit/ebf0a95ec52e69d0fbbbcb3682396dbd8c597051"
+        },
+        "date": 1649463655018,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 105887,
+            "range": "± 368",
             "unit": "ns/iter"
           }
         ]
