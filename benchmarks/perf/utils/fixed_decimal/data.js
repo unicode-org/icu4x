c59ddaa8abb6eecdabefb1bf84c0708472c7ca17
github-action-benchmark <github@users.noreply.github.com>
2022-04-21 17:25:55 +0000

    add Rust Benchmark (cargo) benchmark result for b49d84ef82e31f0779837ba5b9662a6c181bb014

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1650472831422,
+  "lastUpdate": 1650561954955,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -23609,6 +23609,66 @@ window.BENCHMARK_DATA = {
             "name": "from_string/1000000001",
             "value": 71,
             "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "andrewpkq@gmail.com",
+            "name": "andrewpollack",
+            "username": "andrewpollack"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b49d84ef82e31f0779837ba5b9662a6c181bb014",
+          "message": "1712: add TinyAsciStr::is_ascii_alphabetic_lowercase, TinyAsciStr::is_ascii_alphabetic_titlecase, TinyAsciStr::is_ascii_alphabetic_uppercase (#1806)\n\n* Implementing is_ascii_alphabetic_lowercase, titlecase, uppercase\r\n\r\n* Minor adjustment to doc\r\n\r\n* Remove unnecessary comment\r\n\r\n* Optimizing bitops, adjusting comments",
+          "timestamp": "2022-04-21T17:15:37Z",
+          "tree_id": "804b6d435025cb386cc8083a6ec24aeeb22eb6c8",
+          "url": "https://github.com/unicode-org/icu4x/commit/b49d84ef82e31f0779837ba5b9662a6c181bb014"
+        },
+        "date": 1650561932951,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 120724,
+            "range": "± 5133",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 24757,
+            "range": "± 1125",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 68501,
+            "range": "± 3410",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 235,
+            "range": "± 13",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 97,
+            "range": "± 6",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 88,
+            "range": "± 3",
             "unit": "ns/iter"
           }
         ]
