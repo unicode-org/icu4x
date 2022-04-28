454d0b59009f1fb83c5d7fff252137caa8937788
github-action-benchmark <github@users.noreply.github.com>
2022-04-28 03:12:43 +0000

    add Benchmark (ndjson) benchmark result for c0dc998de3dac8318c1e126bd7f4caa4a258bac1

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1651076881594,
+  "lastUpdate": 1651115563137,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -13793,6 +13793,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/a47d4f52c91f88d6484f688a7c4941f894d5f567"
         },
         "date": 1651076863356,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 4507203,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pandusonu@google.com",
+            "name": "Gollapudi Vamsi Krishna",
+            "username": "pandusonu2"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c0dc998de3dac8318c1e126bd7f4caa4a258bac1",
+          "message": "Add Ethiopic calendar (#1779)\n\n* Add ethiopic calendar\n\n* Fix clippy\n\n* Add method to set amete alem in Date and DateTime\n\n* avoid using result\n\n* Fix prev commit\n\n* Make inner of date pub\n\n* Fix prev commit",
+          "timestamp": "2022-04-27T20:11:00-07:00",
+          "tree_id": "dd66d4cb6a26dfdc40284bdf7323551351f3d97d",
+          "url": "https://github.com/unicode-org/icu4x/commit/c0dc998de3dac8318c1e126bd7f4caa4a258bac1"
+        },
+        "date": 1651115546106,
         "tool": "ndjson",
         "benches": [
           {
