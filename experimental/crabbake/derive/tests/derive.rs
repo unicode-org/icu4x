56799ea876b96f0bb1bd5db51332fa6c1e912b04
Robert Bastian <robertbastian@users.noreply.github.com>
2022-04-29 11:21:10 +0200

    fix

--- experimental/crabbake/derive/tests/derive.rs
+++ experimental/crabbake/derive/tests/derive.rs
@@ -2,11 +2,12 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use crabbake::Bakeable;
+use crabbake::{Bakeable, CrateEnv};
 use quote::quote;
 use std::borrow::Cow;
 
 #[derive(Bakeable)]
+#[crabbake(path = test)]
 pub struct IntExample {
     x: u8,
 }
@@ -14,12 +15,13 @@ pub struct IntExample {
 #[test]
 fn test_int_example() {
     assert_eq!(
-        IntExample { x: 17 }.bake().to_string(),
-        quote! { IntExample { x: 17u8, }}.to_string()
+        IntExample { x: 17 }.bake(&CrateEnv::default()).to_string(),
+        quote! { ::test::IntExample { x: 17u8, }}.to_string()
     );
 }
 
 #[derive(Bakeable)]
+#[crabbake(path = test)]
 pub struct GenericsExample<T> {
     x: u32,
     y: T,
@@ -28,12 +30,15 @@ pub struct GenericsExample<T> {
 #[test]
 fn test_generics_example() {
     assert_eq!(
-        GenericsExample { x: 17, y: 100isize }.bake().to_string(),
-        quote! { GenericsExample { x: 17u32, y: 100isize, }}.to_string()
+        GenericsExample { x: 17, y: 100isize }
+            .bake(&CrateEnv::default())
+            .to_string(),
+        quote! { ::test::GenericsExample { x: 17u32, y: 100isize, }}.to_string()
     );
 }
 
 #[derive(Bakeable)]
+#[crabbake(path = test)]
 pub struct CowExample<'a> {
     x: u8,
     y: &'a str,
@@ -50,14 +55,14 @@ fn test_cow_example() {
             z: Cow::Borrowed("bar"),
             w: Cow::Borrowed(&[1, 2, 3]),
         }
-        .bake()
+        .bake(&CrateEnv::default())
         .to_string(),
         quote! {
-            CowExample {
+            ::test::CowExample {
                 x: 17u8,
                 y: "foo",
-                z: alloc::borrow::Cow::Borrowed("bar"),
-                w: alloc::borrow::Cow::Borrowed(&[1u8,2u8,3u8]),
+                z: ::alloc::borrow::Cow::Borrowed("bar"),
+                w: ::alloc::borrow::Cow::Borrowed(&[1u8,2u8,3u8]),
             }
         }
         .to_string()
