1199f96c2f4a00467e2bdb8bfa788c05755b0195
andrewpollack <andrewpkq@gmail.com>
2022-04-30 04:00:23 -0700

    Add negative 0 examples (#1807)

--- utils/fixed_decimal/src/decimal.rs
+++ utils/fixed_decimal/src/decimal.rs
@@ -310,6 +310,7 @@ impl FixedDecimal {
     }
 
     /// Change the value from negative to positive or from positive to negative, modifying self.
+    /// Negative zero is supported.
     ///
     /// # Examples
     ///
@@ -324,6 +325,15 @@ impl FixedDecimal {
     ///
     /// dec.negate();
     /// assert_eq!("42", dec.to_string());
+    ///
+    /// // Negative zero example
+    /// let zero = FixedDecimal::from(0);
+    /// let mut negative_zero = FixedDecimal::from(0);
+    /// negative_zero.negate();
+    ///
+    /// assert_eq!("0", zero.to_string());
+    /// assert_eq!("-0", negative_zero.to_string());
+    /// assert_ne!(zero, negative_zero);
     /// ```
     pub fn negate(&mut self) {
         self.is_negative = !self.is_negative;
@@ -948,6 +958,21 @@ impl FixedDecimal {
     /// )
     /// .expect("Finite, integer-valued quantity");
     /// assert_eq!(decimal.write_to_string(), "12345678000");
+    /// ```
+    ///
+    /// Negative zero is supported.
+    ///
+    /// ```rust
+    /// use fixed_decimal::{DoublePrecision, FixedDecimal};
+    /// use writeable::Writeable;
+    ///
+    /// // IEEE 754 for floating point defines the sign bit separate
+    /// // from the mantissa and exponent, allowing for -0.
+    /// let negative_zero = FixedDecimal::try_from_f64(
+    ///     -0.0,
+    ///      DoublePrecision::Integer
+    /// ).expect("Negative zero");
+    /// assert_eq!(negative_zero.write_to_string(), "-0");
     /// ```
     pub fn try_from_f64(float: f64, precision: DoublePrecision) -> Result<Self, Error> {
         let mut decimal = Self::new_from_f64_raw(float)?;
