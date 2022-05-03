7738b03d78d26e634fe8bdd1579e879ea955b18d
Erik Nordin <nordzilla@mozilla.com>
2022-05-03 14:10:37 -0700

    Remove unused use-declarations

--- components/datetime/tests/patterns/structs/time_zones.rs
+++ components/datetime/tests/patterns/structs/time_zones.rs
@@ -2,7 +2,6 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use icu_datetime::provider::time_zones::{MetaZoneId, TimeZoneBcp47Id};
 use icu_datetime::time_zone;
 use serde::{Deserialize, Serialize};
 use tinystr::{TinyAsciiStr, TinyStr8};
