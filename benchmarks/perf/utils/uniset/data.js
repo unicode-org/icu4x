2eebb7b976d669485a852b9d2d60a2fc4f16a79e
github-action-benchmark <github@users.noreply.github.com>
2022-05-05 15:54:33 +0000

    add Rust Benchmark (cargo) benchmark result for 8cd78d9fc38c44fca80884d87d5efc1c133cc381

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1651765651242,
+  "lastUpdate": 1651766073399,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -26368,6 +26368,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniset/contains_range/best",
             "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/worst",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "andrewpkq@gmail.com",
+            "name": "andrewpollack",
+            "username": "andrewpollack"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8cd78d9fc38c44fca80884d87d5efc1c133cc381",
+          "message": "Add `Date::new_from_iso(date_iso, X)` example for each calendar X (#1844)\n\n* Fixing typo\r\n\r\n* adding examples to top level calendars\r\n\r\n* Adding coptic example\r\n\r\n* Adding Buddhist Date example\r\n\r\n* Bringing up-to-date to main. Fixing Coptic",
+          "timestamp": "2022-05-05T08:45:14-07:00",
+          "tree_id": "bb304a9e88eeff5960b37a50a8498085c0490d6d",
+          "url": "https://github.com/unicode-org/icu4x/commit/8cd78d9fc38c44fca80884d87d5efc1c133cc381"
+        },
+        "date": 1651766050282,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 50956053,
+            "range": "± 1375433",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains/best",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains/worst",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/best",
+            "value": 3,
             "range": "± 0",
             "unit": "ns/iter"
           },
