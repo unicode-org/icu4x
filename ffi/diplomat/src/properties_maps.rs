2a873014e23de274bd08f384ef3dde1418367890
Robert Bastian <robertbastian@users.noreply.github.com>
2022-05-06 13:24:06 +0200

    erased ffi

--- ffi/diplomat/src/properties_maps.rs
+++ ffi/diplomat/src/properties_maps.rs
@@ -2,21 +2,25 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+struct ErasedCodePointMap16MarkerV1;
+
+use icu_provider::DataMarker;
+impl DataMarker for ErasedCodePointMap16MarkerV1 {
+    type Yokeable = <icu_properties::provider::ScriptV1Marker as DataMarker>::Yokeable;
+}
+
 #[diplomat::bridge]
 pub mod ffi {
-    use alloc::boxed::Box;
-    use icu_properties::{
-        maps::{self, CodePointMapResult},
-        provider::ScriptV1Marker,
-    };
-    use icu_provider::prelude::DataPayload;
-
+    use super::ErasedCodePointMap16MarkerV1;
     use crate::provider::ffi::ICU4XDataProvider;
+    use alloc::boxed::Box;
+    use icu_properties::maps::{self, CodePointMapResult};
+    use icu_provider::prelude::DataPayload;
 
     #[diplomat::opaque]
     /// An ICU4X Unicode Set Property object, capable of querying whether a code point is contained in a set based on a Unicode property. For properties whose values fit into 16 bits.
     #[diplomat::rust_link(icu_properties, Mod)]
-    pub struct ICU4XCodePointMapData16(DataPayload<ScriptV1Marker>);
+    pub struct ICU4XCodePointMapData16(DataPayload<ErasedCodePointMap16MarkerV1>);
 
     pub struct ICU4XCodePointMapData16Response {
         /// The [`ICU4XCodePointMapData16`], if creation was successful.
@@ -31,11 +35,11 @@ pub mod ffi {
         pub fn try_get_script(provider: &ICU4XDataProvider) -> ICU4XCodePointMapData16Response {
             use icu_provider::serde::AsDeserializingBufferProvider;
             let provider = provider.0.as_deserializing();
-            Self::prepare_result_from_script(maps::get_script(&provider))
+            Self::prepare_result_from_script(maps::get_script(&provider).map(DataPayload::cast))
         }
 
         fn prepare_result_from_script(
-            result: CodePointMapResult<ScriptV1Marker>,
+            result: CodePointMapResult<ErasedCodePointMap16MarkerV1>,
         ) -> ICU4XCodePointMapData16Response {
             match result {
                 Ok(data) => ICU4XCodePointMapData16Response {
