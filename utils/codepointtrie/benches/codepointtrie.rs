94fa1d12ea039645039b9379dd5ded3caa25fa85
Shane F. Carr <shane@unicode.org>
2022-05-09 14:32:47 +0200

    Add fast benches

--- utils/codepointtrie/benches/codepointtrie.rs
+++ utils/codepointtrie/benches/codepointtrie.rs
@@ -25,7 +25,7 @@ fn one_hundred_code_points(sample_str: &str) -> String {
     sample_str.chars().cycle().take(100).collect()
 }
 
-fn load_code_point_trie() -> CodePointTrie<'static, u8> {
+fn get_trie_small() -> CodePointTrie<'static, u8> {
     CodePointTrie::try_new(
         tries::gc_small::HEADER,
         tries::gc_small::INDEX.as_zerovec(),
@@ -34,34 +34,48 @@ fn load_code_point_trie() -> CodePointTrie<'static, u8> {
     .unwrap()
 }
 
+fn get_trie_fast() -> CodePointTrie<'static, u8> {
+    CodePointTrie::try_new(
+        tries::gc_fast::HEADER,
+        tries::gc_fast::INDEX.as_zerovec(),
+        tries::gc_fast::DATA.as_zerovec(),
+    )
+    .unwrap()
+}
+
 fn overview_bench(c: &mut Criterion) {
     let s = one_hundred_code_points(SAMPLE_STRING_MIXED);
-    let cpt = load_code_point_trie();
+    let cpt_small = get_trie_small();
+    let cpt_fast = get_trie_fast();
 
     c.bench_function("cpt/overview", |b| {
         b.iter(|| {
             black_box(&s)
                 .chars()
-                .map(|c| black_box(&cpt).get(c as u32))
+                .map(|c| black_box(&cpt_small).get(c as u32))
                 .reduce(|s, v| s.wrapping_add(v))
         });
     });
 
     #[cfg(feature = "bench")]
     {
-        lang_bench(c, "eng", sample_str_lng::ENG);
-        lang_bench(c, "pcd", sample_str_lng::PCD);
-        lang_bench(c, "ukr", sample_str_lng::UKR);
-        lang_bench(c, "yue", sample_str_lng::YUE);
-        lang_bench(c, "ccp", sample_str_lng::CCP);
+        lang_bench(c, &cpt_small, "small/eng", sample_str_lng::ENG);
+        lang_bench(c, &cpt_small, "small/pcd", sample_str_lng::PCD);
+        lang_bench(c, &cpt_small, "small/ukr", sample_str_lng::UKR);
+        lang_bench(c, &cpt_small, "small/yue", sample_str_lng::YUE);
+        lang_bench(c, &cpt_small, "small/ccp", sample_str_lng::CCP);
+        lang_bench(c, &cpt_fast, "fast/eng", sample_str_lng::ENG);
+        lang_bench(c, &cpt_fast, "fast/pcd", sample_str_lng::PCD);
+        lang_bench(c, &cpt_fast, "fast/ukr", sample_str_lng::UKR);
+        lang_bench(c, &cpt_fast, "fast/yue", sample_str_lng::YUE);
+        lang_bench(c, &cpt_fast, "fast/ccp", sample_str_lng::CCP);
     }
 }
 
 #[cfg(feature = "bench")]
-fn lang_bench(c: &mut Criterion, lid: &str, sample_str: &str) {
+fn lang_bench(c: &mut Criterion, cpt: &CodePointTrie<u8>, lid: &str, sample_str: &str) {
     let bench_name = format!("cpt/get/{}", lid);
     let s = one_hundred_code_points(sample_str);
-    let cpt = load_code_point_trie();
 
     c.bench_function(&bench_name, |b| {
         b.iter(|| {
--- utils/codepointtrie/benches/tries/mod.rs
+++ utils/codepointtrie/benches/tries/mod.rs
@@ -2,4 +2,5 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+pub mod gc_fast;
 pub mod gc_small;
