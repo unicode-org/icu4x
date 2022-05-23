326c2f245fc4fdf75e51005dd7abb4d823cf5a72
Shane F. Carr <shane@unicode.org>
2022-05-23 10:39:24 -0700

    Update nightly to 2022-02-17, the latest that supports clang-13 (#1928)

--- tools/benchmark/memory/src/main.rs
+++ tools/benchmark/memory/src/main.rs
@@ -74,7 +74,7 @@ fn process_cli_args() -> ProcessedArgs {
 
         toolchain: matches
             .value_of("TOOLCHAIN")
-            .unwrap_or("nightly-2022-01-31")
+            .unwrap_or("nightly-2022-02-17")
             .to_string(),
     }
 }
