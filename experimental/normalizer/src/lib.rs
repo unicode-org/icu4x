20d44e71ebdcb1609701f4a86258660ea2f8b105
Henri Sivonen <hsivonen@hsivonen.fi>
2022-05-25 09:21:56 +0300

    Split collator and normalizer tests out of lib.rs
    Closes #1912

--- experimental/normalizer/src/lib.rs
+++ experimental/normalizer/src/lib.rs
@@ -538,15 +538,4 @@ impl DecomposingNormalizer {
 }
 
 #[cfg(test)]
-mod tests {
-    use crate::DecomposingNormalizer;
-
-    #[test]
-    fn test_basic() {
-        let data_provider = icu_testdata::get_provider();
-
-        let normalizer: DecomposingNormalizer =
-            DecomposingNormalizer::try_new(&data_provider).unwrap();
-        assert_eq!(normalizer.normalize("ä"), "a\u{0308}");
-    }
-}
+mod tests;
