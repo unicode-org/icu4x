6fb1b21b6496a7925fdc95ede5394cf9c86703f8
Robert Bastian <robertbastian@users.noreply.github.com>
2022-05-25 17:50:50 -0700

    clippy

--- provider/blob/src/export/blob_exporter.rs
+++ provider/blob/src/export/blob_exporter.rs
@@ -29,7 +29,6 @@ impl<'w> BlobExporter<'w> {
 }
 
 impl DataExporter<SerializeMarker> for BlobExporter<'_> {
-    #[allow(clippy::unwrap_used)] // TODO(#1668) Clippy exceptions need docs or fixing.
     fn put_payload(
         &self,
         key: ResourceKey,
@@ -41,6 +40,7 @@ impl DataExporter<SerializeMarker> for BlobExporter<'_> {
             output: postcard::flavors::AllocVec(Vec::new()),
         };
         payload.serialize(&mut serializer)?;
+        #[allow(clippy::expect_used)]
         self.resources.lock().expect("poison").push((
             key.get_hash(),
             options.write_to_string().into_owned().into_bytes(),
@@ -49,8 +49,8 @@ impl DataExporter<SerializeMarker> for BlobExporter<'_> {
         Ok(())
     }
 
-    #[allow(clippy::unwrap_used)] // TODO(#1668) Clippy exceptions need docs or fixing.
     fn close(&mut self) -> Result<(), DataError> {
+        #[allow(clippy::expect_used)]
         let zm = self
             .resources
             .get_mut()
