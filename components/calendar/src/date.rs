20803f44ab3a5aa219dacd0a620f4e252a892450
Manish Goregaokar <manishsmail@gmail.com>
2022-05-31 14:26:20 -0700

    Add Ref type for calendars

--- components/calendar/src/date.rs
+++ components/calendar/src/date.rs
@@ -6,6 +6,7 @@ use crate::any_calendar::{AnyCalendar, IncludedInAnyCalendar};
 use crate::{types, Calendar, DateDuration, DateDurationUnit, Iso};
 use alloc::rc::Rc;
 use core::fmt;
+use core::ops::Deref;
 
 /// Types that contain a calendar
 ///
@@ -31,6 +32,30 @@ impl<C: Calendar> AsCalendar for Rc<C> {
     #[inline]
     fn as_calendar(&self) -> &C {
         &*self
+    }
+}
+
+/// This exists as a wrapper around `&'a T` so that
+/// `Date<&'a C>` is possible for calendar `C`. Unfortunately,
+/// [`AsCalendar`] cannot be implemented on `&'a T` directly because
+/// `&'a T` is `#[fundamental]` and the impl would clash with the one above with
+/// `AsCalendar` for `C: Calendar`.
+///
+/// Use `Date<Ref<'a, C>>` where you would use `Date<&'a C>`
+pub struct Ref<'a, C>(pub &'a C);
+
+impl<C: Calendar> AsCalendar for Ref<'_, C> {
+    type Calendar = C;
+    #[inline]
+    fn as_calendar(&self) -> &C {
+        self.0
+    }
+}
+
+impl<'a, C> Deref for Ref<'a, C> {
+    type Target = C;
+    fn deref(&self) -> &C {
+        self.0
     }
 }
 
