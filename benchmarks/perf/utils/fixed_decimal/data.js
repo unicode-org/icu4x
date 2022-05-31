c0333b077e58cbe1f8da37737409ed0d2404ca07
github-action-benchmark <github@users.noreply.github.com>
2022-05-31 16:33:18 +0000

    add Rust Benchmark (cargo) benchmark result for a2fad0d803a35cc4e8c82fb20c8999fc03e470d6

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1653691737079,
+  "lastUpdate": 1654014798404,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -29128,6 +29128,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "from_string/1000000001",
             "value": 86,
+            "range": "± 8",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jedel0124@gmail.com",
+            "name": "jedel1043",
+            "username": "jedel1043"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a2fad0d803a35cc4e8c82fb20c8999fc03e470d6",
+          "message": "Remove implicit `Sized` bounds on some functions (#1969)",
+          "timestamp": "2022-05-31T16:21:32Z",
+          "tree_id": "5cea5271808c699c1ad237f6467aafab331125a9",
+          "url": "https://github.com/unicode-org/icu4x/commit/a2fad0d803a35cc4e8c82fb20c8999fc03e470d6"
+        },
+        "date": 1654014776920,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 109949,
+            "range": "± 3870",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 19374,
+            "range": "± 765",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 53309,
+            "range": "± 4391",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 178,
+            "range": "± 20",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 74,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 80,
             "range": "± 8",
             "unit": "ns/iter"
           }
