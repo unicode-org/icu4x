f36a3b332085c88ce3a68aac67aa390301ee1dfe
Robert Bastian <robertbastian@users.noreply.github.com>
2022-06-03 15:54:21 -0700

    zip

--- provider/datagen/src/source.rs
+++ provider/datagen/src/source.rs
@@ -8,6 +8,8 @@ pub use icu_codepointtrie::TrieType;
 use icu_provider::DataError;
 use std::any::Any;
 use std::fmt::Debug;
+use std::fs::File;
+use std::io::Read;
 use std::path::Path;
 use std::path::PathBuf;
 use std::sync::Arc;
@@ -30,7 +32,11 @@ impl Default for SourceData {
             uprops_paths: None,
             coll_paths: None,
             segmenter_paths: Arc::new(TomlCache::new(
-                PathBuf::from(std::env!("CARGO_MANIFEST_DIR")).join("data"),
+                AbstractFs::new(
+                    PathBuf::from(std::env!("CARGO_MANIFEST_DIR")).join("data"),
+                    String::new(),
+                )
+                .unwrap(),
             )),
             trie_type: TrieType::Small,
         }
@@ -42,36 +48,45 @@ impl SourceData {
     /// a local `cldr-{version}-json-full.zip` directory (see [GitHub downloads](
     /// https://github.com/unicode-org/cldr-json/releases)), and `locale_subset`
     /// is a valid locale subset (currently either "full" or "modern").
-    pub fn with_cldr(self, root: PathBuf, locale_subset: String) -> Self {
-        Self {
-            cldr_paths: Some(Arc::new(CldrCache::new(root, locale_subset))),
+    pub fn with_cldr(self, root: PathBuf, locale_subset: String) -> Result<Self, DataError> {
+        Ok(Self {
+            cldr_paths: Some(Arc::new(CldrCache::new(
+                AbstractFs::new(root, String::new())?,
+                locale_subset,
+            ))),
             ..self
-        }
+        })
     }
 
     /// Adds Unicode Properties data to this `DataSource`. The path should
     /// point to a local `icuexportdata_uprops_full` directory (see
     /// [GitHub downloads](https://github.com/unicode-org/icu/releases)).
-    pub fn with_uprops(self, root: PathBuf, trie_type: TrieType) -> Self {
-        Self {
-            uprops_paths: Some(Arc::new(TomlCache::new(
-                root.join("icuexportdata_uprops_full")
-                    .join(match trie_type {
+    pub fn with_uprops(self, root: PathBuf, trie_type: TrieType) -> Result<Self, DataError> {
+        Ok(Self {
+            uprops_paths: Some(Arc::new(TomlCache::new(AbstractFs::new(
+                root,
+                format!(
+                    "icuexportdata_uprops_full/{}",
+                    match trie_type {
                         TrieType::Fast => "fast",
                         TrieType::Small => "small",
-                    }),
-            ))),
+                    }
+                ),
+            )?))),
             trie_type,
             ..self
-        }
+        })
     }
 
     /// Adds collation data to this `DataSource`.
-    pub fn with_coll(self, root: PathBuf) -> Self {
-        Self {
-            coll_paths: Some(Arc::new(TomlCache::new(root))),
+    pub fn with_coll(self, root: PathBuf) -> Result<Self, DataError> {
+        Ok(Self {
+            coll_paths: Some(Arc::new(TomlCache::new(AbstractFs::new(
+                root,
+                String::new(),
+            )?))),
             ..self
-        }
+        })
     }
 
     #[cfg(test)]
@@ -118,8 +133,8 @@ impl SourceData {
 }
 
 pub(crate) struct TomlCache {
-    root: PathBuf,
-    cache: Arc<FrozenMap<PathBuf, Box<dyn Any + Send + Sync>>>,
+    root: AbstractFs,
+    cache: Arc<FrozenMap<String, Box<dyn Any + Send + Sync>>>,
 }
 
 impl Debug for TomlCache {
@@ -132,29 +147,25 @@ impl Debug for TomlCache {
 }
 
 impl TomlCache {
-    pub fn new(root: PathBuf) -> Self {
+    pub fn new(root: AbstractFs) -> Self {
         Self {
             root,
             cache: Arc::new(FrozenMap::new()),
         }
     }
 
-    pub fn read_and_parse_toml<S, P: AsRef<Path>>(&self, path: P) -> Result<&S, DataError>
+    pub fn read_and_parse_toml<S>(&self, path: &str) -> Result<&S, DataError>
     where
         for<'de> S: serde::Deserialize<'de> + 'static + Send + Sync,
     {
-        let path = self.root.join(path);
-
-        if self.cache.get(&path).is_none() {
-            log::trace!("Reading: {:?}", &path);
-            let file = std::fs::read_to_string(&path)
-                .map_err(|e| DataError::from(e).with_path_context(&path))?;
-            let file: S = toml::from_str(&file)
-                .map_err(|e| crate::error::data_error_from_toml(e).with_path_context(&path))?;
-            self.cache.insert(path.clone(), Box::new(file));
+        if self.cache.get(path).is_none() {
+            let file = self.root.open(&path)?;
+            let file: S = toml::from_slice(&file)
+                .map_err(|e| crate::error::data_error_from_toml(e).with_display_context(&path))?;
+            self.cache.insert(path.to_string(), Box::new(file));
         }
         self.cache
-            .get(&path)
+            .get(path)
             .unwrap()
             .downcast_ref::<S>()
             .ok_or_else(|| DataError::custom("Uprops TOML error").with_type_context::<S>())
@@ -162,14 +173,67 @@ impl TomlCache {
 
     #[cfg_attr(not(feature = "experimental"), allow(dead_code))]
     pub fn list(&self) -> Result<impl Iterator<Item = PathBuf>, DataError> {
-        let mut result = vec![];
-        for entry in std::fs::read_dir(&self.root)
-            .map_err(|e| DataError::from(e).with_path_context(&self.root))?
-        {
-            let entry = entry.map_err(|e| DataError::from(e).with_path_context(&self.root))?;
-            let path = entry.path();
-            result.push(path);
+        self.root.list("").map(Vec::into_iter)
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum AbstractFs {
+    Fs(PathBuf),
+    // The PB points to a valid ZIP file, the String is a prefix inside the ZIP
+    Zip(PathBuf, String),
+}
+
+impl AbstractFs {
+    pub fn new<P: AsRef<Path>>(root: P, prefix: String) -> Result<Self, DataError> {
+        if std::fs::metadata(root.as_ref())?.is_dir() {
+            Ok(Self::Fs(root.as_ref().join(prefix)))
+        } else {
+            zip::ZipArchive::new(File::open(&root)?)
+                .map_err(|e| DataError::custom("Zip").with_display_context(&e))?;
+            Ok(Self::Zip(root.as_ref().into(), prefix))
         }
-        Ok(result.into_iter())
+    }
+
+    pub fn open(&self, path: &str) -> Result<Vec<u8>, DataError> {
+        match self {
+            Self::Fs(root) => {
+                log::trace!("Reading: {}/{}", root.display(), path);
+                std::fs::read(&root.join(path))
+                    .map_err(|e| DataError::from(e).with_path_context(&root.join(path)))
+            }
+            Self::Zip(root, prefix) => {
+                log::trace!("Reading: {}/{}", root.join(prefix).display(), path);
+                let mut buf = Vec::new();
+                zip::ZipArchive::new(File::open(root)?)
+                    .unwrap()
+                    .by_name(&format!("{}/{}", prefix, path))
+                    .map_err(|e| {
+                        DataError::custom("Zip")
+                            .with_display_context(&e)
+                            .with_display_context(path)
+                    })?
+                    .read_to_end(&mut buf)?;
+                Ok(buf)
+            }
+        }
+    }
+
+    pub fn list(&self, path: &str) -> Result<Vec<PathBuf>, DataError> {
+        Ok(match self {
+            Self::Fs(root) => std::fs::read_dir(&root.join(path))?
+                .map(|e| -> Result<_, DataError> { Ok(PathBuf::from(e?.file_name())) })
+                .collect::<Result<_, DataError>>()?,
+            Self::Zip(root, prefix) => zip::ZipArchive::new(File::open(root)?)
+                .unwrap()
+                .file_names()
+                .filter_map(|p| p.strip_prefix(prefix))
+                .filter_map(|p| p.strip_prefix("/"))
+                .filter_map(|p| p.strip_prefix(path))
+                .filter_map(|suffix| suffix.split('/').next())
+                .filter(|s| !s.is_empty())
+                .map(PathBuf::from)
+                .collect(),
+        })
     }
 }
