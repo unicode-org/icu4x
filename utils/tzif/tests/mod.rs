33109eabf05e714966e686b164d83ae596af11e9
Erik Nordin <nordzilla@mozilla.com>
2022-06-07 15:07:29 -0700

    Add integration test for POSIX string

--- utils/tzif/tests/mod.rs
+++ utils/tzif/tests/mod.rs
@@ -4,19 +4,20 @@
 
 use combine::{stream, Parser};
 use std::{fs::File, path::Path};
+use tzif::parse::posix::posix_tz_string;
 use tzif::parse::tzif::tzif;
 use walkdir::WalkDir;
 
 fn parse_tzif_file<P: AsRef<Path>>(path: P) {
     println!("parsing {:?}", path.as_ref().to_str());
-    let read = File::open(path).unwrap();
+    let file = File::open(path).unwrap();
     let stream = stream::buffered::Stream::new(
-        stream::position::Stream::new(stream::read::Stream::new(read)),
-        1,
+        stream::position::Stream::new(stream::read::Stream::new(file)),
+        0, /* lookahead */
     );
     let parsed = tzif().parse(stream);
     assert!(parsed.is_ok());
-    //println!("{:#?}", parsed.unwrap().0);
+    println!("{:#?}", parsed.unwrap().0);
 }
 
 #[test]
@@ -27,4 +28,11 @@ fn parse_tzif_testdata() {
             parse_tzif_file(entry.path())
         }
     }
+}
+
+#[test]
+fn parse_posix_tz_string() {
+    assert!(posix_tz_string()
+        .parse(b"WGT3WGST,M3.5.0/-2,M10.5.0/-1".as_slice())
+        .is_ok());
 }
