9cf2273bc5ac14bae986aabfd3b497b501176861
Erik Nordin <nordzilla@mozilla.com>
2022-06-07 10:20:01 -0700

    Add license header

--- experimental/tzif/tests/mod.rs
+++ experimental/tzif/tests/mod.rs
@@ -1,7 +1,10 @@
-use std::{fs::File, path::Path};
-use tzif::parse::tzif::tzif;
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
 use combine::{stream, Parser};
+use std::{fs::File, path::Path};
+use tzif::parse::tzif::tzif;
 use walkdir::WalkDir;
 
 fn parse_tzif_file<P: AsRef<Path>>(path: P) {
