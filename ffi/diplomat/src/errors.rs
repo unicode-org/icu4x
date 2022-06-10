c5ab594dee989d7187ed0376d0cee61e1af24b26
Manish Goregaokar <manishsmail@gmail.com>
2022-06-10 13:35:30 -0700

    Data errors

--- ffi/diplomat/src/errors.rs
+++ ffi/diplomat/src/errors.rs
@@ -3,6 +3,8 @@
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
 use core::fmt;
+use icu_provider::{DataError, DataErrorKind};
+use self::ffi::ICU4XError;
 
 #[diplomat::bridge]
 pub mod ffi {
@@ -28,11 +30,53 @@ pub mod ffi {
 
         /// Attempted to construct an invalid data struct
         DataStructValidityError = 4,
+
+        // See DataError
+        DataMissingResourceKeyError = 5,
+        DataMissingVariantError = 6,
+        DataMissingLocaleError = 7,
+        DataMissingResourceOptionsError = 8,
+        DataNeedsVariantError = 9,
+        DataNeedsLocaleError = 10,
+        DataExtraneousResourceOptionsError = 11,
+        DataFilteredResourceError = 12,
+        DataMismatchedTypeError = 13,
+        DataMissingPayloadError = 14,
+        DataInvalidStateError = 15,
+        DataCustomError = 16,
+        DataIoError = 17, // ????
+        DataUnavailableBufferFormatError = 18,
+
     }
 }
 
 impl From<fmt::Error> for ICU4XError {
     fn from(_: fmt::Error) -> Self {
         ICU4XError::WriteableError
+    }
+}
+
+impl From<DataError> for ICU4XError {
+    fn from(e: DataError) -> Self {
+        match e.kind {
+            DataErrorKind::MissingResourceKey => ICU4XError::DataMissingResourceKeyError,
+            DataErrorKind::MissingVariant => ICU4XError::DataMissingVariantError,
+            DataErrorKind::MissingLocale => ICU4XError::DataMissingLocaleError,
+            DataErrorKind::MissingResourceOptions => ICU4XError::DataMissingResourceOptionsError,
+            DataErrorKind::NeedsVariant => ICU4XError::DataNeedsVariantError,
+            DataErrorKind::NeedsLocale => ICU4XError::DataNeedsLocaleError,
+            DataErrorKind::ExtraneousResourceOptions => ICU4XError::DataExtraneousResourceOptionsError,
+            DataErrorKind::FilteredResource => ICU4XError::DataFilteredResourceError,
+            DataErrorKind::MismatchedType(..) => ICU4XError::DataMismatchedTypeError,
+            DataErrorKind::MissingPayload => ICU4XError::DataMissingPayloadError,
+            DataErrorKind::InvalidState => ICU4XError::DataInvalidStateError,
+            DataErrorKind::Custom => ICU4XError::DataCustomError,
+            // std only
+            // DataErrorKind::Io(..) => ICU4XError::DataIoError,
+            // datagen only
+            // DataErrorKind::MissingSourceData(..) => ..,
+            DataErrorKind::UnavailableBufferFormat(..) => ICU4XError::DataUnavailableBufferFormatError,
+            _ => ICU4XError::UnknownError,
+        }
     }
 }
