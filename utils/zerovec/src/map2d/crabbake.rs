c0096ed330a20448b2953618f8ce48cb5eaae7ea
Shane Carr <shane@unicode.org>
2022-06-13 19:31:07 -0700

    More impls in zerovec crate

--- utils/zerovec/src/map2d/crabbake.rs
+++ utils/zerovec/src/map2d/crabbake.rs
@@ -2,7 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use crate::{maps::ZeroMapKV, ZeroMap2d};
+use crate::{maps::ZeroMapKV, ZeroMap2d, maps::ZeroMap2dBorrowed};
 use crabbake::*;
 
 impl<'a, K0, K1, V> Bakeable for ZeroMap2d<'a, K0, K1, V>
@@ -21,5 +21,24 @@ where
         let keys1 = self.keys1.bake(env);
         let values = self.values.bake(env);
         quote! { unsafe { #[allow(unused_unsafe)] ::zerovec::ZeroMap2d::from_parts_unchecked(#keys0, #joiner, #keys1, #values) } }
+    }
+}
+
+impl<'a, K0, K1, V> Bakeable for ZeroMap2dBorrowed<'a, K0, K1, V>
+where
+    K0: ZeroMapKV<'a> + ?Sized,
+    K1: ZeroMapKV<'a> + ?Sized,
+    V: ZeroMapKV<'a> + ?Sized,
+    &'a K0::Slice: Bakeable,
+    &'a K1::Slice: Bakeable,
+    &'a V::Slice: Bakeable,
+{
+    fn bake(&self, env: &CrateEnv) -> TokenStream {
+        env.insert("zerovec");
+        let keys0 = self.keys0.bake(env);
+        let joiner = self.joiner.bake(env);
+        let keys1 = self.keys1.bake(env);
+        let values = self.values.bake(env);
+        quote! { unsafe { #[allow(unused_unsafe)] ::zerovec::maps::ZeroMap2dBorrowed::from_parts_unchecked(#keys0, #joiner, #keys1, #values) } }
     }
 }
--- utils/zerovec/src/varzerovec/crabbake.rs
+++ utils/zerovec/src/varzerovec/crabbake.rs
@@ -2,7 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use crate::{ule::VarULE, VarZeroVec};
+use crate::{ule::VarULE, VarZeroSlice, VarZeroVec};
 use crabbake::*;
 
 impl<T: VarULE + ?Sized> Bakeable for VarZeroVec<'_, T> {
@@ -11,5 +11,14 @@ impl<T: VarULE + ?Sized> Bakeable for VarZeroVec<'_, T> {
         let bytes = self.as_bytes();
         // Safe because self.as_bytes is a safe input
         quote! { unsafe { ::zerovec::VarZeroVec::from_bytes_unchecked(&[#(#bytes),*]) } }
+    }
+}
+
+impl<T: VarULE + ?Sized> Bakeable for &VarZeroSlice<T> {
+    fn bake(&self, env: &CrateEnv) -> TokenStream {
+        env.insert("zerovec");
+        let bytes = self.as_bytes();
+        // Safe because self.as_bytes is a safe input
+        quote! { unsafe { ::zerovec::VarZeroSlice::from_bytes_unchecked(&[#(#bytes),*]) } }
     }
 }
