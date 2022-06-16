5c2ac9f08c1dfeef83088209164ba07192e870b2
github-action-benchmark <github@users.noreply.github.com>
2022-06-16 00:34:30 +0000

    add Rust Benchmark (cargo) benchmark result for 6133b3158a30df9c921eb8de6b8526b6b0b42622

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655293696565,
+  "lastUpdate": 1655339670656,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -32206,6 +32206,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniset/contains_range/worst",
             "value": 10,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6133b3158a30df9c921eb8de6b8526b6b0b42622",
+          "message": "Add LocaleFallbacker with locale fallback algorithm (#2036)\n\n- Adds more methods to ResourceOptions for vertical fallback\r\n- Adds reverse direction LSR conversion function\r\n- Changes clear() methods in icu_locid to return the old value",
+          "timestamp": "2022-06-15T17:22:41-07:00",
+          "tree_id": "5721aed1b49ea842d29c120a96f0c2eb9a7fe4af",
+          "url": "https://github.com/unicode-org/icu4x/commit/6133b3158a30df9c921eb8de6b8526b6b0b42622"
+        },
+        "date": 1655339640255,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 35104825,
+            "range": "± 1677613",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains/best",
+            "value": 12,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains/worst",
+            "value": 13,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/best",
+            "value": 12,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/worst",
+            "value": 12,
             "range": "± 0",
             "unit": "ns/iter"
           }
