7076db8eed2c4fcc5d0d08a5b53187d38f925da8
Manish Goregaokar <manishsmail@gmail.com>
2022-06-24 14:25:28 -0700

    Fix FFI

--- ffi/diplomat/src/properties_sets.rs
+++ ffi/diplomat/src/properties_sets.rs
@@ -2,20 +2,11 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-struct ErasedCodePointSetMarkerV1;
-
-use icu_provider::DataMarker;
-impl DataMarker for ErasedCodePointSetMarkerV1 {
-    type Yokeable = <icu_properties::provider::AsciiHexDigitV1Marker as DataMarker>::Yokeable;
-}
-
 #[diplomat::bridge]
 pub mod ffi {
-    use super::ErasedCodePointSetMarkerV1;
     use crate::provider::ffi::ICU4XDataProvider;
     use alloc::boxed::Box;
     use icu_properties::sets;
-    use icu_provider::prelude::DataPayload;
 
     use crate::errors::ffi::ICU4XError;
     use diplomat_runtime::DiplomatResult;
@@ -23,7 +14,7 @@ pub mod ffi {
     #[diplomat::opaque]
     /// An ICU4X Unicode Set Property object, capable of querying whether a code point is contained in a set based on a Unicode property.
     #[diplomat::rust_link(icu_properties, Mod)]
-    pub struct ICU4XCodePointSetData(DataPayload<ErasedCodePointSetMarkerV1>);
+    pub struct ICU4XCodePointSetData(sets::CodePointSetData);
 
     impl ICU4XCodePointSetData {
         /// Gets a set for Unicode property ascii_hex_digit from a [`ICU4XDataProvider`].
@@ -34,7 +25,7 @@ pub mod ffi {
             use icu_provider::serde::AsDeserializingBufferProvider;
             let provider = provider.0.as_deserializing();
             sets::get_ascii_hex_digit(&provider)
-                .map(|data| Box::new(ICU4XCodePointSetData(DataPayload::cast(data))))
+                .map(|data| Box::new(ICU4XCodePointSetData(data)))
                 .map_err(Into::into)
                 .into()
         }
@@ -42,7 +33,7 @@ pub mod ffi {
         /// Checks whether the code point is in the set.
         #[diplomat::rust_link(icu_uniset::UnicodeSet::contains, FnInStruct)]
         pub fn contains(&self, cp: char) -> bool {
-            self.0.get().inv_list.contains(cp)
+            self.0.contains(cp)
         }
     }
 }
