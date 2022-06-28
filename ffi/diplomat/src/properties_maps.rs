2568d2a7b392e2820579ae091bac54c50f99fbae
Manish Goregaokar <manishsmail@gmail.com>
2022-06-28 11:46:51 -0700

    Better properties return values (#2112)
    * Add CodePointSetData
    
    * find and replace tests
    
    * Manual test fixes
    
    * Type erase it
    
    * Add CodePointMapData
    
    * find-and-replace crate::
    
    * fix cargo check
    
    * find-and-replace tests
    
    * find-and-replace doctests
    
    * fix tests
    
    * fix bidi
    
    * fix props test
    
    * more fixes
    
    * Fix FFI
    
    * Fixup normalizer & collator
    
    * Update datagen
    
    * readme
    
    * clip
    
    * fix tests
    
    * rm unused result type
    
    * cast
    
    * Add to_unicode_set()
    
    * update test
    
    * Add doctests for sets
    
    * Remove range-specific code, add conversions to/from unicode sets / CPTs instead
    
    * more examples
    
    * fixes
    
    * fix
    
    * add more doctest

--- ffi/diplomat/src/properties_maps.rs
+++ ffi/diplomat/src/properties_maps.rs
@@ -2,20 +2,11 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-struct ErasedCodePointMap16MarkerV1;
-
-use icu_provider::DataMarker;
-impl DataMarker for ErasedCodePointMap16MarkerV1 {
-    type Yokeable = <icu_properties::provider::ScriptV1Marker as DataMarker>::Yokeable;
-}
-
 #[diplomat::bridge]
 pub mod ffi {
-    use super::ErasedCodePointMap16MarkerV1;
     use crate::provider::ffi::ICU4XDataProvider;
     use alloc::boxed::Box;
-    use icu_properties::maps;
-    use icu_provider::prelude::DataPayload;
+    use icu_properties::{maps, Script};
 
     use crate::errors::ffi::ICU4XError;
     use diplomat_runtime::DiplomatResult;
@@ -23,7 +14,7 @@ pub mod ffi {
     #[diplomat::opaque]
     /// An ICU4X Unicode Set Property object, capable of querying whether a code point is contained in a set based on a Unicode property. For properties whose values fit into 16 bits.
     #[diplomat::rust_link(icu_properties, Mod)]
-    pub struct ICU4XCodePointMapData16(DataPayload<ErasedCodePointMap16MarkerV1>);
+    pub struct ICU4XCodePointMapData16(maps::CodePointMapData<Script>);
 
     pub struct ICU4XCodePointMapData16Response {
         /// The [`ICU4XCodePointMapData16`], if creation was successful.
@@ -41,7 +32,7 @@ pub mod ffi {
             use icu_provider::serde::AsDeserializingBufferProvider;
             let provider = provider.0.as_deserializing();
             maps::get_script(&provider)
-                .map(|data| Box::new(ICU4XCodePointMapData16(DataPayload::cast(data))))
+                .map(|data| Box::new(ICU4XCodePointMapData16(data)))
                 .map_err(Into::into)
                 .into()
         }
@@ -49,7 +40,7 @@ pub mod ffi {
         /// Gets the value for a code point.
         #[diplomat::rust_link(icu_codepointtrie::codepointtrie::CodePointTrie::get_u32, FnInStruct)]
         pub fn get(&self, cp: char) -> u16 {
-            self.0.get().code_point_trie.get(cp.into()).0
+            self.0.get(cp).0
         }
     }
 }
