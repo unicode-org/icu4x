b2cb3cd21af7dd5a3cde151dff39cea3f035506c
Robert Bastian <robertbastian@users.noreply.github.com>
2022-07-12 13:48:10 +0200

    datagen

--- provider/datagen/src/error.rs
+++ provider/datagen/src/error.rs
@@ -4,16 +4,27 @@
 
 use icu_provider::{DataError, DataErrorKind};
 
+pub(crate) const MISSING_CLDR_ERROR: DataError =
+    DataErrorKind::MissingSourceData.with_str_context("cldr");
+
+pub(crate) const MISSING_ICUEXPORT_ERROR: DataError =
+    DataErrorKind::MissingSourceData.with_str_context("icuexport");
+
 /// Identifies errors that are due to missing CLDR data.
 ///
 /// See [`datagen`](crate::datagen).
-pub const MISSING_CLDR_ERROR: DataError = DataErrorKind::MissingSourceData.with_str_context("cldr");
+pub fn is_missing_cldr_error(mut e: DataError) -> bool {
+    e.key = None;
+    e == MISSING_CLDR_ERROR
+}
 
 /// Identifies errors that are due to missing ICU export data.
 ///
 /// See [`datagen`](crate::datagen).
-pub const MISSING_ICUEXPORT_ERROR: DataError =
-    DataErrorKind::MissingSourceData.with_str_context("icuexport");
+pub fn is_missing_icuexport_error(mut e: DataError) -> bool {
+    e.key = None;
+    e == MISSING_ICUEXPORT_ERROR
+}
 
 pub(crate) fn data_error_from_toml(other: toml::de::Error) -> DataError {
     DataError::custom("Toml deserialize").with_display_context(&other)
