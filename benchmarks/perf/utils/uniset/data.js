499b54e987edecbb93fb7f24ae9fec78c2ed1e65
github-action-benchmark <github@users.noreply.github.com>
2022-07-13 09:36:17 +0000

    add Rust Benchmark (cargo) benchmark result for 1175630884e7ae7e700f5ea8501f4865afbad901

--- benchmarks/perf/utils/uniset/data.js
+++ benchmarks/perf/utils/uniset/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1657684341840,
+  "lastUpdate": 1657704976862,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -36094,6 +36094,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniset/contains_range/worst",
             "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1175630884e7ae7e700f5ea8501f4865afbad901",
+          "message": "Add helloworld to testdata (#2173)",
+          "timestamp": "2022-07-13T11:21:20+02:00",
+          "tree_id": "2517ce6523bdf3c720a086bb71f1cd0fed9f35f7",
+          "url": "https://github.com/unicode-org/icu4x/commit/1175630884e7ae7e700f5ea8501f4865afbad901"
+        },
+        "date": 1657704929230,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 44116810,
+            "range": "± 2027959",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains/best",
+            "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains/worst",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/best",
+            "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uniset/contains_range/worst",
+            "value": 3,
             "range": "± 0",
             "unit": "ns/iter"
           }
