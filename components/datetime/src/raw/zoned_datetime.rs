8c1fe5412d2a737e2e376f8f6e1920f2f6387fe0
Manish Goregaokar <manishsmail@gmail.com>
2022-07-16 16:27:27 -0700

    fix

--- components/datetime/src/raw/zoned_datetime.rs
+++ components/datetime/src/raw/zoned_datetime.rs
@@ -151,7 +151,7 @@ impl ZonedDateTimeFormatter {
             decimal_provider,
             fixed_decimal_format_options,
         )
-        .map_err(DateTimeFormatterError::FixedDecimalFormat)?;
+        .map_err(DateTimeFormatterError::FixedDecimalFormatter)?;
 
         let datetime_format = raw::DateTimeFormatter::new(
             locale,
