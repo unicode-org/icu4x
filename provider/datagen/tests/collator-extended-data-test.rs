7fbe5832ecf4b55fa92b7d651bf4ce1ea9792acc
Shane F. Carr <shane@unicode.org>
2022-07-20 22:38:38 -0700

    Move test_fi

--- provider/datagen/tests/collator-extended-data-test.rs
+++ provider/datagen/tests/collator-extended-data-test.rs
@@ -44,6 +44,67 @@ fn check_expectations(collator: &Collator, cases: &[TestCase<'_>]) {
 }
 
 #[test]
+fn test_fi() {
+    // Adapted from ficoll.cpp in ICU4C
+    // Testing that w and v behave as in the root collation is for checking
+    // that the sorting collation doesn't exhibit the behavior of the search
+    // collation, which (somewhat questionably) treats w and v as primary-equal.
+    let cases = [
+        TestCase {
+            left: "wat",
+            right: "vat",
+            expectation: Ordering::Greater,
+        },
+        TestCase {
+            left: "vat",
+            right: "way",
+            expectation: Ordering::Less,
+        },
+        TestCase {
+            left: "aübeck",
+            right: "axbeck",
+            expectation: Ordering::Greater,
+        },
+        TestCase {
+            left: "Låvi",
+            right: "Läwe",
+            expectation: Ordering::Less,
+        },
+        // ICU4C has a duplicate of the case below.
+        // The duplicate is omitted here.
+        // Instead, the subsequent tests are added for ICU4X.
+        TestCase {
+            left: "ä",
+            right: "o",
+            expectation: Ordering::Greater,
+        },
+        TestCase {
+            left: "a\u{0308}",
+            right: "ä",
+            expectation: Ordering::Equal,
+        },
+    ];
+    let locale: Locale = langid!("fi").into();
+    let data_provider = icu_testdata::get_provider();
+
+    let mut options = CollatorOptions::new();
+    options.set_strength(Some(Strength::Tertiary));
+
+    {
+        let collator: Collator =
+            Collator::try_new(locale.clone(), &data_provider, options).unwrap();
+        check_expectations(&collator, &cases);
+    }
+
+    options.set_strength(Some(Strength::Primary));
+
+    {
+        let collator: Collator = Collator::try_new(locale, &data_provider, options).unwrap();
+        check_expectations(&collator, &cases);
+    }
+}
+
+#[test]
 fn test_sv() {
     // This is the same as test_fi. The purpose of this copy is to test that
     // Swedish defaults to "reformed", which behaves like Finnish "standard",
