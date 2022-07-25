01a9276b27f78373553fda7237abc9faa0e543a6
github-action-benchmark <github@users.noreply.github.com>
2022-07-25 11:36:27 +0000

    add Rust Benchmark (cargo) benchmark result for fc84c164bc06ad830455ca49a5f3ab7705d1385f

--- benchmarks/perf/components/datetime/data.js
+++ benchmarks/perf/components/datetime/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1658472022448,
+  "lastUpdate": 1658748987163,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -50759,6 +50759,102 @@ window.BENCHMARK_DATA = {
             "name": "pattern/parse",
             "value": 5101,
             "range": "± 122",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "m_kato@ga2.so-net.ne.jp",
+            "name": "Makoto Kato",
+            "username": "makotokato"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fc84c164bc06ad830455ca49a5f3ab7705d1385f",
+          "message": "Use multiple dictionaries for line/word segmenter. (#2209)\n\n* Use multiple dictionaries for line/word segmenter.\r\n\r\n* Remove unnecessary script mapping since I use data provider to get UAX#24 data.\r\n\r\n* Fix comment of test\r\n\r\n* Remove more complex defines from word segmenter data.\r\n\r\n* Don't load dictionary data on line segmenter when using lstm.\r\n\r\n* Don't load east asian language dictionaries on word segmenter when using lstm.\r\n\r\n* Replace array with struct for Dictionary payloads.\r\n\r\n* Fix build error.\r\n\r\n* Move the owner of dictionary payload from *Segmenter to Dictionary.\r\n\r\n* Add issue number of TODO comment",
+          "timestamp": "2022-07-25T20:22:44+09:00",
+          "tree_id": "ec120d68d65567853d652cf64fb8b91cf901f2b3",
+          "url": "https://github.com/unicode-org/icu4x/commit/fc84c164bc06ad830455ca49a5f3ab7705d1385f"
+        },
+        "date": 1658748952133,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "datetime/datetime_lengths",
+            "value": 100311,
+            "range": "± 897",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/datetime_components",
+            "value": 928965,
+            "range": "± 3079",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/zoned_datetime_overview",
+            "value": 197660,
+            "range": "± 2459",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/DateTimeFormatter/format_to_write",
+            "value": 89616,
+            "range": "± 355",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/DateTimeFormatter/format_to_string",
+            "value": 103292,
+            "range": "± 6126",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/FormattedDateTime/format",
+            "value": 99627,
+            "range": "± 385",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/FormattedDateTime/to_string",
+            "value": 106944,
+            "range": "± 1098",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/ZonedDateTimeFormatter/format_to_write",
+            "value": 185230,
+            "range": "± 355",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/ZonedDateTimeFormatter/format_to_string",
+            "value": 197849,
+            "range": "± 581",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/FormattedZonedDateTime/format",
+            "value": 196190,
+            "range": "± 2997",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "datetime/FormattedZonedDateTime/to_string",
+            "value": 209699,
+            "range": "± 1505",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pattern/parse",
+            "value": 5118,
+            "range": "± 46",
             "unit": "ns/iter"
           }
         ]
