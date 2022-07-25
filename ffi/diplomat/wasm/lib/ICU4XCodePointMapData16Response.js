1b4f5539af5f12769450956c040bed7e1b8ab1d4
Quinn Okabayashi <qokabay1@swarthmore.edu>
2022-07-25 16:56:33 -0700

    update paths

--- ffi/diplomat/wasm/lib/ICU4XCodePointMapData16Response.js
+++ ffi/diplomat/wasm/lib/ICU4XCodePointMapData16Response.js
@@ -1,4 +1,4 @@
-import wasm from "../wasm.mjs"
+import wasm from "./diplomat-wasm.mjs"
 import * as diplomatRuntime from "./diplomat-runtime.js"
 import { ICU4XCodePointMapData16 } from "./ICU4XCodePointMapData16.js"
 
--- ffi/diplomat/wasm/lib/ICU4XFixedDecimalFormatter.js
+++ ffi/diplomat/wasm/lib/ICU4XFixedDecimalFormatter.js
@@ -1,4 +1,4 @@
-import wasm from "../wasm.mjs"
+import wasm from "./diplomat-wasm.mjs"
 import * as diplomatRuntime from "./diplomat-runtime.js"
 import { ICU4XError_js_to_rust, ICU4XError_rust_to_js } from "./ICU4XError.js"
 import { ICU4XFixedDecimalGroupingStrategy_js_to_rust, ICU4XFixedDecimalGroupingStrategy_rust_to_js } from "./ICU4XFixedDecimalGroupingStrategy.js"
