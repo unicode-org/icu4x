cace3ab81fe2494925cded76c250881d8483d068
Younies Mahmoud <younies.mahmoud@gmail.com>
2022-07-27 16:07:02 +0200

    fix the naming for all the places.

--- ffi/diplomat/src/fixed_decimal.rs
+++ ffi/diplomat/src/fixed_decimal.rs
@@ -100,22 +100,22 @@ pub mod ffi {
         }
 
         /// Zero-pad the [`ICU4XFixedDecimal`] on the left to a particular position
-        #[diplomat::rust_link(fixed_decimal::decimal::FixedDecimal::pad_left, FnInStruct)]
-        pub fn pad_left(&mut self, position: i16) {
-            self.0.pad_left(position)
+        #[diplomat::rust_link(fixed_decimal::decimal::FixedDecimal::pad_start, FnInStruct)]
+        pub fn pad_start(&mut self, position: i16) {
+            self.0.pad_start(position)
         }
 
         /// Truncate the [`ICU4XFixedDecimal`] on the left to a particular position, deleting digits if necessary. This is useful for, e.g. abbreviating years
         /// ("2022" -> "22")
-        #[diplomat::rust_link(fixed_decimal::decimal::FixedDecimal::truncate_left, FnInStruct)]
-        pub fn truncate_left(&mut self, position: i16) {
-            self.0.truncate_left(position)
+        #[diplomat::rust_link(fixed_decimal::decimal::FixedDecimal::set_max_position, FnInStruct)]
+        pub fn set_max_position(&mut self, position: i16) {
+            self.0.set_max_position(position)
         }
 
         /// Zero-pad the [`ICU4XFixedDecimal`] on the right to a particular position
-        #[diplomat::rust_link(fixed_decimal::decimal::FixedDecimal::pad_right, FnInStruct)]
-        pub fn pad_right(&mut self, position: i16) {
-            self.0.pad_right(position)
+        #[diplomat::rust_link(fixed_decimal::decimal::FixedDecimal::pad_end, FnInStruct)]
+        pub fn pad_end(&mut self, position: i16) {
+            self.0.pad_end(position)
         }
 
         /// Format the [`ICU4XFixedDecimal`] as a string.
