b4763fb9cfe5abe811b7f98373bba36c83953eaa
Robert Bastian <robertbastian@users.noreply.github.com>
2022-07-27 18:11:02 +0200

    doc

--- provider/datagen/src/transform/mod.rs
+++ provider/datagen/src/transform/mod.rs
@@ -4,15 +4,6 @@
 
 //! This module contains implementations of the [`ICU4X`] [data provider] interface
 //! based on CLDR and ICU export data.
-//!
-//! This module exports feature-specific providers. Use [`crate::create_datagen_provider`]
-//! for a provider that covers all ICU4X keys.
-//!
-//! **Important:** These data provider implementations are not optimized
-//! for production use. Read more in the [data provider] docs.
-//!
-//! [`ICU4X`]: ../icu/index.html
-//! [data provider]: icu_provider
 
 pub mod cldr;
 pub mod icuexport;
