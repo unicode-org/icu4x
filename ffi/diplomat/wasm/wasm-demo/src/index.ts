18d1aa16636c2883f708d2ca38d505b300ff5363
Quinn Okabayashi <qokabay1@swarthmore.edu>
2022-07-27 18:03:43 -0700

    demo app

---
+++ ffi/diplomat/wasm/wasm-demo/src/index.ts
@@ -0,0 +1,14 @@
+import { ICU4XFixedDecimal, ICU4XLocale, ICU4XDataProvider, ICU4XFixedDecimalFormatter, ICU4XFixedDecimalGroupingStrategy } from "icu4x";
+
+const locale = ICU4XLocale.create_bn();
+
+const data_provider = ICU4XDataProvider.create_test();
+
+const fdf = ICU4XFixedDecimalFormatter.try_new(locale, data_provider, ICU4XFixedDecimalGroupingStrategy.Auto);
+
+export function format(n: number): string {
+    if (n > 2147483647 || n < -2147483648 || n % 1 !== 0) {
+        throw Error(`Not an i32: ${n}`);
+    }
+    return fdf.format(ICU4XFixedDecimal.create(n));
+}
