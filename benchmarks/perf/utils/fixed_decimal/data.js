237a324e6345bf52ef3c6d35cd4554a066ff78c5
github-action-benchmark <github@users.noreply.github.com>
2022-07-30 01:48:46 +0000

    add Rust Benchmark (cargo) benchmark result for 0980af95605c54c51258e56c0feeae92a7ba39af

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659140147880,
+  "lastUpdate": 1659145726052,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -41129,6 +41129,66 @@ window.BENCHMARK_DATA = {
             "name": "from_string/1000000001",
             "value": 80,
             "range": "± 5",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0980af95605c54c51258e56c0feeae92a7ba39af",
+          "message": "Add TimeVariant wrapper (#2289)\n\n* Add TimeVariant wrapper\n\n* fix\n\n* clippy\n\n* fix\n\n* fix2",
+          "timestamp": "2022-07-29T18:37:13-07:00",
+          "tree_id": "be82359a578f1b1e6392ff8875b140cd9abb8aa3",
+          "url": "https://github.com/unicode-org/icu4x/commit/0980af95605c54c51258e56c0feeae92a7ba39af"
+        },
+        "date": 1659145688216,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 105390,
+            "range": "± 468",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 21279,
+            "range": "± 92",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 64388,
+            "range": "± 269",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 168,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 65,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 70,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
