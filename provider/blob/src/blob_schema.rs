00e70952d3a6d478d806f33c85e06e4bbbb69d3a
Manish Goregaokar <manishsmail@gmail.com>
2022-08-02 16:14:52 -0700

    fmt

--- provider/blob/src/blob_schema.rs
+++ provider/blob/src/blob_schema.rs
@@ -2,7 +2,6 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-
 use alloc::boxed::Box;
 use icu_provider::prelude::*;
 use zerovec::maps::{ZeroMap2dBorrowed, ZeroMapKV};
