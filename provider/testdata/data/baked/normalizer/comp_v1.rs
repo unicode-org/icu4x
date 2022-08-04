5f6ce805d5a6099ef1b61af2a7f3cf62ef02ce50
Elango <elango@unicode.org>
2022-08-04 14:29:04 -0700

    Move experimental/char16trie to component/collections (#2336)

--- provider/testdata/data/baked/normalizer/comp_v1.rs
+++ provider/testdata/data/baked/normalizer/comp_v1.rs
@@ -3,7 +3,7 @@ type DataStruct = < :: icu_normalizer :: provider :: CanonicalCompositionsV1Mark
 pub static DATA: litemap::LiteMap<&str, &DataStruct, &[(&str, &DataStruct)]> =
     litemap::LiteMap::from_sorted_slice_unchecked(&[("und", UND)]);
 static UND: &DataStruct = &::icu_normalizer::provider::CanonicalCompositionsV1 {
-    canonical_compositions: ::icu_char16trie::char16trie::Char16Trie {
+    canonical_compositions: ::icu_collections::char16trie::Char16Trie {
         data: unsafe {
             ::zerovec::ZeroVec::from_bytes_unchecked(&[
                 0u8, 0u8, 56u8, 0u8, 66u8, 3u8, 128u8, 2u8, 194u8, 12u8, 65u8, 1u8, 223u8, 13u8,
