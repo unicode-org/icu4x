08ea51f5e15e568089733b49e7ddc0c817612585
Quinn Okabayashi <qokabay1@swarthmore.edu>
2022-08-04 12:42:06 -0700

    segmenter demo

--- ffi/diplomat/wasm/wasm-demo/src/ts/app.ts
+++ ffi/diplomat/wasm/wasm-demo/src/ts/app.ts
@@ -1,6 +1,7 @@
 import { ICU4XDataProvider } from 'icu4x';
 import * as fdf from './fixed-decimal';
 import * as dtf from './date-time';
+import * as seg from './segmenter';
 
 import '../scss/styles.scss';
 import 'bootstrap/js/dist/tab';
@@ -11,5 +12,5 @@ const dataProvider = ICU4XDataProvider.create_test();
 
 fdf.setup(dataProvider);
 dtf.setup(dataProvider);
-
+seg.setup(dataProvider);
 
