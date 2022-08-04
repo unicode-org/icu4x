02f8fdec81de55055ea946b0ea18efdafda36523
github-action-benchmark <github@users.noreply.github.com>
2022-08-04 16:33:42 +0000

    add Rust Benchmark (cargo) benchmark result for 8e6289931a594401ca9ce0422d4386190ff5b98a

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659627479384,
+  "lastUpdate": 1659630822622,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -43228,6 +43228,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "from_string/1000000001",
             "value": 73,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8e6289931a594401ca9ce0422d4386190ff5b98a",
+          "message": "ListFormatter and LocaleFallbacker constructor fixups (#2334)",
+          "timestamp": "2022-08-04T09:21:16-07:00",
+          "tree_id": "fca7d81c9a1cc6d4a926be689e3e20e2d5a31ad1",
+          "url": "https://github.com/unicode-org/icu4x/commit/8e6289931a594401ca9ce0422d4386190ff5b98a"
+        },
+        "date": 1659630780159,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 108185,
+            "range": "± 460",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 22856,
+            "range": "± 139",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 66587,
+            "range": "± 428",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 170,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 65,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 74,
             "range": "± 0",
             "unit": "ns/iter"
           }
