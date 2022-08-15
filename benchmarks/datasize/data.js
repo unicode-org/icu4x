c1bf1ed35e1f1dc52bf872955ccac8b2953294e9
github-action-benchmark <github@users.noreply.github.com>
2022-08-15 06:37:30 +0000

    add Benchmark (ndjson) benchmark result for 5f6dd9d0982d4170d44cf58ed9cb0838e785c40b

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660347882423,
+  "lastUpdate": 1660545450329,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -24053,6 +24053,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/606d98c9e49f052db01d522b06521db5df7e0027"
         },
         "date": 1660347841411,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 3924376,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cad97@cad97.com",
+            "name": "Christopher Durham",
+            "username": "CAD97"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5f6dd9d0982d4170d44cf58ed9cb0838e785c40b",
+          "message": "impl EncodeAsVarULE for Cow (#2376)",
+          "timestamp": "2022-08-14T23:35:23-07:00",
+          "tree_id": "80165192bb57dd70e4d65cfa452e3a2c2d8fe357",
+          "url": "https://github.com/unicode-org/icu4x/commit/5f6dd9d0982d4170d44cf58ed9cb0838e785c40b"
+        },
+        "date": 1660545405940,
         "tool": "ndjson",
         "benches": [
           {
