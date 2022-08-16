unicode-org/icu4x

commit 5f6dd9d0982d4170d44cf58ed9cb0838e785c40b

impl EncodeAsVarULE for Cow (#2376)

utils/zerovec/src/ule/encode.rs
@@ -5,6 +5,7 @@
 use crate::ule::*;
 use crate::varzerovec::VarZeroVecFormat;
 use crate::{VarZeroSlice, VarZeroVec, ZeroSlice, ZeroVec};
+use alloc::borrow::Cow;
 use alloc::boxed::Box;
 use alloc::string::String;
 use alloc::vec::Vec;
@@ -107,6 +108,15 @@ unsafe impl<T: VarULE + ?Sized> EncodeAsVarULE<T> for T {
 unsafe impl<T: VarULE + ?Sized> EncodeAsVarULE<T> for &'_ T {
     fn encode_var_ule_as_slices<R>(&self, cb: impl FnOnce(&[&[u8]]) -> R) -> R {
         cb(&[T::as_byte_slice(self)])
+    }
+}
+
+unsafe impl<T: VarULE + ?Sized> EncodeAsVarULE<T> for Cow<'_, T>
+where
+    T: Clone,
+{
+    fn encode_var_ule_as_slices<R>(&self, cb: impl FnOnce(&[&[u8]]) -> R) -> R {
+        cb(&[T::as_byte_slice(self.as_ref())])
     }
 }
 

commit 270e64a8a63b9ff705f0b38425c34abed0983e27

Generalize impl EncodeAsVarULE for Cow
Fixes: 5f6dd9d0982d4170d44cf58ed9cb0838e785c40b

utils/zerovec/src/ule/encode.rs
@@ -5,7 +5,7 @@
 use crate::ule::*;
 use crate::varzerovec::VarZeroVecFormat;
 use crate::{VarZeroSlice, VarZeroVec, ZeroSlice, ZeroVec};
-use alloc::borrow::Cow;
+use alloc::borrow::{Cow, ToOwned};
 use alloc::boxed::Box;
 use alloc::string::String;
 use alloc::vec::Vec;
@@ -113,7 +113,7 @@ unsafe impl<T: VarULE + ?Sized> EncodeAsVarULE<T> for &'_ T {
 
 unsafe impl<T: VarULE + ?Sized> EncodeAsVarULE<T> for Cow<'_, T>
 where
-    T: Clone,
+    T: ToOwned,
 {
     fn encode_var_ule_as_slices<R>(&self, cb: impl FnOnce(&[&[u8]]) -> R) -> R {
         cb(&[T::as_byte_slice(self.as_ref())])