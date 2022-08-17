eba42b60f5c691b50071a159c62c0718987bdf3f
github-action-benchmark <github@users.noreply.github.com>
2022-08-17 22:10:31 +0000

    add Rust Benchmark (cargo) benchmark result for f66fff7a1107465836615fecb007e1fb31f210b5

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660771838943,
+  "lastUpdate": 1660774231122,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -45196,6 +45196,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "to_string/to_string/908070605040302010",
             "value": 202,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 95,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 89,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f66fff7a1107465836615fecb007e1fb31f210b5",
+          "message": "Apply missing_docs to icu_provider_adapters (#2392)",
+          "timestamp": "2022-08-17T14:55:05-07:00",
+          "tree_id": "b1afb6f8d64ebb6bb39c78c1c9d8c3a830b5ae8e",
+          "url": "https://github.com/unicode-org/icu4x/commit/f66fff7a1107465836615fecb007e1fb31f210b5"
+        },
+        "date": 1660774189230,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 122275,
+            "range": "± 511",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 25247,
+            "range": "± 151",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 72210,
+            "range": "± 75",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 219,
             "range": "± 0",
             "unit": "ns/iter"
           },
