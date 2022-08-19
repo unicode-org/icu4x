948628fa3b3a8c79ac5c86cdd56b99b80568275b
Manish Goregaokar <manishsmail@gmail.com>
2022-08-19 09:53:48 -0700

    Add properties FFI for all types (#2405)
    * add try_into_converted
    
    * Use in ffi
    
    * rm result type
    
    * Add ffi for remaining map properties
    
    * regen
    
    * some more tests
    
    * fmt
    
    * clippy
    
    * rm CCC
    
    * check in added files

--- ffi/diplomat/wasm/icu4x/lib/ICU4XCodePointSetData.d.ts
+++ ffi/diplomat/wasm/icu4x/lib/ICU4XCodePointSetData.d.ts
@@ -24,7 +24,7 @@ export class ICU4XCodePointSetData {
 
    * Checks whether the code point is in the set.
 
-   * See the {@link https://unicode-org.github.io/icu4x-docs/doc/icu/collections/codepointinvlist/struct.CodePointSet.html#method.contains Rust documentation} for more information.
+   * See the {@link https://unicode-org.github.io/icu4x-docs/doc/icu/properties/sets/struct.CodePointSetDataBorrowed.html#method.contains Rust documentation} for more information.
    */
   contains(cp: char): boolean;
 }
