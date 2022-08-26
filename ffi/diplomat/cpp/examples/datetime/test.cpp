5daf1d91e9d4836eb01282505d6e11f4519274e0
Manish Goregaokar <manishsmail@gmail.com>
2022-08-25 20:44:24 -0700

    fix tests

--- ffi/diplomat/cpp/examples/datetime/test.cpp
+++ ffi/diplomat/cpp/examples/datetime/test.cpp
@@ -46,7 +46,7 @@ int main() {
     }
 
     locale = ICU4XLocale::create("en-u-ca-japanese").ok().value();
-    ICU4XCalendar cal = ICU4XCalendar::try_new(dp, locale).ok().value();
+    ICU4XCalendar cal = ICU4XCalendar::try_new_for_locale(dp, locale).ok().value();
     ICU4XDateTime any_date = ICU4XDateTime::try_new_from_iso_in_calendar(2020, 10, 5, 13, 33, 15, cal).ok().value();
     ICU4XDateTimeFormatter any_dtf = ICU4XDateTimeFormatter::try_new(dp, locale, ICU4XDateLength::Medium, ICU4XTimeLength::Short).ok().value();
     out = any_dtf.format_datetime(any_date).ok().value();
