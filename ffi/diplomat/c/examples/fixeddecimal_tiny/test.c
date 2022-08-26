092bbd9456763752d24f417bb6dcad051a3476ec
Shane Carr <shane@unicode.org>
2022-08-25 18:36:06 -0700

    Fix another compile

--- ffi/diplomat/c/examples/fixeddecimal_tiny/test.c
+++ ffi/diplomat/c/examples/fixeddecimal_tiny/test.c
@@ -9,10 +9,10 @@
 int main() {
     ICU4XLocale* locale = ICU4XLocale_create_bn();
     ICU4XDataProvider* provider = ICU4XDataProvider_create_test();
-    ICU4XFixedDecimal* decimal = ICU4XFixedDecimal_create(1000007);
+    ICU4XFixedDecimal* decimal = ICU4XFixedDecimal_create_from_u64(1000007);
 
     diplomat_result_box_ICU4XFixedDecimalFormatter_ICU4XError fdf_result =
-        ICU4XFixedDecimalFormatter_try_new(provider, locale, ICU4XFixedDecimalGroupingStrategy_Auto);
+        ICU4XFixedDecimalFormatter_try_new_with_grouping_strategy(provider, locale, ICU4XFixedDecimalGroupingStrategy_Auto);
     if (!fdf_result.is_ok)  {
         printf("Failed to create FixedDecimalFormatter\n");
         return 1;
