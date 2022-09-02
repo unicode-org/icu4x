080b8e4dec6e99f57184db6f50896e82ac0d0eb1
Shane F. Carr <shane@unicode.org>
2022-09-01 21:27:10 -0700

    Fix features in tests

--- components/datetime/src/provider/date_time.rs
+++ components/datetime/src/provider/date_time.rs
@@ -156,6 +156,7 @@ where
         match options {
             DateTimeFormatterOptions::Length(bag) => selector
                 .pattern_for_length_bag(bag, Some(preferences::Bag::from_data_locale(locale))),
+            #[cfg(feature = "experimental")]
             #[allow(clippy::panic)] // explicit panic in experimental mode
             _ => panic!("Non-experimental constructor cannot handle experimental options"),
         }
