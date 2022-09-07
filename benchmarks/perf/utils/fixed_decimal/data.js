8a16fccd5277032a576ff9700bd232cbb7a6697a
github-action-benchmark <github@users.noreply.github.com>
2022-09-07 16:49:45 +0000

    add Rust Benchmark (cargo) benchmark result for 5cf865d5047d02b78496427da9a50fc9745ed591

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662520786384,
+  "lastUpdate": 1662569385495,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -49528,6 +49528,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "from_string/1000000001",
             "value": 74,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5cf865d5047d02b78496427da9a50fc9745ed591",
+          "message": "Adding `Display` for our `Writeable` types (#2518)",
+          "timestamp": "2022-09-07T18:37:07+02:00",
+          "tree_id": "e85100ffd3efb400e06607bb429bba5cc96da09b",
+          "url": "https://github.com/unicode-org/icu4x/commit/5cf865d5047d02b78496427da9a50fc9745ed591"
+        },
+        "date": 1662569337251,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 113204,
+            "range": "± 985",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 25226,
+            "range": "± 247",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 70442,
+            "range": "± 103",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 185,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 95,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 73,
             "range": "± 0",
             "unit": "ns/iter"
           }
