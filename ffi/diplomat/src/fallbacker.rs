c3a6d21cebec04b145c29c29c0188e1d29d58b3d
Shane Carr <shane@unicode.org>
2022-09-12 14:13:21 -0700

    Initial ICU4XLocaleFallbacker and ICU4XLocaleFallbackerWithConfig

---
+++ ffi/diplomat/src/fallbacker.rs
@@ -0,0 +1,86 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+use icu_provider_adapters::fallback::LocaleFallbackConfig;
+
+#[diplomat::bridge]
+pub mod ffi {
+    use alloc::boxed::Box;
+    use diplomat_runtime::DiplomatResult;
+    use icu_decimal::{
+        options::{FixedDecimalFormatterOptions, GroupingStrategy},
+        provider::DecimalSymbolsV1Marker,
+        FixedDecimalFormatter,
+    };
+    use icu_locid::Locale;
+    use icu_provider::DataProvider;
+    use icu_provider::FallbackPriority;
+    use icu_provider::KeyedDataMarker;
+    use icu_provider_adapters::fallback::LocaleFallbackConfig;
+    use icu_provider_adapters::fallback::LocaleFallbacker;
+    use icu_provider_adapters::fallback::LocaleFallbackerWithConfig;
+    use writeable::Writeable;
+
+    use crate::{
+        data_struct::ffi::ICU4XDataStruct, errors::ffi::ICU4XError,
+        fixed_decimal::ffi::ICU4XFixedDecimal, locale::ffi::ICU4XLocale,
+        provider::ffi::ICU4XDataProvider,
+    };
+
+    #[diplomat::opaque]
+    #[diplomat::rust_link(icu_provider_adapters::fallback::LocaleFallbacker, Struct)]
+    pub struct ICU4XLocaleFallbacker(pub LocaleFallbacker);
+
+    #[diplomat::enum_convert(FallbackPriority, needs_wildcard)]
+    #[diplomat::rust_link(icu_provider::FallbackPriority, Enum)]
+    pub enum ICU4XLocaleFallbackPriority {
+        Language,
+        Region,
+        Collation,
+    }
+
+    #[diplomat::rust_link(icu_provider_adapters::fallback::LocaleFallbackConfig, Struct)]
+    pub struct ICU4XLocaleFallbackConfig<'a> {
+        pub priority: ICU4XLocaleFallbackPriority,
+        /// An empty string is considered `None`.
+        pub extension_key: &'a str,
+    }
+
+    #[diplomat::opaque]
+    #[diplomat::rust_link(icu_provider_adapters::fallback::LocaleFallbacker, Struct)]
+    pub struct ICU4XLocaleFallbackerWithConfig<'a>(pub LocaleFallbackerWithConfig<'a>);
+
+    impl ICU4XLocaleFallbacker {
+        pub fn create(provider: &ICU4XDataProvider) -> DiplomatResult<Self, ICU4XError> {
+            LocaleFallbacker::try_new_unstable(&provider.0)
+                .map(ICU4XLocaleFallbacker)
+                .map_err(Into::into)
+                .into()
+        }
+
+        pub fn for_config<'a, 'temp>(
+            &'a self,
+            config: ICU4XLocaleFallbackConfig<'temp>,
+        ) -> DiplomatResult<ICU4XLocaleFallbackerWithConfig<'a>, ICU4XError> {
+            match LocaleFallbackConfig::try_from(config) {
+                Ok(config) => Ok(ICU4XLocaleFallbackerWithConfig(self.0.for_config(config))),
+                Err(e) => Err(e),
+            }
+            .into()
+        }
+    }
+}
+
+impl TryFrom<ffi::ICU4XLocaleFallbackConfig<'_>> for LocaleFallbackConfig {
+    type Error = crate::errors::ffi::ICU4XError;
+    fn try_from(other: ffi::ICU4XLocaleFallbackConfig) -> Result<Self, Self::Error> {
+        let mut result = LocaleFallbackConfig::default();
+        result.priority = other.priority.into();
+        result.extension_key = match other.extension_key {
+            "" => None,
+            s => Some(s.parse()?),
+        };
+        Ok(result)
+    }
+}
