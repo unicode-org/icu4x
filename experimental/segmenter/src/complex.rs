87928ef1680d003087fdba93b019004b04b7b52e
Elango Cheran <elango@unicode.org>
2022-09-20 10:53:11 -0700

    Remove unused imports

--- experimental/segmenter/src/complex.rs
+++ experimental/segmenter/src/complex.rs
@@ -145,10 +145,6 @@ pub fn complex_language_segment_str(
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use icu_locid::locale;
-    use icu_provider::prelude::*;
-
     #[test]
     #[cfg(feature = "serde")]
     fn thai_word_break() {
