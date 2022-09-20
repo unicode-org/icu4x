16c90be4247f0d35852ee6cd55c63e5c4c2680a6
Pawan Dogra <35614614+pdogr@users.noreply.github.com>
2022-09-21 01:57:15 +0530

    Add NicheBytes trait and NichedOptionULE (#2501)
    * Add NicheBytes trait, NichedOption, NichedOptionULE
    
    * Add ULE types for NonZeroU8, NonZeroI8

--- utils/zerovec/src/map/kv.rs
+++ utils/zerovec/src/map/kv.rs
@@ -73,7 +73,7 @@ impl<'a> ZeroMapKV<'a> for usize {
 
 impl<'a, T> ZeroMapKV<'a> for Option<T>
 where
-    T: AsULE + 'static,
+    Option<T>: AsULE + 'static,
 {
     type Container = ZeroVec<'a, Option<T>>;
     type Slice = ZeroSlice<Option<T>>;
