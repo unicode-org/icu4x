a705b615b05d0856f56a25ade7d22c8618090688
github-action-benchmark <github@users.noreply.github.com>
2022-09-22 17:43:32 +0000

    add Rust Benchmark (cargo) benchmark result for ab02aade03ce68ec53f90fe4eeb140e5e97b2e01

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663795387594,
+  "lastUpdate": 1663868612290,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -52408,6 +52408,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "from_string/1000000001",
             "value": 100,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manishsmail@gmail.com",
+            "name": "Manish Goregaokar",
+            "username": "Manishearth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ab02aade03ce68ec53f90fe4eeb140e5e97b2e01",
+          "message": "fix ci (#2610)",
+          "timestamp": "2022-09-22T12:30:15-05:00",
+          "tree_id": "2d864525020adaddf86f9df5d17fc818bf01e12a",
+          "url": "https://github.com/unicode-org/icu4x/commit/ab02aade03ce68ec53f90fe4eeb140e5e97b2e01"
+        },
+        "date": 1663868558409,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 117545,
+            "range": "± 363",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 23437,
+            "range": "± 109",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 63301,
+            "range": "± 708",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 187,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 77,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 83,
             "range": "± 0",
             "unit": "ns/iter"
           }
