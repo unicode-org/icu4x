ff1d4b370b834281e3524118fb41883341a7e2bd
Henri Sivonen <hsivonen@hsivonen.fi>
2022-09-23 09:11:25 +0300

    Warn against breaking the Pernosco integration of CharacterAndClass (#2604)

--- components/collator/src/elements.rs
+++ components/collator/src/elements.rs
@@ -716,6 +716,11 @@ impl CharacterAndClassAndTrieValue {
 /// Note that 0xFF is won't be assigned to an actual
 /// canonical combining class per definition D104
 /// in The Unicode Standard.
+//
+// NOTE: The Pernosco debugger has special knowledge
+// of this struct. Please do not change the bit layout
+// or the crate-module-qualified name of this struct
+// without coordination.
 #[derive(Debug, Clone)]
 struct CharacterAndClass(u32);
 
