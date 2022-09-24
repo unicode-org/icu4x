f827747aea55069b345446390edb3fb9ef311f76
Shane F. Carr <shane@unicode.org>
2022-09-24 00:20:55 -0700

    fmt

--- components/locid/src/extensions/private/mod.rs
+++ components/locid/src/extensions/private/mod.rs
@@ -114,7 +114,9 @@ impl Private {
     }
 
     pub(crate) fn try_from_iter(iter: &mut SubtagIterator) -> Result<Self, ParserError> {
-        let keys = iter.map(Subtag::from_bytes).collect::<Result<Vec<_>, _>>()?;
+        let keys = iter
+            .map(Subtag::from_bytes)
+            .collect::<Result<Vec<_>, _>>()?;
 
         Ok(Self::from_vec_unchecked(keys))
     }
