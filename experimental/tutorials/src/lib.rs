2e872a75a2931aa6e01e8bdc0ae36daf20c6bd96
Daniel Sipasseuth <dsipasseuth@google.com>
2022-09-27 23:40:34 +0100

    fix mod order

--- experimental/tutorials/src/lib.rs
+++ experimental/tutorials/src/lib.rs
@@ -1,10 +1,10 @@
 // This file is part of ICU4X. For terms of use, please see the file
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
-
-#[cfg(doc)]
-pub mod data_provider;
+#[rustfmt::skip]
 #[cfg(doc)]
 pub mod intro;
+#[cfg(doc)]
+pub mod data_provider;
 #[cfg(doc)]
 pub mod writing_a_new_data_struct;
