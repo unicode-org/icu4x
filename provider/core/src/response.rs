191552f7fbf3e5f8e293354e05d5255fea09c43b
Robert Bastian <robertbastian@users.noreply.github.com>
2022-09-27 09:46:27 +0200

    Update provider/core/src/response.rs
    Co-authored-by: Shane F. Carr <shane@unicode.org>

--- provider/core/src/response.rs
+++ provider/core/src/response.rs
@@ -89,9 +89,9 @@ impl Deref for Cart {
         &*self.0
     }
 }
-// Safe because both Rc and Arc are CloneableCart
+// Safe because both Rc and Arc are StableDeref, and our Deref impl delegates to that Deref impl.
 unsafe impl stable_deref_trait::StableDeref for Cart {}
-// Safe because both Rc and Arc are StableDeref
+// Safe because both Rc and Arc are CloneableCart, and our Deref impl delegates to that Deref impl.
 unsafe impl yoke::CloneableCart for Cart {}
 
 impl Cart {
