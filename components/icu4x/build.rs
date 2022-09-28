8a1a6ada1e5b7562e263d41a21c514bcf4013a64
Robert Bastian <robertbastian@users.noreply.github.com>
2022-09-29 00:17:36 +0200

    boom

---
+++ components/icu4x/build.rs
@@ -0,0 +1,4 @@
+fn main() {
+    eprintln!("Looking for the ICU4X project? Use the `icu` crate");
+    std::process::exit(1);
+}
--- components/icu4x/src/lib.rs
+++ components/icu4x/src/lib.rs
@@ -2,4 +2,4 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-//! `icu4x` is an alias to the [`icu`](../icu/index.html) package.
+//! Looking for the ICU4X project? Use the [`icu`](https://docs.rs/icu/latest/icu/) crate.
