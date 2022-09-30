4834db3e1aa6a5611b6c4ca6f4f707f310e53f3b
Robert Bastian <robertbastian@google.com>
2022-09-30 17:24:41 +0200

    rm

--- components/icu4x/README.md
+++
@@ -1,1 +0,0 @@
-Looking for the ICU4X project? Use the [`icu`](https://docs.rs/icu/latest/icu/) crate.
--- components/icu4x/build.rs
+++
@@ -1,8 +0,0 @@
-// This file is part of ICU4X. For terms of use, please see the file
-// called LICENSE at the top level of the ICU4X source tree
-// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
-
-fn main() {
-    eprintln!("Looking for the ICU4X project? Use the `icu` crate");
-    std::process::exit(1);
-}
