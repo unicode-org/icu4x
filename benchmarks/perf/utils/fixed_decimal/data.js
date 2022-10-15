d173d42979bb4dd42926ac842191a64ea37c3968
github-action-benchmark <github@users.noreply.github.com>
2022-10-15 04:22:39 +0000

    add Rust Benchmark (cargo) benchmark result for 48457f5ce2a775094cda3b6431667da14c430ee2

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1665806598896,
+  "lastUpdate": 1665807759265,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -57449,6 +57449,66 @@ window.BENCHMARK_DATA = {
             "name": "from_string/1000000001",
             "value": 82,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "robertbastian@users.noreply.github.com",
+            "name": "Robert Bastian",
+            "username": "robertbastian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "48457f5ce2a775094cda3b6431667da14c430ee2",
+          "message": "More borrow in list JSON deserialization (#2751)",
+          "timestamp": "2022-10-14T21:08:14-07:00",
+          "tree_id": "30e85b03d7e8947c6489fba030a4a47880a3fa1d",
+          "url": "https://github.com/unicode-org/icu4x/commit/48457f5ce2a775094cda3b6431667da14c430ee2"
+        },
+        "date": 1665807688276,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 113324,
+            "range": "± 5087",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 24231,
+            "range": "± 1873",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 68266,
+            "range": "± 6230",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 205,
+            "range": "± 16",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 86,
+            "range": "± 3",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 79,
+            "range": "± 3",
             "unit": "ns/iter"
           }
         ]
