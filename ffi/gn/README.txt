bc6d32d3518a5ef6067e522b8f6b5a4dac33f131
Shane F. Carr <shane@unicode.org>
2022-10-21 18:30:17 -0700

    Makefile cleanups & docs

--- ffi/gn/README.txt
+++
@@ -1,4 +0,0 @@
-This is an example directory structure that compiles a Rust hello world
-example. It is intended to show how to set up a simple Rust GN build.
-
-Don't miss the ".gn" file in this directory!
--- ffi/gn/src/lib.rs
+++ ffi/gn/src/lib.rs
@@ -2,21 +2,18 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-// https://github.com/unicode-org/icu4x/blob/main/docs/process/boilerplate.md#library-annotations
-#![no_std]
-#![cfg_attr(
-    not(test),
-    deny(
-        clippy::indexing_slicing,
-        clippy::unwrap_used,
-        clippy::expect_used,
-        clippy::panic
-    )
-)]
-#![allow(clippy::upper_case_acronyms)]
-#![cfg_attr(target_os = "none", feature(alloc_error_handler))]
+//! This directory contains ICU4X build rules for the
+//! [GN meta-build system](https://gn.googlesource.com/gn/).
+//! 
+//! Operations on the files in this directory can be performed by running `cargo make`
+//! commands having the "gn" prefix from the top level, including:
+//! 
+//! - `cargo make gn-gen`: uses
+//!   [Cargo GNaw](https://fuchsia.googlesource.com/fuchsia/+/refs/heads/main/tools/cargo-gnaw/)
+//!   to re-generate the GN build rules from Cargo build rules.
+//! - `cargo make gn-build`: Builds ICU4X using `gn` and `ninja`.
+//! - `cargo make gn-run`: Runs ICU4X from the binaries built with `gn` and `ninja`.
+//!
+//! This actual Rust crate is only a placeholder for input to Cargo GNaw.
 
-// Necessary to for symbols to be linked in
-extern crate icu_capi;
-
-// TODO: Figure out what else we need to add here (allocator and panic handlers?)
+mod _ {}
