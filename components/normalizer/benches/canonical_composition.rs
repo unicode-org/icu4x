c1d4111efdb2572aa73dc9586d3184a964227445
Daniel Sipasseuth <dsipasseuth@google.com>
2022-10-24 10:27:39 +0100

    add licence header

--- components/normalizer/benches/canonical_composition.rs
+++ components/normalizer/benches/canonical_composition.rs
@@ -1,3 +1,7 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 use criterion::{black_box, BenchmarkId, Criterion, Throughput};
 
 use icu_normalizer::properties::CanonicalComposition;
