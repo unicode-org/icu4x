214ef205e20b130405e8f420ffc05559dfcd77bd
Elango Cheran <elango@unicode.org>
2022-10-31 14:35:22 -0700

    Update invariants checked in constructor

--- components/collections/src/codepointinvliststringlist/mod.rs
+++ components/collections/src/codepointinvliststringlist/mod.rs
@@ -2,10 +2,10 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use alloc::string::{String, ToString};
 use crate::codepointinvlist::{CodePointInversionList, CodePointInversionListError};
+use alloc::string::{String, ToString};
 use displaydoc::Display;
-use zerovec::{VarZeroVec, ZeroVec};
+use zerovec::{ule::AsULE, VarZeroVec, ZeroVec};
 
 pub struct CodePointInversionListStringList<'data> {
     cp_inv_list: CodePointInversionList<'data>,
@@ -13,20 +13,48 @@ pub struct CodePointInversionListStringList<'data> {
 }
 
 impl<'data> CodePointInversionListStringList<'data> {
-    pub fn try_from_lists(
-        cp_list: ZeroVec<'data, u32>,
+    pub fn try_from(
+        cp_inv_list: CodePointInversionList<'data>,
         str_list: VarZeroVec<'data, str>,
     ) -> Result<Self, CodePointInversionListStringListError> {
-        let cp_inv_list = CodePointInversionList::try_from_inversion_list(cp_list)
-            .map_err(|cpile| 
-                CodePointInversionListStringListError::InvalidCodePointInversionList(cpile))?;
-
         // Invariants:
-        //     - no input string is length 1 (a length 1 string should be a code point)
-        //     - no input string is length 0 (?)
-        for s in str_list.iter() {
-            if s.len() < 2 {
-                return Err(CodePointInversionListStringListError::InvalidStringLength(s.to_string()));
+        //   - no input string is length 1 (a length 1 string should be a code point)
+        //   - the list is sorted
+        //   - the elements in the list are unique
+
+        // Verify invariants:
+        // Do so by using the equivalent of str_list.iter().windows(2) to get
+        // overlapping windows of size 2. The above putative code is not possible
+        // because `.windows()` exists on a slice, but VarZeroVec cannot return a slice
+        // because the non-fixed size elements necessitate at least some type
+        // of allocation.
+        {
+            let mut it = str_list.iter();
+            if let Some(mut x) = it.next() {
+                if x.len() == 1 {
+                    return Err(CodePointInversionListStringListError::InvalidStringLength(
+                        x.to_string(),
+                    ));
+                }
+                while let Some(y) = it.next() {
+                    if x.len() == 1 {
+                        return Err(CodePointInversionListStringListError::InvalidStringLength(
+                            x.to_string(),
+                        ));
+                    } else if x == y {
+                        return Err(CodePointInversionListStringListError::StringListNotUnique(
+                            x.to_string(),
+                        ));
+                    } else if x > y {
+                        return Err(CodePointInversionListStringListError::StringListNotSorted(
+                            x.to_string(),
+                            y.to_string(),
+                        ));
+                    }
+
+                    // Next window begins. Update `x` here, `y` will be updated in next loop iteration.
+                    x = y;
+                }
             }
         }
 
@@ -60,12 +88,18 @@ impl<'data> CodePointInversionListStringList<'data> {
 /// Re-exported as [`Error`](Error).
 #[derive(Display, Debug)]
 pub enum CodePointInversionListStringListError {
-    /// A CodePointInversionList was constructed
+    /// An invalid CodePointInversionList was constructed
     #[displaydoc("Invalid code point inversion list: {0:?}")]
     InvalidCodePointInversionList(CodePointInversionListError),
-    /// A String in the string list had an invalid length
+    /// A string in the string list had an invalid length
     #[displaydoc("Invalid string length for string: {0}")]
     InvalidStringLength(String),
+    /// A string in the string list appears more than once
+    #[displaydoc("String list has duplicate: {0}")]
+    StringListNotUnique(String),
+    /// Two strings in the string list compare to each other opposite of sorted order
+    #[displaydoc("Strings in string list not in sorted order: ({0}, {1})")]
+    StringListNotSorted(String, String),
 }
 
 #[cfg(test)]
@@ -75,25 +109,83 @@ mod tests {
     #[test]
     fn test_size_has_strings() {
         let cp_slice = &[0, 1, 0x7F, 0x80, 0xFFFF, 0x1_0000, 0x10_FFFF, 0x11_0000];
-        let cp_list = ZeroVec::alloc_from_slice(cp_slice);
-        let str_slice = &["zero", "ascii_max", "bmp_max", "unicode_max"];
+        let cp_list =
+            CodePointInversionList::try_clone_from_inversion_list_slice(cp_slice).unwrap();
+        let str_slice = &["ascii_max", "bmp_max", "unicode_max", "zero"];
         let str_list = VarZeroVec::<str>::from(str_slice);
 
-        let cpilsl = CodePointInversionListStringList::try_from_lists(cp_list, str_list).unwrap();
+        let cpilsl = CodePointInversionListStringList::try_from(cp_list, str_list).unwrap();
 
         assert!(cpilsl.has_strings());
         assert_eq!(8, cpilsl.size());
     }
 
     #[test]
-    fn test_invalid_string() {
+    fn test_empty_string_allowed() {
         let cp_slice = &[0, 1, 0x7F, 0x80, 0xFFFF, 0x1_0000, 0x10_FFFF, 0x11_0000];
-        let cp_list = ZeroVec::alloc_from_slice(cp_slice);
-        let str_slice = &[""];
+        let cp_list =
+            CodePointInversionList::try_clone_from_inversion_list_slice(cp_slice).unwrap();
+        let str_slice = &["", "ascii_max", "bmp_max", "unicode_max", "zero"];
         let str_list = VarZeroVec::<str>::from(str_slice);
 
-        let cpilsl = CodePointInversionListStringList::try_from_lists(cp_list, str_list);
+        let cpilsl = CodePointInversionListStringList::try_from(cp_list, str_list).unwrap();
 
-        assert!(matches!(cpilsl, Err(CodePointInversionListStringListError::InvalidStringLength(_))));
+        assert!(cpilsl.has_strings());
+        assert_eq!(9, cpilsl.size());
+    }
+
+    #[test]
+    fn test_invalid_string() {
+        let cp_slice = &[0, 1];
+        let cp_list =
+            CodePointInversionList::try_clone_from_inversion_list_slice(cp_slice).unwrap();
+        let str_slice = &["a"];
+        let str_list = VarZeroVec::<str>::from(str_slice);
+
+        let cpilsl = CodePointInversionListStringList::try_from(cp_list, str_list);
+
+        assert!(matches!(
+            cpilsl,
+            Err(CodePointInversionListStringListError::InvalidStringLength(
+                _
+            ))
+        ));
+    }
+
+    #[test]
+    fn test_invalid_string_list_has_duplicate() {
+        let cp_slice = &[0, 1];
+        let cp_list =
+            CodePointInversionList::try_clone_from_inversion_list_slice(cp_slice).unwrap();
+        let str_slice = &["abc", "abc"];
+        let str_list = VarZeroVec::<str>::from(str_slice);
+
+        let cpilsl = CodePointInversionListStringList::try_from(cp_list, str_list);
+
+        assert!(matches!(
+            cpilsl,
+            Err(CodePointInversionListStringListError::StringListNotUnique(
+                _
+            ))
+        ));
+    }
+
+    #[test]
+    fn test_invalid_string_list_not_sorted() {
+        let cp_slice = &[0, 1];
+        let cp_list =
+            CodePointInversionList::try_clone_from_inversion_list_slice(cp_slice).unwrap();
+        let str_slice = &["xyz", "abc"];
+        let str_list = VarZeroVec::<str>::from(str_slice);
+
+        let cpilsl = CodePointInversionListStringList::try_from(cp_list, str_list);
+
+        assert!(matches!(
+            cpilsl,
+            Err(CodePointInversionListStringListError::StringListNotSorted(
+                _,
+                _
+            ))
+        ));
     }
 }
