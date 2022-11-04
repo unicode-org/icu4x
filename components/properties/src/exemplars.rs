319a79666c1f42aa4013050c657cf06f4ab66500
Elango Cheran <elango@unicode.org>
2022-11-04 11:49:46 -0700

    Add initial code for the first exemplar characters getter function

---
+++ components/properties/src/exemplars.rs
@@ -0,0 +1,23 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+//! This module provides APIs for getting exemplar characters for a locale.
+//! 
+//! Exemplars are characters used by a language, separated into different categories.
+//! The categories define, according to typical usage in the langauge, 
+//! which characters occur in which contexts with which frequency.
+//! For more information, see the documentation in the
+//! [Exemplars section in Unicode Technical Standard #35](https://unicode.org/reports/tr35/tr35-general.html#Exemplars)
+//! of the LDML specification.
+
+use icu_provider::prelude::*;
+use crate::sets::UnicodeSetData;
+use crate::PropertiesError;
+
+/// Get the "main" category of exemplar characters.
+pub fn get_exemplars_main(
+    provider: &(impl DataProvider<ExemplarsMainV1Marker> + ?Sized)
+) -> Result<UnicodeSetData, PropertiesError> {
+    Err(PropertiesError::PropDataLoad(DataError::custom("Unimplemented API!")))
+}
