c1e6accfe766821812bbd7435e23201eed96fc02
Elango Cheran <elango@unicode.org>
2022-11-04 12:09:02 -0700

    Apply formatter

--- components/properties/src/exemplars.rs
+++ components/properties/src/exemplars.rs
@@ -3,9 +3,9 @@
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
 //! This module provides APIs for getting exemplar characters for a locale.
-//! 
+//!
 //! Exemplars are characters used by a language, separated into different categories.
-//! The categories define, according to typical usage in the langauge, 
+//! The categories define, according to typical usage in the langauge,
 //! which characters occur in which contexts with which frequency.
 //! For more information, see the documentation in the
 //! [Exemplars section in Unicode Technical Standard #35](https://unicode.org/reports/tr35/tr35-general.html#Exemplars)
@@ -18,7 +18,9 @@ use icu_provider::prelude::*;
 
 /// Get the "main" category of exemplar characters.
 pub fn get_exemplars_main(
-    provider: &(impl DataProvider<ExemplarsMainV1Marker> + ?Sized)
+    provider: &(impl DataProvider<ExemplarsMainV1Marker> + ?Sized),
 ) -> Result<UnicodeSetData, PropertiesError> {
-    Err(PropertiesError::PropDataLoad(DataError::custom("Unimplemented API!")))
+    Err(PropertiesError::PropDataLoad(DataError::custom(
+        "Unimplemented API!",
+    )))
 }
