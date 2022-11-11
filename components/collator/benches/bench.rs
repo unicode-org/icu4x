97cc57556198b9b44e94d394bced9030071bf5d4
Daniel Sipasseuth <dsipasseuth@google.com>
2022-11-11 15:52:34 +0000

    address comments

--- components/collator/benches/bench.rs
+++ components/collator/benches/bench.rs
@@ -2,62 +2,130 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use std::time::Duration;
-
-use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion};
+use criterion::{BatchSize, BenchmarkId, Criterion, criterion_group, criterion_main};
 
 use icu::collator::*;
 use icu::locid::Locale;
+use icu_provider::DataLocale;
+
+fn to_data_locale(locale_str: &str) -> DataLocale {
+    locale_str
+        .parse::<Locale>()
+        .expect(&format!("Failed to parse locale [{}]", locale_str))
+        .into()
+}
 
 pub fn collator_with_locale(criterion: &mut Criterion) {
     // Load file content in reverse order vector.
     let content_latin: (&str, Vec<&str>) = (
-        "TestNames_Latin",
+        "Latin",
         include_str!("data/TestNames_Latin.txt")
             .split('\n')
             .rev()
             .collect::<Vec<&str>>(),
     );
     let content_asian: (&str, Vec<&str>) = (
-        "TestNames_Asian",
+        "Asian",
         include_str!("data/TestNames_Asian.txt")
             .split('\n')
             .rev()
             .collect(),
     );
     let content_russian: (&str, Vec<&str>) = (
-        "TestNames_Russian",
+        "Russian",
         include_str!("data/TestNames_Russian.txt")
             .split('\n')
             .rev()
             .collect(),
     );
+    let content_chinese: (&str, Vec<&str>) = (
+        "Chinese",
+        include_str!("data/TestNames_Chinese.txt")
+            .split('\n')
+            .rev()
+            .collect(),
+    );
+    let content_jp_h: (&str, Vec<&str>) = (
+        "Japanese_h",
+        include_str!("data/TestNames_Japanese_h.txt")
+            .split('\n')
+            .rev()
+            .collect::<Vec<&str>>(),
+    );
+    let content_jp_k: (&str, Vec<&str>) = (
+        "TestNames_Japanese_k",
+        include_str!("data/TestNames_Japanese_k.txt")
+            .split('\n')
+            .rev()
+            .collect::<Vec<&str>>(),
+    );
+    let content_korean: (&str, Vec<&str>) = (
+        "Korean",
+        include_str!("data/TestNames_Korean.txt")
+            .split('\n')
+            .rev()
+            .collect::<Vec<&str>>(),
+    );
+    let content_thai: (&str, Vec<&str>) = (
+        "Thai",
+        include_str!("data/TestNames_Thai.txt")
+            .split('\n')
+            .rev()
+            .collect::<Vec<&str>>(),
+    );
 
-    let performance_parameters = [
-        ("en_US", vec![&content_latin]),
-        ("fr_FR", vec![&content_latin]),
-        ("ja_JP", vec![&content_latin, &content_asian]),
-        ("zh_CN", vec![&content_latin]),
-        ("ru_RU", vec![&content_latin, &content_russian]),
+    // hsivonen@ : All five strengths are benched.
+    // The default is tertiary, so it makes sense to bench that.
+    //
+    // Also, it's particularly interesting to bench quaternary
+    // and identical with Japanese due to the performance remarks ICU4C docs make about Japanese.
+    //
+    // In particular, to get full Japanese standard behavior, you need the identical strength.
+    // Furthermore, CLDR used to default to quaternary for Japanese but now defaults to tertiary
+    // as for every other language for performance reasons.
+    let all_strength = vec![
+        Strength::Primary,
+        Strength::Secondary,
+        Strength::Tertiary,
+        Strength::Quaternary,
+        Strength::Identical,
     ];
+    let performance_parameters = [
+        (to_data_locale("en_US"), vec![&content_latin], &all_strength),
+        (to_data_locale("da_DK"), vec![&content_latin], &all_strength),
+        (to_data_locale("fr_CA"), vec![&content_latin], &all_strength),
+        (
+            to_data_locale("ja_JP"),
+            vec![&content_latin, &content_jp_h, &content_jp_k, &content_asian],
+            &all_strength,
+        ),
+        (to_data_locale("zh-u-co-pinyin"), vec![&content_latin, &content_chinese], &all_strength), // zh_CN
+        (to_data_locale("zh-u-co-stroke"), vec![&content_latin, &content_chinese], &all_strength), // zh_TW
+        (
+            to_data_locale("ru_RU"),
+            vec![&content_latin, &content_russian],
+            &all_strength,
+        ),
+        (
+            to_data_locale("th"),
+            vec![&content_latin, &content_thai],
+            &all_strength,
+        ),
+        (
+            to_data_locale("ko_KR"),
+            vec![&content_latin, &content_korean],
+            &all_strength,
+        ),
+    ];
+    for perf_parameter in performance_parameters {
+        let (locale_under_bench, files_under_bench, benched_strength) = perf_parameter;
 
-    for perf_group in performance_parameters {
-        let (locale_under_test, files_under_test) = perf_group;
-        let current_locale: Locale = locale_under_test.parse().expect("Failed to parse locale.");
+        let mut group = criterion.benchmark_group(locale_under_bench.to_string());
 
-        let mut options = CollatorOptions::new();
-        options.strength = Some(Strength::Primary);
-        let collator: Collator =
-            Collator::try_new_unstable(&icu_testdata::unstable(), &current_locale.into(), options)
-                .unwrap();
-
-        let mut group = criterion.benchmark_group(locale_under_test);
-
-        for content_under_test in files_under_test {
-            let (file_name, elements) = content_under_test;
-
-            // baseline performance, normal sort done by rust libraries.
-            group.bench_function(BenchmarkId::new("rust_sort", file_name), |bencher| {
+        for content_under_bench in files_under_bench {
+            let (file_name, elements) = content_under_bench;
+            // baseline performance, locale-unaware code point sort done by Rust (0 for ordering in the html report)
+            group.bench_function(BenchmarkId::new("0_rust_sort", file_name), |bencher| {
                 bencher.iter_batched(
                     || elements.clone(),
                     |mut lines| lines.sort_unstable(),
@@ -65,14 +133,30 @@ pub fn collator_with_locale(criterion: &mut Criterion) {
                 )
             });
 
-            // ICU collator performance
-            group.bench_function(BenchmarkId::new("icu_sort", file_name), |bencher| {
-                bencher.iter_batched(
-                    || elements.clone(),
-                    |mut lines| lines.sort_unstable_by(|left, right| collator.compare(left, right)),
-                    BatchSize::SmallInput,
+            // index to keep order of strength in the html report
+            for (index, strength) in benched_strength.into_iter().enumerate() {
+                let mut options = CollatorOptions::new();
+                options.strength = Some(*strength);
+                let collator = Collator::try_new_unstable(
+                    &icu_testdata::unstable(),
+                    &locale_under_bench,
+                    options,
                 )
-            });
+                    .unwrap();
+                // ICU4X collator performance, sort is locale-aware
+                group.bench_function(
+                    BenchmarkId::new("1_icu4x_sort", format!("{}_{:?}/{}", index, strength, file_name)),
+                    |bencher| {
+                        bencher.iter_batched(
+                            || elements.clone(),
+                            |mut lines| {
+                                lines.sort_unstable_by(|left, right| collator.compare(left, right))
+                            },
+                            BatchSize::SmallInput,
+                        )
+                    },
+                );
+            }
         }
         group.finish();
     }
@@ -80,9 +164,7 @@ pub fn collator_with_locale(criterion: &mut Criterion) {
 
 criterion_group!(
     name = benches;
-    config = Criterion::default()
-        .measurement_time(Duration::new(60, 0))
-        .sample_size(100);
+    config = Criterion::default();
     targets = collator_with_locale
 );
 
