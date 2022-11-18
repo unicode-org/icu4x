f9e20ecf650d3a2fe299c11ccc1ffb521e9b26bf
Daniel Sipasseuth <dsipasseuth@google.com>
2022-11-18 14:55:40 +0000

    remove throughput imports

--- components/normalizer/benches/composing_normalizer_nfkc.rs
+++ components/normalizer/benches/composing_normalizer_nfkc.rs
@@ -2,7 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use criterion::{BenchmarkId, Criterion, Throughput};
+use criterion::{BenchmarkId, Criterion};
 use icu_normalizer::ComposingNormalizer;
 
 fn function_under_bench(normalizer: &ComposingNormalizer, text: &str) {
