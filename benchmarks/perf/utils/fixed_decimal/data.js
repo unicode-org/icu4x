8968324ca57ed81a57aeb13e3da656d10a9c3cad
github-action-benchmark <github@users.noreply.github.com>
2022-11-28 16:10:47 +0000

    add Rust Benchmark (cargo) benchmark result for 0f244a8f5b5c89286214ed77fb90abfa596c76c8

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669622866931,
+  "lastUpdate": 1669651846649,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -59789,6 +59789,66 @@ window.BENCHMARK_DATA = {
             "name": "from_string/1000000001",
             "value": 83,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0f244a8f5b5c89286214ed77fb90abfa596c76c8",
+          "message": "Pin wasmer-cli version (#2842)",
+          "timestamp": "2022-11-28T16:56:02+01:00",
+          "tree_id": "6f6d99bc30373ab47c212713072f2bf4739b3955",
+          "url": "https://github.com/unicode-org/icu4x/commit/0f244a8f5b5c89286214ed77fb90abfa596c76c8"
+        },
+        "date": 1669651775889,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 101396,
+            "range": "± 596",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 21833,
+            "range": "± 82",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 66227,
+            "range": "± 279",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 165,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 66,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 72,
+            "range": "± 2",
             "unit": "ns/iter"
           }
         ]
