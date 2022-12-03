b4b6ad784c9ad9c51704e9faf418b0816291f216
github-action-benchmark <github@users.noreply.github.com>
2022-12-03 16:15:34 +0000

    add Rust Benchmark (cargo) benchmark result for d474a7997a9a408a45791e29a9308feb63043689

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670001914291,
+  "lastUpdate": 1670084133902,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -60389,6 +60389,66 @@ window.BENCHMARK_DATA = {
             "name": "from_string/1000000001",
             "value": 87,
             "range": "± 3",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kelebra20@gmail.com",
+            "name": "Oleksii Tkachuk",
+            "username": "kelebra"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d474a7997a9a408a45791e29a9308feb63043689",
+          "message": "Address last for/vec.push case in favor of vec.extend (#2861)",
+          "timestamp": "2022-12-03T18:01:30+02:00",
+          "tree_id": "4d8b20bdaa531c376511eb31333a77b2e310466b",
+          "url": "https://github.com/unicode-org/icu4x/commit/d474a7997a9a408a45791e29a9308feb63043689"
+        },
+        "date": 1670084071851,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 103978,
+            "range": "± 1429",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 21366,
+            "range": "± 168",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 56385,
+            "range": "± 716",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 165,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 69,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 73,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
