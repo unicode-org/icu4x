9ff37a2cc834958fa524d0226578e17c32bb9a63
Robin Leroy <egg.robin.leroy@gmail.com>
2022-12-13 15:41:19 +0100

    the licensing will continue until morale improve

--- provider/datagen/src/transform/cldr/decimal/compact_decimal_pattern.rs
+++ provider/datagen/src/transform/cldr/decimal/compact_decimal_pattern.rs
@@ -1,3 +1,7 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 use crate::transform::cldr::decimal::DecimalFormat;
 use icu_compactdecimal::provider::CompactDecimalPatternDataV1;
 use icu_compactdecimal::provider::*;
