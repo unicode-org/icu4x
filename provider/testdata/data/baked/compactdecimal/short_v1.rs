5b4490142b0acbcba2c8a7f9e7472f2f6980d809
Robin Leroy <egg.robin.leroy@gmail.com>
2022-12-15 15:18:15 +0100

    cargo made testdata

--- provider/testdata/data/baked/compactdecimal/short_v1.rs
+++ provider/testdata/data/baked/compactdecimal/short_v1.rs
@@ -46,12 +46,12 @@ static AR_AR_EG_AR_EG_U_NU_LATN_AR_U_NU_LATN: &DataStruct =
                 unsafe {
                     ::zerovec::VarZeroVec::from_bytes_unchecked(&[
                         6u8, 0u8, 0u8, 0u8, 0u8, 0u8, 12u8, 0u8, 22u8, 0u8, 32u8, 0u8, 46u8, 0u8,
-                        60u8, 0u8, 0u8, 3u8, 194u8, 160u8, 216u8, 162u8, 217u8, 132u8, 216u8,
-                        167u8, 217u8, 129u8, 0u8, 3u8, 194u8, 160u8, 216u8, 163u8, 217u8, 132u8,
-                        217u8, 129u8, 0u8, 3u8, 194u8, 160u8, 216u8, 163u8, 217u8, 132u8, 217u8,
-                        129u8, 0u8, 6u8, 194u8, 160u8, 217u8, 133u8, 217u8, 132u8, 217u8, 138u8,
-                        217u8, 136u8, 217u8, 134u8, 0u8, 9u8, 194u8, 160u8, 217u8, 133u8, 217u8,
-                        132u8, 217u8, 138u8, 216u8, 167u8, 216u8, 177u8, 0u8, 12u8, 194u8, 160u8,
+                        60u8, 0u8, 3u8, 0u8, 194u8, 160u8, 216u8, 162u8, 217u8, 132u8, 216u8,
+                        167u8, 217u8, 129u8, 3u8, 0u8, 194u8, 160u8, 216u8, 163u8, 217u8, 132u8,
+                        217u8, 129u8, 3u8, 0u8, 194u8, 160u8, 216u8, 163u8, 217u8, 132u8, 217u8,
+                        129u8, 6u8, 0u8, 194u8, 160u8, 217u8, 133u8, 217u8, 132u8, 217u8, 138u8,
+                        217u8, 136u8, 217u8, 134u8, 9u8, 0u8, 194u8, 160u8, 217u8, 133u8, 217u8,
+                        132u8, 217u8, 138u8, 216u8, 167u8, 216u8, 177u8, 12u8, 0u8, 194u8, 160u8,
                         216u8, 170u8, 216u8, 177u8, 217u8, 132u8, 217u8, 138u8, 217u8, 136u8,
                         217u8, 134u8,
                     ])
@@ -79,14 +79,14 @@ static BN_BN_U_NU_LATN: &DataStruct =
                 unsafe {
                     ::zerovec::VarZeroVec::from_bytes_unchecked(&[
                         7u8, 0u8, 0u8, 0u8, 0u8, 0u8, 10u8, 0u8, 20u8, 0u8, 30u8, 0u8, 48u8, 0u8,
-                        64u8, 0u8, 72u8, 0u8, 0u8, 3u8, 194u8, 160u8, 224u8, 166u8, 185u8, 224u8,
-                        166u8, 190u8, 0u8, 5u8, 194u8, 160u8, 224u8, 166u8, 178u8, 224u8, 166u8,
-                        190u8, 0u8, 7u8, 194u8, 160u8, 224u8, 166u8, 149u8, 224u8, 167u8, 139u8,
-                        0u8, 9u8, 194u8, 160u8, 224u8, 166u8, 182u8, 224u8, 166u8, 164u8, 194u8,
-                        160u8, 224u8, 166u8, 149u8, 224u8, 167u8, 139u8, 0u8, 9u8, 224u8, 166u8,
+                        64u8, 0u8, 72u8, 0u8, 3u8, 0u8, 194u8, 160u8, 224u8, 166u8, 185u8, 224u8,
+                        166u8, 190u8, 5u8, 0u8, 194u8, 160u8, 224u8, 166u8, 178u8, 224u8, 166u8,
+                        190u8, 7u8, 0u8, 194u8, 160u8, 224u8, 166u8, 149u8, 224u8, 167u8, 139u8,
+                        9u8, 0u8, 194u8, 160u8, 224u8, 166u8, 182u8, 224u8, 166u8, 164u8, 194u8,
+                        160u8, 224u8, 166u8, 149u8, 224u8, 167u8, 139u8, 9u8, 0u8, 224u8, 166u8,
                         182u8, 224u8, 166u8, 164u8, 194u8, 160u8, 224u8, 166u8, 149u8, 224u8,
-                        167u8, 139u8, 0u8, 9u8, 224u8, 166u8, 149u8, 224u8, 167u8, 139u8, 0u8,
-                        12u8, 194u8, 160u8, 224u8, 166u8, 178u8, 224u8, 166u8, 190u8, 46u8, 224u8,
+                        167u8, 139u8, 9u8, 0u8, 224u8, 166u8, 149u8, 224u8, 167u8, 139u8, 12u8,
+                        0u8, 194u8, 160u8, 224u8, 166u8, 178u8, 224u8, 166u8, 190u8, 46u8, 224u8,
                         166u8, 149u8, 224u8, 167u8, 139u8, 46u8,
                     ])
                 },
@@ -108,8 +108,8 @@ static CCP_CCP_U_NU_LATN_UND: &DataStruct =
                 unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8, 5u8]) },
                 unsafe {
                     ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                        4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 3u8, 0u8, 6u8, 0u8, 9u8, 0u8, 0u8, 3u8, 75u8,
-                        0u8, 6u8, 77u8, 0u8, 9u8, 71u8, 0u8, 12u8, 84u8,
+                        4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 3u8, 0u8, 6u8, 0u8, 9u8, 0u8, 3u8, 0u8, 75u8,
+                        6u8, 0u8, 77u8, 9u8, 0u8, 71u8, 12u8, 0u8, 84u8,
                     ])
                 },
             )
@@ -130,8 +130,8 @@ static EN_EN_001_EN_ZA_FIL_TH_TH_U_NU_THAI: &DataStruct =
                 unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8, 5u8]) },
                 unsafe {
                     ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                        4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 3u8, 0u8, 6u8, 0u8, 9u8, 0u8, 0u8, 3u8, 75u8,
-                        0u8, 6u8, 77u8, 0u8, 9u8, 66u8, 0u8, 12u8, 84u8,
+                        4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 3u8, 0u8, 6u8, 0u8, 9u8, 0u8, 3u8, 0u8, 75u8,
+                        6u8, 0u8, 77u8, 9u8, 0u8, 66u8, 12u8, 0u8, 84u8,
                     ])
                 },
             )
@@ -150,9 +150,9 @@ static ES: &DataStruct = &::icu_compactdecimal::provider::CompactDecimalPatternD
             unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8, 5u8]) },
             unsafe {
                 ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 7u8, 0u8, 12u8, 0u8, 22u8, 0u8, 0u8, 3u8, 194u8,
-                    160u8, 109u8, 105u8, 108u8, 0u8, 6u8, 194u8, 160u8, 77u8, 0u8, 9u8, 194u8,
-                    160u8, 109u8, 105u8, 108u8, 194u8, 160u8, 77u8, 0u8, 12u8, 194u8, 160u8, 66u8,
+                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 7u8, 0u8, 12u8, 0u8, 22u8, 0u8, 3u8, 0u8, 194u8,
+                    160u8, 109u8, 105u8, 108u8, 6u8, 0u8, 194u8, 160u8, 77u8, 9u8, 0u8, 194u8,
+                    160u8, 109u8, 105u8, 108u8, 194u8, 160u8, 77u8, 12u8, 0u8, 194u8, 160u8, 66u8,
                 ])
             },
         )
@@ -172,10 +172,10 @@ static ES_AR: &DataStruct = &::icu_compactdecimal::provider::CompactDecimalPatte
             unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8, 5u8, 5u8]) },
             unsafe {
                 ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                    5u8, 0u8, 0u8, 0u8, 0u8, 0u8, 5u8, 0u8, 10u8, 0u8, 15u8, 0u8, 25u8, 0u8, 0u8,
-                    3u8, 194u8, 160u8, 75u8, 0u8, 3u8, 194u8, 160u8, 107u8, 0u8, 6u8, 194u8, 160u8,
-                    77u8, 0u8, 9u8, 194u8, 160u8, 109u8, 105u8, 108u8, 194u8, 160u8, 77u8, 0u8,
-                    12u8, 194u8, 160u8, 66u8,
+                    5u8, 0u8, 0u8, 0u8, 0u8, 0u8, 5u8, 0u8, 10u8, 0u8, 15u8, 0u8, 25u8, 0u8, 3u8,
+                    0u8, 194u8, 160u8, 75u8, 3u8, 0u8, 194u8, 160u8, 107u8, 6u8, 0u8, 194u8, 160u8,
+                    77u8, 9u8, 0u8, 194u8, 160u8, 109u8, 105u8, 108u8, 194u8, 160u8, 77u8, 12u8,
+                    0u8, 194u8, 160u8, 66u8,
                 ])
             },
         )
@@ -194,9 +194,9 @@ static FR: &DataStruct = &::icu_compactdecimal::provider::CompactDecimalPatternD
             unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8, 5u8]) },
             unsafe {
                 ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 5u8, 0u8, 10u8, 0u8, 16u8, 0u8, 0u8, 3u8, 194u8,
-                    160u8, 107u8, 0u8, 6u8, 194u8, 160u8, 77u8, 0u8, 9u8, 194u8, 160u8, 77u8,
-                    100u8, 0u8, 12u8, 194u8, 160u8, 66u8, 110u8,
+                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 5u8, 0u8, 10u8, 0u8, 16u8, 0u8, 3u8, 0u8, 194u8,
+                    160u8, 107u8, 6u8, 0u8, 194u8, 160u8, 77u8, 9u8, 0u8, 194u8, 160u8, 77u8,
+                    100u8, 12u8, 0u8, 194u8, 160u8, 66u8, 110u8,
                 ])
             },
         )
@@ -215,8 +215,8 @@ static JA: &DataStruct = &::icu_compactdecimal::provider::CompactDecimalPatternD
             unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8]) },
             unsafe {
                 ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                    3u8, 0u8, 0u8, 0u8, 0u8, 0u8, 5u8, 0u8, 10u8, 0u8, 0u8, 4u8, 228u8, 184u8,
-                    135u8, 0u8, 8u8, 229u8, 132u8, 132u8, 0u8, 12u8, 229u8, 133u8, 134u8,
+                    3u8, 0u8, 0u8, 0u8, 0u8, 0u8, 5u8, 0u8, 10u8, 0u8, 4u8, 0u8, 228u8, 184u8,
+                    135u8, 8u8, 0u8, 229u8, 132u8, 132u8, 12u8, 0u8, 229u8, 133u8, 134u8,
                 ])
             },
         )
@@ -235,10 +235,10 @@ static RU: &DataStruct = &::icu_compactdecimal::provider::CompactDecimalPatternD
             unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8, 5u8]) },
             unsafe {
                 ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 11u8, 0u8, 21u8, 0u8, 33u8, 0u8, 0u8, 3u8, 194u8,
-                    160u8, 209u8, 130u8, 209u8, 139u8, 209u8, 129u8, 46u8, 0u8, 6u8, 194u8, 160u8,
-                    208u8, 188u8, 208u8, 187u8, 208u8, 189u8, 0u8, 9u8, 194u8, 160u8, 208u8, 188u8,
-                    208u8, 187u8, 209u8, 128u8, 208u8, 180u8, 0u8, 12u8, 194u8, 160u8, 209u8,
+                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 11u8, 0u8, 21u8, 0u8, 33u8, 0u8, 3u8, 0u8, 194u8,
+                    160u8, 209u8, 130u8, 209u8, 139u8, 209u8, 129u8, 46u8, 6u8, 0u8, 194u8, 160u8,
+                    208u8, 188u8, 208u8, 187u8, 208u8, 189u8, 9u8, 0u8, 194u8, 160u8, 208u8, 188u8,
+                    208u8, 187u8, 209u8, 128u8, 208u8, 180u8, 12u8, 0u8, 194u8, 160u8, 209u8,
                     130u8, 209u8, 128u8, 208u8, 187u8, 208u8, 189u8,
                 ])
             },
@@ -258,10 +258,10 @@ static SR_LATN: &DataStruct = &::icu_compactdecimal::provider::CompactDecimalPat
             unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8, 5u8]) },
             unsafe {
                 ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 9u8, 0u8, 17u8, 0u8, 26u8, 0u8, 0u8, 3u8, 194u8,
-                    160u8, 104u8, 105u8, 108u8, 106u8, 46u8, 0u8, 6u8, 194u8, 160u8, 109u8, 105u8,
-                    108u8, 46u8, 0u8, 9u8, 194u8, 160u8, 109u8, 108u8, 114u8, 100u8, 46u8, 0u8,
-                    12u8, 194u8, 160u8, 98u8, 105u8, 108u8, 46u8,
+                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 9u8, 0u8, 17u8, 0u8, 26u8, 0u8, 3u8, 0u8, 194u8,
+                    160u8, 104u8, 105u8, 108u8, 106u8, 46u8, 6u8, 0u8, 194u8, 160u8, 109u8, 105u8,
+                    108u8, 46u8, 9u8, 0u8, 194u8, 160u8, 109u8, 108u8, 114u8, 100u8, 46u8, 12u8,
+                    0u8, 194u8, 160u8, 98u8, 105u8, 108u8, 46u8,
                 ])
             },
         )
@@ -280,10 +280,10 @@ static SR_SR_CYRL: &DataStruct = &::icu_compactdecimal::provider::CompactDecimal
             unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8, 5u8]) },
             unsafe {
                 ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 11u8, 0u8, 22u8, 0u8, 35u8, 0u8, 0u8, 3u8, 194u8,
-                    160u8, 209u8, 133u8, 208u8, 184u8, 209u8, 153u8, 46u8, 0u8, 6u8, 194u8, 160u8,
-                    208u8, 188u8, 208u8, 184u8, 208u8, 187u8, 46u8, 0u8, 9u8, 194u8, 160u8, 208u8,
-                    188u8, 208u8, 187u8, 209u8, 128u8, 208u8, 180u8, 46u8, 0u8, 12u8, 194u8, 160u8,
+                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 11u8, 0u8, 22u8, 0u8, 35u8, 0u8, 3u8, 0u8, 194u8,
+                    160u8, 209u8, 133u8, 208u8, 184u8, 209u8, 153u8, 46u8, 6u8, 0u8, 194u8, 160u8,
+                    208u8, 188u8, 208u8, 184u8, 208u8, 187u8, 46u8, 9u8, 0u8, 194u8, 160u8, 208u8,
+                    188u8, 208u8, 187u8, 209u8, 128u8, 208u8, 180u8, 46u8, 12u8, 0u8, 194u8, 160u8,
                     208u8, 177u8, 208u8, 184u8, 208u8, 187u8, 46u8,
                 ])
             },
@@ -303,9 +303,9 @@ static TR: &DataStruct = &::icu_compactdecimal::provider::CompactDecimalPatternD
             unsafe { ::zerovec::ZeroVec::from_bytes_unchecked(&[5u8, 5u8, 5u8, 5u8]) },
             unsafe {
                 ::zerovec::VarZeroVec::from_bytes_unchecked(&[
-                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 5u8, 0u8, 11u8, 0u8, 17u8, 0u8, 0u8, 3u8, 194u8,
-                    160u8, 66u8, 0u8, 6u8, 194u8, 160u8, 77u8, 110u8, 0u8, 9u8, 194u8, 160u8, 77u8,
-                    114u8, 0u8, 12u8, 194u8, 160u8, 84u8, 110u8,
+                    4u8, 0u8, 0u8, 0u8, 0u8, 0u8, 5u8, 0u8, 11u8, 0u8, 17u8, 0u8, 3u8, 0u8, 194u8,
+                    160u8, 66u8, 6u8, 0u8, 194u8, 160u8, 77u8, 110u8, 9u8, 0u8, 194u8, 160u8, 77u8,
+                    114u8, 12u8, 0u8, 194u8, 160u8, 84u8, 110u8,
                 ])
             },
         )
