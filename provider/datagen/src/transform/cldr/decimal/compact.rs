4a39f6b5092ff27b02d4432ead07a0913f6d7a21
Robin Leroy <egg.robin.leroy@gmail.com>
2022-12-15 12:18:27 +0100

    use superstar

--- provider/datagen/src/transform/cldr/decimal/compact.rs
+++ provider/datagen/src/transform/cldr/decimal/compact.rs
@@ -169,7 +169,7 @@ impl IterableDataProvider<LongCompactDecimalFormatDataV1Marker> for crate::Datag
 #[cfg(test)]
 
 mod tests {
-
+    use super::*;
     use icu_locid::locale;
     use icu_provider::zerofrom::ZeroFrom;
     use std::borrow::Cow;
