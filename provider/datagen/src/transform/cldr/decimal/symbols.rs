3c6aa7139814112396741e7bdf1a169ee3a3b964
Robin Leroy <egg.robin.leroy@gmail.com>
2022-12-15 16:02:01 +0100

    license

--- provider/datagen/src/transform/cldr/decimal/symbols.rs
+++ provider/datagen/src/transform/cldr/decimal/symbols.rs
@@ -1,3 +1,7 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 use crate::transform::cldr::cldr_serde;
 use icu_decimal::provider::*;
 use icu_locid::extensions_unicode_key as key;
