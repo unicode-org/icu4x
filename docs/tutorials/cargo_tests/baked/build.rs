3297db09cff0028c57a932179f1aabb04883ce94
Shane F. Carr <shane@unicode.org>
2022-12-23 13:40:31 -0800

    License headers

--- docs/tutorials/cargo_tests/baked/build.rs
+++ docs/tutorials/cargo_tests/baked/build.rs
@@ -1,3 +1,7 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 use icu::locid::langid;
 use icu_datagen::CldrLocaleSubset;
 use icu_datagen::SourceData;
