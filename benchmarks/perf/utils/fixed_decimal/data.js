342e1c19cd74cccd646c2aa90930fc73253b2f6f
github-action-benchmark <github@users.noreply.github.com>
2022-12-26 00:38:11 +0000

    add Rust Benchmark (cargo) benchmark result for 1ac04880b008e030d913155b50c92c776f4e60cb

--- benchmarks/perf/utils/fixed_decimal/data.js
+++ benchmarks/perf/utils/fixed_decimal/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1671986901906,
+  "lastUpdate": 1672015091794,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Rust Benchmark": [
@@ -62429,6 +62429,66 @@ window.BENCHMARK_DATA = {
             "name": "from_string/1000000001",
             "value": 72,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1ac04880b008e030d913155b50c92c776f4e60cb",
+          "message": "Fix SVG path in README.md",
+          "timestamp": "2022-12-25T18:24:24-06:00",
+          "tree_id": "46504a8ea02423ed4d2e091b361ab09e6740ca0a",
+          "url": "https://github.com/unicode-org/icu4x/commit/1ac04880b008e030d913155b50c92c776f4e60cb"
+        },
+        "date": 1672015007934,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fixed_decimal/overview",
+            "value": 108262,
+            "range": "± 6396",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/smaller",
+            "value": 22147,
+            "range": "± 978",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "isize/larger",
+            "value": 61978,
+            "range": "± 3609",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/to_string/908070605040302010",
+            "value": 196,
+            "range": "± 9",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "to_string/write_to/908070605040302010",
+            "value": 88,
+            "range": "± 4",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "from_string/1000000001",
+            "value": 77,
+            "range": "± 4",
             "unit": "ns/iter"
           }
         ]
