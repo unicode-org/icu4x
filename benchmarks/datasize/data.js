c26554015d725ca8b90210512b0f1530488b39b7
github-action-benchmark <github@users.noreply.github.com>
2022-12-26 00:45:06 +0000

    add Benchmark (ndjson) benchmark result for e5922a846d0a7d6f793266a8f892434b8bb61fd0

--- benchmarks/datasize/data.js
+++ benchmarks/datasize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1672014421402,
+  "lastUpdate": 1672015506362,
   "repoUrl": "https://github.com/unicode-org/icu4x",
   "entries": {
     "Benchmark": [
@@ -32963,6 +32963,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/unicode-org/icu4x/commit/1ac04880b008e030d913155b50c92c776f4e60cb"
         },
         "date": 1672014337316,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 4303051,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "shane@unicode.org",
+            "name": "Shane F. Carr",
+            "username": "sffc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e5922a846d0a7d6f793266a8f892434b8bb61fd0",
+          "message": "Use \"Cargo feature\" language everywhere and other tutorial tweaks (#2922)\n\nThey are \"Cargo\" features, not \"Rust\" features or any other name:\r\n\r\nhttps://doc.rust-lang.org/cargo/reference/features.html",
+          "timestamp": "2022-12-25T18:42:23-06:00",
+          "tree_id": "c70d484fe47f9b92406628c692737d5d56ec811d",
+          "url": "https://github.com/unicode-org/icu4x/commit/e5922a846d0a7d6f793266a8f892434b8bb61fd0"
+        },
+        "date": 1672015417869,
         "tool": "ndjson",
         "benches": [
           {
