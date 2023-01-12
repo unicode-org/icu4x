3d10558efb3c55ff3d48c0ade170c6f79c79e6da
Pawan Dogra <padogra@google.com>
2023-01-12 10:03:04 +0000

    remove hashindex and refactor code

--- utils/zerovec/src/hashmap/mod.rs
+++ utils/zerovec/src/hashmap/mod.rs
@@ -27,6 +27,17 @@ const fn split_hash64(hash: u64, m: u32) -> (usize, u32, u32) {
     )
 }
 
+/// Compute hash using [`T1haHasher`] with modulo [`m`] and split using [`split_hash64`].
+#[inline]
+fn compute_hash<A, K: Hash + ?Sized>(k: &A, m: usize) -> (usize, u32, u32)
+where
+    A: Borrow<K>,
+{
+    let mut hasher = T1haHasher::with_seed(SEED);
+    k.borrow().hash(&mut hasher);
+    split_hash64(hasher.finish(), m as u32)
+}
+
 /// Calculate the displacement using (f0, f1), (d0, d1) in modulo m.
 /// The displacement function is (f0 + f1 * d0 + d1) mod m.
 #[inline]
@@ -34,181 +45,118 @@ fn compute_displacement(f: (u32, u32), d: (u32, u32), m: u32) -> usize {
     (f.1.wrapping_mul(d.0).wrapping_add(f.0).wrapping_add(d.1) % m) as usize
 }
 
-/// Compute hash using [`T1haHasher`] with modulo [`m`] and split using [`split_hash64`].
-#[inline]
-fn compute_hash<K: Hash>(k: &K, m: usize) -> (usize, u32, u32) {
-    let mut hasher = T1haHasher::with_seed(SEED);
-    k.hash(&mut hasher);
-    split_hash64(hasher.finish(), m as u32)
-}
+/// Compute displacements for the given `keys`, which split the keys into distinct slots by a
+/// two-level hashing schema.
+/// 1. Compute hash and split into (g, f0, f1).
+/// 2. Bucket and sort the split hash on g in descending order.
+/// 3. In decreasing order of bucket size, try until a (d0, d1) is found that splits the keys
+///    in the bucket into distinct slots.
+/// 4. Mark the slots for current bucket as occupied and store the reverse mapping.
+/// 5. Repeat untill all the keys have been assigned distinct slots.
+///
+/// Returns
+fn compute_displacements<A, K: Hash + PartialEq + ?Sized>(
+    keys: impl ExactSizeIterator<Item = A>,
+) -> (Vec<(u32, u32)>, Vec<usize>)
+where
+    A: Borrow<K>,
+{
+    let len = keys.len();
 
-/// Denotes an index that maps each of the keys into a distinct slot.
-/// A two-level hashing scheme is used to construct this index.
-#[derive(Debug)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-struct HashIndex<'a> {
-    /// Array of (d0, d1) which splits the keys with same first level hash into distinct
-    /// slots.
-    /// The ith index of the array splits the keys with first level hash i.
-    /// If no key with first level hash is found in the original keys, (0, 0) is used as an empty
-    /// placeholder.
-    #[cfg_attr(feature = "serde", serde(borrow))]
-    displacements: ZeroVec<'a, (u32, u32)>,
-}
+    // A vector to track the size of buckets for sorting.
+    let mut bucket_sizes = vec![0; len];
 
-impl<'a> HashIndex<'a> {
-    /// Build a [`HashIndex`] from keys and values.
-    /// A mutable keys, values container is required, as the containers are permuted according
-    /// to the generated perfect hash.
-    /// The steps are to build are.
-    /// 1. Compute hash and split into (g, f0, f1).
-    /// 2. Bucket and sort the split hash on g in descending order.
-    /// 3. In decreasing order of bucket size, try until a (d0, d1) is found that splits the keys
-    ///    in the bucket into distinct slots.
-    /// 4. Mark the slots for current bucket as occupied and store the reverse mapping.
-    /// 5. Repeat untill all the keys have been assigned distinct slots.
-    /// 6. Permute the keys, values according to the reverse mapping.
-    #[inline]
-    #[allow(clippy::indexing_slicing, clippy::unwrap_used)] // proper documentation at each occurence
-    pub fn build_from_kv_containers<'b, K, V>(
-        keys: &mut K::Container,
-        values: &mut V::Container,
-    ) -> Self
-    where
-        K: ZeroMapKV<'a> + 'b + ?Sized + Hash,
-        V: ZeroMapKV<'a> + ?Sized,
-    {
-        let len = keys.zvl_len();
+    // A flattened representation of items in the buckets after applying first level hash function
+    let mut bucket_flatten = Vec::with_capacity(len);
 
-        // A vector to track the size of buckets for sorting.
-        let mut bucket_sizes = vec![0; len];
+    // Compute initial displacement and bucket sizes
 
-        // A flattened representation of items in the buckets after applying first level hash function
-        let mut bucket_flatten = Vec::with_capacity(len);
+    keys.into_iter().enumerate().for_each(|(i, k)| {
+        let h = compute_hash(&k, len);
+        bucket_sizes[h.0] += 1;
+        bucket_flatten.push((h, i))
+    });
 
-        // Compute initial displacement and bucket sizes
-        for i in 0..len {
-            // Compute (g, f0, f1) of the key bytes.
-            let h = K::Container::zvl_get_as_t(
-                // 0 <= i < keys.len()
-                keys.zvl_get(i).unwrap(),
-                |k| compute_hash(&k, len),
-            );
+    // Sort by decreasing order of bucket_sizes.
+    bucket_flatten.sort_by(|&(ha, _), &(hb, _)| {
+        // ha.0, hb.0 are always within bounds of `bucket_sizes`
+        (bucket_sizes[hb.0], hb).cmp(&(bucket_sizes[ha.0], ha))
+    });
 
-            // 0 <= g < keys.len()
-            bucket_sizes[h.0] += 1;
-            bucket_flatten.push((h, i));
-        }
+    // Generation count while iterating buckets.
+    // Each trial of ((d0, d1), bucket chain) is a new generation.
+    // We use this to track which all slots are assigned for the current bucket chain.
+    let mut generation = 0;
 
-        // Sort by decreasing order of bucket_sizes.
-        bucket_flatten.sort_by(|&(ha, _), &(hb, _)| {
-            // ha.0, hb.0 are always within bounds of `bucket_sizes`
-            (bucket_sizes[hb.0], hb).cmp(&(bucket_sizes[ha.0], ha))
-        });
+    // Whether a slot has been occupied by previous buckets with a different first level hash (different
+    // bucket chain).
+    let mut occupied = vec![false; len];
 
-        // Generation count while iterating buckets.
-        // Each trial of ((d0, d1), bucket chain) is a new generation.
-        // We use this to track which all slots are assigned for the current bucket chain.
-        let mut generation = 0;
+    // Track generation count for the slots.
+    // A slot is empty if either it is unoccupied by the previous bucket chains and the
+    // assignment is not equal to generation.
+    let mut assignments = vec![0; len];
 
-        // Whether a slot has been occupied by previous buckets with a different first level hash (different
-        // bucket chain).
-        let mut occupied = vec![false; len];
+    // Vec to store the displacements (saves us a recomputation of hash while assigning slots).
+    let mut current_displacements = Vec::with_capacity(16);
 
-        // Track generation count for the slots.
-        // A slot is empty if either it is unoccupied by the previous bucket chains and the
-        // assignment is not equal to generation.
-        let mut assignments = vec![0; len];
+    // (d0, d1) which splits the bucket into different slots
+    let mut displacements = vec![(0, 0); len];
 
-        // Vec to store the displacements (saves us a recomputation of hash while assigning slots).
-        let mut current_displacements = Vec::with_capacity(16);
+    // Vec to store mapping to the original order of keys.
+    // This is a permutation which will be applied to keys, values at the end.
+    let mut reverse_mapping = vec![0; len];
 
-        // (d0, d1) which splits the bucket into different slots
-        let mut displacements = vec![(0, 0); len];
+    let mut start = 0;
+    while start < len {
+        // Bucket span with the same first level hash
+        // start is always within bounds of `bucket_flatten`
+        let g = bucket_flatten[start].0 .0;
+        // g is always within bounds of `bucket_sizes`
+        let end = start + bucket_sizes[g];
+        // start, end - 1 are always within bounds of `bucket_sizes`
+        let buckets = &bucket_flatten[start..end];
 
-        // Vec to store mapping to the original order of keys.
-        // This is a permutation which will be applied to keys, values at the end.
-        let mut reverse_mapping = vec![0; len];
-
-        let mut start = 0;
-        while start < len {
-            // Bucket span with the same first level hash
-            // start is always within bounds of `bucket_flatten`
-            let g = bucket_flatten[start].0 .0;
-            // g is always within bounds of `bucket_sizes`
-            let end = start + bucket_sizes[g];
-            // start, end - 1 are always within bounds of `bucket_sizes`
-            let buckets = &bucket_flatten[start..end];
-
-            'd0: for d0 in 0..len as u32 {
-                'd1: for d1 in 0..len as u32 {
-                    if (d0, d1) == (0, 0) {
-                        continue;
-                    }
-                    current_displacements.clear();
-                    generation += 1;
-
-                    for ((_, f0, f1), _) in buckets {
-                        let displacement_idx =
-                            compute_displacement((*f0, *f1), (d0, d1), len as u32);
-
-                        // displacement_idx is always within bounds
-                        if occupied[displacement_idx] || assignments[displacement_idx] == generation
-                        {
-                            continue 'd1;
-                        }
-                        assignments[displacement_idx] = generation;
-                        current_displacements.push(displacement_idx);
-                    }
-
-                    // Successfully found a (d0, d1), store it as index g.
-                    // g < displacements.len() due to modulo operation
-                    displacements[g] = (d0, d1);
-
-                    for (i, displacement_idx) in current_displacements.iter().enumerate() {
-                        // `current_displacements` has same size as `buckets`
-                        let (_, idx) = &buckets[i];
-
-                        // displacement_idx is always within bounds
-                        occupied[*displacement_idx] = true;
-                        reverse_mapping[*displacement_idx] = *idx;
-                    }
-                    break 'd0;
+        'd0: for d0 in 0..len as u32 {
+            'd1: for d1 in 0..len as u32 {
+                if (d0, d1) == (0, 0) {
+                    continue;
                 }
+                current_displacements.clear();
+                generation += 1;
+
+                for ((_, f0, f1), _) in buckets {
+                    let displacement_idx = compute_displacement((*f0, *f1), (d0, d1), len as u32);
+
+                    // displacement_idx is always within bounds
+                    if occupied[displacement_idx] || assignments[displacement_idx] == generation {
+                        continue 'd1;
+                    }
+                    assignments[displacement_idx] = generation;
+                    current_displacements.push(displacement_idx);
+                }
+
+                // Successfully found a (d0, d1), store it as index g.
+                // g < displacements.len() due to modulo operation
+                displacements[g] = (d0, d1);
+
+                for (i, displacement_idx) in current_displacements.iter().enumerate() {
+                    // `current_displacements` has same size as `buckets`
+                    let (_, idx) = &buckets[i];
+
+                    // displacement_idx is always within bounds
+                    occupied[*displacement_idx] = true;
+                    reverse_mapping[*displacement_idx] = *idx;
+                }
+                break 'd0;
             }
-
-            start = end;
         }
 
-        keys.zvl_permute(&mut reverse_mapping.clone());
-        values.zvl_permute(&mut reverse_mapping);
-
-        Self {
-            displacements: ZeroVec::alloc_from_slice(&displacements),
-        }
+        start = end;
     }
 
-    /// Given a [`key`] return the probable index of the key or [`None`] if the key is guaranteed to be absent.
-    /// Another check to determine if the key matches the one at the probable index is required.
-    #[inline]
-    pub fn index<K>(&self, key: &K) -> Option<usize>
-    where
-        K: Hash,
-    {
-        let (g, f0, f1) = compute_hash(key, self.displacements.len());
-        #[allow(clippy::unwrap_used)] // g is in-range
-        let (d0, d1) = self.displacements.get(g).unwrap();
-        if (d0, d1) == (0, 0) {
-            return None;
-        }
-        Some(compute_displacement(
-            (f0, f1),
-            (d0, d1),
-            self.displacements.len() as u32,
-        ))
-    }
+    (displacements, reverse_mapping)
 }
-
 /// A perfect zerohashmap built using [`HashIndex`] optimized for lookups over immutable keys.
 ///
 /// # Examples
@@ -227,8 +175,13 @@ where
     K: ZeroMapKV<'a> + ?Sized,
     V: ZeroMapKV<'a> + ?Sized,
 {
+    /// Array of (d0, d1) which splits the keys with same first level hash into distinct
+    /// slots.
+    /// The ith index of the array splits the keys with first level hash i.
+    /// If no key with first level hash is found in the original keys, (0, 0) is used as an empty
+    /// placeholder.
     #[cfg_attr(feature = "serde", serde(borrow))]
-    index: HashIndex<'a>,
+    displacements: ZeroVec<'a, (u32, u32)>,
     keys: K::Container,
     values: V::Container,
 }
@@ -254,6 +207,26 @@ where
     K: ZeroMapKV<'a> + ?Sized + Hash + Eq,
     V: ZeroMapKV<'a> + ?Sized,
 {
+    /// Given a [`key`] return the probable index of the key or [`None`] if the key is guaranteed to be absent.
+    /// Another check to determine if the key matches the one at the probable index is required.
+    #[inline]
+    pub fn index<A>(&self, key: &A) -> Option<usize>
+    where
+        A: Borrow<K>,
+    {
+        let (g, f0, f1) = compute_hash(key, self.displacements.len());
+        #[allow(clippy::unwrap_used)] // g is in-range
+        let (d0, d1) = self.displacements.get(g).unwrap();
+        if (d0, d1) == (0, 0) {
+            return None;
+        }
+        Some(compute_displacement(
+            (f0, f1),
+            (d0, d1),
+            self.displacements.len() as u32,
+        ))
+    }
+
     /// Get the value corresponding to `key`.
     /// If absent `None` is returned.
     ///
@@ -269,11 +242,14 @@ where
     /// assert_eq!(hashmap.get("0"), None);
     /// ```
     #[inline]
-    pub fn get<'b>(&'b self, key: &'b K) -> Option<&'b V::GetType> {
-        let i = self.index.index(&key)?;
+    pub fn get<'b, A>(&'b self, key: A) -> Option<&'b V::GetType>
+    where
+        A: Borrow<K> + 'b,
+    {
+        let i = self.index(&key)?;
         #[allow(clippy::unwrap_used)] // i is in 0..self.keys.len()
         let found = self.keys.zvl_get(i).unwrap();
-        if K::Container::zvl_get_as_t(found, |found| found == key) {
+        if K::Container::zvl_get_as_t(found, |found| found == key.borrow()) {
             self.values.zvl_get(i)
         } else {
             None
@@ -304,15 +280,22 @@ where
             (lower, None) => lower,
         };
 
+        let mut keys_vec = vec![];
+        keys_vec.reserve(size_hint);
         let mut keys = K::Container::zvl_with_capacity(size_hint);
         let mut values = V::Container::zvl_with_capacity(size_hint);
         for (k, v) in iter {
             keys.zvl_push(k.borrow());
+            keys_vec.push(k);
             values.zvl_push(v.borrow());
         }
-        let index = HashIndex::build_from_kv_containers::<K, V>(&mut keys, &mut values);
+        let (displacements, mut reverse_mapping) = compute_displacements(keys_vec.into_iter());
+
+        keys.zvl_permute(&mut reverse_mapping.clone());
+        values.zvl_permute(&mut reverse_mapping);
+
         Self {
-            index,
+            displacements: ZeroVec::alloc_from_slice(&displacements),
             values,
             keys,
         }
