4f3a6400886430cdad6f40e5f41ee797a8c974d2
snktd <sanketd1019@gmail.com>
2023-01-31 13:31:31 -0800

    Adding support for variants for Language_displaynames

--- experimental/displaynames/src/provider.rs
+++ experimental/displaynames/src/provider.rs
@@ -53,4 +53,13 @@ pub struct LanguageDisplayNamesV1<'data> {
     /// Mapping for language to locale display name.
     #[cfg_attr(feature = "serde", serde(borrow))]
     pub names: ZeroMap<'data, UnvalidatedStr, str>,
+    /// Mapping for language to locale display short name.
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    pub short_names: ZeroMap<'data, UnvalidatedStr, str>,
+    /// Mapping for language to locale display long name.
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    pub long_names: ZeroMap<'data, UnvalidatedStr, str>,
+    /// Mapping for language to locale display menu variant name.
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    pub menu_names: ZeroMap<'data, UnvalidatedStr, str>,
 }
