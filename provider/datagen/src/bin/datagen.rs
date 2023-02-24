12d55e670c95ad8e050b2cbd4c2cd07667c63cda
Manish Goregaokar <manishsmail@gmail.com>
2023-02-24 09:49:54 -0800

    bump clap

--- provider/datagen/src/bin/datagen.rs
+++ provider/datagen/src/bin/datagen.rs
@@ -2,65 +2,60 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use clap::{crate_authors, crate_version, App, Arg, ArgGroup};
+use clap::{crate_authors, crate_version, Command, Arg, ArgGroup};
 use eyre::WrapErr;
 use icu_datagen::prelude::*;
 use simple_logger::SimpleLogger;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 fn main() -> eyre::Result<()> {
-    let matches = App::new("icu4x-datagen")
+    let matches = Command::new("icu4x-datagen")
         .version(crate_version!())
         .author(crate_authors!())
         .about(concat!("Learn more at: https://docs.rs/icu_datagen/", crate_version!()))
         .arg(
-            Arg::with_name("VERBOSE")
-                .short("v")
+            Arg::new("VERBOSE")
+                .short('v')
                 .long("verbose")
                 .help("Requests verbose output"),
         )
         .arg(
-            Arg::with_name("FORMAT")
+            Arg::new("FORMAT")
                 .long("format")
-                .takes_value(true)
-                .possible_value("dir")
-                .possible_value("blob")
-                .possible_value("mod")
-                .possible_value("deprecated-default")
+                .num_args(1)
+                .value_parser(["dir", "blob", "mod", "deprecated-default"])
                 .default_value("deprecated-default")
                 .hide_default_value(true)
                 .help("Select the output format: a directory tree of files, a single blob, or a Rust module."),
         )
         .arg(
-            Arg::with_name("OVERWRITE")
-                .short("W")
+            Arg::new("OVERWRITE")
+                .short('W')
                 .long("overwrite")
                 .help("Delete the output before writing data."),
         )
         .arg(
-            Arg::with_name("SYNTAX")
-                .short("s")
+            Arg::new("SYNTAX")
+                .short('s')
                 .long("syntax")
-                .takes_value(true)
-                .possible_value("json")
-                .possible_value("bincode")
-                .possible_value("postcard")
+                .num_args(1)
+                .value_parser(["json", "bincode", "postcard"])
                 .help("--format=dir only: serde serialization format."),
         )
         .arg(
-            Arg::with_name("PRETTY")
-                .short("p")
+            Arg::new("PRETTY")
+                .short('p')
                 .long("pretty")
                 .help("--format=mod, --format=dir only: pretty-print the Rust or JSON output files."),
         )
         .arg(
-            Arg::with_name("FINGERPRINT")
+            Arg::new("FINGERPRINT")
                 .long("fingerprint")
                 .help("--format=dir only: whether to add a fingerprints file to the output.")
         )
         .arg(
-            Arg::with_name("CLDR_TAG")
-                .short("t")
+            Arg::new("CLDR_TAG")
+                .short('t')
                 .long("cldr-tag")
                 .value_name("TAG")
                 .default_value("latest")
@@ -70,20 +65,20 @@ fn main() -> eyre::Result<()> {
                     Ignored if '--cldr-root' is present.\n\
                     Note that some keys do not support versions before 41.0.0.",
                 )
-                .takes_value(true),
+                .num_args(1),
         )
         .arg(
-            Arg::with_name("CLDR_ROOT")
+            Arg::new("CLDR_ROOT")
                 .long("cldr-root")
                 .value_name("PATH")
                 .help(
                     "Path to a local cldr-{version}-json-full.zip directory (see https://github.com/unicode-org/cldr-json/releases).\n\
                     Note that some keys do not support versions before 41.0.0.",
                 )
-                .takes_value(true),
+                .num_args(1),
         )
         .arg(
-            Arg::with_name("ICUEXPORT_TAG")
+            Arg::new("ICUEXPORT_TAG")
                 .long("icuexport-tag")
                 .value_name("TAG")
                 .default_value("latest")
@@ -93,23 +88,23 @@ fn main() -> eyre::Result<()> {
                     Ignored if '--icuexport-root' is present.\n\
                     Note that some keys do not support versions before release-71-1."
                 )
-                .takes_value(true),
+                .num_args(1),
         )
         .arg(
-            Arg::with_name("ICUEXPORT_ROOT")
+            Arg::new("ICUEXPORT_ROOT")
                 .long("icuexport-root")
                 .value_name("PATH")
                 .help(
                     "Path to a local icuexportdata_uprops_full directory (see https://github.com/unicode-org/icu/releases).\n\
                     Note that some keys do not support versions before release-71-1.",
                 )
-                .takes_value(true),
+                .num_args(1),
         )
         .arg(
-            Arg::with_name("TRIE_TYPE")
+            Arg::new("TRIE_TYPE")
                 .long("trie-type")
-                .takes_value(true)
-                .possible_values(&["small", "fast"])
+                .num_args(1)
+                .value_parser(["small", "fast"])
                 .help(
                     "Whether to optimize CodePointTrie data structures for size (\"small\") or speed (\"fast\").\n\
                     Using \"fast\" mode increases performance of CJK text processing and segmentation. For more\n\
@@ -118,34 +113,34 @@ fn main() -> eyre::Result<()> {
                 .default_value("small"),
         )
         .arg(
-            Arg::with_name("COLLATION_HAN_DATABASE")
+            Arg::new("COLLATION_HAN_DATABASE")
                 .long("collation-han-database")
-                .takes_value(true)
-                .possible_values(&["unihan", "implicit"])
+                .num_args(1)
+                .value_parser(["unihan", "implicit"])
                 .default_value("implicit")
                 .help("Which collation han database to use.")
         )
         .arg(
-            Arg::with_name("INCLUDE_COLLATIONS")
+            Arg::new("INCLUDE_COLLATIONS")
                 .long("include-collations")
-                .multiple(true)
-                .takes_value(true)
-                .possible_values(&["gb2312", "big5han", "search", "searchjl", "search*"])
+                .action(clap::ArgAction::Append)
+                .num_args(1)
+                .value_parser(["gb2312", "big5han", "search", "searchjl", "search*"])
                 .help("Which less-common collation tables to include. 'search*' includes all search tables.")
         )
         .arg(
-            Arg::with_name("CLDR_LOCALE_SUBSET")
+            Arg::new("CLDR_LOCALE_SUBSET")
                 .long("cldr-locale-subset")
-                .hidden(true)
+                .hide(true)
                 .help("Deprecated, use --locales full or --locales modern")
-                .takes_value(true),
+                .num_args(1),
         )
         .arg(
-            Arg::with_name("KEYS")
-                .short("k")
+            Arg::new("KEYS")
+                .short('k')
                 .long("keys")
-                .multiple(true)
-                .takes_value(true)
+                .action(clap::ArgAction::Append)
+                .num_args(1)
                 .help(
                     "Include these resource keys in the output. Accepts multiple arguments.\n\
                     Set to 'all' for all keys, 'experimental-all' to include experimental keys,\n\
@@ -153,30 +148,30 @@ fn main() -> eyre::Result<()> {
                 ),
         )
         .arg(
-            Arg::with_name("KEY_FILE")
+            Arg::new("KEY_FILE")
                 .long("key-file")
-                .takes_value(true)
+                .num_args(1)
                 .help(
                     "Path to text file with resource keys to include, one per line. Empty lines \
                     and lines starting with '#' are ignored.",
                 ),
         )
         .arg(
-            Arg::with_name("KEYS_FOR_BIN")
+            Arg::new("KEYS_FOR_BIN")
                 .long("keys-for-bin")
-                .takes_value(true)
+                .num_args(1)
                 .help(
                     "Analyzes the binary and only includes keys that are used by the binary."
                 ),
         )
         .arg(
-            Arg::with_name("ALL_KEYS")
+            Arg::new("ALL_KEYS")
                 .long("all-keys")
-                .hidden(true)
+                .hide(true)
                 .help("Deprecated: alias for --keys all"),
         )
         .group(
-            ArgGroup::with_name("KEY_MODE")
+            ArgGroup::new("KEY_MODE")
                 .arg("KEYS")
                 .arg("KEY_FILE")
                 .arg("KEYS_FOR_BIN")
@@ -184,26 +179,26 @@ fn main() -> eyre::Result<()> {
                 .required(true),
         )
         .arg(
-            Arg::with_name("LOCALES")
-                .short("l")
+            Arg::new("LOCALES")
+                .short('l')
                 .long("locales")
-                .multiple(true)
-                .takes_value(true)
-                .required_unless("ALL_LOCALES")
+                .action(clap::ArgAction::Append)
+                .num_args(1)
+                .required_unless_present("ALL_LOCALES")
                 .help(
                     "Include this locale in the output. Accepts multiple arguments. \
                     Set to 'full' or 'modern' for the respective CLDR locale sets, or 'none' for no locales.",
                 ),
         )
         .arg(
-            Arg::with_name("ALL_LOCALES")
+            Arg::new("ALL_LOCALES")
                 .long("all-locales")
-                .hidden(true)
+                .hide(true)
                 .help("Deprecated: alias for --locales full"),
         )
         .arg(
-            Arg::with_name("OUTPUT")
-                .short("o")
+            Arg::new("OUTPUT")
+                .short('o')
                 .long("out")
                 .help(
                     "Path to output directory or file. Must be empty or non-existent, unless \
@@ -211,19 +206,19 @@ fn main() -> eyre::Result<()> {
                     For --format=blob, omit this option to dump to stdout. \
                     For --format={dir,mod} defaults to 'icu4x_data'.",
                 )
-                .takes_value(true),
+                .num_args(1),
         )
-        .arg(Arg::with_name("INSERT_FEATURE_GATES")
+        .arg(Arg::new("INSERT_FEATURE_GATES")
             .long("insert-feature-gates")
             .help("--format=mod only: insert feature gates for individual `icu_*` crates. Requires --use-separate-crates")
         )
-        .arg(Arg::with_name("USE_SEPARATE_CRATES")
+        .arg(Arg::new("USE_SEPARATE_CRATES")
             .long("use-separate-crates")
             .help("--format=mod only: use types from individual `icu_*` crates instead of the `icu` meta-crate.")
         )
         .get_matches();
 
-    if matches.is_present("VERBOSE") {
+    if matches.contains_id("VERBOSE") {
         SimpleLogger::new()
             .with_level(log::LevelFilter::Trace)
             .init()
@@ -236,20 +231,20 @@ fn main() -> eyre::Result<()> {
             .unwrap()
     }
 
-    let selected_keys = if matches.is_present("ALL_KEYS") {
+    let selected_keys = if matches.contains_id("ALL_KEYS") {
         icu_datagen::all_keys()
-    } else if let Some(paths) = matches.values_of("KEYS") {
-        match paths.collect::<Vec<_>>().as_slice() {
+    } else if let Some(paths) = matches.get_many("KEYS") {
+        match paths.copied().collect::<Vec<_>>().as_slice() {
             ["none"] => vec![],
             ["all"] => icu_datagen::all_keys(),
             ["experimental-all"] => icu_datagen::all_keys_with_experimental(),
             keys => icu_datagen::keys(keys),
         }
-    } else if let Some(key_file_path) = matches.value_of_os("KEY_FILE") {
-        icu_datagen::keys_from_file(key_file_path)
+    } else if let Some(key_file_path) = matches.get_one::<&Path>("KEY_FILE") {
+        icu_datagen::keys_from_file(*key_file_path)
             .with_context(|| key_file_path.to_string_lossy().into_owned())?
-    } else if let Some(bin_path) = matches.value_of_os("KEYS_FOR_BIN") {
-        icu_datagen::keys_from_bin(bin_path)
+    } else if let Some(bin_path) = matches.get_one::<&Path>("KEYS_FOR_BIN") {
+        icu_datagen::keys_from_bin(*bin_path)
             .with_context(|| bin_path.to_string_lossy().into_owned())?
     } else {
         unreachable!("required group")
@@ -260,51 +255,55 @@ fn main() -> eyre::Result<()> {
     }
 
     let mut source_data = SourceData::default();
-    if let Some(path) = matches.value_of("CLDR_ROOT") {
+    if let Some(path) = matches.get_one::<&Path>("CLDR_ROOT") {
         source_data = source_data.with_cldr(PathBuf::from(path), Default::default())?;
     } else {
         source_data = source_data.with_cldr_for_tag(
-            if Some("latest") == matches.value_of("CLDR_TAG") {
+            if Some(&"latest") == matches.get_one("CLDR_TAG") {
                 SourceData::LATEST_TESTED_CLDR_TAG
             } else {
-                matches.value_of("CLDR_TAG").unwrap()
+                *matches.get_one("CLDR_TAG").unwrap()
             },
             Default::default(),
         )?
     }
 
-    if let Some(path) = matches.value_of("ICUEXPORT_ROOT") {
-        source_data = source_data.with_icuexport(PathBuf::from(path))?;
+    if let Some(path) = matches.get_one::<&Path>("ICUEXPORT_ROOT") {
+        source_data = source_data.with_icuexport(PathBuf::from(*path))?;
     } else {
         source_data = source_data.with_icuexport_for_tag(
-            if Some("latest") == matches.value_of("ICUEXPORT_TAG") {
+            if Some(&"latest") == matches.get_one("ICUEXPORT_TAG") {
                 SourceData::LATEST_TESTED_ICUEXPORT_TAG
             } else {
-                matches.value_of("ICUEXPORT_TAG").unwrap()
+                *matches.get_one("ICUEXPORT_TAG").unwrap()
             },
         )?;
     }
 
-    if matches.value_of("TRIE_TYPE") == Some("fast") {
+    if matches.get_one("TRIE_TYPE") == Some(&"fast") {
         source_data = source_data.with_fast_tries();
     }
 
     source_data =
-        source_data.with_collation_han_database(match matches.value_of("COLLATION_HAN_DATABASE") {
-            Some("unihan") => CollationHanDatabase::Unihan,
+        source_data.with_collation_han_database(match matches.get_one("COLLATION_HAN_DATABASE") {
+            Some(&"unihan") => CollationHanDatabase::Unihan,
             _ => CollationHanDatabase::Implicit,
         });
 
-    if let Some(collations) = matches.values_of("INCLUDE_COLLATIONS") {
+    if let Some(collations) = matches.get_many::<String>("INCLUDE_COLLATIONS") {
         source_data =
             source_data.with_collations(collations.into_iter().map(String::from).collect());
     }
 
-    let raw_locales = matches.values_of("LOCALES").unwrap().collect::<Vec<_>>();
+    let raw_locales = matches
+        .get_many("LOCALES")
+        .unwrap()
+        .copied()
+        .collect::<Vec<_>>();
 
     let selected_locales = if raw_locales == ["none"] || selected_keys.is_empty() {
         Some(vec![])
-    } else if raw_locales == ["full"] || matches.is_present("ALL_LOCALES") {
+    } else if raw_locales == ["full"] || matches.contains_id("ALL_LOCALES") {
         None
     } else if let Some(locale_subsets) = raw_locales
         .iter()
@@ -329,29 +328,29 @@ fn main() -> eyre::Result<()> {
         )
     };
 
-    let out = match matches.value_of("FORMAT").expect("required") {
+    let out = match *matches.get_one("FORMAT").expect("required") {
         v @ ("dir" | "deprecated-default") => {
             if v == "deprecated-default" {
                 log::warn!("Defaulting to --format=dir. This will become a required parameter in the future.");
             }
             icu_datagen::Out::Fs {
                 output_path: matches
-                    .value_of_os("OUTPUT")
+                    .get_one::<&Path>("OUTPUT")
                     .map(PathBuf::from)
                     .unwrap_or_else(|| PathBuf::from("icu4x_data")),
-                serializer: match matches.value_of("SYNTAX") {
-                    Some("bincode") => Box::<syntax::Bincode>::default(),
-                    Some("postcard") => Box::<syntax::Postcard>::default(),
-                    _ if matches.is_present("PRETTY") => Box::new(syntax::Json::pretty()),
+                serializer: match matches.get_one("SYNTAX") {
+                    Some(&"bincode") => Box::<syntax::Bincode>::default(),
+                    Some(&"postcard") => Box::<syntax::Postcard>::default(),
+                    _ if matches.contains_id("PRETTY") => Box::new(syntax::Json::pretty()),
                     _ => Box::<syntax::Json>::default(),
                 },
-                overwrite: matches.is_present("OVERWRITE"),
-                fingerprint: matches.is_present("FINGERPRINT"),
+                overwrite: matches.contains_id("OVERWRITE"),
+                fingerprint: matches.contains_id("FINGERPRINT"),
             }
         }
-        "blob" => icu_datagen::Out::Blob(if let Some(path) = matches.value_of_os("OUTPUT") {
+        "blob" => icu_datagen::Out::Blob(if let Some(path) = matches.get_one::<&Path>("OUTPUT") {
             let path_buf = PathBuf::from(path);
-            if !matches.is_present("OVERWRITE") && path_buf.exists() {
+            if !matches.contains_id("OVERWRITE") && path_buf.exists() {
                 eyre::bail!("Output path is present: {:?}", path_buf);
             }
             Box::new(
@@ -363,15 +362,15 @@ fn main() -> eyre::Result<()> {
         }),
         "mod" => {
             let mod_directory = matches
-                .value_of_os("OUTPUT")
+                .get_one::<&Path>("OUTPUT")
                 .map(PathBuf::from)
                 .unwrap_or_else(|| PathBuf::from("icu4x_data"));
 
             let mut options = BakedOptions::default();
-            options.pretty = matches.is_present("PRETTY");
-            options.insert_feature_gates = matches.is_present("INSERT_FEATURE_GATES");
-            options.use_separate_crates = matches.is_present("USE_SEPARATE_CRATES");
-            options.overwrite = matches.is_present("OVERWRITE");
+            options.pretty = matches.contains_id("PRETTY");
+            options.insert_feature_gates = matches.contains_id("INSERT_FEATURE_GATES");
+            options.use_separate_crates = matches.contains_id("USE_SEPARATE_CRATES");
+            options.overwrite = matches.contains_id("OVERWRITE");
 
             icu_datagen::Out::Baked {
                 mod_directory,
