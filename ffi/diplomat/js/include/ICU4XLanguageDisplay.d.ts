af518ea58ab3a065ebba7c181277cedc6a11a0fa
Sanket Desai <90648681+snktd@users.noreply.github.com>
2023-03-21 16:01:26 -0700

    FFI bindings for Language and Region Displaynames. (#3188)
    * FFI bindings for Language and Region Displaynames.
    
    * Fix unrelated ffi files.
    
    * Reverting a few unrelated ffi changes.
    
    * Addressing comments and resolving a few CI errors.
    
    * Resolve conflicts with diplomat version
    
    * Addressing review comments
    
    * Addressing the comments and applying the 'gn' patch

---
+++ ffi/diplomat/js/include/ICU4XLanguageDisplay.d.ts
@@ -0,0 +1,13 @@
+
+/**
+
+ * See the {@link https://unicode-org.github.io/icu4x-docs/doc/icu/displaynames/options/enum.LanguageDisplay.html Rust documentation for `LanguageDisplay`} for more information.
+ */
+export enum ICU4XLanguageDisplay {
+  /**
+   */
+  Dialect = 'Dialect',
+  /**
+   */
+  Standard = 'Standard',
+}
