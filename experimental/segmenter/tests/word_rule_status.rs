7cf24afa9d1e8316fb716c119738df5bd7a48cb5
Shane F. Carr <shane@unicode.org>
2023-04-10 17:52:42 -0700

    Fix test compile

--- experimental/segmenter/tests/word_rule_status.rs
+++ experimental/segmenter/tests/word_rule_status.rs
@@ -2,8 +2,8 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use icu_segmenter::RuleStatusType;
 use icu_segmenter::WordSegmenter;
+use icu_segmenter::WordType;
 
 #[test]
 fn rule_status() {
@@ -12,27 +12,27 @@ fn rule_status() {
     let mut iter = segmenter.segment_str("hello world 123");
 
     assert_eq!(iter.next(), Some(0), "SOT");
-    assert_eq!(iter.word_type(), RuleStatusType::None, "none");
+    assert_eq!(iter.word_type(), WordType::None, "none");
     assert!(!iter.is_word_like(), "SOT is false");
 
     assert_eq!(iter.next(), Some(5), "after hello");
-    assert_eq!(iter.word_type(), RuleStatusType::Letter, "letter");
+    assert_eq!(iter.word_type(), WordType::Letter, "letter");
     assert!(iter.is_word_like(), "Letter is true");
 
     assert_eq!(iter.next(), Some(6), "after space");
-    assert_eq!(iter.word_type(), RuleStatusType::None, "none");
+    assert_eq!(iter.word_type(), WordType::None, "none");
     assert!(!iter.is_word_like(), "None is false");
 
     assert_eq!(iter.next(), Some(11), "after world");
-    assert_eq!(iter.word_type(), RuleStatusType::Letter, "letter");
+    assert_eq!(iter.word_type(), WordType::Letter, "letter");
     assert!(iter.is_word_like(), "Letter is true");
 
     assert_eq!(iter.next(), Some(12), "after space");
-    assert_eq!(iter.word_type(), RuleStatusType::None, "none");
+    assert_eq!(iter.word_type(), WordType::None, "none");
     assert!(!iter.is_word_like(), "None is false");
 
     assert_eq!(iter.next(), Some(15), "after number");
-    assert_eq!(iter.word_type(), RuleStatusType::Number, "number");
+    assert_eq!(iter.word_type(), WordType::Number, "number");
     assert!(iter.is_word_like(), "Number is true");
 }
 
@@ -43,15 +43,15 @@ fn rule_status_th() {
     let mut iter = segmenter.segment_str("ภาษาไทยภาษาไทย");
 
     assert_eq!(iter.next(), Some(0), "SOT");
-    assert_eq!(iter.word_type(), RuleStatusType::None, "none");
+    assert_eq!(iter.word_type(), WordType::None, "none");
     assert!(!iter.is_word_like(), "SOT is false");
 
     assert_eq!(iter.next(), Some(12), "after 1st word");
-    assert_eq!(iter.word_type(), RuleStatusType::Letter, "letter");
+    assert_eq!(iter.word_type(), WordType::Letter, "letter");
     assert!(iter.is_word_like(), "Letter(Thai) is true");
 
     assert_eq!(iter.next(), Some(21), "after 2nd word");
-    assert_eq!(iter.word_type(), RuleStatusType::Letter, "letter");
+    assert_eq!(iter.word_type(), WordType::Letter, "letter");
     assert!(iter.is_word_like(), "Letter(Thai) is true");
 }
 
@@ -63,11 +63,11 @@ fn rule_status_no_word() {
     let mut iter = segmenter.segment_str("hello");
 
     assert_eq!(iter.next(), Some(0), "SOT");
-    assert_eq!(iter.rule_status(), RuleStatusType::None, "none");
+    assert_eq!(iter.rule_status(), WordType::None, "none");
     assert!(!iter.is_word_like(), "always false");
 
     assert_eq!(iter.next(), Some(5), "1st sentence");
-    assert_eq!(iter.rule_status(), RuleStatusType::None, "none");
+    assert_eq!(iter.rule_status(), WordType::None, "none");
     assert!(!iter.is_word_like(), "always false");
 }
 */
