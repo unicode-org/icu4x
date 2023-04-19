6ca3ba644d8943e1fdd98fb3a311fa783606bc34
Robert Bastian <robertbastian@google.com>
2023-04-19 16:31:27 +0200

    fmt

--- components/segmenter/src/complex.rs
+++ components/segmenter/src/complex.rs
@@ -2,10 +2,10 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-#[cfg(feature = "lstm")]
-use crate::lstm::LstmSegmenter;
 use crate::dictionary::DictionarySegmenter;
 use crate::language::*;
+#[cfg(feature = "lstm")]
+use crate::lstm::LstmSegmenter;
 use crate::provider::*;
 use alloc::vec::Vec;
 use icu_locid::{locale, Locale};
