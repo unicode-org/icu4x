d53a2091c5edede29e059af6d0667f710c6b0d15
Pawan Mohan Dogra <pawan.dogra99@gmail.com>
2023-04-20 19:49:36 +0000

    move to wasmer 3.1.1

--- components/collections/codepointtrie_builder/src/wasm.rs
+++ components/collections/codepointtrie_builder/src/wasm.rs
@@ -6,16 +6,11 @@ use crate::CodePointTrieBuilder;
 use crate::CodePointTrieBuilderData;
 use icu_collections::codepointtrie::TrieType;
 use icu_collections::codepointtrie::TrieValue;
-use lazy_static::lazy_static;
+use std::io::{Read, Write};
 use wasmer::{Instance, Module, Store};
-use wasmer_wasi::{Pipe, WasiState};
+use wasmer_wasi::{Pipe, WasiError, WasiState};
 
 const WASM_BYTES: &[u8] = include_bytes!("../list_to_ucptrie.wasm");
-
-lazy_static! {
-    static ref STORE: Store = Store::default();
-    static ref MODULE: Module = Module::new(&STORE, WASM_BYTES).expect("valid WASM");
-}
 
 pub(crate) fn run_wasm<T>(builder: &CodePointTrieBuilder<T>) -> String
 where
@@ -32,37 +27,33 @@ where
         .to_owned(),
         format!("{}", std::mem::size_of::<T::ULE>() * 8),
     ];
-    let mut wasi_env = WasiState::new("list_to_ucptrie")
-        .stdin(Box::new(Pipe::new()))
-        .stdout(Box::new(Pipe::new()))
+    let mut store = Store::default();
+    let module = Module::new(&store, WASM_BYTES).expect("valid WASM");
+
+    let mut stdin = Pipe::new();
+    let mut stdout = Pipe::new();
+
+    let wasi_env = WasiState::new("list_to_ucptrie")
+        .stdin(Box::new(stdin.clone()))
+        .stdout(Box::new(stdout.clone()))
         .args(args)
-        .finalize()
-        .expect("valid arguments + in-memory filesystem");
+        .finalize(&mut store)
+        .expect("Unable to create wasi_env");
 
     // Create the WebAssembly instance with the module and the WasiState
-    let import_object = wasi_env.import_object(&MODULE).expect("walid wasm file");
-    let instance = Instance::new(&MODULE, &import_object).expect("valid wasm file");
+    let import_object = wasi_env
+        .import_object(&mut store, &module)
+        .expect("walid wasm file");
+    let instance = Instance::new(&mut store, &module, &import_object).expect("valid instance");
+    let memory = instance.exports.get_memory("memory").expect("memory");
+    wasi_env.data_mut(&mut store).set_memory(memory.clone());
 
     // To write to the stdin, we need a mutable reference to the pipe
-    //
-    // We access WasiState in a nested scope to ensure we're not holding
-    // the mutex after we need it.
-    {
-        let mut state = wasi_env.state();
-        let wasi_stdin = state
-            .fs
-            .stdin_mut()
-            .expect("valid pipe")
-            .as_mut()
-            .expect("valid pipe");
-        // Write each value to the pipe
-        let CodePointTrieBuilderData::ValuesByCodePoint(values) = builder.data;
-        writeln!(wasi_stdin, "{}", values.len()).expect("valid pipe");
-
-        for value in values {
-            let num: u32 = (*value).into();
-            writeln!(wasi_stdin, "{num}").expect("valid pipe");
-        }
+    let CodePointTrieBuilderData::ValuesByCodePoint(values) = builder.data;
+    writeln!(stdin, "{}", values.len()).expect("valid pipe");
+    for value in values {
+        let num: u32 = (*value).into();
+        writeln!(stdin, "{num}").expect("valid pipe");
     }
 
     // Call the `_start` function to run the tool
@@ -70,26 +61,20 @@ where
         .exports
         .get_function("_start")
         .expect("function exists");
-    let exit_result = start.call(&[]);
+    let exit_result = start.call(&mut store, &[]);
 
     if let Err(e) = exit_result {
-        panic!("list_to_ucptrie failed in C++: args were: {args:?}: {e}");
+        match e.downcast::<WasiError>() {
+            Ok(WasiError::Exit(0)) => {}
+            Ok(e) => panic!("list_to_ucptrie failed in C++: args were: {args:?}: {e:?}"),
+            Err(e) => panic!("list_to_ucptrie failed in C++: args were: {args:?}: {e:?}"),
+        }
     }
-
-    // To read from the stdout/stderr, we again need a mutable reference to the pipe
-    let mut state = wasi_env.state();
-    let wasi_stdout = state
-        .fs
-        .stdout_mut()
-        .expect("valid pipe")
-        .as_mut()
-        .expect("valid pipe");
 
     // The output is a TOML blob, which we can save in a string
     let mut buf = String::new();
-    wasi_stdout
+    stdout
         .read_to_string(&mut buf)
         .expect("pipe contains valid utf-8");
-
     buf
 }
