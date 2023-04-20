88c464553dc6d41834f3166fc815100b2de90058
Robert Bastian <robertbastian@google.com>
2023-04-20 15:31:48 +0200

    remove octal workaround

--- provider/testdata/data/baked/segmenter/line_v1/mod.rs
+++ provider/testdata/data/baked/segmenter/line_v1/mod.rs
@@ -1,6 +1,5 @@
 // @generated
 #![cfg(feature = "icu_segmenter")]
-#![allow(clippy::octal_escapes)]
 type DataStruct =
     <::icu_segmenter::provider::LineBreakDataV1Marker as ::icu_provider::DataMarker>::Yokeable;
 pub fn lookup(locale: &icu_provider::DataLocale) -> Option<&'static DataStruct> {
