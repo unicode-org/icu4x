2c960c573f004dc3b4ea782a6e9f68858a8ed537
Robert Bastian <robertbastian@google.com>
2023-04-28 12:38:44 +0200

    remove fallback

--- provider/datagen/src/transform/segmenter/mod.rs
+++ provider/datagen/src/transform/segmenter/mod.rs
@@ -773,14 +773,9 @@ impl crate::DatagenProvider {
 
         let toml_data: &SegmenterDictionaryData = self
             .source
-            .icuexport()
-            .and_then(|e| e.read_and_parse_toml(filename))
-            .or_else(|e| {
-                self.source
-                    .builtin()
-                    .read_and_parse_toml(filename)
-                    .map_err(|_| e)
-            })?;
+            .builtin()
+            .read_and_parse_toml(filename)
+            .expect("built-in file present and readable");
 
         Ok(UCharDictionaryBreakDataV1 {
             trie_data: ZeroVec::alloc_from_slice(&toml_data.trie_data),
