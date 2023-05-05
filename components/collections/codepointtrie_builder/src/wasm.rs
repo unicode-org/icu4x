e746aa2ce33e8e4dfe839b8e763f8bd19f3f8829
Pawan Dogra <pawan.dogra99@gmail.com>
2023-05-05 04:52:28 +0000

    use wasmer 3.3.0

--- components/collections/codepointtrie_builder/src/wasm.rs
+++ components/collections/codepointtrie_builder/src/wasm.rs
@@ -7,8 +7,8 @@ use crate::CodePointTrieBuilderData;
 use icu_collections::codepointtrie::TrieType;
 use icu_collections::codepointtrie::TrieValue;
 use std::io::{Read, Write};
-use wasmer::{Instance, Module, Store};
-use wasmer_wasi::{Pipe, WasiError, WasiState};
+use wasmer::{Module, Store};
+use wasmer_wasix::{Pipe, WasiEnv};
 
 const WASM_BYTES: &[u8] = include_bytes!("../list_to_ucptrie.wasm");
 
@@ -16,7 +16,7 @@ pub(crate) fn run_wasm<T>(builder: &CodePointTrieBuilder<T>) -> String
 where
     T: TrieValue + Into<u32>,
 {
-    // Set up the execution environment with a WasiState
+    // Set up the execution environment with a WasiEnv
     let args = &[
         format!("{}", builder.default_value.into()),
         format!("{}", builder.error_value.into()),
@@ -30,50 +30,26 @@ where
     let mut store = Store::default();
     let module = Module::new(&store, WASM_BYTES).expect("valid WASM");
 
-    let mut stdin = Pipe::new();
-    let mut stdout = Pipe::new();
+    let (mut stdin_sender, stdin_reader) = Pipe::channel();
+    let (stdout_sender, mut stdout_reader) = Pipe::channel();
 
-    let wasi_env = WasiState::new("list_to_ucptrie")
-        .stdin(Box::new(stdin.clone()))
-        .stdout(Box::new(stdout.clone()))
-        .args(args)
-        .finalize(&mut store)
-        .expect("Unable to create wasi_env");
-
-    // Create the WebAssembly instance with the module and the WasiState
-    let import_object = wasi_env
-        .import_object(&mut store, &module)
-        .expect("walid wasm file");
-    let instance = Instance::new(&mut store, &module, &import_object).expect("valid instance");
-    let memory = instance.exports.get_memory("memory").expect("memory");
-    wasi_env.data_mut(&mut store).set_memory(memory.clone());
-
-    // To write to the stdin, we need a mutable reference to the pipe
     let CodePointTrieBuilderData::ValuesByCodePoint(values) = builder.data;
-    writeln!(stdin, "{}", values.len()).expect("valid pipe");
+    writeln!(stdin_sender, "{}", values.len()).expect("valid pipe");
     for value in values {
         let num: u32 = (*value).into();
-        writeln!(stdin, "{num}").expect("valid pipe");
+        writeln!(stdin_sender, "{num}").expect("valid pipe");
     }
 
-    // Call the `_start` function to run the tool
-    let start = instance
-        .exports
-        .get_function("_start")
-        .expect("function exists");
-    let exit_result = start.call(&mut store, &[]);
-
-    if let Err(e) = exit_result {
-        match e.downcast::<WasiError>() {
-            Ok(WasiError::Exit(0)) => {}
-            Ok(e) => panic!("list_to_ucptrie failed in C++: args were: {args:?}: {e:?}"),
-            Err(e) => panic!("list_to_ucptrie failed in C++: args were: {args:?}: {e:?}"),
-        }
-    }
+    WasiEnv::builder("list_to_ucptrie")
+        .stdin(Box::new(stdin_reader))
+        .stdout(Box::new(stdout_sender))
+        .args(args)
+        .run_with_store(module, &mut store)
+        .expect("run");
 
     // The output is a TOML blob, which we can save in a string
     let mut buf = String::new();
-    stdout
+    stdout_reader
         .read_to_string(&mut buf)
         .expect("pipe contains valid utf-8");
     buf
