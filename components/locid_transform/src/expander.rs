f296e89bb4fbedc6c382c56a28f7eac615b6aabd
Niels Saurer <nielssaurer@google.com>
2023-06-06 12:56:23 +0000

    add todo comment

--- components/locid_transform/src/expander.rs
+++ components/locid_transform/src/expander.rs
@@ -516,6 +516,7 @@ impl LocaleExpander {
         }
     }
 
+    // TODO(3492): consider turning this and a future get_likely_region/get_likely_language public
     #[allow(dead_code)]
     #[inline]
     pub(crate) fn get_likely_script<T: AsRef<LanguageIdentifier>>(
