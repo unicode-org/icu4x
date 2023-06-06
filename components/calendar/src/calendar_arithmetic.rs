5c2ea06b1d718317093527f5c1da3098a32d1110
Shane F. Carr <shane@unicode.org>
2023-06-05 22:25:57 -0700

    Calendar Ord follow-ups (#3470)

--- components/calendar/src/calendar_arithmetic.rs
+++ components/calendar/src/calendar_arithmetic.rs
@@ -7,6 +7,7 @@ use core::convert::TryInto;
 use core::marker::PhantomData;
 use tinystr::tinystr;
 
+// Note: The Ord/PartialOrd impls can be derived because the fields are in the correct order.
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
 #[allow(clippy::exhaustive_structs)] // this type is stable
 pub struct ArithmeticDate<C: CalendarArithmetic> {
