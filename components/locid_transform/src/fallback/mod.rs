0b1c5af7bdde80775896320df262211e0f5b179d
Robert Bastian <robertbastian@users.noreply.github.com>
2023-06-15 00:01:01 +0200

    Baked data for `icu_calendar` (#3534)

--- components/locid_transform/src/fallback/mod.rs
+++ components/locid_transform/src/fallback/mod.rs
@@ -305,7 +305,7 @@ impl LocaleFallbacker {
     }
 
     icu_provider::gen_any_buffer_data_constructors!(locale: skip, options: skip, error: DataError,
-        #[cfg(skip_new)]
+        #[cfg(skip)]
         functions: [
             new,
             try_new_with_any_provider,
