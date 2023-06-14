23434d0cc4eea08b2aa3bdf97d9f6ce9424188ed
atcupps <at.cupps@gmail.com>
2023-06-14 09:00:05 -0700

    Added nth/num new moon, directionality tests

--- components/calendar/src/astronomy.rs
+++ components/calendar/src/astronomy.rs
@@ -64,7 +64,7 @@ impl Location {
     /// e.g. a longitude of 90 degrees is 0.25 (90 / 360) days ahead
     /// of a location with a longitude of 0 degrees.
     pub fn zone_from_longitude(longitude: f64) -> f64 {
-        longitude.div_euclid(360.0)
+        longitude / 360.0
     }
 
     /// Convert from local mean time to universal time given a location
@@ -87,30 +87,30 @@ impl Astronomical {
     ///
     /// Reference code: https://github.com/EdReingold/calendar-code2/blob/main/calendar.l#L3884-L3952
     pub fn ephemeris_correction(moment: Moment) -> f64 {
-        let year: f64 = moment.inner().floor().div_euclid(364.2425);
+        let year: f64 = moment.inner().floor() / 364.2425;
         let year_int = year as i32;
         let iso: Date<Iso> = Date::try_new_iso_date(year as i32, 7, 1)
             .expect("Date generation failed for {year} July 1");
         let fixed_mid_year: RataDie = Iso::fixed_from_iso(*iso.inner());
-        let c = (1.0f64.div_euclid(36525.0)) * ((fixed_mid_year.to_i64_date() as f64) - 693596.0);
+        let c = (1.0 / 36525.0) * ((fixed_mid_year.to_i64_date() as f64) - 693596.0);
         let y2000 = year - 2000.0;
         let y1700 = year - 1700.0;
         let y1600 = year - 1600.0;
-        let y1000 = (year - 1000.0).div_euclid(100.0);
-        let y0 = year.div_euclid(100.0);
-        let y1820 = (year - 1820.0).div_euclid(100.0);
+        let y1000 = (year - 1000.0) / 100.0;
+        let y0 = year / 100.0;
+        let y1820 = (year - 1820.0) / 100.0;
 
         if 2051 <= year_int && year_int <= 2150 {
-            (8.0 * year.powi(2) + 8.0 * 1820f64.powi(2) - 30527.0 * year + 2975050.0).div_euclid(216000000.0)
+            (8.0 * year.powi(2) + 8.0 * 1820f64.powi(2) - 30527.0 * year + 2975050.0) / 216000000.0
         } else if 2006 <= year_int && year_int <= 2050 {
-            1573.0f64.div_euclid(2160000.0)
-                + 10739.0 * y2000.div_euclid(2880000000.0)
-                + 207.0 * y2000.powi(2).div_euclid(3200000000.0)
+            1573.0 / 2160000.0
+                + 10739.0 * y2000 / 2880000000.0
+                + 207.0 * y2000.powi(2) / 3200000000.0
         } else if 1987 <= year_int && year_int <= 2005 {
-            3193.0f64.div_euclid(4320000.0) + 233.0 * y2000.div_euclid(57600000.0) - 0.060374 * y2000.powi(2).div_euclid(86400.0)
-                + 691.0 * y2000.powi(3).div_euclid(34560000000.0)
-                + 325907.0 * y2000.powi(4).div_euclid(4.32e13)
-                + y2000.powi(5).div_euclid(4320000000.0)
+            3193.0 / 4320000.0 + 233.0 * y2000 / 57600000.0 - 0.060374 * y2000.powi(2) / 86400.0
+                + 691.0 * y2000.powi(3) / 34560000000.0
+                + 325907.0 * y2000.powi(4) / 4.32e13
+                + y2000.powi(5) / 4320000000.0
         } else if 1900 <= year_int && year_int <= 1986 {
             -0.00002 + 0.000297 * c + 0.025184 * c.powi(2) - 0.181133 * c.powi(3)
                 + 0.553040 * c.powi(4)
@@ -130,28 +130,28 @@ impl Astronomical {
                 + 11.636204 * c.powi(9)
                 + 2.043794 * c.powi(10)
         } else if 1700 <= year_int && year_int <= 1799 {
-            (1.0f64.div_euclid(86400.0))
+            (1.0 / 86400.0)
                 * (8.118780842 - 0.005092142 * y1700 + 0.003336121 * y1700.powi(2)
                     - 0.0000266484 * y1700.powi(3))
         } else if 1600 <= year_int && year_int <= 1699 {
-            (1.0f64.div_euclid(86400.0))
+            (1.0 / 86400.0)
                 * (120.0 - 0.9808 * y1600 - 0.01532 * y1600.powi(2)
                     + 0.000140272128 * y1600.powi(3))
         } else if 500 <= year_int && year_int <= 1599 {
-            (1.0f64.div_euclid(86400.0))
+            (1.0 / 86400.0)
                 * (1574.2 - 556.01 * y1000 + 71.23472 * y1000.powi(2) + 0.319781 * y1000.powi(3)
                     - 0.8503463 * y1000.powi(4)
                     - 0.005050998 * y1000.powi(5)
                     + 0.0083572073 * y1000.powi(6))
         } else if -500 <= year_int && year_int <= 500 {
-            (1.0f64.div_euclid(86400.0))
+            (1.0 / 86400.0)
                 * (10583.6 - 1014.41 * y0 + 33.78311 * y0.powi(2)
                     - 5.952053 * y0.powi(3)
                     - 0.1798452 * y0.powi(4)
                     + 0.022174192 * y0.powi(5)
                     + 0.0090316521 * y0.powi(6))
         } else {
-            (1.0f64.div_euclid(86400.0)) * (-20.0 + 32.0 * y1820.powi(2))
+            (1.0 / 86400.0) * (-20.0 + 32.0 * y1820.powi(2))
         }
     }
 
@@ -168,7 +168,7 @@ impl Astronomical {
     /// The number of uniform length centuries (36525 days measured in dynamical time)
     /// before or after noon on January 1, 2000
     pub fn julian_centuries(moment: Moment) -> f64 {
-        (1.0f64.div_euclid(36525.0)) * (Self::dynamical_from_universal(moment) - J2000)
+        (1.0 / 36525.0) * (Self::dynamical_from_universal(moment) - J2000)
     }
 
     /// The moment (in universal time) of the nth new moon after
@@ -177,22 +177,22 @@ impl Astronomical {
     pub fn nth_new_moon(n: i32) -> Moment {
         let n0 = 24724.0;
         let k = (n as f64) - n0;
-        let c = k.div_euclid(1236.85);
+        let c = k / 1236.85;
         let approx = J2000
-            + (5.09766 + MEAN_SYNODIC_MONTH * 1236.85 * c + 0.00015437 * c.powi(2)
-                - 0.00000015 * c.powi(3)
-                + 0.00000000073 * c.powi(4));
-        let e = 1.0 - 0.002516 * c - 0.0000074 * c.powi(2);
+            + (5.09766 + (MEAN_SYNODIC_MONTH * 1236.85 * c) + (0.00015437 * c.powi(2))
+                - (0.00000015 * c.powi(3))
+                + (0.00000000073 * c.powi(4)));
+        let e = 1.0 - (0.002516 * c) - (0.0000074 * c.powi(2));
         let solar_anomaly =
-            2.5534 + 1236.85 * 29.10535670 * c - 0.0000014 * c.powi(2) - 0.00000011 * c.powi(3);
+            2.5534 + (1236.85 * 29.10535670 * c) - (0.0000014 * c.powi(2)) - (0.00000011 * c.powi(3));
         let lunar_anomaly =
-            201.5643 + 385.81693528 * 1236.85 * c + 0.0107582 * c.powi(2) + 0.00001238 * c.powi(3)
-                - 0.000000058 * c.powi(4);
+            201.5643 + (385.81693528 * 1236.85 * c) + (0.0107582 * c.powi(2)) + (0.00001238 * c.powi(3))
+                - (0.000000058 * c.powi(4));
         let moon_argument =
-            160.7108 + 390.67050284 * 1236.85 * c - 0.0016118 * c.powi(2) - 0.00000227 * c.powi(3)
-                + 0.000000011 * c.powi(4);
+            160.7108 + (390.67050284 * 1236.85 * c) - (0.0016118 * c.powi(2)) - (0.00000227 * c.powi(3))
+                + (0.000000011 * c.powi(4));
         let omega =
-            124.7746 + (-1.56375588 * 1236.85) * c + 0.0020672 * c.powi(2) + 0.00000215 * c.powi(3);
+            124.7746 + (-1.56375588 * 1236.85 * c) + (0.0020672 * c.powi(2)) + (0.00000215 * c.powi(3));
         let v: [f64; 24] = [
             -0.40720, 0.17241, 0.01608, 0.01039, 0.00739, -0.00514, 0.00208, -0.00111, -0.00057,
             0.00056, -0.00042, 0.00042, 0.00038, -0.00024, -0.00007, 0.00004, 0.00004, 0.00003,
@@ -235,7 +235,7 @@ impl Astronomical {
                     .sin();
         }
         let extra = 0.000325
-            * (299.77 + 132.8475848 * c - 0.009173 * c.powi(2))
+            * (299.77 + (132.8475848 * c) - (0.009173 * c.powi(2)))
                 .to_radians()
                 .sin();
         let mut additional = 0.0;
@@ -253,7 +253,7 @@ impl Astronomical {
         let ms = Self::solar_anomaly(c);
         let ml = Self::lunar_anomaly(c);
         let f = Self::moon_node(c);
-        let e = 1.0 - 0.002516 * c - 0.0000074 * c.powi(2);
+        let e = 1.0 - (0.002516 * c) - (0.0000074 * c.powi(2));
         let v: [f64; 59] = [
             6288774.0, 1274027.0, 658314.0, 213618.0, -185116.0, -114332.0, 58793.0, 57066.0,
             53322.0, 45758.0, -40923.0, -34720.0, -30383.0, 15327.0, -12528.0, 10980.0, 10675.0,
@@ -286,7 +286,7 @@ impl Astronomical {
             0.0, -2.0, 2.0, 0.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0,
             -2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
         ];
-        let mut correction = 1.0f64.div_euclid(1000000.0);
+        let mut correction = 1.0 / 1000000.0;
         let mut correction_operand = 0.0;
         for i in 0..59 {
             correction_operand += v[i]
@@ -296,39 +296,39 @@ impl Astronomical {
                     .sin();
         }
         correction *= correction_operand;
-        let venus = 3958.0f64.div_euclid(1000000.0) * (119.75 + c * 131.849).to_radians().sin();
-        let jupiter = 318.0f64.div_euclid(1000000.0) * (53.09 + c * 479264.29).to_radians().sin();
-        let flat_earth = 1962.0f64.div_euclid(1000000.0) * (l - f).to_radians().sin();
+        let venus = 3958.0 / 1000000.0 * (119.75 + c * 131.849).to_radians().sin();
+        let jupiter = 318.0 / 1000000.0 * (53.09 + c * 479264.29).to_radians().sin();
+        let flat_earth = 1962.0 / 1000000.0 * (l - f).to_radians().sin();
         (l + correction + venus + jupiter + flat_earth + Self::nutation(moment)).rem_euclid(360.0)
     }
 
     fn mean_lunar_longitude(c: f64) -> f64 {
-        (218.3164477 + 481267.88123421 * c - 0.0015786 * c.powi(2) + 1.0f64.div_euclid(538841.0) * c.powi(3)
-            - 1.0f64.div_euclid(65194000.0) * c.powi(4))
+        (218.3164477 + 481267.88123421 * c - 0.0015786 * c.powi(2) + 1.0 / 538841.0 * c.powi(3)
+            - 1.0 / 65194000.0 * c.powi(4))
         .rem_euclid(360.0)
     }
 
     fn lunar_elongation(c: f64) -> f64 {
-        (297.85019021 + 445267.1114034 * c - 0.0018819 * c.powi(2) + 1.0f64.div_euclid(545868.0) * c.powi(3)
-            - 1.0f64.div_euclid(113065000.0) * c.powi(4))
+        (297.85019021 + 445267.1114034 * c - 0.0018819 * c.powi(2) + 1.0 / 545868.0 * c.powi(3)
+            - 1.0 / 113065000.0 * c.powi(4))
         .rem_euclid(360.0)
     }
 
     fn solar_anomaly(c: f64) -> f64 {
-        (357.5291092 + 35999.0502909 * c - 0.0001536 * c.powi(2) + 1.0f64.div_euclid(24490000.0) * c.powi(3))
+        (357.5291092 + 35999.0502909 * c - 0.0001536 * c.powi(2) + 1.0 / 24490000.0 * c.powi(3))
             .rem_euclid(360.0)
     }
 
     fn lunar_anomaly(c: f64) -> f64 {
         (134.9633964 + 477198.8675055 * c + 0.0087414 * c.powi(2)
-            - 1.0f64.div_euclid(69699.0) * c.powi(3)
-            - 1.0f64.div_euclid(14712000.0) * c.powi(4))
+            - 1.0 / 69699.0 * c.powi(3)
+            - 1.0 / 14712000.0 * c.powi(4))
         .rem_euclid(360.0)
     }
 
     fn moon_node(c: f64) -> f64 {
-        (93.2720950 + 483202.0175233 * c - 0.0036539 * c.powi(2) - 1.0f64.div_euclid(3526000.0) * c.powi(3)
-            + 1.0f64.div_euclid(863310000.0) * c.powi(4))
+        (93.2720950 + 483202.0175233 * c - 0.0036539 * c.powi(2) - 1.0 / 3526000.0 * c.powi(3)
+            + 1.0 / 863310000.0 * c.powi(4))
         .rem_euclid(360.0)
     }
 
@@ -343,9 +343,9 @@ impl Astronomical {
     /// of the sun and the moon.
     pub fn lunar_phase(moment: Moment) -> f64 {
         let t0 = Self::nth_new_moon(0);
-        let n = ((moment - t0).div_euclid(MEAN_SYNODIC_MONTH)).round() as i32;
+        let n = ((moment - t0).div_euclid(MEAN_SYNODIC_MONTH).round() as i32);
         let a = (Self::lunar_longitude(moment) - Self::solar_longitude(moment)).rem_euclid(360.0);
-        let b = 360.0 * (((moment - Self::nth_new_moon(n)).div_euclid(MEAN_SYNODIC_MONTH)).rem_euclid(1.0));
+        let b = 360.0 * (((moment - Self::nth_new_moon(n)) / MEAN_SYNODIC_MONTH).rem_euclid(1.0));
         if (a - b).abs() > 180.0 {
             b
         } else {
@@ -436,21 +436,21 @@ impl Astronomical {
     /// Find the time of the new moon preceding a given Moment
     /// (the last new moon before moment)
     pub fn new_moon_before(moment: Moment) -> Moment {
-        Self::nth_new_moon(Self::num_new_moon_at_or_after(moment) - 1)
+        Self::nth_new_moon(Self::num_of_new_moon_at_or_after(moment) - 1)
     }
 
     /// Find the time of the new moon following a given Moment
     /// (the first new moon after moment)
     pub fn new_moon_at_or_after(moment: Moment) -> Moment {
-        Self::nth_new_moon(Self::num_new_moon_at_or_after(moment))
+        Self::nth_new_moon(Self::num_of_new_moon_at_or_after(moment))
     }
 
     // Function to find the number of the new moon at or after a given moment;
     // helper function for new_moon_before and new_moon_at_or_after
-    fn num_new_moon_at_or_after(moment: Moment) -> i32 {
+    fn num_of_new_moon_at_or_after(moment: Moment) -> i32 {
         let t0: Moment = Self::nth_new_moon(0);
         let phi = Self::lunar_phase(moment);
-        let n = ((moment - t0).div_euclid(MEAN_SYNODIC_MONTH) - phi.div_euclid(360.0)).round() as i32;
+        let n = ((moment - t0) / MEAN_SYNODIC_MONTH - phi / 360.0).round() as i32;
         let mut result = n;
         let mut iters = 0;
         let max_iters = 245_000_000;
@@ -568,5 +568,36 @@ mod tests {
             assert!(expected_next_new_moon_moment > next_new_moon - 0.0035, "New moon calculation failed for the test case:\n\n\tMoment: {moment:?} with expected: {expected_next_new_moon_moment:?} and calculated: {next_new_moon:?}\n\n");
             assert!(expected_next_new_moon_moment < next_new_moon + 0.0035, "New moon calculation failed for the test case:\n\n\tMoment: {moment:?} with expected: {expected_next_new_moon_moment:?} and calculated: {next_new_moon:?}\n\n");
         }
+    }
+
+    #[test]
+    fn check_astronomy_0th_new_moon() {
+        // Checks the accuracy of the 0th new moon to be on January 11th
+        let zeroth_new_moon = Astronomical::nth_new_moon(0);
+        assert_eq!(zeroth_new_moon.inner() as i32, 11, "0th new moon check failed with nth_new_moon(0) = {zeroth_new_moon:?}");
+    }
+
+    #[test]
+    fn check_num_of_new_moon_0() {
+        // Tests the function num_of_new_moon_at_or_after() returns 0 for moment 0
+        assert_eq!(Astronomical::num_of_new_moon_at_or_after(Moment::new(0.0)), 0);
+    }
+
+    #[test]
+    fn check_new_moon_directionality() {
+        // Checks that new_moon_before is less than new_moon_at_or_after for a large number of Moments
+        let mut moment: Moment = Moment::new(-15500.0);
+        let max_moment = Moment::new(15501.0);
+        let mut iters: i32 = 0;
+        let max_iters = 10000;
+        while iters < max_iters && moment < max_moment {
+            let before = Astronomical::new_moon_before(moment);
+            let at_or_after = Astronomical::new_moon_at_or_after(moment);
+            assert!(before < at_or_after, "Directionality of fns new_moon_before and new_moon_at_or_after failed for Moment: {moment:?}");
+            iters += 1;
+            moment += 31.0;
+        }
+        assert!(iters > 500, "Testing failed: less than the expected number of testing iterations");
+        assert!(iters < max_iters, "Testing failed: more than the expected number of testing iterations");
     }
 }
