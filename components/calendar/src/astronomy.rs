7aac130e7fcb86724d4d1e7b2f5d62704f67e260
atcupps <at.cupps@gmail.com>
2023-06-20 14:32:45 -0700

    Addressing Clippy errors

--- components/calendar/src/astronomy.rs
+++ components/calendar/src/astronomy.rs
@@ -41,10 +41,10 @@ impl Location {
         longitude: f64,
         elevation: f64,
     ) -> Result<Location, LocationError> {
-        if latitude < -90.0 || latitude > 90.0 {
+        if !(-90.0..=90.0).contains(&latitude) {
             return Err(LocationError::LatitudeOutOfBounds(latitude));
         }
-        if longitude < -180.0 || longitude > 180.0 {
+        if !(-180.0..=180.0).contains(&longitude) {
             return Err(LocationError::LongitudeOutOfBounds(longitude));
         }
         Ok(Location {
@@ -84,13 +84,13 @@ impl Location {
     /// Convert from local mean time to universal time given a location
     #[allow(dead_code)] // TODO: Remove dead_code tag after use
     pub(crate) fn universal_from_local(local_time: Moment, location: Location) -> Moment {
-        local_time - Self::zone_from_longitude(location.longitude as f64)
+        local_time - Self::zone_from_longitude(location.longitude)
     }
 
     /// Convert from universal time to local time given a location
     #[allow(dead_code)] // TODO: Remove dead_code tag after use
     pub(crate) fn local_from_universal(universal_time: Moment, location: Location) -> Moment {
-        universal_time + Self::zone_from_longitude(location.longitude as f64)
+        universal_time + Self::zone_from_longitude(location.longitude)
     }
 }
 
@@ -109,6 +109,7 @@ impl Astronomical {
     pub(crate) fn ephemeris_correction(moment: Moment) -> f64 {
         let year = moment.inner().floor() / 365.2425;
         let year_int = (if year > 0.0 { year + 1.0 } else { year }) as i32;
+        #[allow(clippy::expect_used)] // Month and day are guaranteed to be valid, year is not checked in Iso Date constructor
         let gregorian: Date<Gregorian> = Date::try_new_gregorian_date(year_int, 7, 1)
             .expect("Date generation failed for {year} July 1");
         let iso: Date<Iso> = gregorian.to_iso();
@@ -121,27 +122,27 @@ impl Astronomical {
         let y0 = year_int as f64 / 100.0;
         let y1820 = ((year_int - 1820) as f64) / 100.0;
 
-        if 2051 <= year_int && year_int <= 2150 {
+        if (2051..=2150).contains(&year_int) {
             (8.0 * (year_int as f64).powi(2) + 8.0 * 1820f64.powi(2) - 30527.0 * (year_int as f64)
                 + 2975050.0)
                 / 216000000.0
-        } else if 2006 <= year_int && year_int <= 2050 {
+        } else if (2006..=2050).contains(&year_int) {
             1573.0 / 2160000.0
                 + 10739.0 * y2000 / 2880000000.0
                 + 207.0 * y2000.powi(2) / 3200000000.0
-        } else if 1987 <= year_int && year_int <= 2005 {
+        } else if (1987..=2005).contains(&year_int) {
             (1.0 / 86400.0)
                 * (63.86 + 0.3345 * y2000 - 0.060374 * y2000.powi(2)
                     + 0.0017275 * y2000.powi(3)
                     + 0.000651814 * y2000.powi(4)
                     + 0.00002373599 * y2000.powi(5))
-        } else if 1900 <= year_int && year_int <= 1986 {
+        } else if (1900..=1986).contains(&year_int) {
             -0.00002 + 0.000297 * c + 0.025184 * c.powi(2) - 0.181133 * c.powi(3)
                 + 0.553040 * c.powi(4)
                 - 0.861938 * c.powi(5)
                 + 0.677066 * c.powi(6)
                 - 0.212591 * c.powi(7)
-        } else if 1800 <= year_int && year_int <= 1899 {
+        } else if (1800..=1899).contains(&year_int) {
             -0.000009
                 + 0.003844 * c
                 + 0.083563 * c.powi(2)
@@ -153,21 +154,21 @@ impl Astronomical {
                 + 28.316289 * c.powi(8)
                 + 11.636204 * c.powi(9)
                 + 2.043794 * c.powi(10)
-        } else if 1700 <= year_int && year_int <= 1799 {
+        } else if (1700..=1799).contains(&year_int) {
             (1.0 / 86400.0)
                 * (8.118780842 - 0.005092142 * y1700 + 0.003336121 * y1700.powi(2)
                     - 0.0000266484 * y1700.powi(3))
-        } else if 1600 <= year_int && year_int <= 1699 {
+        } else if (1600..=1699).contains(&year_int) {
             (1.0 / 86400.0)
                 * (120.0 - 0.9808 * y1600 - 0.01532 * y1600.powi(2)
                     + 0.000140272128 * y1600.powi(3))
-        } else if 500 <= year_int && year_int <= 1599 {
+        } else if (500..=1599).contains(&year_int) {
             (1.0 / 86400.0)
                 * (1574.2 - 556.01 * y1000 + 71.23472 * y1000.powi(2) + 0.319781 * y1000.powi(3)
                     - 0.8503463 * y1000.powi(4)
                     - 0.005050998 * y1000.powi(5)
                     + 0.0083572073 * y1000.powi(6))
-        } else if -500 <= year_int && year_int <= 500 {
+        } else if (-500..=499).contains(&year_int) {
             (1.0 / 86400.0)
                 * (10583.6 - 1014.41 * y0 + 33.78311 * y0.powi(2)
                     - 5.952053 * y0.powi(3)
