7f10b155cffcd1bf696a0c71bf128b92883357ae
Robert Bastian <robertbastian@users.noreply.github.com>
2023-06-22 13:35:59 +0200

    Batteries-included baked provider (#3549)

--- provider/testdata/data/baked/macros/propnames_to_long_linear_gcb_v1.data.rs
+++ provider/testdata/data/baked/macros/propnames_to_long_linear_gcb_v1.data.rs
@@ -14,16 +14,10 @@ macro_rules! __impl_propnames_to_long_linear_gcb_v1 {
         #[clippy::msrv = "1.61"]
         impl icu_provider::DataProvider<icu_properties::provider::GraphemeClusterBreakValueToLongNameV1Marker> for $provider {
             fn load(&self, req: icu_provider::DataRequest) -> Result<icu_provider::DataResponse<icu_properties::provider::GraphemeClusterBreakValueToLongNameV1Marker>, icu_provider::DataError> {
-                let locale = req.locale;
-                match {
-                    if locale.is_empty() {
-                        Ok(Self::SINGLETON_PROPNAMES_TO_LONG_LINEAR_GCB_V1)
-                    } else {
-                        Err(icu_provider::DataErrorKind::ExtraneousLocale)
-                    }
-                } {
-                    Ok(payload) => Ok(icu_provider::DataResponse { metadata: Default::default(), payload: Some(icu_provider::DataPayload::from_static_ref(payload)) }),
-                    Err(e) => Err(e.with_req(<icu_properties::provider::GraphemeClusterBreakValueToLongNameV1Marker as icu_provider::KeyedDataMarker>::KEY, req)),
+                if req.locale.is_empty() {
+                    Ok(icu_provider::DataResponse { payload: Some(icu_provider::DataPayload::from_static_ref(Self::SINGLETON_PROPNAMES_TO_LONG_LINEAR_GCB_V1)), metadata: Default::default() })
+                } else {
+                    Err(icu_provider::DataErrorKind::ExtraneousLocale.with_req(<icu_properties::provider::GraphemeClusterBreakValueToLongNameV1Marker as icu_provider::KeyedDataMarker>::KEY, req))
                 }
             }
         }
