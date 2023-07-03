5445f25029e2b979a024ec1e0df97e7207950174
Manish Goregaokar <manishsmail@gmail.com>
2023-07-03 18:22:21 +0200

    Casemapping FFI

---
+++ ffi/diplomat/src/casemap.rs
@@ -0,0 +1,128 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+#[diplomat::bridge]
+pub mod ffi {
+    use crate::{
+        errors::ffi::ICU4XError, locale::ffi::ICU4XLocale, provider::ffi::ICU4XDataProvider,
+    };
+    use alloc::boxed::Box;
+    use diplomat_runtime::DiplomatWriteable;
+    use icu_casemap::CaseMapper;
+    use writeable::Writeable;
+
+    #[diplomat::opaque]
+    #[diplomat::rust_link(icu::casemap::CaseMapper, Struct)]
+    pub struct ICU4XCaseMapper(pub CaseMapper);
+
+    impl ICU4XCaseMapper {
+        /// Construct a new ICU4XCaseMapper instance for NFC
+        #[diplomat::rust_link(icu::casemap::ComposingNormalizer::try_new_nfc_unstable, FnInStruct)]
+        pub fn create(provider: &ICU4XDataProvider) -> Result<Box<ICU4XCaseMapper>, ICU4XError> {
+            Ok(Box::new(ICU4XCaseMapper(CaseMapper::try_new_unstable(
+                &provider.0,
+            )?)))
+        }
+
+        /// Returns the full lowercase mapping of the given string
+        #[diplomat::rust_link(icu::casemap::ComposingNormalizer::lowercase, FnInStruct)]
+        #[diplomat::rust_link(
+            icu::casemap::ComposingNormalizer::lowercase_to_string,
+            FnInStruct,
+            hidden
+        )]
+        pub fn lowercase(
+            &self,
+            s: &str,
+            locale: &ICU4XLocale,
+            write: &mut DiplomatWriteable,
+        ) -> Result<(), ICU4XError> {
+            // #2520
+            // In the future we should be able to make assumptions based on backend
+            core::str::from_utf8(s.as_bytes())
+                .map_err(|e| ICU4XError::DataIoError.log_original(&e))?;
+            self.0.lowercase(s, &locale.0.id).write_to(write)?;
+
+            Ok(())
+        }
+
+        /// Returns the full uppercase mapping of the given string
+        #[diplomat::rust_link(icu::casemap::ComposingNormalizer::uppercase, FnInStruct)]
+        #[diplomat::rust_link(
+            icu::casemap::ComposingNormalizer::uppercase_to_string,
+            FnInStruct,
+            hidden
+        )]
+        pub fn uppercase(
+            &self,
+            s: &str,
+            locale: &ICU4XLocale,
+            write: &mut DiplomatWriteable,
+        ) -> Result<(), ICU4XError> {
+            // #2520
+            // In the future we should be able to make assumptions based on backend
+            core::str::from_utf8(s.as_bytes())
+                .map_err(|e| ICU4XError::DataIoError.log_original(&e))?;
+            self.0.uppercase(s, &locale.0.id).write_to(write)?;
+
+            Ok(())
+        }
+
+        /// Returns the full titlecase mapping of the given string
+        #[diplomat::rust_link(icu::casemap::ComposingNormalizer::titlecase_segment, FnInStruct)]
+        #[diplomat::rust_link(
+            icu::casemap::ComposingNormalizer::titlecase_segment_to_string,
+            FnInStruct,
+            hidden
+        )]
+        pub fn titlecase_segment(
+            &self,
+            s: &str,
+            locale: &ICU4XLocale,
+            write: &mut DiplomatWriteable,
+        ) -> Result<(), ICU4XError> {
+            // #2520
+            // In the future we should be able to make assumptions based on backend
+            core::str::from_utf8(s.as_bytes())
+                .map_err(|e| ICU4XError::DataIoError.log_original(&e))?;
+            self.0.titlecase_segment(s, &locale.0.id).write_to(write)?;
+
+            Ok(())
+        }
+
+        /// Case-folds the characters in the given string
+        #[diplomat::rust_link(icu::casemap::ComposingNormalizer::fold, FnInStruct)]
+        #[diplomat::rust_link(icu::casemap::ComposingNormalizer::fold_string, FnInStruct, hidden)]
+        pub fn fold(&self, s: &str, write: &mut DiplomatWriteable) -> Result<(), ICU4XError> {
+            // #2520
+            // In the future we should be able to make assumptions based on backend
+            core::str::from_utf8(s.as_bytes())
+                .map_err(|e| ICU4XError::DataIoError.log_original(&e))?;
+            self.0.fold(s).write_to(write)?;
+
+            Ok(())
+        }
+        /// Case-folds the characters in the given string
+        /// using Turkic (T) mappings for dotted/dotless I.
+        #[diplomat::rust_link(icu::casemap::ComposingNormalizer::fold_turkic, FnInStruct)]
+        #[diplomat::rust_link(
+            icu::casemap::ComposingNormalizer::fold_turkic_string,
+            FnInStruct,
+            hidden
+        )]
+        pub fn fold_turkic(
+            &self,
+            s: &str,
+            write: &mut DiplomatWriteable,
+        ) -> Result<(), ICU4XError> {
+            // #2520
+            // In the future we should be able to make assumptions based on backend
+            core::str::from_utf8(s.as_bytes())
+                .map_err(|e| ICU4XError::DataIoError.log_original(&e))?;
+            self.0.fold_turkic(s).write_to(write)?;
+
+            Ok(())
+        }
+    }
+}
