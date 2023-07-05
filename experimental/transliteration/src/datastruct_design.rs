82d6598c2f8c4adaf601fce3714945456c91d126
Niels Saurer <nielssaurer@google.com>
2023-07-05 13:38:18 +0000

    compiles

--- experimental/transliteration/src/datastruct_design.rs
+++ experimental/transliteration/src/datastruct_design.rs
@@ -2,6 +2,7 @@
 
 use std::borrow::Cow;
 
+use serde;
 use icu_collections::codepointinvliststringlist::{CodePointInversionListAndStringList, CodePointInversionListAndStringListULE};
 use zerovec::*;
 
@@ -40,11 +41,14 @@ struct Transliterator<'a> {
     // only characters in this set are affected by the transliterator. None is equivalent to the full set
     // because of ULE things, removing the Option<> and adopting full = None semantics might be easier. 
     // filter?
+    #[serde(borrow)]
     filter: UnicodeSet<'a>,
     // required for the case where the first RuleSet appears before the first recursive ID
     // conversion_rule_list
-    prefix_rule_set: RuleSet<'a>,
+    #[serde(borrow)]
+    prefix_rule_set: VarZeroVec<'a, RuleULE>,
     // (recursive_simple_id conversion_rule_list)*
+    #[serde(borrow)]
     groups: VarZeroVec<'a, TransliterationGroupULE>,
 }
 
@@ -53,22 +57,24 @@ struct Transliterator<'a> {
 #[make_varule(TransliterationGroupULE)]
 #[zerovec::skip_derive(Ord)]
 #[zerovec::derive(Serialize, Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 struct TransliterationGroup<'a> {
     // :: Any-Any ;
     // exactly one
     #[zerovec::varule(SimpleIDULE)]
+    #[serde(borrow)]
     inner: SimpleID<'a>,
     // x > b ; a > b ; ...
     // zero or more
-    #[zerovec::varule(RuleSetULE)]
-    rules: RuleSet<'a>,
+    #[serde(borrow)]
+    rules: VarZeroVec<'a, RuleULE>,
 }
 
 // replaced directly by the inner vec
-#[make_varule(RuleSetULE)]
-#[zerovec::skip_derive(Ord)]
-#[zerovec::derive(Serialize, Deserialize)]
-struct RuleSet<'a>(VarZeroVec<'a, RuleULE>);
+// #[make_varule(RuleSetULE)]
+// #[zerovec::skip_derive(Ord)]
+// #[zerovec::derive(Serialize, Deserialize)]
+// struct RuleSet<'a>(VarZeroVec<'a, RuleULE>);
 
 // e.g.: [a-z] Any-Remove/BGN;
 // but actually, in BCP47: [a-z] und
@@ -76,35 +82,34 @@ struct RuleSet<'a>(VarZeroVec<'a, RuleULE>);
 #[make_varule(SimpleIDULE)]
 #[zerovec::skip_derive(Ord)]
 #[zerovec::derive(Serialize, Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 struct SimpleID<'a> {
     #[zerovec::varule(CodePointInversionListAndStringListULE)]
+    #[serde(borrow)]
     filter: UnicodeSet<'a>,
     // TODO: Discuss if this should be in auxiliary
+    #[serde(borrow)]
     translit: Cow<'a, str>,
 }
 
 #[make_varule(RuleULE)]
 #[zerovec::skip_derive(Ord)]
 #[zerovec::derive(Serialize, Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 struct Rule<'a> {
     // special case matchers (such as segments, quantifiers, unicodesets, variables...) are represented as chars in the PUA
+    #[serde(borrow)]
     ante: Cow<'a, str>,
+    #[serde(borrow)]
     key: Cow<'a, str>,
+    #[serde(borrow)]
     post: Cow<'a, str>,
 
     // special case replacers (such as variables, backrefs, function calls)
+    #[serde(borrow)]
     replacer: Cow<'a, str>,
 }
 
-
-
-// struct MatcherTable<'a> {
-    
-// }
-
-// struct ReplacerTable<'a> {
-    
-// }
 
 #[derive(serde::Serialize, serde::Deserialize)]
 struct VarTable<'a> {
@@ -114,21 +119,26 @@ struct VarTable<'a> {
     //
     // is: $a => PUA_offset + 0, $b => PUA_offset + 1
     // and compounds: ["hello", "<PUA_offset + 0> world"]
+    #[serde(borrow)]
     compounds: VarZeroVec<'a, str>,
 
     // having quantifiers_opt: ["<PUA_offset + 5> world"] is the representation of
     // "(?:<some special matcher that has ID 5> ' ' world)?" in the rules  (if ?: was valid non-capturing syntax)
     //
     //
+    #[serde(borrow)]
     quantifiers_opt: VarZeroVec<'a, str>, 
     // zero or more
+    #[serde(borrow)]
     quantifiers_kleene: VarZeroVec<'a, str>, 
     // one or more
+    #[serde(borrow)]
     quantifiers_kleene_plus: VarZeroVec<'a, str>, 
 
+    #[serde(borrow)]
     segments: VarZeroVec<'a, str>,
 
-
+    #[serde(borrow)]
     function_calls: VarZeroVec<'a, FunctionCallULE>,
 }
 
@@ -136,8 +146,11 @@ struct VarTable<'a> {
 #[make_varule(FunctionCallULE)]
 #[zerovec::skip_derive(Ord)]
 #[zerovec::derive(Serialize, Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 struct FunctionCall<'a> {
     #[zerovec::varule(SimpleIDULE)]
+    #[serde(borrow)]
     translit: SimpleID<'a>,
+    #[serde(borrow)]
     arg: Cow<'a, str>,
 }
