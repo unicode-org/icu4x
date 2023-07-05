350b598c42da6859fe1e6e4aded63942e0df9d33
Robert Bastian <robertbastian@users.noreply.github.com>
2023-07-05 14:25:18 +0200

    Baked data for FFI examples (#3630)

--- ffi/diplomat/js/examples/node/index.js
+++ ffi/diplomat/js/examples/node/index.js
@@ -1,12 +1,1 @@
 export * from './lib/index.js'
-
-export async function fullData() {
-  const path = new URL('./data.postcard', import.meta.url);
-  if (typeof fetch === 'undefined') { // Node
-    const fs = await import("fs");
-    return fs.readFileSync(path);
-  } else { // Browser
-    const response = await fetch(path);
-    return await response.arrayBuffer();
-  }
-}
