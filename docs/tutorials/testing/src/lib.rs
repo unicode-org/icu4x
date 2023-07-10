be4c45fe6487dd935ae4a65e3cdd579a11de85b0
Robert Bastian <robertbastian@users.noreply.github.com>
2023-07-10 17:59:29 +0200

    Test all tutorials against crates.io (#3659)

---
+++ docs/tutorials/testing/src/lib.rs
@@ -0,0 +1,22 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+#[doc = include_str!("../../intro.md")]
+mod intro {}
+#[doc = include_str!("../../data_provider.md")]
+mod data_provider {}
+#[doc = include_str!("../../writing_a_new_data_struct.md")]
+mod writing_a_new_data_struct {}
+#[doc = include_str!("../../cpp.md")]
+mod cpp {}
+#[doc = include_str!("../../data_management.md")]
+mod data_management {}
+#[doc = include_str!("../../js.md")]
+mod js {}
+#[doc = include_str!("../../markdown_tips.md")]
+mod markdown_tips {}
+#[doc = include_str!("../../../../README.md")]
+mod readme {}
+#[doc = include_str!("../../../design/data_safety.md")]
+mod data_safety {}
