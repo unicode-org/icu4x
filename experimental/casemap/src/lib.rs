21d02f42c42664a9f0059240d15af0ab69fbd977
Manish Goregaokar <manishsmail@gmail.com>
2023-07-17 08:28:56 -0700

    features

--- experimental/casemap/src/lib.rs
+++ experimental/casemap/src/lib.rs
@@ -29,7 +29,7 @@
 //! [`ICU4X`]: ../icu/index.html
 
 // https://github.com/unicode-org/icu4x/blob/main/docs/process/boilerplate.md#library-annotations
-#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(any(test, feature = "std")), no_std)]
 #![cfg_attr(
     not(test),
     deny(
