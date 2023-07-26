656848a9aadf58a02534408d2edf7c8c944d9ee9
Robert Bastian <robertbastian@google.com>
2023-07-26 15:08:37 +0200

    doc

--- provider/core/src/buf.rs
+++ provider/core/src/buf.rs
@@ -21,9 +21,10 @@ impl DataMarker for BufferMarker {
 /// A data provider that returns opaque bytes.
 ///
 /// Generally, these bytes are expected to be deserializable with Serde. To get an object
-/// implementing [`DataProvider`] via Serde, use [`as_deserializing()`], which requires
-/// enabling at least one of the deserialization Cargo features:
+/// implementing [`DataProvider`] via Serde, use [`as_deserializing()`].
 ///
+/// Passing a  `BufferProvider` to a `*_with_buffer_provider` constructor requires enabling
+/// the deserialization Cargo feature for the expected format(s):
 /// - `deserialize_json`
 /// - `deserialize_postcard_1`
 /// - `deserialize_bincode_1`
