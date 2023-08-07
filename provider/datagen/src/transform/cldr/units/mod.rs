27ccef4342a56aa4228462c369293efaef0228e4
Younies Mahmoud <younies.mahmoud@gmail.com>
2023-08-07 16:00:54 +0200

    fix clippy

--- provider/datagen/src/transform/cldr/units/mod.rs
+++ provider/datagen/src/transform/cldr/units/mod.rs
@@ -2,33 +2,33 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use std::f32::consts::E;
 
-use icu_collections::codepointtrie::CodePointTrieError;
+
+
 use icu_provider::{DataError, DataProvider, DataRequest, DataResponse};
 use icu_unitsconversion::provider::UnitsConstantsV1Maker;
 use crate::transform::cldr::cldr_serde;
 
-use icu_locid::LanguageIdentifier;
-use icu_properties::sets::load_for_general_category_group;
-use icu_properties::GeneralCategoryGroup;
-use tinystr::UnvalidatedTinyAsciiStr;
-use zerovec::maps::MutableZeroVecLike;
-use zerovec::VarZeroVec;
-use zerovec::ZeroMap;
 
-use crate::DatagenProvider;
-use icu_provider::datagen::IterableDataProvider;
-use icu_provider::prelude::*;
-use std::collections::HashMap;
-use tinystr::tinystr;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 impl DataProvider<UnitsConstantsV1Maker> for crate::DatagenProvider {
-    fn load(&self, req: DataRequest) -> Result<DataResponse<UnitsConstantsV1Maker>, DataError> {
+    fn load(&self, _req: DataRequest) -> Result<DataResponse<UnitsConstantsV1Maker>, DataError> {
         // self.check_req::<UnitsConstantsV1Maker>(req)?;
 
-        let units_data: &cldr_serde::units::units::Resource = self
+        let _units_data: &cldr_serde::units::units::Resource = self
             .source
             .cldr()?
             .core()
