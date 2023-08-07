b909055293ce902fa8d087dc4cd72667453468cf
Manish Goregaokar <manishsmail@gmail.com>
2023-08-07 11:57:27 -0700

    review fixes

--- provider/datagen/src/registry.rs
+++ provider/datagen/src/registry.rs
@@ -139,6 +139,7 @@ registry!(
     icu_calendar::provider::JapaneseErasV1Marker = "calendar/japanese@1",
     icu_calendar::provider::JapaneseExtendedErasV1Marker = "calendar/japanext@1",
     icu_calendar::provider::WeekDataV1Marker = "datetime/week_data@1",
+    #[cfg(any(all(), feature = "icu_casemap"))]
     icu_casemap::provider::CaseMapV1Marker = "props/casemap@1",
     icu_casemap::provider::CaseMapUnfoldV1Marker = "props/casemap_unfold@1",
     #[cfg(any(all(), feature = "icu_collator"))]
