c98f91b0819a9d8b6758fa42182103aff14c5706
Younies Mahmoud <younies.mahmoud@gmail.com>
2023-08-10 16:08:01 +0200

    Implementing the data provider for currency essentials (#2894)
    Implementing the data provider for currency essentials

--- provider/datagen/src/registry.rs
+++ provider/datagen/src/registry.rs
@@ -418,6 +418,8 @@ registry!(
         "relativetime/short/year@1",
     icu_relativetime::provider::NarrowYearRelativeTimeFormatDataV1Marker =
         "relativetime/narrow/year@1",
+    #[cfg(feature = "icu_singlenumberformatter")]
+    icu_singlenumberformatter::provider::CurrencyEssentialsV1Marker = "currency/essentials@1",
     #[cfg(any(all(), feature = "icu_segmenter"))]
     icu_segmenter::provider::DictionaryForWordLineExtendedV1Marker =
         "segmenter/dictionary/wl_ext@1",
