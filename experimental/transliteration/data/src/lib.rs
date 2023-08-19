c237a24a12dc75dc22e96cd3fd57d4001ad70e95
Niels Saurer <nielssaurer@google.com>
2023-08-19 22:05:23 +0200

    make compileddata compile

--- experimental/transliteration/data/src/lib.rs
+++ experimental/transliteration/data/src/lib.rs
@@ -10,3 +10,10 @@
 include!(concat!(core::env!("ICU4X_DATA_DIR"), "/macros.rs"));
 #[cfg(not(icu4x_custom_data))]
 include!("../data/macros.rs");
+
+#[macro_export]
+macro_rules! impl_transliteration_transliterator_rules_v1 {
+    ($name:ident) => {
+        // noop
+    };
+}
