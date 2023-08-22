144d0c28d93ef5f0fd7de2e7ecbeb30897e60e1e
atcupps <91998663+atcupps@users.noreply.github.com>
2023-08-22 13:47:32 -0400

    Add license info to `chinese_data.rs`

--- components/calendar/src/chinese_data.rs
+++ components/calendar/src/chinese_data.rs
@@ -1,3 +1,7 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 //! Compiled and compressed data for Chinese calendar years; includes `RataDie` types for the beginning of years
 
 /// The minimum year present in CHINESE_DATA_ARRAY
