07be76d619c0a3356a2d445e49496f9508699205
Niels Saurer <nielssaurer@google.com>
2023-08-29 18:04:14 +0000

    fix license header

--- experimental/transliteration/src/transliterator/mod.rs
+++ experimental/transliteration/src/transliterator/mod.rs
@@ -1,6 +1,7 @@
 // This file is part of ICU4X. For terms of use, please see the file
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 #[allow(clippy::indexing_slicing, clippy::unwrap_used)] // TODO(#3958): Remove.
 mod replaceable;
 
