ce5f96c0bbd77e8e2a62d2ab727294100f1ac61a
Manish Goregaokar <manishsmail@gmail.com>
2023-08-29 17:40:32 -0700

    Add in Moment

--- utils/calendrical_calculations/src/lib.rs
+++ utils/calendrical_calculations/src/lib.rs
@@ -1,2 +1,19 @@
+// https://github.com/unicode-org/icu4x/blob/main/docs/process/boilerplate.md#library-annotations
+#![cfg_attr(not(any(test, feature = "std")), no_std)]
+#![cfg_attr(
+    not(test),
+    deny(
+        clippy::indexing_slicing,
+        clippy::unwrap_used,
+        clippy::expect_used,
+        clippy::panic,
+        clippy::exhaustive_structs,
+        clippy::exhaustive_enums,
+        missing_debug_implementations,
+    )
+)]
+
+// TODO
+// #![warn(missing_docs)]
 
 pub mod rata_die;
