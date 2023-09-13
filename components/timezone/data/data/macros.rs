50667eb8bcefe7469ec65fb23eeab735c10075f8
Shane F. Carr <shane@unicode.org>
2023-09-12 23:30:26 -0700

    Add the two new keys to bakeddata

--- components/timezone/data/data/macros.rs
+++ components/timezone/data/data/macros.rs
@@ -1,5 +1,15 @@
 // @generated
 #[macro_use]
+#[path = "macros/time_zone_bcp47_to_iana_v1.data.rs"]
+mod time_zone_bcp47_to_iana_v1;
+#[doc(inline)]
+pub use __impl_time_zone_bcp47_to_iana_v1 as impl_time_zone_bcp47_to_iana_v1;
+#[macro_use]
+#[path = "macros/time_zone_iana_to_bcp47_v1.data.rs"]
+mod time_zone_iana_to_bcp47_v1;
+#[doc(inline)]
+pub use __impl_time_zone_iana_to_bcp47_v1 as impl_time_zone_iana_to_bcp47_v1;
+#[macro_use]
 #[path = "macros/time_zone_metazone_period_v1.data.rs"]
 mod time_zone_metazone_period_v1;
 #[doc(inline)]
