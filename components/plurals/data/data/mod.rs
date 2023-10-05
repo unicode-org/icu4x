d2669e1bdec396939c774a54cc6da6ace2b79902
José Julián Espina <jedel0124@gmail.com>
2023-10-05 14:40:16 +0000

    Implement data struct `PluralRangesV1` (#4077)

--- components/plurals/data/data/mod.rs
+++ components/plurals/data/data/mod.rs
@@ -5,6 +5,7 @@ macro_rules! impl_data_provider {
         make_provider!($provider);
         impl_plurals_cardinal_v1!($provider);
         impl_plurals_ordinal_v1!($provider);
+        impl_plurals_ranges_v1!($provider);
     };
 }
 #[allow(unused_macros)]
@@ -16,6 +17,7 @@ macro_rules! impl_any_provider {
                 match key.hashed() {
                     h if h == <icu::plurals::provider::CardinalV1Marker as icu_provider::KeyedDataMarker>::KEY.hashed() => icu_provider::DataProvider::<icu::plurals::provider::CardinalV1Marker>::load(self, req).map(icu_provider::DataResponse::wrap_into_any_response),
                     h if h == <icu::plurals::provider::OrdinalV1Marker as icu_provider::KeyedDataMarker>::KEY.hashed() => icu_provider::DataProvider::<icu::plurals::provider::OrdinalV1Marker>::load(self, req).map(icu_provider::DataResponse::wrap_into_any_response),
+                    h if h == <icu::plurals::provider::PluralRangesV1Marker as icu_provider::KeyedDataMarker>::KEY.hashed() => icu_provider::DataProvider::<icu::plurals::provider::PluralRangesV1Marker>::load(self, req).map(icu_provider::DataResponse::wrap_into_any_response),
                     _ => Err(icu_provider::DataErrorKind::MissingDataKey.with_req(key, req)),
                 }
             }
