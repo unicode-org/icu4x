28f566ce4c389037d7218ea4be563199d9183802
Shane F. Carr <shane@unicode.org>
2023-10-09 17:07:53 -0700

    Fix tinywasm build with llvm-15 and newer Node.js

--- ffi/capi/js/examples/tinywasm/ld.py
+++ ffi/capi/js/examples/tinywasm/ld.py
@@ -35,7 +35,7 @@ def main():
         else:
             new_argv += [arg]
             is_export = False
-    result = subprocess.run(["lld-14"] + new_argv, stdout=sys.stdout, stderr=sys.stderr)
+    result = subprocess.run(["lld-15"] + new_argv, stdout=sys.stdout, stderr=sys.stderr)
     return result.returncode
 
 if __name__ == "__main__":
--- ffi/capi/js/examples/tinywasm/tiny.mjs
+++ ffi/capi/js/examples/tinywasm/tiny.mjs
@@ -2,6 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+import "./delete_fetch.mjs";
 import { ICU4XLocale } from "./lib/ICU4XLocale.js";
 import { ICU4XDataProvider } from "./lib/ICU4XDataProvider.js";
 import { ICU4XFixedDecimalFormatter } from "./lib/ICU4XFixedDecimalFormatter.js";
