08642a439247baf2b5eac20721047d28e18b113c
Younies Mahmoud <younies.mahmoud@gmail.com>
2023-10-17 16:06:25 +0200

    add license

--- provider/datagen/src/transform/cldr/units/constants.rs
+++ provider/datagen/src/transform/cldr/units/constants.rs
@@ -1,3 +1,7 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
 use super::helpers::{convert_slices_to_fraction, transform_fraction_to_constant_value};
 use crate::transform::cldr::cldr_serde::{self};
 use icu_provider::{
