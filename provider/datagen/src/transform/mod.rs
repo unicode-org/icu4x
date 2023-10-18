6a3a653a339ec70a64d7dacb2bf63ab432ecdf80
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2023-10-18 11:50:19 +0200

    cfg

--- provider/datagen/src/transform/mod.rs
+++ provider/datagen/src/transform/mod.rs
@@ -31,7 +31,7 @@ impl DatagenProvider {
     {
         if <M as KeyedDataMarker>::KEY.metadata().singleton && !req.locale.is_empty() {
             Err(DataErrorKind::ExtraneousLocale)
-        } else if !self.supported_locales()?.contains(req.locale) {
+        } else if cfg!(debug_assertions) && !self.supported_locales()?.contains(req.locale) {
             Err(DataErrorKind::MissingLocale)
         } else {
             Ok(())
