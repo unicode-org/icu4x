a88ee846a6105eb186d98774dfe1a2c2e7b4fb50
Manish Goregaokar <manishsmail@gmail.com>
2023-10-20 17:17:29 -0700

    Cache the era code maps (#4199)

--- provider/datagen/src/transform/cldr/datetime/symbols.rs
+++ provider/datagen/src/transform/cldr/datetime/symbols.rs
@@ -28,13 +28,13 @@ fn convert_eras(eras: &cldr_serde::ca::Eras, calendar: &str) -> Eras<'static> {
     let mut out_eras = Eras::default();
 
     for (cldr, code) in map {
-        if let Some(name) = eras.names.get(&cldr) {
+        if let Some(name) = eras.names.get(cldr) {
             out_eras.names.insert(code.as_str().into(), name);
         }
-        if let Some(abbr) = eras.abbr.get(&cldr) {
+        if let Some(abbr) = eras.abbr.get(cldr) {
             out_eras.abbr.insert(code.as_str().into(), abbr);
         }
-        if let Some(narrow) = eras.narrow.get(&cldr) {
+        if let Some(narrow) = eras.narrow.get(cldr) {
             out_eras.narrow.insert(code.as_str().into(), narrow);
         }
     }
@@ -86,64 +86,43 @@ fn get_month_code_map(calendar: &str) -> &'static [TinyStr4] {
     }
 }
 
-fn get_era_code_map(calendar: &str) -> BTreeMap<String, TinyStr16> {
-    match calendar {
-        "gregory" => [
-            ("0".to_string(), tinystr!(16, "bce")),
-            ("1".to_string(), tinystr!(16, "ce")),
-        ]
-        .into_iter()
-        .collect(),
-        "buddhist" => [("0".to_string(), tinystr!(16, "be"))]
-            .into_iter()
-            .collect(),
-        "chinese" => [].into_iter().collect(),
-        "japanese" | "japanext" => crate::transform::cldr::calendar::japanese::get_era_code_map(),
-        "coptic" => [
+fn get_era_code_map(calendar: &str) -> impl Iterator<Item = (&str, TinyStr16)> {
+    use either::Either;
+
+    let array: &[_] = match calendar {
+        "gregory" => &[("0", tinystr!(16, "bce")), ("1", tinystr!(16, "ce"))],
+        "buddhist" => &[("0", tinystr!(16, "be"))],
+        "japanese" | "japanext" => {
+            return Either::Right(
+                crate::transform::cldr::calendar::japanese::get_era_code_map()
+                    .iter()
+                    .map(|(k, v)| (&**k, *v)),
+            )
+        }
+        "coptic" => &[
             // Before Diocletian
-            ("0".to_string(), tinystr!(16, "bd")),
+            ("0", tinystr!(16, "bd")),
             // Anno Diocletian/After Diocletian
-            ("1".to_string(), tinystr!(16, "ad")),
-        ]
-        .into_iter()
-        .collect(),
-        "dangi" => [].into_iter().collect(),
-        "indian" => [("0".to_string(), tinystr!(16, "saka"))]
-            .into_iter()
-            .collect(),
-        "islamic" => [("0".to_string(), tinystr!(16, "islamic"))]
-            .into_iter()
-            .collect(),
-        "islamicc" => [("0".to_string(), tinystr!(16, "islamic"))]
-            .into_iter()
-            .collect(),
-        "umalqura" => [("0".to_string(), tinystr!(16, "islamic"))]
-            .into_iter()
-            .collect(),
-        "tbla" => [("0".to_string(), tinystr!(16, "islamic"))]
-            .into_iter()
-            .collect(),
-        "persian" => [("0".to_string(), tinystr!(16, "ah"))]
-            .into_iter()
-            .collect(),
-        "hebrew" => [("0".to_string(), tinystr!(16, "hebrew"))]
-            .into_iter()
-            .collect(),
-        "ethiopic" => [
-            ("0".to_string(), tinystr!(16, "incar")),
-            ("1".to_string(), tinystr!(16, "pre-incar")),
-            ("2".to_string(), tinystr!(16, "mundi")),
-        ]
-        .into_iter()
-        .collect(),
-        "roc" => [
-            ("0".to_string(), tinystr!(16, "roc-inverse")),
-            ("1".to_string(), tinystr!(16, "roc")),
-        ]
-        .into_iter()
-        .collect(),
+            ("1", tinystr!(16, "ad")),
+        ],
+        "dangi" | "chinese" => &[],
+        "indian" => &[("0", tinystr!(16, "saka"))],
+        "islamic" | "islamicc" | "umalqura" | "tbla" => &[("0", tinystr!(16, "islamic"))],
+        "persian" => &[("0", tinystr!(16, "ah"))],
+        "hebrew" => &[("0", tinystr!(16, "hebrew"))],
+        "ethiopic" => &[
+            ("0", tinystr!(16, "incar")),
+            ("1", tinystr!(16, "pre-incar")),
+            ("2", tinystr!(16, "mundi")),
+        ],
+        "roc" => &[
+            ("0", tinystr!(16, "roc-inverse")),
+            ("1", tinystr!(16, "roc")),
+        ],
         _ => panic!("Era map unknown for {calendar}"),
-    }
+    };
+
+    Either::Left(array.iter().copied())
 }
 
 macro_rules! symbols_from {
