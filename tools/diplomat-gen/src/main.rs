41ba05ae5b2f6b0d2a40912beefacd7bb5942ea3
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2023-10-21 19:51:16 +0200

    Versioning `diplomat-tool` through Cargo (#4197)

---
+++ tools/diplomat-gen/src/main.rs
@@ -0,0 +1,38 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+use std::path::Path;
+
+fn main() -> std::io::Result<()> {
+    let capi = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/../../ffi/capi"));
+
+    let Some(lang) = std::env::args().nth(1) else {
+        panic!("Missing argument <language>");
+    };
+
+    diplomat_tool::gen(
+        &capi.join("src/lib.rs"),
+        &lang,
+        &{
+            let include = capi.join(&lang).join("include");
+            std::fs::remove_dir_all(&include)?;
+            std::fs::create_dir(&include)?;
+            include
+        },
+        if lang == "cpp" || lang == "js" {
+            let docs = capi.join(&lang).join("docs/source");
+            let conf = std::fs::read_to_string(docs.join("conf.py"))?;
+            std::fs::remove_dir_all(&docs)?;
+            std::fs::create_dir(&docs)?;
+            std::fs::write(docs.join("conf.py"), conf)?;
+            Some(docs)
+        } else {
+            None
+        }
+        .as_deref(),
+        &Default::default(),
+        None,
+        false,
+    )
+}
