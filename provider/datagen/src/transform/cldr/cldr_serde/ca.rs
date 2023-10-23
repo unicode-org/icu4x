e6165da472a9bf8e3ea41984b5c83ce950f09c1e
Manish Goregaokar <manishsmail@gmail.com>
2023-10-23 16:48:11 -0700

    Add monthPatterns with numeric key

--- provider/datagen/src/transform/cldr/cldr_serde/ca.rs
+++ provider/datagen/src/transform/cldr/cldr_serde/ca.rs
@@ -30,10 +30,17 @@ pub struct StandAloneWidths<Symbols> {
 }
 
 #[derive(Debug, PartialEq, Clone, Deserialize)]
+pub struct Numeric<Symbols> {
+    pub all: Symbols,
+}
+
+#[derive(Debug, PartialEq, Clone, Deserialize)]
 pub struct Contexts<Symbols> {
     pub format: FormatWidths<Symbols>,
     #[serde(rename = "stand-alone")]
     pub stand_alone: Option<StandAloneWidths<Symbols>>,
+    // currently only found on monthPatterns
+    pub numeric: Option<Numeric<Symbols>>,
 }
 
 /// A length, for querying Contexts
@@ -237,7 +244,6 @@ pub struct AvailableFormats(pub HashMap<String, String>);
 pub struct Dates {
     pub months: Contexts<MonthSymbols>,
     #[serde(rename = "monthPatterns")]
-    // Not used yet, will be in the future
     pub month_patterns: Option<Contexts<MonthPatternSymbols>>,
     pub days: Contexts<DaySymbols>,
     #[serde(default)]
