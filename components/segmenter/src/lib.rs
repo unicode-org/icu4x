21aca87512f1981f8aa1cdfffdeb7ccc7ee3d3f2
Robin Leroy <egg.robin.leroy@gmail.com>
2023-10-24 20:17:07 +0200

    Bring back the traces

--- components/segmenter/src/lib.rs
+++ components/segmenter/src/lib.rs
@@ -100,7 +100,7 @@
 //! See [`SentenceSegmenter`] for more examples.
 
 // https://github.com/unicode-org/icu4x/blob/main/docs/process/boilerplate.md#library-annotations
-#![cfg_attr(not(any(test, feature = "std")), no_std)]
+//#![cfg_attr(not(any(test, feature = "std")), no_std)]
 #![cfg_attr(
     not(test),
     deny(
