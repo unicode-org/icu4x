785bf07ad23eeee109150bbb34a930bf7d00dccd
Robin Leroy <egg.robin.leroy@gmail.com>
2023-10-24 20:23:37 +0200

    Revert "Bring back the traces"
    This reverts commit 21aca87512f1981f8aa1cdfffdeb7ccc7ee3d3f2.

--- provider/datagen/src/transform/segmenter/mod.rs
+++ provider/datagen/src/transform/segmenter/mod.rs
@@ -17,7 +17,6 @@ use icu_provider::datagen::IterableDataProvider;
 use icu_provider::prelude::*;
 use icu_segmenter::provider::*;
 use icu_segmenter::symbols::*;
-use itertools::Itertools;
 use std::fmt::Debug;
 use zerovec::ZeroVec;
 
@@ -470,8 +469,6 @@ impl crate::DatagenProvider {
         // sot and eot
         properties_names.push("sot".to_string());
         properties_names.push("eot".to_string());
-        println!("{:?}", properties_names);
-        println!("{}", properties_names.iter().enumerate().map(|(i, name)| format!("{:02X}={}", i, name)).join("\n"));
 
         let rule_size = properties_names.len() * properties_names.len();
         let mut break_state_table = vec![UNKNOWN_RULE; rule_size];
