df82c4d2d5b5820fa57444941daa042fe14871a6
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2023-11-01 01:11:33 +0100

    crate

--- ffi/capi/c/examples/fixeddecimal_tiny/crate/src/lib.rs
+++ ffi/capi/c/examples/fixeddecimal_tiny/crate/src/lib.rs
@@ -2,14 +2,16 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 // Expose icu_capi symbols
 extern crate icu_capi;
 
+#[cfg(not(feature = "std"))]
 #[global_allocator]
 static ALLOCATOR: dlmalloc::GlobalDlmalloc = dlmalloc::GlobalDlmalloc;
 
+#[cfg(not(feature = "std"))]
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
