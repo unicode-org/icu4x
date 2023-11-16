ec0e8af4c4d94980d01ca29bd219f1853851d9f2
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2023-11-16 15:20:26 +0100

    test-options

--- provider/datagen/tests/test-options.rs
+++ provider/datagen/tests/test-options.rs
@@ -6,15 +6,145 @@ use std::collections::{BTreeMap, HashSet};
 
 use elsa::sync::FrozenMap;
 use icu_datagen::prelude::*;
-use icu_decimal::provider::DecimalSymbolsV1Marker;
+use icu_locid_transform::provider::*;
 use icu_provider::datagen::ExportMarker;
+use icu_provider::datagen::*;
+use icu_provider::hello_world::*;
+use icu_provider::make_exportable_provider;
 use icu_provider::prelude::*;
 use postcard::ser_flavors::{AllocVec, Flavor};
 use writeable::Writeable;
 
+struct TestingProvider;
+
+impl DataProvider<HelloWorldV1Marker> for TestingProvider {
+    fn load(&self, req: DataRequest) -> Result<DataResponse<HelloWorldV1Marker>, DataError> {
+        Ok(DataResponse {
+            metadata: Default::default(),
+            payload: Some(DataPayload::from_owned(HelloWorldV1 {
+                message: match req.locale.to_string().as_str() {
+                    "ar" => "Hello in ar",
+                    "ar-EG" => "Hello in ar",
+                    "ar-EG-u-nu-latn" => "Hello in ar-u-nu-latn",
+                    "ar-u-nu-latn" => "Hello in ar-u-nu-latn",
+                    "bn" => "Hello in bn",
+                    "bn-u-nu-latn" => "Hello in bn-u-nu-latn",
+                    "ccp" => "Hello in ccp",
+                    "ccp-u-nu-latn" => "Hello in ccp-u-nu-latn",
+                    "en" => "Hello in en",
+                    "en-001" => "Hello in en",
+                    "en-ZA" => "Hello in en",
+                    "es" => "Hello in es",
+                    "es-AR" => "Hello in es-AR",
+                    "fil" => "Hello in fil",
+                    "fr" => "Hello in fr",
+                    "ja" => "Hello in ja",
+                    "ru" => "Hello in ru",
+                    "sr" => "Hello in sr",
+                    "sr-Latn" => "Hello in sr-Latn",
+                    "th" => "Hello in th",
+                    "th-u-nu-thai" => "Hello in th-u-nu-thai",
+                    "tr" => "Hello in tr",
+                    "und" => "Hello in und",
+                    _ => return Err(DataErrorKind::MissingLocale.into_error()),
+                }
+                .into(),
+            })),
+        })
+    }
+}
+
+impl DataProvider<LocaleFallbackLikelySubtagsV1Marker> for TestingProvider {
+    fn load(
+        &self,
+        req: DataRequest,
+    ) -> Result<DataResponse<LocaleFallbackLikelySubtagsV1Marker>, DataError> {
+        icu_locid_transform::provider::Baked.load(req)
+    }
+}
+
+impl DataProvider<LocaleFallbackParentsV1Marker> for TestingProvider {
+    fn load(
+        &self,
+        req: DataRequest,
+    ) -> Result<DataResponse<LocaleFallbackParentsV1Marker>, DataError> {
+        icu_locid_transform::provider::Baked.load(req)
+    }
+}
+
+impl DataProvider<CollationFallbackSupplementV1Marker> for TestingProvider {
+    fn load(
+        &self,
+        req: DataRequest,
+    ) -> Result<DataResponse<CollationFallbackSupplementV1Marker>, DataError> {
+        icu_locid_transform::provider::Baked.load(req)
+    }
+}
+
+impl IterableDataProvider<HelloWorldV1Marker> for TestingProvider {
+    fn supported_locales(&self) -> Result<Vec<DataLocale>, DataError> {
+        Ok([
+            "ar",
+            "ar-EG",
+            "ar-EG-u-nu-latn",
+            "ar-u-nu-latn",
+            "bn",
+            "bn-u-nu-latn",
+            "ccp",
+            "ccp-u-nu-latn",
+            "en",
+            "en-001",
+            "en-ZA",
+            "es",
+            "es-AR",
+            "fil",
+            "fr",
+            "ja",
+            "ru",
+            "sr",
+            "sr-Latn",
+            "th",
+            "th-u-nu-thai",
+            "tr",
+            "und",
+        ]
+        .into_iter()
+        .map(|s| s.parse::<DataLocale>().unwrap())
+        .collect())
+    }
+}
+
+impl IterableDataProvider<LocaleFallbackLikelySubtagsV1Marker> for TestingProvider {
+    fn supported_locales(&self) -> Result<Vec<DataLocale>, DataError> {
+        Ok(vec![Default::default()])
+    }
+}
+
+impl IterableDataProvider<LocaleFallbackParentsV1Marker> for TestingProvider {
+    fn supported_locales(&self) -> Result<Vec<DataLocale>, DataError> {
+        Ok(vec![Default::default()])
+    }
+}
+
+impl IterableDataProvider<CollationFallbackSupplementV1Marker> for TestingProvider {
+    fn supported_locales(&self) -> Result<Vec<DataLocale>, DataError> {
+        Ok(vec![Default::default()])
+    }
+}
+
+make_exportable_provider!(
+    TestingProvider,
+    [
+        HelloWorldV1Marker,
+        LocaleFallbackLikelySubtagsV1Marker,
+        LocaleFallbackParentsV1Marker,
+        CollationFallbackSupplementV1Marker,
+    ]
+);
+
 #[derive(Default)]
 struct TestingExporter {
-    data: FrozenMap<(DataKey, DataLocale), Vec<u8>>,
+    data: FrozenMap<DataLocale, String>,
 }
 
 impl<'a> DataExporter for &'a mut TestingExporter {
@@ -33,20 +163,24 @@ impl<'a> DataExporter for &'a mut TestingExporter {
             .finalize()
             .expect("Failed to finalize serializer output");
         println!("Putting: {key}/{locale}");
-        self.data.insert((key, locale.clone()), output);
+        self.data.insert(
+            locale.clone(),
+            postcard::from_bytes::<HelloWorldV1>(&output)
+                .unwrap()
+                .message
+                .to_string(),
+        );
         Ok(())
     }
 }
 
 impl TestingExporter {
-    /// Returns a map of the data locales as BCP-47 strings to their buffers.
-    pub fn take_map_and_reset(&mut self) -> BTreeMap<String, Vec<u8>> {
+    /// Returns a map of the data locales as BCP-47 strings to their hello world messages.
+    pub fn take_map_and_reset(&mut self) -> BTreeMap<String, String> {
         core::mem::take(&mut self.data)
             .into_tuple_vec()
             .into_iter()
-            .map(|((_data_key, data_locale), buffer)| {
-                (data_locale.write_to_string().into_owned(), buffer)
-            })
+            .map(|(data_locale, buffer)| (data_locale.write_to_string().into_owned(), buffer))
             .collect()
     }
 }
@@ -59,18 +193,14 @@ fn test_fallback_options() {
         .init()
         .unwrap();
 
-    let provider = DatagenProvider::new_custom()
-        .with_cldr("tests/data/cldr".into())
-        .unwrap()
-        .with_icuexport("tests/data/icuexport".into())
-        .unwrap();
+    let provider = TestingProvider;
 
     let mut testing_exporter = TestingExporter::default();
 
-    let driver = DatagenDriver::new().with_keys([DecimalSymbolsV1Marker::KEY]);
+    let driver = DatagenDriver::new().with_keys([HelloWorldV1Marker::KEY]);
 
     let explicit_locales: HashSet<LanguageIdentifier> = [
-        langid!("arc"), // Aramaic, not in CLDR
+        langid!("arc"), // Not supported
         langid!("ar-EG"),
         langid!("en-GB"),
         langid!("es"),
@@ -91,7 +221,7 @@ fn test_fallback_options() {
         .unwrap();
     let data_all_hybrid = testing_exporter.take_map_and_reset();
 
-    // These are all of the supported locales for DecimalSymbolsV1 in tests/data.
+    // These are all of the supported locales.
     let all_locales = [
         "ar",
         "ar-EG",
@@ -111,7 +241,6 @@ fn test_fallback_options() {
         "ja",
         "ru",
         "sr",
-        // "sr-Cyrl", (normalizes to 'sr')
         "sr-Latn",
         "th",
         "th-u-nu-thai",
@@ -120,7 +249,7 @@ fn test_fallback_options() {
     ];
 
     // All+Hybrid should return exactly the supported locales set.
-    itertools::assert_equal(data_all_hybrid.keys(), all_locales.iter());
+    assert_eq!(data_all_hybrid.keys().collect::<Vec<_>>(), all_locales);
 
     //
     // All+Runtime
@@ -144,18 +273,18 @@ fn test_fallback_options() {
         "bn-u-nu-latn",
         "ccp",
         "ccp-u-nu-latn",
-        // "en", (same as 'und')
-        // "en-001", (same as 'und')
-        // "en-ZA", (same as 'und')
+        "en",
+        // "en-001", (same as 'en')
+        // "en-ZA", (same as 'en')
         "es",
         "es-AR",
-        // "fil", (same as 'und')
+        "fil",
         "fr",
-        // "ja", (same as 'und')
+        "ja",
         "ru",
         "sr", // Note: 'sr' and 'sr-Latn' are the same, but they don't inherit
         "sr-Latn",
-        // "th", (same as 'und')
+        "th",
         "th-u-nu-thai",
         "tr",
         "und",
@@ -167,36 +296,19 @@ fn test_fallback_options() {
         data_all_hybrid.get("ar").unwrap()
     );
     assert_eq!(
-        data_all_hybrid.get("ar-EG-u-nu-latn").unwrap(),
-        data_all_hybrid.get("ar-u-nu-latn").unwrap()
-    );
-    assert_eq!(
-        data_all_hybrid.get("en").unwrap(),
-        data_all_hybrid.get("und").unwrap()
-    );
-    assert_eq!(
         data_all_hybrid.get("en-001").unwrap(),
-        data_all_hybrid.get("und").unwrap()
+        data_all_hybrid.get("en").unwrap()
     );
     assert_eq!(
         data_all_hybrid.get("en-ZA").unwrap(),
-        data_all_hybrid.get("und").unwrap()
-    );
-    assert_eq!(
-        data_all_hybrid.get("fil").unwrap(),
-        data_all_hybrid.get("und").unwrap()
-    );
-    assert_eq!(
-        data_all_hybrid.get("ja").unwrap(),
-        data_all_hybrid.get("und").unwrap()
-    );
-    assert_eq!(
-        data_all_hybrid.get("th").unwrap(),
-        data_all_hybrid.get("und").unwrap()
+        data_all_hybrid.get("en").unwrap()
     );
 
     // All+Runtime should return the supported locales set with deduplication.
-    itertools::assert_equal(data_all_runtime.keys(), all_locales_dedup.iter());
+    assert_eq!(
+        data_all_runtime.keys().collect::<Vec<_>>(),
+        all_locales_dedup
+    );
 
     //
     // Explicit+Hybrid
@@ -232,7 +344,10 @@ fn test_fallback_options() {
     ];
 
     // Explicit+Hybrid should return the expanded explicit locales set above.
-    itertools::assert_equal(data_explicit_hybrid.keys(), explicit_hybrid_locales.iter());
+    assert_eq!(
+        data_explicit_hybrid.keys().collect::<Vec<_>>(),
+        explicit_hybrid_locales
+    );
 
     //
     // Explicit+Runtime
@@ -253,7 +368,7 @@ fn test_fallback_options() {
         // "ar-EG", (same as 'ar')
         "ar-EG-u-nu-latn",
         // "arc", (same as 'und')
-        // "en", (same as 'und')
+        "en",
         // "en-001", (same as 'und')
         // "en-GB", (same as 'und')
         "es",
@@ -266,9 +381,9 @@ fn test_fallback_options() {
     ];
 
     // Explicit+Runtime should return the expanded then deduplicated explicit locales set above.
-    itertools::assert_equal(
-        data_explicit_runtime.keys(),
-        explicit_hybrid_locales_dedup.iter(),
+    assert_eq!(
+        data_explicit_runtime.keys().collect::<Vec<_>>(),
+        explicit_hybrid_locales_dedup,
     );
 
     //
@@ -295,8 +410,8 @@ fn test_fallback_options() {
     ];
 
     // Explicit+Preresolved should return the exact explicit locales set.
-    itertools::assert_equal(
-        data_explicit_preresolved.keys(),
-        explicit_preresolved_locales.iter(),
+    assert_eq!(
+        data_explicit_preresolved.keys().collect::<Vec<_>>(),
+        explicit_preresolved_locales,
     );
 }
