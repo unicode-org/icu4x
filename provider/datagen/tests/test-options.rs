b69f848e89ca309265dd57d17e4bdc1189ef9146
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2023-11-21 21:22:29 +0100

    trim locales

--- provider/datagen/tests/test-options.rs
+++ provider/datagen/tests/test-options.rs
@@ -27,25 +27,11 @@ impl DataProvider<HelloWorldV1Marker> for TestingProvider {
                     "ar-EG" => "Hello in ar",
                     "ar-EG-u-nu-latn" => "Hello in ar-u-nu-latn",
                     "ar-u-nu-latn" => "Hello in ar-u-nu-latn",
-                    "bn" => "Hello in bn",
-                    "bn-u-nu-latn" => "Hello in bn-u-nu-latn",
-                    "ccp" => "Hello in ccp",
-                    "ccp-u-nu-latn" => "Hello in ccp-u-nu-latn",
                     "en" => "Hello in en",
-                    "en-001" => "Hello in en",
                     "en-ZA" => "Hello in en",
-                    "es" => "Hello in es",
-                    "es-AR" => "Hello in es-AR",
-                    "fil" => "Hello in fil",
-                    "fr" => "Hello in fr",
-                    "ja" => "Hello in ja",
-                    "ru" => "Hello in ru",
                     "sr" => "Hello in sr",
                     "sr-Latn" => "Hello in sr-Latn",
-                    "th" => "Hello in th",
-                    "th-u-nu-thai" => "Hello in th-u-nu-thai",
-                    "tr" => "Hello in tr",
-                    "und" => "Hello in und",
+                    "und" => "Hello in en",
                     _ => return Err(DataErrorKind::MissingLocale.into_error()),
                 }
                 .into(),
@@ -88,24 +74,10 @@ impl IterableDataProvider<HelloWorldV1Marker> for TestingProvider {
             "ar-EG",
             "ar-EG-u-nu-latn",
             "ar-u-nu-latn",
-            "bn",
-            "bn-u-nu-latn",
-            "ccp",
-            "ccp-u-nu-latn",
             "en",
-            "en-001",
             "en-ZA",
-            "es",
-            "es-AR",
-            "fil",
-            "fr",
-            "ja",
-            "ru",
             "sr",
             "sr-Latn",
-            "th",
-            "th-u-nu-thai",
-            "tr",
             "und",
         ]
         .into_iter()
@@ -203,9 +175,7 @@ fn test_fallback_options() {
         langid!("arc"), // Not supported
         langid!("ar-EG"),
         langid!("en-GB"),
-        langid!("es"),
         langid!("sr-ME"),
-        langid!("ru-Cyrl-RU"),
     ]
     .into_iter()
     .collect();
@@ -227,24 +197,10 @@ fn test_fallback_options() {
         "ar-EG",
         "ar-EG-u-nu-latn",
         "ar-u-nu-latn",
-        "bn",
-        "bn-u-nu-latn",
-        "ccp",
-        "ccp-u-nu-latn",
         "en",
-        "en-001",
         "en-ZA",
-        "es",
-        "es-AR",
-        "fil",
-        "fr",
-        "ja",
-        "ru",
         "sr",
         "sr-Latn",
-        "th",
-        "th-u-nu-thai",
-        "tr",
         "und",
     ];
 
@@ -269,24 +225,10 @@ fn test_fallback_options() {
         // "ar-EG", (same as 'ar')
         "ar-EG-u-nu-latn", // (same as 'ar-u-nu-latn' but DIFFERENT than 'ar-EG')
         "ar-u-nu-latn",
-        "bn",
-        "bn-u-nu-latn",
-        "ccp",
-        "ccp-u-nu-latn",
-        "en",
-        // "en-001", (same as 'en')
-        // "en-ZA", (same as 'en')
-        "es",
-        "es-AR",
-        "fil",
-        "fr",
-        "ja",
-        "ru",
+        // "en", (same as 'und')
+        // "en-ZA", (same as 'und')
         "sr", // Note: 'sr' and 'sr-Latn' are the same, but they don't inherit
         "sr-Latn",
-        "th",
-        "th-u-nu-thai",
-        "tr",
         "und",
     ];
 
@@ -296,12 +238,12 @@ fn test_fallback_options() {
         data_all_hybrid.get("ar").unwrap()
     );
     assert_eq!(
-        data_all_hybrid.get("en-001").unwrap(),
-        data_all_hybrid.get("en").unwrap()
+        data_all_hybrid.get("en-ZA").unwrap(),
+        data_all_hybrid.get("und").unwrap()
     );
     assert_eq!(
-        data_all_hybrid.get("en-ZA").unwrap(),
-        data_all_hybrid.get("en").unwrap()
+        data_all_hybrid.get("en").unwrap(),
+        data_all_hybrid.get("und").unwrap()
     );
 
     // All+Runtime should return the supported locales set with deduplication.
@@ -322,7 +264,7 @@ fn test_fallback_options() {
         .unwrap();
     let data_explicit_hybrid = testing_exporter.take_map_and_reset();
 
-    // Explicit locales are "arc", "ar-EG", "en-GB", "es", "sr-ME", "ru-Cyrl-RU"
+    // Explicit locales are "arc", "ar-EG", "en-GB", "sr-ME"
     let explicit_hybrid_locales = [
         "ar",              // ancestor of ar-EG
         "ar-EG",           // explicit locale
@@ -330,13 +272,8 @@ fn test_fallback_options() {
         // "ar-u-nu-latn", // ??? should this be included?
         "arc",    // Aramaic, inheriting from und
         "en",     // ancestor of en-GB
-        "en-001", // ancestor of en-GB
         "en-GB",  // explicit locale not in supported locales
         // "en-ZA", // not reachable
-        "es",         // explicit and supported
-        "es-AR",      // descendant of es
-        "ru",         // ancestor of ru-Cyrl-RU
-        "ru-Cyrl-RU", // explicit locale, even though it is not normalized
         // "sr", // not reachable from sr-ME
         "sr-Latn", // ancestor of sr-ME
         "sr-ME",   // explicit locale not in supported locales
@@ -361,20 +298,16 @@ fn test_fallback_options() {
         .unwrap();
     let data_explicit_runtime = testing_exporter.take_map_and_reset();
 
-    // Explicit locales are "arc", "ar-EG", "en-GB", "es", "sr-ME", "ru-Cyrl-RU"
+    // Explicit locales are "arc", "ar-EG", "en-GB", "sr-ME"
     let explicit_hybrid_locales_dedup = [
         "ar",
         // "ar-Arab-EG", (same as 'ar')
         // "ar-EG", (same as 'ar')
         "ar-EG-u-nu-latn",
         // "arc", (same as 'und')
-        "en",
+        // "en", (same as 'und')
         // "en-001", (same as 'und')
         // "en-GB", (same as 'und')
-        "es",
-        "es-AR",
-        "ru",
-        // "ru-Cyrl-RU", (same as 'ru')
         "sr-Latn",
         // "sr-ME", (same as 'sr-Latn')
         "und",
@@ -398,14 +331,12 @@ fn test_fallback_options() {
         .unwrap();
     let data_explicit_preresolved = testing_exporter.take_map_and_reset();
 
-    // Explicit locales are "arc", "ar-EG", "en-GB", "es", "sr-ME", "ru-Cyrl-RU"
+    // Explicit locales are "arc", "ar-EG", "en-GB", "sr-ME"
     let explicit_preresolved_locales = [
         "ar-EG",
         "ar-EG-u-nu-latn", // extensions included even in preresolved mode
         "arc",
         "en-GB",
-        "es",
-        "ru-Cyrl-RU",
         "sr-ME",
     ];
 
