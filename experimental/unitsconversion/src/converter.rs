d928b0e919fefa8e01d3f60f5b96870fe17e178f
Younies Mahmoud <younies.mahmoud@gmail.com>
2023-12-04 15:12:36 +0100

    implement convertibility

--- experimental/unitsconversion/src/converter.rs
+++ experimental/unitsconversion/src/converter.rs
@@ -2,25 +2,98 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use crate::ConversionError;
+// TODO: we do not want to use `std`
+use std::collections::HashMap;
+
+use crate::{
+    measureunit::MeasureUnitParser,
+    provider::{MeasureUnitItem, UnitsInfoV1},
+    ConversionError,
+};
 
 /// Represents the possible cases for the convertibility between two units.
 pub enum Convertibility {
+    /// The units are convertible.
+    /// For example, `meter` and `foot` are convertible.
     Convertible,
+
+    /// The units are reciprocal.
+    /// For example, `meter-per-second` and `second-per-meter` are reciprocal.
     Reciprocal,
+
+    /// The units are not convertible.
+    /// For example, `meter` and `second` are not convertible.
     NotConvertible,
 }
 
 /// A factory for creating a converter.
-pub struct ConverterFactory {}
+/// Also, it can check the convertibility between two units.
+pub struct ConverterFactory<'data> {
+    /// Contains the necessary data for the conversion factory.
+    payload: UnitsInfoV1<'data>,
+}
 
-impl ConverterFactory {
+impl ConverterFactory<'_> {
+    fn parser(&self) -> MeasureUnitParser<'_> {
+        MeasureUnitParser::new(&self.payload.units_conversion_trie)
+    }
+
     /// Extract the convertibility from the given units in the form of CLDR identifiers.
     pub fn extract_convertibility(
         &self,
         unit1: &str,
         unit2: &str,
     ) -> Result<Convertibility, ConversionError> {
-        todo!();
+        let parser = self.parser();
+        let unit1 = parser.try_from_identifier(unit1)?;
+        let unit2 = parser.try_from_identifier(unit2)?;
+
+        struct DetermineConvertibility {
+            convertible: i16,
+            reciprocal: i16,
+        }
+
+        fn insert_units_powers(
+            unit_items: &Vec<MeasureUnitItem>,
+            sign: i8,
+            map: &mut HashMap<u16, DetermineConvertibility>,
+        ) {
+            for item in unit_items {
+                if let Some(determine_convertibility) = map.get_mut(&item.unit_id) {
+                    determine_convertibility.convertible += (item.power * sign) as i16;
+                    determine_convertibility.reciprocal += (item.power * sign) as i16;
+                } else {
+                    map.insert(
+                        item.unit_id,
+                        DetermineConvertibility {
+                            convertible: (item.power * sign) as i16,
+                            reciprocal: (item.power * sign) as i16,
+                        },
+                    );
+                }
+            }
+        }
+
+        let mut map = HashMap::<u16, DetermineConvertibility>::new();
+        insert_units_powers(&unit1, 1, &mut map);
+        insert_units_powers(&unit2, -1, &mut map);
+
+        let (convertible_sum, reciprocal_sum) = map.values().fold(
+            (0, 0),
+            |(convertible_sum, reciprocal_sum), determine_convertibility| {
+                (
+                    convertible_sum + determine_convertibility.convertible,
+                    reciprocal_sum + determine_convertibility.reciprocal,
+                )
+            },
+        );
+
+        if convertible_sum == 0 {
+            Ok(Convertibility::Convertible)
+        } else if reciprocal_sum == 0 {
+            Ok(Convertibility::Reciprocal)
+        } else {
+            Ok(Convertibility::NotConvertible)
+        }
     }
 }
