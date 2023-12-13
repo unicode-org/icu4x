0ae2a4af19ad22eb121c86c2b198f767ea5ebdb8
Shane F. Carr <shane@unicode.org>
2023-12-12 16:24:20 -0800

    Testing

--- provider/core/src/refcount.rs
+++ provider/core/src/refcount.rs
@@ -2,10 +2,10 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+use alloc::boxed::Box;
 use core::fmt::Debug;
 use core::mem::ManuallyDrop;
 use core::ptr::NonNull;
-use alloc::boxed::Box;
 use yoke::CloneableCart;
 
 #[cfg(not(feature = "sync"))]
@@ -25,15 +25,21 @@ pub(crate) struct OptionSelectedRcBytes(NonNull<WrappedType>);
 impl OptionSelectedRcBytes {
     #[inline]
     pub const fn new_sentinel() -> Self {
-        // Safety: `SENTINEL` is non-null
+        // Safety: `SENTINEL_PTR` is non-null
         Self(unsafe { NonNull::new_unchecked(SENTINEL_PTR) })
     }
 
     #[inline]
     pub fn from_selected_rc(selected_rc: SelectedRc<WrappedType>) -> Self {
         let ptr = SelectedRc::into_raw(selected_rc);
-        debug_assert_ne!(ptr, SENTINEL_PTR, "Creating from [A]Rc should never be the sentinel ptr");
-        debug_assert_ne!(ptr, 0 as *const WrappedType, "Creating from [A]Rc should never be the null ptr");
+        debug_assert_ne!(
+            ptr, SENTINEL_PTR,
+            "Creating from [A]Rc should never be the sentinel ptr"
+        );
+        debug_assert_ne!(
+            ptr, 0 as *const WrappedType,
+            "Creating from [A]Rc should never be the null ptr"
+        );
         // Safety: ptr is non-null
         Self(unsafe { NonNull::new_unchecked(ptr as *mut WrappedType) })
     }
@@ -41,13 +47,26 @@ impl OptionSelectedRcBytes {
     #[inline]
     pub fn take(&mut self) -> Option<SelectedRc<WrappedType>> {
         let ptr = self.0.as_ptr() as *const WrappedType;
-        *self = Self::new_sentinel();
+        // Safety: `SENTINEL_PTR` is non-null
+        self.0 = unsafe { NonNull::new_unchecked(SENTINEL_PTR) };
         if ptr == SENTINEL_PTR {
             None
         } else {
             // Safety: this type has only 2 constructors, and we already
             // checked for the sentinel value.
             Some(unsafe { SelectedRc::from_raw(ptr) })
+        }
+    }
+
+    pub fn as_bytes(&self) -> Option<&[u8]> {
+        let ptr = self.0.as_ptr() as *const WrappedType;
+        if ptr == SENTINEL_PTR {
+            None
+        } else {
+            // Safety: this type has only 2 constructors, and we already
+            // checked for the sentinel value.
+            let wrapped_type: &WrappedType = unsafe { self.0.as_ref() };
+            Some(&*wrapped_type)
         }
     }
 }
@@ -74,13 +93,105 @@ impl Clone for OptionSelectedRcBytes {
     }
 }
 
+// Safety: type has the same semantics as Option<Rc<_>>
+// which implements CloneableCart
 unsafe impl CloneableCart for OptionSelectedRcBytes {}
 
+// Safety: same bounds as Arc
 #[cfg(feature = "sync")]
-unsafe impl Send for OptionSelectedRcBytes {}
+unsafe impl Send for OptionSelectedRcBytes where WrappedType: Sync + Send {}
 
+// Safety: same bounds as Arc
 #[cfg(feature = "sync")]
-unsafe impl Sync for OptionSelectedRcBytes {}
+unsafe impl Sync for OptionSelectedRcBytes where WrappedType: Sync + Send {}
 
-// unsafe impl<T: ?Sized + Sync + Send> Send for Arc<T, A> {}
-// unsafe impl<T: ?Sized + Sync + Send> Sync for Arc<T, A> {}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::alloc::{GlobalAlloc, Layout, System};
+    use std::cell::Cell;
+
+    #[global_allocator]
+    static ALLOCATOR: MeasuringAllocator = MeasuringAllocator;
+
+    // Inspired by the assert_no_alloc crate
+    struct MeasuringAllocator;
+
+    impl MeasuringAllocator {
+        // We need to track allocations on each thread independently
+        thread_local! {
+            static ACTIVE: Cell<bool> = Cell::new(false);
+            static TOTAL_ALLOCATED: Cell<u64> = Cell::new(0);
+            static TOTAL_DEALLOCATED: Cell<u64> = Cell::new(0);
+        }
+
+        pub fn start_measure() {
+            Self::ACTIVE.with(|c| c.set(true));
+        }
+
+        pub fn end_measure() -> (u64, u64) {
+            Self::ACTIVE.with(|c| c.set(false));
+            (
+                Self::TOTAL_ALLOCATED.with(|c| c.take()),
+                Self::TOTAL_DEALLOCATED.with(|c| c.take()),
+            )
+        }
+    }
+
+    unsafe impl GlobalAlloc for MeasuringAllocator {
+        unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+            if Self::ACTIVE.with(|f| f.get()) {
+                Self::TOTAL_ALLOCATED.with(|c| c.set(c.get() + layout.size() as u64));
+            }
+            System.alloc(layout)
+        }
+
+        unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+            if Self::ACTIVE.with(|f| f.get()) {
+                Self::TOTAL_DEALLOCATED.with(|c| c.set(c.get() + layout.size() as u64));
+            }
+            System.dealloc(ptr, layout)
+        }
+    }
+
+    #[test]
+    fn test_new_sentinel() {
+        MeasuringAllocator::start_measure();
+        {
+            OptionSelectedRcBytes::new_sentinel();
+        }
+        let (allocated, deallocated) = MeasuringAllocator::end_measure();
+        assert_eq!(allocated, deallocated);
+        assert_eq!(allocated, 0);
+    }
+
+    #[test]
+    fn test_new_rc() {
+        MeasuringAllocator::start_measure();
+        {
+            OptionSelectedRcBytes::from_selected_rc(Default::default());
+        }
+        let (allocated, deallocated) = MeasuringAllocator::end_measure();
+        assert_eq!(allocated, deallocated);
+        assert_eq!(allocated, 32);
+    }
+
+    #[test]
+    fn test_rc_clone() {
+        MeasuringAllocator::start_measure();
+        {
+            let x = OptionSelectedRcBytes::from_selected_rc(Default::default());
+            {
+                let y = x.clone();
+                assert_eq!(x.as_bytes(), y.as_bytes());
+            }
+            let (allocated, deallocated) = MeasuringAllocator::end_measure();
+            assert_eq!(allocated, 32);
+            assert_eq!(deallocated, 0);
+            MeasuringAllocator::start_measure();
+        }
+        let (allocated, deallocated) = MeasuringAllocator::end_measure();
+        assert_eq!(allocated, 0);
+        assert_eq!(deallocated, 32);
+    }
+}
