69a932708ef43457798949e58bff1e977e6ad824
Younies Mahmoud <younies.mahmoud@gmail.com>
2023-12-19 12:17:00 +0100

    Use const TRIE for powers

--- experimental/unitsconversion/src/power.rs
+++ experimental/unitsconversion/src/power.rs
@@ -4,6 +4,26 @@
 
 use zerotrie::{ZeroTrieSimpleAscii, ZeroTrieSimpleAsciiCursor};
 
+const TRIE: ZeroTrieSimpleAscii<[u8; 64]> = ZeroTrieSimpleAscii::from_sorted_str_tuples(&[
+    ("cubic", 3),
+    ("pow1", 1),
+    ("pow10", 10),
+    ("pow11", 11),
+    ("pow12", 12),
+    ("pow13", 13),
+    ("pow14", 14),
+    ("pow15", 15),
+    ("pow2", 2),
+    ("pow3", 3),
+    ("pow4", 4),
+    ("pow5", 5),
+    ("pow6", 6),
+    ("pow7", 7),
+    ("pow8", 8),
+    ("pow9", 9),
+    ("square", 2),
+]);
+
 // TODO: consider returning Option<(u8, &str)> instead of (1, part) for the case when the power is not found.
 // TODO: complete all the cases for the powers.
 // TODO: consider using a trie for the powers.
@@ -11,29 +31,7 @@ use zerotrie::{ZeroTrieSimpleAscii, ZeroTrieSimpleAsciiCursor};
 ///     - If the power is not found, the function returns (1, part).
 ///     - If the power is found, the function will return (power, part without the string of the power).
 pub fn get_power(part: &str) -> (u8, &str) {
-    use std::collections::BTreeMap;
-    let mut powers = BTreeMap::<Vec<u8>, usize>::new();
-    powers.insert(b"pow1".to_vec(), 1);
-    powers.insert(b"pow2".to_vec(), 2);
-    powers.insert(b"square".to_vec(), 2);
-    powers.insert(b"pow3".to_vec(), 3);
-    powers.insert(b"cubic".to_vec(), 3);
-    powers.insert(b"pow4".to_vec(), 4);
-    powers.insert(b"pow5".to_vec(), 5);
-    powers.insert(b"pow6".to_vec(), 6);
-    powers.insert(b"pow7".to_vec(), 7);
-    powers.insert(b"pow8".to_vec(), 8);
-    powers.insert(b"pow9".to_vec(), 9);
-    powers.insert(b"pow10".to_vec(), 10);
-    powers.insert(b"pow11".to_vec(), 11);
-    powers.insert(b"pow12".to_vec(), 12);
-    powers.insert(b"pow13".to_vec(), 13);
-    powers.insert(b"pow14".to_vec(), 14);
-    powers.insert(b"pow15".to_vec(), 15);
-
-    let trie = ZeroTrieSimpleAscii::try_from(&powers).unwrap();
-
-    let mut cursor = trie.cursor();
+    let mut cursor = TRIE.cursor();
     let mut longest_match = (1, part);
     for (i, b) in part.bytes().enumerate() {
         cursor.step(b);
