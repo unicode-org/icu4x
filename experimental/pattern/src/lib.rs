42951d51f14f8d7f17b24a7bdbe4c324c50643ef
Shane F. Carr <shane@unicode.org>
2024-02-02 22:47:08 -0800

    Clean up WriteToParts impl

--- experimental/pattern/src/lib.rs
+++ experimental/pattern/src/lib.rs
@@ -55,7 +55,7 @@ const PLACEHOLDER_RANGE_SIZE: usize = MAX_PLACEHOLDER_CODE_POINT - MIN_PLACEHOLD
 /// let pattern = NumericPlaceholderPattern::from_store("Hello, \x01 and \x02!");
 ///
 /// assert_writeable_eq!(
-///     pattern.interpolate_with(&["Alice", "Bob"], writeable::Part { category: "foo", value: "bar" }),
+///     pattern.interpolate(["Alice", "Bob"]),
 ///     "Hello, Alice and Bob!"
 /// );
 /// ```
@@ -107,18 +107,66 @@ where
 
     /// Returns a [`Writeable`] that interpolates items from the given replacement provider
     /// into this pattern string.
-    pub fn interpolate_with<'a, P>(
+    pub fn interpolate<'a, P>(&'a self, replacements: P) -> impl Writeable + fmt::Display + 'a
+    where
+        P: NumericPlaceholderProvider + 'a,
+    {
+        WriteableNumericPlaceholderPattern {
+            pattern: self.as_borrowed_store(),
+            replacements,
+        }
+    }
+
+    /// Interpolates items with [writeable::Part]s.
+    ///
+    /// Two parts are used:
+    ///
+    /// 1. `literal_part` for [`NumericPlaceholderPatternItem::Literal`]
+    /// 2. `element_part` for [`NumericPlaceholderPatternItem::Placeholder`]
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use icu_pattern_2::NumericPlaceholderPattern;
+    /// use writeable::assert_writeable_parts_eq;
+    ///
+    /// let pattern = NumericPlaceholderPattern::from_store("Hello, \x01 and \x02!");
+    ///
+    /// const LITERAL_PART: writeable::Part = writeable::Part {
+    ///     category: "demo",
+    ///     value: "literal",
+    /// };
+    /// const ELEMENT_PART: writeable::Part = writeable::Part {
+    ///     category: "demo",
+    ///     value: "element",
+    /// };
+    ///
+    /// assert_writeable_parts_eq!(
+    ///     pattern.interpolate_with_parts(["Alice", "Bob"], LITERAL_PART, ELEMENT_PART),
+    ///     "Hello, Alice and Bob!",
+    ///     [
+    ///         (0, 7, LITERAL_PART),
+    ///         (7, 12, ELEMENT_PART),
+    ///         (12, 17, LITERAL_PART),
+    ///         (17, 20, ELEMENT_PART),
+    ///         (20, 21, LITERAL_PART),
+    ///     ]
+    /// );
+    /// ```
+    pub fn interpolate_with_parts<'a, P>(
         &'a self,
         replacements: P,
-        part: writeable::Part,
+        literal_part: writeable::Part,
+        element_part: writeable::Part,
     ) -> impl Writeable + fmt::Display + 'a
     where
         P: NumericPlaceholderProvider + 'a,
     {
-        WriteableNumericPlaceholderPatternWithPart {
+        WriteableNumericPlaceholderPatternWithParts {
             pattern: self.as_borrowed_store(),
             replacements,
-            part,
+            literal_part,
+            element_part,
         }
     }
 }
@@ -261,29 +309,27 @@ where
     }
 }
 
-struct WriteableNumericPlaceholderPatternWithPart<'a, P> {
+struct WriteableNumericPlaceholderPattern<'a, P> {
     pattern: NumericPlaceholderPattern<&'a str>,
     replacements: P,
-    // TODO: Include the part for the element?
-    part: writeable::Part,
 }
 
-impl<P> Writeable for WriteableNumericPlaceholderPatternWithPart<'_, P>
+impl<P> Writeable for WriteableNumericPlaceholderPattern<'_, P>
 where
     P: NumericPlaceholderProvider,
 {
-    fn write_to_parts<S: PartsWrite + ?Sized>(&self, sink: &mut S) -> fmt::Result {
+    fn write_to<W: fmt::Write + ?Sized>(&self, sink: &mut W) -> fmt::Result {
         for item in self.pattern.iter() {
             match item {
                 NumericPlaceholderPatternItem::Literal(s) => {
-                    sink.with_part(self.part, |w| w.write_str(s))?;
+                    sink.write_str(s)?;
                 }
                 NumericPlaceholderPatternItem::Placeholder(number) => {
                     let Some(element_writeable) = self.replacements.replacement_for(number) else {
                         debug_assert!(false, "could not load placeholder value {number}");
                         return Err(fmt::Error);
                     };
-                    element_writeable.write_to_parts(sink)?;
+                    element_writeable.write_to(sink)?;
                 }
             }
         }
@@ -291,7 +337,47 @@ where
     }
 }
 
-impl<P> fmt::Display for WriteableNumericPlaceholderPatternWithPart<'_, P>
+impl<P> fmt::Display for WriteableNumericPlaceholderPattern<'_, P>
+where
+    P: NumericPlaceholderProvider,
+{
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.write_to(f)
+    }
+}
+
+struct WriteableNumericPlaceholderPatternWithParts<'a, P> {
+    pattern: NumericPlaceholderPattern<&'a str>,
+    replacements: P,
+    literal_part: writeable::Part,
+    element_part: writeable::Part,
+}
+
+impl<P> Writeable for WriteableNumericPlaceholderPatternWithParts<'_, P>
+where
+    P: NumericPlaceholderProvider,
+{
+    fn write_to_parts<S: PartsWrite + ?Sized>(&self, sink: &mut S) -> fmt::Result {
+        for item in self.pattern.iter() {
+            match item {
+                NumericPlaceholderPatternItem::Literal(s) => {
+                    sink.with_part(self.literal_part, |w| w.write_str(s))?;
+                }
+                NumericPlaceholderPatternItem::Placeholder(number) => {
+                    let Some(element_writeable) = self.replacements.replacement_for(number) else {
+                        debug_assert!(false, "could not load placeholder value {number}");
+                        return Err(fmt::Error);
+                    };
+                    sink.with_part(self.element_part, |w| element_writeable.write_to_parts(w))?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+impl<P> fmt::Display for WriteableNumericPlaceholderPatternWithParts<'_, P>
 where
     P: NumericPlaceholderProvider,
 {
