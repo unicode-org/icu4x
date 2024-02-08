d7bdac394f4d35838f7027454c724bc07aefd3ca
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-02-08 15:58:32 +0100

    fix fmt

--- experimental/components/tests/displaynames/tests.rs
+++ experimental/components/tests/displaynames/tests.rs
@@ -2,10 +2,10 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use std::borrow::Cow;
 use icu_experimental::displaynames::{DisplayNamesOptions, LocaleDisplayNamesFormatter};
 use icu_locid::locale;
 use icu_locid::Locale;
+use std::borrow::Cow;
 
 #[test]
 fn test_concatenate() {
