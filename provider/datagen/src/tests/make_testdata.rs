0f0995205c32527de042d6532d4b7238a51e67f2
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2024-02-14 17:17:06 +0100

    test

--- provider/datagen/src/tests/make_testdata.rs
+++ provider/datagen/src/tests/make_testdata.rs
@@ -18,6 +18,11 @@ include!("../../tests/locales.rs.data");
 #[test]
 #[cfg(feature = "use_wasm")]
 fn make_testdata() {
+    simple_logger::SimpleLogger::new()
+        .env()
+        .with_level(log::LevelFilter::Info)
+        .init()
+        .unwrap();
     // Only produce output if the variable is set. Test is hermetic otherwise.
     let exporter: Box<dyn DataExporter> = if std::option_env!("ICU4X_WRITE_TESTDATA").is_none() {
         Box::new(PostcardTestingExporter {
@@ -28,11 +33,6 @@ fn make_testdata() {
             fingerprints: std::io::sink(),
         })
     } else {
-        simple_logger::SimpleLogger::new()
-            .env()
-            .with_level(log::LevelFilter::Info)
-            .init()
-            .unwrap();
 
         Box::new(MultiExporter::new(vec![
             #[cfg(feature = "fs_provider")]
