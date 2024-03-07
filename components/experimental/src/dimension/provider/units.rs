2dc2e997d5f2d86ecafade17fcc7877fbd4e2011
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-03-07 11:05:10 +0100

    download cldr units data

---
+++ components/experimental/src/dimension/provider/units.rs
@@ -0,0 +1,18 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+// Provider structs must be stable
+#![allow(clippy::exhaustive_structs, clippy::exhaustive_enums)]
+
+//! Data provider struct definitions for this ICU4X component.
+//!
+//! Read more about data providers: [`icu_provider`]
+
+
+use alloc::borrow::Cow;
+use icu_provider::prelude::*;
+use tinystr::UnvalidatedTinyAsciiStr;
+use zerovec::{VarZeroVec, ZeroMap};
+
+
