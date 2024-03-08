8387180cc2d2051a6cbafcfb967d45d6ff3ba495
Shane F. Carr <shane@unicode.org>
2024-03-08 10:03:51 +0100

    Implement Serde for Pattern (#4665)

--- utils/pattern/tests/derive_test.rs
+++ utils/pattern/tests/derive_test.rs
@@ -11,24 +11,56 @@ use icu_pattern::{Pattern, SinglePlaceholder};
 
 #[cfg_attr(feature = "yoke", derive(yoke::Yokeable))]
 #[cfg_attr(feature = "zerofrom", derive(zerofrom::ZeroFrom))]
-// #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "databake", derive(databake::Bake), databake(path = crate))]
-struct DeriveTest_SinglePlaceholderPattern_ZeroVec<'data> {
-    // #[cfg_attr(feature = "serde", serde(borrow))]
-    _data: Pattern<SinglePlaceholder, Cow<'data, str>>,
+#[derive(Debug, PartialEq)]
+struct DeriveTest_SinglePlaceholderPattern_Cow<'data> {
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    data: Pattern<SinglePlaceholder, Cow<'data, str>>,
 }
 
 #[test]
 #[cfg(all(feature = "databake", feature = "alloc"))]
-fn bake_SinglePlaceholderPattern_ZeroVec() {
+fn bake_SinglePlaceholderPattern_Cow() {
     use databake::*;
     extern crate std;
     test_bake!(
-        DeriveTest_SinglePlaceholderPattern_ZeroVec<'static>,
-        crate::DeriveTest_SinglePlaceholderPattern_ZeroVec {
-            _data: icu_pattern::Pattern::<icu_pattern::SinglePlaceholder, _>::from_store_unchecked(
+        DeriveTest_SinglePlaceholderPattern_Cow<'static>,
+        crate::DeriveTest_SinglePlaceholderPattern_Cow {
+            data: icu_pattern::Pattern::<icu_pattern::SinglePlaceholder, _>::from_store_unchecked(
                 alloc::borrow::Cow::Borrowed(""),
             )
         },
     );
+}
+
+#[test]
+#[cfg(feature = "serde")]
+fn json_SinglePlaceholderPattern_Cow() {
+    let pattern_owned = Pattern::<SinglePlaceholder, String>::try_from_str("Hello, {0}!").unwrap();
+    let pattern_cow: Pattern<SinglePlaceholder, Cow<str>> =
+        Pattern::from_store_unchecked(Cow::Owned(pattern_owned.take_store()));
+    let data = DeriveTest_SinglePlaceholderPattern_Cow { data: pattern_cow };
+    let data_json = serde_json::to_string(&data).unwrap();
+    assert_eq!(
+        data_json,
+        r#"{"data":[{"Literal":"Hello, "},{"Placeholder":"Singleton"},{"Literal":"!"}]}"#
+    );
+    let data_deserialized: DeriveTest_SinglePlaceholderPattern_Cow =
+        serde_json::from_str(&data_json).unwrap();
+    assert_eq!(data, data_deserialized);
+}
+
+#[test]
+#[cfg(feature = "serde")]
+fn postcard_SinglePlaceholderPattern_Cow() {
+    let pattern_owned = Pattern::<SinglePlaceholder, String>::try_from_str("Hello, {0}!").unwrap();
+    let pattern_cow: Pattern<SinglePlaceholder, Cow<str>> =
+        Pattern::from_store_unchecked(Cow::Owned(pattern_owned.take_store()));
+    let data = DeriveTest_SinglePlaceholderPattern_Cow { data: pattern_cow };
+    let data_postcard = postcard::to_stdvec(&data).unwrap();
+    assert_eq!(data_postcard, b"\x09\x08Hello, !");
+    let data_deserialized: DeriveTest_SinglePlaceholderPattern_Cow =
+        postcard::from_bytes(&data_postcard).unwrap();
+    assert_eq!(data, data_deserialized);
 }
