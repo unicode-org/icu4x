a4e1cf170aac21a5152044e0a4e8dad656cc0bfa
Shane F. Carr <shane@unicode.org>
2024-03-12 13:06:42 +0100

    Add an example skeleton struct impl

--- components/datetime/src/neo_skeleton.rs
+++ components/datetime/src/neo_skeleton.rs
@@ -7,6 +7,7 @@
 use icu_provider::prelude::*;
 use crate::CldrCalendar;
 use crate::provider::neo::*;
+use icu_provider::NeverMarker;
 
 pub trait NeoSkeleton<C: CldrCalendar> {
     type YearNamesV1Marker: KeyedDataMarker<Yokeable = YearNamesV1<'static>>;
@@ -15,5 +16,41 @@ pub trait NeoSkeleton<C: CldrCalendar> {
     type DayPeriodNamesV1Marker: KeyedDataMarker<Yokeable = LinearNamesV1<'static>>;
     type DateSkeletonPatternsV1Marker: KeyedDataMarker<Yokeable = PackedSkeletonDataV1<'static>>;
     type TimeSkeletonPatternsV1Marker: KeyedDataMarker<Yokeable = PackedSkeletonDataV1<'static>>;
+    type DateTimePatternV1Marker: KeyedDataMarker<Yokeable = DateTimePatternV1<'static>>;
     type DateTimeSkeletonPatternsV1Marker: KeyedDataMarker<Yokeable = DateTimeSkeletonsV1<'static>>;
+}
+
+/// A specification for the length of a date or component of a date.
+///
+/// Contrary to [`crate::options::length::Time`] and
+/// [`crate::options::length::Date`], this has only three levels, with no
+/// `Full`; this is because `Full` corresponds to additional components,
+/// rather than to making the components wider than in `Long`.
+#[derive(Debug, Clone)]
+pub enum Length {
+    /// A long date, typically spelled-out, as in “January 1, 2000”.
+    Long,
+    /// A medium-sized date; typically abbreviated, as in “Jan. 1, 2000”.
+    Medium,
+    /// A short date; typically numeric, as in “1/1/2000”.
+    Short,
+}
+
+#[derive(Debug)]
+pub struct YearMonthSkeleton {
+    pub length: Length,
+}
+
+impl<C> NeoSkeleton<C> for YearMonthSkeleton where C: CldrCalendar {
+    // Data to include
+    type YearNamesV1Marker = C::YearNamesV1Marker;
+    type MonthNamesV1Marker = C::MonthNamesV1Marker;
+    type DateSkeletonPatternsV1Marker = DateSkeletonPatternsV1Marker;
+
+    // Data to exclude
+    type WeekdayNamesV1Marker = NeverMarker<LinearNamesV1<'static>>;
+    type DayPeriodNamesV1Marker = NeverMarker<LinearNamesV1<'static>>;
+    type TimeSkeletonPatternsV1Marker = NeverMarker<PackedSkeletonDataV1<'static>>;
+    type DateTimePatternV1Marker = NeverMarker<DateTimePatternV1<'static>>;
+    type DateTimeSkeletonPatternsV1Marker = NeverMarker<DateTimeSkeletonsV1<'static>>;
 }
