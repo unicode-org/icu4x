1a754c711b186b52eae3938281c2c21828fd486b
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-03-15 14:24:37 +0100

    implement the to_write function (halfway)

--- components/experimental/src/dimension/format.rs
+++ components/experimental/src/dimension/format.rs
@@ -3,11 +3,19 @@
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
 use fixed_decimal::FixedDecimal;
+use icu_pattern::{Pattern, PlaceholderValueProvider};
+use tinystr::tinystr;
 use writeable::Writeable;
+use zerovec::{maps::ZeroVecLike, ule::AsULE};
 
-use crate::dimension::{options::CurrencyFormatterOptions, provider::currency::CurrencyEssentialsV1};
+use crate::dimension::{
+    options::CurrencyFormatterOptions, provider::currency::CurrencyEssentialsV1,
+};
 
-use super::CurrencyCode;
+use super::{
+    provider::currency::{self, CurrencyPatterns, PlaceholderValue},
+    CurrencyCode,
+};
 
 pub struct FormattedCurrency<'l> {
     pub(crate) value: &'l FixedDecimal,
@@ -21,6 +29,58 @@ impl<'l> Writeable for FormattedCurrency<'l> {
     where
         W: core::fmt::Write + ?Sized,
     {
-        todo!("Implement FormattedCurrency::write_to")
+        let pattern = CurrencyPatterns::from_unaligned(
+            *self
+                .essential
+                .currency_patterns_map
+                .get(&self.currency_code.0.to_unvalidated())
+                .unwrap_or(&self.essential.default_pattern.to_unaligned()),
+        );
+
+        let placeholder = match self.options.width {
+            super::options::Width::Short => match pattern.short_place_holder_index {
+                Some(currency::PlaceholderValue::Index(index)) => self
+                    .essential
+                    .place_holders
+                    .get(index.into())
+                    .ok_or(core::fmt::Error)?,
+                Some(currency::PlaceholderValue::ISO) => self.currency_code.0.to_string().as_str(),
+                None => self.currency_code.0.to_string().as_str(),
+            },
+            super::options::Width::Narrow => match pattern.narrow_place_holder_index {
+                Some(currency::PlaceholderValue::Index(index)) => self
+                    .essential
+                    .place_holders
+                    .get(index.into())
+                    .ok_or(core::fmt::Error)?,
+                Some(currency::PlaceholderValue::ISO) => self.currency_code.0.to_string().as_str(),
+                None => self.currency_code.0.to_string().as_str(),
+            },
+        };
+
+        let pattern = match self.options.width {
+            super::options::Width::Short => match pattern.short_pattern_standard {
+                currency::PatternSelection::Standard => {
+                    self.essential.standard_pattern.ok_or(core::fmt::Error)?
+                }
+                currency::PatternSelection::StandardAlphaNextToNumber => self
+                    .essential
+                    .standard_alpha_next_to_number_pattern
+                    .ok_or(core::fmt::Error)?,
+            },
+            super::options::Width::Narrow => match pattern.narrow_pattern_standard {
+                currency::PatternSelection::Standard => {
+                    self.essential.standard_pattern.ok_or(core::fmt::Error)?
+                }
+                currency::PatternSelection::StandardAlphaNextToNumber => self
+                    .essential
+                    .standard_alpha_next_to_number_pattern
+                    .ok_or(core::fmt::Error)?,
+            },
+        };
+
+        // complete
+
+        Ok(())
     }
 }
