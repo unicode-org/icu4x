98c6c19f07c39ed5aa6e4bff58c6b3ce3688438c
Robert Bastian <robertbastian@unicode.org>
2024-03-26 16:34:38 +0100

    fmt

--- tools/ffi_coverage/src/allowlist.rs
+++ tools/ffi_coverage/src/allowlist.rs
@@ -344,7 +344,7 @@ lazy_static::lazy_static! {
         "icu::calendar::any_calendar::AnyCalendarKind",
         "icu::casemap::titlecase::TitlecaseMapper",
         "icu::calendar::types::Time::midnight",
-        
+
 
         // TODO-2.0 these errors will have changed
         "fixed_decimal::Error",
