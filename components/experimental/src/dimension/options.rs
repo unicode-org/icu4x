e676ce2752724b27da7d6b6e87f0ac51ae69f0f9
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-03-27 15:02:48 +0100

    fix doc

--- components/experimental/src/dimension/options.rs
+++ components/experimental/src/dimension/options.rs
@@ -2,7 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-//! Options for [`CurrencyFormatter`](crate::dimension::CurrencyFormatter).
+//! Options for [`CurrencyFormatter`](crate::dimension::formatter::CurrencyFormatter).
 
 /// A collection of configuration options that determine the formatting behavior of
 /// [`CurrencyFormatter`](crate::dimension::formatter::CurrencyFormatter).
