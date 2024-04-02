7b64ab9a3ccb3b08c10a9ef94012495537528260
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-04-02 16:36:46 +0200

    start

--- ffi/capi/src/units_converter.rs
+++ ffi/capi/src/units_converter.rs
@@ -4,5 +4,37 @@
 
 #[diplomat::bridge]
 pub mod ffi {
+    use alloc::boxed::Box;
 
+    #[diplomat::opaque]
+    /// An ICU4X Units Converter Factory object, capable of creating converters a [`ICU4XUnitsConverter`]
+    /// from [`ICU4XMeasureUnit`]s.
+    /// Also, it can parse the CLDR unit identifier (e.g. `meter-per-square-second`) and get the [`ICU4XMeasureUnit`].
+    #[diplomat::rust_link(icu::experimental::units::converter_factory::ConverterFactory, Struct)]
+    #[diplomat::rust_link(icu::experimental::units::converter_factory::ConverterFactory, Struct, hidden)]
+    pub struct ICU4XUnitsConverterFactory(pub ConverterFactory);
+
+    impl ICU4XUnitsConverterFactory {
+        /// Creates a new [`ICU4XUnitsConverterFactory`] from supplemental data
+        // (TODO: is it locale data? or something else?)
+        #[diplomat::rust_link(
+            icu::experimental::units::converter_factory::ConverterFactory::new,
+            FnInStruct
+        )]
+        pub fn create() -> Result<Box<ICU4XUnitsConverterFactory>, ICU4XError> {
+            Ok(Box::new(ICU4XUnitsConverterFactory(call_constructor!(
+                ConverterFactory::new,
+                ConverterFactory::try_new_with_any_provider,
+                ConverterFactory::try_new_with_buffer_provider,
+            )?)))
+        }
+
+        pub fn converter(
+            &self,
+            input_unit: &ICU4XMeasureUnit,
+            output_unit: &ICU4XMeasureUnit,
+        ) -> Option<Box<ICU4XUnitsConverter>> {
+            self.0.converter(input_unit, output_unit)
+        }
+    }
 }
