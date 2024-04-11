5fe50ec16452dc5eb1848d86247fbcfe6757d594
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-04-11 03:31:47 +0200

    complete the FFI of units

--- ffi/capi/src/units_converter.rs
+++ ffi/capi/src/units_converter.rs
@@ -29,12 +29,30 @@ pub mod ffi {
             )?)))
         }
 
+        /// Creates a new [`ICU4XUnitsConverter`] from the input and output `ICU4XMeasureUnit`s.
         pub fn converter(
             &self,
             input_unit: &ICU4XMeasureUnit,
             output_unit: &ICU4XMeasureUnit,
         ) -> Option<Box<ICU4XUnitsConverter>> {
-            self.0.converter(input_unit, output_unit)
+            self.0.converter(input_unit.0, output_unit.0)
+        }
+
+        /// Parses the CLDR unit identifier (e.g. `meter-per-square-second`) and returns the corresponding [`ICU4XMeasureUnit`].
+        pub fn parse(&self, unit_id: &str) -> Result<ICU4XMeasureUnit, ICU4XError> {
+            Ok(ICU4XMeasureUnit(call_method!(self.0, parse, unit_id)?))
         }
     }
+
+    #[diplomat::opaque]
+    /// An ICU4X Measurement Unit object which represents a single unit of measurement such as `meter`, `second`, `kilometer-per-hour`, `square-meter`, etc.
+    #[diplomat::rust_link(icu::experimental::units::measureunit::MeasureUnit, Struct)]
+    #[diplomat::rust_link(icu::experimental::units::measureunit::MeasureUnit, Struct, hidden)]
+    pub struct ICU4XMeasureUnit(pub ConverterFactory);
+
+    #[diplomat::opaque]
+    /// An ICU4X Units Converter object, capable of converting between two [`ICU4XMeasureUnit`]s.
+    #[diplomat::rust_link(icu::experimental::units::converter::UnitsConverter, Struct)]
+    #[diplomat::rust_link(icu::experimental::units::converter::UnitsConverter, Struct, hidden)]
+    pub struct ICU4XUnitsConverter(pub UnitsConverter);
 }
