6df291fa313e992c09adbd107c31590ae0a0a5e4
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-04-17 04:57:47 +0200

    Implement IcuRatio to represent big numbers. (#4799)

--- components/experimental/src/units/converter.rs
+++ components/experimental/src/units/converter.rs
@@ -2,8 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use num_bigint::BigInt;
-use num_rational::Ratio;
+use crate::units::ratio::IcuRatio;
 
 /// A converter for converting between two single or compound units.
 /// For example:
@@ -18,7 +17,7 @@ pub struct UnitsConverter(pub(crate) UnitsConverterInner);
 
 impl UnitsConverter {
     /// Converts the given value from the input unit to the output unit.
-    pub fn convert(&self, value: &Ratio<BigInt>) -> Ratio<BigInt> {
+    pub fn convert(&self, value: &IcuRatio) -> IcuRatio {
         self.0.convert(value)
     }
 }
@@ -36,7 +35,7 @@ pub(crate) enum UnitsConverterInner {
 
 impl UnitsConverterInner {
     /// Converts the given value from the input unit to the output unit based on the inner converter type.
-    fn convert(&self, value: &Ratio<BigInt>) -> Ratio<BigInt> {
+    fn convert(&self, value: &IcuRatio) -> IcuRatio {
         match self {
             UnitsConverterInner::Proportional(converter) => converter.convert(value),
             UnitsConverterInner::Reciprocal(converter) => converter.convert(value),
@@ -56,7 +55,7 @@ pub(crate) struct ReciprocalConverter {
 
 impl ReciprocalConverter {
     /// Converts the given value from the input unit to the output unit.
-    pub(crate) fn convert(&self, value: &Ratio<BigInt>) -> Ratio<BigInt> {
+    pub(crate) fn convert(&self, value: &IcuRatio) -> IcuRatio {
         self.proportional.convert(value).recip()
     }
 }
@@ -68,14 +67,13 @@ pub(crate) struct OffsetConverter {
     pub(crate) proportional: ProportionalConverter,
 
     /// The offset value to be added to the result of the proportional converter.
-    pub(crate) offset: Ratio<BigInt>,
+    pub(crate) offset: IcuRatio,
 }
 
 impl OffsetConverter {
     /// Converts the given value from the input unit to the output unit.
-    pub(crate) fn convert(&self, value: &Ratio<BigInt>) -> Ratio<BigInt> {
-        let result = self.proportional.convert(value);
-        result + &self.offset
+    pub(crate) fn convert(&self, value: &IcuRatio) -> IcuRatio {
+        &self.proportional.convert(value) + &self.offset
     }
 }
 
@@ -89,14 +87,13 @@ impl OffsetConverter {
 /// Also, it cannot convert between two units that are not single, such as `meter` to `foot-and-inch`.
 #[derive(Debug)]
 pub(crate) struct ProportionalConverter {
-    // TODO(#4554): Implement a New Struct `IcuRatio` to Encapsulate `Ratio<BigInt>`.
     /// The conversion rate between the input and output units.
-    pub(crate) conversion_rate: Ratio<BigInt>,
+    pub(crate) conversion_rate: IcuRatio,
 }
 
 impl ProportionalConverter {
     /// Converts the given value from the input unit to the output unit.
-    pub fn convert(&self, value: &Ratio<BigInt>) -> Ratio<BigInt> {
-        value * &self.conversion_rate
+    pub fn convert(&self, value: &IcuRatio) -> IcuRatio {
+        &self.conversion_rate * value
     }
 }
