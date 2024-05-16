c76757e1accc0b151d080448b065ecd716b18218
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2024-05-16 09:31:58 +0200

    Fix docs test that uses networking (#4882)

--- provider/blob/src/export/mod.rs
+++ provider/blob/src/export/mod.rs
@@ -22,7 +22,7 @@
 //! // Export something
 //! DatagenDriver::new()
 //!     .with_keys([icu_provider::hello_world::HelloWorldV1Marker::KEY])
-//!     .with_all_locales()
+//!     .with_locales_and_fallback([LocaleFamily::FULL], Default::default())
 //!     .export(&icu_provider::hello_world::HelloWorldProvider, exporter)
 //!     .unwrap();
 //!
