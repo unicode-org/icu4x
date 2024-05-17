6641d88237a8854d2e7681bfad7a71d8795fe184
Robert Bastian <robertbastian@unicode.org>
2024-05-17 10:28:29 +0200

    gen

--- provider/baked/plurals/src/lib.rs
+++ provider/baked/plurals/src/lib.rs
@@ -4,7 +4,7 @@
 
 //! Data for the `icu_plurals` crate
 //!
-//! This data was generated with CLDR version 45.0.0, ICU version release-75-1, and
+//! This data was generated with CLDR version 45.0.0, ICU version icu4x/2024-05-16/75.x, and
 //! LSTM segmenter version v0.1.0.
 
 #![no_std]
