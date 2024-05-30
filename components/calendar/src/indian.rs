62b3d9446098cae344179e244c50a1ef70bb884e
Shane F. Carr <shane@unicode.org>
2024-05-29 18:18:46 -0700

    Add non-optional `AnyCalendarKind` getter to `IntoAnyCalendar`

--- components/calendar/src/indian.rs
+++ components/calendar/src/indian.rs
@@ -31,7 +31,6 @@
 //! assert_eq!(datetime_indian.time.second.number(), 0);
 //! ```
 
-use crate::any_calendar::AnyCalendarKind;
 use crate::calendar_arithmetic::{ArithmeticDate, CalendarArithmetic};
 use crate::iso::Iso;
 use crate::{types, Calendar, CalendarError, Date, DateDuration, DateDurationUnit, DateTime, Time};
@@ -240,8 +239,8 @@ impl Calendar for Indian {
         "Indian"
     }
 
-    fn any_calendar_kind(&self) -> Option<AnyCalendarKind> {
-        Some(AnyCalendarKind::Indian)
+    fn any_calendar_kind(&self) -> Option<crate::AnyCalendarKind> {
+        Some(crate::any_calendar::IntoAnyCalendar::kind(self))
     }
 }
 
