09a720621e6033bb2517bf3cdcd13c17a6118649
Kartavya Vashishtha <sendtokartavya@gmail.com>
2024-05-30 19:54:18 -0400

    fmt(duration)

--- components/experimental/src/duration/duration.rs
+++ components/experimental/src/duration/duration.rs
@@ -1,25 +1,25 @@
 #[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
 /// Represents a duration of time.
 pub struct Duration {
-	/// The number of years in the duration.
+    /// The number of years in the duration.
     pub years: u64,
-	/// The number of months in the duration.
+    /// The number of months in the duration.
     pub months: u64,
-	/// The number of weeks in the duration.
+    /// The number of weeks in the duration.
     pub weeks: u64,
-	/// The number of days in the duration.
+    /// The number of days in the duration.
     pub days: u64,
-	/// The number of hours in the duration.
+    /// The number of hours in the duration.
     pub hours: u64,
-	/// The number of minutes in the duration.
+    /// The number of minutes in the duration.
     pub minutes: u64,
-	/// The number of seconds in the duration.
+    /// The number of seconds in the duration.
     pub seconds: u64,
-	/// The number of milliseconds in the duration.
+    /// The number of milliseconds in the duration.
     pub milliseconds: u64,
-	/// The number of microseconds in the duration.
+    /// The number of microseconds in the duration.
     pub microseconds: u64,
-	/// The number of nanoseconds in the duration.
+    /// The number of nanoseconds in the duration.
     pub nanoseconds: u64,
 }
 
