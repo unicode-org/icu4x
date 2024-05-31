62337607c48626d0e376d081949bd6bea3e44b16
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-05-31 02:17:08 +0200

    start units provider

--- components/experimental/src/dimension/provider/units.rs
+++ components/experimental/src/dimension/provider/units.rs
@@ -9,10 +9,61 @@
 //!
 //! Read more about data providers: [`icu_provider`]
 
-
 use alloc::borrow::Cow;
 use icu_provider::prelude::*;
 use tinystr::UnvalidatedTinyAsciiStr;
-use zerovec::{VarZeroVec, ZeroMap};
+use zerovec::{VarZeroVec, ZeroMap2d};
 
+// TODO: implement the units provider
+// thoughts:
+//   use the category (e.g. length, temperature, etc.) as secondary key
+//   have four maps:
+//     - one for the long width patterns (e.g. "{0} meter")
+//     - one for the short width (e.g. "{0} m"?)
+//     - one for the narrow width (e.g. "{0} m")
+//     - one for the DisplayNames (e.g. "meter")
 
+#[derive(Debug)]
+pub struct UnitsProvider<'data> {
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    long_width: ZeroMap2d<'data, str, Count, str>,
+
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    short_width: ZeroMap2d<'data, str, Count, str>,
+
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    narrow_width: ZeroMap2d<'data, str, Count, str>,
+}
+
+// TODO: revise this.
+/// A CLDR plural keyword, or the explicit value 1.
+/// See <https://www.unicode.org/reports/tr35/tr35-numbers.html#Language_Plural_Rules>.
+#[zerovec::make_ule(CountULE)]
+#[zerovec::derive(Debug)]
+#[derive(Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize))]
+#[cfg_attr(
+    feature = "datagen", 
+    derive(serde::Serialize, databake::Bake),
+    databake(path = icu_experimental::compactdecimal::provider)
+)]
+#[repr(u8)]
+pub enum Count {
+    /// The CLDR keyword `zero`.
+    Zero = 0,
+    /// The CLDR keyword `one`.
+    One = 1,
+    /// The CLDR keyword `two`.
+    Two = 2,
+    /// The CLDR keyword `few`.
+    Few = 3,
+    /// The CLDR keyword `many`.
+    Many = 4,
+    /// The CLDR keyword `other`.
+    Other = 5,
+    /// The explicit 1 case, see <https://www.unicode.org/reports/tr35/tr35-numbers.html#Explicit_0_1_rules>.
+    Explicit1 = 6,
+    // TODO: revise this
+    // NOTE(egg): No explicit 0, because the compact decimal pattern selection
+    // algorithm does not allow such a thing to arise.
+}
