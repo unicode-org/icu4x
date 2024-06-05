f90e2928f9ef65e400b83d7c490024eea2aed361
Robert Bastian <robertbastian@unicode.org>
2024-06-05 22:41:02 +0200

    comments

--- components/experimental/tests/transliterate/data/baked/macros.rs
+++ components/experimental/tests/transliterate/data/baked/macros.rs
@@ -16,9 +16,8 @@ macro_rules! __make_provider {
     ($ name : ty) => {
         #[clippy::msrv = "1.70"]
         impl $name {
-            #[doc(hidden)]
             #[allow(dead_code)]
-            pub const MUST_USE_MAKE_PROVIDER_MACRO: () = ();
+            pub(crate) const MUST_USE_MAKE_PROVIDER_MACRO: () = ();
         }
         icu_provider::impl_data_provider_never_marker!($name);
     };
@@ -26,12 +25,33 @@ macro_rules! __make_provider {
 #[doc(inline)]
 pub use __make_provider as make_provider;
 #[macro_use]
-#[path = "macros/transliterator_rules_v1.rs.data"]
-mod transliterator_rules_v1;
+#[path = "macros/normalizer_comp_v1.rs.data"]
+mod normalizer_comp_v1;
 #[doc(inline)]
-pub use __impl_transliterator_rules_v1 as impl_transliterator_rules_v1;
+pub use __impl_normalizer_comp_v1 as impl_normalizer_comp_v1;
 #[doc(inline)]
-pub use __impliterable_transliterator_rules_v1 as impliterable_transliterator_rules_v1;
+pub use __impliterable_normalizer_comp_v1 as impliterable_normalizer_comp_v1;
+#[macro_use]
+#[path = "macros/normalizer_decomp_v1.rs.data"]
+mod normalizer_decomp_v1;
+#[doc(inline)]
+pub use __impl_normalizer_decomp_v1 as impl_normalizer_decomp_v1;
+#[doc(inline)]
+pub use __impliterable_normalizer_decomp_v1 as impliterable_normalizer_decomp_v1;
+#[macro_use]
+#[path = "macros/normalizer_nfd_v1.rs.data"]
+mod normalizer_nfd_v1;
+#[doc(inline)]
+pub use __impl_normalizer_nfd_v1 as impl_normalizer_nfd_v1;
+#[doc(inline)]
+pub use __impliterable_normalizer_nfd_v1 as impliterable_normalizer_nfd_v1;
+#[macro_use]
+#[path = "macros/normalizer_nfdex_v1.rs.data"]
+mod normalizer_nfdex_v1;
+#[doc(inline)]
+pub use __impl_normalizer_nfdex_v1 as impl_normalizer_nfdex_v1;
+#[doc(inline)]
+pub use __impliterable_normalizer_nfdex_v1 as impliterable_normalizer_nfdex_v1;
 #[macro_use]
 #[path = "macros/normalizer_nfkd_v1.rs.data"]
 mod normalizer_nfkd_v1;
@@ -47,13 +67,6 @@ pub use __impl_normalizer_nfkdex_v1 as impl_normalizer_nfkdex_v1;
 #[doc(inline)]
 pub use __impliterable_normalizer_nfkdex_v1 as impliterable_normalizer_nfkdex_v1;
 #[macro_use]
-#[path = "macros/normalizer_comp_v1.rs.data"]
-mod normalizer_comp_v1;
-#[doc(inline)]
-pub use __impl_normalizer_comp_v1 as impl_normalizer_comp_v1;
-#[doc(inline)]
-pub use __impliterable_normalizer_comp_v1 as impliterable_normalizer_comp_v1;
-#[macro_use]
 #[path = "macros/normalizer_uts46d_v1.rs.data"]
 mod normalizer_uts46d_v1;
 #[doc(inline)]
@@ -61,23 +74,9 @@ pub use __impl_normalizer_uts46d_v1 as impl_normalizer_uts46d_v1;
 #[doc(inline)]
 pub use __impliterable_normalizer_uts46d_v1 as impliterable_normalizer_uts46d_v1;
 #[macro_use]
-#[path = "macros/normalizer_decomp_v1.rs.data"]
-mod normalizer_decomp_v1;
+#[path = "macros/transliterator_rules_v1.rs.data"]
+mod transliterator_rules_v1;
 #[doc(inline)]
-pub use __impl_normalizer_decomp_v1 as impl_normalizer_decomp_v1;
+pub use __impl_transliterator_rules_v1 as impl_transliterator_rules_v1;
 #[doc(inline)]
-pub use __impliterable_normalizer_decomp_v1 as impliterable_normalizer_decomp_v1;
-#[macro_use]
-#[path = "macros/normalizer_nfdex_v1.rs.data"]
-mod normalizer_nfdex_v1;
-#[doc(inline)]
-pub use __impl_normalizer_nfdex_v1 as impl_normalizer_nfdex_v1;
-#[doc(inline)]
-pub use __impliterable_normalizer_nfdex_v1 as impliterable_normalizer_nfdex_v1;
-#[macro_use]
-#[path = "macros/normalizer_nfd_v1.rs.data"]
-mod normalizer_nfd_v1;
-#[doc(inline)]
-pub use __impl_normalizer_nfd_v1 as impl_normalizer_nfd_v1;
-#[doc(inline)]
-pub use __impliterable_normalizer_nfd_v1 as impliterable_normalizer_nfd_v1;
+pub use __impliterable_transliterator_rules_v1 as impliterable_transliterator_rules_v1;
