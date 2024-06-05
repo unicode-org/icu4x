ae22738f750100f9ae7eeffd8b6d34ee7c69d62c
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-06-05 18:46:57 +0200

    fix the message

--- provider/datagen/src/provider/tests/make_testdata.rs
+++ provider/datagen/src/provider/tests/make_testdata.rs
@@ -302,7 +302,7 @@ impl<F: Write + Send + Sync> DataExporter for PostcardTestingExporter<F> {
         assert!(transient_violations == EXPECTED_TRANSIENT_VIOLATIONS && violations == EXPECTED_VIOLATIONS,
             "Expected violations list does not match found violations!\n\
             If the new list is smaller, please update EXPECTED_VIOLATIONS in make-testdata.rs\n\
-            If it is bigger and that was unexpected, please make sure the key remains zero-copy, or ask ICU4X team members if it is okay\
+            If it is bigger and that was unexpected, please make sure the key remains zero-copy, or ask ICU4X team members if it is okay \
             to temporarily allow for this key to be allowlisted.\n\
             Expected:\n{EXPECTED_VIOLATIONS:?}\nFound:\n{violations:?}\nExpected (transient):\n{EXPECTED_TRANSIENT_VIOLATIONS:?}\nFound (transient):\n{transient_violations:?}"
         );
