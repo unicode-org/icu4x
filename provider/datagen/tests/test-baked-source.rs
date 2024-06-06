caf36bafda8bf4e88a93a8b21ff9c286c068055c
Robert Bastian <robertbastian@unicode.org>
2024-06-06 18:07:51 +0200

    t

--- provider/datagen/tests/test-baked-source.rs
+++ provider/datagen/tests/test-baked-source.rs
@@ -4,12 +4,8 @@
 
 extern crate alloc;
 
-#[path = "testutil.rs"]
-mod testutil;
-
-use icu_datagen::prelude::*;
-use icu_datetime::provider::neo::GregorianDateNeoSkeletonPatternsV1Marker;
-use icu_provider::make_exportable_provider;
+// Don't import anything, to assert that compiled data compiles without imports.
+// In particular, *do not* import `icu_provider::prelude::*`.
 
 struct Baked;
 
@@ -17,30 +13,35 @@ const _: () = {
     #[allow(unused_imports)]
     #[path = "data/baked/mod.rs"]
     mod baked_data;
-    mod icu {
-        pub use icu_datetime as datetime;
-    }
     baked_data::make_provider!(Baked);
-    baked_data::impl_datetime_patterns_gregory_skeleton_v1!(Baked);
-    baked_data::impliterable_datetime_patterns_gregory_skeleton_v1!(Baked);
+    baked_data::impl_core_helloworld_v1!(Baked);
+    baked_data::impliterable_core_helloworld_v1!(Baked);
 };
 
-make_exportable_provider!(Baked, [GregorianDateNeoSkeletonPatternsV1Marker,]);
+#[test]
+fn iter() {
+    use icu_provider::datagen::IterableDataProvider;
+    use icu_provider::hello_world::HelloWorldProvider;
+    assert_eq!(
+        Baked.supported_requests(),
+        HelloWorldProvider.supported_requests(),
+    )
+}
 
 #[test]
-fn test() {
-    let mut exporter = testutil::TestingExporter::default();
-    DatagenDriver::new()
-        .with_markers([GregorianDateNeoSkeletonPatternsV1Marker::INFO])
-        .with_locales_and_fallback([LocaleFamily::FULL], Default::default())
-        .export(&Baked, &mut exporter)
-        .unwrap();
-    let results = exporter.finish();
+fn load() {
+    use icu_provider::datagen::IterableDataProvider;
+    use icu_provider::hello_world::HelloWorldProvider;
+    use icu_provider::prelude::*;
 
-    // The test has mostly already passed by getting here successfully.
-    // Check only some basic properties of the exported data.
-    assert!(!results.is_empty());
-    assert!(results.len() > 10);
-    assert!(results.len() < 100);
-    assert!(results.values().next().unwrap().len() > 10);
+    for (locale, marker_attributes) in HelloWorldProvider.supported_requests().unwrap().iter() {
+        let req = DataRequest {
+            locale,
+            marker_attributes,
+            ..Default::default()
+        };
+        let expected = HelloWorldProvider.load(req).unwrap().payload;
+        let baked = Baked.load(req).unwrap().payload;
+        assert_eq!(baked, expected);
+    }
 }
