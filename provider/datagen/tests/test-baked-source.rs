ad40adc65929194ea8c5842924b5d27c5248dcd4
Robert Bastian <robertbastian@unicode.org>
2024-06-06 18:13:37 +0200

    s

--- provider/datagen/tests/test-baked-source.rs
+++ provider/datagen/tests/test-baked-source.rs
@@ -4,8 +4,8 @@
 
 extern crate alloc;
 
-// Don't import anything, to assert that compiled data compiles without imports.
-// In particular, *do not* import `icu_provider::prelude::*`.
+#[path = "testutil.rs"]
+mod testutil;
 
 struct Baked;
 
@@ -13,35 +13,10 @@ const _: () = {
     #[allow(unused_imports)]
     #[path = "data/baked/mod.rs"]
     mod baked_data;
-    baked_data::make_provider!(Baked);
-    baked_data::impl_core_helloworld_v1!(Baked);
-    baked_data::impliterable_core_helloworld_v1!(Baked);
-};
-
-#[test]
-fn iter() {
-    use icu_provider::datagen::IterableDataProvider;
-    use icu_provider::hello_world::HelloWorldProvider;
-    assert_eq!(
-        Baked.supported_requests(),
-        HelloWorldProvider.supported_requests(),
-    )
-}
-
-#[test]
-fn load() {
-    use icu_provider::datagen::IterableDataProvider;
-    use icu_provider::hello_world::HelloWorldProvider;
-    use icu_provider::prelude::*;
-
-    for (locale, marker_attributes) in HelloWorldProvider.supported_requests().unwrap().iter() {
-        let req = DataRequest {
-            locale,
-            marker_attributes,
-            ..Default::default()
-        };
-        let expected = HelloWorldProvider.load(req).unwrap().payload;
-        let baked = Baked.load(req).unwrap().payload;
-        assert_eq!(baked, expected);
+    mod icu {
+        pub use icu_datetime as datetime;
     }
-}
+    baked_data::make_provider!(Baked);
+    baked_data::impl_datetime_patterns_gregory_skeleton_v1!(Baked);
+    baked_data::impliterable_datetime_patterns_gregory_skeleton_v1!(Baked);
+};
