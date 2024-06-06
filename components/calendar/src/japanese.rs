ad65b6d854df00526e0659a47ba4e3d957a8162f
Shane F. Carr <shane@unicode.org>
2024-06-06 08:44:41 -0700

    Fix Japanese calendar year 0 (#5006)
    Fixes https://github.com/unicode-org/icu4x/issues/4968
    
    I'd like to make a release for this one because it's a one-line change
    and we're already working on a patch release.

--- components/calendar/src/japanese.rs
+++ components/calendar/src/japanese.rs
@@ -663,7 +663,7 @@ impl Japanese {
         // for japanext, meiji for japanese),
         // In such a case, we instead fall back to Gregorian era codes
         if date < start {
-            if date.year < 0 {
+            if date.year <= 0 {
                 (1 - date.year, tinystr!(16, "bce"))
             } else {
                 (date.year, tinystr!(16, "ce"))
@@ -824,7 +824,11 @@ mod tests {
         assert_eq!(
             date, reconstructed,
             "Failed to roundtrip with {era:?}, {year}, {month}, {day}"
-        )
+        );
+
+        // Extra coverage for https://github.com/unicode-org/icu4x/issues/4968
+        assert_eq!(reconstructed.year().era, era);
+        assert_eq!(reconstructed.year().number, year);
     }
 
     fn single_test_roundtrip_ext(
