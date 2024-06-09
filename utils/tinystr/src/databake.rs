963623f62647fb2241c081ab097f7666b29e40aa
Zibi Braniecki <zibi@amazon.com>
2024-06-09 00:44:56 -0700

    Add constructing TinyAsciiStr from utf16

--- utils/tinystr/src/databake.rs
+++ utils/tinystr/src/databake.rs
@@ -29,7 +29,7 @@ impl<const N: usize> databake::Bake for UnvalidatedTinyAsciiStr<N> {
                 let bytes = self.0.bake(env);
                 env.insert("tinystr");
                 databake::quote! {
-                    tinystr::UnvalidatedTinyAsciiStr::from_bytes_unchecked(*#bytes)
+                    tinystr::UnvalidatedTinyAsciiStr::from_utf8_unchecked(*#bytes)
                 }
             }
         }
@@ -44,5 +44,5 @@ fn test() {
 #[test]
 fn test_unvalidated() {
     test_bake!(UnvalidatedTinyAsciiStr<10>, const: crate::tinystr!(10usize, "foo").to_unvalidated(), tinystr);
-    test_bake!(UnvalidatedTinyAsciiStr<3>, const: crate::UnvalidatedTinyAsciiStr::from_bytes_unchecked(*b"AB\xCD"), tinystr);
+    test_bake!(UnvalidatedTinyAsciiStr<3>, const: crate::UnvalidatedTinyAsciiStr::from_utf8_unchecked(*b"AB\xCD"), tinystr);
 }
