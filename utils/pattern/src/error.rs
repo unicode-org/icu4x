0b7129603620ca9351071a33ea8f6fe6b6adaf1c
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-06-14 12:42:07 +0200

    fix doc

--- utils/pattern/src/error.rs
+++ utils/pattern/src/error.rs
@@ -13,7 +13,7 @@ pub enum PatternError {
     InvalidPlaceholder,
 }
 
-/// Error type for [`crate::Pattern::try_from_utf8`].
+/// Error type for [`crate::Pattern::try_from_bytes_store`].
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum PatternOrUtf8Error<E> {
