f26a103aa8536ba1f0221a8c50939ee978a41e5c
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-06-18 14:57:12 +0200

    IMPLEMENT

--- utils/pattern/src/implementations.rs
+++ utils/pattern/src/implementations.rs
@@ -2,9 +2,9 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use zerovec::{maps::ZeroMapKV, ule::VarULE, VarZeroSlice, VarZeroVec};
+use zerovec::{maps::ZeroMapKV, ule::VarULE, VarZeroSlice, VarZeroVec, ZeroVecError};
 
-use crate::{Pattern, SinglePlaceholder};
+use crate::{Pattern, SinglePlaceholder, SinglePlaceholderPattern};
 
 impl<'a> ZeroMapKV<'a> for Pattern<SinglePlaceholder, str> {
     type Container = VarZeroVec<'a, Pattern<SinglePlaceholder, str>>;
@@ -14,11 +14,13 @@ impl<'a> ZeroMapKV<'a> for Pattern<SinglePlaceholder, str> {
 }
 
 unsafe impl VarULE for Pattern<SinglePlaceholder, str> {
-    fn validate_byte_slice(_bytes: &[u8]) -> Result<(), zerovec::ZeroVecError> {
-        todo!()
+    fn validate_byte_slice(bytes: &[u8]) -> Result<(), ZeroVecError> {
+        SinglePlaceholderPattern::try_from_bytes_store(bytes)
+            .map_err(|_| ZeroVecError::VarZeroVecFormatError)?;
+        Ok(())
     }
 
-    unsafe fn from_byte_slice_unchecked(_bytes: &[u8]) -> &Self {
-        todo!()
+    unsafe fn from_byte_slice_unchecked(bytes: &[u8]) -> &Self {
+        &SinglePlaceholderPattern::try_from_bytes_store(bytes).unwrap()
     }
 }
