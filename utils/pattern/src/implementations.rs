7a62ba43bdacb574ac14826f6905d1613ff1e5b9
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-06-19 16:35:56 +0200

    use `<String>`

--- utils/pattern/src/implementations.rs
+++ utils/pattern/src/implementations.rs
@@ -7,14 +7,14 @@ use crate::{Pattern, SinglePlaceholder, SinglePlaceholderPattern};
 use zerovec::__zerovec_internal_reexport::boxed::Box;
 use zerovec::{maps::ZeroMapKV, ule::VarULE, VarZeroSlice, VarZeroVec, ZeroVecError};
 
-impl<'a> ZeroMapKV<'a> for Pattern<SinglePlaceholder, str> {
-    type Container = VarZeroVec<'a, Pattern<SinglePlaceholder, str>>;
-    type Slice = VarZeroSlice<Pattern<SinglePlaceholder, str>>;
-    type GetType = Pattern<SinglePlaceholder, str>;
-    type OwnedType = Box<Pattern<SinglePlaceholder, str>>;
+impl<'a> ZeroMapKV<'a> for Pattern<SinglePlaceholder, String> {
+    type Container = VarZeroVec<'a, Pattern<SinglePlaceholder, String>>;
+    type Slice = VarZeroSlice<Pattern<SinglePlaceholder, String>>;
+    type GetType = Pattern<SinglePlaceholder, String>;
+    type OwnedType = Box<Pattern<SinglePlaceholder, String>>;
 }
 
-unsafe impl VarULE for Pattern<SinglePlaceholder, str> {
+unsafe impl VarULE for Pattern<SinglePlaceholder, String> {
     fn validate_byte_slice(bytes: &[u8]) -> Result<(), ZeroVecError> {
         SinglePlaceholderPattern::try_from_bytes_store(bytes)
             .map_err(|_| ZeroVecError::VarZeroVecFormatError)?;
@@ -23,7 +23,9 @@ unsafe impl VarULE for Pattern<SinglePlaceholder, str> {
 
     unsafe fn from_byte_slice_unchecked(bytes: &[u8]) -> &Self {
         // SAFETY: The `bytes` slice must be validated by `Self::validate_byte_slice`.
-        let store = core::str::from_utf8_unchecked(bytes);
-        SinglePlaceholderPattern::from_borrowed_store_unchecked(store)
+        let store = core::str::from_utf8_unchecked(bytes).to_owned();
+        Box::leak(Box::new(SinglePlaceholderPattern::from_store_unchecked(
+            store,
+        )))
     }
 }
