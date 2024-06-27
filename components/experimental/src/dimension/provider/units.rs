affdab29d33f5c3fb98cefdc768af653c5f420a0
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-06-27 16:03:54 +0200

    fix

--- components/experimental/src/dimension/provider/units.rs
+++ components/experimental/src/dimension/provider/units.rs
@@ -12,16 +12,6 @@
 use icu_provider::prelude::*;
 use zerovec::ZeroMap;
 
-#[cfg(feature = "compiled_data")]
-/// Baked data
-///
-/// <div class="stab unstable">
-/// 🚧 This code is considered unstable; it may change at any time, in breaking or non-breaking ways,
-/// including in SemVer minor releases. In particular, the `DataProvider` implementations are only
-/// guaranteed to match with this version's `*_unstable` providers. Use with caution.
-/// </div>
-pub use crate::provider::Baked;
-
 #[icu_provider::data_struct(UnitsDisplayNameV1Marker = "units/displaynames@1")]
 #[derive(Clone, PartialEq, Debug)]
 #[cfg_attr(
