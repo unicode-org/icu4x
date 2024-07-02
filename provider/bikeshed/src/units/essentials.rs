eaa6c39da8b352f3464ccf7830aef82a7673c49c
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-07-02 12:37:28 +0200

    Add bakeddata

--- provider/bikeshed/src/units/essentials.rs
+++ provider/bikeshed/src/units/essentials.rs
@@ -19,9 +19,6 @@ use zerovec::ZeroMap;
 
 impl DataProvider<UnitsEssentialsV1Marker> for DatagenProvider {
     fn load(&self, req: DataRequest) -> Result<DataResponse<UnitsEssentialsV1Marker>, DataError> {
-        const BINARY_PREFIX: &str = "1024p";
-        const DECIMAL_PREFIX: &str = "10p";
-
         /// Fills the prefixes map with the SI prefixes (binary and decimal)
         ///
         /// # Errors
@@ -31,46 +28,50 @@ impl DataProvider<UnitsEssentialsV1Marker> for DatagenProvider {
         fn fill_si(
             data: &BTreeMap<String, Patterns>,
             prefixes_map: &mut BTreeMap<PatternKey, String>,
-            prefix_str: &str,
         ) -> Result<(), DataError> {
-            data.iter()
-                .filter(|(key, _)| key.starts_with(prefix_str))
-                .map(|(key, patterns)| {
-                    let trimmed_key = key.trim_start_matches(prefix_str);
+            const BINARY_PREFIX: &str = "1024p";
+            const DECIMAL_PREFIX: &str = "10p";
 
-                    let pattern_key_result = match prefix_str {
-                        BINARY_PREFIX => trimmed_key.parse::<u8>().map(PatternKey::Binary),
-                        DECIMAL_PREFIX => trimmed_key.parse::<i8>().map(PatternKey::Decimal),
-                        _ => {
-                            return Err(DataError::custom(
-                                "Invalid prefix: must be Binary or Decimal",
-                            )
-                            .with_debug_context(key))
-                        }
-                    }
-                    .map_err(|e| {
-                        DataError::custom("Failed to parse pattern key value")
-                            .with_debug_context(&format!("Key: {}, Error: {}", key, e))
-                    });
+            for prefix_str in [BINARY_PREFIX, DECIMAL_PREFIX].iter() {
+                data.iter()
+                    .filter(|(key, _)| key.starts_with(prefix_str))
+                    .map(|(key, patterns)| {
+                        let trimmed_key = key.trim_start_matches(prefix_str);
 
-                    pattern_key_result.map(|pattern_key| (pattern_key, patterns))
-                })
-                .try_for_each(|elem| match elem {
-                    Ok((key, patterns)) => {
-                        if let Some(pattern) = patterns.unit_prefix_pattern.as_ref() {
-                            prefixes_map.insert(key, pattern.to_string());
-                            Ok(())
-                        } else {
-                            Err(DataError::custom("Failed to get pattern").with_debug_context(&key))
+                        let pattern_key_result = match *prefix_str {
+                            BINARY_PREFIX => trimmed_key.parse::<u8>().map(PatternKey::Binary),
+                            DECIMAL_PREFIX => trimmed_key.parse::<i8>().map(PatternKey::Decimal),
+                            _ => {
+                                return Err(DataError::custom(
+                                    "Invalid prefix: must be Binary or Decimal",
+                                )
+                                .with_debug_context(key))
+                            }
                         }
-                    }
-                    Err(e) => Err(e),
-                })?;
+                        .map_err(|e| {
+                            DataError::custom("Failed to parse pattern key value")
+                                .with_debug_context(&format!("Key: {}, Error: {}", key, e))
+                        });
+
+                        pattern_key_result.map(|pattern_key| (pattern_key, patterns))
+                    })
+                    .try_for_each(|elem| match elem {
+                        Ok((key, patterns)) => {
+                            if let Some(pattern) = patterns.unit_prefix_pattern.as_ref() {
+                                prefixes_map.insert(key, pattern.to_string());
+                                Ok(())
+                            } else {
+                                Err(DataError::custom("Failed to get pattern")
+                                    .with_debug_context(&key))
+                            }
+                        }
+                        Err(e) => Err(e),
+                    })?;
+            }
 
             Ok(())
         }
 
-        
         self.check_req::<UnitsEssentialsV1Marker>(req)?;
 
         // Get units
@@ -148,8 +149,7 @@ impl DataProvider<UnitsEssentialsV1Marker> for DatagenProvider {
             });
         }
 
-        fill_si(length_data, &mut prefixes, BINARY_PREFIX)?;
-        fill_si(length_data, &mut prefixes, DECIMAL_PREFIX)?;
+        fill_si(length_data, &mut prefixes)?;
 
         let result = UnitsEssentialsV1 {
             per: per.into(),
