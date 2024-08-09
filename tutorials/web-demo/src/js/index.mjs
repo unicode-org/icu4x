3cc6535c1a4166ba62f594be4f717153a66a8297
Tyler K <tyly04@gmail.com>
2024-08-09 12:12:52 -0700

    Bootstrap CSS update

--- tutorials/web-demo/src/js/index.mjs
+++ tutorials/web-demo/src/js/index.mjs
@@ -1,7 +1,8 @@
 import { RenderInfo } from "icu4x/demo";
 
 Object.values(RenderInfo.termini).forEach((t) => {
-	let a = document.createElement("li");
+	let a = document.createElement("a");
+	a.classList.add("list-group-item", "list-group-item-action");
 	a.innerHTML = `<a href="template.html/?func=${t.funcName}">${t.funcName}</a>`;
 	document.getElementById("links").appendChild(a);
 });
