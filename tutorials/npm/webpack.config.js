737640079e50a75201ede480aa077cf48939c032
Tyler K <tyly04@gmail.com>
2024-08-13 20:40:43 -0700

    Cleaning up for PR

--- tutorials/npm/webpack.config.js
+++ tutorials/npm/webpack.config.js
@@ -1,69 +1,62 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default {
-  entry: {
-    index: [
-      './src/ts/app.ts',
-      './src/scss/styles.scss',
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: () => [
-                  require('autoprefixer')
-                ]
-              }
-            }
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    fallback: {
-      "fs": false,
-    },
-  },
-  mode: "production",
-  // mode: "development",
-  output: {
-    filename: 'bundle.js',
-    path: new URL('dist', import.meta.url).pathname,
-  },
-  devServer: {
-    static: '.',
-    port: 8080,
-    hot: true, 
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    }
-  },
-  experiments: {
-    topLevelAwait: true,
-  },
-  optimization: {
-    minimize: false
-  },
+	plugins: [new MiniCssExtractPlugin({
+		filename: "[name].css"
+	})],
+	entry: {
+	  index: [
+		'./src/js/index.mjs',
+		'./src/scss/styles.scss',
+	  ],
+	  rendering: [
+		'./src/js/runtime.mjs'
+	  ]
+	},
+	module: {
+	  rules: [
+		{
+			test: /\.(scss)$/,
+			use: [
+				MiniCssExtractPlugin.loader,
+				"css-loader",
+				{
+				loader: 'sass-loader'
+				}
+			]
+		}
+	  ]
+	},
+	resolve: {
+	  extensions: ['.mjs'],
+	  fallback: {
+		"fs": false,
+	  },
+	},
+	// mode: "production",
+	mode: "development",
+	output: {
+	  filename: '[name].bundle.js',
+	  path: path.resolve(__dirname, 'public/dist'),
+	},
+	devServer: {
+	  static: '.',
+	  port: 8080,
+	  hot: true, 
+	  headers: {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+		"Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+	  }
+	},
+	experiments: {
+	  topLevelAwait: true,
+	},
+	optimization: {
+	  minimize: false
+	},
 };
