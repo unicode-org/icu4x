abe2b2e48bf004591a3b467e5cb98f3bbea87888
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-08-15 14:44:34 +0200

    fix

--- components/experimental/src/dimension/provider/count.rs
+++ components/experimental/src/dimension/provider/count.rs
@@ -2,9 +2,9 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+/// Represents the plural rules count.
 #[zerovec::make_ule(CountULE)]
 #[zerovec::derive(Debug)]
-
 #[derive(Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize))]
 #[cfg_attr(
@@ -14,21 +14,10 @@
 )]
 #[repr(u8)]
 pub enum Count {
-    /// CompactPattern `zero`.
     Zero = 0,
-
-    /// CompactPattern `one`.
     One = 1,
-
-    /// CompactPattern `two`.
     Two = 2,
-
-    /// Compact Pattern `few`.
     Few = 3,
-
-    /// CompactPattern `many`.
     Many = 4,
-
-    /// CompactPattern `other`.
     Other = 5,
 }
