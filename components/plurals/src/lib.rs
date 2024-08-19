48d772ac4e0bf09abe57d0f6809c1a72da156928
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-08-19 23:20:14 +0200

    chore: Add databake support for plurals library

--- components/plurals/src/lib.rs
+++ components/plurals/src/lib.rs
@@ -144,7 +144,9 @@ pub enum PluralRuleType {
 /// assert_eq!(pr.category_for(5_usize), PluralCategory::Other);
 /// ```
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Ord, PartialOrd)]
-#[cfg_attr(feature = "datagen", derive(serde::Serialize))]
+#[cfg_attr(feature = "datagen", 
+    derive(serde::Serialize, databake::Bake),
+    databake(path = icu_plurals))]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize))]
 #[repr(u8)]
 #[zerovec::make_ule(PluralCategoryULE)]
