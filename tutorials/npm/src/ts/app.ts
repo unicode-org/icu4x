591a3bf05951293dc5b7c01ea07be008bf839db8
Tyler K <tyly04@gmail.com>
2024-08-19 15:39:10 -0700

    Adding NPM folder back

---
+++ tutorials/npm/src/ts/app.ts
@@ -0,0 +1,16 @@
+import { DataProvider } from 'icu4x';
+import * as fdf from './fixed-decimal';
+import * as dtf from './date-time';
+import * as seg from './segmenter';
+
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/collapse';
+
+(async function init() {
+    const dataProvider = DataProvider.create_compiled();
+    fdf.setup(dataProvider);
+    dtf.setup(dataProvider);
+    seg.setup(dataProvider);
+    (document.querySelector("#bigspinner") as HTMLElement).style.display = "none";
+})()
