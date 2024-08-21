99099fc82096107ec1fa75d16ee2242f50f3351e
Robert Bastian <robertbastian@unicode.org>
2024-08-21 12:01:09 +0200

    CurrencyPatternsDataV1

--- provider/source/src/currency/patterns.rs
+++ provider/source/src/currency/patterns.rs
@@ -9,9 +9,9 @@ use crate::SourceDataProvider;
 use std::collections::HashSet;
 
 use icu::experimental::dimension::provider::currency_patterns::*;
+use icu::experimental::relativetime::provider::PluralPattern;
 use icu_provider::prelude::*;
 use icu_provider::DataProvider;
-use zerovec::ZeroMap;
 
 impl DataProvider<CurrencyPatternsDataV1Marker> for SourceDataProvider {
     fn load(
@@ -23,12 +23,17 @@ impl DataProvider<CurrencyPatternsDataV1Marker> for SourceDataProvider {
             .numbers()
             .read_and_parse(req.id.locale, "numbers.json")?;
 
-        let default_system = numbers_resource
+        let mut default_system = numbers_resource
             .main
             .value
             .numbers
             .default_numbering_system
             .as_str();
+
+        // https://github.com/unicode-org/icu4x/issues/5374
+        if *req.id.locale == DataLocale::from(icu::locale::locale!("sd")) {
+            default_system = "latn";
+        }
 
         let currency_patterns = &numbers_resource
             .main
@@ -48,26 +53,23 @@ impl DataProvider<CurrencyPatternsDataV1Marker> for SourceDataProvider {
                 // TODO(#5334):
                 //      Before graduating the currency crate,
                 //      Check that the .json data files are completed and no need to fallback chain up to the root.
-                unit_patterns: ZeroMap::from_iter(
-                    [
-                        (PatternCount::Zero, patterns.pattern_zero.as_deref()),
-                        (PatternCount::One, patterns.pattern_one.as_deref()),
-                        (PatternCount::Two, patterns.pattern_two.as_deref()),
-                        (PatternCount::Few, patterns.pattern_few.as_deref()),
-                        (PatternCount::Many, patterns.pattern_many.as_deref()),
-                        (PatternCount::Other, patterns.pattern_other.as_deref()),
-                    ]
-                    .into_iter()
-                    .filter_map(|(count, pattern)| match (count, pattern) {
-                        (PatternCount::Other, pattern) => Some((count, pattern?)),
-                        // NOTE:
-                        //      According to [Unicode Technical Standard #35](https://unicode.org/reports/tr35/tr35-numbers.html),
-                        //      if a specific count is not available, the `other` pattern should be used.
-                        //      Therefore, if the pattern is equal to the `other` pattern, we should not include it in the map.
-                        (_, pattern) if pattern == patterns.pattern_other.as_deref() => None,
-                        _ => Some((count, pattern?)),
-                    }),
-                ),
+                patterns: PluralPattern::try_new(
+                    patterns.pattern_other.as_deref().ok_or_else(|| {
+                        DataError::custom("Missing patterns")
+                            .with_debug_context(currency_patterns)
+                            .with_debug_context(&req.id)
+                    })?,
+                    patterns.pattern_zero.as_deref(),
+                    patterns.pattern_one.as_deref(),
+                    patterns.pattern_two.as_deref(),
+                    patterns.pattern_few.as_deref(),
+                    patterns.pattern_many.as_deref(),
+                )
+                .map_err(|_| {
+                    DataError::custom("Invalid patterns")
+                        .with_debug_context(patterns)
+                        .with_debug_context(&req.id)
+                })?,
             }),
         })
     }
