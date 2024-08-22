869c776df78dc230806eb9a4263595efb73aa928
Shane F. Carr <shane@unicode.org>
2024-08-22 15:54:20 -0700

    cargo make diplomat-gen

--- ffi/capi/bindings/dart/DecomposingNormalizer.g.dart
+++ ffi/capi/bindings/dart/DecomposingNormalizer.g.dart
@@ -53,7 +53,7 @@ final class DecomposingNormalizer implements ffi.Finalizable {
   /// Ill-formed input is treated as if errors had been replaced with REPLACEMENT CHARACTERs according
   /// to the WHATWG Encoding Standard.
   ///
-  /// See the [Rust documentation for `normalize_utf8`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizer.html#method.normalize_utf8) for more information.
+  /// See the [Rust documentation for `normalize_utf8`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizerBorrowed.html#method.normalize_utf8) for more information.
   String normalize(String s) {
     final temp = _FinalizedArena();
     final write = _Write();
@@ -66,7 +66,7 @@ final class DecomposingNormalizer implements ffi.Finalizable {
   /// Ill-formed input is treated as if errors had been replaced with REPLACEMENT CHARACTERs according
   /// to the WHATWG Encoding Standard.
   ///
-  /// See the [Rust documentation for `is_normalized_utf8`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizer.html#method.is_normalized_utf8) for more information.
+  /// See the [Rust documentation for `is_normalized_utf8`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizerBorrowed.html#method.is_normalized_utf8) for more information.
   bool isNormalized(String s) {
     final temp = _FinalizedArena();
     final result = _icu4x_DecomposingNormalizer_is_normalized_mv1(_ffi, s._utf8AllocIn(temp.arena));
@@ -78,7 +78,7 @@ final class DecomposingNormalizer implements ffi.Finalizable {
   /// Ill-formed input is treated as if errors had been replaced with REPLACEMENT CHARACTERs according
   /// to the WHATWG Encoding Standard.
   ///
-  /// See the [Rust documentation for `is_normalized_utf16`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizer.html#method.is_normalized_utf16) for more information.
+  /// See the [Rust documentation for `is_normalized_utf16`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizerBorrowed.html#method.is_normalized_utf16) for more information.
   bool isNormalizedUtf16(String s) {
     final temp = _FinalizedArena();
     final result = _icu4x_DecomposingNormalizer_is_normalized_utf16_mv1(_ffi, s._utf16AllocIn(temp.arena));
@@ -87,7 +87,7 @@ final class DecomposingNormalizer implements ffi.Finalizable {
 
   /// Return the index a slice of potentially-invalid UTF-8 is normalized up to
   ///
-  /// See the [Rust documentation for `is_normalized_utf8_up_to`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizer.html#method.is_normalized_utf8_up_to) for more information.
+  /// See the [Rust documentation for `is_normalized_utf8_up_to`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizerBorrowed.html#method.is_normalized_utf8_up_to) for more information.
   int isNormalizedUpTo(String s) {
     final temp = _FinalizedArena();
     final result = _icu4x_DecomposingNormalizer_is_normalized_up_to_mv1(_ffi, s._utf8AllocIn(temp.arena));
@@ -96,7 +96,7 @@ final class DecomposingNormalizer implements ffi.Finalizable {
 
   /// Return the index a slice of potentially-invalid UTF-8 is normalized up to
   ///
-  /// See the [Rust documentation for `is_normalized_utf16_up_to`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizer.html#method.is_normalized_utf16_up_to) for more information.
+  /// See the [Rust documentation for `is_normalized_utf16_up_to`](https://docs.rs/icu/latest/icu/normalizer/struct.DecomposingNormalizerBorrowed.html#method.is_normalized_utf16_up_to) for more information.
   int isNormalizedUtf16UpTo(String s) {
     final temp = _FinalizedArena();
     final result = _icu4x_DecomposingNormalizer_is_normalized_utf16_up_to_mv1(_ffi, s._utf16AllocIn(temp.arena));
