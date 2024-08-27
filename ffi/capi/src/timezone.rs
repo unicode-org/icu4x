8f91908e39319ff49ff46d62243e506438c8fa8e
Shane F. Carr <shane@unicode.org>
2024-08-26 18:36:30 -0700

    features

--- ffi/capi/src/timezone.rs
+++ ffi/capi/src/timezone.rs
@@ -11,6 +11,7 @@ pub mod ffi {
 
     use crate::errors::ffi::TimeZoneInvalidIdError;
     use crate::errors::ffi::TimeZoneInvalidOffsetError;
+    #[cfg(feature = "compiled_data")]
     use crate::errors::ffi::TimeZoneUnknownError;
 
     #[diplomat::opaque]
