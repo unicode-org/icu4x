c3e6abcfb231521b1abe3519e3e27cc284e54fde
Shane F. Carr <shane@unicode.org>
2024-08-28 17:19:24 -0700

    Migrate datetime benches to semantic datetime (#5464)
    - Deletes benches whose only purpose is measuring Writeable characteristics

--- components/datetime/benches/fixtures/mod.rs
+++ components/datetime/benches/fixtures/mod.rs
@@ -2,7 +2,6 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use icu_datetime::options;
 use icu_datetime::options::DateTimeFormatterOptions;
 use serde::{Deserialize, Serialize};
 
@@ -22,15 +21,14 @@ pub struct TestInput {
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub enum TestOptions {
-    #[serde(rename = "length")]
-    Length(options::length::Bag),
-    #[serde(rename = "components")]
+pub struct TestOptions {
+    pub length: Option<icu_datetime::options::length::Bag>,
     #[cfg(feature = "experimental")]
-    Components(options::components::Bag),
-    #[serde(rename = "components")]
-    #[cfg(not(feature = "experimental"))]
-    Components(serde_json::Value),
+    pub components: Option<icu_datetime::options::components::Bag>,
+    #[cfg(feature = "experimental")]
+    pub semantic: Option<icu_datetime::neo_skeleton::NeoSkeleton>,
+    #[cfg(feature = "experimental")]
+    pub preferences: Option<icu_datetime::options::preferences::Bag>,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -56,11 +54,12 @@ pub struct PatternsFixture(pub Vec<String>);
 
 #[allow(dead_code)]
 pub fn get_options(input: &TestOptions) -> Option<DateTimeFormatterOptions> {
-    match input {
-        TestOptions::Length(bag) => Some((*bag).into()),
-        #[cfg(feature = "experimental")]
-        TestOptions::Components(bag) => Some((*bag).into()),
-        #[cfg(not(feature = "experimental"))]
-        TestOptions::Components(_) => None,
+    if let Some(bag) = input.length {
+        return Some(bag.into());
     }
+    #[cfg(feature = "experimental")]
+    if let Some(bag) = input.components {
+        return Some(bag.into());
+    }
+    None
 }
