df7f269aa5080a17ef05a9d6f6748a4e5c5321a8
Robert Bastian <robertbastian@unicode.org>
2024-09-18 11:37:29 +0200

    unstable annotation

--- components/properties/src/provider/props.rs
+++ components/properties/src/provider/props.rs
@@ -2,7 +2,13 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-//! Data defintions for CodePointMapData open enums.
+//! 🚧 \[Unstable\] Data defintions for CodePointMapData open enums.
+//!
+//! <div class="stab unstable">
+//! 🚧 This code is considered unstable; it may change at any time, in breaking or non-breaking ways,
+//! including in SemVer minor releases. While the serde representation of data structs is guaranteed
+//! to be stable, their Rust representation might not be. Use with caution.
+//! </div>
 
 /// Enumerated property Bidi_Class
 ///
