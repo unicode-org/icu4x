367035ac7ce2925cd64185e3c1252b3ba237be1a
Tyler K <ambiguousemails@proton.me>
2024-10-01 17:23:17 -0700

    Segmenter demo (#5624)
    Adds the segmenter to the demo files.

---
+++ ffi/capi/bindings/demo_gen/WordSegmenter.mjs
@@ -0,0 +1,64 @@
+import { lib } from "./index.mjs";
+
+export default {
+	"WordSegmenter.segment": {
+		func: (model, text) => {
+			let provider = lib.DataProvider.compiled();
+
+			var segmenter;
+			switch (model) {
+				case "Auto":
+					segmenter = lib.WordSegmenter.createAuto(provider);
+					break;
+				case "LSTM":
+					segmenter = lib.WordSegmenter.createLSTM(provider);
+					break;
+				case "Dictionary":
+					segmenter = lib.WordSegmenter.createDictionary(provider);
+			}
+
+			const iter = segmenter.segment(text);
+			const segments = [];
+			
+			let utf8Index = 0;
+			let utf16Index = 0;
+			while (true) {
+				const next = iter.next();
+
+				if (next === -1) {
+					segments.push(text.slice(utf16Index));
+					break;
+				} else {
+					const oldUtf16Index = utf16Index;
+					while (utf8Index < next) {
+						const codePoint = text.codePointAt(utf16Index);
+						const utf8Len = (codePoint <= 0x7F) ? 1
+							: (codePoint <= 0x7FF) ? 2
+							: (codePoint <= 0xFFFF) ? 3
+							: 4;
+						const utf16Len = (codePoint <= 0xFFFF) ? 1
+							: 2;
+						utf8Index += utf8Len;
+						utf16Index += utf16Len;
+					}
+					segments.push(text.slice(oldUtf16Index, utf16Index));
+				}
+			}
+			
+			return segments.join("<span class=\"seg-delim\"> . </span>");
+		},
+		funcName: "WordSegmenter.segment",
+		parameters: [
+			{
+				name: "Model Type (Auto, LSTM, or Dictionary)",
+				type: "string",
+				defaultValue: "Auto"
+			},
+			{
+				name: "Text",
+				type: "string",
+				defaultValue: "โดยที่การยอมรับนับถือเกียรติศักดิ์ประจำตัว และสิทธิเท่าเทียมกันและโอนมิได้ของบรรดา สมาชิก ทั้ง หลายแห่งครอบครัว มนุษย์เป็นหลักมูลเหตุแห่งอิสรภาพ ความยุติธรรม และสันติภาพในโลก"
+			}
+		]
+	}
+};
