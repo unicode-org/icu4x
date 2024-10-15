e79dd22280900b8f74eefa085a43d2e4bed44f89
Manish Goregaokar <manishsmail@gmail.com>
2024-10-15 15:01:23 -0700

    mention week_of subtlety

--- components/calendar/src/week_of.rs
+++ components/calendar/src/week_of.rs
@@ -13,6 +13,11 @@ use icu_provider::prelude::*;
 pub const MIN_UNIT_DAYS: u16 = 14;
 
 /// Calculator for week-of-month and week-of-year based on locale-specific configurations.
+///
+/// Note that things get subtly tricky for weeks that straddle the boundary between two years: different locales
+/// may consider them as belonging to the preceding or succeeding year based on where exactly the week gets split.
+/// While this struct can be populated by values supplied by the programmer, we do recommend using one of the
+/// constructors to get locale data.
 #[derive(Clone, Copy, Debug)]
 #[non_exhaustive]
 pub struct WeekCalculator {
