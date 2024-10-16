27ff10bd611e4dc2a9fb495279987bfaf9161849
Tyler K <tyly04@gmail.com>
2024-10-16 15:40:00 -0700

    Adjust segmenter demo to use utf16

--- tutorials/npm/src/ts/segmenter.ts
+++ tutorials/npm/src/ts/segmenter.ts
@@ -43,26 +43,20 @@ export class SegmenterDemo {
     #render(): void {
         const segments = [];
 
-        let utf8Index = 0;
         let utf16Index = 0;
-        const iter8 = this.#segmenter.segment(this.#text);
+        const iter16 = this.#segmenter.segment(this.#text);
         while (true) {
-            const next = iter8.next();
+            const next = iter16.next();
 
             if (next === -1) {
                 segments.push(this.#text.slice(utf16Index));
                 break;
             } else {
                 const oldUtf16Index = utf16Index;
-                while (utf8Index < next) {
+                while (utf16Index < next) {
                     const codePoint = this.#text.codePointAt(utf16Index);
-                    const utf8Len = (codePoint <= 0x7F) ? 1
-                        : (codePoint <= 0x7FF) ? 2
-                        : (codePoint <= 0xFFFF) ? 3
-                        : 4;
                     const utf16Len = (codePoint <= 0xFFFF) ? 1
                         : 2;
-                    utf8Index += utf8Len;
                     utf16Index += utf16Len;
                 }
                 segments.push(this.#text.slice(oldUtf16Index, utf16Index));
