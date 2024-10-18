545f3fdfdda144a5d711c54271310147fd5f5fa1
Robert Bastian <robertbastian@unicode.org>
2024-10-18 13:01:27 -0700

    wire up

--- components/experimental/tests/transliterate/data/provider.rs
+++ components/experimental/tests/transliterate/data/provider.rs
@@ -10,12 +10,5 @@ const _: () = {
         pub(super) use icu_collections as collections;
     }
     self::make_provider!(TestingProvider);
-    impl_canonical_compositions_v1_marker!(TestingProvider);
-    impl_non_recursive_decomposition_supplement_v1_marker!(TestingProvider);
-    impl_canonical_decomposition_data_v1_marker!(TestingProvider);
-    impl_canonical_decomposition_tables_v1_marker!(TestingProvider);
-    impl_compatibility_decomposition_supplement_v1_marker!(TestingProvider);
-    impl_compatibility_decomposition_tables_v1_marker!(TestingProvider);
-    impl_uts46_decomposition_supplement_v1_marker!(TestingProvider);
     impl_transliterator_rules_v1_marker!(TestingProvider);
 };
