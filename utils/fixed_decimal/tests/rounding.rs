6cd257bfea5aa14b9df9c93f170ba6f04d7761b0
Younies Mahmoud <younies.mahmoud@gmail.com>
2024-10-23 18:27:30 +0200

    fix rounding.rs

--- utils/fixed_decimal/tests/rounding.rs
+++ utils/fixed_decimal/tests/rounding.rs
@@ -4,7 +4,6 @@
 
 use core::ops::RangeInclusive;
 use fixed_decimal::RoundingMode;
-use fixed_decimal::Sign;
 use fixed_decimal::UnsignedFixedDecimal;
 use writeable::Writeable;
 
@@ -66,27 +65,21 @@ pub fn test_within_ranges() {
     struct TestCase {
         rounding_mode_name: &'static str,
         rounding_mode: RoundingMode,
-        range_n2000: RangeInclusive<i32>,
-        range_n1000: RangeInclusive<i32>,
-        range_0: RangeInclusive<i32>,
-        range_1000: RangeInclusive<i32>,
-        range_2000: RangeInclusive<i32>,
+        range_0: RangeInclusive<u32>,
+        range_1000: RangeInclusive<u32>,
+        range_2000: RangeInclusive<u32>,
     }
     let cases: [TestCase; 9] = [
         TestCase {
             rounding_mode_name: "ceil",
             rounding_mode: RoundingMode::Ceil,
-            range_n2000: -2999..=-2000,
-            range_n1000: -1999..=-1000,
-            range_0: -999..=0,
+            range_0: 0..=0,
             range_1000: 1..=1000,
             range_2000: 1001..=2000,
         },
         TestCase {
             rounding_mode_name: "floor",
             rounding_mode: RoundingMode::Floor,
-            range_n2000: -2000..=-1001,
-            range_n1000: -1000..=-1,
             range_0: 0..=999,
             range_1000: 1000..=1999,
             range_2000: 2000..=2999,
@@ -94,8 +87,6 @@ pub fn test_within_ranges() {
         TestCase {
             rounding_mode_name: "expand",
             rounding_mode: RoundingMode::Expand,
-            range_n2000: -2000..=-1001,
-            range_n1000: -1000..=-1,
             range_0: 0..=0,
             range_1000: 1..=1000,
             range_2000: 1001..=2000,
@@ -103,54 +94,42 @@ pub fn test_within_ranges() {
         TestCase {
             rounding_mode_name: "trunc",
             rounding_mode: RoundingMode::Trunc,
-            range_n2000: -2999..=-2000,
-            range_n1000: -1999..=-1000,
-            range_0: -999..=999,
+            range_0: 0..=999,
             range_1000: 1000..=1999,
             range_2000: 2000..=2999,
         },
         TestCase {
             rounding_mode_name: "half_ceil",
             rounding_mode: RoundingMode::HalfCeil,
-            range_n2000: -2500..=-1501,
-            range_n1000: -1500..=-501,
-            range_0: -500..=449,
+            range_0: 0..=449,
             range_1000: 500..=1449,
             range_2000: 1500..=2449,
         },
         TestCase {
             rounding_mode_name: "half_floor",
             rounding_mode: RoundingMode::HalfFloor,
-            range_n2000: -2449..=-1500,
-            range_n1000: -1449..=-500,
-            range_0: -449..=500,
+            range_0: 0..=500,
             range_1000: 501..=1500,
             range_2000: 1501..=2500,
         },
         TestCase {
             rounding_mode_name: "half_expand",
             rounding_mode: RoundingMode::HalfExpand,
-            range_n2000: -2449..=-1500,
-            range_n1000: -1449..=-500,
-            range_0: -449..=449,
+            range_0: 0..=449,
             range_1000: 500..=1449,
             range_2000: 1500..=2449,
         },
         TestCase {
             rounding_mode_name: "half_trunc",
             rounding_mode: RoundingMode::HalfTrunc,
-            range_n2000: -2500..=-1501,
-            range_n1000: -1500..=-501,
-            range_0: -500..=500,
+            range_0: 0..=500,
             range_1000: 501..=1500,
             range_2000: 1501..=2500,
         },
         TestCase {
             rounding_mode_name: "half_even",
             rounding_mode: RoundingMode::HalfEven,
-            range_n2000: -2500..=-1500,
-            range_n1000: -1449..=-501,
-            range_0: -500..=500,
+            range_0: 0..=500,
             range_1000: 501..=1449,
             range_2000: 1500..=2500,
         },
@@ -158,41 +137,14 @@ pub fn test_within_ranges() {
     for TestCase {
         rounding_mode_name,
         rounding_mode,
-        range_n2000,
-        range_n1000,
         range_0,
         range_1000,
         range_2000,
     } in cases
     {
-        for n in range_n2000 {
-            let mut fd = UnsignedFixedDecimal::from(n);
-            fd.round_with_mode(3, rounding_mode);
-            assert_eq!(fd.write_to_string(), "-2000", "{rounding_mode_name}: {n}");
-            let mut fd = UnsignedFixedDecimal::from(n - 1000000).multiplied_pow10(-5);
-            fd.round_with_mode(-2, rounding_mode);
-            assert_eq!(
-                fd.write_to_string(),
-                "-10.02",
-                "{rounding_mode_name}: {n} ÷ 10^5 ± 10"
-            );
-        }
-        for n in range_n1000 {
-            let mut fd = UnsignedFixedDecimal::from(n);
-            fd.round_with_mode(3, rounding_mode);
-            assert_eq!(fd.write_to_string(), "-1000", "{rounding_mode_name}: {n}");
-            let mut fd = UnsignedFixedDecimal::from(n - 1000000).multiplied_pow10(-5);
-            fd.round_with_mode(-2, rounding_mode);
-            assert_eq!(
-                fd.write_to_string(),
-                "-10.01",
-                "{rounding_mode_name}: {n} ÷ 10^5 ± 10"
-            );
-        }
         for n in range_0 {
             let mut fd = UnsignedFixedDecimal::from(n);
             fd.round_with_mode(3, rounding_mode);
-            fd.set_sign(Sign::None); // get rid of -0
             assert_eq!(fd.write_to_string(), "000", "{rounding_mode_name}: {n}");
             let (mut fd, expected) = if n < 0 {
                 (
