e8e81db50828c520ab13ab8763ff53b71d806b74
Shane F. Carr <shane@unicode.org>
2024-10-29 14:16:39 -0700

    Move neo.rs to neo/mod.rs; add comment

--- components/datetime/src/provider/pattern/mod.rs
+++ components/datetime/src/provider/pattern/mod.rs
@@ -2,6 +2,8 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+//! Internal structured pattern types.
+
 mod common;
 mod error;
 pub mod hour_cycle;
