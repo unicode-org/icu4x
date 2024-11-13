d7d46b65c9f511c59b314027e1c34835ab3502b0
Shane F. Carr <shane@unicode.org>
2024-11-13 14:26:43 -0800

    Decouple fixture components bag from library components bag; remove serde

--- components/datetime/tests/fixtures/mod.rs
+++ components/datetime/tests/fixtures/mod.rs
@@ -4,6 +4,7 @@
 
 #![cfg(feature = "serde")]
 
+use icu_datetime::{neo_skeleton, options::components};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
@@ -25,10 +26,56 @@ pub struct TestInput {
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct TestOptions {
-    pub length: Option<icu_datetime::options::length::Bag>,
-    pub components: Option<icu_datetime::options::components::Bag>,
+    pub length: Option<TestOptionsLength>,
+    pub components: Option<TestComponentsBag>,
     pub semantic: Option<icu_datetime::fieldset::dynamic::CompositeFieldSet>,
     pub preferences: Option<icu_datetime::options::preferences::Bag>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct TestOptionsLength {
+    pub date: Option<TestLength>,
+    pub time: Option<TestLength>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum TestLength {
+    #[serde(rename = "short")]
+    Short,
+    #[serde(rename = "medium")]
+    Medium,
+    #[serde(rename = "long")]
+    Long,
+    #[serde(rename = "full")]
+    Full,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct TestComponentsBag {
+    pub era: Option<components::Text>,
+    pub year: Option<components::Year>,
+    pub month: Option<components::Month>,
+    pub week: Option<components::Week>,
+    pub day: Option<components::Day>,
+    pub weekday: Option<components::Text>,
+
+    pub hour: Option<components::Numeric>,
+    pub minute: Option<components::Numeric>,
+    pub second: Option<components::Numeric>,
+    pub fractional_second: Option<neo_skeleton::FractionalSecondDigits>,
+
+    pub time_zone_name: Option<components::TimeZoneName>,
+
+    pub hour_cycle: Option<TestHourCycle>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum TestHourCycle {
+    H11,
+    H12,
+    H23,
+    H24,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
