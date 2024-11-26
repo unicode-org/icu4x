0d46522d0a78e575abbacf1fce2ad692dbf0f113
Nikita-str <newmynamespace@gmail.com>
2024-11-27 02:03:00 +0300

    Julian calendar calc benches

--- utils/calendrical_calculations/benches/iso.rs
+++ utils/calendrical_calculations/benches/iso.rs
@@ -7,6 +7,10 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use calendrical_calculations::bench_support::iso_old as old;
 use calendrical_calculations::iso as new;
 
+#[cfg(feature = "bench")]
+use calendrical_calculations::bench_support::julian_old as j_old;
+use calendrical_calculations::julian as j_new;
+
 const MONTH_DAYS: [u8; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 type IsoFromFixedFn = fn(RataDie) -> Result<(i32, u8, u8), I32CastError>;
 
@@ -38,7 +42,7 @@ fn prep_gen_rata_die_vec(n: i64) -> Vec<RataDie> {
     bb(ret)
 }
 
-fn from_iso(f: fn(i32, u8, u8) -> RataDie, ymd_vec: &Vec<(i32, u8, u8)>) -> i64 {
+fn fixed_from(f: fn(i32, u8, u8) -> RataDie, ymd_vec: &Vec<(i32, u8, u8)>) -> i64 {
     // The problem here is that LTO(it is turned on for benches in the workspace)
     // is very good at cyclic optimization when we goes year by year:
     // ```
@@ -107,7 +111,7 @@ fn day_of_year(f: fn(i32, u8, u8) -> u16, ymd_vec: &Vec<(i32, u8, u8)>) -> i32 {
     sum
 }
 
-fn iso_from_fixed(f: IsoFromFixedFn, rd_vec: &Vec<RataDie>) -> i64 {
+fn from_fixed(f: IsoFromFixedFn, rd_vec: &Vec<RataDie>) -> i64 {
     let map = |r: Result<(i32, u8, u8), I32CastError>| {
         let x = r.unwrap();
         x.0 as i64 + (x.1 + x.2) as i64
@@ -143,19 +147,34 @@ fn iso_from_year_day(f: fn(i32, u16) -> (u8, u8), rd_vec: &Vec<(i32, u16)>) -> i
 }
 
 #[no_mangle]
-fn bench_fixed_from_iso(c: &mut Criterion) {
+fn bench_fixed_from(c: &mut Criterion) {
     const Y: i32 = 1_000;
     const DELTA: i32 = 2_000;
     let ymd_vec = prep_gen_ymd_vec(Y, DELTA);
 
-    #[cfg(feature = "bench")]
-    c.bench_function("OLD/fixed_from_iso", |b| {
-        b.iter(|| from_iso(old::fixed_from_iso, bb(&ymd_vec)))
-    });
+    {
+        let mut group = c.benchmark_group("fixed_from_iso");
 
-    c.bench_function("NEW/fixed_from_iso", |b| {
-        b.iter(|| from_iso(new::fixed_from_iso, bb(&ymd_vec)))
-    });
+        #[cfg(feature = "bench")]
+        group.bench_function("OLD", |b| {
+            b.iter(|| fixed_from(old::fixed_from_iso, bb(&ymd_vec)))
+        });
+        group.bench_function("NEW", |b| {
+            b.iter(|| fixed_from(new::fixed_from_iso, bb(&ymd_vec)))
+        });
+    }
+
+    {
+        let mut group = c.benchmark_group("fixed_from_julian");
+
+        #[cfg(feature = "bench")]
+        group.bench_function("OLD", |b| {
+            b.iter(|| fixed_from(j_old::fixed_from_julian, bb(&ymd_vec)))
+        });
+        group.bench_function("NEW", |b| {
+            b.iter(|| fixed_from(j_new::fixed_from_julian, bb(&ymd_vec)))
+        });
+    }
 }
 
 fn bench_day_of_week(c: &mut Criterion) {
@@ -163,11 +182,13 @@ fn bench_day_of_week(c: &mut Criterion) {
     const DELTA: i32 = 2_000;
     let ymd_vec = prep_gen_ymd_vec(Y, DELTA);
 
+    let mut group = c.benchmark_group("day_of_week");
+
     #[cfg(feature = "bench")]
-    c.bench_function("OLD/day_of_week", |b| {
+    group.bench_function("iso/OLD", |b| {
         b.iter(|| day_of_week(old::day_of_week, bb(&ymd_vec)))
     });
-    c.bench_function("NEW/day_of_week", |b| {
+    group.bench_function("iso/NEW", |b| {
         b.iter(|| day_of_week(new::day_of_week, bb(&ymd_vec)))
     });
 }
@@ -177,25 +198,45 @@ fn bench_day_of_year(c: &mut Criterion) {
     const DELTA: i32 = 2_000;
     let ymd_vec = prep_gen_ymd_vec(Y, DELTA);
 
+    let mut group = c.benchmark_group("day_of_year");
+
     #[cfg(feature = "bench")]
-    c.bench_function("OLD/day_of_year", |b| {
+    group.bench_function("iso/OLD", |b| {
         b.iter(|| day_of_year(old::day_of_year, bb(&ymd_vec)))
     });
-    c.bench_function("NEW/day_of_year", |b| {
+    group.bench_function("iso/NEW", |b| {
         b.iter(|| day_of_year(new::day_of_year, bb(&ymd_vec)))
+    });
+
+    #[cfg(feature = "bench")]
+    group.bench_function("julian/OLD", |b| {
+        b.iter(|| day_of_year(j_old::day_of_year, bb(&ymd_vec)))
+    });
+    group.bench_function("julian/NEW", |b| {
+        b.iter(|| day_of_year(j_new::day_of_year, bb(&ymd_vec)))
     });
 }
 
-fn bench_iso_from_fixed(c: &mut Criterion) {
+fn bench_from_fixed(c: &mut Criterion) {
     const N: i64 = 10_000;
     let rd_vec = prep_gen_rata_die_vec(N);
 
+    let mut group = c.benchmark_group("from_fixed");
+
     #[cfg(feature = "bench")]
-    c.bench_function("OLD/iso_from_fixed", |b| {
-        b.iter(|| iso_from_fixed(old::iso_from_fixed, bb(&rd_vec)))
+    group.bench_function("iso/OLD", |b| {
+        b.iter(|| from_fixed(old::iso_from_fixed, bb(&rd_vec)))
     });
-    c.bench_function("NEW/iso_from_fixed", |b| {
-        b.iter(|| iso_from_fixed(new::iso_from_fixed, bb(&rd_vec)))
+    group.bench_function("iso/NEW", |b| {
+        b.iter(|| from_fixed(new::iso_from_fixed, bb(&rd_vec)))
+    });
+
+    #[cfg(feature = "bench")]
+    group.bench_function("julian/OLD", |b| {
+        b.iter(|| from_fixed(j_old::julian_from_fixed, bb(&rd_vec)))
+    });
+    group.bench_function("julian/NEW", |b| {
+        b.iter(|| from_fixed(j_new::julian_from_fixed, bb(&rd_vec)))
     });
 }
 
@@ -203,12 +244,14 @@ fn bench_iso_from_year_day(c: &mut Criterion) {
     const Y: i32 = 1_000;
     const DELTA: i32 = 2_000;
 
+    let mut group = c.benchmark_group("from_year_day");
+
     let yd_vec = prep_gen_yd_vec(Y, DELTA, 1, 365);
     #[cfg(feature = "bench")]
-    c.bench_function("OLD/iso_from_year_day/AVG", |b| {
+    group.bench_function("iso/OLD/AVG", |b| {
         b.iter(|| iso_from_year_day(old::iso_from_year_day, bb(&yd_vec)))
     });
-    c.bench_function("NEW/iso_from_year_day/AVG", |b| {
+    group.bench_function("iso/NEW/AVG", |b| {
         b.iter(|| iso_from_year_day(new::iso_from_year_day, bb(&yd_vec)))
     });
 
@@ -216,33 +259,33 @@ fn bench_iso_from_year_day(c: &mut Criterion) {
     // And they ~ the same perf in 3rd/4th months
     let yd_vec = prep_gen_yd_vec(Y, DELTA, 3, 57);
     #[cfg(feature = "bench")]
-    c.bench_function("OLD/iso_from_year_day/START", |b| {
+    group.bench_function("iso/OLD/START", |b| {
         b.iter(|| iso_from_year_day(old::iso_from_year_day, bb(&yd_vec)))
     });
-    c.bench_function("NEW/iso_from_year_day/START", |b| {
+    group.bench_function("iso/NEW/START", |b| {
         b.iter(|| iso_from_year_day(new::iso_from_year_day, bb(&yd_vec)))
     });
 
     let yd_vec = prep_gen_yd_vec(Y, DELTA, 300, 360);
     #[cfg(feature = "bench")]
-    c.bench_function("OLD/iso_from_year_day/END", |b| {
+    group.bench_function("iso/OLD/END", |b| {
         b.iter(|| iso_from_year_day(old::iso_from_year_day, bb(&yd_vec)))
     });
-    c.bench_function("NEW/iso_from_year_day/END", |b| {
+    group.bench_function("iso/NEW/END", |b| {
         b.iter(|| iso_from_year_day(new::iso_from_year_day, bb(&yd_vec)))
     });
 }
 
-criterion_group!(benchmark_fixed_from_iso, bench_fixed_from_iso);
+criterion_group!(benchmark_fixed_from, bench_fixed_from);
 criterion_group!(benchmark_year_from_fixed, bench_day_of_week);
 criterion_group!(benchmark_day_of_year, bench_day_of_year);
-criterion_group!(benchmark_iso_from_fixed, bench_iso_from_fixed);
+criterion_group!(benchmark_from_fixed, bench_from_fixed);
 criterion_group!(benchmark_iso_from_year_day, bench_iso_from_year_day);
 
 criterion_main!(
-    benchmark_fixed_from_iso,
+    benchmark_fixed_from,
     benchmark_year_from_fixed,
     benchmark_day_of_year,
-    benchmark_iso_from_fixed,
+    benchmark_from_fixed,
     benchmark_iso_from_year_day
 );
