02eaa6e822cdc742dcd25e4070396f496e63d467
Shane F. Carr <shane@unicode.org>
2024-12-13 18:31:39 -0800

    Add fn to icu_capi

--- ffi/capi/src/fixed_decimal.rs
+++ ffi/capi/src/fixed_decimal.rs
@@ -278,6 +278,12 @@ pub mod ffi {
             self.0.absolute.trim_end()
         }
 
+        #[diplomat::rust_link(fixed_decimal::FixedDecimal::trim_end_if_integer, FnInStruct)]
+        #[diplomat::rust_link(fixed_decimal::FixedDecimal::trimmed_end_if_integer, FnInStruct, hidden)]
+        pub fn trim_end_if_integer(&mut self) {
+            self.0.absolute.trim_end_if_integer()
+        }
+
         /// Zero-pad the [`SignedFixedDecimal`] on the left to a particular position
         #[diplomat::rust_link(fixed_decimal::FixedDecimal::pad_start, FnInStruct)]
         #[diplomat::rust_link(fixed_decimal::FixedDecimal::padded_start, FnInStruct, hidden)]
