2f8eca940fc54672f2b7abe3963f453db3ce27e6
Shane F. Carr <shane@unicode.org>
2024-12-16 14:51:43 -0800

    DateTimeFormatter AnyCalendarKind getter

--- components/datetime/src/neo.rs
+++ components/datetime/src/neo.rs
@@ -21,7 +21,7 @@ use crate::MismatchedCalendarError;
 use core::fmt;
 use core::marker::PhantomData;
 use icu_calendar::any_calendar::IntoAnyCalendar;
-use icu_calendar::{AnyCalendar, AnyCalendarPreferences};
+use icu_calendar::{AnyCalendar, AnyCalendarKind, AnyCalendarPreferences};
 use icu_decimal::FixedDecimalFormatterPreferences;
 use icu_locale_core::preferences::extensions::unicode::keywords::{
     CalendarAlgorithm, HourCycle, NumberingSystem,
@@ -764,6 +764,36 @@ impl<FSet: DateTimeMarkers> DateTimeFormatter<FSet> {
             names: self.names,
             _calendar: PhantomData,
         })
+    }
+
+    /// Returns the calendar system used in this formatter.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use icu::calendar::AnyCalendarKind;
+    /// use icu::calendar::Date;
+    /// use icu::datetime::fieldsets::YMD;
+    /// use icu::datetime::DateTimeFormatter;
+    /// use icu::locale::locale;
+    /// use writeable::assert_writeable_eq;
+    ///
+    /// let formatter = DateTimeFormatter::try_new(
+    ///     locale!("th").into(),
+    ///     YMD::long(),
+    /// )
+    /// .unwrap();
+    /// 
+    /// assert_writeable_eq!(
+    ///     formatter.format_any_calendar(&Date::try_new_iso(2024, 12, 16).unwrap()),
+    ///     "16 ธันวาคม 2567"
+    /// );
+    ///
+    /// assert_eq!(formatter.calendar_kind(), AnyCalendarKind::Buddhist);
+    /// assert_eq!(formatter.calendar_kind().as_bcp47_string(), "buddhist");
+    /// ```
+    pub fn calendar_kind(&self) -> AnyCalendarKind {
+        self.calendar.kind()
     }
 }
 
