3a0ab4fd8c8b38dedff115702ebdbde9a4a9040d
Manish Goregaokar <manishsmail@gmail.com>
2025-01-09 10:16:32 -0800

    Implement display on all error types

--- components/experimental/src/duration/validated_options.rs
+++ components/experimental/src/duration/validated_options.rs
@@ -61,19 +61,23 @@ pub struct ValidatedDurationFormatterOptions {
 
 /// Error type for [`DurationFormatterOptions`] validation.
 #[non_exhaustive]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, displaydoc::Display)]
 pub enum DurationFormatterOptionsError {
     /// Returned when a unit field is set to [`FieldDisplay::Always`] and the style is set to [`FieldStyle::Fractional`].
+    #[displaydoc("Returned when a unit field is set to Always and the style is set to Fractional")]
     DisplayAlwaysFractional,
 
     /// Returned when a previous unit's style is [`FieldStyle::Fractional`], but the following unit's style is not [`FieldStyle::Fractional`].
+    #[displaydoc("Returned when a previous unit's style is Fractional, but the following unit's style is not Fractional")]
     PreviousFractional,
 
     /// Returned when a previous unit's style is set to [`FieldStyle::Numeric`] or [`FieldStyle::TwoDigit`] and the following unit's style is not
     /// [`FieldStyle::Fractional`], [`FieldStyle::Numeric`], or [`FieldStyle::TwoDigit`].
+    #[displaydoc("Returned when a previous unit's style is set to Numeric or TwoDigit and the following unit's style is not Fractional, Numeric, or TwoDigit")]
     PreviousNumeric,
 
     /// Returned when the number of fractional digits is out of acceptable range. See [`FractionalDigits::Fixed`].
+    #[displaydoc("Returned when the number of fractional digits is out of acceptable range")]
     FractionalDigitsOutOfRange,
 }
 
--- components/locale_core/src/preferences/extensions/unicode/errors.rs
+++ components/locale_core/src/preferences/extensions/unicode/errors.rs
@@ -6,7 +6,7 @@
 
 /// Error returned by parsers of unicode extensions as preferences.
 #[non_exhaustive]
-#[derive(Debug)]
+#[derive(Debug, displaydoc::Display)]
 pub enum PreferencesParseError {
     /// The given keyword value is not a valid preference variant.
     InvalidKeywordValue,
