0ce718b6c3dd5e66869380cf8908a9bbd11032b3
Robert Bastian <robertbastian@unicode.org>
2025-01-13 11:46:06 +0100

    icu/datagen no_std

--- components/icu/src/datagen.rs
+++ components/icu/src/datagen.rs
@@ -2,17 +2,39 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+extern crate alloc;
+
+use alloc::collections::{BTreeMap, BTreeSet};
 use icu_provider::prelude::*;
-use std::collections::HashSet;
-use std::path::Path;
 
 macro_rules! cb {
     ($($marker:path = $path:literal,)+ #[experimental] $($emarker:path = $epath:literal,)+) => {
-        fn markers_for_bin_inner(bytes: &[u8]) -> Result<HashSet<DataMarkerInfo>, DataError> {
-            use std::sync::OnceLock;
+        /// Parses a compiled binary and returns a list of [`DataMarkerInfo`]s that it uses *at runtime*.
+        ///
+        /// This function is intended to be used for binaries that use `AnyProvider` or `BufferProvider`,
+        /// for which the compiler cannot remove unused data. Using this function on a binary that only
+        /// uses compiled data (through the `compiled_data` feature or manual baked data) will not return
+        /// any markers, as the markers only exist in the type system.
+        ///
+        /// # Example
+        ///
+        /// ```no_run
+        /// # use icu_provider::DataMarker;
+        /// # use std::path::Path;
+        /// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+        /// assert_eq!(
+        ///     icu::markers_for_bin(&std::fs::read(Path::new("target/release/my-app"))?)?,
+        ///     std::collections::BTreeSet::from_iter([
+        ///         icu::list::provider::AndListV2Marker::INFO,
+        ///         icu::list::provider::OrListV2Marker::INFO,
+        ///     ]),
+        /// );
+        /// # Ok(())
+        /// # }
+        /// ```
+        pub fn markers_for_bin(bytes: &[u8]) -> Result<BTreeSet<DataMarkerInfo>, DataError> {
             use crate as icu;
-            static LOOKUP: OnceLock<std::collections::HashMap<&'static str, Option<DataMarkerInfo>>> = OnceLock::new();
-            let lookup = LOOKUP.get_or_init(|| {
+            let lookup =
                 [
                     ("core/helloworld@1", Some(icu_provider::hello_world::HelloWorldV1Marker::INFO)),
                     $(
@@ -27,8 +49,7 @@ macro_rules! cb {
 
                 ]
                 .into_iter()
-                .collect()
-            });
+                .collect::<BTreeMap<_,_>>();
 
             use memchr::memmem::*;
 
@@ -45,7 +66,7 @@ macro_rules! cb {
                         .find(marker_fragment)
                         .and_then(|end| marker_fragment.get(..end))
                 })
-                .map(std::str::from_utf8)
+                .map(core::str::from_utf8)
                 .filter_map(Result::ok)
                 .filter_map(|p| {
                     match lookup.get(p) {
@@ -60,37 +81,9 @@ macro_rules! cb {
 }
 icu_provider_registry::registry!(cb);
 
-/// Parses a compiled binary and returns a list of [`DataMarkerInfo`]s that it uses *at runtime*.
-///
-/// This function is intended to be used for binaries that use `AnyProvider` or `BufferProvider`,
-/// for which the compiler cannot remove unused data. Using this function on a binary that only
-/// uses compiled data (through the `compiled_data` feature or manual baked data) will not return
-/// any markers, as the markers only exist in the type system.
-///
-/// # Example
-///
-/// ```no_run
-/// # use icu_provider::DataMarker;
-/// # use std::path::Path;
-/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
-/// assert_eq!(
-///     icu::markers_for_bin(Path::new("target/release/my-app"))?,
-///     std::collections::HashSet::from_iter([
-///         icu::list::provider::AndListV2Marker::INFO,
-///         icu::list::provider::OrListV2Marker::INFO,
-///     ]),
-/// );
-/// # Ok(())
-/// # }
-/// ```
-pub fn markers_for_bin(path: &Path) -> Result<HashSet<DataMarkerInfo>, DataError> {
-    markers_for_bin_inner(&std::fs::read(path)?)
-}
-
 #[test]
 fn test_markers_for_bin() {
-    let hashset =
-        markers_for_bin_inner(include_bytes!("../tests/data/tutorial_buffer.wasm")).unwrap();
+    let hashset = markers_for_bin(include_bytes!("../tests/data/tutorial_buffer.wasm")).unwrap();
     let mut sorted = hashset.into_iter().collect::<Vec<_>>();
     sorted.sort();
     assert_eq!(
