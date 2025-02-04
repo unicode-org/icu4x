4e425917b883709170b8876118320e542e8b45f5
Robert Bastian <robertbastian@unicode.org>
2025-02-04 12:17:52 +0100

    no parsing

--- components/properties/src/names.rs
+++ components/properties/src/names.rs
@@ -719,30 +719,6 @@ pub trait NamedEnumeratedProperty: ParseableEnumeratedProperty {
     fn nep_short_identity_static(
         stat: &'static Self::DataStructShortBorrowed<'static>,
     ) -> &'static Self::DataStructShort;
-
-    /// Convenience method for `PropertyParser::new().get_loose(s)`
-    ///
-    /// ✨ *Enabled with the `compiled_data` Cargo feature.*
-    #[cfg(feature = "compiled_data")]
-    fn try_from_str(s: &str) -> Option<Self> {
-        PropertyParser::new().get_loose(s)
-    }
-    /// Convenience method for `PropertyNamesLong::new().get(*self).unwrap()`
-    ///
-    /// ✨ *Enabled with the `compiled_data` Cargo feature.*
-    #[cfg(feature = "compiled_data")]
-    fn long_name(&self) -> &'static str {
-        PropertyNamesLong::new().get(*self).unwrap_or("unreachable")
-    }
-    /// Convenience method for `PropertyNamesShort::new().get(*self).unwrap()`
-    ///
-    /// ✨ *Enabled with the `compiled_data` Cargo feature.*
-    #[cfg(feature = "compiled_data")]
-    fn short_name(&self) -> &'static str {
-        PropertyNamesShort::new()
-            .get(*self)
-            .unwrap_or("unreachable")
-    }
 }
 
 macro_rules! impl_value_getter {
