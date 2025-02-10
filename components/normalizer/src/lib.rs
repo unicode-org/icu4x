e8a32045fda51cff23e35aa25a0a0a4b841e9b66
Robert Bastian <robertbastian@unicode.org>
2025-02-10 19:21:38 +0100

    fix

--- components/normalizer/src/lib.rs
+++ components/normalizer/src/lib.rs
@@ -113,10 +113,10 @@ struct CanonicalCombiningClass(pub(crate) u8);
 
 #[cfg(not(feature = "icu_properties"))]
 impl CanonicalCombiningClass {
-    fn from_icu4c_value(v: u8) -> Self {
+    const fn from_icu4c_value(v: u8) -> Self {
         Self(v)
     }
-    fn to_icu4c_value(self) -> u8 {
+    const fn to_icu4c_value(self) -> u8 {
         self.0
     }
 }
