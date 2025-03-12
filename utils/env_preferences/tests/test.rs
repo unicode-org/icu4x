8724b97977c5f2cd87c6772c204c9936b00817f4
Finchie <developer.finchie@gmail.com>
2025-03-12 23:51:25 +0800

    Add POSIX/Windows dataset tests to `env_preferences`
    Simple tests to make sure all known locales can be parsed without any
    explicit errors, as a supplement to regular system-specific test cases.

--- utils/env_preferences/tests/test.rs
+++ utils/env_preferences/tests/test.rs
@@ -2,6 +2,7 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+mod datasets;
 mod parse;
 
 #[cfg(target_os = "linux")]
