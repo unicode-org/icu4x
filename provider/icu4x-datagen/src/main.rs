42d2ad877c60df363e2821c61b64b40e5b45701b
Manish Goregaokar <manishsmail@gmail.com>
2025-03-13 00:33:39 +0000

    Update provider/icu4x-datagen/src/main.rs
    Co-authored-by: Robert Bastian <4706271+robertbastian@users.noreply.github.com>

--- provider/icu4x-datagen/src/main.rs
+++ provider/icu4x-datagen/src/main.rs
@@ -130,7 +130,7 @@ struct Cli {
     tzdb_tag: String,
 
     #[arg(long, value_name = "PATH")]
-    #[arg(help = "Path to a local tzdb directory (see https://github.com/eggert/tz).")]
+    #[arg(help = "Path to a local tzdb directory (see https://data.iana.org/time-zones/releases/).")]
     #[cfg(feature = "provider")]
     tzdb_root: Option<PathBuf>,
 
