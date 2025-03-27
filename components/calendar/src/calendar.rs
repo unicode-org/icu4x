ea3c7259e128513301cdb9d25a862b06bf9c95da
Robert Bastian <robertbastian@unicode.org>
2025-03-27 17:23:27 +0100

    remove trait method

--- components/calendar/src/calendar.rs
+++ components/calendar/src/calendar.rs
@@ -47,10 +47,6 @@ pub trait Calendar {
     /// Count the number of days in a given month, specified by providing a date
     /// from that year/month
     fn days_in_month(&self, date: &Self::DateInner) -> u8;
-    /// Calculate the day of the week and return it
-    fn day_of_week(&self, date: &Self::DateInner) -> types::Weekday {
-        self.date_to_iso(date).day_of_week()
-    }
     // fn week_of_year(&self, date: &Self::DateInner) -> u8;
 
     #[doc(hidden)] // unstable
