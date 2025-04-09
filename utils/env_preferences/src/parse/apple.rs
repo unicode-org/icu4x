ce2e70a64bc68ffd5c7b1a7d72755b38e7804d19
Finchie <developer.finchie@gmail.com>
2025-04-09 18:11:17 +0800

    Fix Apple parsing code in `env_preferences`
    This was missed when updating Linux/Windows code, and now should build
    correctly.

--- utils/env_preferences/src/parse/apple.rs
+++ utils/env_preferences/src/parse/apple.rs
@@ -2,20 +2,22 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-use icu_locale_core::{Locale, ParseError};
+use icu_locale_core::Locale;
 
-use crate::RetrievalError;
+use crate::ParseError;
 
 pub struct AppleLocale<'src> {
     src: &'src str,
 }
 
 impl<'src> AppleLocale<'src> {
-    pub fn try_from_str(src: &'src str) -> Result<Self, RetrievalError> {
+    pub fn try_from_str(src: &'src str) -> Result<Self, ParseError> {
         Ok(Self { src })
     }
 
     pub fn try_convert_lossy(&self) -> Result<Locale, ParseError> {
-        Locale::try_from_str(self.src)
+        let locale = Locale::try_from_str(self.src)?;
+
+        Ok(locale)
     }
 }
