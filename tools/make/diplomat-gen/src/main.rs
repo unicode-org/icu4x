6b054fdbe6b36e0f35b909b7f3bad766590bfc48
Manish Goregaokar <manishsmail@gmail.com>
2025-04-10 16:00:41 -0700

    update

--- tools/make/diplomat-gen/src/main.rs
+++ tools/make/diplomat-gen/src/main.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 
 use diplomat_tool::config::Config;
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), String> {
     let capi = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/../../../ffi/capi"));
 
     let Some(lang) = std::env::args().nth(1) else {
@@ -23,12 +23,13 @@ fn main() -> std::io::Result<()> {
         lang.as_str(),
         &{
             let include = capi.join("bindings").join(&lang);
-            std::fs::remove_dir_all(&include)?;
-            std::fs::create_dir(&include)?;
+            std::fs::remove_dir_all(&include).map_err(|e| e.to_string())?;
+            std::fs::create_dir(&include).map_err(|e| e.to_string())?;
             include
         },
         &Default::default(),
         library_config,
         false,
     )
+    .map_err(|e| e.to_string())
 }
