76b7cfcbd897b4532645429014343213438f5c16
Shane F. Carr <shane@unicode.org>
2025-05-05 17:05:29 -0700

    Split DateTimeWriteError into FormattedDateTimeUncheckedError

--- components/datetime/src/unchecked.rs
+++ components/datetime/src/unchecked.rs
@@ -4,14 +4,16 @@
 
 //! Datetime formatting without static checking of invariants.
 
-use crate::format::datetime::try_write_pattern_items;
 pub use crate::format::DateTimeInputUnchecked;
+use core::fmt;
+use crate::DateTimeFormatter;
+use crate::FixedCalendarDateTimeFormatter;
+use crate::format::datetime::try_write_pattern_items;
 use crate::pattern::*;
 use crate::raw::neo::*;
 use crate::scaffold::*;
-use crate::DateTimeFormatter;
-use crate::FixedCalendarDateTimeFormatter;
-use core::fmt;
+use icu_calendar::types::MonthCode;
+use tinystr::TinyStr16;
 use writeable::TryWriteable;
 
 #[cfg(doc)]
@@ -48,7 +50,7 @@ impl<C: CldrCalendar, FSet: DateTimeNamesMarker> FixedCalendarDateTimeFormatter<
     /// use icu::datetime::fieldsets::enums::CompositeFieldSet;
     /// use icu::datetime::input::{Date, Time};
     /// use icu::datetime::FixedCalendarDateTimeFormatter;
-    /// use icu::datetime::pattern::DateTimeWriteError;
+    /// use icu::datetime::unchecked::FormattedDateTimeUncheckedError;
     /// use icu::datetime::unchecked::DateTimeInputUnchecked;
     /// use icu::locale::locale;
     /// use writeable::assert_try_writeable_eq;
@@ -80,7 +82,7 @@ impl<C: CldrCalendar, FSet: DateTimeNamesMarker> FixedCalendarDateTimeFormatter<
     /// assert_try_writeable_eq!(
     ///     result,
     ///     "{d} {M} {G} {y}",
-    ///     Err(DateTimeWriteError::MissingInputField("day_of_month"))
+    ///     Err(FormattedDateTimeUncheckedError::MissingInputField("day_of_month"))
     /// );
     /// ```
     ///
@@ -125,7 +127,7 @@ impl<FSet: DateTimeNamesMarker> DateTimeFormatter<FSet> {
     /// use icu::datetime::fieldsets::enums::CompositeFieldSet;
     /// use icu::datetime::input::{Date, Time};
     /// use icu::datetime::DateTimeFormatter;
-    /// use icu::datetime::pattern::DateTimeWriteError;
+    /// use icu::datetime::unchecked::FormattedDateTimeUncheckedError;
     /// use icu::datetime::unchecked::DateTimeInputUnchecked;
     /// use icu::locale::locale;
     /// use writeable::assert_try_writeable_eq;
@@ -157,7 +159,7 @@ impl<FSet: DateTimeNamesMarker> DateTimeFormatter<FSet> {
     /// assert_try_writeable_eq!(
     ///     result,
     ///     "{d} {M} {G} {y}",
-    ///     Err(DateTimeWriteError::MissingInputField("day_of_month"))
+    ///     Err(FormattedDateTimeUncheckedError::MissingInputField("day_of_month"))
     /// );
     /// ```
     ///
@@ -187,19 +189,33 @@ pub struct FormattedDateTimeUnchecked<'a> {
 }
 
 impl TryWriteable for FormattedDateTimeUnchecked<'_> {
-    type Error = DateTimeWriteError;
+    type Error = FormattedDateTimeUncheckedError;
     fn try_write_to_parts<S: writeable::PartsWrite + ?Sized>(
         &self,
         sink: &mut S,
     ) -> Result<Result<(), Self::Error>, fmt::Error> {
-        try_write_pattern_items(
+        let err = match try_write_pattern_items(
             self.pattern.metadata(),
             self.pattern.iter_items(),
             &self.input,
             &self.names,
             self.names.decimal_formatter,
             sink,
-        )
+        ) {
+            Ok(Ok(())) => return Ok(Ok(())),
+            Err(fmt::Error) => return Err(fmt::Error),
+            Ok(Err(err)) => err
+        };
+        Ok(Err(match err {
+            DateTimeWriteError::InvalidMonthCode(month_code) => Self::Error::InvalidMonthCode(month_code),
+            DateTimeWriteError::InvalidEra(tiny_ascii_str) => Self::Error::InvalidEra(tiny_ascii_str),
+            DateTimeWriteError::InvalidCyclicYear { value, max } => Self::Error::InvalidCyclicYear { value, max },
+            DateTimeWriteError::DecimalFormatterNotLoaded => Self::Error::DecimalFormatterNotLoaded,
+            DateTimeWriteError::NamesNotLoaded(error_field) => Self::Error::NamesNotLoaded(error_field),
+            DateTimeWriteError::MissingInputField(name) => Self::Error::MissingInputField(name),
+            DateTimeWriteError::UnsupportedLength(error_field) => Self::Error::UnsupportedLength(error_field),
+            DateTimeWriteError::UnsupportedField(error_field) => Self::Error::UnsupportedField(error_field),
+        }))
     }
 
     // TODO(#489): Implement writeable_length_hint
@@ -213,4 +229,82 @@ impl FormattedDateTimeUnchecked<'_> {
     pub fn pattern(&self) -> DateTimePattern {
         self.pattern.to_pattern()
     }
+}
+
+#[non_exhaustive]
+#[derive(Debug, PartialEq, Copy, Clone, displaydoc::Display)]
+/// Error for `TryWriteable` implementations
+pub enum FormattedDateTimeUncheckedError {
+    /// The [`MonthCode`] of the input is not valid for this calendar.
+    ///
+    /// This is guaranteed not to happen for `icu::calendar` inputs, but may happen for custom inputs.
+    ///
+    /// The output will contain the raw [`MonthCode`] as a fallback value.
+    #[displaydoc("Invalid month {0:?}")]
+    InvalidMonthCode(MonthCode),
+    /// The era code of the input is not valid for this calendar.
+    ///
+    /// This is guaranteed not to happen for `icu::calendar` inputs, but may happen for custom inputs.
+    ///
+    /// The output will contain the era code as the fallback.
+    #[displaydoc("Invalid era {0:?}")]
+    InvalidEra(TinyStr16),
+    /// The [`CyclicYear::year`] of the input is not valid for this calendar.
+    ///
+    /// This is guaranteed not to happen for `icu::calendar` inputs, but may happen for custom inputs.
+    ///
+    /// The output will contain [`CyclicYear::related_iso`] as a fallback value.
+    #[displaydoc("Invalid cyclic year {value} (maximum {max})")]
+    InvalidCyclicYear {
+        /// Value
+        value: u8,
+        /// Max
+        max: u8,
+    },
+
+    /// The [`DecimalFormatter`] has not been loaded.
+    ///
+    /// This *only* happens if the formatter has been created using
+    /// [`FixedCalendarDateTimeNames::with_pattern_unchecked`], the pattern requires decimal
+    /// formatting, and the decimal formatter was not loaded.
+    ///
+    /// The output will contain fallback values using Latin numerals.
+    #[displaydoc("DecimalFormatter not loaded")]
+    DecimalFormatterNotLoaded,
+    /// The localized names for a field have not been loaded.
+    ///
+    /// This *only* happens if the formatter has been created using
+    /// [`FixedCalendarDateTimeNames::with_pattern_unchecked`], and the pattern requires names
+    /// that were not loaded.
+    ///
+    /// The output will contain fallback values using field identifiers (such as `tue` for `Weekday::Tuesday`,
+    /// `M02` for month 2, etc.).
+    #[displaydoc("Names for {0:?} not loaded")]
+    NamesNotLoaded(ErrorField),
+    /// An input field (such as "hour" or "month") is missing.
+    ///
+    /// This *only* happens if the formatter has been created using
+    /// [`FixedCalendarDateTimeNames::with_pattern_unchecked`], and the pattern requires fields
+    /// that are not returned by the input type.
+    ///
+    /// The output will contain the string `{X}` instead, where `X` is the symbol for which the input is missing.
+    #[displaydoc("Incomplete input, missing value for {0:?}")]
+    MissingInputField(&'static str),
+    /// The pattern contains a field that has a valid symbol but invalid length.
+    ///
+    /// This *only* happens if the formatter has been created using
+    /// [`FixedCalendarDateTimeNames::with_pattern_unchecked`], and the pattern contains
+    /// a field with a length not supported in formatting.
+    ///
+    /// The output will contain fallback values similar to [`DateTimeWriteError::NamesNotLoaded`].
+    #[displaydoc("Field length for {0:?} is invalid")]
+    UnsupportedLength(ErrorField),
+    /// Unsupported field
+    ///
+    /// This *only* happens if the formatter has been created using
+    /// [`FixedCalendarDateTimeNames::with_pattern_unchecked`], and the pattern contains unsupported fields.
+    ///
+    /// The output will contain the string `{unsupported:X}`, where `X` is the symbol of the unsupported field.
+    #[displaydoc("Unsupported field {0:?}")]
+    UnsupportedField(ErrorField),
 }
