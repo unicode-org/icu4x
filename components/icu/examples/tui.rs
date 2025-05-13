646f2c95a1019595b910156a256c490beeefce62
Robert Bastian <robertbastian@unicode.org>
2025-05-13 19:09:56 +0200

    fmt

--- components/icu/examples/tui.rs
+++ components/icu/examples/tui.rs
@@ -6,12 +6,12 @@
 // in a pseudo-real-world application of Textual User Interface.
 
 use icu::calendar::{Date, Gregorian};
-use icu::locale::locale;
-use icu::plurals::{PluralCategory, PluralRules};
-use icu::time::TimeZoneInfo;
 use icu::collections::codepointinvlist::CodePointInversionListBuilder;
 use icu::datetime::fieldsets::{self, YMDT};
 use icu::datetime::FixedCalendarDateTimeFormatter;
+use icu::locale::locale;
+use icu::plurals::{PluralCategory, PluralRules};
+use icu::time::TimeZoneInfo;
 use icu::time::{Time, ZonedDateTime};
 use std::env;
 
