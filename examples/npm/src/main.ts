c524ac2e00740b4996694e8e33ba7a8024cd9f05
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2025-05-19 23:53:29 +0200

    Prepare the NPM package for release (#6616)

--- examples/npm/src/main.ts
+++ examples/npm/src/main.ts
@@ -1,8 +1,12 @@
-import { Decimal, DecimalFormatter, DecimalGroupingStrategy, Locale } from "icu4x";
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-var locale = Locale.fromString("bn");
+import { Decimal, DecimalFormatter, DecimalGroupingStrategy, Locale } from 'icu';
+
+var locale = Locale.fromString('bn');
 var groupingStrategy = DecimalGroupingStrategy.Auto;
-var decimal = Decimal.fromString("123");
+var decimal = Decimal.fromString('123');
 
 var formatter = DecimalFormatter.createWithGroupingStrategy(
     locale,
