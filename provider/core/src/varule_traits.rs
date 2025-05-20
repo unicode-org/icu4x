eedf89057ca94b1fee329a6447209d5e4f441f0b
Younies Mahmoud <younies.mahmoud@gmail.com>
2025-05-20 14:28:16 +0200

    fix nit

--- provider/core/src/varule_traits.rs
+++ provider/core/src/varule_traits.rs
@@ -122,13 +122,13 @@ impl<T, const N: usize> MaybeEncodeAsVarULE for [T; N] {
     }
 }
 
+impl MaybeAsVarULE for u16 {
+    type EncodedStruct = [()];
+}
+
 #[cfg(feature = "export")]
 impl MaybeEncodeAsVarULE for u16 {
     fn maybe_encode_as_varule(&self) -> Option<&Self::EncodedStruct> {
         None
     }
-}
-
-impl MaybeAsVarULE for u16 {
-    type EncodedStruct = [()];
 }
