95c5b24cb024074616f90cfecb00d1b17bbf41be
Robert Bastian <robertbastian@unicode.org>
2025-05-22 15:28:48 +0200

    f

--- components/datetime/src/lib.rs
+++ components/datetime/src/lib.rs
@@ -101,7 +101,7 @@
 //! not fixed in code. However, there are legitimate use cases where only one calendar system is supported,
 //! in which case [`DateTimeFormatter`] would link unused data. In this case [`FixedCalendarDateTimeFormatter`]
 //! can be used, which is generic in a calendar type and only links the data for that calendar.
-//! 
+//!
 //! Using [`FixedCalendarDateTimeFormatter`] also avoids linking code that converts inputs to the user's calendar.
 //! For field sets that don't contain dates, this can also be achieved using [`NoCalendarFormatter`].
 
