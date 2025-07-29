932aaa3f6b7afe322c305d124fe5c237f008be6d
Manish Goregaokar <manishsmail@gmail.com>
2025-07-29 17:04:17 +0530

    Add compiled data FFI for simple casemap functions (#6774)
    Will be useful for gboard.
    
    
    Progress on #6050 
    <!--
    Thank you for your pull request to ICU4X!
    
    Reminder: try to use [Conventional
    Comments](https://conventionalcomments.org/) to make comments clearer.
    
    Please see
    https://github.com/unicode-org/icu4x/blob/main/CONTRIBUTING.md for
    general
    information on contributing to ICU4X.
    -->

--- ffi/capi/bindings/cpp/icu4x/CaseMapper.hpp
+++ ffi/capi/bindings/cpp/icu4x/CaseMapper.hpp
@@ -46,13 +46,23 @@ namespace capi {
 
     char32_t icu4x_CaseMapper_simple_lowercase_mv1(const icu4x::capi::CaseMapper* self, char32_t ch);
 
+    char32_t icu4x_CaseMapper_simple_lowercase_with_compiled_data_mv1(char32_t ch);
+
     char32_t icu4x_CaseMapper_simple_uppercase_mv1(const icu4x::capi::CaseMapper* self, char32_t ch);
+
+    char32_t icu4x_CaseMapper_simple_uppercase_with_compiled_data_mv1(char32_t ch);
 
     char32_t icu4x_CaseMapper_simple_titlecase_mv1(const icu4x::capi::CaseMapper* self, char32_t ch);
 
+    char32_t icu4x_CaseMapper_simple_titlecase_with_compiled_data_mv1(char32_t ch);
+
     char32_t icu4x_CaseMapper_simple_fold_mv1(const icu4x::capi::CaseMapper* self, char32_t ch);
 
+    char32_t icu4x_CaseMapper_simple_fold_with_compiled_data_mv1(char32_t ch);
+
     char32_t icu4x_CaseMapper_simple_fold_turkic_mv1(const icu4x::capi::CaseMapper* self, char32_t ch);
+
+    char32_t icu4x_CaseMapper_simple_fold_turkic_with_compiled_data_mv1(char32_t ch);
 
     void icu4x_CaseMapper_destroy_mv1(CaseMapper* self);
 
@@ -251,9 +261,19 @@ inline char32_t icu4x::CaseMapper::simple_lowercase(char32_t ch) const {
   return result;
 }
 
+inline char32_t icu4x::CaseMapper::simple_lowercase_with_compiled_data(char32_t ch) {
+  auto result = icu4x::capi::icu4x_CaseMapper_simple_lowercase_with_compiled_data_mv1(ch);
+  return result;
+}
+
 inline char32_t icu4x::CaseMapper::simple_uppercase(char32_t ch) const {
   auto result = icu4x::capi::icu4x_CaseMapper_simple_uppercase_mv1(this->AsFFI(),
     ch);
+  return result;
+}
+
+inline char32_t icu4x::CaseMapper::simple_uppercase_with_compiled_data(char32_t ch) {
+  auto result = icu4x::capi::icu4x_CaseMapper_simple_uppercase_with_compiled_data_mv1(ch);
   return result;
 }
 
@@ -263,15 +283,30 @@ inline char32_t icu4x::CaseMapper::simple_titlecase(char32_t ch) const {
   return result;
 }
 
+inline char32_t icu4x::CaseMapper::simple_titlecase_with_compiled_data(char32_t ch) {
+  auto result = icu4x::capi::icu4x_CaseMapper_simple_titlecase_with_compiled_data_mv1(ch);
+  return result;
+}
+
 inline char32_t icu4x::CaseMapper::simple_fold(char32_t ch) const {
   auto result = icu4x::capi::icu4x_CaseMapper_simple_fold_mv1(this->AsFFI(),
     ch);
   return result;
 }
 
+inline char32_t icu4x::CaseMapper::simple_fold_with_compiled_data(char32_t ch) {
+  auto result = icu4x::capi::icu4x_CaseMapper_simple_fold_with_compiled_data_mv1(ch);
+  return result;
+}
+
 inline char32_t icu4x::CaseMapper::simple_fold_turkic(char32_t ch) const {
   auto result = icu4x::capi::icu4x_CaseMapper_simple_fold_turkic_mv1(this->AsFFI(),
     ch);
+  return result;
+}
+
+inline char32_t icu4x::CaseMapper::simple_fold_turkic_with_compiled_data(char32_t ch) {
+  auto result = icu4x::capi::icu4x_CaseMapper_simple_fold_turkic_with_compiled_data_mv1(ch);
   return result;
 }
 
--- ffi/capi/bindings/js/CaseMapper.mjs
+++ ffi/capi/bindings/js/CaseMapper.mjs
@@ -303,6 +303,23 @@ export class CaseMapper {
     }
 
     /**
+     * Returns the simple lowercase mapping of the given character, using compiled data (avoids having to allocate a CaseMapper object)
+     *
+     * See the [Rust documentation for `simple_lowercase`](https://docs.rs/icu/2.0.0/icu/casemap/struct.CaseMapperBorrowed.html#method.simple_lowercase) for more information.
+     */
+    static simpleLowercaseWithCompiledData(ch) {
+
+        const result = wasm.icu4x_CaseMapper_simple_lowercase_with_compiled_data_mv1(ch);
+
+        try {
+            return result;
+        }
+
+        finally {
+        }
+    }
+
+    /**
      * Returns the simple uppercase mapping of the given character.
      *
      * This function only implements simple and common mappings.
@@ -314,6 +331,23 @@ export class CaseMapper {
     simpleUppercase(ch) {
 
         const result = wasm.icu4x_CaseMapper_simple_uppercase_mv1(this.ffiValue, ch);
+
+        try {
+            return result;
+        }
+
+        finally {
+        }
+    }
+
+    /**
+     * Returns the simple uppercase mapping of the given character, using compiled data (avoids having to allocate a CaseMapper object)
+     *
+     * See the [Rust documentation for `simple_uppercase`](https://docs.rs/icu/2.0.0/icu/casemap/struct.CaseMapperBorrowed.html#method.simple_uppercase) for more information.
+     */
+    static simpleUppercaseWithCompiledData(ch) {
+
+        const result = wasm.icu4x_CaseMapper_simple_uppercase_with_compiled_data_mv1(ch);
 
         try {
             return result;
@@ -345,6 +379,23 @@ export class CaseMapper {
     }
 
     /**
+     * Returns the simple titlecase mapping of the given character, using compiled data (avoids having to allocate a CaseMapper object)
+     *
+     * See the [Rust documentation for `simple_titlecase`](https://docs.rs/icu/2.0.0/icu/casemap/struct.CaseMapperBorrowed.html#method.simple_titlecase) for more information.
+     */
+    static simpleTitlecaseWithCompiledData(ch) {
+
+        const result = wasm.icu4x_CaseMapper_simple_titlecase_with_compiled_data_mv1(ch);
+
+        try {
+            return result;
+        }
+
+        finally {
+        }
+    }
+
+    /**
      * Returns the simple casefolding of the given character.
      *
      * This function only implements simple folding.
@@ -365,7 +416,24 @@ export class CaseMapper {
     }
 
     /**
-     * Returns the simple casefolding of the given character in the Turkic locale
+     * Returns the simple casefolding of the given character, using compiled data (avoids having to allocate a CaseMapper object)
+     *
+     * See the [Rust documentation for `simple_fold`](https://docs.rs/icu/2.0.0/icu/casemap/struct.CaseMapperBorrowed.html#method.simple_fold) for more information.
+     */
+    static simpleFoldWithCompiledData(ch) {
+
+        const result = wasm.icu4x_CaseMapper_simple_fold_with_compiled_data_mv1(ch);
+
+        try {
+            return result;
+        }
+
+        finally {
+        }
+    }
+
+    /**
+     * Returns the simple casefolding of the given character in the Turkic locale.
      *
      * This function only implements simple folding.
      * For full folding, use `CaseMapperBorrowed::fold_turkic`.
@@ -375,6 +443,23 @@ export class CaseMapper {
     simpleFoldTurkic(ch) {
 
         const result = wasm.icu4x_CaseMapper_simple_fold_turkic_mv1(this.ffiValue, ch);
+
+        try {
+            return result;
+        }
+
+        finally {
+        }
+    }
+
+    /**
+     * Returns the simple Turkic casefolding of the given character, using compiled data (avoids having to allocate a CaseMapper object)
+     *
+     * See the [Rust documentation for `simple_fold_turkic`](https://docs.rs/icu/2.0.0/icu/casemap/struct.CaseMapperBorrowed.html#method.simple_fold_turkic) for more information.
+     */
+    static simpleFoldTurkicWithCompiledData(ch) {
+
+        const result = wasm.icu4x_CaseMapper_simple_fold_turkic_with_compiled_data_mv1(ch);
 
         try {
             return result;
