unicode-org/icu4x

commit 93291e44f6f0da78f54862ed9116d418c4af15d5

Add compressible_bytes data (#6604)
As a side effect, always load CollationSpecialPrimaries, since we no
longer know at collator instantiation time if some of the data in the
struct is going to be used.

Preparation for #6537

---------

Co-authored-by: Robert Bastian <robertbastian@unicode.org>

utils/zerovec/src/zerovec/mod.rs
@@ -165,6 +165,17 @@ impl<U> EyepatchHackVector<U> {
         // this always represents a valid vector
         Vec::from_raw_parts(self.buf.as_ptr() as *mut U, len, self.capacity)
     }
+
+    fn truncate(&mut self, max: usize) {
+        // SAFETY: The elements in buf are `ULE`, so they don't need to be dropped
+        // even if we own them.
+        self.buf = unsafe {
+            NonNull::new_unchecked(core::ptr::slice_from_raw_parts_mut(
+                self.buf.as_mut().as_mut_ptr(),
+                core::cmp::max(max, self.buf.as_ref().len()),
+            ))
+        };
+    }
 }
 
 #[cfg(feature = "alloc")]
@@ -1067,6 +1078,13 @@ where
             let slice = unsafe { { this }.vector.as_arbitrary_slice() };
             Cow::Borrowed(slice)
         }
+    }
+
+    /// Truncates this vector to `min(self.len(), max)`.
+    #[inline]
+    pub fn truncated(mut self, max: usize) -> Self {
+        self.vector.truncate(max);
+        self
     }
 }
 

Cargo.toml
@@ -172,7 +172,7 @@ icu_provider_registry = { version = "~2.0.0", path = "provider/registry", defaul
 # Baked data
 icu_calendar_data = { version = "~2.0.0", path = "provider/data/calendar", default-features = false }
 icu_casemap_data = { version = "~2.0.0", path = "provider/data/casemap", default-features = false }
-icu_collator_data = { version = "~2.0.0", path = "provider/data/collator", default-features = false }
+icu_collator_data = { version = "~2.0.1", path = "provider/data/collator", default-features = false }
 icu_datetime_data = { version = "~2.0.0", path = "provider/data/datetime", default-features = false }
 icu_decimal_data = { version = "~2.0.0", path = "provider/data/decimal", default-features = false }
 icu_list_data = { version = "~2.0.0", path = "provider/data/list", default-features = false }

components/collator/Cargo.toml
@@ -5,6 +5,7 @@
 [package]
 name = "icu_collator"
 description = "API for comparing strings according to language-dependent conventions"
+version = "2.0.1"
 
 authors.workspace = true
 categories.workspace = true
@@ -14,7 +15,6 @@ include.workspace = true
 license.workspace = true
 repository.workspace = true
 rust-version.workspace = true
-version.workspace = true
 [package.metadata.docs.rs]
 all-features = true
 

provider/data/calendar/Cargo.toml
@@ -18,7 +18,7 @@ rust-version.workspace = true
 
 [package.metadata.sources]
 cldr = { tagged = "47.0.0" }
-icuexport = { tagged = "icu4x/2025-05-01/77.x" }
+icuexport = { tagged = "icu4x/2025-05-21/77.x" }
 segmenter_lstm = { tagged = "v0.1.0" }
 
 [lints.rust]

provider/data/casemap/Cargo.toml
@@ -18,7 +18,7 @@ rust-version.workspace = true
 
 [package.metadata.sources]
 cldr = { tagged = "47.0.0" }
-icuexport = { tagged = "icu4x/2025-05-01/77.x" }
+icuexport = { tagged = "icu4x/2025-05-21/77.x" }
 segmenter_lstm = { tagged = "v0.1.0" }
 
 [lints.rust]

provider/data/collator/Cargo.toml
@@ -6,7 +6,7 @@
 name = "icu_collator_data"
 description = "Data for the icu_collator crate"
 license = "Unicode-3.0"
-version.workspace = true
+version = "2.0.1"
 
 authors.workspace = true
 categories.workspace = true
@@ -18,7 +18,7 @@ rust-version.workspace = true
 
 [package.metadata.sources]
 cldr = { tagged = "47.0.0" }
-icuexport = { tagged = "icu4x/2025-05-01/77.x" }
+icuexport = { tagged = "icu4x/2025-05-21/77.x" }
 segmenter_lstm = { tagged = "v0.1.0" }
 
 [lints.rust]

provider/data/datetime/Cargo.toml
@@ -18,7 +18,7 @@ rust-version.workspace = true
 
 [package.metadata.sources]
 cldr = { tagged = "47.0.0" }
-icuexport = { tagged = "icu4x/2025-05-01/77.x" }
+icuexport = { tagged = "icu4x/2025-05-21/77.x" }
 segmenter_lstm = { tagged = "v0.1.0" }
 
 [lints.rust]

provider/data/decimal/Cargo.toml
@@ -18,7 +18,7 @@ rust-version.workspace = true
 
 [package.metadata.sources]
 cldr = { tagged = "47.0.0" }
-icuexport = { tagged = "icu4x/2025-05-01/77.x" }
+icuexport = { tagged = "icu4x/2025-05-21/77.x" }
 segmenter_lstm = { tagged = "v0.1.0" }
 
 [lints.rust]

commit 5c8b93a9d7617da87ee19e477839026079d22100

Fix safety issue in ZeroVec::truncated
Introduced by #6604

utils/zerovec/Cargo.toml
@@ -5,7 +5,7 @@
 [package]
 name = "zerovec"
 description = "Zero-copy vector backed by a byte array"
-version = "0.11.3"
+version = "0.11.4"
 categories = ["rust-patterns", "memory-management", "caching", "no-std", "data-structures"]
 keywords = ["zerocopy", "serialization", "zero-copy", "serde"]
 

utils/zerovec/src/zerovec/mod.rs
@@ -172,7 +172,7 @@ impl<U> EyepatchHackVector<U> {
         self.buf = unsafe {
             NonNull::new_unchecked(core::ptr::slice_from_raw_parts_mut(
                 self.buf.as_mut().as_mut_ptr(),
-                core::cmp::max(max, self.buf.as_ref().len()),
+                core::cmp::min(max, self.buf.as_ref().len()),
             ))
         };
     }