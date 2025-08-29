d3db5e660cd37c1c302e6696414a053e26f40cf0
Manish Goregaokar <manishsmail@gmail.com>
2025-08-28 23:49:16 -0700

    remove assertion comments

--- components/calendar/src/tests/extrema.rs
+++ components/calendar/src/tests/extrema.rs
@@ -12,59 +12,41 @@ fn check_extrema() {
     let min_date_iso = Date::try_new_iso(-271821, 4, 19).unwrap();
     let max_date_iso = Date::try_new_iso(275760, 9, 13).unwrap();
 
-    // Assertion failure:
-    // > Month should be in range of u8! Value -1428 failed for RD RataDie(-99280838)
-    {
-        let cal = Chinese::new();
-        let min_date = min_date_iso.to_calendar(Ref(&cal));
-        let max_date = max_date_iso.to_calendar(Ref(&cal));
+    let cal = Chinese::new();
+    let min_date = min_date_iso.to_calendar(Ref(&cal));
+    let max_date = max_date_iso.to_calendar(Ref(&cal));
 
-        println!(
-            "min.year = {:?}, max.year = {:?}",
-            min_date.year(),
-            max_date.year()
-        );
-    }
+    println!(
+        "min.year = {:?}, max.year = {:?}",
+        min_date.year(),
+        max_date.year()
+    );
 
-    // Assertion failure:
-    // > Month should be in range of u8! Value -1428 failed for RD RataDie(-99280838)
-    {
-        let cal = Dangi::new();
-        let min_date = min_date_iso.to_calendar(Ref(&cal));
-        let max_date = max_date_iso.to_calendar(Ref(&cal));
+    let cal = Dangi::new();
+    let min_date = min_date_iso.to_calendar(Ref(&cal));
+    let max_date = max_date_iso.to_calendar(Ref(&cal));
 
-        println!(
-            "min.year = {:?}, max.year = {:?}",
-            min_date.year(),
-            max_date.year()
-        );
-    }
+    println!(
+        "min.year = {:?}, max.year = {:?}",
+        min_date.year(),
+        max_date.year()
+    );
+    let cal = HijriSimulated::new_mecca();
+    let min_date = min_date_iso.to_calendar(Ref(&cal));
+    let max_date = max_date_iso.to_calendar(Ref(&cal));
 
-    // Assertion failure:
-    // > assertion failed: Date::try_new_observational_islamic_date(y, m, d, IslamicObservational::new_always_calculating()).is_ok()
-    {
-        let cal = HijriSimulated::new_mecca();
-        let min_date = min_date_iso.to_calendar(Ref(&cal));
-        let max_date = max_date_iso.to_calendar(Ref(&cal));
+    println!(
+        "min.year = {:?}, max.year = {:?}",
+        min_date.year(),
+        max_date.year()
+    );
+    let cal = HijriUmmAlQura::new();
+    let min_date = min_date_iso.to_calendar(Ref(&cal));
+    let max_date = max_date_iso.to_calendar(Ref(&cal));
 
-        println!(
-            "min.year = {:?}, max.year = {:?}",
-            min_date.year(),
-            max_date.year()
-        );
-    }
-
-    // Assertion failure:
-    // > assertion failed: Date::try_new_ummalqura_date(y, m, d, IslamicUmmAlQura::new_always_calculating()).is_ok()
-    {
-        let cal = HijriUmmAlQura::new();
-        let min_date = min_date_iso.to_calendar(Ref(&cal));
-        let max_date = max_date_iso.to_calendar(Ref(&cal));
-
-        println!(
-            "min.year = {:?}, max.year = {:?}",
-            min_date.year(),
-            max_date.year()
-        );
-    }
+    println!(
+        "min.year = {:?}, max.year = {:?}",
+        min_date.year(),
+        max_date.year()
+    );
 }
