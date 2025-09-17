35601298945852c7aa6d9700aae28a92f4c5bfac
younies <younies.mahmoud@gmail.com>
2025-09-17 16:02:05 +0200

    impl SourceDataProvider

--- provider/source/src/units/categorized_display_name.rs
+++ provider/source/src/units/categorized_display_name.rs
@@ -18,144 +18,142 @@ use icu_provider::prelude::*;
 use icu_provider::DataMarkerAttributes;
 use std::collections::HashSet;
 
-fn get_display_name_payload<M>(
-    source_data_provider: &SourceDataProvider,
-    req: DataRequest,
-) -> Result<DataResponse<M>, DataError>
-where
-    M: DataMarker<DataStruct = UnitsDisplayName<'static>>,
-{
-    let (length, unit) = req
-        .id
-        .marker_attributes
-        .split_once('-')
-        .ok_or_else(|| DataErrorKind::InvalidRequest.with_req(M::INFO, req))?;
+impl SourceDataProvider {
+    fn get_display_name_payload<M>(&self, req: DataRequest) -> Result<DataResponse<M>, DataError>
+    where
+        M: DataMarker<DataStruct = UnitsDisplayName<'static>>,
+    {
+        let (length, unit) = req
+            .id
+            .marker_attributes
+            .split_once('-')
+            .ok_or_else(|| DataErrorKind::InvalidRequest.with_req(M::INFO, req))?;
 
-    let units_format_data: &cldr_serde::units::data::Resource = source_data_provider
-        .cldr()?
-        .units()
-        .read_and_parse(req.id.locale, "units.json")?;
-
-    let units_format_data = &units_format_data.main.value.units;
-
-    let unit_patterns = match length {
-        "long" => &units_format_data.long,
-        "short" => &units_format_data.short,
-        "narrow" => &units_format_data.narrow,
-        _ => {
-            return Err(DataErrorKind::InvalidRequest
-                .with_req(M::INFO, req)
-                .with_debug_context(length))
-        }
-    }
-    .categories
-    .iter()
-    .find_map(|(_, units_map)| units_map.get(unit))
-    .ok_or_else(|| {
-        DataErrorKind::IdentifierNotFound
-            .with_req(M::INFO, req)
-            .with_debug_context(length)
-    })?;
-
-    Ok(DataResponse {
-        metadata: Default::default(),
-        payload: DataPayload::from_owned(UnitsDisplayName {
-            patterns: unit_patterns.try_into_plural_elements_packed_cow()?,
-        }),
-    })
-}
-
-fn get_display_name_iter_ids_cached(
-    source_data_provider: &SourceDataProvider,
-    unit_type: UnitType,
-    category: &str,
-) -> Result<HashSet<DataIdentifierCow<'static>>, DataError> {
-    let mut data_locales = HashSet::new();
-    let numbers = source_data_provider.cldr()?.numbers();
-    let locales = numbers.list_locales()?;
-    for locale in locales {
-        let likely_subtags: &cldr_serde::likely_subtags::Resource = source_data_provider
-            .cldr()?
-            .core()
-            .read_and_parse("supplemental/likelySubtags.json")?;
-
-        // Try to find a likely subtag for the language, and extract its region.
-        let region = locale
-            .region
-            .map(|r| r.to_string())
-            .or_else(|| {
-                let lang_id = LanguageIdentifier::from(locale.language);
-                likely_subtags
-                    .supplemental
-                    .likely_subtags
-                    .get(&lang_id)
-                    .and_then(|id| id.region.map(|r| r.to_string()))
-            })
-            .ok_or_else(|| {
-                let lang_id = LanguageIdentifier::from(locale.language);
-                DataError::custom("No region found for language: {}").with_debug_context(&lang_id)
-            })?;
-
-        // Load and parse the unit constants from the supplemental data file.
-        let preferences: &cldr_serde::units::preferences::Resource = source_data_provider
-            .cldr()?
-            .core()
-            .read_and_parse("supplemental/unitPreferenceData.json")?;
-
-        let categorized_units_list = preferences.supplemental.categorized_units_list();
-
-        let units_format_data: &cldr_serde::units::data::Resource = source_data_provider
+        let units_format_data: &cldr_serde::units::data::Resource = self
             .cldr()?
             .units()
-            .read_and_parse(&locale, "units.json")?;
+            .read_and_parse(req.id.locale, "units.json")?;
+
         let units_format_data = &units_format_data.main.value.units;
 
-        for length in &["long", "short", "narrow"] {
-            let length_patterns = match *length {
-                "long" => &units_format_data.long,
-                "short" => &units_format_data.short,
-                "narrow" => &units_format_data.narrow,
-                _ => {
-                    return Err(DataErrorKind::IdentifierNotFound
-                        .into_error()
-                        .with_debug_context(length))
-                }
-            };
-
-            let units_map = length_patterns.categories.get(category).ok_or_else(|| {
-                DataErrorKind::InvalidRequest
-                    .into_error()
-                    .with_debug_context(category)
-            })?;
-            for (unit, patterns) in units_map {
-                if patterns.other.is_none() {
-                    continue;
-                }
-
-                if cldr_serde::units::preferences::Supplemental::unit_type(
-                    category,
-                    unit,
-                    &region,
-                    &categorized_units_list,
-                ) != unit_type
-                {
-                    continue;
-                }
-
-                data_locales.insert(DataIdentifierCow::from_owned(
-                    DataMarkerAttributes::try_from_string(format!("{length}-{unit}")).map_err(
-                        |_| {
-                            DataError::custom("Failed to parse the attribute")
-                                .with_debug_context(&unit)
-                        },
-                    )?,
-                    locale,
-                ));
+        let unit_patterns = match length {
+            "long" => &units_format_data.long,
+            "short" => &units_format_data.short,
+            "narrow" => &units_format_data.narrow,
+            _ => {
+                return Err(DataErrorKind::InvalidRequest
+                    .with_req(M::INFO, req)
+                    .with_debug_context(length))
             }
         }
+        .categories
+        .iter()
+        .find_map(|(_, units_map)| units_map.get(unit))
+        .ok_or_else(|| {
+            DataErrorKind::IdentifierNotFound
+                .with_req(M::INFO, req)
+                .with_debug_context(length)
+        })?;
+
+        Ok(DataResponse {
+            metadata: Default::default(),
+            payload: DataPayload::from_owned(UnitsDisplayName {
+                patterns: unit_patterns.try_into_plural_elements_packed_cow()?,
+            }),
+        })
     }
 
-    Ok(data_locales)
+    fn get_display_name_iter_ids_cached(
+        &self,
+        unit_type: UnitType,
+        category: &str,
+    ) -> Result<HashSet<DataIdentifierCow<'static>>, DataError> {
+        let mut data_locales = HashSet::new();
+        let numbers = self.cldr()?.numbers();
+        let locales = numbers.list_locales()?;
+        for locale in locales {
+            let likely_subtags: &cldr_serde::likely_subtags::Resource = self
+                .cldr()?
+                .core()
+                .read_and_parse("supplemental/likelySubtags.json")?;
+
+            // Try to find a likely subtag for the language, and extract its region.
+            let region = locale
+                .region
+                .map(|r| r.to_string())
+                .or_else(|| {
+                    let lang_id = LanguageIdentifier::from(locale.language);
+                    likely_subtags
+                        .supplemental
+                        .likely_subtags
+                        .get(&lang_id)
+                        .and_then(|id| id.region.map(|r| r.to_string()))
+                })
+                .ok_or_else(|| {
+                    let lang_id = LanguageIdentifier::from(locale.language);
+                    DataError::custom("No region found for language: {}")
+                        .with_debug_context(&lang_id)
+                })?;
+
+            // Load and parse the unit constants from the supplemental data file.
+            let preferences: &cldr_serde::units::preferences::Resource = self
+                .cldr()?
+                .core()
+                .read_and_parse("supplemental/unitPreferenceData.json")?;
+
+            let categorized_units_list = preferences.supplemental.categorized_units_list();
+
+            let units_format_data: &cldr_serde::units::data::Resource =
+                self.cldr()?.units().read_and_parse(&locale, "units.json")?;
+            let units_format_data = &units_format_data.main.value.units;
+
+            for length in &["long", "short", "narrow"] {
+                let length_patterns = match *length {
+                    "long" => &units_format_data.long,
+                    "short" => &units_format_data.short,
+                    "narrow" => &units_format_data.narrow,
+                    _ => {
+                        return Err(DataErrorKind::IdentifierNotFound
+                            .into_error()
+                            .with_debug_context(length))
+                    }
+                };
+
+                let units_map = length_patterns.categories.get(category).ok_or_else(|| {
+                    DataErrorKind::InvalidRequest
+                        .into_error()
+                        .with_debug_context(category)
+                })?;
+                for (unit, patterns) in units_map {
+                    if patterns.other.is_none() {
+                        continue;
+                    }
+
+                    if cldr_serde::units::preferences::Supplemental::unit_type(
+                        category,
+                        unit,
+                        &region,
+                        &categorized_units_list,
+                    ) != unit_type
+                    {
+                        continue;
+                    }
+
+                    data_locales.insert(DataIdentifierCow::from_owned(
+                        DataMarkerAttributes::try_from_string(format!("{length}-{unit}")).map_err(
+                            |_| {
+                                DataError::custom("Failed to parse the attribute")
+                                    .with_debug_context(&unit)
+                            },
+                        )?,
+                        locale,
+                    ));
+                }
+            }
+        }
+
+        Ok(data_locales)
+    }
 }
 
 macro_rules! impl_units_display_name_provider {
@@ -163,13 +161,13 @@ macro_rules! impl_units_display_name_provider {
         impl DataProvider<$marker> for SourceDataProvider {
             fn load(&self, req: DataRequest) -> Result<DataResponse<$marker>, DataError> {
                 self.check_req::<$marker>(req)?;
-                get_display_name_payload::<$marker>(self, req)
+                self.get_display_name_payload::<$marker>(req)
             }
         }
 
         impl crate::IterableDataProviderCached<$marker> for SourceDataProvider {
             fn iter_ids_cached(&self) -> Result<HashSet<DataIdentifierCow<'static>>, DataError> {
-                get_display_name_iter_ids_cached(self, $unit_type, $category)
+                self.get_display_name_iter_ids_cached($unit_type, $category)
             }
         }
     };
