d08dc4433379cd3b4110fd175287f2743b65d99d
Manish Goregaokar <manishsmail@gmail.com>
2025-09-18 12:15:31 -0700

    Remove namespaces

--- ffi/capi/src/properties_bidi.rs
+++ ffi/capi/src/properties_bidi.rs
@@ -4,7 +4,6 @@
 
 #[diplomat::bridge]
 #[diplomat::abi_rename = "icu4x_{0}_mv1"]
-#[diplomat::attr(auto, namespace = "icu4x")]
 pub mod ffi {
     use diplomat_runtime::DiplomatChar;
     use icu_properties::props;
