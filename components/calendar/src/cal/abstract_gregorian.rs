ff8321a16a44962ba570c7d6716d1db2591ce7a5
Robert Bastian <robertbastian@unicode.org>
2025-10-14 14:56:43 +0200

    iso

--- components/calendar/src/cal/abstract_gregorian.rs
+++ components/calendar/src/cal/abstract_gregorian.rs
@@ -112,6 +112,8 @@ impl<Y: GregorianYears> Calendar for AbstractGregorian<Y> {
         calendrical_calculations::gregorian::fixed_from_gregorian(date.year, date.month, date.day)
     }
 
+    const HAS_CHEAP_ISO_CONVERSION: bool = true;
+
     fn from_iso(&self, iso: IsoDateInner) -> Self::DateInner {
         iso.0
     }
@@ -219,6 +221,8 @@ macro_rules! impl_with_abstract_gregorian {
                 let $self_ident = self;
                 crate::cal::abstract_gregorian::AbstractGregorian($eras_expr).to_rata_die(&date.0)
             }
+
+            const HAS_CHEAP_ISO_CONVERSION: bool = true;
 
             fn from_iso(&self, iso: crate::cal::iso::IsoDateInner) -> Self::DateInner {
                 let $self_ident = self;
