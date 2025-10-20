0455cbc76805bcace221573f242e2a7a82d10322
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2025-10-20 19:33:57 +0200

    Clean up `icu_calendar::provider::{chinese_based, hijri}` modules (#7129)
    This is not "provider" code anymore.

--- components/calendar/src/cal/chinese/qing_data.rs
+++ components/calendar/src/cal/chinese/qing_data.rs
@@ -5,24 +5,28 @@
 //! This data is confirmed by both the [Korea Astronomy and Space Science Institute](https://astro.kasi.re.kr/life/pageView/5),
 //! as well as the [Hong Kong Observatory](https://www.hko.gov.hk/en/gts/time/conversion.htm).
 
-use crate::provider::chinese_based::{ChineseBasedCache, PackedChineseBasedYearInfo};
-use calendrical_calculations::gregorian::fixed_from_gregorian as gregorian;
+use super::PackedLunarChineseYearData;
 
-pub const DATA: ChineseBasedCache = ChineseBasedCache {
-    first_related_iso_year: 1900,
-    #[rustfmt::skip]
-    data: { let l = true; let s = false; &[
-        PackedChineseBasedYearInfo::new(1900, [s, l, s, s, l, s, l, l, s, l, l, s, l], Some(9), gregorian(1900, 1, 31)),
-        PackedChineseBasedYearInfo::new(1901, [s, l, s, s, l, s, l, s, l, l, l, s, s], None, gregorian(1901, 2, 19)),
-        PackedChineseBasedYearInfo::new(1902, [l, s, l, s, s, l, s, l, s, l, l, l, s], None, gregorian(1902, 2, 8)),
-        PackedChineseBasedYearInfo::new(1903, [s, l, s, l, s, s, l, s, s, l, l, s, l], Some(6), gregorian(1903, 1, 29)),
-        PackedChineseBasedYearInfo::new(1904, [l, l, s, l, s, s, l, s, s, l, l, s, s], None, gregorian(1904, 2, 16)),
-        PackedChineseBasedYearInfo::new(1905, [l, l, s, l, l, s, s, l, s, l, s, l, s], None, gregorian(1905, 2, 4)),
-        PackedChineseBasedYearInfo::new(1906, [s, l, l, s, l, s, l, s, l, s, l, s, l], Some(5), gregorian(1906, 1, 25)),
-        PackedChineseBasedYearInfo::new(1907, [s, l, s, l, s, l, l, s, l, s, l, s, s], None, gregorian(1907, 2, 13)),
-        PackedChineseBasedYearInfo::new(1908, [l, s, s, l, l, s, l, s, l, l, s, l, s], None, gregorian(1908, 2, 2)),
-        PackedChineseBasedYearInfo::new(1909, [s, l, s, s, l, s, l, s, l, l, l, s, l], Some(3), gregorian(1909, 1, 22)),
-        PackedChineseBasedYearInfo::new(1910, [s, l, s, s, l, s, l, s, l, l, l, s, s], None, gregorian(1910, 2, 10)),
-        PackedChineseBasedYearInfo::new(1911, [l, s, l, s, s, l, s, s, l, l, s, l, l], Some(7), gregorian(1911, 1, 30)),
-    ]},
+pub const STARTING_YEAR: i32 = 1900;
+
+#[rustfmt::skip]
+#[allow(clippy::unwrap_used)] // const
+pub const DATA: &[PackedLunarChineseYearData] = {
+    use calendrical_calculations::gregorian::fixed_from_gregorian as gregorian;
+    let l = true; // long
+    let s = false; // short
+    &[
+        PackedLunarChineseYearData::new(1900, [s, l, s, s, l, s, l, l, s, l, l, s, l], Some(9), gregorian(1900, 1, 31)),
+        PackedLunarChineseYearData::new(1901, [s, l, s, s, l, s, l, s, l, l, l, s, s], None, gregorian(1901, 2, 19)),
+        PackedLunarChineseYearData::new(1902, [l, s, l, s, s, l, s, l, s, l, l, l, s], None, gregorian(1902, 2, 8)),
+        PackedLunarChineseYearData::new(1903, [s, l, s, l, s, s, l, s, s, l, l, s, l], Some(6), gregorian(1903, 1, 29)),
+        PackedLunarChineseYearData::new(1904, [l, l, s, l, s, s, l, s, s, l, l, s, s], None, gregorian(1904, 2, 16)),
+        PackedLunarChineseYearData::new(1905, [l, l, s, l, l, s, s, l, s, l, s, l, s], None, gregorian(1905, 2, 4)),
+        PackedLunarChineseYearData::new(1906, [s, l, l, s, l, s, l, s, l, s, l, s, l], Some(5), gregorian(1906, 1, 25)),
+        PackedLunarChineseYearData::new(1907, [s, l, s, l, s, l, l, s, l, s, l, s, s], None, gregorian(1907, 2, 13)),
+        PackedLunarChineseYearData::new(1908, [l, s, s, l, l, s, l, s, l, l, s, l, s], None, gregorian(1908, 2, 2)),
+        PackedLunarChineseYearData::new(1909, [s, l, s, s, l, s, l, s, l, l, l, s, l], Some(3), gregorian(1909, 1, 22)),
+        PackedLunarChineseYearData::new(1910, [s, l, s, s, l, s, l, s, l, l, l, s, s], None, gregorian(1910, 2, 10)),
+        PackedLunarChineseYearData::new(1911, [l, s, l, s, s, l, s, s, l, l, s, l, l], Some(7), gregorian(1911, 1, 30)),
+    ]
 };
