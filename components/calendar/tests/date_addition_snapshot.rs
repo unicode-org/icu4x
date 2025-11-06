644e38c806c576794bc441d2f10b9c421df0815b
Satvik Upadhyaya <futureiitianisme@gmail.com>
2025-11-06 09:36:12 +0000

    Add comprehensive snapshot tests for date addition across multiple calendars and durations

--- components/calendar/tests/date_addition_snapshot.rs
+++ components/calendar/tests/date_addition_snapshot.rs
@@ -1,14 +1,57 @@
-use icu_calendar::{options::DateAddOptions, types::DateDuration, Date};
-use insta::assert_snapshot;
+use icu_calendar::{
+    AnyCalendar, AnyCalendarKind, Date,
+    types::DateDuration,
+    options::DateAddOptions,
+};
+use insta::assert_debug_snapshot;
 
 #[test]
-fn test_date_add_until_snapshot() {
-    let mut date = Date::try_new_iso(2024, 11, 5).unwrap();
-    let duration = DateDuration::for_days(10);
-    let options = DateAddOptions::default();
+fn test_date_add_across_calendars_and_durations() {
+    let calendars = vec![
+        AnyCalendarKind::Iso,
+        AnyCalendarKind::Gregorian,
+        AnyCalendarKind::Buddhist,
+    ];
 
-    date.try_add_with_options(duration, options).unwrap();
+    let iso_dates = vec![
+        (2023, 1, 1),
+        (2024, 6, 15),
+        (2025, 12, 31),
+    ];
 
-    let result = format!("Resulting ISO date after +10 days: {:?}", date.to_iso());
-    assert_snapshot!("added_date_result", result);
+    let durations = vec![
+        DateDuration { years: 0, months: 0, weeks: 0, days: 10, is_negative: false },
+        DateDuration { years: 0, months: 2, weeks: 0, days: 0, is_negative: false },
+        DateDuration { years: 1, months: 0, weeks: 0, days: 0, is_negative: false },
+        DateDuration { years: 1, months: 3, weeks: 0, days: 15, is_negative: false },
+    ];
+
+    let opts = DateAddOptions::default();
+
+    for (y, m, d) in &iso_dates {
+        for duration in &durations {
+            for cal_kind in &calendars {
+                let cal = AnyCalendar::new(*cal_kind);
+
+                let mut date = Date::try_new_iso(*y, *m, *d)
+                    .expect("Valid ISO date")
+                    .to_calendar(cal.clone());
+
+                date.try_add_with_options(duration.clone(), opts)
+                    .expect("Addition should succeed");
+
+                let result_iso = date.to_iso();
+
+                assert_debug_snapshot!(
+                    format!("{:?}_{:?}_{:?}_{:?}", y, m, d, cal_kind),
+                    (
+                        format!("Start: {:?}-{:?}-{:?}", y, m, d),
+                        format!("Calendar: {:?}", cal_kind),
+                        format!("Duration added: {:?}", duration),
+                        format!("Resulting ISO date: {:?}", result_iso)
+                    )
+                );
+            }
+        }
+    }
 }
