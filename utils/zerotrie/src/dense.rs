8119ab2fde7f44caf42bf8aca0edf7051244dbb0
Shane F. Carr <shane@unicode.org>
2025-12-01 01:16:01 -1000

    Make a specific dense impl

--- utils/zerotrie/src/dense.rs
+++ utils/zerotrie/src/dense.rs
@@ -4,54 +4,55 @@
 
 use crate::ZeroTrieSimpleAscii;
 use zerovec::ZeroSlice;
-use zerovec::ule::AsULE;
 
-/// The key type for storage in a [`DenseSparse2d`].
-pub trait DenseSparse2dConfig {
-	/// The type of the key fields, probably `str` or `[u8]`.
-    type Key: ?Sized;
-    /// The type of the value, probably `u16` or `u32`.
-    type Value: AsULE;
-    /// The type of trie for inner storage.
-    type TrieBorrowed<'a>;
-    /// The type of dense inner storage.
-    type DenseBorrowed<'a>;
-    /// Returns a string to delimit key0 and key1 for sparse storage.
-    fn delimit<'a>(&self, key0: &'a Self::Key, key1: &'a Self::Key) -> &'_ Self::Key;
+/// A data structure designed for 2-dimensional ASCII keys with a fixed
+/// delimiter that exhibit a mix of density and sparsity.
+pub struct DenseSparse2dAsciiWithFixedDelimiterBorrowed<'a> {
+    primary: &'a ZeroTrieSimpleAscii<[u8]>,
+	suffixes: &'a ZeroTrieSimpleAscii<[u8]>,
+	dense: &'a ZeroSlice<u16>,
+	suffix_count: u16,
+    delimiter: u8,
 }
 
-/// A key type that allows ASCII strings with a fixed delimiter. No strings
-/// can contain the delimiter.
-#[non_exhaustive]
-pub struct AsciiWithFixedDelimiter {
-    /// The delimiter, used between fields.
-    pub delimiter: &'static str,
-}
-
-impl DenseSparse2dConfig for AsciiWithFixedDelimiter {
-	type Key = str;
-	type Value = u32;
-	type TrieBorrowed<'a> = &'a ZeroTrieSimpleAscii<[u8]>;
-	type DenseBorrowed<'a> = &'a ZeroSlice<u16>;
-	fn delimit<'a>(&self, key0: &'a str, key1: &'a str) -> &'_ str {
-		debug_assert!(!key0.contains(self.delimiter));
-		debug_assert!(!key1.contains(self.delimiter));
-		&self.delimiter
-	}
-}
-
-/// A data structure designed for 2-dimensional keys that exhibit a mix of
-/// density and sparsity.
-pub struct DenseSparse2dBorrowed<'a, Config: DenseSparse2dConfig> {
-    primary: Config::TrieBorrowed<'a>,
-	secondary: Config::TrieBorrowed<'a>,
-	dense: Config::DenseBorrowed<'a>,
-	offsets: &'a ZeroSlice<Config::Value>,
-    config: Config,
-}
-
-impl<'a, Config: DenseSparse2dConfig> DenseSparse2dBorrowed<'a, Config> {
-    pub fn get(key0: &Config::Key, key1: &Config::Key) -> usize {
-        todo!()
+impl<'a> DenseSparse2dAsciiWithFixedDelimiterBorrowed<'a> {
+	/// Queries the data structure for a value
+    pub fn get(&self, prefix: &str, suffix: &str) -> Option<usize> {
+        let mut cursor = self.primary.cursor();
+        for b in prefix.as_bytes().iter() {
+        	if cursor.is_empty() {
+        		return None;
+        	}
+        	cursor.step(*b);
+        }
+        let row_index = cursor.take_value();
+        cursor.step(self.delimiter);
+        let row_value_offset = cursor.take_value();
+        // Check for a sparse value
+        for b in suffix.as_bytes().iter() {
+        	if cursor.is_empty() {
+        		break;
+        	}
+        	cursor.step(*b);
+        }
+        if let Some(value) = cursor.take_value() {
+        	// Found a sparse value
+        	return Some(value);
+        }
+        let (Some(row_index), Some(row_value_offset)) = (row_index, row_value_offset) else {
+        	// There is no dense row for this prefix
+        	return None;
+        };
+        let Some(column_index) = self.suffixes.get(suffix) else {
+        	// There is no dense column for this suffix
+        	return None;
+        };
+        let suffix_count = usize::from(self.suffix_count);
+        let Some(offset) = self.dense.get(suffix_count*row_index + column_index) else {
+        	// The row and column indexes should be in-range
+        	debug_assert!(false);
+        	return None;
+        };
+        Some(usize::from(offset) + usize::from(row_value_offset))
     }
 }
