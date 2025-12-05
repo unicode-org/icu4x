600bc5ffdf84af93e1685e08b6af0e3c79392775
Robert Bastian <robertbastian@unicode.org>
2025-12-05 18:55:51 +0100

    extended-year

--- components/calendar/src/any_calendar.rs
+++ components/calendar/src/any_calendar.rs
@@ -1589,7 +1589,7 @@ mod tests {
         } else {
             assert_eq!(
                 year,
-                date.extended_year(),
+                date.year().extended_year(),
                 "Failed to roundtrip year for calendar {}",
                 calendar.debug_name()
             );
