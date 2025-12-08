0656e9836d629e2dc733c28393744dbeaff02a34
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2025-12-08 15:15:34 +0100

    Validate property names (#7284)

--- components/properties/src/props.rs
+++ components/properties/src/props.rs
@@ -1884,8 +1884,8 @@ make_binary_property! {
 }
 
 make_binary_property! {
-    name: "Alnum";
-    short_name: "Alnum";
+    name: "alnum";
+    short_name: "alnum";
     ident: Alnum;
     data_marker: crate::provider::PropertyBinaryAlnumV1;
     singleton: SINGLETON_PROPERTY_BINARY_ALNUM_V1;
@@ -1966,8 +1966,8 @@ make_binary_property! {
 }
 
 make_binary_property! {
-    name: "Blank";
-    short_name: "Blank";
+    name: "blank";
+    short_name: "blank";
     ident: Blank;
     data_marker: crate::provider::PropertyBinaryBlankV1;
     singleton: SINGLETON_PROPERTY_BINARY_BLANK_V1;
@@ -2410,8 +2410,8 @@ make_binary_property! {
 }
 
 make_binary_property! {
-    name: "Graph";
-    short_name: "Graph";
+    name: "graph";
+    short_name: "graph";
     ident: Graph;
     data_marker: crate::provider::PropertyBinaryGraphV1;
     singleton: SINGLETON_PROPERTY_BINARY_GRAPH_V1;
@@ -2542,7 +2542,7 @@ make_binary_property! {
 }
 
 make_binary_property! {
-    name: "Id_Continue";
+    name: "ID_Continue";
     short_name: "IDC";
     ident: IdContinue;
     data_marker: crate::provider::PropertyBinaryIdContinueV1;
@@ -2594,7 +2594,7 @@ make_binary_property! {
 }
 
 make_binary_property! {
-    name: "Id_Start";
+    name: "ID_Start";
     short_name: "IDS";
     ident: IdStart;
     data_marker: crate::provider::PropertyBinaryIdStartV1;
@@ -2623,7 +2623,7 @@ make_binary_property! {
 }
 
 make_binary_property! {
-    name: "Ids_Binary_Operator";
+    name: "IDS_Binary_Operator";
     short_name: "IDSB";
     ident: IdsBinaryOperator;
     data_marker: crate::provider::PropertyBinaryIdsBinaryOperatorV1;
@@ -2644,7 +2644,7 @@ make_binary_property! {
 }
 
 make_binary_property! {
-    name: "Ids_Trinary_Operator";
+    name: "IDS_Trinary_Operator";
     short_name: "IDST";
     ident: IdsTrinaryOperator;
     data_marker: crate::provider::PropertyBinaryIdsTrinaryOperatorV1;
@@ -2799,7 +2799,7 @@ make_binary_property! {
 
 make_binary_property! {
     name: "NFC_Inert";
-    short_name: "NFC_Inert";
+    short_name: "nfcinert";
     ident: NfcInert;
     data_marker: crate::provider::PropertyBinaryNfcInertV1;
     singleton: SINGLETON_PROPERTY_BINARY_NFC_INERT_V1;
@@ -2808,7 +2808,7 @@ make_binary_property! {
 
 make_binary_property! {
     name: "NFD_Inert";
-    short_name: "NFD_Inert";
+    short_name: "nfdinert";
     ident: NfdInert;
     data_marker: crate::provider::PropertyBinaryNfdInertV1;
     singleton: SINGLETON_PROPERTY_BINARY_NFD_INERT_V1;
@@ -2817,7 +2817,7 @@ make_binary_property! {
 
 make_binary_property! {
     name: "NFKC_Inert";
-    short_name: "NFKC_Inert";
+    short_name: "nfkcinert";
     ident: NfkcInert;
     data_marker: crate::provider::PropertyBinaryNfkcInertV1;
     singleton: SINGLETON_PROPERTY_BINARY_NFKC_INERT_V1;
@@ -2826,7 +2826,7 @@ make_binary_property! {
 
 make_binary_property! {
     name: "NFKD_Inert";
-    short_name: "NFKD_Inert";
+    short_name: "nfkdinert";
     ident: NfkdInert;
     data_marker: crate::provider::PropertyBinaryNfkdInertV1;
     singleton: SINGLETON_PROPERTY_BINARY_NFKD_INERT_V1;
@@ -2897,8 +2897,8 @@ make_binary_property! {
 }
 
 make_binary_property! {
-    name: "Print";
-    short_name: "Print";
+    name: "print";
+    short_name: "print";
     ident: Print;
     data_marker: crate::provider::PropertyBinaryPrintV1;
     singleton: SINGLETON_PROPERTY_BINARY_PRINT_V1;
@@ -2998,7 +2998,7 @@ make_binary_property! {
 
 make_binary_property! {
     name: "Segment_Starter";
-    short_name: "Segment_Starter";
+    short_name: "segstart";
     ident: SegmentStarter;
     data_marker: crate::provider::PropertyBinarySegmentStarterV1;
     singleton: SINGLETON_PROPERTY_BINARY_SEGMENT_STARTER_V1;
@@ -3008,7 +3008,7 @@ make_binary_property! {
 
 make_binary_property! {
     name: "Case_Sensitive";
-    short_name: "Case_Sensitive";
+    short_name: "Sensitive";
     ident: CaseSensitive;
     data_marker: crate::provider::PropertyBinaryCaseSensitiveV1;
     singleton: SINGLETON_PROPERTY_BINARY_CASE_SENSITIVE_V1;
@@ -3133,7 +3133,7 @@ make_binary_property! {
 
 make_binary_property! {
     name: "White_Space";
-    short_name: "space";
+    short_name: "WSpace";
     ident: WhiteSpace;
     data_marker: crate::provider::PropertyBinaryWhiteSpaceV1;
     singleton: SINGLETON_PROPERTY_BINARY_WHITE_SPACE_V1;
@@ -3156,8 +3156,8 @@ make_binary_property! {
 }
 
 make_binary_property! {
-    name: "Xdigit";
-    short_name: "Xdigit";
+    name: "xdigit";
+    short_name: "xdigit";
     ident: Xdigit;
     data_marker: crate::provider::PropertyBinaryXdigitV1;
     singleton: SINGLETON_PROPERTY_BINARY_XDIGIT_V1;
@@ -3227,6 +3227,8 @@ pub use crate::emoji::EmojiSet;
 
 macro_rules! make_emoji_set {
     (
+        name: $name:literal;
+        short_name: $short_name:literal;
         ident: $ident:ident;
         data_marker: $data_marker:ty;
         singleton: $singleton:ident;
@@ -3244,11 +3246,15 @@ macro_rules! make_emoji_set {
             #[cfg(feature = "compiled_data")]
             const SINGLETON: &'static crate::provider::PropertyUnicodeSet<'static> =
                 &crate::provider::Baked::$singleton;
+            const NAME: &'static [u8] = $name.as_bytes();
+            const SHORT_NAME: &'static [u8] = $short_name.as_bytes();
         }
     }
 }
 
 make_emoji_set! {
+    name: "Basic_Emoji";
+    short_name: "Basic_Emoji";
     ident: BasicEmoji;
     data_marker: crate::provider::PropertyBinaryBasicEmojiV1;
     singleton: SINGLETON_PROPERTY_BINARY_BASIC_EMOJI_V1;
--- provider/source/src/properties/bidi.rs
+++ provider/source/src/properties/bidi.rs
@@ -8,24 +8,6 @@ use crate::SourceDataProvider;
 use icu::properties::provider::PropertyEnumBidiMirroringGlyphV1;
 use icu_provider::prelude::*;
 
-#[cfg(any(feature = "use_wasm", feature = "use_icu4c"))]
-impl SourceDataProvider {
-    fn get_code_point_prop_map<'a>(
-        &'a self,
-        key: &str,
-    ) -> Result<&'a super::uprops_serde::code_point_prop::CodePointPropertyMap, DataError> {
-        self.icuexport()?
-            .read_and_parse_toml::<super::uprops_serde::code_point_prop::Main>(&format!(
-                "uprops/{}/{}.toml",
-                self.trie_type(),
-                key
-            ))?
-            .enum_property
-            .first()
-            .ok_or_else(|| DataErrorKind::MarkerNotFound.into_error())
-    }
-}
-
 // implement data provider 2 different ways, based on whether or not
 // features exist that enable the use of CPT Builder (ex: `use_wasm` or `use_icu4c`)
 impl DataProvider<PropertyEnumBidiMirroringGlyphV1> for SourceDataProvider {
@@ -34,46 +16,40 @@ impl DataProvider<PropertyEnumBidiMirroringGlyphV1> for SourceDataProvider {
         &self,
         req: DataRequest,
     ) -> Result<DataResponse<PropertyEnumBidiMirroringGlyphV1>, DataError> {
-        use icu::collections::codepointinvlist::CodePointInversionListBuilder;
-        use icu::collections::codepointtrie::CodePointTrie;
         use icu::collections::codepointtrie::TrieType;
         use icu::collections::codepointtrie::TrieValue;
         use icu::properties::props::BidiMirroringGlyph;
         use icu::properties::props::BidiPairedBracketType;
+        use icu::properties::props::EnumeratedProperty;
         use icu_codepointtrie_builder::{CodePointTrieBuilder, CodePointTrieBuilderData};
 
         self.check_req::<PropertyEnumBidiMirroringGlyphV1>(req)?;
 
-        // Bidi_M / Bidi_Mirrored
-        let bidi_m_data = self.get_binary_prop_for_code_point_set("Bidi_M")?;
-        let mut bidi_m_builder = CodePointInversionListBuilder::new();
-        for (start, end) in &bidi_m_data.ranges {
-            bidi_m_builder.add_range32(start..=end);
-        }
-        let bidi_m_cpinvlist = bidi_m_builder.build();
+        let bidi_m_cpinvlist = self
+            .get_binary_prop_for_code_point_set("Bidi_Mirrored", "Bidi_M")?
+            .build_inversion_list();
 
-        // bmg / Bidi_Mirroring_Glyph
-        let bmg_data = &self.get_code_point_prop_map("bmg")?.code_point_trie;
-        let bmg_trie = CodePointTrie::try_from(bmg_data).map_err(|e| {
-            DataError::custom("Could not parse CodePointTrie TOML").with_display_context(&e)
-        })?;
+        let bmg_trie = self
+            .get_enumerated_prop(
+                core::str::from_utf8(BidiMirroringGlyph::NAME).unwrap(),
+                core::str::from_utf8(BidiMirroringGlyph::SHORT_NAME).unwrap(),
+            )?
+            .build_codepointtrie()?;
 
-        // bpt / Bidi_Paired_Bracket_Type
-        let bpt_data = &self.get_enumerated_prop("bpt")?.code_point_trie;
-        let bpt_trie = CodePointTrie::try_from(bpt_data).map_err(|e| {
-            DataError::custom("Could not parse CodePointTrie TOML").with_display_context(&e)
-        })?;
+        let bpt = self.get_enumerated_prop("Bidi_Paired_Bracket_Type", "bpt")?;
+        let bpt_trie = bpt.build_codepointtrie::<u16>()?;
+        let bpt_lookup = bpt.values_to_names_long();
 
         let trie_vals = (0..=(char::MAX as u32)).map(|cp| {
             let mut r = BidiMirroringGlyph::default();
-            r.mirrored = bidi_m_cpinvlist.contains32(cp);
-            r.mirroring_glyph = r
-                .mirrored
-                .then_some(bmg_trie.get32(cp))
-                .filter(|&cp| cp as u32 != 0);
-            r.paired_bracket_type = match bpt_trie.get32(cp) {
-                1 => BidiPairedBracketType::Open,
-                2 => BidiPairedBracketType::Close,
+            if !bidi_m_cpinvlist.contains32(cp) {
+                return r;
+            }
+            r.mirrored = true;
+            r.mirroring_glyph = Some(bmg_trie.get32(cp)).filter(|&cp| cp as u32 != 0);
+            r.paired_bracket_type = match bpt_lookup[&(bpt_trie.get32(cp))] {
+                "Open" => BidiPairedBracketType::Open,
+                "Close" => BidiPairedBracketType::Close,
                 _ => BidiPairedBracketType::None,
             };
             if r.mirrored && r.mirroring_glyph.is_none() {
