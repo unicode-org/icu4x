63065dc94329984b70bc198673c61eb38a4ff80f
SATVIKsynopsis <futureiitianisme@gmail.com>
2025-12-10 00:56:38 +0530

    Remove example and Cargo changes from this PR

--- components/calendar/examples/date_try_from_fields.rs
+++ components/calendar/examples/date_try_from_fields.rs
@@ -1,25 +1,15 @@
-// This file is part of ICU4X. For terms of use, please see the file
-// called LICENSE at the top level of the ICU4X source tree
-// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
-
-#![no_main]
-
-use icu_calendar::error::DateFromFieldsError;
-use icu_calendar::types::DateFields;
 use icu_calendar::{AnyCalendar, AnyCalendarKind, Date};
+use icu_calendar::types::DateFields;
+use icu_calendar::error::DateFromFieldsError;
 
-#[no_mangle]
-pub extern "C" fn main() {
-    real_main().unwrap();
-}
-
-fn real_main() -> Result<(), DateFromFieldsError> {
+fn main() -> Result<(), DateFromFieldsError> {
     let cal = AnyCalendar::new(AnyCalendarKind::Iso);
 
     let mut fields = DateFields::default();
-    fields.extended_year = Some(2025);
-    fields.month_code = Some(b"M07");
-    fields.day = Some(8);
+
+    fields.extended_year = Some(2024);
+    fields.month_code = Some(b"M03");  
+    fields.day = Some(15);
 
     let date = Date::try_from_fields(fields, Default::default(), cal)?;
 
