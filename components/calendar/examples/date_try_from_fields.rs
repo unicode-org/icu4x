76bf5fdb266213c7a002d6b9d9e59eb1badb620a
SATVIKsynopsis <futureiitianisme@gmail.com>
2025-12-09 22:25:10 +0530

    added the date_try_from_fields example file

---
+++ components/calendar/examples/date_try_from_fields.rs
@@ -0,0 +1,23 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+use icu_calendar::error::DateFromFieldsError;
+use icu_calendar::types::DateFields;
+use icu_calendar::{AnyCalendar, AnyCalendarKind, Date};
+
+fn main() -> Result<(), DateFromFieldsError> {
+    let cal = AnyCalendar::new(AnyCalendarKind::Iso);
+
+    let mut fields = DateFields::default();
+
+    fields.extended_year = Some(2025);
+    fields.month_code = Some(b"M07");
+    fields.day = Some(8);
+
+    let date = Date::try_from_fields(fields, Default::default(), cal)?;
+
+    println!("Constructed date = {:?}", date);
+
+    Ok(())
+}
