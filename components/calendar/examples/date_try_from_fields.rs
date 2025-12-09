066dc633ab7877d1bdb5a8939a6a341643b24b6c
SATVIKsynopsis <futureiitianisme@gmail.com>
2025-12-09 23:22:48 +0530

    added #[no_main] and icu_benchmark_macros

--- components/calendar/examples/date_try_from_fields.rs
+++ components/calendar/examples/date_try_from_fields.rs
@@ -2,10 +2,13 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+#![no_main]
+use icu_benchmark_macros::{main_with_provider, println};
 use icu_calendar::error::DateFromFieldsError;
 use icu_calendar::types::DateFields;
 use icu_calendar::{AnyCalendar, AnyCalendarKind, Date};
 
+#[main_with_provider]
 fn main() -> Result<(), DateFromFieldsError> {
     let cal = AnyCalendar::new(AnyCalendarKind::Iso);
 
@@ -15,7 +18,7 @@ fn main() -> Result<(), DateFromFieldsError> {
     fields.month_code = Some(b"M07");
     fields.day = Some(8);
 
-    let date = Date::try_from_fields(fields, Default::default(), cal)?;
+    let date = Date::try_from_fields(fields, Default::default(), &cal)?;
 
     println!("Constructed date = {:?}", date);
 
