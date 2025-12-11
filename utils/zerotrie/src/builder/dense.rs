b57d7975b8cd1e78c31aecd85d6c566e86466e61
Shane F. Carr <shane@unicode.org>
2025-12-10 17:21:39 -0800

    Add ZeroAsciiDenseSparse2dTrie for more efficient storage of data keys with many attributes (#7264)
    #6920

---
+++ utils/zerotrie/src/builder/dense.rs
@@ -0,0 +1,186 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+use crate::dense::DenseType;
+use crate::dense::ZeroAsciiDenseSparse2dTrieOwned;
+use crate::ZeroTrieBuildError;
+use crate::ZeroTrieSimpleAscii;
+use alloc::collections::BTreeMap;
+use alloc::collections::BTreeSet;
+use alloc::string::String;
+use alloc::vec::Vec;
+use zerovec::ZeroVec;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Row<'a> {
+    prefix: &'a str,
+    row_value_offset: usize,
+    offsets: Vec<DenseType>,
+}
+
+#[derive(Default)]
+pub(crate) struct DenseSparse2dAsciiWithFixedDelimiterBuilder<'a> {
+    primary: Vec<(&'a str, &'a str, usize)>,
+    suffixes: BTreeSet<&'a str>,
+    dense: Vec<Row<'a>>,
+    delimiter: u8,
+}
+
+impl<'a> DenseSparse2dAsciiWithFixedDelimiterBuilder<'a> {
+    /// Add a prefix and all values associated with the prefix to the builder.
+    pub(crate) fn add_prefix(
+        &mut self,
+        prefix: &'a str,
+        values: &BTreeMap<&'a str, usize>,
+    ) -> Result<(), ZeroTrieBuildError> {
+        // TODO: Is there a more Rusty way to compute min and max together?
+        let mut min = usize::MAX;
+        let mut max = 0;
+        for value in values.values() {
+            min = core::cmp::min(min, *value);
+            max = core::cmp::max(max, *value);
+        }
+        // >= because DenseType::MAX is the sentinel
+        if max - min >= usize::from(DenseType::MAX) {
+            // How to implement this when we need it:
+            // 1. Select the row offset that gets the greatest number of values into the dense matrix.
+            // 2. Put all out-of-range values into the primary trie, as we do with sparse rows.
+            todo!("values in row are not in a sufficiently compact range");
+        }
+        let sentinel = min + usize::from(DenseType::MAX);
+        // Partition the entries based on whether they can be encoded as dense
+        let (dense_or_sparse, sparse_only) = values
+            .iter()
+            .map(|(suffix, value)| (*suffix, *value))
+            .partition::<BTreeMap<&'a str, usize>, _>(|(suffix, _value)| {
+                // TODO: Also filter for suffixes that are out of range of the dense row offset
+                self.suffixes.contains(suffix)
+            });
+        // Check whether the sparse trie is smaller than the dense row
+        let sub_trie = dense_or_sparse
+            .iter()
+            .map(|(suffix, value)| (*suffix, *value))
+            .collect::<ZeroTrieSimpleAscii<Vec<u8>>>();
+        if sub_trie.byte_len() > self.suffixes.len() * core::mem::size_of::<DenseType>() {
+            // Create a dense prefix
+            let row_value_offset = min;
+            let offsets = self
+                .suffixes
+                .iter()
+                .map(|suffix| {
+                    let value = sub_trie.get(suffix).unwrap_or(sentinel);
+                    let Ok(offset) = DenseType::try_from(value - row_value_offset) else {
+                        unreachable!("this should have been handled earlier");
+                    };
+                    offset
+                })
+                .collect::<Vec<DenseType>>();
+            self.dense.push(Row {
+                prefix,
+                row_value_offset,
+                offsets,
+            });
+            for (suffix, value) in sparse_only.iter() {
+                self.primary.push((prefix, *suffix, *value));
+            }
+            Ok(())
+        } else {
+            // Create a sparse prefix
+            for (suffix, value) in values.iter() {
+                self.primary.push((prefix, *suffix, *value));
+            }
+            Ok(())
+        }
+    }
+
+    /// Assemble the intermediate structures into the final layout.
+    pub(crate) fn build(mut self) -> Result<ZeroAsciiDenseSparse2dTrieOwned, ZeroTrieBuildError> {
+        self.dense.sort();
+        let Ok(suffix_count) = DenseType::try_from(self.suffixes.len()) else {
+            return Err(ZeroTrieBuildError::CapacityExceeded);
+        };
+        let delimiter = self.delimiter as char;
+        let mut primary_contents = BTreeMap::new();
+        for (prefix, suffix, value) in self.primary.iter() {
+            if prefix.contains(delimiter) || suffix.contains(delimiter) {
+                debug_assert!(false, "handled earlier");
+                return Err(ZeroTrieBuildError::IllegalDelimiter);
+            }
+            let mut delimited = String::with_capacity(prefix.len() + suffix.len() + 1);
+            delimited.push_str(prefix);
+            delimited.push(delimiter);
+            delimited.push_str(suffix);
+            primary_contents.insert(delimited, *value);
+        }
+        let mut dense = Vec::<DenseType>::with_capacity(self.dense.len() * self.suffixes.len());
+        for (
+            row_index,
+            Row {
+                prefix,
+                row_value_offset,
+                offsets,
+            },
+        ) in self.dense.iter().enumerate()
+        {
+            primary_contents.insert(String::from(*prefix), row_index);
+            let mut prefix_with_delim = String::with_capacity(prefix.len() + 1);
+            prefix_with_delim.push_str(prefix);
+            prefix_with_delim.push(delimiter);
+            primary_contents.insert(prefix_with_delim, *row_value_offset);
+            dense.extend(offsets);
+        }
+        let suffixes = self
+            .suffixes
+            .iter()
+            .enumerate()
+            .map(|(column_index, suffix)| (*suffix, column_index))
+            .collect::<BTreeMap<&str, usize>>();
+        Ok(ZeroAsciiDenseSparse2dTrieOwned {
+            primary: ZeroTrieSimpleAscii::try_from_btree_map_str(&primary_contents)?,
+            suffixes: ZeroTrieSimpleAscii::try_from_btree_map_str(&suffixes)?,
+            dense: ZeroVec::from_slice_or_alloc(dense.as_slice()).into_owned(),
+            suffix_count,
+            delimiter: self.delimiter,
+        })
+    }
+}
+
+impl ZeroAsciiDenseSparse2dTrieOwned {
+    /// Builds one of these from a two-dimensional BTreeMap and a delimiter.
+    ///
+    /// Keep in mind the recommendations for optimal data size described in
+    /// the [class docs].
+    ///
+    /// [class docs]: ZeroAsciiDenseSparse2dTrieOwned
+    pub fn try_from_btree_map_str(
+        entries: &BTreeMap<&str, BTreeMap<&str, usize>>,
+        delimiter: u8,
+    ) -> Result<Self, ZeroTrieBuildError> {
+        let mut builder = DenseSparse2dAsciiWithFixedDelimiterBuilder {
+            delimiter,
+            ..Default::default()
+        };
+        // TODO: Prune low-frequency suffixes.
+        // For now, build with all suffixes.
+        builder.suffixes = entries
+            .values()
+            .flat_map(|inner| inner.keys())
+            .copied()
+            .map(|s| {
+                if s.contains(delimiter as char) {
+                    Err(ZeroTrieBuildError::IllegalDelimiter)
+                } else {
+                    Ok(s)
+                }
+            })
+            .collect::<Result<_, ZeroTrieBuildError>>()?;
+        for (prefix, values) in entries.iter() {
+            if prefix.contains(delimiter as char) {
+                return Err(ZeroTrieBuildError::IllegalDelimiter);
+            }
+            builder.add_prefix(prefix, values)?;
+        }
+        builder.build()
+    }
+}
---
+++ utils/zerotrie/tests/dense_test.rs
@@ -0,0 +1,139 @@
+// This file is part of ICU4X. For terms of use, please see the file
+// called LICENSE at the top level of the ICU4X source tree
+// (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
+
+use itertools::Either;
+use itertools::Itertools;
+use rand::Rng;
+use rand::SeedableRng;
+use std::collections::BTreeMap;
+use zerotrie::dense::ZeroAsciiDenseSparse2dTrieBorrowed;
+use zerotrie::dense::ZeroAsciiDenseSparse2dTrieOwned;
+use zerotrie::dense::ZeroAsciiDenseSparse2dTrieVarULE;
+use zerotrie::ZeroTrieSimpleAscii;
+use zerovec::VarZeroCow;
+
+mod testdata {
+    include!("data/data.rs");
+}
+
+const BASIC_DATA: &[&str] = &[
+    "ar/FR", "ar/IR", "ar/SA", "ar/UK", "ar/US", "en/AU", "en/FR", "en/UK", "en/US", "fr/FR",
+    "fr/SA", "fr/UK", "fr/US", "it/IT",
+];
+
+const NON_EXISTENT_BASIC_DATA: &[&str] = &["ar/IT", "en/ZA", "it/FR", "zh/UK"];
+
+fn strings_to_2d_btreemap<'a>(
+    strings: &[&'a str],
+    delimiter: &str,
+) -> BTreeMap<&'a str, BTreeMap<&'a str, usize>> {
+    let mut map = BTreeMap::<&str, BTreeMap<&str, usize>>::default();
+    for (value, (prefix, suffix)) in strings
+        .iter()
+        .flat_map(|s| s.split_once(delimiter))
+        .enumerate()
+    {
+        map.entry(prefix).or_default().insert(suffix, value);
+    }
+    map
+}
+
+fn check_data(
+    data: &BTreeMap<&str, BTreeMap<&str, usize>>,
+    trie: ZeroAsciiDenseSparse2dTrieBorrowed,
+    polarity: bool,
+) {
+    for (prefix, values) in data.iter() {
+        for (suffix, value) in values.iter() {
+            if polarity {
+                assert_eq!(trie.get(prefix, suffix), Some(*value), "{prefix}/{suffix}");
+            } else {
+                assert_eq!(trie.get(prefix, suffix), None, "{prefix}/{suffix}");
+            }
+        }
+    }
+}
+
+#[must_use]
+fn check_encoding(trie: ZeroAsciiDenseSparse2dTrieBorrowed) -> usize {
+    let ule =
+        VarZeroCow::<ZeroAsciiDenseSparse2dTrieVarULE>::from_encodeable(&trie.as_encodeable());
+    let decoded = ZeroAsciiDenseSparse2dTrieBorrowed::from(&*ule);
+    assert_eq!(decoded, trie);
+    ule.as_bytes().len()
+}
+
+#[must_use]
+fn make_simple_ascii_trie(
+    data: &BTreeMap<&str, BTreeMap<&str, usize>>,
+) -> ZeroTrieSimpleAscii<Vec<u8>> {
+    let mut data_for_simple_ascii_trie = BTreeMap::new();
+    for (prefix, values) in data.iter() {
+        for (suffix, value) in values.iter() {
+            data_for_simple_ascii_trie.insert(format!("{prefix}/{suffix}"), *value);
+        }
+    }
+    ZeroTrieSimpleAscii::try_from_btree_map_str(&data_for_simple_ascii_trie).unwrap()
+}
+
+#[test]
+fn test_basic() {
+    let data_as_map = strings_to_2d_btreemap(BASIC_DATA, "/");
+    let dense =
+        ZeroAsciiDenseSparse2dTrieOwned::try_from_btree_map_str(&data_as_map, b'/').unwrap();
+    check_data(&data_as_map, dense.as_borrowed(), true);
+    let non_existent_data_as_map = strings_to_2d_btreemap(NON_EXISTENT_BASIC_DATA, "/");
+    check_data(&non_existent_data_as_map, dense.as_borrowed(), false);
+    let byte_len = check_encoding(dense.as_borrowed());
+    assert_eq!(byte_len, 106);
+    let simple_trie = make_simple_ascii_trie(&data_as_map);
+    assert_eq!(simple_trie.byte_len(), 71);
+}
+
+#[test]
+fn test_locales_with_aux() {
+    let data_as_map = strings_to_2d_btreemap(testdata::locales_with_aux::STRINGS, "-x-");
+    let dense =
+        ZeroAsciiDenseSparse2dTrieOwned::try_from_btree_map_str(&data_as_map, b'/').unwrap();
+    check_data(&data_as_map, dense.as_borrowed(), true);
+    let byte_len = check_encoding(dense.as_borrowed());
+    assert_eq!(byte_len, 4045);
+    let simple_trie = make_simple_ascii_trie(&data_as_map);
+    assert_eq!(simple_trie.byte_len(), 4614);
+}
+
+#[test]
+fn test_short_subtags() {
+    let (prefixes, suffixes): (Vec<&str>, Vec<&str>) = testdata::short_subtags::STRINGS
+        .iter()
+        .copied()
+        .partition_map(|s| match s.strip_prefix("und-") {
+            Some(suffix) => Either::Right(suffix),
+            None => Either::Left(s),
+        });
+    assert_eq!(prefixes.len(), 1427);
+    assert_eq!(suffixes.len(), 450);
+    let mut counter = 0;
+    let mut rng = rand_pcg::Lcg64Xsh32::seed_from_u64(2025);
+    let mut data: BTreeMap<&str, BTreeMap<&str, usize>> = BTreeMap::new();
+    for prefix in prefixes.iter() {
+        // Two types of keys: big and small. Pick at random with a certain chance of big.
+        let is_big = rng.random::<f64>() < 0.3;
+        for suffix in suffixes.iter() {
+            // Include the suffix with a probability dependent on is_big.
+            let include_value = rng.random::<f64>() < if is_big { 0.8 } else { 0.05 };
+            if include_value {
+                data.entry(prefix).or_default().insert(suffix, counter);
+                counter += 1;
+            }
+        }
+    }
+    assert_eq!(data.values().map(|m| m.len()).sum::<usize>(), 175306);
+    let dense = ZeroAsciiDenseSparse2dTrieOwned::try_from_btree_map_str(&data, b'/').unwrap();
+    check_data(&data, dense.as_borrowed(), true);
+    let byte_len = check_encoding(dense.as_borrowed());
+    assert_eq!(byte_len, 555240);
+    let simple_trie = make_simple_ascii_trie(&data);
+    assert_eq!(simple_trie.byte_len(), 1099634);
+}
