359f19842bdf74f0f7dc5d77f9ac7e6f27ab9b88
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2025-12-16 17:04:14 +0100

    Move artifacts build from Dart to a bash script (#7319)
    I broke the Dart script 🙄

---
+++ ffi/capi/build.sh
@@ -0,0 +1,35 @@
+NIGHTLY="${PINNED_CI_NIGHTLY:=nightly-2025-09-27}"
+
+case $TARGET in
+    "riscv64-linux-android" | "riscv64gc-unknown-linux-gnu" | "wasm32-unknown-unknown") 
+        NO_STD="1" ;;
+    *) 
+        NO_STD="0" ;;
+esac
+
+if [[ "$TYPE" = "static" ]] || [[ $NO_STD == 1 ]]; then
+    rustup toolchain install --no-self-update $NIGHTLY --component rust-src
+    rustup target add $TARGET --toolchain $NIGHTLY
+else
+    rustup target add $TARGET
+fi
+
+if [[ $NO_STD == 1 ]]; then
+    RUSTFLAGS="-Zunstable-options -Cpanic=immediate-abort $RUSTFLAGS"
+fi
+
+cargo \
+    $( ([[ $TYPE == "static" ]] || [[ $NO_STD == 1 ]]) && echo "+$NIGHTLY" ) \
+    "rustc" \
+    "--manifest-path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/Cargo.toml" \
+    "--crate-type=$( [[ "$TYPE" == "static" ]] && echo "staticlib" || echo "cdylib" )" \
+    "--release" \
+    "--config=profile.release.panic=\"abort\"" \
+    "--config=profile.release.codegen-units=1" \
+    "--no-default-features" \
+    "--features=$( [[ $NO_STD == 1 ]] && echo "libc_alloc,looping_panic_handler" || echo "simple_logger"),$FEATURES" \
+    $( [[ $NO_STD = 1 ]] && echo "-Zbuild-std=core,alloc") \
+    $( ( [[ $TYPE = "static" ]] || [[ $NO_STD == 1 ]] ) && echo "-Zbuild-std=std,panic_abort" ) \
+    "--target=$TARGET" \
+    "--" \
+    "--emit" "link=$OUT"
