d0b2f412f4a4270d69ed2156627b639f7e4a4018
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2025-12-31 13:15:05 +0100

    reorder properties_names

--- ffi/capi/src/properties_names.rs
+++ ffi/capi/src/properties_names.rs
@@ -63,7 +63,84 @@ pub mod ffi {
             .map(|u_16| u_16 as i16)
             .unwrap_or(-1)
         }
+    }
 
+    /// A type capable of looking up General Category Group values from a string name.
+    #[diplomat::opaque]
+    #[diplomat::rust_link(icu::properties::PropertyParser, Struct)]
+    #[diplomat::rust_link(icu::properties::props::GeneralCategory, Enum)]
+    #[diplomat::attr(demo_gen, disable)] // TODO needs custom page
+    pub struct GeneralCategoryNameToGroupMapper(
+        icu_properties::PropertyParser<icu_properties::props::GeneralCategoryGroup>,
+    );
+
+    impl GeneralCategoryNameToGroupMapper {
+        /// Get the mask value matching the given name, using strict matching
+        ///
+        /// Returns 0 if the name is unknown for this property
+        #[diplomat::rust_link(icu::properties::PropertyParserBorrowed::get_strict, FnInStruct)]
+        #[diplomat::rust_link(
+            icu::properties::PropertyParserBorrowed::get_strict_u16,
+            FnInStruct,
+            hidden
+        )]
+        pub fn get_strict(&self, name: &DiplomatStr) -> GeneralCategoryGroup {
+            if let Ok(name) = core::str::from_utf8(name) {
+                self.0.as_borrowed().get_strict(name)
+            } else {
+                None
+            }
+            .map(Into::into)
+            .unwrap_or_default()
+        }
+
+        /// Get the mask value matching the given name, using loose matching
+        ///
+        /// Returns 0 if the name is unknown for this property
+        #[diplomat::rust_link(icu::properties::PropertyParserBorrowed::get_loose, FnInStruct)]
+        #[diplomat::rust_link(
+            icu::properties::PropertyParserBorrowed::get_loose_u16,
+            FnInStruct,
+            hidden
+        )]
+        pub fn get_loose(&self, name: &DiplomatStr) -> GeneralCategoryGroup {
+            if let Ok(name) = core::str::from_utf8(name) {
+                self.0.as_borrowed().get_loose(name)
+            } else {
+                None
+            }
+            .map(Into::into)
+            .unwrap_or_default()
+        }
+        /// Create a name-to-mask mapper for the `General_Category` property, using compiled data.
+        #[diplomat::rust_link(icu::properties::props::GeneralCategoryGroup, Struct)]
+        #[diplomat::attr(auto, constructor)]
+        #[cfg(feature = "compiled_data")]
+        pub fn create() -> Box<GeneralCategoryNameToGroupMapper> {
+            Box::new(GeneralCategoryNameToGroupMapper(
+                icu_properties::PropertyParser::<icu_properties::props::GeneralCategoryGroup>::new(
+                )
+                .static_to_owned(),
+            ))
+        }
+        /// Create a name-to-mask mapper for the `General_Category` property, using a particular data source.
+        #[diplomat::rust_link(icu::properties::props::GeneralCategoryGroup, Struct)]
+        #[diplomat::attr(all(supports = fallible_constructors, supports = named_constructors), named_constructor = "with_provider")]
+        #[cfg(feature = "buffer_provider")]
+        pub fn create_with_provider(
+            provider: &DataProvider,
+        ) -> Result<Box<GeneralCategoryNameToGroupMapper>, DataError> {
+            Ok(Box::new(
+                GeneralCategoryNameToGroupMapper(icu_properties::PropertyParser::<
+                    icu_properties::props::GeneralCategoryGroup,
+                >::try_new_unstable(
+                    &provider.get_unstable()?
+                )?),
+            ))
+        }
+    }
+
+    impl PropertyValueNameToEnumMapper {
         /// Create a name-to-enum mapper for the `General_Category` property, using compiled data.
         #[diplomat::rust_link(icu::properties::props::GeneralCategory, Enum)]
         #[diplomat::attr(auto, named_constructor = "general_category")]
@@ -415,81 +492,6 @@ pub mod ffi {
                 )?
                 .erase(),
             )))
-        }
-    }
-
-    /// A type capable of looking up General Category Group values from a string name.
-    #[diplomat::opaque]
-    #[diplomat::rust_link(icu::properties::PropertyParser, Struct)]
-    #[diplomat::rust_link(icu::properties::props::GeneralCategory, Enum)]
-    #[diplomat::attr(demo_gen, disable)] // TODO needs custom page
-    pub struct GeneralCategoryNameToGroupMapper(
-        icu_properties::PropertyParser<icu_properties::props::GeneralCategoryGroup>,
-    );
-
-    impl GeneralCategoryNameToGroupMapper {
-        /// Get the mask value matching the given name, using strict matching
-        ///
-        /// Returns 0 if the name is unknown for this property
-        #[diplomat::rust_link(icu::properties::PropertyParserBorrowed::get_strict, FnInStruct)]
-        #[diplomat::rust_link(
-            icu::properties::PropertyParserBorrowed::get_strict_u16,
-            FnInStruct,
-            hidden
-        )]
-        pub fn get_strict(&self, name: &DiplomatStr) -> GeneralCategoryGroup {
-            if let Ok(name) = core::str::from_utf8(name) {
-                self.0.as_borrowed().get_strict(name)
-            } else {
-                None
-            }
-            .map(Into::into)
-            .unwrap_or_default()
-        }
-
-        /// Get the mask value matching the given name, using loose matching
-        ///
-        /// Returns 0 if the name is unknown for this property
-        #[diplomat::rust_link(icu::properties::PropertyParserBorrowed::get_loose, FnInStruct)]
-        #[diplomat::rust_link(
-            icu::properties::PropertyParserBorrowed::get_loose_u16,
-            FnInStruct,
-            hidden
-        )]
-        pub fn get_loose(&self, name: &DiplomatStr) -> GeneralCategoryGroup {
-            if let Ok(name) = core::str::from_utf8(name) {
-                self.0.as_borrowed().get_loose(name)
-            } else {
-                None
-            }
-            .map(Into::into)
-            .unwrap_or_default()
-        }
-        /// Create a name-to-mask mapper for the `General_Category` property, using compiled data.
-        #[diplomat::rust_link(icu::properties::props::GeneralCategoryGroup, Struct)]
-        #[diplomat::attr(auto, constructor)]
-        #[cfg(feature = "compiled_data")]
-        pub fn create() -> Box<GeneralCategoryNameToGroupMapper> {
-            Box::new(GeneralCategoryNameToGroupMapper(
-                icu_properties::PropertyParser::<icu_properties::props::GeneralCategoryGroup>::new(
-                )
-                .static_to_owned(),
-            ))
-        }
-        /// Create a name-to-mask mapper for the `General_Category` property, using a particular data source.
-        #[diplomat::rust_link(icu::properties::props::GeneralCategoryGroup, Struct)]
-        #[diplomat::attr(all(supports = fallible_constructors, supports = named_constructors), named_constructor = "with_provider")]
-        #[cfg(feature = "buffer_provider")]
-        pub fn create_with_provider(
-            provider: &DataProvider,
-        ) -> Result<Box<GeneralCategoryNameToGroupMapper>, DataError> {
-            Ok(Box::new(
-                GeneralCategoryNameToGroupMapper(icu_properties::PropertyParser::<
-                    icu_properties::props::GeneralCategoryGroup,
-                >::try_new_unstable(
-                    &provider.get_unstable()?
-                )?),
-            ))
         }
     }
 }
