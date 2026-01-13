281498e9b37969b5c7bcd2b3f577b27f4df7842a
Shane F. Carr <shane@unicode.org>
2026-01-12 20:17:55 -0800

    Refactor, add another test

--- components/datetime/src/provider/fields/symbols.rs
+++ components/datetime/src/provider/fields/symbols.rs
@@ -5,6 +5,7 @@
 use crate::options::SubsecondDigits;
 #[cfg(feature = "datagen")]
 use crate::provider::fields::FieldLength;
+use crate::DateTimeFormatterPreferences;
 use core::{cmp::Ordering, convert::TryFrom};
 use displaydoc::Display;
 use icu_locale_core::preferences::extensions::unicode::keywords::HourCycle;
@@ -592,19 +593,117 @@ field_type!(
 );
 
 impl Hour {
-    pub(crate) fn from_hour_cycle(hour_cycle: HourCycle, region: Option<Region>) -> Self {
-        const JP: Region = region!("JP");
-        match hour_cycle {
+    pub(crate) fn from_prefs(prefs: DateTimeFormatterPreferences) -> Option<Self> {
+        let Some(hour_cycle) = prefs.hour_cycle else {
+            return None;
+        };
+        let field = match hour_cycle {
             HourCycle::H11 => Self::H11,
             HourCycle::H12 => Self::H12,
             HourCycle::H23 => Self::H23,
-            HourCycle::Clock12 => match region {
-                Some(JP) => Self::H11,
-                _ => Self::H12,
-            },
+            HourCycle::Clock12 => {
+                let region = prefs
+                    .locale_preferences
+                    .to_data_locale_region_priority()
+                    .region;
+                const JP: Region = region!("JP");
+                match region {
+                    Some(JP) => Self::H11,
+                    _ => Self::H12,
+                }
+            }
             HourCycle::Clock24 => Self::H23,
             _ => unreachable!(),
-        }
+        };
+        Some(field)
+    }
+}
+
+#[test]
+fn test_hour_cycle_selection() {
+    struct TestCase {
+        locale: &'static str,
+        expected: Option<Hour>,
+    }
+    let cases = [
+        TestCase {
+            locale: "en-US",
+            expected: None,
+        },
+        TestCase {
+            locale: "en-US-u-hc-h11",
+            expected: Some(Hour::H11),
+        },
+        TestCase {
+            locale: "en-US-u-hc-h12",
+            expected: Some(Hour::H12),
+        },
+        TestCase {
+            locale: "en-US-u-hc-h23",
+            expected: Some(Hour::H23),
+        },
+        TestCase {
+            locale: "en-US-u-hc-c12",
+            expected: Some(Hour::H12),
+        },
+        TestCase {
+            locale: "en-US-u-hc-c24",
+            expected: Some(Hour::H23),
+        },
+        TestCase {
+            locale: "fr-FR",
+            expected: None,
+        },
+        TestCase {
+            locale: "fr-FR-u-hc-h11",
+            expected: Some(Hour::H11),
+        },
+        TestCase {
+            locale: "fr-FR-u-hc-h12",
+            expected: Some(Hour::H12),
+        },
+        TestCase {
+            locale: "fr-FR-u-hc-h23",
+            expected: Some(Hour::H23),
+        },
+        TestCase {
+            locale: "fr-FR-u-hc-c12",
+            expected: Some(Hour::H12),
+        },
+        TestCase {
+            locale: "fr-FR-u-hc-c24",
+            expected: Some(Hour::H23),
+        },
+        TestCase {
+            locale: "ja-JP",
+            expected: None,
+        },
+        TestCase {
+            locale: "ja-JP-u-hc-h11",
+            expected: Some(Hour::H11),
+        },
+        TestCase {
+            locale: "ja-JP-u-hc-h12",
+            expected: Some(Hour::H12),
+        },
+        TestCase {
+            locale: "ja-JP-u-hc-h23",
+            expected: Some(Hour::H23),
+        },
+        TestCase {
+            locale: "ja-JP-u-hc-c12",
+            expected: Some(Hour::H11),
+        },
+        TestCase {
+            locale: "ja-JP-u-hc-c24",
+            expected: Some(Hour::H23),
+        },
+    ];
+    for TestCase { locale, expected } in cases {
+        let locale = icu_locale_core::Locale::try_from_str(locale).unwrap();
+        let prefs = crate::DateTimeFormatterPreferences::from(&locale);
+        let actual = Hour::from_prefs(prefs);
+        assert_eq!(expected, actual, "{}", locale);
     }
 }
 
--- components/datetime/src/raw/neo.rs
+++ components/datetime/src/raw/neo.rs
@@ -49,15 +49,7 @@ impl RawPreferences {
     #[inline]
     pub(crate) fn from_prefs(prefs: DateTimeFormatterPreferences) -> Self {
         Self {
-            hour_cycle: prefs.hour_cycle.map(|hc| {
-                fields::Hour::from_hour_cycle(
-                    hc,
-                    prefs
-                        .locale_preferences
-                        .to_data_locale_region_priority()
-                        .region,
-                )
-            }),
+            hour_cycle: fields::Hour::from_prefs(prefs),
         }
     }
 }
