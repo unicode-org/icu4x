491208907f609c5eeb445334fbaaff8024609a1b
Shane F. Carr <shane@unicode.org>
2026-01-12 20:23:35 -0800

    Fixes in icu_locale_core

--- components/locale_core/src/preferences/extensions/unicode/keywords/hour_cycle.rs
+++ components/locale_core/src/preferences/extensions/unicode/keywords/hour_cycle.rs
@@ -11,24 +11,26 @@ enum_keyword!(
     HourCycle {
         /// The 12-hour clock used in much of the world. Hours are numbered 1–12.
         /// 
-        /// Corresponds to 'h' in patterns.
+        /// Corresponds to 'h' in UTS #35 pattern strings.
         ("h12" => H12),
         /// The 24-hour clock. Hour are numbered 0–23.
         /// 
-        /// Corresponds to 'H' in patterns.
+        /// Corresponds to 'H' in UTS #35 pattern strings.
         ("h23" => H23),
         /// Variant of the 12-hour clock, sometimes used in Japan. Hours are numbered 0–11.
         /// 
-        /// Corresponds to 'K' in patterns.
+        /// Corresponds to 'K' in UTS #35 pattern strings.
         ("h11" => H11),
         /// The 12-hour clock automatically selected based on the locale. Hours are numbered
         /// 1-12, 0-11, or another 12-hour clock based on locale data.
         /// 
-        /// Corresponds to 'j' in patterns.
+        /// Corresponds to 'j' in UTS #35 skeleton strings,
+        /// except it can resolve to only `h` or `K`.
         ("c12" => Clock12),
         /// The 24-hour clock automatically selected based on the locale. Hours are numbered
         /// 0-23 or another 24-hour clock based on locale data.
         /// 
-        /// Corresponds to 'J' in patterns.
+        /// Corresponds to 'j' in UTS #35 skeleton strings,
+        /// except it can resolve to only `H`.
         ("c24" => Clock24),
 }, "hc");
