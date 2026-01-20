2e475c2732e747f4b27597089aba73b24525b45e
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-01-20 12:54:31 +0100

    ffi

--- ffi/freertos/src/lib.rs
+++ ffi/freertos/src/lib.rs
@@ -2,19 +2,8 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-//! This crate is a shim that enables one to use icu4x on Cortex-M + FreeRTOS by setting up the
-//! relevant Rust runtime hooks.
-//!
-//! Note that compiling to this platform needs Rust nightly, and this crate attempts to
-//! build across multiple nightly versions.
-//!
-//! This crate has a build script that will attempt to detect the nightly version and configure
-//! things appropriately, where possible. Older nightlies will end up setting the
-//! `--cfg needs_alloc_error_handler` flag: if using a custom build system and a nightly from
-//! 2022 or earlier, please set this flag.
-
 // https://github.com/unicode-org/icu4x/blob/main/documents/process/boilerplate.md#library-annotations
-#![no_std]
+#![cfg_attr(not(any(test, doc)), no_std)]
 #![cfg_attr(
     not(test),
     deny(
@@ -28,6 +17,19 @@
         missing_debug_implementations,
     )
 )]
+#![warn(missing_docs)]
+
+//! This crate is a shim that enables one to use icu4x on Cortex-M + FreeRTOS by setting up the
+//! relevant Rust runtime hooks.
+//!
+//! Note that compiling to this platform needs Rust nightly, and this crate attempts to
+//! build across multiple nightly versions.
+//!
+//! This crate has a build script that will attempt to detect the nightly version and configure
+//! things appropriately, where possible. Older nightlies will end up setting the
+//! `--cfg needs_alloc_error_handler` flag: if using a custom build system and a nightly from
+//! 2022 or earlier, please set this flag.
+
 #![allow(clippy::upper_case_acronyms)]
 #![cfg_attr(
     all(target_os = "none", needs_alloc_error_handler),
