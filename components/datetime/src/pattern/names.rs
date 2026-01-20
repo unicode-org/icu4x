1d1b11b019664a5a378cb3ee6f4a9991d409b538
Manish Goregaokar <manishsmail@gmail.com>
2026-01-20 11:15:56 -0500

    Organize neo datetime provider code (#7429)
    Fixes https://github.com/unicode-org/icu4x/issues/5613
    
    I used the same organization as in
    https://github.com/unicode-org/icu4x/pull/7386: `names` and
    `semantic_skeleta`. Glue patterns go in `names` for now, but they're
    easy to move.

--- components/datetime/src/pattern/names.rs
+++ components/datetime/src/pattern/names.rs
@@ -10,8 +10,9 @@ use super::{
 use crate::error::ErrorField;
 use crate::fieldsets::enums::{CompositeDateTimeFieldSet, CompositeFieldSet};
 use crate::provider::fields::{self, FieldLength, FieldSymbol};
-use crate::provider::neo::{marker_attrs, *};
+use crate::provider::names::*;
 use crate::provider::pattern::PatternItem;
+use crate::provider::semantic_skeletons::marker_attrs;
 use crate::provider::time_zones::tz;
 use crate::size_test_macro::size_test;
 use crate::FixedCalendarDateTimeFormatter;
@@ -3744,7 +3745,7 @@ impl RawDateTimeNamesBorrowed<'_> {
 
         match (year_names, era_year.era_index) {
             (YearNames::VariableEras(era_names), None) => {
-                crate::provider::neo::get_year_name_from_map(
+                crate::provider::names::get_year_name_from_map(
                     era_names,
                     era_year.era.as_str().into(),
                 )
