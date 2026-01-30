afe2773431a94f5e3cbb39db48a438ceedef489f
SATVIKsynopsis <futureiitianisme@gmail.com>
2026-01-30 15:38:11 +0530

    added the missing test

--- components/collator/tests/tests.rs
+++ components/collator/tests/tests.rs
@@ -144,6 +144,7 @@ fn test_implicit_unihan() {
     assert_all_comparisons(&collator, "\u{4E01}", "\u{4E00}", Ordering::Greater);
 
     assert_all_comparisons(&collator, "\u{4E18}", "\u{4E42}", Ordering::Less);
+    assert_all_comparisons(&collator, "\u{4E94}", "\u{50F6}", Ordering::Less);
 }
 
 #[test]
