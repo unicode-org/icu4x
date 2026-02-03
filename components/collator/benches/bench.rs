3e6bfcfb409f494fa87cfe4923d66b8a9a696fa9
psg-19 <pratushgupt22@gmail.com>
2026-02-03 22:17:15 +0530

    added greek collator benchmark

--- components/collator/benches/bench.rs
+++ components/collator/benches/bench.rs
@@ -132,6 +132,14 @@ pub fn collator_with_locale(criterion: &mut Criterion) {
             .rev()
             .collect::<Vec<&str>>(),
     );
+    let content_greek: (&str, Vec<&str>) = (
+        "TestNames_Greek",
+        include_str!("data/TestNames_Greek.txt")
+            .lines()
+            .filter(|&s| !s.starts_with('#'))
+            .rev()
+            .collect::<Vec<&str>>(),
+    );
 
     // hsivonen@ : All five strengths are benched.
     // The default is tertiary, so it makes sense to bench that.
@@ -189,6 +197,11 @@ pub fn collator_with_locale(criterion: &mut Criterion) {
         (
             locale!("ko-KR"),
             vec![&content_latin, &content_korean],
+            &all_strength,
+        ),
+        (
+            locale!("el"),
+            vec![&content_latin, &content_greek],
             &all_strength,
         ),
     ];
