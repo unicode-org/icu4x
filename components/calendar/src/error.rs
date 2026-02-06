c4d85c6df6e11b2e2372709c8cbb877cf1bc628e
Shane F. Carr <shane@unicode.org>
2026-02-06 00:47:41 -0800

    Add more docs test to DateFields and DateFromFieldsError (#7597)
    #7161

--- components/calendar/src/error.rs
+++ components/calendar/src/error.rs
@@ -154,6 +154,26 @@ mod unstable {
         #[displaydoc("{0}")]
         Range(RangeError),
         /// The era code is invalid for the calendar.
+        ///
+        /// # Examples
+        ///
+        /// ```
+        /// use icu::calendar::cal::Hebrew;
+        /// use icu::calendar::error::DateFromFieldsError;
+        /// use icu::calendar::types::DateFields;
+        /// use icu::calendar::Date;
+        ///
+        /// let mut fields = DateFields::default();
+        /// fields.era = Some(b"ce"); // valid in Gregorian, but not Hebrew
+        /// fields.era_year = Some(1);
+        /// fields.ordinal_month = Some(1);
+        /// fields.day = Some(1);
+        ///
+        /// let err = Date::try_from_fields(fields, Default::default(), Hebrew::new())
+        ///     .expect_err("era is unknown for Hebrew");
+        ///
+        /// assert_eq!(err, DateFromFieldsError::UnknownEra);
+        /// ```
         #[displaydoc("Unknown era or invalid syntax")]
         UnknownEra,
         /// The month code syntax is invalid.
