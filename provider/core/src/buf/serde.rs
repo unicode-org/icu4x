0d73021b155c088d7af1f7568d4a2c224d5fd24c
Shane F. Carr <shane@unicode.org>
2026-02-09 19:41:44 -0800

    Add serde converter

--- provider/core/src/buf/serde.rs
+++ provider/core/src/buf/serde.rs
@@ -15,6 +15,7 @@ use crate::buf::BufferFormat;
 use crate::buf::BufferProvider;
 use crate::data_provider::DynamicDryDataProvider;
 use crate::prelude::*;
+use crate::unstable::BoundLocaleDataProvider;
 use crate::DryDataProvider;
 use serde::de::Deserialize;
 use yoke::Yokeable;
@@ -56,6 +57,22 @@ where
     /// ✨ *Enabled with the `serde` Cargo feature.*
     fn as_deserializing(&self) -> DeserializingBufferProvider<'_, Self> {
         DeserializingBufferProvider(self)
+    }
+}
+
+impl<'a, P> DeserializingBufferProvider<'a, P> {
+    /// Wraps the given provider in a [`DeserializingBufferProvider`].
+    ///
+    /// This requires enabling the deserialization Cargo feature
+    /// for the expected format(s):
+    ///
+    /// - `deserialize_json`
+    /// - `deserialize_postcard_1`
+    /// - `deserialize_bincode_1`
+    ///
+    /// ✨ *Enabled with the `serde` Cargo feature.*
+    pub fn new(inner: &'a P) -> Self {
+        Self(inner)
     }
 }
 
@@ -227,6 +244,32 @@ where
 {
     fn dry_load(&self, req: DataRequest) -> Result<DataResponseMetadata, DataError> {
         self.0.dry_load_data(M::INFO, req)
+    }
+}
+
+impl<P, M> BoundLocaleDataProvider<M> for DeserializingBufferProvider<'_, P>
+where
+    M: DynamicDataMarker,
+    P: BoundLocaleDataProvider<BufferMarker> + ?Sized,
+    for<'de> <M::DataStruct as Yokeable<'de>>::Output: Deserialize<'de>,
+{
+    fn load_bound(
+        &self,
+        req: crate::request::DataAttributesRequest,
+    ) -> Result<DataResponse<M>, DataError> {
+        let buffer_response = self.0.load_bound(req)?;
+        let buffer_format = buffer_response.metadata.buffer_format.ok_or_else(|| {
+            DataErrorKind::Deserialize
+                .with_str_context("BufferProvider didn't set BufferFormat")
+                .with_debug_context(&req)
+        })?;
+        Ok(DataResponse {
+            metadata: buffer_response.metadata,
+            payload: buffer_response
+                .payload
+                .into_deserialized(buffer_format)
+                .map_err(|e| e.with_debug_context(&req))?,
+        })
     }
 }
 
