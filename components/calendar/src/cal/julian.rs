76659c3c7cf857a8fef65db5c854a9f1a6ff8694
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-02-11 00:30:01 +0100

    update comments

--- components/calendar/src/cal/julian.rs
+++ components/calendar/src/cal/julian.rs
@@ -274,7 +274,7 @@ impl Date<Julian> {
     /// Construct new Julian [`Date`].
     ///
     /// Years are arithmetic, meaning there is a year 0 preceded by negative years, with a
-    /// valid range of `-1,000,000..=1,000,000`.
+    /// valid range of `-9999..=9999`.
     ///
     /// ```rust
     /// use icu::calendar::Date;
