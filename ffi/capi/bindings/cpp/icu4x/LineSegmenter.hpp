807a8c33a26c6b32dac4f7ef134bf76a940e3f1b
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-02-11 10:42:24 +0100

    Followup #7590 (#7624)
    Taking a non-`Default`, non-`Clone` struct by value is not very
    ergonomic. Changed the load methods to use `&mut self`, which also lets
    us add FFI.

--- ffi/capi/bindings/cpp/icu4x/LineSegmenter.hpp
+++ ffi/capi/bindings/cpp/icu4x/LineSegmenter.hpp
@@ -53,6 +53,12 @@ namespace capi {
     typedef struct icu4x_LineSegmenter_create_for_non_complex_scripts_with_options_v2_and_provider_mv1_result {union {icu4x::capi::LineSegmenter* ok; icu4x::capi::DataError err;}; bool is_ok;} icu4x_LineSegmenter_create_for_non_complex_scripts_with_options_v2_and_provider_mv1_result;
     icu4x_LineSegmenter_create_for_non_complex_scripts_with_options_v2_and_provider_mv1_result icu4x_LineSegmenter_create_for_non_complex_scripts_with_options_v2_and_provider_mv1(const icu4x::capi::DataProvider* provider, const icu4x::capi::Locale* content_locale, icu4x::capi::LineBreakOptionsV2 options);
 
+    typedef struct icu4x_LineSegmenter_load_lstm_models_with_provider_mv1_result {union { icu4x::capi::DataError err;}; bool is_ok;} icu4x_LineSegmenter_load_lstm_models_with_provider_mv1_result;
+    icu4x_LineSegmenter_load_lstm_models_with_provider_mv1_result icu4x_LineSegmenter_load_lstm_models_with_provider_mv1(icu4x::capi::LineSegmenter* self, const icu4x::capi::DataProvider* provider);
+
+    typedef struct icu4x_LineSegmenter_load_dictinoary_models_with_provider_mv1_result {union { icu4x::capi::DataError err;}; bool is_ok;} icu4x_LineSegmenter_load_dictinoary_models_with_provider_mv1_result;
+    icu4x_LineSegmenter_load_dictinoary_models_with_provider_mv1_result icu4x_LineSegmenter_load_dictinoary_models_with_provider_mv1(icu4x::capi::LineSegmenter* self, const icu4x::capi::DataProvider* provider);
+
     icu4x::capi::LineBreakIteratorUtf8* icu4x_LineSegmenter_segment_utf8_mv1(const icu4x::capi::LineSegmenter* self, icu4x::diplomat::capi::DiplomatStringView input);
 
     icu4x::capi::LineBreakIteratorUtf16* icu4x_LineSegmenter_segment_utf16_mv1(const icu4x::capi::LineSegmenter* self, icu4x::diplomat::capi::DiplomatString16View input);
@@ -135,6 +141,18 @@ inline icu4x::diplomat::result<std::unique_ptr<icu4x::LineSegmenter>, icu4x::Dat
         content_locale ? content_locale->AsFFI() : nullptr,
         options.AsFFI());
     return result.is_ok ? icu4x::diplomat::result<std::unique_ptr<icu4x::LineSegmenter>, icu4x::DataError>(icu4x::diplomat::Ok<std::unique_ptr<icu4x::LineSegmenter>>(std::unique_ptr<icu4x::LineSegmenter>(icu4x::LineSegmenter::FromFFI(result.ok)))) : icu4x::diplomat::result<std::unique_ptr<icu4x::LineSegmenter>, icu4x::DataError>(icu4x::diplomat::Err<icu4x::DataError>(icu4x::DataError::FromFFI(result.err)));
+}
+
+inline icu4x::diplomat::result<std::monostate, icu4x::DataError> icu4x::LineSegmenter::load_lstm_models_with_provider(const icu4x::DataProvider& provider) {
+    auto result = icu4x::capi::icu4x_LineSegmenter_load_lstm_models_with_provider_mv1(this->AsFFI(),
+        provider.AsFFI());
+    return result.is_ok ? icu4x::diplomat::result<std::monostate, icu4x::DataError>(icu4x::diplomat::Ok<std::monostate>()) : icu4x::diplomat::result<std::monostate, icu4x::DataError>(icu4x::diplomat::Err<icu4x::DataError>(icu4x::DataError::FromFFI(result.err)));
+}
+
+inline icu4x::diplomat::result<std::monostate, icu4x::DataError> icu4x::LineSegmenter::load_dictinoary_models_with_provider(const icu4x::DataProvider& provider) {
+    auto result = icu4x::capi::icu4x_LineSegmenter_load_dictinoary_models_with_provider_mv1(this->AsFFI(),
+        provider.AsFFI());
+    return result.is_ok ? icu4x::diplomat::result<std::monostate, icu4x::DataError>(icu4x::diplomat::Ok<std::monostate>()) : icu4x::diplomat::result<std::monostate, icu4x::DataError>(icu4x::diplomat::Err<icu4x::DataError>(icu4x::DataError::FromFFI(result.err)));
 }
 
 inline std::unique_ptr<icu4x::LineBreakIteratorUtf8> icu4x::LineSegmenter::segment(std::string_view input) const {
