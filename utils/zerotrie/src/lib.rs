602f88357a8d44dae92a70bf03846ef9818e3930
Shane F. Carr <shane@unicode.org>
2026-02-27 15:59:43 -0800

    review feedback

--- utils/zerotrie/src/lib.rs
+++ utils/zerotrie/src/lib.rs
@@ -29,7 +29,8 @@
 //!
 //! A small amount of unsafe Rust is used in these situations:
 //!
-//! - Constructing unsized transparent newtypes (i.e. <https://github.com/rust-lang/rust/issues/18806>)
+//! - Constructing unsized transparent newtypes (i.e. <https://github.com/rust-lang/rust/issues/18806>),
+//!   which is reachable from builder code and when creating a ZeroTrie DST.
 //! - Implementing unsafe traits when the `zerovec` feature is enabled
 //!
 //! # Examples
