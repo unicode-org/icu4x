9e1dcbe9262a199cd1998ecbe477f57039f64bb5
Shane F. Carr <shane@unicode.org>
2026-02-27 16:01:46 -0800

    clippy

--- utils/zerotrie/src/lib.rs
+++ utils/zerotrie/src/lib.rs
@@ -30,7 +30,7 @@
 //! A small amount of unsafe Rust is used in these situations:
 //!
 //! - Constructing unsized transparent newtypes (i.e. <https://github.com/rust-lang/rust/issues/18806>),
-//!   which is reachable from builder code and when creating a ZeroTrie DST.
+//!   which is reachable from builder code and when creating a `&TypedZeroTrie<[u8]>` DST.
 //! - Implementing unsafe traits when the `zerovec` feature is enabled
 //!
 //! # Examples
