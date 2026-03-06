979a758a8889cdac6c89bec9a1070368799b6ab4
Benjamin A. Beasley <code@musicinmybrain.net>
2026-03-06 15:00:10 +0000

    In icu_properties, limit use of the icu crate to doctests

--- components/properties/src/code_point_set.rs
+++ components/properties/src/code_point_set.rs
@@ -254,9 +254,9 @@ pub trait BinaryProperty: crate::private::Sealed + Sized {
 mod tests {
     #[test]
     fn test_general_category() {
-        use icu::properties::props::GeneralCategory;
-        use icu::properties::props::GeneralCategoryGroup;
-        use icu::properties::CodePointMapData;
+        use crate::props::GeneralCategory;
+        use crate::props::GeneralCategoryGroup;
+        use crate::CodePointMapData;
 
         let digits_data = CodePointMapData::<GeneralCategory>::new()
             .get_set_for_value_group(GeneralCategoryGroup::Number);
@@ -271,8 +271,8 @@ mod tests {
 
     #[test]
     fn test_script() {
-        use icu::properties::props::Script;
-        use icu::properties::CodePointMapData;
+        use crate::props::Script;
+        use crate::CodePointMapData;
 
         let thai_data = CodePointMapData::<Script>::new().get_set_for_value(Script::Thai);
         let thai = thai_data.as_borrowed();
@@ -286,8 +286,8 @@ mod tests {
 
     #[test]
     fn test_gc_groupings() {
-        use icu::properties::props::{GeneralCategory, GeneralCategoryGroup};
-        use icu::properties::CodePointMapData;
+        use crate::props::{GeneralCategory, GeneralCategoryGroup};
+        use crate::CodePointMapData;
         use icu_collections::codepointinvlist::CodePointInversionListBuilder;
 
         let test_group = |category: GeneralCategoryGroup, subcategories: &[GeneralCategory]| {
@@ -383,8 +383,8 @@ mod tests {
 
     #[test]
     fn test_gc_surrogate() {
-        use icu::properties::props::GeneralCategory;
-        use icu::properties::CodePointMapData;
+        use crate::props::GeneralCategory;
+        use crate::CodePointMapData;
 
         let surrogates_data = CodePointMapData::<GeneralCategory>::new()
             .get_set_for_value(GeneralCategory::Surrogate);
