a311bcbe87870da1751011f12d464cf6b0080a34
Manish Goregaokar <manishsmail@gmail.com>
2026-03-09 10:52:21 -0700

    Update components/calendar/src/calendar_arithmetic.rs
    Co-authored-by: Robert Bastian <4706271+robertbastian@users.noreply.github.com>

--- components/calendar/src/calendar_arithmetic.rs
+++ components/calendar/src/calendar_arithmetic.rs
@@ -235,7 +235,7 @@ pub(crate) trait DateFieldsResolver: Calendar {
         12
     }
 
-    /// A minimum bound for the number of months over `years` years, starting from the given year.
+    /// A lower bound for the number of months in `years` years, starting from the beginning of this year.
     ///
     /// This may be equal to the number of months, but should never be over. Ideally,
     /// implementations should be close enough to the actual answer such that the error
