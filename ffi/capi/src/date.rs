0cf69f9729db441e04040e686efd165a5567a587
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-03-10 17:02:53 +0100

    ffi

--- ffi/capi/src/date.rs
+++ ffi/capi/src/date.rs
@@ -524,7 +524,7 @@ pub mod ffi {
         )]
         #[diplomat::rust_link(icu::calendar::types::Month, Struct, hidden)]
         #[diplomat::rust_link(icu::calendar::types::MonthInfo, Struct, hidden)]
-        #[diplomat::rust_link(icu::calendar::types::MonthInfo::as_input, FnInStruct, hidden)]
+        #[diplomat::rust_link(icu::calendar::types::MonthInfo::to_input, FnInStruct, hidden)]
         #[diplomat::attr(auto, getter)]
         pub fn month_code(&self, write: &mut diplomat_runtime::DiplomatWrite) {
             let code = self.0.month().to_input().code();
