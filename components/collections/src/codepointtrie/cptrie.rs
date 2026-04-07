9313b51d294a80fd73571b0f38534e51d1d9b663
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-04-07 12:53:18 +0200

    unused_unsafe

--- components/collections/src/codepointtrie/cptrie.rs
+++ components/collections/src/codepointtrie/cptrie.rs
@@ -1330,7 +1330,10 @@ impl<T: TrieValue + databake::Bake> databake::Bake for CodePointTrie<'_, T> {
         let index = self.index.bake(env);
         let data = self.data.bake(env);
         let error_value = self.error_value.bake(env);
-        databake::quote! { unsafe { icu_collections::codepointtrie::CodePointTrie::from_parts_unstable_unchecked_v1(#header, #index, #data, #error_value) } }
+        databake::quote! { unsafe {
+            #[allow(unused_unsafe)]
+            icu_collections::codepointtrie::CodePointTrie::from_parts_unstable_unchecked_v1(#header, #index, #data, #error_value)
+        }}
     }
 }
 
