429d795d6fa6c5ab0faa14f8f6f31d788c903652
Shane F. Carr <shane@unicode.org>
2026-07-07 11:03:29 +0200

    single_result

--- components/experimental/tests/displaynames/tests.rs
+++ components/experimental/tests/displaynames/tests.rs
@@ -22,140 +22,139 @@ fn test_concatenate() {
         pub input_1: &'a Locale,
         pub expected: &'a str,
         pub should_borrow: bool,
-        /// TODO(#8100): single should not error
-        pub single_should_err: bool,
+        pub single_result: Result<(), LanguageIdentifierNameFallbackError>,
     }
     let cases = [
         TestCase {
             input_1: &locale!("de-CH"),
             expected: "Swiss High German",
             should_borrow: true,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("zh-Hans"),
             expected: "Simplified Chinese",
             should_borrow: true,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("es-419"),
             expected: "Latin American Spanish",
             should_borrow: true,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("es-Cyrl-MX"),
             expected: "Mexican Spanish (Cyrillic)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &"en-Latn-GB-fonipa-scouse".parse().unwrap(),
             expected: "British English (Latin, IPA Phonetics, Scouse)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("de-Latn-CH"),
             expected: "Swiss High German (Latin)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("zh-Hans-CN"),
             expected: "Simplified Chinese (China)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("es-419-fonipa"),
             expected: "Latin American Spanish (IPA Phonetics)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("es-Latn-419"),
             expected: "Latin American Spanish (Latin)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             // Language not found
             input_1: &locale!("xx"),
             expected: "xx",
             should_borrow: true,
-            single_should_err: true,
+            single_result: Err(LanguageIdentifierNameFallbackError),
         },
         TestCase {
             input_1: &locale!("xx-YY"),
             expected: "xx (YY)",
             should_borrow: false,
-            single_should_err: true,
+            single_result: Err(LanguageIdentifierNameFallbackError),
         },
         TestCase {
             // Script not found
             input_1: &locale!("en-Qzzz"),
             expected: "English (Qzzz)",
             should_borrow: false,
-            single_should_err: true,
+            single_result: Err(LanguageIdentifierNameFallbackError),
         },
         TestCase {
             // Region not found
             input_1: &locale!("en-QZ"),
             expected: "English (QZ)",
             should_borrow: false,
-            single_should_err: true,
+            single_result: Err(LanguageIdentifierNameFallbackError),
         },
         TestCase {
             // Variant not found
             input_1: &locale!("en-qzzzz"),
             expected: "English (qzzzz)",
             should_borrow: false,
-            single_should_err: true,
+            single_result: Err(LanguageIdentifierNameFallbackError),
         },
         TestCase {
             input_1: &"aa-Brai-CC-fonipa-posix".parse().unwrap(),
             expected: "Afar (Braille, Cocos (Keeling) Islands, IPA Phonetics, Computer)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("nl-BE"),
             expected: "Flemish",
             should_borrow: true,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("nl-Latn-BE"),
             expected: "Flemish (Latin)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &"zh-Hans-fonipa".parse().unwrap(),
             expected: "Simplified Chinese (IPA Phonetics)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("hi-Latn"),
             expected: "Hindi (Latin)",
             should_borrow: true,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             input_1: &locale!("zh-Hant-HK"),
             expected: "Traditional Chinese (Hong Kong SAR China)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
         TestCase {
             // Multiple variants
             input_1: &Locale::try_from_str("es-fonipa-posix-valencia").unwrap(),
             expected: "Spanish (IPA Phonetics, Computer, Valencian)",
             should_borrow: false,
-            single_should_err: false,
+            single_result: Ok(()),
         },
     ];
     for cas in &cases {
@@ -187,18 +186,9 @@ fn test_concatenate() {
             single_options,
         )
         .unwrap();
-        assert_writeable_eq!(single_display_name.as_borrowed(), cas.expected);
         let borrowed = single_display_name.as_borrowed();
         assert_writeable_eq!(borrowed, cas.expected);
-        if cas.single_should_err {
-            assert_try_writeable_eq!(
-                borrowed,
-                cas.expected,
-                Err(LanguageIdentifierNameFallbackError)
-            );
-        } else {
-            assert_try_writeable_eq!(borrowed, cas.expected, Ok(()));
-        }
+        assert_try_writeable_eq!(borrowed, cas.expected, cas.single_result);
     }
 }
 
