805d236b5963b870f9417bd908677f3e4016d5ae
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-07-08 22:30:39 +0200

    trait

--- components/experimental/src/dimension/currency/formatter.rs
+++ components/experimental/src/dimension/currency/formatter.rs
@@ -21,7 +21,7 @@ use icu_decimal::{
     AbstractFormatter, CompactDecimalFormatter, DecimalFormatter, DecimalFormatterPreferences,
 };
 use icu_locale_core::preferences::{define_preferences, prefs_convert};
-use icu_plurals::{PluralRules, PluralRulesPreferences};
+use icu_plurals::{PluralRules, PluralRulesPreferences, PluralSubject};
 use icu_provider::prelude::*;
 use tinystr::TinyAsciiStr;
 use writeable::Writeable;
@@ -1391,7 +1391,7 @@ impl<V: AbstractFormatter> CurrencyFormatter<V> {
                 patterns,
                 plural_rules,
             } => {
-                let operands = V::plural_operands(&formatted_value);
+                let operands = formatted_value.plural_operands();
                 let currency_str = extended.get().get(operands, plural_rules);
                 let pattern = patterns.get().get(operands, plural_rules);
                 (pattern, currency_str, rounded_value.sign)
