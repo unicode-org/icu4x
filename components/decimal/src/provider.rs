be1a183594841a9acc04a07025bccf9729160763
Younies Mahmoud <younies.mahmoud@gmail.com>
2026-07-29 15:30:31 +0000

    decimal: add custom decimal and grouping separator options to DecimalFormatter

--- components/decimal/src/provider.rs
+++ components/decimal/src/provider.rs
@@ -306,7 +306,7 @@ impl<'a> core::ops::Deref for DecimalSymbols<'a> {
 
 impl DecimalSymbols<'static> {
     /// Create a new en-US format for use in testing
-    #[cfg(feature = "datagen")]
+    #[cfg(any(test, feature = "datagen"))]
     pub fn new_en_for_testing() -> Self {
         let strings = DecimalSymbolStrsBuilder {
             minus_sign_prefix: VarZeroCow::new_borrowed("-"),
