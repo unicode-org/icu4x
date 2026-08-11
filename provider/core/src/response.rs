db7182ed98002dc7ebb830bebb50c74e4141ab6d
Shane F. Carr <shane@unicode.org>
2026-08-11 21:55:53 +0200

    Add more DataPayloadOr methods (#8346)
    ## Changelog
    
    icu_provider: add more methods to DataPayloadOr
    
    - New function: `DataPayloadOr::map_other`
    - New function: `DataPayloadOr::cast`

--- provider/core/src/response.rs
+++ provider/core/src/response.rs
@@ -990,6 +990,39 @@ where
             DataPayloadOrInner::Inner(DataPayloadOrInnerInner::Other(o)) => Err(o),
         }
     }
+
+    /// Maps the Other type to a new Other type.
+    #[inline]
+    pub fn map_other<O2>(self, f: impl FnOnce(O) -> O2) -> DataPayloadOr<M, O2> {
+        DataPayloadOr(match self.0 {
+            DataPayloadOrInner::Yoke(yoke) => DataPayloadOrInner::Yoke(yoke),
+            DataPayloadOrInner::Inner(DataPayloadOrInnerInner::StaticRef(r)) => {
+                DataPayloadOrInner::Inner(DataPayloadOrInnerInner::StaticRef(r))
+            }
+            DataPayloadOrInner::Inner(DataPayloadOrInnerInner::Other(o)) => {
+                DataPayloadOrInner::Inner(DataPayloadOrInnerInner::Other(f(o)))
+            }
+        })
+    }
+
+    /// Maps the Marker type to a compatible Marker type.
+    ///
+    /// See [`DataResponse::cast`].
+    #[inline]
+    pub fn cast<M2>(self) -> DataPayloadOr<M2, O>
+    where
+        M2: DynamicDataMarker<DataStruct = M::DataStruct>,
+    {
+        DataPayloadOr(match self.0 {
+            DataPayloadOrInner::Yoke(yoke) => DataPayloadOrInner::Yoke(yoke),
+            DataPayloadOrInner::Inner(DataPayloadOrInnerInner::StaticRef(r)) => {
+                DataPayloadOrInner::Inner(DataPayloadOrInnerInner::StaticRef(r))
+            }
+            DataPayloadOrInner::Inner(DataPayloadOrInnerInner::Other(o)) => {
+                DataPayloadOrInner::Inner(DataPayloadOrInnerInner::Other(o))
+            }
+        })
+    }
 }
 
 impl<M> DataPayloadOr<M, ()>
