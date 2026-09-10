52b2c29afd5536afa92f50081b6ccbfd4f26e90c
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-09-10 14:37:58 +0200

    Deprecate bies (#8475)
    ## Changelog
    
    N/A

--- tools/graveyard/src/lib.rs
+++ tools/graveyard/src/lib.rs
@@ -2,6 +2,4 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-//! This ICU4X crate is deprecated.
-//!
-//! Its functionality can now be found in the [`icu_segmenter`](https://docs.rs/icu_segmenter) crate.
+//! This crate is deprecated.
