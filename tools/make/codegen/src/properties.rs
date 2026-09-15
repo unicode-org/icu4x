80d122ed148c83dfdf9aeb76c663c2a100c1c443
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2026-09-15 16:17:45 +0200

    Move properties codegen sources (#8494)
    It's not test data.
    
    ## Changelog
    
    N/A

--- tools/make/codegen/src/properties.rs
+++ tools/make/codegen/src/properties.rs
@@ -105,8 +105,7 @@ static VARIANTS: LazyLock<BTreeMap<&str, BTreeMap<u32, (&str, &str, Vec<&str>, b
         let mut names = BTreeMap::<_, BTreeMap<_, _>>::new();
 
         for line in
-            include_str!("../../../../provider/source/tests/data/rscd/ucd/PropertyValueAliases.txt")
-                .lines()
+            include_str!("../../../../components/properties/data/PropertyValueAliases.txt").lines()
         {
             let line = line.split('#').next().unwrap().trim();
             if line.is_empty() {
@@ -141,8 +140,7 @@ static VARIANTS: LazyLock<BTreeMap<&str, BTreeMap<u32, (&str, &str, Vec<&str>, b
                 .insert(short_name, (long_name, aliases));
         }
 
-        for line in
-            include_str!("../../../../components/properties/tests/data/prop_numbers.txt").lines()
+        for line in include_str!("../../../../components/properties/data/prop_numbers.txt").lines()
         {
             let line = line.split('#').next().unwrap().trim();
             if line.is_empty() {
